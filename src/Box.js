3a6ca88252aa710da88b398dde0c5b2479f44449
Diana Mounter <broccolini@github.com>
2018-02-18 10:53:04 -0500

    add generic Page component with CSS, Meta, and Box components

---
+++ src/Box.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import {
+  space,
+  width,
+  flex,
+  color
+} from 'styled-system'
+
+const Box = styled.div`
+  ${space}
+  ${width}
+  ${flex}
+  ${color}
+`
+
+export default Box
---
+++ src/Meta.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Meta = () => (
+  <React.Fragment>
+    <meta name='viewport' content='width=device-width,initial-scale=1' />
+    <meta name='generator' content='Compositor X0' />
+    <link rel='icon' href='assets/favicon.png' />
+    <link rel='apple-touch-icon' href='assets/apple-touch-icon.png' />
+    <meta name='og:title' content='Primer React' />
+    <meta name='description' content='Primer components built with React.js.' />
+  </React.Fragment>
+)
+
+export default Meta
