091809ed47fc6fdb168816638d5919d5eea9f461
Diana Mounter <broccolini@github.com>
2018-02-18 10:50:01 -0500

    add basic theme file

---
+++ src/theme.js
@@ -0,0 +1,55 @@
+
+const theme = {
+  breakpoints: [
+    '544px',
+    '768px',
+    '1012px',
+    '1280px'
+  ],
+  fonts: [
+    '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'
+  ],
+  lineHeight: 1.5,
+  colors: {
+    bodytext: '#24292e',
+    black: '#1b1f23',
+    gray900: '#24292e',
+    blue500: '#0366d6',
+    green500: '#28a745',
+    yellow500: '#ffd33d',
+    orange500: '#f66a0a',
+    red500: '#d73a49',
+    purple500: '#6f42c1',
+    gray000: '#fafbfc',
+    gray100: '#f6f8fa',
+    gray200: '#e1e4e8',
+    gray300: '#d1d5da',
+    gray400: '#959da5',
+    white: '#fff',
+    blackfade15: `rgba(27, 31 ,35, 0.15)`,
+    whitefade15: `rgba(255, 255 ,255, 0.15)`
+  },
+  radii: [ 0, 3, 6 ],
+  space: [
+    0,
+    4,
+    8,
+    16,
+    24,
+    32,
+    40,
+    48
+  ],
+  fontSizes: [
+    12,
+    14,
+    16,
+    20,
+    24,
+    32,
+    40,
+    48
+  ]
+}
+
+export default theme
