5ca278efef45899cc4e665611d3ed7661c7cb46a
Diana Mounter <broccolini@github.com>
2018-02-18 23:33:52 -0500

    temporarily link to built primer styles:
    - this lets us use primer class names in components
    - linking to all modules except primer-base to avoid base overrides

--- src/Meta.js
+++ src/Meta.js
@@ -4,6 +4,19 @@ const Meta = () => (
   <React.Fragment>
     <meta name='viewport' content='width=device-width,initial-scale=1' />
     <meta name='generator' content='Compositor X0' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-box/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-breadcrumb/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-buttons/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-forms/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-layout/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-navigation/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-support/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-table/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-tooltips/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-truncate/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-utilities/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-product/build/build.css' />
+    <link rel='stylesheet' href='https://unpkg.com/primer-marketing/build/build.css' />
     <link rel='icon' href='assets/favicon.png' />
     <link rel='apple-touch-icon' href='assets/apple-touch-icon.png' />
     <meta name='og:title' content='Primer React' />
