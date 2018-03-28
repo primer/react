d1321620dcf9d886fcc49a847a236716bd82c601
Shawn Allen <shawn.allen@github.com>
2018-03-28 15:39:46 -0700

    :recycle: move unpkg package names into a list

--- src/Meta.js
+++ src/Meta.js
@@ -1,22 +1,28 @@
 import React from 'react'
 
+const primerPackages = [
+  'primer-box',
+  'primer-breadcrumb',
+  'primer-buttons',
+  'primer-forms',
+  'primer-layout',
+  'primer-marketing',
+  'primer-navigation',
+  'primer-product',
+  'primer-support',
+  'primer-table',
+  'primer-tooltips',
+  'primer-truncate',
+  'primer-utilities',
+]
+
 const Meta = () => (
   <React.Fragment>
     <meta name='viewport' content='width=device-width,initial-scale=1' />
     <meta name='generator' content='Compositor X0' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-box/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-breadcrumb/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-buttons/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-forms/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-layout/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-navigation/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-support/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-table/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-tooltips/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-truncate/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-utilities/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-product/build/build.css' />
-    <link rel='stylesheet' href='https://unpkg.com/primer-marketing/build/build.css' />
+    {primerPackages.map(pkg => (
+      <link rel='stylesheet' href={`https://unpkg.com/${pkg}/build/build.css`} />
+    ))}
     <link rel='icon' href='assets/favicon.png' />
     <link rel='apple-touch-icon' href='assets/apple-touch-icon.png' />
     <meta name='og:title' content='Primer React' />
