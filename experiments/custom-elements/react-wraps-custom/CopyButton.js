3c865ab3aaae61973188e015715c57d9f972b061
Shawn Allen <shawn.allen@github.com>
2018-05-09 13:57:57 -0700

    simplify even more

--- experiments/custom-elements/react-wraps-custom/CopyButton.js
+++ experiments/custom-elements/react-wraps-custom/CopyButton.js
@@ -1,8 +1,4 @@
 import React from 'react'
 import 'clipboard-copy-element'
 
-export default ({children, ...props}) => (
-  <clipboard-copy {...props}>
-    {children}
-  </clipboard-copy>
-)
+export default props => <clipboard-copy {...props}/>
