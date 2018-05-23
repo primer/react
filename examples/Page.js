4e685c176858a58a70b842feb0f93a3fb3e8ac57
Emily <emplums@github.com>
2018-05-23 11:26:26 -0700

    move example components to examples folder

--- examples/Page.js
+++ examples/Page.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import theme from './theme'
 import Meta from './Meta'
 import CSS from './CSS'
-import Box from './Box'
+import Box from '../src/Box'
 
 // Generic page wrapper component
 const Page = props => (
