6a4c059afb611efbfdefd90d4446b20458eed636
Shawn Allen <shawn.allen@github.com>
2018-05-24 13:05:51 -0700

    fix Flex reference

--- src/Flex.js
+++ src/Flex.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import mapWithClassnames from './system-classnames'
 
-const Box = props => <div {...mapWithClassnames(props, 'd-flex')} />
+const Flex = props => <div {...mapWithClassnames(props, 'd-flex')} />
 
 export default Flex
