c1ae95a36df1f8fd487ab21e65799df4ba897780
Shawn Allen <shawn.allen@github.com>
2018-05-24 13:47:50 -0700

    only export default primer props mapper

--- src/system-classnames.js
+++ src/system-classnames.js
@@ -1,12 +1,3 @@
 import map from 'system-classnames/primer'
-import classnames from 'classnames'
 
-const mapWithClassnames = (props, classes) => {
-  const {className, ...mapped} = map(props)
-  const otherClasses = (typeof classes === 'function')
-    ? classes(props)
-    : classes
-  return Object.assign(mapped, {className: otherClasses})
-}
-
-export default mapWithClassnames
+export default map
