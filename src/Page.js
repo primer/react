c3ca73d2b4c5b6bc53d7c5d8378efd877af2ad32
Shawn Allen <shawn.allen@github.com>
2018-05-24 13:30:59 -0700

    output <body> explicitly

--- src/Page.js
+++ src/Page.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import theme from './theme'
-import Meta from './Meta'
-import CSS from './CSS'
 import Box from './Box'
+import CSS from './CSS'
+import Meta from './Meta'
+import React from 'react'
+import theme from './theme'
+import {ThemeProvider} from 'styled-components'
 
 // Generic page wrapper component
 const Page = props => (
@@ -14,9 +14,11 @@ const Page = props => (
       <CSS />
     </head>
     <ThemeProvider theme={theme}>
-      <Box color='bodytext'>
-        {props.children}
-      </Box>
+      <body>
+        <Box text='bodytext'>
+          {props.children}
+        </Box>
+      </body>
     </ThemeProvider>
   </React.Fragment>
 )
