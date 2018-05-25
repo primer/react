2c7d02135bcf3c85f59f0881b9132d29dde9cafe
Shawn Allen <shawn.allen@github.com>
2018-05-25 15:10:45 -0700

    tweak

--- src/chameleon.js
+++ src/chameleon.js
@@ -3,10 +3,7 @@ import React from 'react'
 export default function chameleon(defaultTag, mapProps, castable) {
   const Component = ({tag: Tag = defaultTag, ...props}) => <Tag {...mapProps(props)} />
   if (castable) {
-    Component.withComponent = (tag, recastable) => {
-      const Casted = chameleon(tag, mapProps, recastable)
-      return Casted
-    }
+    Component.withComponent = (tag, recastable) => chameleon(tag, mapProps, recastable)
   }
   return Component
 }
