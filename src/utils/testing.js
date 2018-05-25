12d0134ef4799122a9dc4b3deefaff99bf69aefc
Shawn Allen <shawn.allen@github.com>
2018-05-25 15:13:53 -0700

    add docs for testing utils

--- src/utils/testing.js
+++ src/utils/testing.js
@@ -1,9 +1,29 @@
 import renderer from 'react-test-renderer'
 
+/**
+ * Render the component (a React.createElement() or JSX expression)
+ * into its intermediate object representation with 'type',
+ * 'props', and 'children' keys
+ *
+ * The returned object can be matched with expect().toEqual(), e.g.
+ *
+ * ```js
+ * expect(render(<Foo />)).toEqual(render(<div foo='bar' />))
+ * ```
+ */
 export function render(component) {
   return renderer.create(component).toJSON()
 }
 
+/**
+ * Get the HTML class names rendered by the component instance
+ * as an array.
+ *
+ * ```js
+ * expect(renderClasses(<div className='a b' />))
+ *   .toEqual(['a', 'b'])
+ * ```
+ */
 export function renderClasses(component) {
   const {props: {className}} = render(component)
   return className ? className.trim().split(' ') : []
