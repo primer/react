4ab393df58e795ecbcc45b2b7ba5d4919048a170
Shawn Allen <shawn.allen@github.com>
2018-05-25 15:08:28 -0700

    be more explicit about defaultProps

--- src/chameleon.js
+++ src/chameleon.js
@@ -3,9 +3,8 @@ import React from 'react'
 export default function chameleon(defaultTag, mapProps, castable) {
   const Component = ({tag: Tag = defaultTag, ...props}) => <Tag {...mapProps(props)} />
   if (castable) {
-    Component.withComponent = (tag, recastable, members) => {
+    Component.withComponent = (tag, recastable) => {
       const Casted = chameleon(tag, mapProps, recastable)
-      if (members) Object.assign(Casted, members)
       return Casted
     }
   }
