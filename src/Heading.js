ff1b1dac6144bac4ed47d86b8ec854615c8612eb
Shawn Allen <shawn.allen@github.com>
2018-05-25 14:38:19 -0700

    refactor Heading defaultProps logic

--- src/Heading.js
+++ src/Heading.js
@@ -2,18 +2,13 @@ import React from 'react'
 import Text from './Text'
 
 const Heading = Text.withComponent('h1', true)
+Heading.defaultProps = {m: 0, fontSize: 5}
 
-Heading.defaultProps = {
-  fontSize: 5,
-  m: 0
-}
-
-const {defaultProps} = Heading
-Heading.h1 = Heading // .withComponent('h1') is redundant
-Heading.h2 = Heading.withComponent('h2', {defaultProps})
-Heading.h3 = Heading.withComponent('h3', {defaultProps})
-Heading.h4 = Heading.withComponent('h4', {defaultProps})
-Heading.h5 = Heading.withComponent('h5', {defaultProps})
-Heading.h6 = Heading.withComponent('h6', {defaultProps})
+Heading.h1 = Heading
+Heading.h2 = Heading.withComponent('h2', true, {defaultProps: {m: 0, fontSize: 4}})
+Heading.h3 = Heading.withComponent('h3', true, {defaultProps: {m: 0, fontSize: 3}})
+Heading.h4 = Heading.withComponent('h4', true, {defaultProps: {m: 0, fontSize: 2}})
+Heading.h5 = Heading.withComponent('h5', true, {defaultProps: {m: 0, fontSize: 1}})
+Heading.h6 = Heading.withComponent('h6', true, {defaultProps: {m: 0, fontSize: 0}})
 
 export default Heading
