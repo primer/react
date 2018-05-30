17e50150d8d0b92c24fc22cf6105ef8eb7515b86
Shawn Allen <shawn.allen@github.com>
2018-05-30 16:11:46 -0700

    clean up Avatar tests, add getImageURL() check

--- src/__tests__/Avatar.js
+++ src/__tests__/Avatar.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Avatar, {IMAGE_BASE_URL} from '../Avatar'
+import Avatar, {IMAGE_BASE_URL, getImageURL} from '../Avatar'
 import renderer from 'react-test-renderer'
 
 const render = component => renderer.create(component).toJSON()
 
 describe('Avatar', () => {
-  const urlFor = (username, size) => `${IMAGE_BASE_URL}primer?v=3&s=${size}`
+  const urlFor = (username, size) => `${IMAGE_BASE_URL}${username}?v=3&s=${size}`
   it('renders small by default', () => {
     expect(render(<Avatar username='primer'/>))
       .toEqual(render(<img className='avatar avatar-small' src={`${IMAGE_BASE_URL}primer?v=3&s=40`} width={20} height={20} alt='primer' />))
@@ -21,5 +21,10 @@ describe('Avatar', () => {
   it('respects isChild', () => {
     expect(render(<Avatar username='primer' isChild />))
       .toEqual(render(<img className='avatar avatar-small avatar-child' src={urlFor('primer', 40)} width={20} height={20} alt='primer' />))
+  })
+
+  it('exports getImageURL() and supports src prop', () => {
+    expect(render(<Avatar username='primer' src={getImageURL('primer', {v: 4, size: 100})} />))
+      .toEqual(render(<img className='avatar avatar-small' src={`${IMAGE_BASE_URL}primer?v=4&size=100`} width={20} height={20} alt='primer' />))
   })
 })
