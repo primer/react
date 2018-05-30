cb5f06249f2d1b90bb4f35e4fa18b016a2fc9b6f
Shawn Allen <shawn.allen@github.com>
2018-05-30 16:46:36 -0700

    export Details from src/index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ export { default as ButtonPrimary } from './ButtonPrimary'
 export { default as ButtonOutline } from './ButtonOutline'
 export { default as ButtonLink } from './ButtonLink'
 
+export { default as Details } from './Details'
+
 export { default as Heading } from './Heading'
 export { default as Label } from './Label'
 export { default as Link } from './Link'
