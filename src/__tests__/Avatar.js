7482500c5d95040c40e0a57c8aef5546a2d0bfb5
Shawn Allen <shawn.allen@github.com>
2018-05-30 16:04:08 -0700

    refactor Avatar tests

--- src/__tests__/Avatar.js
+++ src/__tests__/Avatar.js
@@ -4,11 +4,22 @@ import renderer from 'react-test-renderer'
 
 const render = component => renderer.create(component).toJSON()
 
-it('Avatar does its thing', () => {
-  expect(render(<Avatar username='primer'/>))
-    .toEqual(render(<img className='avatar avatar-small' src={`${IMAGE_BASE_URL}primer?v=3&s=40`} width={20} height={20} alt='primer' />))
-  expect(render(<Avatar username='primer' size={40} />))
-    .toEqual(render(<img className='avatar' src={`${IMAGE_BASE_URL}primer?v=3&s=80`} width={40} height={40} alt='primer' />))
-  expect(render(<Avatar username='primer' baseURL='/' />))
-    .toEqual(render(<img className='avatar avatar-small' src={`/primer?v=3&s=40`} width={20} height={20} alt='primer' />))
+describe('Avatar', () => {
+  const urlFor = (username, size) => `${IMAGE_BASE_URL}primer?v=3&s=${size}`
+  it('renders small by default', () => {
+    expect(render(<Avatar username='primer'/>))
+      .toEqual(render(<img className='avatar avatar-small' src={`${IMAGE_BASE_URL}primer?v=3&s=40`} width={20} height={20} alt='primer' />))
+  })
+
+  it('respects the size prop', () => {
+    expect(render(<Avatar username='primer' size={40} />))
+      .toEqual(render(<img className='avatar' src={urlFor('primer', 80)} width={40} height={40} alt='primer' />))
+    expect(render(<Avatar username='primer' src='/primer.jpg' />))
+      .toEqual(render(<img className='avatar avatar-small' src='/primer.jpg' width={20} height={20} alt='primer' />))
+  })
+
+  it('respects isChild', () => {
+    expect(render(<Avatar username='primer' isChild />))
+      .toEqual(render(<img className='avatar avatar-small avatar-child' src={urlFor('primer', 40)} width={20} height={20} alt='primer' />))
+  })
 })
