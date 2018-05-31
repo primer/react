d909ccceff46ee831f60a1c6c0c9f30be8d7d6d1
Shawn Allen <shawn.allen@github.com>
2018-05-31 14:30:05 -0700

    dramatically simplify Avatar, nix username prop and URL stuff

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,48 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import URLSearchParams from 'url-search-params'
-
-const IMAGE_BASE_URL = 'https://avatars.githubusercontent.com/'
-
-function getImageURL(username, params) {
-  const query = params ? '?' + new URLSearchParams(params) : ''
-  return `${IMAGE_BASE_URL}${username}${query}`
-}
 
 const Avatar = props => {
   const {
-    username = 'github',
-    size = 20,
+    alt,
     isChild,
+    size = 20,
+    src,
     ...rest
   } = props
 
-  const params = {
-    v: 3, // XXX is this necessary?
-    s: size * 2
-  }
-
-  const {
-    alt = username,
-    src = getImageURL(username, params)
-  } = rest
+  const className = classnames(
+    'avatar',
+    {
+      'avatar-small': size <= 24,
+      'avatar-child': isChild,
+    }
+  )
 
   return (
     <img
-      className={classnames(
-        'avatar',
-        {
-          'avatar-small': size <= 24,
-          'avatar-child': isChild,
-        }
-      )}
-      src={src}
+      className={className}
       alt={alt}
+      src={src}
       width={size}
       height={size}
     />
   )
 }
 
+Avatar.propTypes = {
+  alt: PropTypes.string,
+  isChild: PropTypes.bool,
+  size: PropTypes.number,
+  src: PropTypes.string,
+}
+
 export default Avatar
-export {IMAGE_BASE_URL, getImageURL}
--- src/__tests__/Avatar.js
+++ src/__tests__/Avatar.js
@@ -1,30 +1,27 @@
 import React from 'react'
-import Avatar, {IMAGE_BASE_URL, getImageURL} from '../Avatar'
+import Avatar from '../Avatar'
 import renderer from 'react-test-renderer'
 
 const render = component => renderer.create(component).toJSON()
 
 describe('Avatar', () => {
-  const urlFor = (username, size) => `${IMAGE_BASE_URL}${username}?v=3&s=${size}`
   it('renders small by default', () => {
-    expect(render(<Avatar username='primer'/>))
-      .toEqual(render(<img className='avatar avatar-small' src={`${IMAGE_BASE_URL}primer?v=3&s=40`} width={20} height={20} alt='primer' />))
+    expect(render(<Avatar />))
+      .toEqual(render(<img className='avatar avatar-small' width={20} height={20} />))
   })
 
   it('respects the size prop', () => {
-    expect(render(<Avatar username='primer' size={40} />))
-      .toEqual(render(<img className='avatar' src={urlFor('primer', 80)} width={40} height={40} alt='primer' />))
-    expect(render(<Avatar username='primer' src='/primer.jpg' />))
-      .toEqual(render(<img className='avatar avatar-small' src='/primer.jpg' width={20} height={20} alt='primer' />))
+    expect(render(<Avatar size={40} />))
+      .toEqual(render(<img className='avatar' width={40} height={40} />))
   })
 
   it('respects isChild', () => {
-    expect(render(<Avatar username='primer' isChild />))
-      .toEqual(render(<img className='avatar avatar-small avatar-child' src={urlFor('primer', 40)} width={20} height={20} alt='primer' />))
+    expect(render(<Avatar isChild />))
+      .toEqual(render(<img className='avatar avatar-small avatar-child' width={20} height={20} />))
   })
 
-  it('exports getImageURL() and supports src prop', () => {
-    expect(render(<Avatar username='primer' src={getImageURL('primer', {v: 4, size: 100})} />))
-      .toEqual(render(<img className='avatar avatar-small' src={`${IMAGE_BASE_URL}primer?v=4&size=100`} width={20} height={20} alt='primer' />))
+  it('passes through the src prop', () => {
+    expect(render(<Avatar src='primer.png' />))
+      .toEqual(render(<img className='avatar avatar-small' src='primer.png' width={20} height={20} />))
   })
 })
