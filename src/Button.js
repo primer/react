a1ab3d9e7556e5bb77e5bcd6509acf0ab1d1f51d
Shawn Allen <shawn.allen@github.com>
2018-05-31 11:24:26 -0700

    reorder props alphabetically, tidy

--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import classnames from 'classnames'
 
-const Button = ({ size, disabled, block, linkStyle, onClick, children }) => (
-    <button
-      disabled={disabled}
-      onClick={onClick}
-      type="button"
-      className={classnames(
-        {
-          'btn': !linkStyle,
-          'btn-link': linkStyle,
-          'btn-sm': size === 'small',
-          'btn-large': size === 'large',
-          'btn-block': block,
-        }
-      )}
-    >
-      {children}
-    </button>
+const Button = ({ block, children, disabled, linkStyle, onClick, size }) => (
+  <button
+    disabled={disabled}
+    onClick={onClick}
+    type="button"
+    className={classnames(
+      {
+        'btn': !linkStyle,
+        'btn-link': linkStyle,
+        'btn-sm': size === 'small',
+        'btn-large': size === 'large',
+        'btn-block': block,
+      }
+    )}
+  >
+    {children}
+  </button>
 )
 
 export default Button
