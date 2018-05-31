95829d28212ef4d710ff7aa98b3d5434902a9c85
Shawn Allen <shawn.allen@github.com>
2018-05-31 11:37:14 -0700

    flesh out Button scheme prop, tidy

--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,40 @@
 import React from 'react'
 import classnames from 'classnames'
 
-const Button = ({ block, children, disabled, linkStyle, onClick, size }) => (
-  <button
-    disabled={disabled}
-    onClick={onClick}
-    type="button"
-    className={classnames(
-      {
-        'btn': !linkStyle,
-        'btn-link': linkStyle,
-        'btn-sm': size === 'small',
-        'btn-large': size === 'large',
-        'btn-block': block,
-      }
-    )}
-  >
-    {children}
-  </button>
-)
+const Button = props => {
+  const {
+    block,
+    children,
+    disabled,
+    linkStyle,
+    onClick,
+    scheme,
+    size
+  } = props
+
+  const className = classnames(
+    {
+      'btn': !linkStyle,
+      'btn-link': linkStyle
+    },
+    scheme && `btn-${scheme}`,
+    {
+      'btn-sm': size === 'small',
+      'btn-large': size === 'large',
+      'btn-block': block,
+    }
+  )
+
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      onClick={disabled ? undefined : onClick}
+      className={className}
+    >
+      {children}
+    </button>
+  )
+}
 
 export default Button
