ba63825f949a344e97e5bd3b65e5bfb8ae5fd65a
Shawn Allen <shawn.allen@github.com>
2018-06-01 09:54:18 -0700

    add Component.withProps() in chameleon()

--- src/chameleon.js
+++ src/chameleon.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {componentWithProps} from './props'
 
 export default function chameleon(defaultTag, mapProps, castable) {
   const Component = ({tag: Tag = defaultTag, ...props}) => <Tag {...mapProps(props)} />
   if (castable) {
     Component.withComponent = (tag, recastable) => chameleon(tag, mapProps, recastable)
   }
+  Component.withProps = props => componentWithProps(Component, props)
   return Component
 }
