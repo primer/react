ec2e10e7ad8a1e50f297c4b7bafdbe29e0560b02
Shawn Allen <shawn.allen@github.com>
2018-06-05 12:01:20 -0700

    more DonutChart tests

--- src/__tests__/DonutChart.js
+++ src/__tests__/DonutChart.js
@@ -21,7 +21,22 @@ describe('DonutChart', () => {
     expect(slice.props.fill).toEqual(theme.colors.red[5])
   })
 
-  xit('renders DonutSlice children', () => {
+  it('renders DonutSlice children', () => {
+    const donut = render((
+      <DonutChart>
+        <DonutSlice state='success' value={1} />
+        <DonutSlice state='failure' value={1} />
+      </DonutChart>
+    ))
+    expect(donut.children.length).toEqual(1)
+    const slices = donut.children[0].children
+    expect(slices.length).toEqual(2)
+    expect(slices.map(slice => slice.type)).toEqual(['path', 'path'])
+    expect(slices[0].props.fill).toEqual(theme.colors.green[5])
+    expect(slices[1].props.fill).toEqual(theme.colors.red[5])
+  })
+
+  it('renders a single DonutSlice child', () => {
     const donut = render((
       <DonutChart>
         <DonutSlice state='success' value={1} />
