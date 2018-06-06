df6b0f7e8766234fffbde0011bad0ea8a20a16a2
Emily <emplums@github.com>
2018-06-06 11:44:41 -0700

    bring back in other button options

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import classnames from 'classnames'
 
-function Button({is: Tag = 'button', children, grouped, scheme, ...props}) {
-  const className = ['btn']
+function Button({is: Tag = 'button', children, size, block, linkStyle,  grouped, scheme, ...props}) {
+  const className = [linkStyle ? 'btn-link' : 'btn']
   if (scheme) className.push(`btn-${scheme}`)
   if (grouped) className.push('BtnGroup-item')
+  if (size === "small") className.push('btn-sm')
+  if (size === "large") className.push('btn-large')
+  if (block) className.push('block')
   return (
     <Tag {...props} className={className.join(' ')}>{children}
     </Tag>
