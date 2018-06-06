a5652e911a99d74a0ce4640996aa8b3098559773
Shawn Allen <shawn.allen@github.com>
2018-06-06 16:20:13 -0700

    more/better Caret examples

--- examples/index.js
+++ examples/index.js
@@ -126,8 +126,14 @@ const Index = props => (
       <Example name='Caret'>
         <Block p={4}>
           {['top', 'right', 'bottom', 'left'].map((edge, i) => (
-            <Box p={2} mb={4} position='relative' maxWidth={200} key={i}>
-              edge={edge}
+            <Box p={2} mb={4} position='relative' maxWidth={400} key={i}>
+              <Text mono>edge='{edge}'</Text>
+              <Caret edge={edge} />
+            </Box>
+          ))}
+          {['top', 'right', 'bottom', 'left'].map((edge, i) => (
+            <Box shadow='medium' p={2} mb={4} position='relative' maxWidth={400} key={i}>
+              <Text mono>edge={edge} over shadow='medium'</Text>
               <Caret edge={edge} />
             </Box>
           ))}
