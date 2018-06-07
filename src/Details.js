6c15a2baa191747aacc71e35af34a38c6ead3537
Emily <emplums@github.com>
2018-06-06 17:59:03 -0700

    remove uncessary classnames import

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
 
 function getRenderer(children) {
   return typeof children === 'function' ? children : () => children
