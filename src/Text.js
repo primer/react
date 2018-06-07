7bd9ead66f9885e6d6e35e36bd15ead7fb8cd51b
Shawn Allen <shawn.allen@github.com>
2018-06-07 13:34:41 -0700

    add mono and nowrap props to Text

--- src/Text.js
+++ src/Text.js
@@ -15,6 +15,8 @@ const classifyTextProps = classifier({
   }, value => `f${value}`, true),
   fontWeight: value => `text-${value}`,
   lineHeight: value => `lh-${value}`,
+  mono: 'text-mono',
+  nowrap: 'no-wrap'
 })
 
 const textProps = props => classifyTextProps(map(props))
