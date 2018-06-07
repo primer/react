d1fab586db8ee806ccd7fe841e9c9c6032499536
Shawn Allen <shawn.allen@github.com>
2018-06-07 15:39:01 -0700

    add CaretBox to coordinate bg and border colors

---
+++ src/CaretBox.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Box from './Box'
+import Caret from './Caret'
+import {colors} from './theme'
+
+const borderValuesByName = {
+  ...colors.border,
+  true: colors.gray[2], // default
+}
+
+const bgValuesByName = colors.bg
+
+export default function CaretBox(props) {
+  const {
+    bg,
+    border,
+    caret: edge,
+    children,
+    ...boxProps
+  } = props
+
+  const borderColorName = Array.isArray(border)
+    ? border.filter(value => (value in borderValuesByName)).pop()
+    : border
+
+  const caretProps = {
+    edge,
+    borderColor: borderValuesByName[borderColorName],
+    fill: bgValuesByName[bg]
+  }
+
+  return (
+    <Box {...boxProps} bg={bg} border={border}>
+      {children}
+      <Caret {...caretProps} />
+    </Box>
+  )
+}
+
+CaretBox.propTypes = {
+  ...Box.propTypes,
+  caret: Caret.propTypes.edge
+}
+
+CaretBox.defaultProps = {
+  ...Box.defaultProps,
+  position: 'relative'
+}
