c03193995124c3f459ecb0d6ed30af34b750443b
Emily <emplums@github.com>
2018-06-07 14:49:40 -0700

    replace arrow with css arrow

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -4,14 +4,26 @@ import Details from './Details'
 import Button from './Button'
 import Caret from './Caret'
 
+const arrowStyles = {
+  content: '',
+  border: '4px solid',
+  borderRightColor: 'transparent',
+  borderLeftColor: 'transparent',
+  borderBottomColor: 'transparent',
+  width: '0',
+  height: '0'
+}
+
 const Dropdown = ({ title, scheme, children}) => (
   <div className='BtnGroup'>
     <Details className='details-reset BtnGroup-form d-flex'>
       {({open, toggle}) => (
         <React.Fragment>
-          <Button tag='summary' scheme={scheme} grouped onClick={toggle}>{title} ▼</Button>
+          <Button tag='summary' scheme={scheme} grouped onClick={toggle}>
+            {title} <div className='d-inline-block v-align-middle' style={arrowStyles}/>
+          </Button>
           <div className='border box-shadow position-absolute px-3 py-2 bg-white mt-1 rounded-1 list-style-none' style={{zIndex: 99999}}>
-            <div className="position-absolute ml-0 mt-n2"><Caret edge={'top'} align={'start'} /></div>
+            <div className='position-absolute ml-0 mt-n2'><Caret edge={'top'} align={'start'} /></div>
             {children}
           </div>
         </React.Fragment>
