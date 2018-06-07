f9ac705e5d9c4a1787ebf0847fb21d02aa2fd570
Jon Rohan <rohan@github.com>
2018-06-07 14:10:48 -0700

    Updating output

--- src/__tests__/TextInput.js
+++ src/__tests__/TextInput.js
@@ -5,21 +5,21 @@ import {render} from '../utils/testing'
 describe('TextInput', () => {
   it('renders', () => {
     expect(render(<TextInput name='zipcode' />))
-      .toEqual(render(<input name='zipcode' type='text' className='form-control' autocomplete='off' />))
+      .toEqual(render(<input name='zipcode' type='text' className='form-control' />))
   })
 
   it('renders small', () => {
     expect(render(<TextInput name='zipcode' size='small' />))
-      .toEqual(render(<input name='zipcode' type='text' className='form-control input-sm' autocomplete='off' />))
+      .toEqual(render(<input name='zipcode' type='text' className='form-control input-sm' />))
   })
 
   it('renders large', () => {
     expect(render(<TextInput name='zipcode' size='large' />))
-      .toEqual(render(<input name='zipcode' type='text' className='form-control input-lg' autocomplete='off' />))
+      .toEqual(render(<input name='zipcode' type='text' className='form-control input-lg' />))
   })
 
   it('renders block', () => {
     expect(render(<TextInput name='zipcode' block />))
-      .toEqual(render(<input name='zipcode' type='text' className='form-control input-block' autocomplete='off' />))
+      .toEqual(render(<input name='zipcode' type='text' className='form-control input-block' />))
   })
 })
