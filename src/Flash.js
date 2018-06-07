a88b6337a86a96c85c043cf347747702095b2a00
Shawn Allen <shawn.allen@github.com>
2018-06-07 13:27:45 -0700

    tidy up Flash, don't pass props to HTML

--- src/Flash.js
+++ src/Flash.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import classnames from 'classnames'
 
-const Flash = props => (
-    <div
-      {...props}
-      className={classnames(
-        props.className,
-        'flash', {
-          'flash-full': props.full,
-          'flash-warn': props.yellow,
-          'flash-error': props.red,
-          'flash-success': props.green,       }
-      )}
-    />
+const Flash = ({children, className, full, yellow, red, green}) => (
+  <div
+    className={classnames(
+      className,
+      'flash', {
+        'flash-full': full,
+        'flash-warn': yellow,
+        'flash-error': red,
+        'flash-success': green
+      }
+    )}>
+    {children}
+  </div>
 )
 
 export default Flash
