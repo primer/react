19ae9e302f54f267144aaa90db6e7fdf6bbd6523
Emily <emplums@github.com>
2018-06-14 13:01:30 -0700

    fix dropdown items

--- src/MergeButton.js
+++ src/MergeButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classnames from 'classnames'
 import Details from './Details'
 import Button from './Button'
-import Caret from './Caret'
+import CaretBox from './CaretBox'
 import Text from './Text'
 
 const arrowStyles = {
@@ -31,22 +31,23 @@ const MergeButton = ({ scheme, onClick, numCommits, children}) => (
           <Button tag='summary' scheme={scheme} onClick={toggle} style={borderStyles}>
             <div className='d-inline-block v-align-middle' style={arrowStyles}/>
           </Button>
-          <div className='border box-shadow position-absolute px-3 py-2 bg-white mt-1 rounded-1 list-style-none' style={{zIndex: 99999}}>
-            <div className='position-absolute ml-0 mt-n2'><Caret edge={'top'} align={'start'} /></div>
-            <ul>
-              <li>
-                <Text tag={'p'} fontWeight={600}>Create a merge commit</Text>
-                <Text tag={'p'}>All commits from this branch will be added to the base branch via a merge commit.</Text>
-              </li>
-              <li>
-                <Text tag={'p'} fontWeight={600}>Squash and merge</Text>
-                <Text tag={'p'}>The {numCommits} from this branch will be combined into one commit in the base branch.</Text>
-              </li>
-              <li>
-                <Text tag={'p'} fontWeight={600}>Rebase and merge</Text>
-                <Text tag={'p'}>The ${numCommits} from this branch will be rebased and added to the base branch</Text>
-              </li>
-            </ul>
+          <div className='position-absolute mt-1' style={{zIndex: 99999}}>
+            <CaretBox caret={'top-left'}>
+              <ul className='list-style-none p-0 m-0'>
+                <li className={'border-bottom p-2'}>
+                  <Text tag={'p'} fontWeight={'bold'}>Create a merge commit</Text>
+                  <Text tag={'p'}>All commits from this branch will be added to the base branch via a merge commit.</Text>
+                </li>
+                <li className={'border-bottom p-2'}>
+                  <Text tag={'p'} fontWeight={'bold'}>Squash and merge</Text>
+                  <Text tag={'p'}>The {numCommits} from this branch will be combined into one commit in the base branch.</Text>
+                </li>
+                <li className={'p-2'}>
+                  <Text tag={'p'} fontWeight={'bold'}>Rebase and merge</Text>
+                  <Text tag={'p'}>The {numCommits} from this branch will be rebased and added to the base branch</Text>
+                </li>
+              </ul>
+            </CaretBox>
           </div>
         </React.Fragment>
       )}
