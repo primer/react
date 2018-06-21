ff22096dbfa6c086067d4a9a147d5f002b58e1e4
Emily <emplums@github.com>
2018-06-21 14:45:39 -0700

    Revert "update paths"
    This reverts commit f1526a7c3e0b8825dc41252cd24f90967bbd9802.

--- examples/DemoPage.js
+++ examples/DemoPage.js
@@ -26,7 +26,7 @@ const examples = [
 const DemoPage = () => {
   return (
     <Library
-      basename='/primer-react/docs/demos'
+      basename='/docs/demos'
       title='Demo Library'
       examples={examples}
       renderSideNav={({
--- examples/docs.js
+++ examples/docs.js
@@ -8,14 +8,14 @@ const Index = props => (
   <div>
     <nav className='UnderlineNav'>
       <div className='UnderlineNav-body'>
-        <NavLink to='/primer-react/docs/components' className='UnderlineNav-item no-underline'>primer-react</NavLink>
-        <NavLink to='/primer-react/docs/demos' className='UnderlineNav-item no-underline'>Demos</NavLink>
-        <NavLink to='/primer-react/docs/sandbox' className='UnderlineNav-item no-underline'>Sandbox</NavLink>
+        <NavLink to='/docs/components' className='UnderlineNav-item no-underline'>primer-react</NavLink>
+        <NavLink to='/docs/demos' className='UnderlineNav-item no-underline'>Demos</NavLink>
+        <NavLink to='/docs/sandbox' className='UnderlineNav-item no-underline'>Sandbox</NavLink>
       </div>
     </nav>
-    <Route path='/primer-react/docs/demos' component={DemoPage} />
-    <Route path='/primer-react/docs/components' component={ComponentPage} />
-    <Route path='/primer-react/docs/sandbox' component={Sandbox} />
+    <Route path='/docs/demos' component={DemoPage} />
+    <Route path='/docs/components' component={ComponentPage} />
+    <Route path='/docs/sandbox' component={Sandbox} />
   </div>
 )
 
