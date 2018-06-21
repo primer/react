52a01f4208aba0eea2b8949344913025b6171a19
Emily <emplums@github.com>
2018-06-21 13:29:51 -0700

    tweak routes

--- examples/docs.js
+++ examples/docs.js
@@ -8,14 +8,14 @@ const Index = props => (
   <div>
     <nav className='UnderlineNav'>
       <div className='UnderlineNav-body'>
-        <NavLink to='/docs/primer-react' className='UnderlineNav-item no-underline' activeClassName='selected'>primer-react</NavLink>
-        <NavLink to='/docs/demos' className='UnderlineNav-item no-underline' activeClassName='selected'>Demos</NavLink>
-        <NavLink to='/docs/sandbox' className='UnderlineNav-item no-underline' activeClassName='selected'>Sandbox</NavLink>
+        <NavLink to='/components' className='UnderlineNav-item no-underline' activeClassName='selected'>primer-react</NavLink>
+        <NavLink to='/demos' className='UnderlineNav-item no-underline' activeClassName='selected'>Demos</NavLink>
+        <NavLink to='/sandbox' className='UnderlineNav-item no-underline' activeClassName='selected'>Sandbox</NavLink>
       </div>
     </nav>
-    <Route path='/docs/demos' component={DemoPage} />
-    <Route path='/docs/primer-react' component={ComponentPage} />
-    <Route path='/docs/sandbox' component={Sandbox} />
+    <Route path='/demos' component={DemoPage} />
+    <Route path='/components' component={ComponentPage} />
+    <Route path='/sandbox' component={Sandbox} />
   </div>
 )
 
