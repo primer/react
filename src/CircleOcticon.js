ca7970bd93d49ca50c3e37036bd881e8e0aacdfe
Shawn Allen <shawn.allen@github.com>
2018-06-27 16:06:15 -0700

    update CircleIcon with new Octicon API

--- src/CircleOcticon.js
+++ src/CircleOcticon.js
@@ -3,21 +3,21 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import Octicon from '@github/octicons-react'
 
-const CircleOcticon = ({name, size, bg, color}) => {
-  const wrapperClasses = [
+const CircleOcticon = ({size, bg, color, ...rest}) => {
+  const className = classnames(
     'circle d-flex flex-items-center flex-justify-center',
     bg ? `bg-${bg}` : null,
     color ? `text-${color}` : null
-  ]
+  )
   return (
-    <div style={{width: `${size}px`, height: `${size}px`}} className={classnames(wrapperClasses)}>
-      <Octicon name={name} size={size} />
+    <div style={{width: `${size}px`, height: `${size}px`}} className={className}>
+      <Octicon size={size} {...rest} />
     </div>
   )
 }
 
 CircleOcticon.propTypes = {
-  name: PropTypes.string,
+  ...Octicon.propTypes,
   size: PropTypes.number
 }
 
