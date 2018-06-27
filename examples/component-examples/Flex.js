51d23554f63a3d8bef333778b0e88c3c4c607bc8
Emily <emplums@github.com>
2018-06-27 15:48:35 -0700

    add flexItem tests and examples

--- examples/component-examples/Flex.js
+++ examples/component-examples/Flex.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { PropsForm } from '@compositor/kit'
-import { FlexContainer, FlexItem, Block, Box, Text, theme } from  '../../src'
+import { FlexContainer, FlexItem, Block, Text, theme } from  '../../src'
 import ExampleHeading from '../doc-components/ExampleHeading'
 
 const propObj = {
@@ -42,6 +42,30 @@ const FlexExample = {
             </PropsForm.Select>
           )}
         </PropsForm>
+        <ExampleHeading mt={2}>FlexContainer + FlexItems set to flexAuto</ExampleHeading>
+        <FlexContainer wrap='nowrap' width={300} height={300} border={true}>
+          <FlexItem flexAuto>
+            <Block p={3} bg='blue'>Item 1</Block>
+          </FlexItem>
+          <FlexItem flexAuto>
+            <Block p={3} bg='green'>Item 2</Block>
+          </FlexItem>
+          <FlexItem flexAuto>
+            <Block p={3} bg='yellow'>Item 3</Block>
+          </FlexItem>
+        </FlexContainer>
+        <ExampleHeading mt={2}>FlexContainer + FlexItems with first item set to alignSelf='center'</ExampleHeading>
+        <FlexContainer wrap='nowrap' width={300} height={300} border={true}>
+          <FlexItem alignSelf='center'>
+            <Block p={3} bg='blue'>Item 1</Block>
+          </FlexItem>
+          <FlexItem>
+            <Block p={3} bg='green'>Item 2</Block>
+          </FlexItem>
+          <FlexItem>
+            <Block p={3} bg='yellow'>Item 3</Block>
+          </FlexItem>
+        </FlexContainer>
     </Block>
   )
 }
---
+++ src/__tests__/FlexItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FlexItem from '../FlexItem'
+import {render, renderClasses} from '../utils/testing'
+
+it('FlexItem renders alignSelf classes', () => {
+  expect(renderClasses(<FlexItem alignSelf={'center'} />))
+    .toEqual(['flex-self-center'])
+})
+
+it('FlexItem renders flexAuto class', () => {
+  expect(renderClasses(<FlexItem flexAuto />))
+    .toEqual(['flex-auto'])
+})
