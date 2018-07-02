5dcbf93968763a6dac0869b01f97450b011a59b2
Shawn Allen <shawn.allen@github.com>
2018-07-02 15:39:33 -0700

    eslint-disable some jsx-a11y rules in Details tests

--- src/__tests__/Details.js
+++ src/__tests__/Details.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
 import React from 'react'
 import Details from '../Details'
 import {mount, render} from '../utils/testing'
