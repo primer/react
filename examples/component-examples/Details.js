77dcb27b30850705f89d55c33eaaae229f613fea
Shawn Allen <shawn.allen@github.com>
2018-07-03 13:30:10 -0700

    disable some jsx-a11y rules in Details examples

--- examples/component-examples/Details.js
+++ examples/component-examples/Details.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
 import React from 'react'
 import {Block, Details} from '../../src'
 import ExampleHeading from '../doc-components/ExampleHeading'
