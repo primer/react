0d8fbe147eb018560ddcf149d97ac10a089b488b
Shawn Allen <shawn.allen@github.com>
2018-07-09 14:19:37 -0700

    use UnderLineNav for main nav

--- examples/_app.js
+++ examples/_app.js
@@ -2,31 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {NavLink} from 'react-router-dom'
 import Styles from './doc-components/Styles'
+import {UnderlineNav, UnderlineNavItem} from '../src'
 
-const Page = ({render}) => (
-  <React.Fragment>
-    <Styles />
-    <div className="text-dark-gray">
-      <nav className="UnderlineNav">
-        <div className="UnderlineNav-body">
-          <NavLink to="/components" className="UnderlineNav-item no-underline" activeClassName="selected">
-            primer-react
-          </NavLink>
-          <NavLink to="/demos" className="UnderlineNav-item no-underline" activeClassName="selected">
+export default function Page({render}) {
+  return (
+    <React.Fragment>
+      <Styles />
+      <div className="text-dark-gray">
+        <UnderlineNav>
+          <UnderlineNavItem tag={NavLink} to="/components">
+            Components
+          </UnderlineNavItem>
+          <UnderlineNavItem tag={NavLink} to="/demos">
             Demos
-          </NavLink>
-          <NavLink to="/sandbox" className="UnderlineNav-item no-underline" activeClassName="selected">
+          </UnderlineNavItem>
+          <UnderlineNavItem tag={NavLink} to="/sandbox">
             Sandbox
-          </NavLink>
-        </div>
-      </nav>
-      {render()}
-    </div>
-  </React.Fragment>
-)
+          </UnderlineNavItem>
+        </UnderlineNav>
+        {render()}
+      </div>
+    </React.Fragment>
+  )
+}
 
 Page.propTypes = {
   render: PropTypes.func
 }
-
-export default Page
