7e5c80c5aff2c34acc0d72be4d1fe95ebc35b802
Shawn Allen <shawn.allen@github.com>
2018-07-09 14:20:58 -0700

    add primer-react@version link to main nav

--- examples/_app.js
+++ examples/_app.js
@@ -2,14 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {NavLink} from 'react-router-dom'
 import Styles from './doc-components/Styles'
-import {UnderlineNav, UnderlineNavItem} from '../src'
+import Octicon, {Package} from '@github/octicons-react'
+import {Block, Link, Text, UnderlineNav, UnderlineNavItem} from '../src'
+import {name, repository, version} from '../package.json'
+
+const pkg = `${name}@${version}`
+const releaseURL = `https://github.com/${repository}/releases/v${version}`
 
 export default function Page({render}) {
   return (
     <React.Fragment>
       <Styles />
       <div className="text-dark-gray">
-        <UnderlineNav>
+        <UnderlineNav actions={(
+          <Text color="gray-light" mono px={4}>
+            <Octicon icon={Package} className="mr-2" />
+            <Link href={releaseURL}>{pkg}</Link>
+          </Text>
+        )}>
           <UnderlineNavItem tag={NavLink} to="/components">
             Components
           </UnderlineNavItem>
