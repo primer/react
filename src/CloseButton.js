bd819f71316edcfe5b8dccdb621801209d2e4fc7
Shawn Allen <shawn.allen@github.com>
2018-07-10 16:00:32 -0700

    pass scheme="octicon" to Button from CloseButton

--- src/CloseButton.js
+++ src/CloseButton.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 import Octicon, {X} from '@github/octicons-react'
 
+const closeIcon = <Octicon icon={X} />
+
 export default function CloseButton(props) {
   return (
-    <Button {...props}>
-      <Octicon icon={X} />
+    <Button {...props} scheme="octicon">
+      {closeIcon}
     </Button>
   )
 }
