c239875246f3b80efae6751e48b498d1a6fa93c0
Shawn Allen <shawn.allen@github.com>
2018-07-10 15:57:43 -0700

    add very basic CloseButton example

---
+++ examples/component-examples/CloseButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Box, CloseButton} from '../../src'
+
+const example = {
+  name: 'CloseButton',
+  element: (
+    <div>
+      <Box className="clearfix">
+        <CloseButton />
+      </Box>
+    </div>
+  )
+}
+
+export default example
