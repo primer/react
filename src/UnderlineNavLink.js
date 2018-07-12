50acd1d261433585b6c5435aeacc81997ab2384c
Shawn Allen <shawn.allen@github.com>
2018-07-11 17:08:13 -0700

    clean up UnderlineNavLink, add whitespace props

--- src/UnderlineNavLink.js
+++ src/UnderlineNavLink.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import {ITEM_CLASS} from './UnderlineNav'
+import {mapWhitespaceProps} from './props'
 
 export default function UnderlineNavLink(props) {
-  const {children, className: defaultClassName, tag: Tag = 'a', underline, ...rest} = props
-  const className = classnames('UnderlineNav-item', !underline && 'no-underline', 'px-3', defaultClassName)
+  const {children, className: defaultClassName, tag: Tag, ...rest} = mapWhitespaceProps(props)
+  const className = classnames(defaultClassName, ITEM_CLASS)
 
   return (
-    <Tag className={className} activeClassName="selected" {...rest}>
+    <Tag className={className} {...rest}>
       {children}
     </Tag>
   )
 }
 
+UnderlineNavLink.defaultProps = {
+  tag: 'a'
+}
+
 UnderlineNavLink.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  tag: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-  underline: PropTypes.bool
+  tag: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
 }
