943f42f81e64a669f817f5c46916a51f5da49296
Shawn Allen <shawn.allen@github.com>
2018-07-12 13:37:01 -0700

    get full UnderlineNavLink test coverage

--- src/__tests__/UnderlineNavLink.js
+++ src/__tests__/UnderlineNavLink.js
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid */
 import React from 'react'
-import {ITEM_CLASS} from '../UnderlineNav'
+import {ITEM_CLASS, SELECTED_CLASS} from '../UnderlineNav'
 import UnderlineNavLink from '../UnderlineNavLink'
 import {render} from '../utils/testing'
 
@@ -8,8 +9,12 @@ describe('Caret', () => {
     expect(render(<UnderlineNavLink />).type).toEqual('a')
   })
 
-  it('renders the given tag prop', () => {
+  it('renders the given "tag" prop', () => {
     const Type = props => <b {...props} />
     expect(render(<UnderlineNavLink tag={Type} />)).toEqual(render(<b className={ITEM_CLASS} />))
+  })
+
+  it('respects the "selected" prop', () => {
+    expect(render(<UnderlineNavLink selected />)).toEqual(render(<a className={`${ITEM_CLASS} ${SELECTED_CLASS}`} />))
   })
 })
