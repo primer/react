cba9a56381d2831caead738fdbff45dc1854ba65
Emily <emplums@github.com>
2018-07-16 16:40:23 -0700

    add m + p utils to CircleOcticon

--- src/CircleOcticon.js
+++ src/CircleOcticon.js
@@ -2,16 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import Octicon from '@githubprimer/octicons-react'
+import {mapWhitespaceProps} from './props'
 import {colors} from './theme'
 
-export default function CircleOcticon({size, bg, color, ...rest}) {
-  const className = classnames(
+export default function CircleOcticon(props) {
+  const {size, bg, color, className, ...rest} = mapWhitespaceProps(props)
+  const classes = classnames(
+    className,
     'circle d-flex flex-items-center flex-justify-center',
     bg && `bg-${bg}`,
     color && `text-${color}`
   )
   return (
-    <div style={{width: `${size}px`, height: `${size}px`}} className={className}>
+    <div style={{width: `${size}px`, height: `${size}px`}} className={classes}>
       <Octicon size={size} {...rest} />
     </div>
   )
