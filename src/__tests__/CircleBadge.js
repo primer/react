cd4c766a10457e1bceb126b8514b4a0efabd3c10
Shawn Allen <shawn.allen@github.com>
2018-07-16 19:24:00 -0400

    get better CircleBadge test coverage, tidy up

--- src/__tests__/CircleBadge.js
+++ src/__tests__/CircleBadge.js
@@ -12,6 +12,7 @@ describe('CircleBadge', () => {
   it('renders medium by default', () => {
     expect(rendersClass(<CircleBadge />, 'CircleBadge--medium')).toEqual(true)
   })
+
   it('respects tag prop', () => {
     expect(
       render(
@@ -27,6 +28,7 @@ describe('CircleBadge', () => {
       )
     )
   })
+
   it('applies title', () => {
     expect(
       render(
@@ -36,29 +38,33 @@ describe('CircleBadge', () => {
       ).props['title']
     ).toEqual('primer logo')
   })
+
   it('adds bg class', () => {
     expect(rendersClass(<CircleBadge bg="blue" />, 'bg-blue')).toEqual(true)
   })
-  it('adds CircleBadge--icon class to children', () => {
-    const comp = mount(<CircleBadge>{imgInput}</CircleBadge>).render()
-    expect(
-      comp
-        .children()
-        .first()
-        .hasClass('CircleBadge-icon')
-    ).toEqual(true)
+
+  it('adds CircleBadge-icon class to children', () => {
+    const comp = mount(<CircleBadge>{imgInput}</CircleBadge>)
+    expect(comp.find('img').hasClass('CircleBadge-icon')).toEqual(true)
   })
+
+  it('does not duplicate "CircleBadge-icon" classes', () => {
+    const comp = mount(
+      <CircleBadge>
+        <img className="CircleBadge-icon" alt="" src="primer.jpg" />
+      </CircleBadge>
+    )
+    expect(comp.find('img').props().className).toEqual('CircleBadge-icon')
+  })
+
   it('preserves child class names', () => {
     const comp = mount(
       <CircleBadge>
         <img className="primer" alt="" src="primer.jpg" />
       </CircleBadge>
-    ).render()
+    )
     expect(
-      comp
-        .children()
-        .first()
-        .hasClass('primer')
+      comp.find('img').hasClass('primer')
     ).toEqual(true)
   })
 })
