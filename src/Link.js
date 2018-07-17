038e59b018cecd8cc17e1ffcb38e5a21144dc366
Emily <emplums@github.com>
2018-07-17 15:14:57 -0700

    add m + p utils to Link

--- src/Link.js
+++ src/Link.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import {mapWhitespaceProps} from './props'
 
-export default function Link({children, className, muted, scheme, nounderline, ...rest}) {
+export default function Link(props) {
+  const {children, className, muted, scheme, nounderline, ...rest} = mapWhitespaceProps(props)
   const colorClass = scheme ? `link-${scheme}` : muted ? 'muted-link' : 'text-blue'
   return (
     <a className={classnames(className, colorClass, nounderline && 'no-underline')} {...rest}>
