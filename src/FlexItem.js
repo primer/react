54c8f6c9451426f21c194d12be48a3ee04bd28c0
Emily <emplums@github.com>
2018-07-18 08:45:39 -0700

    update FlexItem syntax

--- src/FlexItem.js
+++ src/FlexItem.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import {mapWhitespaceProps} from  './props'
 
-const FlexItem = (props) => {
-  const {tag: Tag = 'div', children, flexAuto, alignSelf, className} = mapWhitespaceProps(props)
+const FlexItem = ({tag: Tag = 'div', children, flexAuto, alignSelf, ...rest}) => {
+  const {className} = mapWhitespaceProps(rest)
   const classes = classnames(className, {'flex-auto': flexAuto}, alignSelf && `flex-self-${alignSelf}`)
   return <Tag className={classes}>{children}</Tag>
 }
