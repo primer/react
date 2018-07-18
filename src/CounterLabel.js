02cc63f5cfbfc1e7f76c900a0b8b32bdf5afc9c4
Emily <emplums@github.com>
2018-07-18 08:39:44 -0700

    update CounterLabel syntax

--- src/CounterLabel.js
+++ src/CounterLabel.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import {mapWhitespaceProps} from  './props'
 
-export default function CounterLabel(props) {
-  const {theme, children, className} = mapWhitespaceProps(props)
+export default function CounterLabel({theme, children, ...rest}) {
+  const {className} = mapWhitespaceProps(rest)
   return <span className={classnames(className, 'Counter', theme && `Counter--${theme}`)}>{children}</span>
 }
 
