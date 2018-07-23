b347b7f106323affed970f282aa0d008127297f8
Emily <emplums@github.com>
2018-07-23 14:43:32 -0700

    add LiveEditor to Dropdown examples

--- examples/component-examples/Dropdown.js
+++ examples/component-examples/Dropdown.js
@@ -1,6 +1,35 @@
 import React from 'react'
+import {LiveEditor} from '@compositor/kit'
 import {Block, Dropdown} from '../../src'
 import ExampleHeading from '../doc-components/ExampleHeading'
+
+const dropdownPrimary =
+`<Dropdown scheme={'primary'}>
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+</Dropdown>`
+
+const dropdown =
+`<Dropdown>
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+</Dropdown>`
+
+const dropdownTitle =
+`<Dropdown title="Options">
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+</Dropdown>`
+
 
 const DropdownExample = {
   name: 'Dropdown',
@@ -8,33 +37,15 @@ const DropdownExample = {
     <div>
       <Block mb={4}>
         <ExampleHeading>Dropdown Primary</ExampleHeading>
-        <Dropdown scheme={'primary'}>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
-        </Dropdown>
+        <LiveEditor code={dropdownPrimary} scope={{Dropdown}} />
       </Block>
       <Block my={4}>
         <ExampleHeading>Dropdown</ExampleHeading>
-        <Dropdown>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
-        </Dropdown>
+        <LiveEditor code={dropdown} scope={{Dropdown}} />
       </Block>
       <Block my={4}>
         <ExampleHeading>Dropdown with title</ExampleHeading>
-        <Dropdown title="Options">
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
-        </Dropdown>
+        <LiveEditor code={dropdownTitle} scope={{Dropdown}}/>
       </Block>
     </div>
   )
