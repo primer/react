77f9b98b52dc0ddfd85bc73b11158480c728feca
Emily <emplums@github.com>
2018-07-23 15:17:15 -0700

    add LiveEditor to TextInput examples

--- examples/component-examples/Form.js
+++ examples/component-examples/Form.js
@@ -1,23 +1,20 @@
 import React from 'react'
-import {TextInput, Box} from '../../src'
+import {LiveEditor} from '@compositor/kit'
+import {TextInput, Block} from '../../src'
 import ExampleHeading from '../doc-components/ExampleHeading'
 
-const FormExample = {
-  name: 'Form elements',
+const TextInputExample = {
+  name: 'TextInput',
   element: (
     <div>
-      <ExampleHeading>Input</ExampleHeading>
-      <TextInput name="zipcode" />
-      <ExampleHeading>Input Sizes</ExampleHeading>
-      <Box>
-        <TextInput name="zipcode" size="small" placeholder="Small input" />
-      </Box>
-      <Box>
-        <TextInput name="zipcode" size="large" placeholder="Large input" />
-      </Box>
-      <ExampleHeading>Block input</ExampleHeading>
-      <TextInput block placeholder="Full width block input" />
+      <ExampleHeading>Text Input</ExampleHeading>
+      <LiveEditor code={`<TextInput name="zipcode" />`} scope={{TextInput}}/>
+      <ExampleHeading>Text Input Sizes</ExampleHeading>
+      <LiveEditor code={`<TextInput name="zipcode" size="small" placeholder="Small input" />`} scope={{TextInput}}/>
+      <LiveEditor code={`<TextInput name="zipcode" size="large" placeholder="Large input" />`} scope={{TextInput}}/>
+      <ExampleHeading>Text Input - Block</ExampleHeading>
+      <LiveEditor code={`<TextInput block placeholder="Full width block input" />`} scope={{TextInput}}/>
     </div>
   )
 }
-export default FormExample
+export default TextInputExample
