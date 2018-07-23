3b4d5653f2c868882dca5a251f1a8ce794955dde
Emily <emplums@github.com>
2018-07-23 15:49:29 -0700

    add LiveEditor to UnderlineNav examples

--- examples/component-examples/UnderlineNav.js
+++ examples/component-examples/UnderlineNav.js
@@ -1,8 +1,34 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react'
+import {LiveEditor} from '@compositor/kit'
+import {NavLink} from 'react-router-dom'
 import ExampleHeading from '../doc-components/ExampleHeading'
 import {Block, Text, UnderlineNav, UnderlineNavLink} from '../../src'
-import {NavLink} from 'react-router-dom'
+
+const underlineNavLinkExample =
+`<UnderlineNav>
+  <UnderlineNavLink href="#foo" selected>
+    Selected
+  </UnderlineNavLink>
+  <UnderlineNavLink href="#bar">Bar</UnderlineNavLink>
+  <UnderlineNavLink href="#baz">Baz</UnderlineNavLink>
+</UnderlineNav>`
+
+const linkTagExample =
+`<UnderlineNav>
+  <a href="#foo">Foo</a>
+  <a href="#bar" selected>
+    Selected
+  </a>
+  <a href="#baz">Baz</a>
+</UnderlineNav>`
+
+const navLinkExample =
+`<UnderlineNav>
+  <NavLink to="#foo">Foo</NavLink>
+  <NavLink to="#bar">Two</NavLink>
+  <NavLink to="/">Selected</NavLink>
+</UnderlineNav>`
 
 export default {
   name: 'UnderlineNav',
@@ -12,37 +38,21 @@ export default {
         <ExampleHeading>
           Using <Text mono>{'<UnderlineNavLink>'}</Text>
         </ExampleHeading>
-        <UnderlineNav>
-          <UnderlineNavLink href="#foo" selected>
-            Selected
-          </UnderlineNavLink>
-          <UnderlineNavLink href="#bar">Bar</UnderlineNavLink>
-          <UnderlineNavLink href="#baz">Baz</UnderlineNavLink>
-        </UnderlineNav>
+        <LiveEditor code={underlineNavLinkExample} scope={{UnderlineNav, UnderlineNavLink}} />
       </Block>
 
       <Block mb={4}>
         <ExampleHeading>
           Using <Text mono>{'<a>'}</Text> tags
         </ExampleHeading>
-        <UnderlineNav>
-          <a href="#foo">Foo</a>
-          <a href="#bar" selected>
-            Selected
-          </a>
-          <a href="#baz">Baz</a>
-        </UnderlineNav>
+        <LiveEditor code={linkTagExample} scope={{UnderlineNav}} />
       </Block>
 
       <Block mb={4}>
         <ExampleHeading>
           Using <Text mono>{'<NavLink>'}</Text> from react-router
         </ExampleHeading>
-        <UnderlineNav>
-          <NavLink to="#foo">Foo</NavLink>
-          <NavLink to="#bar">Two</NavLink>
-          <NavLink to="/">Selected</NavLink>
-        </UnderlineNav>
+        <LiveEditor code={navLinkExample} scope={{UnderlineNav, NavLink}} />
       </Block>
     </div>
   )
