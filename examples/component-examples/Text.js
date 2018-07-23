5ad252b0124f5d2fdf88aa4d0f919d739aac5472
Emily <emplums@github.com>
2018-07-23 15:36:28 -0700

    add LiveEditor to Text examples

--- examples/component-examples/Text.js
+++ examples/component-examples/Text.js
@@ -1,26 +1,17 @@
 import React from 'react'
+import {LiveEditor} from '@compositor/kit'
 import {Text} from '../../src'
 
 const TextExample = {
   name: 'Text',
   element: (
     <div>
-      <Text tag="div">Text</Text>
-      <Text tag="div" fontWeight="bold">
-        Text bold
-      </Text>
-      <Text tag="div" color="green">
-        Text green
-      </Text>
-      <Text tag="div" lineHeight="condensed">
-        Text lineHeight 'condensed'
-      </Text>
-      <Text tag="div" fontSize={4}>
-        Text fontSize 4
-      </Text>
-      <Text tag="div" p={4}>
-        Text padding 4
-      </Text>
+      <LiveEditor code={`<Text tag="div">Text</Text>`} scope={{Text}}/>
+      <LiveEditor code={`<Text tag="div" fontWeight="bold">Text bold</Text>`} scope={{Text}}/>
+      <LiveEditor code={`<Text tag="div" color="green">Text green</Text>`} scope={{Text}}/>
+      <LiveEditor code={`<Text tag="div" lineHeight="condensed">Text lineHeight 'condensed'</Text>`} scope={{Text}}/>
+      <LiveEditor code={`<Text tag="div" fontSize={4}>Text fontSize 4</Text>`} scope={{Text}}/>
+      <LiveEditor code={`<Text tag="div" p={4}>Text padding 4</Text>`} scope={{Text}}/>
     </div>
   )
 }
