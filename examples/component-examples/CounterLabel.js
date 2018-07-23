8ca7a5eb7740c62a6b04e5ec8653b8f2d2d725bf
Emily <emplums@github.com>
2018-07-23 14:27:52 -0700

    add LiveEditor to CounterLabel example

--- examples/component-examples/CounterLabel.js
+++ examples/component-examples/CounterLabel.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import {LiveEditor} from '@compositor/kit'
 import {CounterLabel} from '../../src'
 
 const CounterLabelExample = {
   name: 'CounterLabel',
   element: (
     <div>
-      <CounterLabel>12</CounterLabel>
-      <CounterLabel theme={'gray'}>13</CounterLabel>
-      <CounterLabel theme={'gray-light'}>13</CounterLabel>
+      <LiveEditor code={`<CounterLabel>12</CounterLabel>`} scope={{CounterLabel}}/>
+      <LiveEditor code={`<CounterLabel theme={'gray'}>13</CounterLabel>`} scope={{CounterLabel}}/>
+      <LiveEditor code={`<CounterLabel theme={'gray-light'}>13</CounterLabel>`} scope={{CounterLabel}}/>
     </div>
   )
 }
