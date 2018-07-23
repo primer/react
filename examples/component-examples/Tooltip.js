f217cd7a674912fcefb29bbba59e0f01498a23e5
Emily <emplums@github.com>
2018-07-23 15:44:43 -0700

    add LiveEditor to Tooltip examples

--- examples/component-examples/Tooltip.js
+++ examples/component-examples/Tooltip.js
@@ -1,48 +1,66 @@
 import React from 'react'
+import {LiveEditor} from '@compositor/kit'
 import {Box, Block, Tooltip} from '../../src'
 import ExampleHeading from '../doc-components/ExampleHeading'
+
+const basicExample =
+`<Box p={3}>
+  <Tooltip text="Hello, Tooltip!">Text with a tooltip</Tooltip>
+</Box>`
+
+const directionExample = (dir) =>
+`<Box p={3} my={2} key={dir}>
+  <Tooltip text="Hello, Tooltip!" direction={dir}>
+    Tooltip direction={dir}
+  </Tooltip>
+</Box>
+`
+
+const wrapExample =
+`<Box p={3} my={2}>
+  <Tooltip
+    text="Hello, Tooltip! This tooltip has a sentence that will wrap to a newline."
+    wrap
+    direction="ne"
+    align="left"
+  >
+    Word wrapping tooltip
+  </Tooltip>
+</Box>`
+
+const alignmentExample = (align) =>
+`<Box p={3} my={2} key={align}>
+  <Tooltip text="Hello, Tooltip!" direction="ne" align={align}>
+    Tooltip align={align}
+  </Tooltip>
+</Box>`
+
+const noDelayExample =
+`<Box p={3} my={2}>
+  <Tooltip noDelay text="Hello, Tooltip!">
+    Text with a tooltip
+  </Tooltip>
+</Box>`
 
 const TooltipExample = {
   name: 'Tooltip',
   element: (
     <div>
-      <Box p={3}>
-        <Tooltip text="Hello, Tooltip!">Text with a tooltip</Tooltip>
-      </Box>
+      <ExampleHeading mt={3}>Basic Tooltip</ExampleHeading>
+      <LiveEditor code={basicExample} scope={{Tooltip, Box}}/>
       <Block p={2} my={2}>
         <ExampleHeading mt={3}>Directions</ExampleHeading>
         {Tooltip.directions.map(dir => (
-          <Box p={3} my={2} key={dir}>
-            <Tooltip text="Hello, Tooltip!" direction={dir}>
-              Tooltip direction={dir}
-            </Tooltip>
-          </Box>
+          <LiveEditor code={directionExample(dir)} scope={{Tooltip, Box, dir}} />
         ))}
         <ExampleHeading mt={3}>Alignment</ExampleHeading>
         {Tooltip.alignments.map(align => (
-          <Box p={3} my={2} key={align}>
-            <Tooltip text="Hello, Tooltip!" direction="ne" align={align}>
-              Tooltip align={align}
-            </Tooltip>
-          </Box>
+          <LiveEditor code={alignmentExample(align)} scope={{Box, Tooltip, align}} />
         ))}
         <ExampleHeading mt={3}>Word wrap</ExampleHeading>
-        <Box p={3} my={2}>
-          <Tooltip
-            text="Hello, Tooltip! This tooltip has a sentence that will wrap to a newline."
-            wrap
-            direction="ne"
-            align="left"
-          >
-            Word wrapping tooltip
-          </Tooltip>
-        </Box>
+        <LiveEditor code={wrapExample} scope={{Tooltip, Box}} />
         <ExampleHeading mt={3}>No Delay</ExampleHeading>
-        <Box p={3} my={2}>
-          <Tooltip noDelay text="Hello, Tooltip!">
-            Text with a tooltip
-          </Tooltip>
-        </Box>
+        <LiveEditor code={noDelayExample} scope={{Tooltip, Box}} />
       </Block>
     </div>
   )
