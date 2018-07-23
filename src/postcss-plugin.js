322794f6c2350d43e203f5224a3b9f38db33f0dd
Shawn Allen <shawn.allen@github.com>
2018-07-23 14:02:20 -0700

    convert build-css to a postcss plugin

---
+++ src/postcss-plugin.js
@@ -0,0 +1,68 @@
+const postcss = require('postcss')
+const defaultTheme = require('../src/theme')
+
+module.exports = postcss.plugin('primer-generate', opts => {
+  const {theme = defaultTheme} = opts || {}
+
+  const breakpointNames = ['sm', 'md', 'lg', 'xl']
+  const {breakpoints, colors, fontSizes, maxWidths, radii, space} = theme
+  const {border, bg, ...namedColors} = colors
+  const spacers = space.reduce((map, len, i) => (map[i] = `${len}px`, map), {})
+
+  const shorthandProps = {m: 'margin', p: 'padding'}
+
+  return (root, result) => {
+    for (const [color, values] of Object.entries(namedColors)) {
+      if (Array.isArray(values)) {
+        generateUtility(`.color-${color}`, 'color', values[5])
+        if (!bg.hasOwnProperty(color)) {
+          generateUtility(`.bg-${color}`, 'background-color', values[5])
+        }
+      }
+    }
+
+    generateUtilities(namedColors, parts => `.color-${parts.join('-')}`, 'color')
+    generateUtilities(border, parts => `.border-${parts.join('-')}`, 'border-color')
+    generateUtilities(bg, parts => `.bg-${parts.join('-')}`, 'background-color')
+
+    const breakRules = breakpointNames.reduce((map, name, i) => {
+      map[name] = postcss.atRule({name: 'media', params: `(min-width: ${breakpoints[i]})`})
+      return map
+    }, {})
+
+    for (const [prefix, property] of Object.entries(shorthandProps)) {
+      generateUtilities(spacers, ([i]) => `.${prefix}-${i}`, property)
+      for (const [name, rule] of Object.entries(breakRules)) {
+        generateUtilities(spacers, ([i]) => `.${prefix}-${name}-${i}`, property, rule)
+      }
+    }
+
+    for (const [name, rule] of Object.entries(breakRules)) {
+      root.append(rule)
+    }
+
+    function generateUtility(selector, prop, value) {
+      const rule = postcss.rule({selector})
+      rule.append(postcss.decl({prop, value, important: true}))
+      return rule
+    }
+
+    function generateUtilities(values, makeSelector, property, parent = root) {
+      const rules = []
+      for (const [key, value] of Object.entries(values)) {
+        if (Array.isArray(value)) {
+          for (const [i, val] of Object.entries(value)) {
+            rules.push(generateUtility(makeSelector([key, i]), property, val))
+          }
+        } else if (typeof value === 'object') {
+          rules.push(...generateUtilities(value, k => makeSelector([key, k])))
+        } else {
+          rules.push(generateUtility(makeSelector([key]), property, value))
+        }
+      }
+      for (const rule of rules) {
+        parent.append(rule)
+      }
+    }
+  }
+})
