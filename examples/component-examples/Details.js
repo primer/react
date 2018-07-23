ae5b24904498d8f1653310bff69a263eeb513af5
Emily <emplums@github.com>
2018-07-23 14:32:16 -0700

    add LiveEditor to Details example

--- examples/component-examples/Details.js
+++ examples/component-examples/Details.js
@@ -1,7 +1,28 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
 import React from 'react'
+import {LiveEditor} from '@compositor/kit'
 import {Block, Details} from '../../src'
 import ExampleHeading from '../doc-components/ExampleHeading'
+
+const example1 = `
+<Details>
+  <summary className="btn">Click me</summary>
+  <p>This should show and hide</p>
+</Details>
+`
+
+const example2 = `
+<Details>
+  {({open, toggle}) => (
+    <React.Fragment>
+      <summary className="btn" onClick={toggle}>
+        {open ? 'Hide' : 'Show'}
+      </summary>
+      <p>This should show and hide</p>
+    </React.Fragment>
+  )}
+</Details>
+`
 
 const DetailsExample = {
   name: 'Details',
@@ -9,27 +30,15 @@ const DetailsExample = {
     <div>
       <Block mb={4}>
         <ExampleHeading>With static children</ExampleHeading>
-        <Details>
-          <summary className="btn">Click me</summary>
-          <p>This should show and hide</p>
-        </Details>
+        <LiveEditor code={example1} scope={{Details}}/>
       </Block>
       <Block my={4}>
         <ExampleHeading>With children as a function</ExampleHeading>
-        <Details>
-          {({open, toggle}) => (
-            <React.Fragment>
-              <summary className="btn" onClick={toggle}>
-                {open ? 'Hide' : 'Show'}
-              </summary>
-              <p>This should show and hide</p>
-            </React.Fragment>
-          )}
-        </Details>
+        <LiveEditor code={example2} scope={{Details}}/>
       </Block>
       <Block my={4}>
         <ExampleHeading>With render prop</ExampleHeading>
-        <Details render={() => 'hi'} />
+        <LiveEditor code={`<Details render={() => 'hi'} />`} scope={{Details}} />
       </Block>
     </div>
   )
