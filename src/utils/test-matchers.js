3ae46bec0cb49fd219fa56a307ff948e22711716
Shawn Allen <shawn.allen@github.com>
2018-07-24 16:43:22 -0700

    tidy up test matchers

--- src/utils/test-matchers.js
+++ src/utils/test-matchers.js
@@ -1,3 +1,29 @@
+import {render} from './testing'
+
+const {stringify} = JSON
+
+expect.extend({
+  toHaveClass(node, klass) {
+    const classes = getClasses(node)
+    const pass = classes.includes(klass)
+    return {
+      pass,
+      message: () => `expected ${stringify(classes)} to include: ${stringify(klass)}`
+    }
+  },
+
+  toHaveClasses(node, klasses, only = false) {
+    const classes = getClasses(node)
+    const pass = only
+      ? this.equals(classes.sort(), klasses.sort())
+      : klasses.every(klass => classes.includes(klass))
+    return {
+      pass,
+      message: () => `expected ${stringify(classes)} to include: ${stringify(klasses)}`
+    }
+  }
+})
+
 function getProps(node) {
   return typeof node.props === 'function' ? node.props() : node.props
 }
@@ -6,31 +32,3 @@ function getClasses(node) {
   const {className = ''} = getProps(node)
   return className.trim().split(/ +/)
 }
-
-expect.extend({
-  toRenderClass(node, klass) {
-    const classes = getClasses(node)
-    const pass = classes.includes(klass)
-    return {
-      pass,
-      message: () => `expected "${classes.join(' ')}" to include: "${klass}"`
-    }
-  },
-
-  toRenderClasses(node, klasses) {
-    const classes = getClasses(node)
-    const pass = klasses.every(klass => classes.includes(klass))
-    return {
-      pass,
-      message: () => `expected "${classes.join(' ')}" to include: ["${klasses.join('", "')}"]`
-    }
-  },
-
-  toRenderStyle(node, expected) {
-    const {style} = getProps(node)
-    return {
-      pass: this.equals(style, expected),
-      message: () => `expected ${JSON.stringify(style)} to equal ${JSON.stringify(expected)}`
-    }
-  }
-})
