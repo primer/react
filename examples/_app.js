dbae6d792d162f9db56453e11b15fe8ed42b897d
Shawn Allen <shawn.allen@github.com>
2018-07-25 14:32:49 -0700

    use StyleProvider in examples/_app.js

--- examples/_app.js
+++ examples/_app.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {NavLink} from 'react-router-dom'
-import Styles from './doc-components/Styles'
 import Octicon, {Package} from '@githubprimer/octicons-react'
-import {Block, Link, Text, UnderlineNav} from '../src'
+import {Block, Link, StyleProvider, Text, UnderlineNav} from '../src'
 import {name, repository, version} from '../package.json'
 
 const pkg = `${name}@${version}`
@@ -12,7 +11,7 @@ const releaseURL = `https://github.com/${repository}/releases/v${version}`
 export default function Page({render}) {
   return (
     <React.Fragment>
-      <Styles />
+      <StyleProvider />
       <div className="text-dark-gray">
         <UnderlineNav
           actions={
