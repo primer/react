637ea41cdc3cdb9818d983ff03a70a58a970a5a2
Shawn Allen <shawn.allen@github.com>
2018-07-26 12:02:42 -0700

    refactor Colors examples

--- examples/component-examples/Colors.js
+++ examples/component-examples/Colors.js
@@ -1,28 +1,38 @@
 import React from 'react'
-import {Block, theme} from '../../src'
-import Swatch from '../doc-components/Swatch'
+import {Block, FlexContainer, theme} from '../../src'
 
 const ColorsExample = {
   name: 'Colors',
   element: (
     <div>
       {['gray', 'blue', 'green', 'purple', 'yellow', 'orange'].map(hue => (
-        <div className="d-flex" key={hue}>
-          {theme.colors[hue].map((color, j) => <Swatch name={hue} index={j} key={color} color={color} />)}
-        </div>
+        <FlexContainer key={hue}>
+          {theme.colors[hue].map((colorValue, i) => {
+            const colorKey = `${hue}.${i}`
+            return (
+              <Block>
+                <Block bg={colorKey} m={1} mt={3} p={6} />
+                <Heading tag="h3" fontSize={2} px={1}>
+                  {colorKey}
+                </Heading>
+                <Text px={1} mono>{colorValue}</Text>
+              </Block>
+            )
+          })}
+        </FlexContainer>
       ))}
       <div className="d-flex">
-        <Block bg="blue" p={4} m={1} />
-        <Block bg="green" p={4} m={1} />
-        <Block bg="purple" p={4} m={1} />
-        <Block bg="yellow" p={4} m={1} />
-        <Block bg="red" p={4} m={1} />
+        <Block bg="blue.5" p={4} m={1} />
+        <Block bg="green.5" p={4} m={1} />
+        <Block bg="purple.5" p={4} m={1} />
+        <Block bg="yellow.5" p={4} m={1} />
+        <Block bg="red.5" p={4} m={1} />
         <Block bg="white" p={4} m={1} border />
-        <Block bg="gray" p={4} m={1} />
-        <Block bg="gray-light" p={4} m={1} />
-        <Block bg="blue-light" p={4} m={1} />
-        <Block bg="purple-light" p={4} m={1} />
-        <Block bg="red-light" p={4} m={1} />
+        <Block bg="gray.5" p={4} m={1} />
+        <Block bg="gray.5" p={4} m={1} />
+        <Block bg="blue.2" p={4} m={1} />
+        <Block bg="purple.2" p={4} m={1} />
+        <Block bg="red.2" p={4} m={1} />
       </div>
     </div>
   )
