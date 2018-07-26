ffa880397372b320290e34db9bbd3a3626cabc32
Shawn Allen <shawn.allen@github.com>
2018-07-26 12:45:24 -0700

    nix "light" fontSize example values

--- examples/component-examples/Heading.js
+++ examples/component-examples/Heading.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import {LiveEditor} from '@compositor/kit'
-import {Heading} from '../../src'
+import {Heading, theme} from '../../src'
+
+const fontSizes = Object.keys(theme.fontSizes)
 
 const HeadingExample = {
   name: 'Heading',
   element: (
     <div>
       <Heading mb={2}>Default Heading</Heading>
-      {[0, 1, 2, 3, 4, 5, /* 6, 7, */ '00-light', '0-light', '1-light', '2-light', '3-light'].map(fontSize => (
+      {fontSizes.map(fontSize => (
         <LiveEditor
           key={fontSize}
-          code={`<Heading fontSize={"${fontSize}"} mb={2}>With fontSize={"${fontSize}"}</Heading>`}
+          code={`<Heading fontSize={${fontSize}} mb={2}>With fontSize={${fontSize}}</Heading>`}
           scope={{Heading, fontSize}}
         />
       ))}
