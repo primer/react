59948d72a510b0ee221a7ede8488484babd202ac
Shawn Allen <shawn.allen@github.com>
2018-07-30 14:05:11 -0700

    add "borders" and "shadows" entries to theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,53 +1,30 @@
-const colors = {
-  bodytext: '#24292e',
-  black: '#1b1f23',
-  white: '#fff',
-  gray: ['#fafbfc', '#f6f8fa', '#e1e4e8', '#d1d5da', '#959da5', '#6a737d', '#586069', '#444d56', '#2f363d', '#24292e'],
-  blue: ['#f1f8ff', '#dbedff', '#c8e1ff', '#79b8ff', '#2188ff', '#0366d6', '#005cc5', '#044289', '#032f62', '#05264c'],
-  green: ['#f0fff4', '#dcffe4', '#bef5cb', '#85e89d', '#34d058', '#28a745', '#22863a', '#176f2c', '#165c26', '#144620'],
-  yellow: [
-    '#fffdef',
-    '#fffbdd',
-    '#fff5b1',
-    '#ffea7f',
-    '#ffdf5d',
-    '#ffd33d',
-    '#f9c513',
-    '#dbab09',
-    '#b08800',
-    '#735c0f'
-  ],
-  orange: [
-    '#fff8f2',
-    '#ffebda',
-    '#ffd1ac',
-    '#ffab70',
-    '#fb8532',
-    '#f66a0a',
-    '#e36209',
-    '#d15704',
-    '#c24e00',
-    '#a04100'
-  ],
-  red: ['#ffeef0', '#ffdce0', '#fdaeb7', '#f97583', '#ea4a5a', '#d73a49', '#cb2431', '#b31d28', '#9e1c23', '#86181d'],
-  purple: [
-    '#f5f0ff',
-    '#e6dcfd',
-    '#d1bcf9',
-    '#b392f0',
-    '#8a63d2',
-    '#6f42c1',
-    '#5a32a3',
-    '#4c2889',
-    '#3a1d6e',
-    '#29134e'
-  ],
-  blackfade15: 'rgba(27, 31 ,35, 0.15)',
-  blackfade20: 'rgba(27, 31 ,35, 0.20)',
-  whitefade15: 'rgba(255, 255 ,255, 0.15)'
-}
+import chroma from 'chroma-js'
 
-const {blue, gray, green, purple, red, yellow} = colors
+const black = '#1b1f23'
+const white = '#fff'
+const blue = ['#f1f8ff', '#dbedff', '#c8e1ff', '#79b8ff', '#2188ff', '#0366d6', '#005cc5', '#044289', '#032f62', '#05264c']
+const gray = ['#fafbfc', '#f6f8fa', '#e1e4e8', '#d1d5da', '#959da5', '#6a737d', '#586069', '#444d56', '#2f363d', '#24292e']
+const green = ['#f0fff4', '#dcffe4', '#bef5cb', '#85e89d', '#34d058', '#28a745', '#22863a', '#176f2c', '#165c26', '#144620']
+const orange = ['#fff8f2', '#ffebda', '#ffd1ac', '#ffab70', '#fb8532', '#f66a0a', '#e36209', '#d15704', '#c24e00', '#a04100']
+const purple = ['#f5f0ff', '#e6dcfd', '#d1bcf9', '#b392f0', '#8a63d2', '#6f42c1', '#5a32a3', '#4c2889', '#3a1d6e', '#29134e']
+const red = ['#ffeef0', '#ffdce0', '#fdaeb7', '#f97583', '#ea4a5a', '#d73a49', '#cb2431', '#b31d28', '#9e1c23', '#86181d']
+const yellow = ['#fffdef', '#fffbdd', '#fff5b1', '#ffea7f', '#ffdf5d', '#ffd33d', '#f9c513', '#dbab09', '#b08800', '#735c0f']
+
+const colors = {
+  bodytext: gray[9],
+  black,
+  white,
+  blue,
+  gray,
+  green,
+  orange,
+  purple,
+  red,
+  yellow,
+  blackfade15: rgba(black, 0.15),
+  blackfade20: rgba(black, 0.20),
+  whitefade15: rgba(white, 0.15)
+}
 
 colors.border = {
   'black-fade': colors.blackfade15,
@@ -104,6 +81,16 @@ const theme = {
   ],
   lineHeight: 1.5,
   colors,
+  borders: [
+    0,
+    '1px solid'
+  ],
+  shadows: {
+    small: `0 1px 1px ${rgba(colors.black, 0.1)}`,
+    medium: `0 1px 5px ${colors.blackfade15}`,
+    large: `0 1px 15px ${colors.blackfade15}`,
+    'extra-large': `0 10px 50px ${rgba(colors.black, 0.07)}`
+  },
   radii: [0, 3, 6],
   space: [0, 4, 8, 16, 24, 32, 40, 48],
   fontSizes: [12, 14, 16, 20, 24, 32, 40, 48]
@@ -111,3 +98,7 @@ const theme = {
 
 export default theme
 export {colors}
+
+function rgba(color, alpha) {
+  return chroma(color).alpha(alpha).css()
+}
