36c04b723d897657c13dff03c78e7db61021d6d3
Jon Rohan <rohan@github.com>
2018-08-02 16:59:50 -0700

    Updating UnderlineNavLink.js to styled-systems emotion

--- src/UnderlineNavLink.js
+++ src/UnderlineNavLink.js
@@ -2,32 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import {ITEM_CLASS, SELECTED_CLASS} from './UnderlineNav'
-import {mapWhitespaceProps} from './props'
+import {withSystemProps, COMMON} from './system-props'
 
-export default function UnderlineNavLink({children, selected, tag: Tag, ...rest}) {
-  const {className} = mapWhitespaceProps(rest)
+function UnderlineNavLink({className, selected, is: Tag, ...rest}) {
   const classes = classnames(ITEM_CLASS, selected && SELECTED_CLASS, className)
 
   if (typeof rest.to === 'string') {
     rest.activeClassName = SELECTED_CLASS
   }
 
-  return (
-    <Tag className={classes} {...rest}>
-      {children}
-    </Tag>
-  )
+  return <Tag className={classes} {...rest} />
 }
 
-UnderlineNavLink.displayName = 'UnderlineNavLink'
-
 UnderlineNavLink.defaultProps = {
-  tag: 'a'
+  is: 'a'
 }
 
 UnderlineNavLink.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  selected: PropTypes.bool,
-  tag: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
+  is: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+  selected: PropTypes.bool
 }
+
+export default withSystemProps(UnderlineNavLink, COMMON)
