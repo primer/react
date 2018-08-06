7ffb51957c8845b6cd266b04a62f94c30d90e1bc
Shawn Allen <shawn.allen@github.com>
2018-08-06 12:36:39 -0700

    don't call withSystemProps() on CaretBox

--- src/CaretBox.js
+++ src/CaretBox.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import Box from './Box'
 import Caret from './Caret'
-import {withSystemProps} from './system-props'
 
 function CaretBox(props) {
   // don't destructure these, just grab them
   const {bg, borderColor} = props
   const {caret, children, ...rest} = props
-  const caretProps = {
-    location: caret,
-    borderColor,
-    bg
-  }
-
+  const caretProps = {bg, borderColor, location: caret}
   return (
     <Box {...rest}>
       {children}
@@ -31,4 +25,4 @@ CaretBox.defaultProps = {
   position: 'relative'
 }
 
-export default withSystemProps(CaretBox, ['color'])
+export default CaretBox
