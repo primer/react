953adf4e6f03e85138073a6909a5f20b5d008ad4
Shawn Allen <shawn.allen@github.com>
2018-08-06 11:42:53 -0700

    remove outdated tests from system-props, etc.

--- src/__tests__/system-props.js
+++ src/__tests__/system-props.js
@@ -2,63 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {space, fontFamily} from 'styled-system'
 import {render} from '../utils/testing'
-import {getSystemProps, composeSystemProps, withSystemProps} from '../system-props'
+import {withSystemProps} from '../system-props'
 
 describe('system props', () => {
-  describe('getSystemProps()', () => {
-    it('maps known prop names to styled-system', () => {
-      expect(getSystemProps(['space', 'fontFamily'])).toEqual([space, fontFamily])
-    })
-
-    it('accepts functions as-is', () => {
-      function foo() {}
-      expect(getSystemProps([foo])).toEqual([foo])
-    })
-
-    it('throws on unknown prop strings', () => {
-      expect(() => getSystemProps(['foo'])).toThrow()
-    })
-  })
-
-  describe('composeSystemProps()', () => {
-    it('returns a composed function', () => {
-      const plusOne = d => d + 1
-      const timesTwo = d => d * 2
-      expect(composeSystemProps([plusOne, timesTwo])(1)).toBe(4)
-    })
-
-    it('adds propTypes for each function', () => {
-      const a = () => ({})
-      a.propTypes = {a: PropTypes.number}
-      const b = () => ({})
-      b.propTypes = {b: PropTypes.string}
-      expect(composeSystemProps([a, b]).propTypes).toEqual({
-        a: PropTypes.number,
-        b: PropTypes.string
-      })
-    })
-
-    it('merges multiple propTypes for each function', () => {
-      const a = () => ({})
-      a.propTypes = {a: PropTypes.number}
-      const b = () => ({})
-      b.propTypes = {b: PropTypes.string, c: PropTypes.bool}
-      expect(composeSystemProps([a, b]).propTypes).toEqual({
-        a: PropTypes.number,
-        b: PropTypes.string,
-        c: PropTypes.bool
-      })
-    })
-  })
-
   describe('withSystemProps()', () => {
-    it('merges all the propTypes', () => {
+    xit('merges all the propTypes', () => {
       const Component = () => <b />
       const Wrapped = withSystemProps(Component, ['fontFamily'])
-      expect(Wrapped.propTypes).toEqual({
-        ...fontFamily.propTypes,
-        ...Component.propTypes
-      })
+      expect(Wrapped.propTypes).toEqual(fontFamily.propTypes)
     })
 
     it('renders the underlying component', () => {
@@ -71,7 +22,7 @@ describe('system props', () => {
     })
 
     const theme = {
-      space: [0, 8, 16],
+      space: [0, '8px', '16px'],
       fonts: {
         sans: 'Helvetica,Verdana,sans-serif'
       }
@@ -83,17 +34,16 @@ describe('system props', () => {
       const result = render(<Wrapped m={2} theme={theme} />)
       expect(Component.mock.calls).toHaveLength(1)
       expect(result).toMatchSnapshot()
-      expect(result).toHaveStyleRule('margin', `${theme.space[2]}px`)
+      expect(result).toHaveStyleRule('margin', theme.space[2])
     })
 
     it('applies multiple theme values', () => {
-      const Component = jest.fn(props => <b {...props} />)
+      const Component = props => <b {...props} />
       const Wrapped = withSystemProps(Component, ['fontFamily', 'space'])
       const result = render(<Wrapped fontFamily="sans" m={2} theme={theme} />)
-      expect(Component.mock.calls).toHaveLength(1)
       expect(result).toMatchSnapshot()
       expect(result).toHaveStyleRule('font-family', theme.fonts.sans)
-      expect(result).toHaveStyleRule('margin', `${theme.space[2]}px`)
+      expect(result).toHaveStyleRule('margin', theme.space[2])
     })
   })
 })
