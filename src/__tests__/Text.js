19d0f44926c67a03fd176df1c603ceccf01a56db
Shawn Allen <shawn.allen@github.com>
2018-08-08 09:43:56 -0700

    update Text tests for fontSize, default theme

--- src/__tests__/Text.js
+++ src/__tests__/Text.js
@@ -1,23 +1,7 @@
 import React from 'react'
 import Text from '../Text'
+import theme, {colors} from '../theme'
 import {render} from '../utils/testing'
-
-const theme = {
-  breakpoints: ['400px', '640px', '960px', '1280px'],
-  colors: {
-    green: ['#010', '#020', '#030', '#040', '#050', '#060']
-  },
-  fontSizes: [12, 14, 16, 20, 24, 32, 40, 48],
-  fonts: {
-    normal: 'Helvetica,sans-serif',
-    mono: 'Consolas,monospace'
-  },
-  lineHeights: {
-    normal: 1.5,
-    condensed: 1.25,
-    condensedUltra: 1
-  }
-}
 
 describe('Text', () => {
   it('is a system component', () => {
@@ -28,72 +12,52 @@ describe('Text', () => {
     expect(render(<Text />).type).toEqual('span')
   })
 
-  it('respects the is prop', () => {
+  it('respects the "is" prop', () => {
     expect(render(<Text is="b" />).type).toEqual('b')
   })
 
+  it('renders font-size', () => {
+    for (const fontSize of theme.fontSizes) {
+      expect(render(<Text fontSize={fontSize} />)).toHaveStyleRule('font-size', `${fontSize}px`)
+    }
+  })
+
   it('renders margin', () => {
-    expect(render(<Text m={1} theme={theme} />)).toHaveStyleRule('margin', '4px')
-    expect(render(<Text m={[0, 1, 2, 3, 4]} theme={theme} />)).toMatchSnapshot()
-    expect(render(<Text m={[1, 1, 3, 3]} theme={theme} />)).toMatchSnapshot()
+    expect(render(<Text m={1} />)).toHaveStyleRule('margin', '4px')
+    expect(render(<Text m={[0, 1, 2, 3, 4]} />)).toMatchSnapshot()
+    expect(render(<Text m={[1, 1, 3, 3]} />)).toMatchSnapshot()
   })
 
   it('renders padding', () => {
-    expect(render(<Text p={1} theme={theme} />)).toHaveStyleRule('padding', '4px')
-    expect(render(<Text p={[0, 1, 2, 3, 4]} theme={theme} />)).toMatchSnapshot()
-    expect(render(<Text p={[1, 1, 3, 3]} theme={theme} />)).toMatchSnapshot()
+    expect(render(<Text p={1} />)).toHaveStyleRule('padding', '4px')
+    expect(render(<Text p={[0, 1, 2, 3, 4]} />)).toMatchSnapshot()
+    expect(render(<Text p={[1, 1, 3, 3]} />)).toMatchSnapshot()
   })
 
   it('respects color', () => {
-    expect(render(<Text color="green.5" theme={theme} />)).toHaveStyleRule('color', theme.colors.green[5])
+    expect(render(<Text color="green.5" />)).toHaveStyleRule('color', colors.green[5])
+    expect(render(<Text color="#f0f" />)).toHaveStyleRule('color', '#f0f')
   })
 
   it('respects fontWeight', () => {
-    expect(render(<Text fontWeight="bold" theme={theme} />)).toHaveStyleRule('font-weight', 'bold')
-    expect(render(<Text fontWeight="normal" theme={theme} />)).toHaveStyleRule('font-weight', 'normal')
+    expect(render(<Text fontWeight="bold" />)).toHaveStyleRule('font-weight', 'bold')
+    expect(render(<Text fontWeight="normal" />)).toHaveStyleRule('font-weight', 'normal')
   })
 
   it('respects lineHeight', () => {
-    expect(render(<Text lineHeight="normal" theme={theme} />)).toHaveStyleRule(
-      'line-height',
-      String(theme.lineHeights.normal)
-    )
-    expect(render(<Text lineHeight="condensed" theme={theme} />)).toHaveStyleRule(
-      'line-height',
-      String(theme.lineHeights.condensed)
-    )
-    expect(render(<Text lineHeight="condensedUltra" theme={theme} />)).toHaveStyleRule(
-      'line-height',
-      String(theme.lineHeights.condensedUltra)
-    )
+    for (const [name, value] of Object.entries(theme.lineHeights)) {
+      expect(render(<Text lineHeight={name} />)).toHaveStyleRule('line-height', String(value))
+    }
   })
 
   it('respects fontFamily="mono"', () => {
-    expect(render(<Text fontFamily="mono" theme={theme} />)).toHaveStyleRule('font-family', theme.fonts.mono)
+    // emotion removes the whitespace between font-family values
+    const mono = theme.fonts.mono.replace(/, /g, ',')
+    expect(render(<Text fontFamily="mono" />)).toHaveStyleRule('font-family', mono)
   })
 
-  xit('respects nowrap', () => {
-    expect(render(<Text nowrap theme={theme} />)).toEqual(render(<span className="no-wrap" />))
-  })
-
-  xit('renders fontSize with f* classes using inverse scale', () => {
-    expect(render(<Text fontSize={0} theme={theme} />)).toEqual(render(<span className="f6" />))
-    expect(render(<Text fontSize={1} theme={theme} />)).toEqual(render(<span className="f5" />))
-    expect(render(<Text fontSize={2} theme={theme} />)).toEqual(render(<span className="f4" />))
-    expect(render(<Text fontSize={3} theme={theme} />)).toEqual(render(<span className="f3" />))
-    expect(render(<Text fontSize={4} theme={theme} />)).toEqual(render(<span className="f2" />))
-    expect(render(<Text fontSize={5} theme={theme} />)).toEqual(render(<span className="f1" />))
-    expect(render(<Text fontSize={6} theme={theme} />)).toEqual(render(<span className="f0" />))
-  })
-
-  xit('does not pass on arbitrary attributes', () => {
-    const defaultOutput = render(<Text />)
-    expect(render(<Text bugs="bar" />)).toEqual(defaultOutput)
-    expect(render(<Text hidden />)).toEqual(defaultOutput)
-  })
-
-  xit('respects other values for fontSize', () => {
-    expect(render(<Text fontSize="2em" theme={theme} />)).toHaveStyleRule('font-size', '2em')
-    expect(render(<Text fontSize={100} theme={theme} />)).toHaveStyleRule('font-size', '100px')
+  it('respects other values for fontSize', () => {
+    expect(render(<Text fontSize="2em" />)).toHaveStyleRule('font-size', '2em')
+    expect(render(<Text fontSize={100} />)).toHaveStyleRule('font-size', '100px')
   })
 })
