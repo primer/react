cc3f6d9fe578ca95ccc76ac2b7b6e8d350f26860
Emily <emplums@github.com>
2018-08-13 14:34:16 -0700

    update Link

--- examples/component-examples/Link.js
+++ examples/component-examples/Link.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import {LiveEditor} from '@compositor/kit'
-import {Block, Link} from '../../src'
+import {Box, Link} from '../../src'
 
 const LinkExample = {
   name: 'Link',
   element: (
     <div>
-      <Block mb={1}>
+      <Box mb={1}>
         <LiveEditor code={`<Link href="https://github.com">Link</Link>`} scope={{Link}} />
-      </Block>
-      <Block mb={1}>
+      </Box>
+      <Box mb={1}>
         <LiveEditor code={`<Link muted href="https://github.com">Link muted</Link>`} scope={{Link}} />
-      </Block>
-      <Block mb={1}>
+      </Box>
+      <Box mb={1}>
         <LiveEditor code={`<Link scheme="gray" href="https://github.com">Link gray</Link>`} scope={{Link}} />
-      </Block>
-      <Block mb={1}>
+      </Box>
+      <Box mb={1}>
         <LiveEditor code={`<Link scheme="gray-dark" href="https://github.com">Link gray-dark</Link>`} scope={{Link}} />
-      </Block>
+      </Box>
     </div>
   )
 }
