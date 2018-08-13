9aa1d2bbfc1c73f48e27da36d22af6de159b17b4
Emily <emplums@github.com>
2018-08-13 14:34:33 -0700

    update Tooltip

--- examples/component-examples/Tooltip.js
+++ examples/component-examples/Tooltip.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import {LiveEditor} from '@compositor/kit'
-import {Box, Block, Tooltip} from '../../src'
+import {BorderBox, Box, Tooltip} from '../../src'
 import ExampleHeading from '../doc-components/ExampleHeading'
 
-const basicExample = `<Box p={3}>
+const basicExample = `<BorderBox p={3}>
   <Tooltip text="Hello, Tooltip!">Text with a tooltip</Tooltip>
-</Box>`
+</BorderBox>`
 
 const directionExample = dir =>
-  `<Box p={3} my={2} key="${dir}">
+  `<BorderBox p={3} my={2} key="${dir}">
   <Tooltip text="Hello, Tooltip!" direction="${dir}">
     Tooltip direction="${dir}"
   </Tooltip>
-</Box>
+</BorderBox>
 `
 
-const wrapExample = `<Box p={3} my={2}>
+const wrapExample = `<BorderBox p={3} my={2}>
   <Tooltip
     text="Hello, Tooltip! This tooltip has a sentence that will wrap to a newline."
     wrap
@@ -24,41 +24,41 @@ const wrapExample = `<Box p={3} my={2}>
   >
     Word wrapping tooltip
   </Tooltip>
-</Box>`
+</BorderBox>`
 
 const alignmentExample = align =>
-  `<Box p={3} my={2} key="${align}">
+  `<BorderBox p={3} my={2} key="${align}">
   <Tooltip text="Hello, Tooltip!" direction="ne" align="${align}">
     Tooltip align="${align}"
   </Tooltip>
-</Box>`
+</BorderBox>`
 
-const noDelayExample = `<Box p={3} my={2}>
+const noDelayExample = `<BorderBox p={3} my={2}>
   <Tooltip noDelay text="Hello, Tooltip!">
     Text with a tooltip
   </Tooltip>
-</Box>`
+</BorderBox>`
 
 const TooltipExample = {
   name: 'Tooltip',
   element: (
     <div>
       <ExampleHeading mt={3}>Basic Tooltip</ExampleHeading>
-      <LiveEditor code={basicExample} scope={{Tooltip, Box}} />
-      <Block p={2} my={2}>
+      <LiveEditor code={basicExample} scope={{Tooltip, BorderBox}} />
+      <Box p={2} my={2}>
         <ExampleHeading mt={3}>Directions</ExampleHeading>
         {Tooltip.directions.map(dir => (
-          <LiveEditor key={dir} code={directionExample(dir)} scope={{Tooltip, Box, dir}} />
+          <LiveEditor key={dir} code={directionExample(dir)} scope={{Tooltip, BorderBox, dir}} />
         ))}
         <ExampleHeading mt={3}>Alignment</ExampleHeading>
         {Tooltip.alignments.map(align => (
-          <LiveEditor key={align} code={alignmentExample(align)} scope={{Box, Tooltip, align}} />
+          <LiveEditor key={align} code={alignmentExample(align)} scope={{BorderBox, Tooltip, align}} />
         ))}
         <ExampleHeading mt={3}>Word wrap</ExampleHeading>
-        <LiveEditor code={wrapExample} scope={{Tooltip, Box}} />
+        <LiveEditor code={wrapExample} scope={{Tooltip, BorderBox}} />
         <ExampleHeading mt={3}>No Delay</ExampleHeading>
-        <LiveEditor code={noDelayExample} scope={{Tooltip, Box}} />
-      </Block>
+        <LiveEditor code={noDelayExample} scope={{Tooltip, BorderBox}} />
+      </Box>
     </div>
   )
 }
