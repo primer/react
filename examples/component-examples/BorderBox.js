65d412b33b720b29373391f25dfa52b42e982fcc
Emily <emplums@github.com>
2018-08-13 14:54:34 -0700

    update BorderBox example copy

--- examples/component-examples/BorderBox.js
+++ examples/component-examples/BorderBox.js
@@ -7,34 +7,34 @@ const BorderBoxExample = {
   element: (
     <div>
       <Box mb={6}>
-        <LiveEditor code={`<BorderBox>This is a box</BorderBox>`} scope={{BorderBox}} />
+        <LiveEditor code={`<BorderBox>This is a BorderBox</BorderBox>`} scope={{BorderBox}} />
       </Box>
       <Box mb={6}>
-        <LiveEditor code={`<BorderBox p={2}>This is a box with padding.</BorderBox>`} scope={{BorderBox}} />
+        <LiveEditor code={`<BorderBox p={2}>This is a BorderBox with padding.</BorderBox>`} scope={{BorderBox}} />
       </Box>
       <Box mb={6}>
-        <LiveEditor code={`<BorderBox boxShadow="small" m={4} p={2}>This is a box with shadow.</BorderBox>`} scope={{BorderBox}} />
+        <LiveEditor code={`<BorderBox boxShadow="small" m={4} p={2}>This is a BorderBox with shadow.</BorderBox>`} scope={{BorderBox}} />
       </Box>
       <Box mb={6}>
         <LiveEditor
-          code={`<BorderBox boxShadow="medium" m={4} p={2}>This is a box with a medium shadow.</BorderBox>`}
+          code={`<BorderBox boxShadow="medium" m={4} p={2}>This is a BorderBox with a medium shadow.</BorderBox>`}
           scope={{BorderBox}}
         />
       </Box>
       <Box mb={6}>
         <LiveEditor
-          code={`<BorderBox boxShadow="large" m={4} p={2}>This is a box with a large shadow.</BorderBox>`}
+          code={`<BorderBox boxShadow="large" m={4} p={2}>This is a BorderBox with a large shadow.</BorderBox>`}
           scope={{BorderBox}}
         />
       </Box>
       <Box mb={6}>
         <LiveEditor
-          code={`<BorderBox boxShadow="extra-large" m={4} p={2}>This is a box with an extra-large shadow.</BorderBox>`}
+          code={`<BorderBox boxShadow="extra-large" m={4} p={2}>This is a BorderBox with an extra-large shadow.</BorderBox>`}
           scope={{BorderBox}}
         />
       </Box>
       <Box mb={6}>
-        <LiveEditor code={`<BorderBox borderColor="green.5" p={2}>This is a box with a green border.</BorderBox>`} scope={{BorderBox}} />
+        <LiveEditor code={`<BorderBox borderColor="green.5" p={2}>This is a BorderBox with a green border.</BorderBox>`} scope={{BorderBox}} />
       </Box>
     </div>
   )
