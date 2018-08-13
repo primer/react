3567c0faf9b36ff3d963d1d5b4904d599137753a
Emily <emplums@github.com>
2018-08-13 14:21:33 -0700

    fix update Box example

---
+++ examples/component-examples/BorderBox.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {LiveEditor} from '@compositor/kit'
+import {BorderBox, Box} from '../../src'
+
+const BorderBoxExample = {
+  name: 'BorderBox',
+  element: (
+    <div>
+      <Block mb={6}>
+        <LiveEditor code={`<BorderBox>This is a box</BorderBox>`} scope={{BorderBox}} />
+      </Block>
+      <Block mb={6}>
+        <LiveEditor code={`<BorderBox p={2}>This is a box with padding.</BorderBox>`} scope={{BorderBox}} />
+      </Block>
+      <Block mb={6}>
+        <LiveEditor code={`<BorderBox boxShadow="small" m={4} p={2}>This is a box with shadow.</BorderBox>`} scope={{BorderBox}} />
+      </Block>
+      <Block mb={6}>
+        <LiveEditor
+          code={`<BorderBox boxShadow="medium" m={4} p={2}>This is a box with a medium shadow.</BorderBox>`}
+          scope={{BorderBox}}
+        />
+      </Block>
+      <Block mb={6}>
+        <LiveEditor
+          code={`<BorderBox boxShadow="large" m={4} p={2}>This is a box with a large shadow.</BorderBox>`}
+          scope={{BorderBox}}
+        />
+      </Block>
+      <Block mb={6}>
+        <LiveEditor
+          code={`<BorderBox boxShadow="extra-large" m={4} p={2}>This is a box with an extra-large shadow.</BorderBox>`}
+          scope={{BorderBox}}
+        />
+      </Block>
+      <Block mb={6}>
+        <LiveEditor code={`<BorderBox borderColor="green.5" p={2}>This is a box with a green border.</BorderBox>`} scope={{BorderBox}} />
+      </Block>
+    </div>
+  )
+}
+
+export default BorderBoxExample
