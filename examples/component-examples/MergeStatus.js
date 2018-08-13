cdab5868925d551322db5946e89d55c85174eb62
Emily <emplums@github.com>
2018-08-13 14:34:21 -0700

    update MergeStatus

--- examples/component-examples/MergeStatus.js
+++ examples/component-examples/MergeStatus.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import {LiveEditor} from '@compositor/kit'
-import {MergeStatus, Block} from '../../src'
+import {MergeStatus, Box} from '../../src'
 
 const MergeStatusExample = {
   name: 'MergeStatus',
   element: (
     <div>
-      <Block mb={2}>
+      <Box mb={2}>
         <LiveEditor code={`<MergeStatus state="pending" />`} scope={{MergeStatus}} />
-      </Block>
-      <Block mb={2}>
+      </Box>
+      <Box mb={2}>
         <LiveEditor code={`<MergeStatus state="invalid" />`} scope={{MergeStatus}} />
-      </Block>
-      <Block mb={2}>
+      </Box>
+      <Box mb={2}>
         <LiveEditor code={`<MergeStatus state="merged" />`} scope={{MergeStatus}} />
-      </Block>
-      <Block mb={2}>
+      </Box>
+      <Box mb={2}>
         <LiveEditor code={`<MergeStatus state="ready" />`} scope={{MergeStatus}} />
-      </Block>
+      </Box>
     </div>
   )
 }
