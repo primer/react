4054c7d851045ae9caa5d35740726953a5576b5a
Emily <emplums@github.com>
2018-08-13 14:24:21 -0700

    rename Block to Box in BorderBox example

--- examples/component-examples/BorderBox.js
+++ examples/component-examples/BorderBox.js
@@ -6,36 +6,36 @@ const BorderBoxExample = {
   name: 'BorderBox',
   element: (
     <div>
-      <Block mb={6}>
+      <Box mb={6}>
         <LiveEditor code={`<BorderBox>This is a box</BorderBox>`} scope={{BorderBox}} />
-      </Block>
-      <Block mb={6}>
+      </Box>
+      <Box mb={6}>
         <LiveEditor code={`<BorderBox p={2}>This is a box with padding.</BorderBox>`} scope={{BorderBox}} />
-      </Block>
-      <Block mb={6}>
+      </Box>
+      <Box mb={6}>
         <LiveEditor code={`<BorderBox boxShadow="small" m={4} p={2}>This is a box with shadow.</BorderBox>`} scope={{BorderBox}} />
-      </Block>
-      <Block mb={6}>
+      </Box>
+      <Box mb={6}>
         <LiveEditor
           code={`<BorderBox boxShadow="medium" m={4} p={2}>This is a box with a medium shadow.</BorderBox>`}
           scope={{BorderBox}}
         />
-      </Block>
-      <Block mb={6}>
+      </Box>
+      <Box mb={6}>
         <LiveEditor
           code={`<BorderBox boxShadow="large" m={4} p={2}>This is a box with a large shadow.</BorderBox>`}
           scope={{BorderBox}}
         />
-      </Block>
-      <Block mb={6}>
+      </Box>
+      <Box mb={6}>
         <LiveEditor
           code={`<BorderBox boxShadow="extra-large" m={4} p={2}>This is a box with an extra-large shadow.</BorderBox>`}
           scope={{BorderBox}}
         />
-      </Block>
-      <Block mb={6}>
+      </Box>
+      <Box mb={6}>
         <LiveEditor code={`<BorderBox borderColor="green.5" p={2}>This is a box with a green border.</BorderBox>`} scope={{BorderBox}} />
-      </Block>
+      </Box>
     </div>
   )
 }
