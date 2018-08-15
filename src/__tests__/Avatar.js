51aa97dd0fd7b17dc7453145ae902b4465754071
Shawn Allen <shawn.allen@github.com>
2018-08-14 21:32:11 -0700

    update Avatar tests

--- src/__tests__/Avatar.js
+++ src/__tests__/Avatar.js
@@ -17,19 +17,32 @@ describe('Avatar', () => {
     expect(Avatar.systemComponent).toEqual(true)
   })
 
+  it('renders default props', () => {
+    expect(render(<Avatar />)).toMatchSnapshot()
+  })
+
   it('renders small by default', () => {
-    expect(renderClasses(<Avatar />)).toContain('avatar-small')
+    const size = 20
+    const result = render(<Avatar />)
+    expect(result.props.width).toEqual(size)
+    expect(result.props.height).toEqual(size)
   })
 
   it('respects the size prop', () => {
-    expect(render(<Avatar size={40} alt="github" />)).toMatchSnapshot()
+    const result = render(<Avatar size={40} alt="github" />)
+    expect(result.props.width).toEqual(40)
+    expect(result.props.height).toEqual(40)
   })
 
   it('respects isChild', () => {
-    expect(renderClasses(<Avatar isChild />)).toContain('avatar-child')
+    expect(render(<Avatar isChild />)).toMatchSnapshot()
   })
 
   it('passes through the src prop', () => {
-    expect(render(<Avatar src="primer.png" />)).toMatchSnapshot()
+    expect(render(<Avatar src="primer.png" />).props.src).toEqual('primer.png')
+  })
+
+  it('respects margin props', () => {
+    expect(render(<Avatar m={2} />)).toHaveStyleRule('margin', px(theme.space[2]))
   })
 })
