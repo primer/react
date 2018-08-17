c2e07c25f6cd270a3a31c86a73a93a1c375ed8f6
Shawn Allen <shawn.allen@github.com>
2018-08-17 01:12:50 -0700

    reorganize demos

--- examples/demos.js
+++ examples/demos.js
@@ -2,47 +2,7 @@ import React from 'react'
 import {LiveEditor} from '@compositor/kit'
 import {Box} from '../src'
 import {Library} from './doc-components'
-import MergeBox from './demos/MergeBox'
-import MergeButton from './demos/MergeButton'
-
-const examples = [
-  {
-    name: 'MergeBox',
-    element: (
-      <Box p={4}>
-        <LiveEditor
-          code={`
-<MergeBox
-  state='pending'
-  numCommits={21}
-  repoUrl='https://github.com/primer/primer-react'
-  branchName='master'
-  onMerge={() => alert('merge!')}
-/>
-            `.trim()}
-          scope={{MergeBox}}
-        />
-      </Box>
-    )
-  },
-  {
-    name: 'MergeButton',
-    element: (
-      <Box p={4}>
-        <LiveEditor
-          code={`
-<MergeButton
-  primary
-  numCommits={2}
-  onClick={() => alert('merge!')}
-/>
-            `.trim()}
-          scope={{MergeButton}}
-        />
-      </Box>
-    )
-  }
-]
+import * as examples from './demos/'
 
 export default function DemoPage(props) {
   const basename = process.env.NODE_ENV === 'development' ? '/demos/' : '/primer-react/demo/'
