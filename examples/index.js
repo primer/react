634cfa4989bf3e87477c0ed82c63903f51b97d31
Shawn Allen <shawn.allen@github.com>
2018-08-17 10:56:52 -0700

    remove home page

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,1 @@
-import React from 'react'
-import {Box, Heading} from '../src'
-
-export default function IndexPage() {
-  return (
-    <Box px={3}>
-      <Heading is="h1">Hello!</Heading>
-    </Box>
-  )
-}
+export default from './components'
