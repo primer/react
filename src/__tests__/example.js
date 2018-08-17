d87b05826257f80fd553ab71461c983b34d8f040
Shawn Allen <shawn.allen@github.com>
2018-08-17 10:18:45 -0700

    flesh out example test suite with more tests

--- src/__tests__/example.js
+++ src/__tests__/example.js
@@ -1,6 +1,28 @@
 import React from 'react'
-import {render} from '../utils/testing'
+// 1. uncomment this line and change "SomeComponent"
+// import SomeComponent from '../SomeComponent'
+import theme from '../theme'
+import {render, renderStyles} from '../utils/testing'
+import {COMMON} from '../system-props'
 
-it('Renders components', () => {
-  expect(render(<div className="foo" />)).toEqual(render(<div className="foo" />))
+// 2. remove the leading "x" from this line to enable the test
+xdescribe('SomeComponent', () => {
+
+  // if applicable, ensure that this is a "system component"
+  it('is a system component', () => {
+    expect(SomeComponent.systemComponent).toBe(true)
+  })
+
+  // ensure that it implements common props
+  it('implements common system props', () => {
+    expect(SomeComponent).toImplementSystemProps(COMMON)
+  })
+
+  // this is generally how you test that a prop renders one or more styles
+  it('renders "x" prop into styles', () => {
+    expect(renderStyles(<SomeComponent scheme="green" />)).toMatchKeys({
+      'background-color': theme.colors.green[5]
+    })
+  })
+
 })
