675b5919bfc4dd100e5ad894f55cbd2949d25e9d
Shawn Allen <shawn.allen@github.com>
2018-08-20 12:45:19 -0700

    try fixed basename

--- examples/components.js
+++ examples/components.js
@@ -4,7 +4,7 @@ import {Library} from './doc-components'
 import * as examples from './component-examples'
 
 export default function Components(props) {
-  const basename = process.env.NODE_ENV === 'development' ? '/components/' : '/primer-react/components/'
+  const basename = '/components/'
   return (
     <Library basename={basename} title="Components" examples={examples} {...props}>
       These pages demonstrate primer-react components.
--- examples/demos.js
+++ examples/demos.js
@@ -4,7 +4,7 @@ import {Library} from './doc-components'
 import * as examples from './demos/'
 
 export default function DemoPage(props) {
-  const basename = process.env.NODE_ENV === 'development' ? '/demos/' : '/primer-react/demo/'
+  const basename = 'development'
   return (
     <Library basename={basename} title="Demo Library" examples={examples} {...props}>
       These are more involved demos that illustrate how to combine primer-react components into more interesting and/or
