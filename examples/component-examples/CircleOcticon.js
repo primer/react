d6be18a790f215e54f89b3f8977532c4e5159091
Shawn Allen <shawn.allen@github.com>
2018-08-20 13:17:54 -0700

    flesh out CircleOcticon examples

--- examples/component-examples/CircleOcticon.js
+++ examples/component-examples/CircleOcticon.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import {Check} from '@githubprimer/octicons-react'
+import {Check, X, Zap} from '@githubprimer/octicons-react'
 import {LiveEditor} from '@compositor/kit'
-import {CircleOcticon} from '../../src'
+import {CircleOcticon, FlexContainer} from '../../src'
 
 export default {
   name: 'CircleOcticon',
   element: (
-    <div className="d-flex">
-      <LiveEditor
-        code={`<CircleOcticon icon={Check} size={32} bg="green.5" color="white" />`}
-        scope={{CircleOcticon, Check}}
-      />
-    </div>
+    <LiveEditor
+      code={`
+<FlexContainer>
+  <CircleOcticon icon={Check} size={32} bg="green.5" color="white" mr={2} />
+  <CircleOcticon icon={Zap} size={48} bg="blue.5" color="white" mr={2} />
+  <CircleOcticon icon={X} size={64} bg="red.5" color="white" mr={2} />
+</FlexContainer>
+        `.trim()}
+      scope={{CircleOcticon, Check, FlexContainer, X, Zap}}
+    />
   )
 }
