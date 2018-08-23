664770215bf3f822be7823f57259ed13542e6036
Shawn Allen <shawn.allen@github.com>
2018-08-23 14:02:17 -0700

    add default border props to Box per #222

--- src/Box.js
+++ src/Box.js
@@ -1,5 +1,12 @@
 import {withSystemProps, LAYOUT} from './system-props'
 
-const Box = withSystemProps('div', LAYOUT)
+const Box = withSystemProps(
+  {
+    is: 'div',
+    border: 0,
+    borderColor: 'gray.2'
+  },
+  LAYOUT
+)
 
 export default Box
