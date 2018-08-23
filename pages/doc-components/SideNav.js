0ee60fcdc56940266409eb862542ec8e05cd383d
Emily <emplums@github.com>
2018-08-23 16:34:14 -0700

    make the SideNav chill out

--- pages/doc-components/SideNav.js
+++ pages/doc-components/SideNav.js
@@ -1,35 +1,16 @@
 import React from 'react'
 import {Button, Link} from '../../src'
 
-class SideNav extends React.Component {
+const SideNav = () =>
+  <React.Fragment>
+    <nav className="menu">
+      <a className="menu-item" href="/">Home</a>
+      <a className="menu-item" onClick={this.showComponents}>Components</a>
+      {this.state.showComponents &&
+        <Link pl={4} className="menu-item" href="/components/avatar">Avatar</Link>
+      }
+      <a className="menu-item" href="/demos">Demos</a>
+    </nav>
+  </React.Fragment>
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      showComponents: false
-    }
-  }
-
-  showComponents = (e) => {
-    e.preventDefault();
-    this.setState(prevState => ({
-      showComponents: !prevState.showComponents
-    }))
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <nav className="menu">
-          <a className="menu-item" href="/">Home</a>
-          <a className="menu-item" onClick={this.showComponents}>Components</a>
-          {this.state.showComponents &&
-            <Link pl={4} className="menu-item" href="/components/avatar">Avatar</Link>
-          }
-          <a className="menu-item" href="/demos">Demos</a>
-        </nav>
-      </React.Fragment>
-    )
-  }
-}
 export default SideNav
