bd8c5176085a89cfef1e9b42fe1537521f70565e
Shawn Allen <shawn.allen@github.com>
2018-08-24 11:49:32 -0700

    simplify MergeButton even more

--- examples/doc-components/MergeButton.js
+++ examples/doc-components/MergeButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'react-emotion'
 import {withSystemProps, LAYOUT, listStyle} from '../../src/system-props'
-import {Absolute, Button, FlexContainer, PointerBox, Details, Text} from '../../src'
+import {Absolute, Box, Button, Heading, FlexContainer, PointerBox, Details, Text} from '../../src'
 
 const Arrow = styled('span')({
   display: 'inline-block',
@@ -25,16 +25,17 @@ const List = withSystemProps(
   [...LAYOUT, listStyle]
 )
 
-const ListItem = withSystemProps(
-  {
-    is: 'li',
-    borderColor: 'gray.2',
-    m: 0,
-    p: 2,
-    pl: 4
-  },
-  LAYOUT
-)
+const MergeOption = ({title, value, children, ...boxProps}) => {
+  return (
+    <Box is="label" display="block" borderColor="gray.2" p={2} pl={4} {...boxProps} css={{fontWeight: 'normal'}}>
+      <Heading fontSize={1}>{title}</Heading>
+      <Text is="div" fontSize={0}>
+        {children}
+      </Text>
+      <input type="radio" name="merge-type" value={value} hidden />
+    </Box>
+  )
+}
 
 export default function MergeButton({numCommits, onClick, primary}) {
   const borderStyles = {
@@ -63,30 +64,15 @@ export default function MergeButton({numCommits, onClick, primary}) {
             <Absolute mt={1} width={300} zIndex={99999} boxShadow="small">
               <PointerBox caret="top-left">
                 <List>
-                  <ListItem borderBottom={1}>
-                    <Text is="p" m={0} fontSize={1} fontWeight="bold">
-                      Create a merge commit
-                    </Text>
-                    <Text is="p" m={0} fontSize={0}>
-                      All commits from this branch will be added to the base branch via a merge commit.
-                    </Text>
-                  </ListItem>
-                  <ListItem borderBottom={1}>
-                    <Text is="p" m={0} fontSize={1} fontWeight="bold">
-                      Squash and merge
-                    </Text>
-                    <Text is="p" m={0} fontSize={0}>
-                      The {commits} from this branch will be combined into one commit in the base branch.
-                    </Text>
-                  </ListItem>
-                  <ListItem>
-                    <Text is="p" m={0} fontSize={1} fontWeight="bold">
-                      Rebase and merge
-                    </Text>
-                    <Text is="p" fontSize={0} m={0}>
-                      The {commits} from this branch will be rebased and added to the base branch
-                    </Text>
-                  </ListItem>
+                  <MergeOption title="Create a merge commit" value="merge" borderBottom={1}>
+                    All commits from this branch will be added to the base branch via a merge commit.
+                  </MergeOption>
+                  <MergeOption title="Squash and merge" value="squash" borderBottom={1}>
+                    The {commits} from this branch will be combined into one commit in the base branch.
+                  </MergeOption>
+                  <MergeOption title="Rebase and merge" value="rebase">
+                    The {commits} from this branch will be rebased and added to the base branch
+                  </MergeOption>
                 </List>
               </PointerBox>
             </Absolute>
