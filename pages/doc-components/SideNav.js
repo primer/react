c2af7be7bf6066bf9de83a47c1afcf401871780d
Emily <emplums@github.com>
2018-08-23 17:17:01 -0700

    update sidenav

--- pages/doc-components/SideNav.js
+++ pages/doc-components/SideNav.js
@@ -4,12 +4,8 @@ import {Button, Link} from '../../src'
 const SideNav = () =>
   <React.Fragment>
     <nav className="menu">
-      <a className="menu-item" href="/">Home</a>
-      <a className="menu-item" onClick={this.showComponents}>Components</a>
-      {this.state.showComponents &&
-        <Link pl={4} className="menu-item" href="/components/avatar">Avatar</Link>
-      }
-      <a className="menu-item" href="/demos">Demos</a>
+      <span className="menu-heading">Components</span>
+      <Link pl={4} className="menu-item" href="/components/avatar">Avatar</Link>
     </nav>
   </React.Fragment>
 
