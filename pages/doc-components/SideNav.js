b46ef725fd45548a78eeb60e93779d915287017e
Emily <emplums@github.com>
2018-08-26 14:22:05 -0700

    fix sidenav component

--- pages/doc-components/SideNav.js
+++ pages/doc-components/SideNav.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import {NavLink} from 'mdx-docs'
-import {Text, Link, Box} from '../../src'
+import Link from 'next/link'
+import {Text, Box} from '../../src'
 
 const SideNav = () =>
   <Box mt={4} ml={4}>
-    <NavLink href="/getting-started">Getting Started</NavLink>
-    <NavLink href="/system-props">System Props</NavLink>
-    <NavLink href="/primer-theme">Primer Theme</NavLink>
-    <Box mt={2} mb={3}><Text fontWeight='bold'>Components</Text></Box>
-    <Box mb={1}><Link nounderline scheme="gray-dark" pl={4} href="/components/avatar">Avatar</Link></Box>
+    <Link href="/getting-started">
+      <Text is="p" fontWeight='bold'>Getting Started</Text>
+    </Link>
+    <Link href="/system-props">
+      <Text is="p" fontWeight='bold'>System Props</Text>
+    </Link>
+    <Link href="/primer-theme">
+      <Text is="p" fontWeight='bold'>Primer Theme</Text>
+    </Link>
+    <Box mt={2} mb={3}>
+      <Text is="p" fontWeight='bold'>Components</Text>
+    </Box>
+    <Box mb={1}>
+      <Link href="/components/avatar">
+        <Text color="black" pl={4}>Avatar</Text>
+      </Link>
+    </Box>
   </Box>
 
 export default SideNav
