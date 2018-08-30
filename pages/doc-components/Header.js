db3ce6875757d685948f32445ca262e5a17a04de
Emily <emplums@github.com>
2018-08-30 10:00:59 -0700

    Header changes to new design

--- pages/doc-components/Header.js
+++ pages/doc-components/Header.js
@@ -1,44 +1,40 @@
 import Octicon, {MarkGithub} from '@githubprimer/octicons-react'
-import Link from 'next/link'
-import {UnderlineNav, UnderlineNavLink, Text, FlexContainer, Box} from '../../src'
+import NextLink from 'next/link'
+import {Text, FlexContainer, Link, Box} from '../../src'
 import {name, repository, version} from '../../package.json'
 
 const pkg = `${name}@${version}`
 const releaseURL = `https://github.com/${repository}/releases/v${version}`
 
 const Header = () =>
-  <UnderlineNav
-    actions={
-      <div>
-        <Link href="/docs">
-          <UnderlineNavLink px={4} className="d-inline-block">
-            Docs
-          </UnderlineNavLink>
-        </Link>
-        <Link href="/dev-mode">
-          <UnderlineNavLink px={4} className="d-inline-block">
-            Dev mode
-          </UnderlineNavLink>
-        </Link>
-        <Link href="/demos">
-          <UnderlineNavLink px={4} className="d-inline-block">
-            Demos
-          </UnderlineNavLink>
-        </Link>
-        <Link  href="/sandbox">
-          <UnderlineNavLink mr={0} px={4} className="d-inline-block">
-            Sandbox
-          </UnderlineNavLink>
-        </Link>
-      </div>
-    }
-    mr={1}
-  >
-    <FlexContainer alignItems="center" ml={5}>
+  <FlexContainer mr={1} p={2} alignItems="center" justifyContent="space-between" bg="black" color="white">
+    <FlexContainer alignItems="center">
       <Octicon size="medium" icon={MarkGithub} className="mr-2" />
-      <Text color="black" fontWeight="bold">Primer-react</Text>
+      <Text>Primer-react</Text>
     </FlexContainer>
-  </UnderlineNav>
+    <div>
+      <NextLink href="/docs">
+        <Link color="white" px={4} className="d-inline-block text-white">
+          Docs
+        </Link>
+      </NextLink>
+      <NextLink href="/dev-mode">
+        <Link px={4} className="d-inline-block text-white">
+          Dev mode
+        </Link>
+      </NextLink>
+      <NextLink href="/demos">
+        <Link color="white" px={4} className="d-inline-block text-white">
+          Demos
+        </Link>
+      </NextLink>
+      <NextLink  href="/sandbox">
+        <Link color="white" mr={0} px={4} className="d-inline-block text-white">
+          Sandbox
+        </Link>
+      </NextLink>
+    </div>
+  </FlexContainer>
 
 
 export default Header
