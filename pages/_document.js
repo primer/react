5c086764e68ffb4c0baefb5fb1f1ffa6f211fed6
Shawn Allen <shawn.allen@github.com>
2018-09-04 14:35:10 -0700

    lint, clean up _document wrapper

--- pages/_document.js
+++ pages/_document.js
@@ -1,47 +1,50 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import React from 'react'
+import Document, {Head, Main, NextScript} from 'next/document'
+import {ServerStyleSheet} from 'styled-components'
 
-const BaseCSS = ({ css }) =>
+/* eslint-disable react/no-danger */
+const BaseCSS = ({css}) => (
   <style
     dangerouslySetInnerHTML={{
       __html: css
     }}
   />
+)
+/* eslint-enable */
 
 BaseCSS.defaultProps = {
   css: '*{box-sizing:border-box}body{margin:0}'
 }
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
+  static getInitialProps({renderPage}) {
     const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => (
-      sheet.collectStyles(<App {...props} />)
-    ))
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
     const styles = sheet.getStyleElement()
-    return { ...page, styles }
+    return {
+      ...page,
+      styles,
+      primerCSSModules: ['buttons', 'forms', 'layout', 'navigation', 'product', 'tooltips', 'utilities']
+    }
   }
 
-  render () {
-    const { styles } = this.props
+  render() {
+    const {styles, primerCSSModules} = this.props
 
     return (
-      <html>
+      <html lang="en">
         <Head>
           <title>primer-react</title>
-          <meta name='viewport' content='width=device-width,initial-scale=1' />
-          <meta name='generator' content='Compositor X0' />
-          <link rel='stylesheet' href='https://unpkg.com/primer-buttons/build/build.css' />
-          <link rel='stylesheet' href='https://unpkg.com/primer-forms/build/build.css' />
-          <link rel='stylesheet' href='https://unpkg.com/primer-layout/build/build.css' />
-          <link rel='stylesheet' href='https://unpkg.com/primer-navigation/build/build.css' />
-          <link rel='stylesheet' href='https://unpkg.com/primer-product/build/build.css'/>
-          <link rel='stylesheet' href='https://unpkg.com/primer-tooltips/build/build.css'/>
-          <link rel='stylesheet' href='https://unpkg.com/primer-utilities/build/build.css'/>
-          <link rel='icon' href='assets/favicon.png' />
-          <link rel='apple-touch-icon' href='assets/apple-touch-icon.png' />
-          <meta name='og:title' content='Primer React' />
-          <meta name='description' content='Primer components built with React.js.' />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <meta name="generator" content="Compositor X0" />
+          <link rel="icon" href="assets/favicon.png" />
+          <link rel="apple-touch-icon" href="assets/apple-touch-icon.png" />
+          <meta name="og:title" content="Primer React" />
+          <meta name="description" content="Primer components built with React.js." />
+          {primerCSSModules &&
+            primerCSSModules.map(name => (
+              <link rel="stylesheet" href={`https://unpkg.com/${name}/build/build.css`} key={name} />
+            ))}
           <BaseCSS />
           {styles}
         </Head>
