b57f39654fd548308615dd126342fc07281ee815
Shawn Allen <shawn.allen@github.com>
2018-09-04 14:34:42 -0700

    lint, move docs theme to separate file

--- pages/_app.js
+++ pages/_app.js
@@ -1,72 +1,51 @@
 import React from 'react'
-import App, { Container } from 'next/app'
-import {ThemeProvider} from 'emotion-theming'
-import {
-  Layout,
-  Pagination
-} from 'mdx-docs'
+import App, {Container} from 'next/app'
+import {Layout} from 'mdx-docs'
 import Octicon, {iconsByName} from '@githubprimer/octicons-react'
-import * as components from '../src'
-import {theme} from '../src'
-import * as docComponents from  './doc-components'
+import * as primerComponents from '../src'
+import * as docComponents from './doc-components'
 import SideNav from './doc-components/SideNav'
 import Header from './doc-components/Header'
+import customTheme from './doc-components/theme'
 
-const iconsObject = () => {
-  let obj = {}
-  Object.keys(iconsByName).map(key => {
-    obj[iconsByName[key].name] = iconsByName[key]
-  })
-  return obj
-}
+const iconsObject = Object.keys(iconsByName).reduce((map, key) => {
+  map[iconsByName[key].name] = iconsByName[key]
+  return map
+}, {})
 
-const customTheme = {
-  font: theme.fonts.normal,
-  LiveEditor: {
-    whiteSpace: 'pre-wrap'
-  },
-  LayoutSidebar: {
-    top: '64px',
-    backgroundColor: theme.colors.gray[0]
-  },
-  NavLink: {
-    padding: '8px 0px',
-    fontSize: '16px'
-  }
-}
+const components = {...primerComponents, ...docComponents, Octicon, ...iconsObject}
+
 const routes = [
-  { name: 'Home', path: '/' },
-  { name: 'Components', path: '/components/' },
-  { name: 'Demos', path: '/demos'},
-  { name: 'Avatar', path: '/components/avatar'}
+  {name: 'Home', path: '/'},
+  {name: 'Components', path: '/components/'},
+  {name: 'Demos', path: '/demos'},
+  {name: 'Avatar', path: '/components/avatar'}
 ]
 
 export default class MyApp extends App {
-  static async getInitialProps ({ Component, router, ctx }) {
+  static async getInitialProps({Component, ctx}) {
     let page = {}
 
     if (Component.getInitialProps) {
       page = await Component.getInitialProps(ctx)
     }
 
-    return { page }
+    return {page}
   }
 
-  render () {
-    const { Component, page } = this.props
+  render() {
+    const {Component, page} = this.props
     return (
       <Container>
         <Layout
-          components={Object.assign({}, components, docComponents, {'Octicon': Octicon}, iconsObject())}
+          components={components}
           {...this.props}
           routes={routes}
           theme={customTheme}
-          header={(<Header />)}
-          sidebar={(
-            <SideNav />
-          )}
+          header={<Header />}
+          sidebar={<SideNav />}
           layoutMain
-          >
+        >
           <Component {...page} />
         </Layout>
       </Container>
---
+++ pages/doc-components/theme.js
@@ -0,0 +1,16 @@
+import {theme} from '../../src'
+
+export default {
+  font: theme.fonts.normal,
+  LiveEditor: {
+    whiteSpace: 'pre-wrap'
+  },
+  LayoutSidebar: {
+    top: '64px',
+    backgroundColor: theme.colors.gray[0]
+  },
+  NavLink: {
+    padding: '8px 0px',
+    fontSize: '16px'
+  }
+}
