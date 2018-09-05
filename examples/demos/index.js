e20de1608b8e24b46c12b9c0749d160237356176
Emily <emplums@github.com>
2018-09-05 15:43:00 -0700

    delete examples folder

--- examples/demos/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Colors} from './Colors'
-export {default as FontSizes} from './FontSizes'
-export {default as MergeBox} from './MergeBox'
-export {default as MergeButton} from './MergeButton'
