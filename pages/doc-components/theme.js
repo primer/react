edc196f7cfc0561cb80793bedc71bf8923ebe80b
Emily <emplums@github.com>
2018-09-05 13:29:26 -0700

    update LiveEditor styles

--- pages/doc-components/theme.js
+++ pages/doc-components/theme.js
@@ -2,8 +2,22 @@ import {theme} from '../../src'
 
 export default {
   font: theme.fonts.normal,
+  /* figure out how to use our theme values here */
   LiveEditor: {
-    whiteSpace: 'pre-wrap'
+    whiteSpace: 'pre-wrap',
+    backgroundColor: '#2f363d',
+    color: '#fff',
+    padding: '8px',
+    borderBottomLeftRadius: '3px',
+    borderBottomRightRadius: '3px'
+  },
+  LiveCode: {
+    border: '1px #e1e4e8 solid',
+    borderRadius: '3px'
+  },
+  LivePreview: {
+    textAlign: 'center',
+    padding: '24px'
   },
   LayoutSidebar: {
     top: '64px',
