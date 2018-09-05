53a841caf7390105a4e4f24f62acae1f56c8163c
Emily <emplums@github.com>
2018-09-05 13:21:40 -0700

    tidy up sidenav

--- pages/doc-components/SideNav.js
+++ pages/doc-components/SideNav.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import {default as NextLink} from 'next/link'
-import {Text, Box, Button, FlexContainer, Relative} from '../../src'
+import {Text, Box, Button, Link, FlexContainer, Relative} from '../../src'
 import * as docs from '../components'
 
 const SideNav = () => (
   <Relative>
-    <Box p={4} bg="gray.0" display="inline-block">
-      <FlexContainer flexDirection="column" alignItems="start" p={5} borderBottom={1} borderColor="gray.2">
+    <Box width={248} bg="gray.0" display="inline-block">
+      <FlexContainer p={4} flexDirection="column" alignItems="start" p={5} borderBottom={1} borderColor="gray.2">
         <NextLink href="/getting-started">
-          <Button linkStyle color="black" m={0} mb={3}>
+          <Link scheme="gray-dark" href="/getting-started" m={0} mb={4}>
             Getting Started
-          </Button>
+          </Link>
         </NextLink>
         <NextLink href="/system-props">
-          <Button linkStyle color="black" m={0} mb={3}>
+          <Link scheme="gray-dark" href="/system-props" m={0} mb={4}>
             System Props
-          </Button>
+          </Link>
         </NextLink>
         <NextLink href="/primer-theme">
-          <Button linkStyle color="black" m={0}>
+          <Link scheme="gray-dark" href="/getting-started" m={0}>
             Primer Theme
-          </Button>
+          </Link>
         </NextLink>
       </FlexContainer>
       <Box pt={5} pl={5}>
@@ -28,11 +28,11 @@ const SideNav = () => (
           Components
         </Text>
         {Object.values(docs).map(({displayName: name}) => (
-          <Box mb={1} key={name}>
+          <Box mb={4} key={name}>
             <NextLink href={`/components/${name}`}>
-              <Button linkStyle m={2} ml={4}>
+              <Link href={`/components/${name}`} ml={4}>
                 {name}
-              </Button>
+              </Link>
             </NextLink>
           </Box>
         ))}
