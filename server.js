3952ba2339063706fd3d08eab247b791c52c8f66
Shawn Allen <shawn.allen@github.com>
2018-09-05 21:29:13 -0700

    use app.setAssetPrefix() in the server

--- server.js
+++ server.js
@@ -9,18 +9,21 @@ const port = parseInt(process.env.PORT, 10) || 3001
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handle = app.getRequestHandler()
 
+const deployURL = process.env.NOW_URL || ''
+const deployHost = parse(deployURL).host
+
 const staticDir = join(__dirname, 'static')
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsed = parse(req.url, true)
-    const path = join(staticDir, parsed.pathname)
-    if (existsSync(path)) {
-      app.serveStatic(req, res, path)
-    } else {
-      handle(req, res, parsed)
-    }
-  }).listen(port, err => {
+    const {host} = req.headers
+    const baseURL = deployHost && host !== deployHost ? deployURL : ''
+    // set the asset prefix to the deployed URL if the Host header differs
+    app.setAssetPrefix(baseURL)
+    handle(req, res, parsed)
+  })
+  .listen(port, err => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
