6c965fd19d7ce6fa61dc2d3e355c2dba0089254e
Shawn Allen <shawn.allen@github.com>
2018-09-05 21:29:30 -0700

    get the asset prefix from __NEXT_DATA__

--- pages/_document.js
+++ pages/_document.js
@@ -5,34 +5,29 @@ import {parse} from 'url'
 // see: https://github.com/zeit/next-plugins/tree/master/packages/next-sass#usage
 import '../src/primer-react.scss'
 
-const deployURL = process.env.NOW_URL || ''
-const deployHostname = parse(deployURL).hostname
-
 export default class MyDocument extends Document {
-  static getInitialProps({renderPage, req}) {
+  static getInitialProps({renderPage, req, ...rest}) {
     const sheet = new ServerStyleSheet()
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
     const styles = sheet.getStyleElement()
-    const {hostname} = req.headers
-    const baseURL = deployHostname && hostname !== deployHostname ? deployURL : ''
-    return {
-      ...page,
-      styles,
-      baseURL
-    }
+    return {...page, styles}
   }
 
   render() {
-    const {baseURL, styles} = this.props
+    const {
+      styles,
+      // this comes from the app.setAssetPrefix() call in server.js
+      __NEXT_DATA__: {assetPrefix = ''}
+    } = this.props
 
     return (
       <html lang="en">
         <Head>
           <title>primer-react</title>
           <meta charSet="utf8" />
-          <link rel="icon" href={`${baseURL}/assets/favicon.png`} />
-          <link rel="apple-touch-icon" href={`${baseURL}/assets/apple-touch-icon.png`} />
-          <link rel="stylesheet" href={`${baseURL}/_next/static/style.css`} />
+          <link rel="icon" href={`${assetPrefix}/static/assets/favicon.png`} />
+          <link rel="apple-touch-icon" href={`${assetPrefix}/static/assets/apple-touch-icon.png`} />
+          <link rel="stylesheet" href={`${assetPrefix}/_next/static/style.css`} />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta name="generator" content="Compositor X0" />
           <meta name="og:title" content="Primer React" />
