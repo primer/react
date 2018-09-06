218d8ae2c7df5df06c44bca8a7ceb4d990493ce5
Shawn Allen <shawn.allen@github.com>
2018-09-06 09:44:53 -0700

    simplify withPlugins() call

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPlugins = require('next-compose-plugins')
 const sass = require('@zeit/next-sass')
 const mdx = require('@zeit/next-mdx')({extension: /\.mdx?$/})
 
-module.exports = withPlugins([[sass], [mdx]], {
+module.exports = withPlugins([sass, mdx], {
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   sassLoaderOptions: {
     includePaths: ['node_modules']
