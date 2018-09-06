ab6f8ae88fa14bafeb22bb813349e16758e3ef13
Emily <emplums@github.com>
2018-09-06 10:28:06 -0700

    change header buttons to links

--- pages/doc-components/Header.js
+++ pages/doc-components/Header.js
@@ -5,7 +5,7 @@ import {Text, FlexContainer, Button, Link, Sticky} from '../../src'
 
 const Header = () => (
   <Sticky zIndex={100}>
-    <FlexContainer p={1} alignItems="center" justifyContent="space-between" bg="black" color="white">
+    <FlexContainer p={3} alignItems="center" justifyContent="space-between" bg="black" color="white">
       <NextLink href="/components">
         <Link className="text-white" nounderline href="/components">
           <FlexContainer ml={4} alignItems="center" justifyContent="center">
@@ -16,19 +16,19 @@ const Header = () => (
       </NextLink>
       <div>
         <NextLink href="/components">
-          <Button linkStyle color="white" py={3} px={4}>
+          <Link className="text-white" href="/components" px={4}>
             Docs
-          </Button>
+          </Link>
         </NextLink>
         <NextLink href="/components/dev-mode">
-          <Button linkStyle color="white" py={3} px={4}>
+          <Link className="text-white" href="/components/dev-mode"px={4}>
             Dev mode
-          </Button>
+          </Link>
         </NextLink>
         <NextLink href="/components/sandbox">
-          <Button linkStyle color="white" py={3} mr={0} px={4}>
+          <Link className="text-white" href="/components/sandbox" mr={0} px={4}>
             Sandbox
-          </Button>
+          </Link>
         </NextLink>
       </div>
     </FlexContainer>
