b09f705a4c88bd6c0fd100572bdf2defe3169c1c
Shawn Allen <shawn.allen@github.com>
2018-09-06 14:36:08 -0700

    set assetPrefix in next.config.js to NOW_URL

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const sass = require('@zeit/next-sass')
 const mdx = require('@zeit/next-mdx')({extension: /\.mdx?$/})
 
 module.exports = withPlugins([sass, mdx], {
+  /*
+   * Note: Prefixing assets with the fully qualified deployment URL
+   * makes them available even when the site is served from a path alias, as in
+   * <https://primer.style/components>
+   */
+  assetPrefix: process.env.NOW_URL,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   sassLoaderOptions: {
     includePaths: ['node_modules']
