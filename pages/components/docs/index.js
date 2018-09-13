ecc4609b77226a67c6210b8290da8ac3927cf026
Emily <emplums@github.com>
2018-09-13 08:29:45 -0700

    change file extension in index

--- pages/components/docs/index.js
+++ pages/components/docs/index.js
@@ -1,6 +1,6 @@
 export {meta as Avatar} from './Avatar.md'
 export {meta as BorderBox} from './BorderBox.md'
-export {meta as Box} from './Box.js'
+export {meta as Box} from './Box.md'
 export {meta as BranchName} from './BranchName.md'
 export {meta as Buttons} from './Buttons.md'
 export {meta as CircleBadge} from './CircleBadge.md'
