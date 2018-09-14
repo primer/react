01062ea8ec6aec54afa58ed9302f774e91f0e034
Shawn Allen <shawn.allen@github.com>
2018-09-14 10:04:38 -0700

    render Emotion styles statically

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Document, {Head, Main, NextScript} from 'next/document'
+import {extractCritical} from 'emotion-server'
 
 export default class MyDocument extends Document {
+  static getInitialProps({renderPage}) {
+    const page = renderPage()
+    const {css} = extractCritical(page.html)
+    return {...page, css}
+  }
+
   render() {
     const {
+      css,
       // the assetPrefix is set in next.config.js
       __NEXT_DATA__: {assetPrefix = ''}
     } = this.props
@@ -19,6 +27,8 @@ export default class MyDocument extends Document {
           <link rel="apple-touch-icon" href={asset('/static/assets/apple-touch-icon.png')} />
           {/* See: https://github.com/zeit/next-plugins/tree/master/packages/next-sass#usage */}
           <link rel="stylesheet" href={asset('/_next/static/style.css')} />
+          {/* eslint-disable-next-line react/no-danger */}
+          <style data-emotion dangerouslySetInnerHTML={{__html: css}} />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta name="og:title" content="Primer React" />
           <meta name="description" content="Primer components built with React.js." />
