31c435e060d52640c9ea468b2a0231cf55c3ca63
Diana Mounter <broccolini@github.com>
2018-09-20 18:58:59 -0400

    hero layout tweeks

--- pages/doc-components/IndexHero.js
+++ pages/doc-components/IndexHero.js
@@ -5,12 +5,12 @@ import IndexImage from './IndexImage'
 const IndexHero = () => (
 
   <Box bg="black">
-    <Box maxWidth={1012} py={6} mb={4} mx={'auto'} px={6}>
-      <Box mt={4} mb={6}>
+    <Box maxWidth={1012} py={6} mb={3} mx={'auto'} px={6}>
+      <Box mt={4} mb={4}>
         <Heading color="blue.4" fontSize={7} pb={3}>Primer Components</Heading>
         <Text color="blue.2" fontSize={2} fontWeight={400} className="text-mono">v2.0.4-beta</Text>
       </Box>
-      <Box mb={6} mx={-4}>
+      <Box mb={6} >
         <IndexImage />
       </Box>
     </Box>
