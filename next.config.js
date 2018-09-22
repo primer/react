abf69b208636f7a1bce72c64772ff1fa0268432c
Shawn Allen <shawn.allen@github.com>
2018-09-21 21:09:16 -0700

    move assetPrefix into publicRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -4,15 +4,10 @@ const mdx = require('@zeit/next-mdx')
 module.exports = withPlugins([
   mdx({extension: /\.mdx?$/})
 ], {
-  /*
-   * Note: Prefixing assets with the fully qualified deployment URL
-   * makes them available even when the site is served from a path alias, as in
-   * <https://primer.style/components>
-   */
-  assetPrefix: process.env.NOW_URL,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  sassLoaderOptions: {
-    includePaths: ['node_modules']
+
+  publicRuntimeConfig: {
+    assetPrefix: process.env.NOW_URL
   },
 
   webpack(config) {
