a3c0793fd3832b639c968a3e08143797f03b9673
Shawn Allen <shawn.allen@github.com>
2018-09-21 21:25:45 -0700

    lint

--- pages/doc-components/_utils.js
+++ pages/doc-components/_utils.js
@@ -1,9 +1,7 @@
 import getConfig from 'next/config'
 
-export const assetPath = (
-  getConfig().publicRuntimeConfig.assetPrefix || ''
-) + '/static/assets/'
+export const assetPath = `${getConfig().publicRuntimeConfig.assetPrefix || ''}/static/assets/`
 
 export function getAssetPath(path) {
-  return assetPath + path
+  return `${assetPath}${path}`
 }
