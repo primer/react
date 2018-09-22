1cf3b4025d095b843d5b3ca954dbdf4864da2ea1
Shawn Allen <shawn.allen@github.com>
2018-09-21 21:24:13 -0700

    add pages/doc-components/_utils with getAssetPath()

---
+++ pages/doc-components/_utils.js
@@ -0,0 +1,9 @@
+import getConfig from 'next/config'
+
+export const assetPath = (
+  getConfig().publicRuntimeConfig.assetPrefix || ''
+) + '/static/assets/'
+
+export function getAssetPath(path) {
+  return assetPath + path
+}
