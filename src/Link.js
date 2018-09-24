d4a4835c112bd5212e2fe373a63434486d16a6c7
Emily <emplums@github.com>
2018-09-24 15:18:54 -0700

    set default color to blue.4 and remove other color props

--- src/Link.js
+++ src/Link.js
@@ -5,12 +5,11 @@ import {themeGet} from 'styled-system'
 import {withSystemProps, COMMON} from './system-props'
 import {colors} from './theme'
 
-const styledLink = styled(Link)`
+const styledLink = styled('a')`
   ${textDecoration};
-  &:hover {
+    &:hover {
     text-decoration: underline;
   }
-  ${color};
 `
 
 function textDecoration({underline}) {
@@ -19,30 +18,7 @@ function textDecoration({underline}) {
   }
 }
 
-function color({muted, scheme, ...rest}) {
-  return {
-    color:
-      scheme === 'gray-dark'
-        ? themeGet('colors.gray.9', colors.gray[9])(rest)
-        : muted || scheme === 'gray'
-          ? themeGet('colors.gray.6', colors.gray[6])(rest)
-          : themeGet('colors.blue.5', colors.blue[5])(rest)
-  }
-}
-
-function Link({children, className, ...rest}) {
-  return (
-    <a className={className} {...rest}>
-      {children}
-    </a>
-  )
-}
-
-Link.propTypes = {
-  href: PropTypes.string,
-  muted: PropTypes.bool,
-  underline: PropTypes.bool,
-  scheme: PropTypes.oneOf(['gray', 'gray-dark'])
-}
-
-export default withSystemProps(styledLink, COMMON)
+export default withSystemProps({
+  is: styledLink,
+  color: 'blue.4'
+}, COMMON)
