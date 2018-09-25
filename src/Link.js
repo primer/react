e2d2a53da6011ac1275316026838b241c03d1f31
Emily <emplums@github.com>
2018-09-25 14:40:50 -0700

    give Link both COMMON and TYPOGRAPHY system props

--- src/Link.js
+++ src/Link.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'react-emotion'
 import {themeGet} from 'styled-system'
-import {withSystemProps, COMMON} from './system-props'
+import {withSystemProps, TYPOGRAPHY} from './system-props'
 import {colors} from './theme'
 
 const styledLink = styled('a')`
@@ -21,4 +21,4 @@ function textDecoration({underline}) {
 export default withSystemProps({
   is: styledLink,
   color: 'blue.4'
-}, COMMON)
+}, TYPOGRAPHY)
