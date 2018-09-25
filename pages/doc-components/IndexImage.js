4fea8a0936f9e05e062eac9e73972b5b6993b30e
Emily <emplums@github.com>
2018-09-25 16:31:07 -0700

    undo accidental index image changes

--- pages/doc-components/IndexImage.js
+++ pages/doc-components/IndexImage.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const IndexImage = () => {
   let assetPrefix = ''
-  if (process.env.NODE_ENV === 'production') {
-    assetPrefix = 'primer-components.now.sh'
+  const nowData = global.__NOW_DATA__
+  if (nowData) {
+    assetPrefix = nowData.assetPrefix
   }
   const asset = path => assetPrefix + path
   return <img alt="" src={asset('/static/assets/primer-components.svg')} />
