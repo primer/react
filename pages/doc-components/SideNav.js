79106ff6983063363adc879e57b93c33522d66bc
Emily <emplums@github.com>
2018-09-25 15:06:37 -0700

    lint

--- pages/doc-components/SideNav.js
+++ pages/doc-components/SideNav.js
@@ -8,12 +8,12 @@ const SideNav = () => (
     <Box width={256} height="100%" bg="gray.0" display="inline-block" borderRight={1} borderColor="gray.2">
       <FlexContainer flexDirection="column" alignItems="start" p={5} borderBottom={1} borderColor="gray.2">
         <NextLink href="/components/docs/system-props">
-          <Link color='gray.9' href="/components/docs/system-props" m={0} mb={4}>
+          <Link color="gray.9" href="/components/docs/system-props" m={0} mb={4}>
             System Props
           </Link>
         </NextLink>
         <NextLink href="/components/docs/primer-theme">
-          <Link color='gray.9' href="/components/docs/primer-theme" m={0}>
+          <Link color="gray.9" href="/components/docs/primer-theme" m={0}>
             Primer Theme
           </Link>
         </NextLink>
@@ -21,7 +21,7 @@ const SideNav = () => (
       <Box pt={5} pl={5}>
         <Text is="p" color="black" m={0} mb={3}>
           <NextLink href="/components/docs/Avatar">
-            <Link color='gray.9' href="/components/docs/Avatar">
+            <Link color="gray.9" href="/components/docs/Avatar">
               Components
             </Link>
           </NextLink>
