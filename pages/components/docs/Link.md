0c7992bc00e0fc2137661272512e9e0e9eea4cd4
Emily <emplums@github.com>
2018-09-25 16:30:09 -0700

    update docs

--- pages/components/docs/Link.md
+++ pages/components/docs/Link.md
@@ -15,7 +15,6 @@ Link components get `COMMON` and `TYPOGRAPHY` system props. Read our [System Pro
 | Prop name | Type | Description |
 | :- | :- | :- |
 | href | String | URL to be used for the Link |
-| nounderline | Boolean | Removes underline on hover |
 | underline | Boolean | Adds underline to the Link |
 
 export const meta = {displayName: 'Link'}
--- pages/doc-components/IndexImage.js
+++ pages/doc-components/IndexImage.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 const IndexImage = () => {
   let assetPrefix = ''
-  const nowData = global.__NOW_DATA__
-  if (nowData) {
-    assetPrefix = nowData.assetPrefix
+  if (process.env.NODE_ENV === 'production') {
+    assetPrefix = 'primer-components.now.sh'
   }
   const asset = path => assetPrefix + path
   return <img alt="" src={asset('/static/assets/primer-components.svg')} />
