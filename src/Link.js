e8a0e7a578e68855c24711abcdbfd8187ab0a9f3
Emily <emplums@github.com>
2018-10-05 11:44:00 -0700

    remove unecessary get import

--- src/Link.js
+++ src/Link.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import styled from 'react-emotion'
 import {withSystemProps, TYPOGRAPHY} from './system-props'
 import theme from './theme'
-import {themeGet, get} from 'styled-system'
+import {themeGet} from 'styled-system'
 
 const getHoverColor = (hoverColor = 'blue.5') => {
   const path = `colors.${hoverColor}`
