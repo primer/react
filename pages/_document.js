c65fafb38444cef56e4bf59e2b08613b506fec92
Emily Brick <emilybrick@github.com>
2018-10-09 18:38:16 -0400

    Update og:image for primer components (#303)
    * updated og metadata for primer components
    
    * fixed jsx closing tag
    
    * testing-image
    
    * testing prettier
    
    * ran linter
    
    * linting errors
    
    * updated image and url
    
    * fixed og url
    
    * updated image
    
    * updated og image

--- pages/_document.js
+++ pages/_document.js
@@ -37,8 +37,13 @@ export default class MyDocument extends Document {
           <link rel="icon" href={getAssetPath('favicon.png')} />
           <link rel="apple-touch-icon" href={getAssetPath('apple-touch-icon.png')} />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <meta name="og:title" content="Primer React" />
+          <meta name="og:title" content="Primer Components" />
+          <meta name="og:url" content="https://primer.style/components/" />
           <meta name="description" content="Primer components built with React.js." />
+          <meta
+            name="og:image"
+            content="https://user-images.githubusercontent.com/586552/46702062-ca82eb80-cbef-11e8-8c0b-4a9252dc04c6.png"
+          />
           {styles}
         </Head>
         <body>
