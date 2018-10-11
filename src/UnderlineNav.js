35de15778dfff4cd4c2eb1d4706166def4be9840
Emily <emplums@github.com>
2018-10-11 11:44:35 -0700

    rename UnderlineNavLink to UnderlineNav.Link

--- src/UnderlineNav.js
+++ src/UnderlineNav.js
@@ -10,8 +10,8 @@ injectGlobal(sass`
   @import "primer-navigation/lib/underline-nav.scss";
 `)
 
-export const ITEM_CLASS = 'UnderlineNav-item no-underline'
-export const SELECTED_CLASS = 'selected'
+const ITEM_CLASS = 'UnderlineNav-item no-underline'
+const SELECTED_CLASS = 'selected'
 
 function UnderlineNav({actions, className, align, children, full, label}) {
   const classes = classnames(className, 'UnderlineNav', align && `UnderlineNav--${align}`, full && 'UnderlineNav--full')
@@ -23,9 +23,15 @@ function UnderlineNav({actions, className, align, children, full, label}) {
   )
 }
 
-// make it possible to destructure these from UnderlineNav:
-// const {ITEM_CLASS} = UnderlineNav
-Object.assign(UnderlineNav, {ITEM_CLASS, SELECTED_CLASS})
+const UnderlineNavLink = ({className, selected, is: Tag, ...rest}) => {
+  const classes = classnames(ITEM_CLASS, selected && SELECTED_CLASS, className)
+
+  if (typeof rest.to === 'string') {
+    rest.activeClassName = SELECTED_CLASS
+  }
+
+  return <Tag className={classes} {...rest} />
+}
 
 UnderlineNav.propTypes = {
   actions: PropTypes.node,
@@ -34,5 +40,17 @@ UnderlineNav.propTypes = {
   full: PropTypes.bool,
   label: PropTypes.string
 }
+
+UnderlineNavLink.defaultProps = {
+  is: 'a'
+}
+
+UnderlineNavLink.propTypes = {
+  is: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+  selected: PropTypes.bool
+}
+
+UnderlineNav.Link = withSystemProps(UnderlineNavLink, COMMON)
+
 
 export default withSystemProps(UnderlineNav, COMMON)
