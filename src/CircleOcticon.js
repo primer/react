9141a68be58b584f057e021a1d59e21f7a6d0268
Emily <emplums@github.com>
2018-10-24 17:33:50 -0700

    set bg on CircleOcticon

--- src/CircleOcticon.js
+++ src/CircleOcticon.js
@@ -6,9 +6,9 @@ import BorderBox from './BorderBox'
 
 function CircleOcticon(props) {
   const {size} = props
-  const {icon, ...rest} = props
+  const {icon, bg, ...rest} = props
   return (
-    <BorderBox overflow='hidden' border='none' size={size} borderRadius='50%'>
+    <BorderBox bg={bg} overflow='hidden' border='none' size={size} borderRadius='50%'>
       <Flex {...rest} alignItems="center" justifyContent="center">
           <Octicon icon={icon} size={size} />
       </Flex>
