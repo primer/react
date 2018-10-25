8f59e16bfd95fd04ecc933f9baa8334edb48744b
Emily <emplums@github.com>
2018-10-25 10:44:14 -0700

    remove linkStyle

--- src/Button.js
+++ src/Button.js
@@ -9,12 +9,11 @@ injectGlobal(sass`
   @import "primer-buttons/index.scss";
 `)
 
-function Button({is: Tag, children, size, linkStyle, grouped, scheme, onClick, disabled, className, ...rest}) {
+function Button({is: Tag, children, size, grouped, scheme, onClick, disabled, className, ...rest}) {
   const classes = classnames(
     className,
+    'btn',
     {
-      btn: !linkStyle,
-      'btn-link': linkStyle,
       'btn-sm': size === 'sm',
       'btn-large': size === 'large',
       'BtnGroup-item': grouped
@@ -38,7 +37,6 @@ Button.propTypes = {
   disabled: PropTypes.bool,
   grouped: PropTypes.bool,
   is: PropTypes.oneOfType([PropTypes.oneOf(['button', 'a', 'summary', 'input']), PropTypes.func]),
-  linkStyle: PropTypes.bool,
   onClick: PropTypes.func,
   scheme: PropTypes.string,
   size: PropTypes.oneOf(['sm', 'large'])
