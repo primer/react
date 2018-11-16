02ef566dd50943ab7881aac19f9c1def87ad38c9
Emily <emplums@github.com>
2018-11-16 10:51:59 -0800

    lint

--- pages/doc-components/PropsList.js
+++ pages/doc-components/PropsList.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const PropsList = ({systemProps}) => <div>{systemProps.join(', ')}</div>
 
 PropsList.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.string)
+  systemProps: PropTypes.arrayOf(PropTypes.string)
 }
 
 export default PropsList
