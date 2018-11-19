d16d2524bb5af9c1da811de78f48f4bb4ef9f93c
Emily <emplums@github.com>
2018-11-19 14:17:09 -0800

    without system-components
    This works, but props related to CSS are not getting removed from the rendered HTML

--- src/BorderBox.js
+++ src/BorderBox.js
@@ -1,18 +1,30 @@
 import {withSystemProps} from './system-props'
+import {borders, borderColor, fontSize, borderRadius, boxShadow} from 'styled-system'
+import styled from 'react-emotion'
+import theme, {colors} from './theme'
 import Box from './Box'
 
-const BorderBox = withSystemProps(
-  {
-    is: Box,
-    border: 1,
-    borderColor: 'gray.2',
-    borderRadius: 1
-  },
-  ['boxShadow']
-)
+const BorderBox = styled(Box)`
+ ${borders}
+ ${borderColor}
+ ${borderRadius}
+ ${boxShadow}
+`
 
+// set default props in defaultProps instead of in call to withSystemProps
+BorderBox.defaultProps = {
+  border: theme.borders[1],
+  borderColor: colors.gray[2],
+  borderRadius: theme.radii[1],
+  theme // set our theme to the default if no theme is provided
+}
+
+// spread prop types here
 BorderBox.propTypes = {
-  ...Box.propTypes
+  ...Box.propTypes,
+  ...borders.propTypes,
+  ...borderColor.propTypes,
+  ...boxShadow.propTypes,
 }
 
 export default BorderBox
