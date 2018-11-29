d40f580bf52fe030f3d840cc926f016c03bdacae
Emily <emplums@github.com>
2018-11-29 09:51:35 -0800

    refactor Box

--- src/Box.js
+++ src/Box.js
@@ -1,5 +1,17 @@
-import {withSystemProps, LAYOUT, COMMON} from './system-props'
+import styled from 'styled-components'
+import {LAYOUT, COMMON} from './constants'
+import theme from './theme'
 
-const Box = withSystemProps('div', [...LAYOUT, ...COMMON])
+const Box = styled.div`
+  ${LAYOUT}
+  ${COMMON}
+`
+
+Box.defaultProps = {theme}
+
+Box.propTypes = {
+  ...LAYOUT.propTypes,
+  ...COMMON.propTypes
+}
 
 export default Box
