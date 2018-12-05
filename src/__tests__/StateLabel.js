4b2a4049e795d1dd16a4e2de4f3fc39ba4cc4fcc
Emily <emplums@github.com>
2018-12-05 11:57:43 -0800

    refactor StateLabel

--- src/__tests__/StateLabel.js
+++ src/__tests__/StateLabel.js
@@ -1,35 +1,31 @@
 import React from 'react'
 import StateLabel from '../StateLabel'
-import {render} from '../utils/testing'
+import {render, mount} from '../utils/testing'
 import {COMMON} from '../system-props'
 
 describe('StateLabel', () => {
-  it('is a system component', () => {
-    expect(StateLabel.systemComponent).toEqual(true)
-  })
-
   it('implements common system props', () => {
     expect(StateLabel).toImplementSystemProps(COMMON)
   })
 
   it('respects the status prop', () => {
-    expect(render(<StateLabel status="issueOpened" />)).toMatchSnapshot()
-    expect(render(<StateLabel status="issueClosed" />)).toMatchSnapshot()
-    expect(render(<StateLabel status="pullMerged" />)).toMatchSnapshot()
+    expect(render(mount(<StateLabel status="issueOpened" />))).toMatchSnapshot()
+    expect(render(mount(<StateLabel status="issueClosed" />))).toMatchSnapshot()
+    expect(render(mount(<StateLabel status="pullMerged" />))).toMatchSnapshot()
   })
 
   it('respects the small flag', () => {
-    expect(render(<StateLabel small />)).toMatchSnapshot()
-    expect(render(<StateLabel small={false} />)).toMatchSnapshot()
+    expect(render(mount(<StateLabel small />))).toMatchSnapshot()
+    expect(render(mount(<StateLabel small={false} />))).toMatchSnapshot()
   })
 
   it('renders children', () => {
-    expect(render(<StateLabel>hi</StateLabel>)).toMatchSnapshot()
+    expect(render(mount(<StateLabel>hi</StateLabel>))).toMatchSnapshot()
   })
 
   it('does not pass on arbitrary attributes', () => {
     const defaultOutput = render(<StateLabel />)
-    expect(render(<StateLabel data-foo="bar" />)).toEqual(defaultOutput)
-    expect(render(<StateLabel hidden />)).toEqual(defaultOutput)
+    expect(render(mount(<StateLabel data-foo="bar" />))).toEqual(defaultOutput)
+    expect(render(mount(<StateLabel hidden />))).toEqual(defaultOutput)
   })
 })
