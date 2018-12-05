872699738fbf8324d2eb0b834b9ee7c2fc84b590
Emily <emplums@github.com>
2018-12-05 10:56:21 -0800

    removed OcticonButton component

--- src/__tests__/OcticonButton.js
+++ src/__tests__/OcticonButton.js
@@ -1,38 +1,34 @@
 import React from 'react'
 import OcticonButton from '../OcticonButton'
-import {render} from '../utils/testing'
+import {render, mount} from '../utils/testing'
 import {COMMON} from '../system-props'
 
 const Circle = ({r = 8}) => <circle cx={r} cy={r} r={r} />
 Circle.size = [16, 16]
 
 describe('OcticonButton', () => {
-  it('is a system component', () => {
-    expect(OcticonButton.systemComponent).toEqual(true)
-  })
-
   it('implements common system props', () => {
     expect(OcticonButton).toImplementSystemProps(COMMON)
   })
 
   it('renders a button with an Octicon', () => {
-    expect(render(<OcticonButton icon={Circle} label="circle" />)).toMatchSnapshot()
+    expect(render(mount(<OcticonButton icon={Circle} label="circle" />))).toMatchSnapshot()
   })
 
   it('respects the "disabled" prop', () => {
-    expect(render(<OcticonButton icon={Circle} disabled label="circle" />).props.disabled).toBe(true)
+    expect(render(mount(<OcticonButton icon={Circle} disabled label="circle" />)).props.disabled).toBe(true)
   })
 
   it('sets "aria-label" to the "label" prop', () => {
-    expect(render(<OcticonButton icon={Circle} label="circle" />).props['aria-label']).toEqual('circle')
+    expect(render(mount(<OcticonButton icon={Circle} label="circle" />)).props['aria-label']).toEqual('circle')
   })
 
   it('passes the onClick handler to the <button>', () => {
     function click() {}
-    expect(render(<OcticonButton icon={Circle} onClick={click} label="circle" />).props.onClick).toEqual(click)
+    expect(render(mount(<OcticonButton icon={Circle} onClick={click} label="circle" />)).props.onClick).toEqual(click)
   })
 
   it('passes the "size" prop to the Octicon', () => {
-    expect(render(<OcticonButton icon={Circle} size={128} label="circle" />)).toMatchSnapshot()
+    expect(render(mount(<OcticonButton icon={Circle} size={128} label="circle" />))).toMatchSnapshot()
   })
 })
