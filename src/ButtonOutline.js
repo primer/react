509c954224b25508d8213a294c4522db058c8faa
Emily <emplums@github.com>
2018-12-23 12:01:17 -0800

    ButtonOutline

--- src/ButtonOutline.js
+++ src/ButtonOutline.js
@@ -1,6 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 import Button from './Button'
 
-const ButtonOutline = props => <Button {...props} scheme="outline" />
+const ButtonOutline = styled(Button)`
+  color: rgb(3, 102, 214);
+  background-color: rgb(255, 255, 255);
+  background-image: none;
+
+  &:hover {
+    color: rgb(255, 255, 255);
+    background-color: rgb(3, 102, 214);
+    background-image: none;
+    border-color: rgb(3, 102, 214);
+  }
+
+  &:active {
+    color: rgb(255, 255, 255);
+    background-color: rgb(3, 102, 214);
+    background-image: none;
+    border-color: rgb(3, 102, 214);
+  }
+
+  &:focus {
+    box-shadow: rgba(3, 102, 214, 0.4) 0px 0px 0px 0.2em;
+    border-color: rgb(3, 102, 214);
+  }
+`
 
 export default ButtonOutline
