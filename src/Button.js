cd32e9ce33067cfe75209615624d43e6efe23f76
Emily <emplums@github.com>
2018-12-23 11:37:24 -0800

    refactor Button

--- src/Button.js
+++ src/Button.js
@@ -1,42 +1,54 @@
 import React from 'react'
-import classnames from 'classnames'
 import PropTypes from 'prop-types'
-import sass from 'sass.macro'
-import styled, {createGlobalStyle} from 'styled-components'
-import {COMMON} from './constants'
+import styled from 'styled-components'
+import {COMMON, Base} from './constants'
 import theme from './theme'
-import {width} from 'styled-system'
+import {width, themeGet} from 'styled-system'
 
-const GlobalStyles = createGlobalStyle`
-  ${sass`
-    @import "primer-buttons/index.scss";
-  `}
-`
 
-function ButtonBase({is: Tag, children, theme, size, grouped, scheme, onClick, disabled, className, ...rest}) {
-  const classes = classnames(
-    className,
-    'btn',
-    {
-      'btn-sm': size === 'sm',
-      'btn-large': size === 'large',
-      'BtnGroup-item': grouped
-    },
-    scheme ? `btn-${scheme}` : null
-  )
+const Button = styled(Base)`
+  position: relative;
+  display: inline-block;
+  padding: 6px 12px;
+  color: rgb(36, 41, 46);
+  background-color: rgb(239, 243, 246);
+  background-image: linear-gradient(-180deg, rgb(250, 251, 252) 0%, rgb(239, 243, 246) 90%);
+  font-size: ${themeGet('fontSizes.1', theme.fontSizes[1])}px;
+  font-weight: ${themeGet('fontWeights.bold', theme.fontWeights.bold)};
+  line-height: 20px;
+  white-space: nowrap;
+  vertical-align: middle;
+  cursor: pointer;
+  user-select: none;
+  background-repeat: repeat-x;
+  background-position: -1px -1px;
+  background-size: 110% 110%;
+  border: 1px solid rgba(27, 31, 35, 0.2);
+  border-radius: 0.25em;
+  appearance: none;
+  ${COMMON}
+  ${width}
 
-  return (
-    <React.Fragment>
-      <GlobalStyles />
-      <Tag {...rest} type="button" disabled={disabled} onClick={disabled ? undefined : onClick} className={classes}>
-        {children}
-      </Tag>
-    </React.Fragment>
-  )
-}
+  &:hover {
+    background-color: rgb(230, 235, 241);
+    background-image: linear-gradient(-180deg, rgb(240, 243, 246) 0%, rgb(230, 235, 241) 90%);
+    background-position: -0.5em center;
+    border-color: rgba(27, 31, 35, 0.35);
+    text-decoration: none;
+    background-repeat: repeat-x;
+  }
 
-const Button = styled(ButtonBase)`
-  ${COMMON} ${width};
+  &:active {
+    background-color: rgb(233, 236, 239);
+    background-image: none;
+    box-shadow: rgba(27, 31, 35, 0.15) 0px 0.15em 0.3em inset;
+    border-color: rgba(27, 31, 35, 0.35);
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 0.2em;
+  }
 `
 
 Button.defaultProps = {
