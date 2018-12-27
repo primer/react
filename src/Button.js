787841c3c38d484c47f7b9d6422fb1c08a69e43a
Emily <emplums@github.com>
2018-12-27 15:54:33 -0800

    use polish for old sass functions

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import {darken, desaturate, transparentize} from 'polished'
 import {COMMON, Base} from './constants'
-import theme from './theme'
+import theme, {colors} from './theme'
 import {width, themeGet} from 'styled-system'
 
 function fontSize({size = '14px'}) {
@@ -14,50 +15,73 @@ function fontSize({size = '14px'}) {
 }
 
 
-const Button = styled(Base)`
-  position: relative;
-  display: inline-block;
-  padding: 6px 12px;
-  color: rgb(36, 41, 46);
-  background-color: rgb(239, 243, 246);
-  background-image: linear-gradient(-180deg, rgb(250, 251, 252) 0%, rgb(239, 243, 246) 90%);
-  font-weight: ${themeGet('fontWeights.bold', theme.fontWeights.bold)};
-  line-height: 20px;
-  white-space: nowrap;
-  vertical-align: middle;
-  cursor: pointer;
-  user-select: none;
-  background-repeat: repeat-x;
-  background-position: -1px -1px;
-  background-size: 110% 110%;
-  border: 1px solid rgba(27, 31, 35, 0.2);
-  border-radius: 0.25em;
-  appearance: none;
-  ${fontSize};
-  ${COMMON};
-  ${width};
 
-  &:hover {
-    background-color: rgb(230, 235, 241);
-    background-image: linear-gradient(-180deg, rgb(240, 243, 246) 0%, rgb(230, 235, 241) 90%);
-    background-position: -0.5em center;
-    border-color: rgba(27, 31, 35, 0.35);
-    text-decoration: none;
+const Button = styled(Base)((props) => {
+  const color = props.theme.colors.gray[9]
+  const bg = props.theme.colors.gray[0]
+  const bg2 = darken(0.02, props.theme.colors.gray[1])
+  const black = props.theme.colors.black
+
+  return `
+    position: relative;
+    display: inline-block;
+    padding: 6px 12px;
+    color: ${color};
+    background-color: ${bg2};
+    background-image: linear-gradient(-180deg, ${bg} 0%, ${bg2} 90%);
+    font-size: ${theme.fontSizes[1]}px;
+    font-weight: ${theme.fontWeights.bold};
+    line-height: 20px;
+    white-space: nowrap;
+    vertical-align: middle;
+    cursor: pointer;
+    user-select: none;
     background-repeat: repeat-x;
-  }
+    background-position: -1px -1px;
+    background-size: 110% 110%;
+    border: 1px solid ${transparentize(0.8, black)};
+    border-radius: 0.25em;
+    appearance: none;
+    ${fontSize};
+    ${COMMON};
+    ${width};
 
-  &:active {
-    background-color: rgb(233, 236, 239);
-    background-image: none;
-    box-shadow: rgba(27, 31, 35, 0.15) 0px 0.15em 0.3em inset;
-    border-color: rgba(27, 31, 35, 0.35);
-  }
+    &:hover {
+      background-color: ${darken(0.03, bg2)};
+      background-image: linear-gradient(-180deg, ${darken(0.03, bg)} 0%, ${darken(0.03, bg2)} 90%);
+      background-position: -0.5em center;
+      border-color: rgba(27, 31, 35, 0.35);
+      text-decoration: none;
+      background-repeat: repeat-x;
+    }
 
-  &:focus {
-    outline: none;
-    box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 0.2em;
-  }
-`
+    &:active {
+      background-color: ${darken(0.06, desaturate(0.1, bg))};
+      background-image: none;
+      box-shadow: rgba(27, 31, 35, 0.15) 0px 0.15em 0.3em inset; //$btn-active-shadow;
+      border-color: rgba(27, 31, 35, 0.35); //convert black to rbg here
+    }
+
+    &:selected {
+      background-color: ${darken(0.06, desaturate(0.1, bg))};
+      background-image: none;
+      box-shadow: rgba(27, 31, 35, 0.15) 0px 0.15em 0.3em inset; //$btn-active-shadow;
+      border-color: rgba(27, 31, 35, 0.35); //convert black to rbg here
+    }
+
+    &:disabled {
+      color: rgba(${color}, 0.4);
+      background-color: ${bg2};
+      background-image: none;
+      border-color: ${transparentize(0.8, black)}
+      box-shadow: none;
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 0.2em;
+    }`
+})
 
 Button.defaultProps = {
   is: 'button',
