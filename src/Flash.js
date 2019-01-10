06f0764a4904177565340b0c56742b1ada1062f6
Emily <emplums@github.com>
2019-01-10 12:13:51 -0800

    Flash refactor

--- src/Flash.js
+++ src/Flash.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
-import sass from 'sass.macro'
-import {injectGlobal} from 'emotion'
 import styled from 'styled-components'
-import {COMMON} from './constants'
+import {COMMON, get} from './constants'
 import theme from './theme'
 
-injectGlobal(sass`
-  @import "primer-alerts/index.scss";
-`)
-
 const schemeMap = {
-  green: 'success',
-  red: 'error',
-  yellow: 'warn'
+  green: {color: 'colors.green.8', bg: 'colors.green.1', borderColor: 'colors.blackfade15'},
+  red: {color: 'colors.red.9', bg: 'colors.red.1', borderColor: 'colors.blackfade15'},
+  yellow: {color: 'colors.yellow.9', bg: 'colors.yellow.1', borderColor: 'colors.blackfade15'},
+  base: {color: 'colors.blue.8', bg: 'colors.blue.1', borderColor: 'colors.blackfade15'} //TODO put in correct border color
 }
 
-function FlashBase({children, className, full, scheme}) {
-  const classes = classnames(className, 'flash', full && 'flash-full', scheme && `flash-${schemeMap[scheme]}`)
-  return <div className={classes}>{children}</div>
-}
+const Flash = styled.div`
+  position: relative;
+  padding: ${get('space.3')}px;
+  color: ${props => get(schemeMap[props.scheme] ? schemeMap[props.scheme].color : '')};
+  background-color: ${props => get(schemeMap[props.scheme] ? schemeMap[props.scheme].bg : '')};
+  border-color: ${props => get(schemeMap[props.scheme] ? schemeMap[props.scheme].borderColor : '')};
+  border-style: solid;
+  border-width: ${props => props.full ? '1px 0px' : '1px'};
+  border-radius: ${props => props.full ? '0' : '3px'};
 
-const Flash = styled(FlashBase)(COMMON)
+  p:last-child {
+    margin-bottom: 0;
+  }
+  ${COMMON}
+`
 
 Flash.defaultProps = {
-  theme
+  theme,
+  scheme: 'base'
 }
 
 Flash.propTypes = {
