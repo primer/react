22876893b936742eb5692e13b0567307e906d6e5
Emily <emplums@github.com>
2019-01-11 12:59:42 -0800

    undo

--- src/FilterList.js
+++ src/FilterList.js
@@ -9,7 +9,9 @@ function ItemBase({children, count, theme, is: Tag, ...rest}) {
   return (
     <a key={nanoid()} {...rest}>
       {count && (
-        <span title="results" className="count">{count}</span>
+        <span title="results" className="count">
+          {count}
+        </span>
       )}
       {children}
     </a>
