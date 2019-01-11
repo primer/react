c9e48da8c3c996066bbc4fd62e53100a8ce0d695
Emily <emplums@github.com>
2019-01-10 20:10:05 -0800

    Flash tweak

--- src/Flash.js
+++ src/Flash.js
@@ -4,10 +4,10 @@ import {COMMON, get} from './constants'
 import theme from './theme'
 
 const schemeMap = {
-  green: {color: 'colors.green.8', bg: 'colors.green.1', borderColor: 'colors.blackfade15'},
-  red: {color: 'colors.red.9', bg: 'colors.red.1', borderColor: 'colors.blackfade15'},
-  yellow: {color: 'colors.yellow.9', bg: 'colors.yellow.1', borderColor: 'colors.blackfade15'},
-  base: {color: 'colors.blue.8', bg: 'colors.blue.1', borderColor: 'colors.blackfade15'} //TODO put in correct border color
+  green: {color: 'colors.green.8', bg: 'colors.green.1'},
+  red: {color: 'colors.red.9', bg: 'colors.red.1'},
+  yellow: {color: 'colors.yellow.9', bg: 'colors.yellow.1'},
+  base: {color: 'colors.blue.8', bg: 'colors.blue.1'}
 }
 
 const Flash = styled.div`
@@ -15,7 +15,7 @@ const Flash = styled.div`
   padding: ${get('space.3')}px;
   color: ${props => get(schemeMap[props.scheme] ? schemeMap[props.scheme].color : '')};
   background-color: ${props => get(schemeMap[props.scheme] ? schemeMap[props.scheme].bg : '')};
-  border-color: ${props => get(schemeMap[props.scheme] ? schemeMap[props.scheme].borderColor : '')};
+  border-color: ${get('colors.blackfade15');
   border-style: solid;
   border-width: ${props => (props.full ? '1px 0px' : '1px')};
   border-radius: ${props => (props.full ? '0' : '3px')};
