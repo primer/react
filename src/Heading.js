0d008dced8cbe492c1e550ab6e9ed4f023145ba8
Emily <emplums@github.com>
2019-02-19 00:05:59 -0800

    Fix default font weight & size in Heading

--- src/Heading.js
+++ src/Heading.js
@@ -1,16 +1,17 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import {TYPOGRAPHY, COMMON, Base} from './constants'
+import {TYPOGRAPHY, COMMON, Base, get} from './constants'
 import theme from './theme'
 
 const Heading = styled(Base)`
+  font-weight: ${get('fontWeights.bold')};
+  font-size: ${get('fontSizes.5')};
   ${TYPOGRAPHY} ${COMMON};
 `
 
 Heading.defaultProps = {
   theme,
   m: 0,
-  fontSize: 5,
   is: 'h1'
 }
 
