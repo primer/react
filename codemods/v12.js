fde873bf8093a062493d6390df0be5b0e94ed814
Emily <emplums@github.com>
2019-03-13 14:27:27 -0700

    add codemod

---
+++ codemods/v12.js
@@ -0,0 +1,13 @@
+const prettify = require('./lib/prettify')
+const modifyProps = require('./lib/modifyProps')
+
+module.exports = (file, api) => {
+  const j = api.jscodeshift
+  const ast = j(file.source)
+
+  modifyProps(ast, j, '@primer/components', {
+    is: 'as'
+  })
+
+  return prettify(ast, file)
+}
