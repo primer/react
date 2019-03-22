c27516a15d37ec5a657c621ce1fcc480d781fe5b
emplums <emplums@github.com>
2019-03-21 17:06:22 -0700

    lint wow

--- pages/doc-components/BoxShadow.js
+++ pages/doc-components/BoxShadow.js
@@ -1,7 +1,7 @@
 import {Box} from '../..'
 import styled from 'styled-components'
 
- const BoxShadow = styled(Box)`
+const BoxShadow = styled(Box)`
   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
 `
 export default BoxShadow
