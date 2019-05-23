8719d28df34404d82bd30ca6528d8cd7f8191cdb
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-05-23 19:58:38 +0530

    make it work right with static code

--- pages/doc-components/LiveCode.js
+++ pages/doc-components/LiveCode.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import {LiveCode} from 'mdx-live'
+import {LiveCode as MDXLive} from 'mdx-live'
 import copy from 'clipboard-copy'
 
 import {Relative, Absolute, Button, StyledOcticon} from '../..'
 import {Clippy, Check} from '@githubprimer/octicons-react'
 
-function LiveCodeContainer({children}) {
+function LiveCode({children, className: language}) {
   const [code, setCode] = React.useState(children)
 
   return (
     <Relative>
-      <LiveCode code={code} editorProps={{onChange: code => setCode(code)}} />
-      <CopyButton code={code} />
+      {language === 'language-.jsx' ? (
+        <MDXLive language={language} code={code} editorProps={{onChange: code => setCode(code)}} />
+      ) : (
+        <pre>{code}</pre>
+      )}
+
+      <Absolute right={8} bottom={8}>
+        <CopyButton code={code} />
+      </Absolute>
     </Relative>
   )
 }
@@ -38,12 +45,10 @@ function CopyButton({code}) {
   )
 
   return (
-    <Absolute right={4} bottom={4}>
-      <Button onClick={onClick}>
-        <StyledOcticon icon={copied ? Check : Clippy} color={copied ? 'green.5' : 'inherit'} />
-      </Button>
-    </Absolute>
+    <Button onClick={onClick}>
+      <StyledOcticon icon={copied ? Check : Clippy} color={copied ? 'green.5' : 'inherit'} />
+    </Button>
   )
 }
 
-export default LiveCodeContainer
+export default LiveCode
