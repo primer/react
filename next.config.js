51ec87b4861ff39e748ed522b54069b00289eebb
Emily Plummer <emplums@github.com>
2019-06-26 15:36:22 -0700

    use config from blueprints

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withNextPages = require('@primer/next-pages/plugin')
 const configure = require('@primer/blueprints/lib/config')
+const {join} = require('path')
 
-module.exports = configure(withNextPages())
+module.exports = configure(withNextPages({
+  webpack(config, {dev}) {
+    config.resolve.alias = {
+      [__dirname + '$']: join(__dirname, 'src/index.js'),
+      [join(__dirname, 'css$')]: join(__dirname, 'src/css.js')
+    }
+    return config
+  }
+}))
