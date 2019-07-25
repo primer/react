3fc3694fb87eb36e420ca4af6fabf861ec4592c2
Emily Plummer <emplums@github.com>
2019-07-25 12:22:29 -0700

    update tests

--- src/Details.js
+++ src/Details.js
@@ -32,6 +32,7 @@ function DetailsBase({children, overlay, render = getRenderer(children), default
   )
 
   function toggle(event) {
+    console.log('TOGGLE!')
     setOpen(event.target.open)
   }
 
