4079bd09bd9a5e800036f0d05ac71443840bded4
Emily Plummer <emplums@github.com>
2019-07-26 10:30:31 -0700

    move whitespace to TYPOGRAPHY

--- src/Text.js
+++ src/Text.js
@@ -1,20 +1,11 @@
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import {system} from 'styled-system'
 import theme from './theme'
 import {TYPOGRAPHY, COMMON} from './constants'
-
-const whiteSpace = system({
-  whiteSpace: {
-    property: 'whiteSpace',
-    cssProperty: 'whiteSpace'
-  }
-})
 
 const Text = styled.span`
   ${TYPOGRAPHY};
   ${COMMON};
-  ${whiteSpace};
 `
 
 Text.defaultProps = {
