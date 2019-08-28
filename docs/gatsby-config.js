2a499860bab2e8b9fa68184f907b61c0f4c5fae8
Emily Plummer <emplums@github.com>
2019-08-28 10:20:47 -0700

    lint

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'Primer Components',
     shortName: 'Components',
-    description: 'React components for the Primer design system',
+    description: 'React components for the Primer design system'
   },
   plugins: ['@primer/gatsby-theme-doctocat'],
-  pathPrefix: '/components',
+  pathPrefix: '/components'
 }
