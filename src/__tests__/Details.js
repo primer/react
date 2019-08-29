8794a6c40a4683e5f208558d7e44a0b1684f6eea
Emily Plummer <emplums@github.com>
2019-08-29 11:59:29 -0700

    use master version of snapshots

--- src/__tests__/Details.js
+++ src/__tests__/Details.js
@@ -3,30 +3,83 @@ import React from 'react'
 import Details from '../Details'
 import {mount, render} from '../utils/testing'
 import {COMMON} from '../constants'
-import { render as hooksRender, fireEvent, getByTestId} from '@testing-library/react'
 
 describe('Details', () => {
   it('implements system props', () => {
     expect(Details).toImplementSystemProps(COMMON)
   })
 
+  it('Respects the open prop', () => {
+    expect(mount(<Details open />).props().open).toEqual(true)
+  })
+
   it('has default theme', () => {
     expect(Details).toSetDefaultTheme()
   })
 
+  xit('Renders children as-is', () => {
+    expect(render(<Details>hi</Details>)).toEqual(render(<details open={false}>hi</details>))
+    expect(
+      render(
+        <Details>
+          <summary>hi</summary>
+          bye
+        </Details>
+      )
+    ).toEqual(
+      render(
+        <details open={false}>
+          <summary>hi</summary>
+          bye
+        </details>
+      )
+    )
+  })
+
+  xit('Renders with a render prop', () => {
+    expect(render(<Details render={() => 'hi'} />)).toEqual(render(<details open={false}>hi</details>))
+  })
+
+  xit('Renders with children as a function', () => {
+    expect(render(<Details>{() => 'hi'}</Details>)).toEqual(render(<details open={false}>hi</details>))
+  })
+
+  xit('Passes open state to render function', () => {
+    const renderOpenAsString = ({open}) => String(open)
+    expect(render(<Details>{renderOpenAsString}</Details>)).toEqual(render(<details open={false}>false</details>))
+    expect(render(<Details open>{renderOpenAsString}</Details>)).toEqual(render(<details open>true</details>))
+  })
+
   it('Can be toggled', () => {
-    const { container } = hooksRender(
-      <Details open={false} data-testid='details'>
-        {({open}) => <summary data-testid='summary'>{open ? 'open' : 'closed'}</summary>}
+    const wrapper = mount(
+      <Details open={false}>
+        {({open, toggle}) => <summary onClick={toggle}>{open ? 'close' : 'open'}</summary>}
       </Details>
     )
-    const summary = getByTestId(container, 'summary');
-    expect(summary.textContent).toBe('closed');
-    fireEvent(summary,
-      new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true,
-      }))
-    expect(getByTestId(container, 'summary').textContent).toBe('open');
+
+    /**
+     * XXX note: when using the react element wrapper, the
+     * selector '[open]' doesn't properly resolve the presence
+     * of the 'open' HTML attribute. To get around this, we have
+     * to test the underlying DOM node's actual 'open'
+     * attribute.
+     */
+    const dom = wrapper.getDOMNode()
+    const summary = wrapper.find('summary')
+
+    expect(dom.hasAttribute('open')).toEqual(false)
+    expect(summary.text()).toEqual('open')
+
+    summary.simulate('click')
+
+    expect(dom.hasAttribute('open')).toEqual(true)
+    expect(summary.text()).toEqual('close')
+
+    summary.simulate('click')
+
+    expect(dom.hasAttribute('open')).toEqual(false)
+    expect(summary.text()).toEqual('open')
+
+    wrapper.unmount()
   })
 })
