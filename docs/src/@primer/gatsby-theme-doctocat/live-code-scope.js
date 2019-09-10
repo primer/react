4aa659acf787c39834143f2edda2ffcc4e9e302f
Emily Plummer <emplums@github.com>
2019-09-10 13:47:47 -0700

    fix octicon import

--- docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
+++ docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
@@ -1,4 +1,10 @@
 import * as primerComponents from '@primer/components'
 import * as doctocatComponents from '@primer/gatsby-theme-doctocat'
+import Octicon, {iconsByName} from '@primer/octicons-react'
 
-export default {...primerComponents, ...doctocatComponents}
+const iconComponents = Object.keys(iconsByName).reduce((map, key) => {
+  map[iconsByName[key].name] = iconsByName[key]
+  return map
+}, {})
+
+export default {...primerComponents, ...doctocatComponents, ...iconComponents}
