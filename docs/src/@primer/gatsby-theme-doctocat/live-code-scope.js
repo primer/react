ee57931e4cf513bbc4b066110fc499933d332d18
Emily Plummer <emplums@github.com>
2019-09-10 13:43:05 -0700

    remove octicon import

--- docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
+++ docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
@@ -1,5 +1,4 @@
 import * as primerComponents from '@primer/components'
 import * as doctocatComponents from '@primer/gatsby-theme-doctocat'
-import * as octicons from '@primer/octicons-react'
 
-export default {...primerComponents, ...doctocatComponents, ...octicons}
+export default {...primerComponents, ...doctocatComponents}
