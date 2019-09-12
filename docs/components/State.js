36116fa5c61479e9de188dd0cfbbdd74dba1b5fa
Emily Plummer <emplums@github.com>
2019-09-12 10:34:16 -0700

    fix lint

--- docs/components/State.js
+++ docs/components/State.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
- const State = props => {
+const State = props => {
   const result = React.useState(props.default)
 
-   return props.children(result)
+  return props.children(result)
 }
 
- export default State
+export default State
