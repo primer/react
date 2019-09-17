0aa318acf700185d48ad0463838d37f6ae14b270
Emily Plummer <emplums@github.com>
2019-09-17 16:35:25 -0700

    fix icons

--- docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
+++ docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
@@ -1,11 +1,6 @@
 import * as primerComponents from '@primer/components'
 import * as doctocatComponents from '@primer/gatsby-theme-doctocat'
-import Octicon, {iconsByName} from '@primer/octicons-react'
+import Octicon, {Check, Zap, X} from '@primer/octicons-react'
 import State from '../../../components/State'
 
-const iconComponents = Object.keys(iconsByName).reduce((map, key) => {
-  map[iconsByName[key].name] = iconsByName[key]
-  return map
-}, {})
-
-export default {...primerComponents, ...doctocatComponents, Octicon, ...iconComponents, State}
+export default {...primerComponents, ...doctocatComponents, State, Octicon, Check, Zap, X}
