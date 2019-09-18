9f00f63a0f39d7c09bdfb8d6481741a556680420
Emily Plummer <emplums@github.com>
2019-09-17 17:04:07 -0700

    update doctocat, remove iframe

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -1,16 +1,13 @@
 import {BaseStyles, Box} from '@primer/components'
-import Frame from './frame'
 import React from 'react'
 
 // Users can shadow this file to wrap live previews.
 // This is useful for applying global styles.
 function LivePreviewWrapper({children}) {
   return (
-    <Frame>
-      <BaseStyles>
-        <Box p={3}>{children}</Box>
-      </BaseStyles>
-    </Frame>
+    <BaseStyles>
+      <Box p={3}>{children}</Box>
+    </BaseStyles>
   )
 }
 
