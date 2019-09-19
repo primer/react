f6201695e86f36a4d93d0759b50d7a8d36a4d425
Emily Plummer <emplums@github.com>
2019-09-19 16:17:41 -0700

    icon wrapper is uneccessary

--- src/CircleOcticon.js
+++ src/CircleOcticon.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Octicon from '@primer/octicons-react'
+import PropTypes from 'prop-types''
 import Flex from './Flex'
 import theme from './theme'
 import BorderBox from './BorderBox'
 
 function CircleOcticon(props) {
   const {size, as} = props
-  const {icon, bg, as: asProp, ...rest} = props
+  const {icon as Icon, bg, as: asProp, ...rest} = props
   return (
     <BorderBox as={as} bg={bg} overflow="hidden" border="none" size={size} borderRadius="50%">
       <Flex {...rest} alignItems="center" justifyContent="center">
-        <Octicon icon={icon} size={size} />
+        <Icon size={size} />
       </Flex>
     </BorderBox>
   )
