97acbbfdccb3d82af912d7501f2fa13f331fcc9a
Emily Plummer <emplums@github.com>
2019-09-27 16:26:49 -0700

    ProgressBar

---
+++ src/ProgressBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import {COMMON, get} from './constants'
+
+
+const Bar = styled.span`
+  width: ${props => props.progress ? `${props.progress}%` : ''};
+  ${COMMON}
+`
+
+const ProgressContainer = styled.span`
+  display: flex;
+  height: ${get('spacing.2')};
+  overflow: hidden;
+  background-color: ${get('colors.gray.2')};
+  border-radius: ${get('radii.1')}px;
+  height: ${props => props.large ? '10px' : '5px'};
+  ${COMMON}
+`
+
+const ProgressBar = ({progress, bg, ...rest}) => {
+  return (
+    <ProgressContainer {...rest}>
+      <Bar progress={progress} bg={bg}/>
+    </ProgressContainer>
+
+  )
+}
+
+ProgressBar.defaultProps = {
+  bg: 'green.5'
+}
+
+export default ProgressBar
