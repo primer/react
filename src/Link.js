84ed5b1997f77e3bec16c924b3dfd511d714aa10
Derrick Marcey <dmarcey@github.com>
2019-09-30 13:50:09 -0400

    Fixed hoverColor test. Added tests for muted prop.

--- src/Link.js
+++ src/Link.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import {system} from 'styled-system'
 import {COMMON, TYPOGRAPHY, get} from './constants'
 import theme from './theme'
 import elementType from './utils/elementType'
@@ -16,13 +17,20 @@ const buttonStyles = {
   appearance: 'none'
 }
 
+const hoverColor = system({
+  hoverColor: {
+    property: 'color',
+    scale: 'colors'
+  }
+})
+
 const Link = styled.a.attrs(props => ({
   color: props.color ? props.color : props.muted ? 'gray.6' : 'blue.5'
 }))`
   text-decoration: ${props => (props.underline ? 'underline' : 'none')};
   &:hover {
     text-decoration: ${props => (props.muted ? 'none' : 'underline')};
-    ${props => (props.muted ? 'color: ' + get('colors.blue.5')(theme) : '')};
+    ${props => (props.hoverColor ? hoverColor : props.muted ? `color: ${get('colors.blue.5')(theme)}` : '')};
   }
   ${props => (props.as === 'button' ? buttonStyles : '')};
   ${TYPOGRAPHY} ${COMMON};
