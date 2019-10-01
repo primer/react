a305c1d79d1f15b341c45945f161ea8c723c84bd
Emily Plummer <emplums@github.com>
2019-10-01 10:49:06 -0700

    Create ts.config.js

---
+++ ts.config.js
@@ -0,0 +1,16 @@
+{
+  "compilerOptions": {
+    "jsx": "react",
+    "module": "commonjs",
+    "target": "es6",
+    "allowJs": true,
+    "rootDir": "src",
+    "outDir": "dist",
+    "sourceMap": true,
+    "strict": false,
+    "esModuleInterop": true,
+    "moduleResolution": "node"
+  },
+  "include": ["src"],
+  "exclude": ["dist", "node_modules"]
+}
