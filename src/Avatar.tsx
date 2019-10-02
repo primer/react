0d73515bdf3c6bdedc2ceb7fcc1f980f9da3bd79
Emily Plummer <emplums@github.com>
2019-10-02 08:49:26 -0700

    refactor avatar

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -2,8 +2,10 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import {get} from './constants'
 import {space} from 'styled-system'
+//@ts-ignore
 import systemPropTypes from '@styled-system/prop-types'
 import theme from './theme'
+import { AvatarProps } from '@primer/components'
 
 function borderRadius({size}) {
   return {
@@ -11,7 +13,7 @@ function borderRadius({size}) {
   }
 }
 
-const Avatar = styled.img.attrs(props => ({
+const Avatar = styled.img.attrs<AvatarProps>(props => ({
   height: props.size,
   width: props.size
 }))`
