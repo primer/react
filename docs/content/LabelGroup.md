3977eeed96249db68f7312964f7315d9201f73c2
Emily Plummer <emplums@github.com>
2019-10-04 12:43:08 -0700

    LabelGroup

---
+++ docs/content/LabelGroup.md
@@ -0,0 +1,21 @@
+---
+title: LabelGroup
+---
+
+The LabelGroup component is used to add commonly used margins and wrapping for groups of Labels.
+
+## Default example
+
+```jsx live
+<LabelGroup>
+  <Label m={1}>Default label</Label>
+  <Label m={1} scheme="gray-darker">Darker gray label</Label>
+  <Label m={1} outline>Default outline label</Label>
+</LabelGroup>
+```
+
+## System props
+
+Label components get `COMMON` system props. Read our [System Props](/system-props) doc page for a full list of available props.
+
+## Component props
---
+++ src/LabelGroup.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import theme from './theme'
+import {COMMON} from './constants'
+
+const transformChildren = children => {
+  return React.Children.map(children, child => {
+    return React.cloneElement(child, {mr: 1})
+  })
+}
+
+const LabelGroup = styled.span.attrs(props => ({
+  children: transformChildren(props.children)
+}))`
+  ${COMMON}
+  & span:last-child {
+    margin-right: 0;
+  }
+`
+
+LabelGroup.defaultProps = {
+  theme
+}
+
+LabelGroup.propTypes = {
+  ...COMMON.propTypes
+}
+
+export default LabelGroup
