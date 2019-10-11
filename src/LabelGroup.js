01b530214e0dbfeba4f1a131b27c275907c3cc85
Derrick Marcey <dmarcey@github.com>
2019-10-11 08:55:08 -0400

    Support children in LabelGroup which without `mr`

--- src/LabelGroup.js
+++ src/LabelGroup.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 import theme from './theme'
-import {COMMON} from './constants'
+import {COMMON, get} from './constants'
 
 const transformChildren = children => {
   return React.Children.map(children, child => {
-    return React.cloneElement(child, {mr: 1, className: 'LabelItem'})
+    return React.cloneElement(child, {className: 'LabelItem'})
   })
 }
 
@@ -13,6 +13,9 @@ const LabelGroup = styled.span.attrs(props => ({
   children: transformChildren(props.children)
 }))`
   ${COMMON}
+  & .LabelItem {
+    margin-right: ${get('space.1')}px;
+  }
   & .LabelItem:last-child {
     margin-right: 0;
   }
