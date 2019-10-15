0700fcc84a3781b0ee5da7a3bcf51d23db9eaf82
Emily Plummer <emplums@github.com>
2019-10-15 14:23:33 -0700

    add Animation component

---
+++ docs/content/Animations.md
@@ -0,0 +1,17 @@
+---
+title: Animations
+---
+
+Use Animation helper components to provide animations to UI.
+
+
+```jsx live
+<Animations.Pulse>
+  <StyledOcticon size={32} icon={Octoface}/>
+</Animations.Pulse>
+```
+
+## System props
+
+Animation components get `COMMON` system props. Read our [System Props](/system-props) doc page for a full list of available props.
+
---
+++ src/Animations.js
@@ -0,0 +1,33 @@
+import styled, {keyframes} from 'styled-components'
+import {COMMON} from './constants'
+import theme from './theme'
+
+let Animations = {}
+
+const pulseKeyframes = keyframes`
+  0% {
+    opacity: 0.3;
+  }
+
+  10% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0.3;
+  }
+`
+
+Animations.Pulse = styled.span`
+  animation-name: ${pulseKeyframes};
+  animation-duration: 2s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  ${COMMON}
+`
+
+Animations.Pulse.defaultProps = {
+  theme
+}
+
+export default Animations
