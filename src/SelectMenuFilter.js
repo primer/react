2866e50202cabffcfc2a3489cc03d900bf38e466
Derrick Marcey <dmarcey@github.com>
2019-11-21 14:15:42 -0500

    Remove unused import.

--- src/SelectMenuFilter.js
+++ src/SelectMenuFilter.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useState, useContext, useEffect} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import styled from 'styled-components'
 import {COMMON, get} from './constants'
 import theme from './theme'
