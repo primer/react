c955ce2dcd32ded9e60a517c82b3c944925c0d14
Emily Plummer <emplums@github.com>
2020-01-03 11:50:59 -0800

    add BranchName

--- src/__tests__/BranchName.js
+++ src/__tests__/BranchName.js
@@ -2,8 +2,18 @@ import React from 'react'
 import BranchName from '../BranchName'
 import {render} from '../utils/testing'
 import {COMMON} from '../constants'
+import {render as HTMLRender, cleanup} from "@testing-library/react";
+import {axe, toHaveNoViolations} from 'jest-axe'
+import 'babel-polyfill'
+expect.extend(toHaveNoViolations)
 
 describe('BranchName', () => {
+  it("should have no axe violations", async () => {
+    const {container} = HTMLRender(<BranchName/>)
+    const results = await axe(container)
+    expect(results).toHaveNoViolations()
+    cleanup()
+  })
   it('renders an <a> by default', () => {
     expect(render(<BranchName />).type).toEqual('a')
   })
