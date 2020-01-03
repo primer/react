7b811968a6c8dae4f283e89ee45bfb8acd6c4264
Emily Plummer <emplums@github.com>
2020-01-03 11:59:57 -0800

    Update Dropdown.js

--- src/__tests__/Dropdown.js
+++ src/__tests__/Dropdown.js
@@ -2,8 +2,19 @@ import React from 'react'
 import Dropdown from '../Dropdown'
 import {render} from '../utils/testing'
 import {COMMON} from '../constants'
+import {render as HTMLRender, cleanup} from "@testing-library/react";
+import {axe, toHaveNoViolations} from 'jest-axe'
+import 'babel-polyfill'
+expect.extend(toHaveNoViolations)
 
 describe('Dropdown', () => {
+  it("should have no axe violations", async () => {
+    const {container} = HTMLRender(<Dropdown title="open"/>)
+    const results = await axe(container)
+    expect(results).toHaveNoViolations()
+    cleanup()
+  })
+
   it('matches the snapshots', () => {
     expect(render(<Dropdown>hi</Dropdown>)).toMatchSnapshot()
     expect(render(<Dropdown title="hi">hello!</Dropdown>)).toMatchSnapshot()
