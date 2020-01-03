99ce429acd5408921747f3f25f387b86edf474bf
Emily Plummer <emplums@github.com>
2020-01-03 13:00:36 -0800

    update StyledOcticon test

--- src/__tests__/StyledOcticon.js
+++ src/__tests__/StyledOcticon.js
@@ -3,10 +3,21 @@ import {X} from '@primer/octicons-react'
 import StyledOcticon from '../StyledOcticon'
 import {render} from '../utils/testing'
 import {COMMON} from '../constants'
+import {render as HTMLRender, cleanup} from "@testing-library/react";
+import {axe, toHaveNoViolations} from 'jest-axe'
+import 'babel-polyfill'
+expect.extend(toHaveNoViolations)
 
 describe('StyledOcticon', () => {
   it('implements system props', () => {
     expect(StyledOcticon).toImplementSystemProps(COMMON)
+  })
+
+  it('should have no axe violations', async () => {
+    const {container} = HTMLRender(<StyledOcticon icon={X} />)
+    const results = await axe(container)
+    expect(results).toHaveNoViolations()
+    cleanup()
   })
 
   it('matches the snapshot', () => {
