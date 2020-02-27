749e1d4a29d6a73ec435398d341f2ec743991a3c
Derrick Marcey <dmarcey@github.com>
2020-02-27 10:26:54 -0500

    Added stickersheet component

--- playroom/components.js
+++ playroom/components.js
@@ -1,3 +1,3 @@
 export * from '../src/index'
-export {WithPseudoClass} from './util'
+export {WithPseudoClass, StickerSheet} from './util'
 export {Check, Zap, X, Search, PrimitiveDot, Octoface, Person, Mail} from '@primer/octicons-react'
--- playroom/util.js
+++ playroom/util.js
@@ -1,4 +1,5 @@
 import React, {useRef, useEffect, useState} from 'react'
+import {Flex, Text} from '../src'
 
 const useAddRuleForPseudoClass = (ref, pseudoClass) => {
   const [modifiedClassName, setModifiedClassName] = useState('')
@@ -9,13 +10,12 @@ const useAddRuleForPseudoClass = (ref, pseudoClass) => {
     for (const className of ref.current.classList) {
       const fullSelector = `.${className}${pseudoClass}`
       const classNameWithSelector = fullSelector.replace(':', '-')
-      let newRule = ''
       for (const ss of document.styleSheets) {
+        let newRule = ''
         for (const rule of ss.cssRules) {
           if (fullSelector === rule.selectorText) {
             newRule = `${classNameWithSelector} { ${rule.style.cssText}}`
             setModifiedClassName(classNameWithSelector.substring(1))
-            break
           }
         }
         if (newRule) {
@@ -39,4 +39,45 @@ export const WithPseudoClass = props => {
     ref,
     className: modifiedClassName
   })
+}
+
+export const StickerSheet = ({props, pseudoClasses, children}) => {
+  const rows = [{}]
+  for (const prop of props) {
+    for (const value of prop.values) {
+      const modifiedProps = {}
+      modifiedProps[prop.prop] = value
+      rows.push(modifiedProps)
+    }
+  }
+  const propsModifiedChildren = rows.map((modifiedProps, index) => {
+    const keys = Object.keys(modifiedProps)
+    const label = keys.length === 0 ? 'normal' : keys.map(key => `${key}: ${modifiedProps[key]}`).join(', ')
+    return (
+      <Flex flexDirection="column" key={index.toString()}>
+        <Text mr={2}>{label}</Text>
+        <Flex mb={2}>
+          {React.Children.map(children, child => React.cloneElement(child, {...modifiedProps, mr: 2}))}
+        </Flex>
+      </Flex>
+    )
+  })
+  const pseudoClassChildren = pseudoClasses.map((pseudoClass, index) => {
+    return (
+      <Flex flexDirection="column" key={index.toString()}>
+        <Text mr={2}>{pseudoClass}</Text>
+        <Flex key={index.toString()} mb={2}>
+          {React.Children.map(children, child => (
+            <WithPseudoClass pseudoClass={pseudoClass}>{React.cloneElement(child, {mr: 2})}</WithPseudoClass>
+          ))}
+        </Flex>
+      </Flex>
+    )
+  })
+  return (
+    <Flex flexDirection="column">
+      {propsModifiedChildren}
+      {pseudoClassChildren}
+    </Flex>
+  )
 }
