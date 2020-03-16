8d815fdcae58f770ffc201408d63235b66e32419
Taylor Johnson <taylor.christian.johnson@gmail.com>
2020-03-15 23:44:25 -0400

    Update Heading Component
    Change default heading level to `h2` and include prop type validation for styled-component `as` prop.

--- src/Heading.js
+++ src/Heading.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import {TYPOGRAPHY, COMMON, get} from './constants'
 import theme from './theme'
 
-const Heading = styled.h1`
+const Heading = styled.h2`
   font-weight: ${get('fontWeights.bold')};
   font-size: ${get('fontSizes.5')};
   margin: 0;
@@ -17,7 +17,8 @@ Heading.defaultProps = {
 Heading.propTypes = {
   ...COMMON.propTypes,
   ...TYPOGRAPHY.propTypes,
-  theme: PropTypes.object
+  theme: PropTypes.object,
+  as: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
 }
 
 export default Heading
