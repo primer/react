c038e018d01617da82e39915b1bd1a582496aa22
Emily Plummer <emplums@github.com>
2020-03-26 19:46:29 -0700

    tests

--- src/__tests__/SelectMenu.js
+++ src/__tests__/SelectMenu.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SelectMenu from '../SelectMenu'
 import Button from '../Button'
-import {render} from '../utils/testing'
+import {render, mount} from '../utils/testing'
 import {COMMON} from '../constants'
 import {render as HTMLRender, cleanup} from '@testing-library/react'
 import {axe, toHaveNoViolations} from 'jest-axe'
@@ -14,7 +14,7 @@ const BasicSelectMenu = () => {
       <Button as="summary">Projects</Button>
       <SelectMenu.Modal title="Projects">
         <SelectMenu.List>
-          <SelectMenu.Item href="#">Primer Components bugs</SelectMenu.Item>
+          <SelectMenu.Item selected href="#">Primer Components bugs</SelectMenu.Item>
           <SelectMenu.Item href="#">Primer Components roadmap</SelectMenu.Item>
           <SelectMenu.Divider>stuff</SelectMenu.Divider>
           <SelectMenu.Item href="#"> Project 3</SelectMenu.Item>
@@ -28,12 +28,12 @@ const BasicSelectMenu = () => {
 
 const MenuWithTabs = () => {
   return (
-    <SelectMenu>
+    <SelectMenu initialTab="Organization">
       <Button as="summary">Projects</Button>
       <SelectMenu.Modal title="Projects">
         <SelectMenu.Tabs>
           <SelectMenu.Tab index={0} tabName="Repository"/>
-          <SelectMenu.Tab index={1} tabName="Organization"/>
+          <SelectMenu.Tab index={1} data-test="orgTab" tabName="Organization"/>
         </SelectMenu.Tabs>
         <SelectMenu.TabPanel tabName="Repository">
           <SelectMenu.Item href="#">Primer Components bugs</SelectMenu.Item>
@@ -65,21 +65,31 @@ describe('SelectMenu', () => {
     expect(SelectMenu).toSetDefaultTheme()
   })
 
+  // shallow rendering doesn't work because the component needs `ref` to be defined to render the keyboard hook
+  // using mount returns [Function type] for the type
   it('respects the "as" prop', () => {
-    expect(render(<SelectMenu as="span" />).type).toEqual('span')
+    expect(mount(<BasicSelectMenu as="span" />).type).toEqual('span')
   })
 
+  // same as above, returns ReactWraper{} instead of snapshot
   it('snapshot matches', () => {
-    expect(render(<SelectMenu />)).toMatchSnapshot()
+    expect(mount(<BasicSelectMenu />)).toMatchSnapshot()
   })
 
   // initialTab shows correct tab and applies hidden to the other tab
+  it('shows correct initial tab', () => {
+      const wrapper = mount(<MenuWithTabs/>)
+      expect(wrapper.find('[aria-selected="true"]').first().prop('tabName')).toEqual("Organization");
+  })
 
   // clicking on a tab opens the tab
 
-  // clicking on the trigger opens the menu
-
   // selected list items have the selected icon showing
+  // it seems like finding by aria-selected isn't working?! finding by a data- attribute works
+  it('shows check svg in item when selected', () => {
+      const wrapper = mount(<Basic/>)
+      expect(wrapper.find('[aria-selected="true"]').first().prop('tabName')).toEqual("Organization");
+  })
 
   // unselected list items do not have the selected icon showing
 
