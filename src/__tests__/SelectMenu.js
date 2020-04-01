c7122c5fafa2fd77c7a449627a12fb6e17c0b310
Emily Plummer <emplums@github.com>
2020-04-01 11:57:21 -0700

    test clicking on items works

--- src/__tests__/SelectMenu.js
+++ src/__tests__/SelectMenu.js
@@ -2,14 +2,13 @@ import React from 'react'
 import SelectMenu from '../SelectMenu'
 import Button from '../Button'
 import {mount, render, renderRoot} from '../utils/testing'
-import TestRenderer from 'react-test-renderer'
 import {COMMON} from '../constants'
 import {render as HTMLRender, cleanup} from '@testing-library/react'
 import {axe, toHaveNoViolations} from 'jest-axe'
 import 'babel-polyfill'
 expect.extend(toHaveNoViolations)
 
-const BasicSelectMenu = () => {
+const BasicSelectMenu = ({onClick}) => {
   return (
     <SelectMenu>
       <Button as="summary">Projects</Button>
@@ -18,7 +17,7 @@ const BasicSelectMenu = () => {
           <SelectMenu.Item selected href="#">
             Primer Components bugs
           </SelectMenu.Item>
-          <SelectMenu.Item href="#">Primer Components roadmap</SelectMenu.Item>
+          <SelectMenu.Item onClick={onClick} data-test="menu-item" href="#">Primer Components roadmap</SelectMenu.Item>
           <SelectMenu.Divider>stuff</SelectMenu.Divider>
           <SelectMenu.Item href="#"> Project 3</SelectMenu.Item>
           <SelectMenu.Item href="#">Project 4</SelectMenu.Item>
@@ -35,7 +34,7 @@ const MenuWithTabs = () => {
       <Button as="summary">Projects</Button>
       <SelectMenu.Modal title="Projects">
         <SelectMenu.Tabs>
-          <SelectMenu.Tab index={0} data-test="repo-tab" aria-role={true} tabName="Repository" />
+          <SelectMenu.Tab index={0} data-test="repo-tab" tabName="Repository" />
           <SelectMenu.Tab index={1} tabName="Organization" />
         </SelectMenu.Tabs>
         <SelectMenu.TabPanel tabName="Repository">
@@ -77,19 +76,26 @@ describe('SelectMenu', () => {
     expect(testInstance.findByProps({"aria-selected": true}).props.children).toBe('Organization')
   })
 
-  // clicking on a tab opens the tab
-  xit('clicking on a tab opens the tab', () => {
-    const root = renderRoot(<MenuWithTabs/>)
-    expect(root.findByProps({"aria-selected": true}).props["data-test"]).toBe('repo-tab')
+  it('clicking on a tab opens the tab', () => {
+    const component = mount(<MenuWithTabs/>);
+    const tab = component.find("[data-test='repo-tab']").first()
+    tab.simulate('click')
+    expect(tab.getDOMNode().attributes["aria-selected"]).toBeTruthy()
   });
-
 
   it('selected items have aria-checked', () => {
     const testInstance = renderRoot(<BasicSelectMenu/>)
     expect(testInstance.findByProps({"aria-checked": true}).props.children[1]).toBe('Primer Components bugs')
   })
 
-  // clicking on a list item calls onClick prop
+  it('clicking on a list item calls user provided onClick handler', () => {
+    const mockClick = jest.fn()
+    const component = mount(<BasicSelectMenu onClick={mockClick} />)
+    const item = component.find("[data-test='menu-item']").first()
+    console.log(item.debug())
+    item.simulate('click')
+    expect(mockClick.mock.calls.length).toEqual(1)
+  });
 
   // clicking on tab calls onClick prop
 
