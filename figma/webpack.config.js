842716ad2892a7b60930b3ee140ace890a9526ec
macintoshhelper <macintoshhelper@users.noreply.github.com>
2020-04-04 17:24:17 +0100

    add figma webpack rule

--- figma/webpack.config.js
+++ figma/webpack.config.js
@@ -2,6 +2,21 @@ const path = require('path')
 const configure = require('react-figma-webpack-config')
 
 module.exports = configure({
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        use: {
+          loader: require.resolve('ts-loader'),
+          options: {
+            // this will disable any type checking for js/jsx files, but use TS to transform jsx
+            transpileOnly: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
   resolve: {
     alias: {
       react: path.resolve(__dirname, './node_modules/react/'),
