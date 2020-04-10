f74ec177d52058a23d33a82c88f0d14d10764aaa
Emily Plummer <emplums@github.com>
2020-04-10 16:00:26 -0700

    fix borders

--- src/SelectMenu/SelectMenuTabs.js
+++ src/SelectMenu/SelectMenuTabs.js
@@ -7,7 +7,6 @@ const tabWrapperStyles = css`
   display: flex;
   flex-shrink: 0;
   margin-bottom: -1px; // hide border of element below
-  border-top: ${get('borders.1')} ${get('colors.border.gray')};
   -webkit-overflow-scrolling: touch;
   overflow-x: auto;
   overflow-y: hidden;
@@ -19,7 +18,7 @@ const tabWrapperStyles = css`
 
   @media (min-width: ${get('breakpoints.0')}) {
     padding: 0 ${get('space.2')};
-    border-top: 0;
+    margin-top: ${get('space.3')};
   }
 `
 
