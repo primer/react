c98df7cefe21571966f1827fb83f11c9edca0f8c
Michelle Tilley <michelle@michelletilley.net>
2020-04-13 15:16:01 -0700

    Wrap Heading in withSx

--- src/Heading.js
+++ src/Heading.js
@@ -1,14 +1,15 @@
+import withSx from './hoc/withSx'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import {TYPOGRAPHY, COMMON, get} from './constants'
 import theme from './theme'
 
-const Heading = styled.h1`
+const Heading = withSx(styled.h1`
   font-weight: ${get('fontWeights.bold')};
   font-size: ${get('fontSizes.5')};
   margin: 0;
   ${TYPOGRAPHY} ${COMMON};
-`
+`)
 
 Heading.defaultProps = {
   theme
