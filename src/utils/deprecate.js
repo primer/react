2fcfe7c40a023b5b63e279e2ff61fbe4c1d2bbba
Emily Plummer <emplums@github.com>
2020-04-29 16:56:41 -0700

    remove conditional for now

--- src/utils/deprecate.js
+++ src/utils/deprecate.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-console */
 
 export default function({name, message, version}){
-  if (__DEV__) {
-    console.warn(`WARNING! ${name} is deprecated and will be removed in version ${version}. ${message}`)
-  }
+  console.warn(`WARNING! ${name} is deprecated and will be removed in version ${version}. ${message}`)
 }
