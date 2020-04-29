349f1754ded8466a0f1ccc370c9f2a87f7db889c
Emily Plummer <emplums@github.com>
2020-04-29 16:47:07 -0700

    babel transform

--- src/utils/deprecate.js
+++ src/utils/deprecate.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 
 export default function({name, message, version}){
-  if (process.env.NODE_ENV !== "production") {
+  if (__DEV__) {
     console.warn(`WARNING! ${name} is deprecated and will be removed in version ${version}. ${message}`)
   }
 }
