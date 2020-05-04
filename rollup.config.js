06948aef59f695bac1f7ec37bf510936f9f6fa2c
Michelle Tilley <michelle@michelletilley.net>
2020-05-04 14:04:50 -0700

    Add bundle analyzer and make ReactDOM a peer dep

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
+import visualizer from 'rollup-plugin-visualizer'
 
 // NOTE: this can be removed once the next version of rollup-plugin-commonjs is released
 const namedExports = {
@@ -11,19 +12,20 @@ const namedExports = {
 }
 
 const formats = ['esm', 'umd'] // 'cjs' ?
-const plugins = [babel({exclude: 'node_modules/**'}), resolve(), commonjs({namedExports}), terser()]
+const plugins = [babel({exclude: 'node_modules/**'}), resolve(), commonjs({namedExports}), terser(), visualizer()]
 
 export default [
   {
     input: 'src/index.js',
     plugins,
-    external: ['styled-components', 'react'],
+    external: ['styled-components', 'react', 'react-dom'],
     output: formats.map(format => ({
       file: `dist/browser.${format}.js`,
       format,
       name: 'primer',
       globals: {
         react: 'React',
+        'react-dom': 'ReactDOM',
         'styled-components': 'styled'
       }
     }))
