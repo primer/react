991a33ee42cf6394f54f83fc94b04ad101bd71e5
Emily Plummer <emplums@github.com>
2020-05-20 12:31:49 -0700

    deprecate PointerBox

--- src/PointerBox.js
+++ src/PointerBox.js
@@ -3,17 +3,26 @@ import PropTypes from 'prop-types'
 import BorderBox from './BorderBox'
 import Caret from './Caret'
 import theme from './theme'
+import {Relative} from './Position'
+import {useDeprecation} from './utils/deprecate'
 
 function PointerBox(props) {
-  // don't destructure these, just grab them so that they are applied to the caret and the BorderBox
-  const {bg, borderColor, borderWidth, borderStyle} = props
+  // don't destructure these, just grab them
+  const {bg, border, borderColor, as} = props
   const {caret, children, ...boxProps} = props
-  const caretProps = {bg, borderColor, borderWidth, borderStyle, location: caret}
+  const caretProps = {bg, borderColor, borderWidth: border, location: caret}
+  useDeprecation({
+    name: 'PointerBox',
+    version: '21.0.0',
+    message: 'Use the Popover component instead. See https://primer.style/components/Popover for more details.'
+  })
   return (
-    <BorderBox sx={{position: 'relative'}} {...boxProps}>
-      {children}
-      <Caret {...caretProps} />
-    </BorderBox>
+    <Relative as={as}>
+      <BorderBox {...boxProps}>
+        {children}
+        <Caret {...caretProps} />
+      </BorderBox>
+    </Relative>
   )
 }
 
