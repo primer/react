c0e81e06af1a7d2dd86b7be6d2e0cbacd57dae1d
Emily Plummer <emplums@github.com>
2020-07-01 16:07:12 -0700

    wildcard import doesnt work

--- docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
+++ docs/src/@primer/gatsby-theme-doctocat/live-code-scope.js
@@ -1,11 +1,31 @@
 import * as primerComponents from '@primer/components'
 import * as doctocatComponents from '@primer/gatsby-theme-doctocat'
-import * as octiconComponents from '@primer/octicons-react'
+import {
+  CheckIcon,
+  ZapIcon,
+  XIcon,
+  SearchIcon,
+  PrimitiveDotIcon,
+  OctofaceIcon,
+  PersonIcon,
+  MailIcon,
+  GitCommitIcon,
+  FlameIcon
+} from '@primer/octicons-react'
 import State from '../../../components/State'
 
 export default {
   ...primerComponents,
   ...doctocatComponents,
-  ...octiconComponents,
-  State
+  State,
+  CheckIcon,
+  SearchIcon,
+  ZapIcon,
+  XIcon,
+  PrimitiveDotIcon,
+  OctofaceIcon,
+  PersonIcon,
+  MailIcon,
+  GitCommitIcon,
+  FlameIcon
 }
