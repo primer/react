7b6b2eaced6809c52f3961f92183ef2dd5c67ed2
Emily Plummer <emplums@github.com>
2020-09-25 11:38:27 -0700

    Update SelectMenu.js

--- src/SelectMenu/SelectMenu.js
+++ src/SelectMenu/SelectMenu.js
@@ -51,7 +51,7 @@ const StyledSelectMenu = styled.details`
 `
 
 // 'as' is spread out because we don't want users to be able to change the tag.
-const SelectMenu = React.forwardRef(({children, initialTab, as, ...rest}, forwardedRef)) => {
+const SelectMenu = React.forwardRef(({children, initialTab, as, ...rest}, forwardedRef) => {
   const ref = forwardedRef || useRef(null)
   const [selectedTab, setSelectedTab] = useState(initialTab)
   const [open, setOpen] = useState(false)
@@ -76,7 +76,7 @@ const SelectMenu = React.forwardRef(({children, initialTab, as, ...rest}, forwar
       </StyledSelectMenu>
     </MenuContext.Provider>
   )
-}
+})
 
 SelectMenu.MenuContext = MenuContext
 SelectMenu.List = SelectMenuList
