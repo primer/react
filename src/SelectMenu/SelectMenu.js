41f303ae3867927ea426e6c2534c8ed30d67cd51
Emily Plummer <emplums@github.com>
2020-09-25 11:43:40 -0700

    Update SelectMenu.js

--- src/SelectMenu/SelectMenu.js
+++ src/SelectMenu/SelectMenu.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import sx from '../sx'
