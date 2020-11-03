8b4a51df5df8d70089fc72a4eeb57987c1ad9903
Emily Plummer <emplums@github.com>
2020-11-02 17:14:27 -0800

    add autoDismiss to toast hook as option

--- src/hooks/useToasts.js
+++ src/hooks/useToasts.js
@@ -1,19 +1,23 @@
 import {nanoid} from 'nanoid'
 import {useState} from 'react'
 
-const useToasts = () => {
+const useToasts = (config) => {
+    const {autoDismiss, toastLimit} = config
     const [toasts, setToasts] = useState([])
 
     const addToast = (freshToast) => {
       const toastId = nanoid()
-      const timeoutId = window.setTimeout(removeToast, 5000, toastId, freshToast.message)
+      let timeoutId
+      if (autoDismiss) {
+        timeoutId = window.setTimeout(removeToast, 5000, toastId, freshToast.message)
+      }
       setToasts([{id: toastId, timeoutId: timeoutId, ...freshToast}, ...toasts])
     }
 
     const removeToast = (id, message) => {
       setToasts(currentToasts =>
         currentToasts.filter(toast => {
-          if (toast.id === id && toast.timeoutId) {
+          if (autoDismiss && toast.id === id && toast.timeoutId) {
             window.clearTimeout(toast.timeoutId);
           }
           return toast.id !== id
