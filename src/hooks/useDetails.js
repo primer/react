0b959d71516845f2c1ef5020b0774cc6acfb7eda
Emily Plummer <emplums@github.com>
2020-11-24 11:51:19 -0800

    fix a few things in hook

--- src/hooks/useDetails.js
+++ src/hooks/useDetails.js
@@ -1,30 +1,31 @@
 import {useCallback, useEffect, useState, useRef} from 'react'
 
-function useDetails({ref, overlay, defaultOpen}) {
+function useDetails({ref, overlay, defaultOpen, onClickOutside}) {
   const [open, setOpen] = useState(defaultOpen)
   const backupRef = useRef(null)
   const customRef = ref ?? backupRef
 
-  const onClickOutside = useCallback(
+  const onClickOutsideInternal = useCallback(
     event => {
       if (event.target.closest('details') !== customRef.current) {
+        onClickOutside && onClickOutside(event)
         if (!event.defaultPrevented) {
           setOpen(false)
         }
       }
     },
-    [customRef, setOpen]
+    [customRef, setOpen, onClickOutside]
   )
 
   // handles the overlay behavior - closing the menu when clicking outside of it
   useEffect(() => {
     if (open && overlay) {
-      document.addEventListener('click', onClickOutside)
+      document.addEventListener('click', onClickOutsideInternal)
       return () => {
-        document.removeEventListener('click', onClickOutside)
+        document.removeEventListener('click', onClickOutsideInternal)
       }
     }
-  }, [open, overlay, onClickOutside])
+  }, [open, overlay, onClickOutsideInternal])
 
   const handleToggle = e => {
     if (!e.defaultPrevented) {
