e3bc2d50853b83d120881091fb2468c97e35fc0d
Emily Plummer <emplums@github.com>
2020-12-07 13:54:58 -0800

    add timeout prop

--- src/hooks/useToasts.js
+++ src/hooks/useToasts.js
@@ -1,19 +1,19 @@
 import {nanoid} from 'nanoid'
 import {useState} from 'react'
 
-const useToasts = autoDismiss => {
+const useToasts = ({autoDismiss, timeout = 5000}) => {
   const [toasts, setToasts] = useState([])
 
   const addToast = freshToast => {
     const toastId = nanoid()
     let timeoutId
     if (autoDismiss) {
-      timeoutId = window.setTimeout(removeToast, 5000, toastId, freshToast.message)
+      timeoutId = window.setTimeout(removeToast, timeout, toastId, freshToast.message)
     }
     setToasts([{id: toastId, timeoutId, ...freshToast}])
   }
 
-  const removeToast = (id, message) => {
+  const removeToast = id => {
     // technically you could just replace the entire state because we're only allowing
     // one toast at a time, but i wrote this so that if we want to allow more than one
     // at a time in the future we don't have to update this function - @emplums
