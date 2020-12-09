71f8fdc6043be65a23097aaf3743afa3d90e2888
Emily Plummer <emplums@github.com>
2020-12-09 13:44:02 -0800

    pass through children would probably be a good idea

--- src/ToastContainer.js
+++ src/ToastContainer.js
@@ -1,25 +1,21 @@
 import React, {createContext} from 'react'
 import PropTypes from 'prop-types'
 import Toast from './Toast'
-import Box from './Box'
-import theme from './theme'
 import useToasts from './hooks/useToasts'
 
 const ToastContext = createContext()
 
 const ToastContainer = (props) => {
-  const {autoDismiss, timeout, ...rest} = props
-  const {addToast, getToastProps} = useToasts({autoDismiss, timeout})
+  const {addToast, getToastProps} = useToasts(props)
   const {toasts, ...toastProps} = getToastProps()
 
   return (
     <ToastContext.Provider value={addToast}>
-      <Box {...rest}>
-        {toasts &&
-          toasts.map((toast) => {
-            return <Toast key={toast.id} className={toast.className} toast={toast} {...toastProps} />
-          })}
-      </Box>
+      {props.children}
+      {toasts &&
+        toasts.map((toast) => {
+          return <Toast key={toast.id} className={toast.className} toast={toast} {...toastProps} />
+        })}
     </ToastContext.Provider>
   )
 }
@@ -28,12 +24,7 @@ ToastContainer.Context = {
   addToast: PropTypes.func,
 }
 
-ToastContainer.defaultProps = {
-  theme,
-}
-
 ToastContainer.propTypes = {
-  ...Box.propTypes,
   autoDismiss: PropTypes.bool,
   timeout: PropTypes.number,
 }
