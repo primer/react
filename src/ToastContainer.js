77947cfb8b2b9b5a9d035ea7e52a43ce37031693
Emily Plummer <emplums@github.com>
2020-12-14 16:14:11 -0800

    Update ToastContainer.js

--- src/ToastContainer.js
+++ src/ToastContainer.js
@@ -1,17 +1,18 @@
-import React, {createContext} from 'react'
 import PropTypes from 'prop-types'
+import React, {createContext} from 'react'
 import Toast from './Toast'
-import useToastsInternal from './hooks/useToastsInternal'
+import useToastsInternal from './useToastsInternal'
 
 export const ToastContext = createContext()
 
 const ToastContainer = (props) => {
-  const {addToast, getToastProps} = useToastsInternal(props)
+  const {autoDismiss, timeout, children} = props
+  const {addToast, getToastProps} = useToastsInternal({autoDismiss, timeout})
   const {toasts, ...toastProps} = getToastProps()
 
   return (
-    <ToastContext.Provider value={addToast}>
-      {props.children}
+    <ToastContext.Provider value={{addToast}}>
+      {children}
       {toasts &&
         toasts.map((toast) => {
           return <Toast key={toast.id} className={toast.className} toast={toast} {...toastProps} />
