d2be30f4f0abde22d0cc110cf5e67c61bdcf97a8
Emily Plummer <emplums@github.com>
2020-12-30 10:53:30 -0800

    fix button styles

--- src/Button/ButtonClose.js
+++ src/Button/ButtonClose.js
@@ -6,13 +6,8 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const StyledButton = styled.button`
-  width: 32px;
-  height: 32px;
-  padding: 8px;
-  margin: -6px -6px -6px 0px;
-  border-radius: 3px;
-  color: ${get('colors.text.white')};
-  border: 0;
+  border: none;
+  padding: 0;
   background: transparent;
   outline: none;
   cursor: pointer;
