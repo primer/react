b816ddaf7453f4c5fe6f4e74e8d0b17caf767c12
Cole Bemis <colebemis@github.com>
2021-01-12 11:52:07 -0800

    Try removing rule overrides

--- docs/gatsby-node.js
+++ docs/gatsby-node.js
@@ -5,19 +5,19 @@ exports.onCreateWebpackConfig = ({actions, plugins, loaders, getConfig}) => {
   // Add our `__DEV__` and `process.env.NODE_ENV` defines
   config.plugins.push(plugins.define(defines[process.env.NODE_ENV || 'development']))
 
-  config.module.rules = [
-    // ...config.module.rules,
-    ...config.module.rules.filter((rule) => String(rule.test) !== String(/\.tsx?$/)),
-    // Create a custom configuration.
-    {
-      // The new configuration is based off the original...
-      ...loaders.js(),
-      test: /\.(ts|js)x?$/,
-      exclude: (modulePath) => /node_modules/.test(modulePath),
-      // ...except that we want to run Primer React through webpack as well.
-      // By default, Gatsby won't use the define plugin we added above on Primer React.
-      include: (modulePath) => /@primer\/components/.test(modulePath),
-    },
-  ]
+  // config.module.rules = [
+  //   // ...config.module.rules,
+  //   ...config.module.rules.filter((rule) => String(rule.test) !== String(/\.tsx?$/)),
+  //   // Create a custom configuration.
+  //   {
+  //     // The new configuration is based off the original...
+  //     ...loaders.js(),
+  //     test: /\.(ts|js)x?$/,
+  //     exclude: (modulePath) => /node_modules/.test(modulePath),
+  //     // ...except that we want to run Primer React through webpack as well.
+  //     // By default, Gatsby won't use the define plugin we added above on Primer React.
+  //     include: (modulePath) => /@primer\/components/.test(modulePath),
+  //   },
+  // ]
   actions.replaceWebpackConfig(config)
 }
