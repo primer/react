958e3b1325fb920007fb4ac507dbc9106b870eaa
Emily Plummer <emplums@github.com>
2021-02-09 15:57:19 -0800

    add tests

---
+++ src/__tests__/useSafeTimeout.tsx
@@ -0,0 +1,21 @@
+import { renderHook} from '@testing-library/react-hooks'
+import useSafeTimeout from '../hooks/useSafeTimeout'
+
+test('should call callback after time', async () => {
+  const { result, waitFor } = renderHook(() => useSafeTimeout())
+  const mockFunction = jest.fn()
+  result.current.safeSetTimeout(mockFunction, 300)
+  await waitFor(() => expect(mockFunction).toHaveBeenCalled())
+})
+
+
+test('should clear timeouts', async () => {
+  jest.useFakeTimers();
+  const { result} = renderHook(() => useSafeTimeout())
+  const mockFunction = jest.fn()
+  let timeoutId = null
+  timeoutId = result.current.safeSetTimeout(mockFunction, 300)
+  result.current.safeClearTimeout(timeoutId)
+  expect(clearTimeout).toHaveBeenCalledTimes(1);
+  expect(clearTimeout).toHaveBeenLastCalledWith(timeoutId);
+})
