490fe505df1b7645cf8271fe8b1036de3beb5894
Emily Plummer <emplums@github.com>
2021-02-11 14:57:07 -0800

    remove unused eslint disable

--- src/hooks/useSafeTimeout.ts
+++ src/hooks/useSafeTimeout.ts
@@ -1,11 +1,14 @@
 import {useCallback, useEffect, useRef} from 'react'
 
+type SetTimeout = (handler: TimerHandler, timeout?: number, ...args: any[]) => number
+type ClearTimeout = (id: number) => void
+
 /**
  * Safely call `setTimeout` and `clearTimeout` within a component.
  *
  * This hook ensures that all timeouts are cleared when the component unmounts.
  */
-export default function useSafeTimeout(): {safeSetTimeout: typeof window.setTimeout; safeClearTimeout: typeof window.clearTimeout} {
+export default function useSafeTimeout(): {safeSetTimeout: SetTimeout; safeClearTimeout: ClearTimeout} {
   const timers = useRef<Record<string, number>>({})
 
   const safeSetTimeout = useCallback((handler: TimerHandler, timeout?: number | undefined, ...args: any[]): number => {
@@ -21,7 +24,6 @@ export default function useSafeTimeout(): {safeSetTimeout: typeof window.setTime
 
   useEffect(() => {
     return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
       Object.values(timers.current).forEach(clearTimeout)
     }
   }, [])
