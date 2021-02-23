55131bc2d2c018178841d1e38a4684a687cb1e44
Cole Bemis <colebemis@github.com>
2021-02-22 19:55:40 -0800

    Remove propTypes from stylefn type

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,6 @@
 import {themeGet} from '@styled-system/theme-get'
-import React from 'react'
 import * as styledSystem from 'styled-system'
 import theme from './theme'
-
-interface StyleFn extends styledSystem.styleFn {
-  propTypes?: React.WeakValidationMap<any>
-}
 
 const {get: getKey, compose, system} = styledSystem
 
@@ -13,7 +8,7 @@ export const get = (key: string) => themeGet(key, getKey(theme, key))
 
 // Common props
 
-export const COMMON: StyleFn = compose(styledSystem.space, styledSystem.color, styledSystem.display)
+export const COMMON = compose(styledSystem.space, styledSystem.color, styledSystem.display)
 
 export interface SystemCommonProps
   extends styledSystem.ColorProps,
@@ -29,7 +24,7 @@ const whiteSpace = system({
   }
 })
 
-export const TYPOGRAPHY: StyleFn = compose(styledSystem.typography, whiteSpace)
+export const TYPOGRAPHY = compose(styledSystem.typography, whiteSpace)
 
 export interface SystemTypographyProps extends styledSystem.TypographyProps {
   whiteSpace?: 'normal' | 'nowrap' | 'pre' | 'pre-wrap' | 'pre-line'
@@ -37,30 +32,30 @@ export interface SystemTypographyProps extends styledSystem.TypographyProps {
 
 // Border props
 
-export const BORDER: StyleFn = compose(styledSystem.border, styledSystem.shadow)
+export const BORDER = compose(styledSystem.border, styledSystem.shadow)
 
 export interface SystemBorderProps extends styledSystem.BorderProps, styledSystem.ShadowProps {}
 
 // Layout props
 
-export const LAYOUT: StyleFn = styledSystem.layout
+export const LAYOUT = styledSystem.layout
 
 export type SystemLayoutProps = styledSystem.LayoutProps
 
 // Position props
 
-export const POSITION: StyleFn = styledSystem.position
+export const POSITION = styledSystem.position
 
 export type SystemPositionProps = styledSystem.PositionProps
 
 // Flex props
 
-export const FLEX: StyleFn = styledSystem.flexbox
+export const FLEX = styledSystem.flexbox
 
 export type SystemFlexProps = styledSystem.FlexboxProps
 
 // Grid props
 
-export const GRID: StyleFn = styledSystem.grid
+export const GRID = styledSystem.grid
 
 export type SystemGridProps = styledSystem.GridProps
