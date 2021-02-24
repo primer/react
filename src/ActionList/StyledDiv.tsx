55161b5916619901aca65c6f166049d15a9039d9
Clay Miller <clay@smockle.com>
2021-02-24 12:02:31 -0500

    feat: Style 'ActionListSectionHeader'

---
+++ src/ActionList/StyledDiv.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styled from 'styled-components'
+import sx, {SxProp} from '../sx'
+
+export const StyledDiv = styled('div')<React.ComponentPropsWithoutRef<'div'> & SxProp>`
+  ${sx}
+`
---
+++ src/ActionList/variables.ts
@@ -0,0 +1,20 @@
+// expanded from src/support/variables/layout.scss
+// adds 12px to the scale as of https://github.com/github/design-systems/issues/992
+//
+// https://github.com/github/design-systems/issues/1272
+
+export const s8 = 8
+export const s20 = 20
+export const s32 = 32
+
+// pulled from src/support/variables/color-system.scss
+
+// -------- Grays --------
+
+export const gray100 = '#f6f8fa'
+export const gray200 = '#e1e4e8'
+export const gray500 = '#6a737d'
+
+// Typographic colors, from some color mode file
+
+export const textSecondary = gray500
