9fcec1544801d628840ff122730c08a9a86b2d3c
Emily Plummer <emplums@github.com>
2021-02-24 16:06:53 -0800

    dont call click handler on right clicks or any clicks that arent primary button

--- src/hooks/useOnOutsideClick.tsx
+++ src/hooks/useOnOutsideClick.tsx
@@ -1,15 +1,22 @@
 import React, {useEffect, useCallback} from 'react'
 
+type TouchOrMouseEvent = MouseEvent | TouchEvent
+
 export type UseOnOutsideClickParameters = {
   overlayRef: React.RefObject<HTMLDivElement>
   triggerRef: React.RefObject<HTMLElement>
   isOpen: boolean
-  onClickOutside: (e: MouseEvent) => void
+  onClickOutside: (e: TouchOrMouseEvent) => void
 }
 
 
-const shouldCallClickHandler = (triggerRef: React.RefObject<HTMLElement>, overlayRef: React.RefObject<HTMLDivElement>, e: MouseEvent) => {
+
+const shouldCallClickHandler = (triggerRef: React.RefObject<HTMLElement>, overlayRef: React.RefObject<HTMLDivElement>, e: TouchOrMouseEvent) => {
   let shouldCallHandler = true
+  // only call click handler when primary button is pressed
+  if (e instanceof MouseEvent && e.button > 0) {
+    shouldCallHandler = false
+  }
 
     if (overlayRef && 'current' in overlayRef && overlayRef.current?.contains(e.target as Node)) {
       shouldCallHandler = false
@@ -23,7 +30,7 @@ const shouldCallClickHandler = (triggerRef: React.RefObject<HTMLElement>, overla
 
 export const useOnOutsideClick = ({overlayRef, triggerRef, isOpen, onClickOutside}: UseOnOutsideClickParameters): void => {
   const onOutsideClickInternal = useCallback(
-    (e: MouseEvent) => {
+    (e: TouchOrMouseEvent) => {
       if (!shouldCallClickHandler(triggerRef, overlayRef, e)) return
       onClickOutside(e)
     }, [onClickOutside, overlayRef, triggerRef]
@@ -32,8 +39,10 @@ export const useOnOutsideClick = ({overlayRef, triggerRef, isOpen, onClickOutsid
   useEffect(() => {
     if (isOpen) {
       document.addEventListener('mousedown', onOutsideClickInternal)
+      document.addEventListener('touchstart', onOutsideClickInternal)
       return () => {
         document.removeEventListener('mousedown', onOutsideClickInternal)
+        document.removeEventListener('touchstart', onOutsideClickInternal)
       }
     }
   }, [isOpen, onOutsideClickInternal])
