c44ee363e9d663b873ea721e42dac8e5366ebcd1
Van Anderson <van.m.anderson@gmail.com>
2021-02-26 09:37:26 -0600

    implement functional color variables for SubNav

--- src/SubNav.tsx
+++ src/SubNav.tsx
@@ -77,19 +77,19 @@ const SubNavLink = styled.a.attrs<StyledSubNavLinkProps>(props => ({
   font-size: ${get('fontSizes.1')};
   line-height: 20px; //custom value for SubNav
   min-height: 34px; //custom value for SubNav
-  color: ${get('colors.gray.9')};
+  color: ${get('colors.text.primary')};
   text-align: center;
   text-decoration: none;
-  border-top: 1px solid ${get('colors.gray.2')};
-  border-bottom: 1px solid ${get('colors.gray.2')};
-  border-right: 1px solid ${get('colors.gray.2')};
+  border-top: 1px solid ${get('colors.border.primary')};
+  border-bottom: 1px solid ${get('colors.border.primary')};
+  border-right: 1px solid ${get('colors.border.primary')};
   display: flex;
   align-items: center;
 
   &:first-of-type {
     border-top-left-radius: ${get('radii.2')};
     border-bottom-left-radius: ${get('radii.2')};
-    border-left: 1px solid ${get('colors.gray.2')};
+    border-left: 1px solid ${get('colors.border.primary')};
   }
 
   &:last-of-type {
@@ -100,20 +100,20 @@ const SubNavLink = styled.a.attrs<StyledSubNavLinkProps>(props => ({
   &:hover,
   &:focus {
     text-decoration: none;
-    background-color: ${get('colors.gray.1')};
+    background-color: ${get('colors.bg.canvasInset')};
     transition: 0.2s ease;
 
     .SubNav-octicon {
-      color: ${get('colors.gray.5')};
+      color: ${get('colors.icon.secondary')};
     }
   }
 
   &.selected {
-    color: ${get('colors.white')};
-    background-color: ${get('colors.blue.5')};
+    color: ${get('colors.text.inverse')};
+    background-color: ${get('colors.bg.infoInverse')};
     border: 0;
     .SubNav-octicon {
-      color: ${get('colors.gray.5')};
+      color: ${get('colors.icon.secondary')};
     }
   }
 
