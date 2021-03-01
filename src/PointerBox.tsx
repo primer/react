8be4ea46d2e025538fc3d579e0734648d1e614d6
Cole Bemis <colebemis@github.com>
2021-03-01 15:03:30 -0800

    Get theme from context in PointerBox

--- src/PointerBox.tsx
+++ src/PointerBox.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {ThemeContext} from 'styled-components'
 import BorderBox, {BorderBoxProps} from './BorderBox'
 import Caret, {CaretProps} from './Caret'
 
@@ -10,10 +11,17 @@ export type PointerBoxProps = {
 } & BorderBoxProps
 
 function PointerBox(props: PointerBoxProps) {
+  const themeContext = React.useContext(ThemeContext)
   // don't destructure these, just grab them
-  const {bg, border, borderColor} = props
+  const {bg, border, borderColor, theme} = props
   const {caret, children, ...boxProps} = props
-  const caretProps = {bg, borderColor, borderWidth: border, location: caret}
+  const caretProps = {
+    bg,
+    borderColor,
+    borderWidth: border,
+    location: caret,
+    theme: theme ?? themeContext
+  }
   return (
     <BorderBox sx={{position: 'relative'}} {...boxProps}>
       {children}
