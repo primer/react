4870bc7331d6826d101b7f41a485e17e634d409f
Emily Plummer <emplums@github.com>
2021-03-02 16:17:56 -0800

    break out into behavior + hook

---
+++ src/behaviors/initialFocus.ts
@@ -0,0 +1,33 @@
+export type UseInitialFocusProps = {
+  initialFocusElement?: HTMLElement | null
+  containerElement: HTMLElement
+  isOpen: boolean
+}
+
+
+function visible(el: HTMLInputElement) {
+  return !el.hidden && (!el.type || el.type !== 'hidden') && (el.offsetWidth > 0 || el.offsetHeight > 0)
+}
+
+function focusable(el: HTMLElement) {
+  const inputEl = el as HTMLInputElement
+  return inputEl.tabIndex >= 0 && !inputEl.disabled && visible(inputEl)
+}
+
+
+function getFirstFocusableItem(containerElement: HTMLElement){
+  let firstItem = null
+  const items = Array.from(containerElement.querySelectorAll<HTMLElement>('*')).filter(focusable)
+  if (items.length === 0) return
+  firstItem = items[0]
+  return firstItem as HTMLElement
+}
+
+export function initialFocus({initialFocusElement, containerElement, isOpen}: UseInitialFocusProps)  {
+  if (isOpen && initialFocusElement) {
+    initialFocusElement.focus()
+  } else {
+    const firstItem = getFirstFocusableItem(containerElement)
+    firstItem ? firstItem.focus() : null
+  }
+}
--- src/hooks/useInitialFocus.ts
+++ src/hooks/useInitialFocus.ts
@@ -1,33 +1,18 @@
-export type UseInitialFocusProps = {
-  initialFocusElement?: HTMLElement
-  containerElement: HTMLElement
+import { useEffect } from 'react'
+import {initialFocus} from '../behaviors/initialFocus'
+import { useProvidedRefOrCreate } from './useProvidedRefOrCreate'
+
+
+type InitialFocusProps = {
+  initialFocusRef?: React.RefObject<HTMLElement>,
+  containerRef: React.RefObject<HTMLElement>
   isOpen: boolean
 }
 
-
-function visible(el: HTMLInputElement) {
-  return !el.hidden && (!el.type || el.type !== 'hidden') && (el.offsetWidth > 0 || el.offsetHeight > 0)
-}
-
-function focusable(el: HTMLElement) {
-  const inputEl = el as HTMLInputElement
-  return inputEl.tabIndex >= 0 && !inputEl.disabled && visible(inputEl)
-}
-
-
-function getFirstFocusableItem(containerElement: HTMLElement){
-  let firstItem = null
-  const items = Array.from(containerElement.querySelectorAll<HTMLElement>('*')).filter(focusable)
-  if (items.length === 0) return
-  firstItem = items[0]
-  return firstItem as HTMLElement
-}
-
-export const useInitialFocus = ({initialFocusElement, containerElement, isOpen}: UseInitialFocusProps) => {
-  if (isOpen && initialFocusElement) {
-    initialFocusElement.focus()
-  } else {
-    const firstItem = getFirstFocusableItem(containerElement)
-    firstItem ? firstItem.focus() : null
-  }
+export function useInitialFocus({initialFocusRef, containerRef, isOpen}: InitialFocusProps) {
+  const focusElement = initialFocusRef ? initialFocusRef.current : null
+  const containerElement = useProvidedRefOrCreate(containerRef)
+  useEffect(() => {
+    initialFocus({initialFocusElement: focusElement, containerElement, isOpen})
+  }, [isOpen, initialFocusRef, containerRef])
 }
