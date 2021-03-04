9de73a40d9acd9faf3903c5f4b82a6fb8cad84f5
Trevor Gau <trevorsg@gmail.com>
2021-03-04 12:59:36 -0500

    Add an iterateFocusableElements dom util

---
+++ src/__tests__/behaviors/iterateFocusableElements.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+
+import React from 'react'
+import {iterateFocusableElements} from '../../utils/iterateFocusable'
+import {render} from '@testing-library/react'
+
+it('Should iterate through focusable elements only', () => {
+  const {container} = render(
+    <div>
+      <div>
+        <textarea></textarea>
+      </div>
+      <input />
+      <button>Hello</button>
+      <p>Not focusable</p>
+      <div tabIndex={0}>
+        <a tabIndex={-1} href="#boo">
+          Not focusable
+        </a>
+        <a href="#yah">Focusable</a>
+      </div>
+    </div>
+  )
+
+  const focusable = Array.from(iterateFocusableElements(container))
+  expect(focusable.length).toEqual(5)
+  expect(focusable[0].tagName.toLowerCase()).toEqual('textarea')
+  expect(focusable[1].tagName.toLowerCase()).toEqual('input')
+  expect(focusable[2].tagName.toLowerCase()).toEqual('button')
+  expect(focusable[3].tagName.toLowerCase()).toEqual('div')
+  expect(focusable[4].tagName.toLowerCase()).toEqual('a')
+  expect(focusable[4].getAttribute('href')).toEqual('#yah')
+})
+
+it('Should iterate through focusable elements in reverse', () => {
+  const {container} = render(
+    <div>
+      <div>
+        <textarea></textarea>
+      </div>
+      <input />
+      <button>Hello</button>
+      <p>Not focusable</p>
+      <div tabIndex={0}>
+        <a tabIndex={-1} href="#boo">
+          Not focusable
+        </a>
+        <a href="#yah">Focusable</a>
+      </div>
+    </div>
+  )
+
+  const focusable = Array.from(iterateFocusableElements(container, true))
+  expect(focusable.length).toEqual(5)
+  expect(focusable[0].tagName.toLowerCase()).toEqual('a')
+  expect(focusable[0].getAttribute('href')).toEqual('#yah')
+  expect(focusable[1].tagName.toLowerCase()).toEqual('div')
+  expect(focusable[2].tagName.toLowerCase()).toEqual('button')
+  expect(focusable[3].tagName.toLowerCase()).toEqual('input')
+  expect(focusable[4].tagName.toLowerCase()).toEqual('textarea')
+})
