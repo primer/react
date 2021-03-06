b1bc0975417dd9c71b4c74783d899d2bea23e17d
Emily Plummer <emplums@github.com>
2021-03-05 16:20:03 -0800

    add tests for useIntitialFocus

---
+++ src/__tests__/behaviors/initialFocus.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Button} from '../..'
+import {render} from '@testing-library/react'
+import { initialFocus } from '../../behaviors/initialFocus'
+
+const Component = () => {
+  return (
+    <div data-testid="container">
+      <button className="one">button one</button>
+      <button className="two">button two</button>
+    </div>
+  )
+}
+
+it('should focus element passed into function', async () => {
+  const {getByText, getByTestId} =  render(<Component />)
+  const buttonTwo = getByText('button two')
+  const container = getByTestId("container")
+  initialFocus({initialFocusElement: buttonTwo, containerElement: container as HTMLElement})
+  expect(document.activeElement).toEqual(buttonTwo)
+})
+
+
+it('should focus the first focusable element if no initial element is passed in', async () => {
+  const {getByText, getByTestId} = render(<Component />)
+  const buttonOne = getByText('button one')
+  const container = getByTestId("container")
+  initialFocus({containerElement: container as HTMLElement})
+  expect(document.activeElement).toEqual(buttonOne)
+})
