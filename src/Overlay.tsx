86ebff8d3e0f185e2df2182d33e62321546123a8
Emily Plummer <emplums@github.com>
2021-03-11 18:24:18 -0800

    remove mobile variant for now

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -9,35 +9,7 @@ import { useOverlay } from './hooks/useOverlay'
 type StyledOverlayProps = {
   width?: keyof typeof widthMap
   height?: keyof typeof heightMap
-  mobileVariant?: 'bottomSheet' | 'fullScreen'
 }
-
-const bottomSheetStyles = css`
-  @media (max-width: 544px) {
-    position: fixed;
-    width: 100vw;
-    max-width: 480px;
-    left: calc((max(100vw, 480px) - 480px) / 2);
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
-
-    bottom: 0 !important;
-
-    animation: overlay--bottom-sheet-appear 300ms ${get('animation.easeOutCubic')};
-
-    @keyframes overlay--bottom-sheet-appear {
-      0% {
-        opacity: 0;
-        transform: translate(0, 100%);
-      }
-      100% {
-        opacity: 1;
-        transform: translate(0, 0%);
-      }
-    }
-  }
-`
-
 
 const heightMap = {
   'sm': '480px',
@@ -75,8 +47,6 @@ const StyledOverlay  = styled.div<StyledOverlayProps>`
       opacity: 1;
     }
   }
-  ${props => props.mobileVariant === 'bottomSheet' ? bottomSheetStyles : ''}
-
 `
 
 export type OverlayProps = {
@@ -98,7 +68,6 @@ const Overlay =
 Overlay.defaultProps = {
   theme,
   height: 'auto',
-  mobileVariant: 'bottomSheet',
   width: 'auto'
 }
 
