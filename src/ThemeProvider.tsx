37f7156443b0b50c5e29c6de015c98b86e67ad3e
Cole Bemis <colebemis@github.com>
2021-03-16 13:57:43 -0700

    Add colorMode, dayScheme, nightScheme props

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -3,16 +3,41 @@ import {ThemeProvider as SCThemeProvider} from 'styled-components'
 import defaultTheme from './theme'
 import deepmerge from 'deepmerge' // TODO: review dependency
 
+const DEFAULT_COLOR_MODE = 'day'
+const DEFAULT_DAY_SCHEME = 'light'
+const DEFAULT_NIGHT_SCHEME = 'dark'
+
 type Theme = any
+type ColorMode = 'day' | 'night'
+type ColorModeWithAuto = ColorMode | 'auto'
 
 export type ThemeProviderProps = {
   theme?: Theme
+  colorMode?: ColorModeWithAuto
+  dayScheme?: string
+  nightScheme?: string
   children: React.ReactNode
 }
 
-function ThemeProvider({theme = defaultTheme, children}: ThemeProviderProps) {
-  const resolvedTheme = applyColorScheme(theme, 'light')
+function ThemeProvider({theme = defaultTheme, colorMode, dayScheme, nightScheme, children}: ThemeProviderProps) {
+  const colorScheme = getColorScheme(
+    colorMode ?? DEFAULT_COLOR_MODE,
+    dayScheme ?? DEFAULT_DAY_SCHEME,
+    nightScheme ?? DEFAULT_NIGHT_SCHEME
+  )
+  const resolvedTheme = applyColorScheme(theme, colorScheme)
   return <SCThemeProvider theme={resolvedTheme}>{children}</SCThemeProvider>
+}
+
+function getColorScheme(colorMode: ColorModeWithAuto, dayScheme: string, nightScheme: string) {
+  switch (colorMode) {
+    case 'auto':
+      return dayScheme // TODO: implement auto mode
+    case 'day':
+      return dayScheme
+    case 'night':
+      return nightScheme
+  }
 }
 
 function applyColorScheme(theme: Theme, colorScheme: string) {
--- src/__tests__/ThemeProvider.tsx
+++ src/__tests__/ThemeProvider.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import {ThemeProvider, Text} from '..'
 import 'jest-styled-components'
 
-it('accepts theme prop', () => {
+it('respects theme prop', () => {
   const theme = {
     colors: {
       text: '#f00'
@@ -61,4 +61,98 @@ it('defaults to light color scheme', () => {
   )
 
   expect(screen.getByText('Hello')).toHaveStyleRule('color', '#00f')
+})
+
+it('defaults to dark color scheme in night mode', () => {
+  const theme = {
+    colors: {
+      text: '#f00'
+    },
+    colorSchemes: {
+      light: {
+        colors: {
+          text: '#00f'
+        }
+      },
+      dark: {
+        colors: {
+          text: '#0f0'
+        }
+      }
+    }
+  }
+
+  render(
+    <ThemeProvider theme={theme} colorMode="night">
+      <Text color="text">Hello</Text>
+    </ThemeProvider>
+  )
+
+  expect(screen.getByText('Hello')).toHaveStyleRule('color', '#0f0')
+})
+
+it('respects nightScheme prop', () => {
+  const theme = {
+    colors: {
+      text: '#f00'
+    },
+    colorSchemes: {
+      light: {
+        colors: {
+          text: '#00f'
+        }
+      },
+      dark: {
+        colors: {
+          text: '#0f0'
+        }
+      },
+      dark_dimmed: {
+        colors: {
+          text: '#ff0'
+        }
+      }
+    }
+  }
+
+  render(
+    <ThemeProvider theme={theme} colorMode="night" nightScheme="dark_dimmed">
+      <Text color="text">Hello</Text>
+    </ThemeProvider>
+  )
+
+  expect(screen.getByText('Hello')).toHaveStyleRule('color', '#ff0')
+})
+
+it('respects dayScheme prop', () => {
+  const theme = {
+    colors: {
+      text: '#f00'
+    },
+    colorSchemes: {
+      light: {
+        colors: {
+          text: '#00f'
+        }
+      },
+      dark: {
+        colors: {
+          text: '#0f0'
+        }
+      },
+      dark_dimmed: {
+        colors: {
+          text: '#ff0'
+        }
+      }
+    }
+  }
+
+  render(
+    <ThemeProvider theme={theme} colorMode="day" dayScheme="dark" nightScheme="dark_dimmed">
+      <Text color="text">Hello</Text>
+    </ThemeProvider>
+  )
+
+  expect(screen.getByText('Hello')).toHaveStyleRule('color', '#0f0')
 })
