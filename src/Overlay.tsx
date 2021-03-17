f96d41ad9b975c16f392c4d2cf7167661a93c846
Emily Plummer <emplums@github.com>
2021-03-17 16:38:23 -0700

    add jsdoc comment

--- src/Overlay.tsx
+++ src/Overlay.tsx
@@ -51,7 +51,6 @@ const StyledOverlay = styled.div<StyledOverlayProps>`
   ${COMMON};
   ${POSITION};
 `
-
 export type OverlayProps = {
   ignoreClickRefs: React.RefObject<HTMLElement>[]
   initialFocusRef?: React.RefObject<HTMLElement>
@@ -64,7 +63,21 @@ export type OverlayProps = {
 } & Omit<ComponentProps<typeof StyledOverlay>, 'visibility'>
 
 // omit positions props
-
+/**
+ * An `Overlay` is a flexible floating surface, used to display transient content such as menus,
+ * selection options, dialogs, and more. Overlays use shadows to express elevation. The `Overlay`
+ * component handles all behaviors needed by overlay UIs as well as the common styles that all overlays * should have.
+ * @param positionSettings Settings for calculating the anchored position.
+ * @param positionDeps Dependencies to determine when to re-calculate the position of the overlay.
+ * @param ignoreClickRefs Optional. An array of ref objects to ignore clicks on in the `onOutsideClick` behavior. This is often used to ignore clicking on the element that toggles the open/closed state for the `Overlay` to prevent the `Overlay` from being toggled twice.
+ * @param initialFocusRef Optional. Ref for the element to focus when the `Overlay` is opened. If nothing is provided, the first focusable element in the `Overlay` body is focused.
+ * @param anchorRef Required. Element the `Overlay` should be anchored to.
+ * @param returnFocusRef Required. Ref for the element to focus when the `Overlay` is closed.
+ * @param onClickOutside  Required. Function to call when clicking outside of the `Overlay`. Typically this function sets the `Overlay` visibility state to `false`.
+ * @param onEscape Required. Function to call when user presses `Escape`. Typically this function sets the `Overlay` visibility state to `false`.
+ * @param width Sets the width of the `Overlay`, pick from our set list of widths, or pass `auto` to automatically set the width based on the content of the `Overlay`. `sm` corresponds to `256px`, `md` corresponds to `320px`, `lg` corresponds to `480px`, and `xl` corresponds to `640px`.
+ * @param height Sets the height of the `Overlay`, pick from our set list of heights, or pass `auto` to automatically set the height based on the content of the `Overlay`. `sm` corresponds to `480px` and `md` corresponds to `640px`.
+ */
 const Overlay = ({
   onClickOutside,
   role = 'dialog',
