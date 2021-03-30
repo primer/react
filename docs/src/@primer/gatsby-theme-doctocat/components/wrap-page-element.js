825b5511b29d56239fee20a40ebe5e5a0caafc3e
Cole Bemis <colebemis@github.com>
2021-03-30 12:54:42 -0700

    Update theme in the docs

--- docs/src/@primer/gatsby-theme-doctocat/components/wrap-page-element.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/wrap-page-element.js
@@ -1,13 +1,19 @@
-import {BaseStyles, ThemeProvider} from '@primer/components'
-import React from 'react'
+import {BaseStyles, theme, ThemeProvider} from '@primer/components'
 import SkipLink from '@primer/gatsby-theme-doctocat/src/components/skip-link'
+import primitives from '@primer/primitives'
+import deepmerge from 'deepmerge'
+import React from 'react'
 
 // Temporarily shadowing this file to wrap the page in our custom ThemeProvider.
 // We can remove this file when Doctocat uses the custom ThemeProvider.
 
+// Doctocat still uses presentional color variables.
+// We can remove this customTheme when Doctocat uses funtional color variables
+const customTheme = deepmerge(theme, {colors: primitives.colors.light.scale})
+
 function wrapPageElement({element}) {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={customTheme}>
       <BaseStyles>
         <SkipLink />
         {element}
