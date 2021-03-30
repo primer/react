519d96f80cd06cd91b0efed98cf06e1fbd130b53
Emily Plummer <emplums@github.com>
2021-03-30 11:33:17 -0700

    Update useOpenAndCloseFocus.ts

--- src/hooks/useOpenAndCloseFocus.ts
+++ src/hooks/useOpenAndCloseFocus.ts
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import {iterateTabbableElements} from '../utils/iterateTabbable'
+import {iterateFocusableElements} from '../utils/iterateFocusableElements'
 
 export type UseOpenAndCloseFocusSettings = {
   initialFocusRef?: React.RefObject<HTMLElement>
@@ -17,7 +17,7 @@ export function useOpenAndCloseFocus({
     if (initialFocusRef && initialFocusRef.current) {
       initialFocusRef.current.focus()
     } else if (containerRef && containerRef.current) {
-      const firstItem = iterateTabbableElements(containerRef.current).next().value
+      const firstItem = iterateFocusableElements(containerRef.current).next().value
       firstItem?.focus()
     }
     return function() {
