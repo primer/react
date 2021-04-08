089ddd03cfc7262f906bbb41db07a55499970213
Cole Bemis <colebemis@github.com>
2021-04-08 12:00:42 -0700

    Move as prop definition

--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -121,7 +121,10 @@ const Wrapper = styled.span<StyledWrapperProps>`
   ${sx};
 `
 
-type TextInputInternalProps = {icon?: React.ComponentType<{className?: string}>} & ComponentProps<typeof Wrapper> &
+type TextInputInternalProps = {
+  as?: React.ReactElement // This is a band-aid fix until we have better type support for the `as` prop
+  icon?: React.ComponentType<{className?: string}>
+} & ComponentProps<typeof Wrapper> &
   ComponentProps<typeof Input>
 
 // using forwardRef is important so that other components (ex. SelectMenu) can autofocus the input
