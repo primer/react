8eb67b485e2248f0ee040617d94718d9d2f0f6ae
Van Anderson <van.m.anderson@gmail.com>
2021-04-14 22:51:08 -0500

    chore: delete isRefObject

--- src/utils/isRefObject.ts
+++
@@ -1,5 +0,0 @@
-import type {ForwardedRef, RefObject} from 'react'
-
-export function isRefObject<T extends HTMLElement>(x: ForwardedRef<T>): x is RefObject<T> {
-  return !!(x && 'current' in x)
-}
