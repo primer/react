2b89594cd5932677a3b0832a680667fe7c156c7a
Trevor Gau <trevorsg@gmail.com>
2021-04-19 14:44:29 -0400

    Fix tests.

--- src/Position.tsx
+++ src/Position.tsx
@@ -20,18 +20,21 @@ export type AbsoluteProps = Omit<PositionProps, 'position'>
 export const Absolute = React.forwardRef((props: AbsoluteProps, ref) => {
   return <Position {...props} position="absolute" ref={ref} />
 })
+Absolute.displayName = 'Absolute'
 
 // Fixed
 export type FixedProps = Omit<PositionProps, 'position'>
 export const Fixed = React.forwardRef((props: AbsoluteProps, ref) => {
   return <Position {...props} position="fixed" ref={ref} />
 })
+Fixed.displayName = 'Fixed'
 
 // Relative
 export type RelativeProps = Omit<PositionProps, 'position'>
 export const Relative = React.forwardRef((props: RelativeProps, ref) => {
   return <Position {...props} position="relative" ref={ref} />
 })
+Relative.displayName = 'Relative'
 
 // Sticky
 export type StickyProps = Omit<PositionProps, 'position'>
@@ -39,3 +42,4 @@ export const Sticky = React.forwardRef((props: StickyProps, ref) => {
   return <Position {...props} position="sticky" ref={ref} />
 })
 Sticky.defaultProps = {top: 0, zIndex: 1}
+Sticky.displayName = 'Sticky'
