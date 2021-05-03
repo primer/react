a529e9556a92bf14ba9f4044ea2e37d3dfff298b
Van Anderson <van.m.anderson@gmail.com>
2021-05-03 10:57:32 -0500

    update hook doc comment

--- src/hooks/useProvidedStateOrCreate.ts
+++ src/hooks/useProvidedStateOrCreate.ts
@@ -1,12 +1,13 @@
 import {useCallback, useState} from 'react'
 
 /**
- * There are some situations where we only want to create a new ref if one is not provided to a component
- * or hook as a prop. However, due to the `rules-of-hooks`, we cannot conditionally make a call to `React.useRef`
- * only in the situations where the ref is not provided as a prop.
+ * There are some situations where we want to give users the option to control state externally with their own state handlers
+ * or default to using internal state handlers. Because of the 'rules-of-hooks', we cannot conditionally make a call to `React.useState`
+ * only in the situations where the state is not provided as a prop.
  * This hook aims to encapsulate that logic, so the consumer doesn't need to be concerned with violating `rules-of-hooks`.
- * @param providedRef The ref to use - if undefined, will use the ref from a call to React.useRef
- * @type TRef The type of the RefObject which should be created.
+ * @param externalState The state to use - if undefined, will use the state from a call to React.useState
+ * @param setExternalState The setState to use - if undefined, will use the state from a call to React.useState
+ * @param defaultState The defaultState to use, if using internal state.
  */
 export function useProvidedStateOrCreate<T>(
   externalState?: T,
