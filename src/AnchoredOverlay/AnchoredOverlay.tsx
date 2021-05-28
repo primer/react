81c8ca98f0737320cf3b4d79820614d2aab8179d
Van Anderson <van.m.anderson@gmail.com>
2021-05-28 14:39:36 -0500

    don't redeclare generic var

--- src/AnchoredOverlay/AnchoredOverlay.tsx
+++ src/AnchoredOverlay/AnchoredOverlay.tsx
@@ -10,9 +10,7 @@ export interface AnchoredOverlayProps extends Pick<OverlayProps, 'height' | 'wid
    * A custom function component used to render the anchor element.
    * Will receive the selected text as `children` prop when an item is activated.
    */
-  renderAnchor: <T extends React.HTMLAttributes<HTMLElement>>(
-    props: T
-  ) => JSX.Element | (<T extends React.HTMLAttributes<HTMLElement>>(props: T) => null)
+  renderAnchor: <T extends React.HTMLAttributes<HTMLElement>>(props: T) => JSX.Element | ((props: T) => null)
 
   /**
    * An override to the internal ref that will be spread on to the renderAnchor
