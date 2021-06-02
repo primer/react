fbce635806a8e9f595888e898ca1be66d42aef92
Van Anderson <van.m.anderson@gmail.com>
2021-06-02 11:21:00 -0500

    implement scrollflash into SelectPanel

---
+++ src/hooks/useScrollFlash.ts
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react'
+/**
+ * This hook will flash the scrollbars for a ref of a container that has scrollable overflow
+ * @param scrollContainerRef The ref of the scrollable content
+ */
+export default function useScrollFlash(scrollContainerRef: React.RefObject<HTMLElement>) {
+  // https://adxlv.computer/projects/flash-scrollers/
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current
+    if (!scrollContainer) {
+      return
+    }
+    const currentScroll = scrollContainer.scrollTop
+    const maxScroll = scrollContainer.scrollHeight
+
+    const altScroll = (() => {
+      if (currentScroll < 1 || currentScroll < maxScroll) return currentScroll + 1
+      else return currentScroll - 1
+    })()
+
+    scrollContainer.scrollTop = altScroll
+    scrollContainer.scrollTop = currentScroll
+
+    const flashScrollersEvent = new Event('flashScrollers')
+    scrollContainer.dispatchEvent(flashScrollersEvent)
+  }, [scrollContainerRef])
+}
