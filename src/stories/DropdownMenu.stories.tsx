23be0ed7aeff3cc21a678769eb5ba0ddf9a8d155
Jonathan Fuchs <jfuchs@github.com>
2021-08-10 13:21:56 -0700

    Adds open, onOpenChange, and anchorRef props to DropdownMenu (#1372)
    * Adds open, onOpenChange, and anchorRef props to DropdownMenu
    
    * Changeset
    
    * Update src/DropdownMenu/DropdownMenu.tsx
    
    Co-authored-by: Cole Bemis <colebemis@github.com>
    
    * comment update
    
    Co-authored-by: Cole Bemis <colebemis@github.com>

--- src/stories/DropdownMenu.stories.tsx
+++ src/stories/DropdownMenu.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {theme, ThemeProvider} from '..'
 import {ItemInput} from '../ActionList/List'
 import BaseStyles from '../BaseStyles'
+import Box from '../Box'
 import {DropdownMenu, DropdownButton} from '../DropdownMenu'
 import TextInput from '../TextInput'
 
@@ -52,3 +53,32 @@ export function FavoriteColorStory(): JSX.Element {
   )
 }
 FavoriteColorStory.storyName = 'Favorite Color'
+
+export function ExternalAnchorStory(): JSX.Element {
+  const items = React.useMemo(() => [{text: '🔵 Cyan'}, {text: '🔴 Magenta'}, {text: '🟡 Yellow'}], [])
+  const [selectedItem, setSelectedItem] = React.useState<ItemInput | undefined>()
+  const anchorRef = React.useRef<HTMLDivElement>(null)
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="flex-start">
+      <Box display="flex" flexDirection="row">
+        <DropdownButton onClick={() => setOpen(true)}>Click me to open the dropdown</DropdownButton>
+        <Box ref={anchorRef} bg="papayawhip" p={4} ml={40} borderRadius={2} display="inline-block">
+          Anchored on me!
+        </Box>
+      </Box>
+      <DropdownMenu
+        renderAnchor={null}
+        anchorRef={anchorRef}
+        open={open}
+        onOpenChange={setOpen}
+        placeholder="🎨"
+        items={items}
+        selectedItem={selectedItem}
+        onChange={setSelectedItem}
+      />
+    </Box>
+  )
+}
+ExternalAnchorStory.storyName = 'DropdownMenu with External Anchor'
