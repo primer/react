84628b8e3695a1e1da9433107e9760fa8db0149c
Siddharth Kshetrapal <sid@Siddharths-MacBook-Pro.local>
2021-09-09 19:48:15 +0200

    add alt and src + update tests

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -8,6 +8,10 @@ type StyledAvatarProps = {
   size?: number
   /** Sets the shape of the avatar to a square if true. If false, the avatar will be circular. */
   square?: boolean
+  /** URL of the avatar image. */
+  src: string
+  /** Provide an alt text in case the Avatar is used without the user's name next to it. */
+  alt?: string
 } & SystemCommonProps &
   SxProp
 
