8cd72520c3b1cc5c08ec6d24d450132ccbb5c5c6
Katie Langerman <langermank@github.com>
2021-09-17 19:09:32 -0400

    add colormode and colorscheme toggles to storybook toolbar (#1436)
    * add colormode and colorscheme to sb toolbar
    
    * addressing feedback
    
    * hard code colorMode
    
    * Revert lockfile version
    
    Co-authored-by: Cole Bemis <colebemis@github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,62 @@
 import {addons} from '@storybook/addons'
+import {ThemeProvider, themeGet, theme} from '../src'
+import {createGlobalStyle} from 'styled-components'
+
+// set global theme styles for each story
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${themeGet('colors.bg.primary')};
+    color: ${themeGet('colors.text.primary')};
+  }
+`
+export const globalTypes = {
+  colorMode: {
+    name: 'Color mode',
+    description: 'Color mode (day, night, auto)',
+    defaultValue: 'day',
+    toolbar: {
+      icon: 'paintbrush',
+      // array of colorMode items
+      items: ['day', 'night', 'auto'],
+      showName: true
+    }
+  },
+  dayScheme: {
+    name: 'Day color scheme',
+    description: 'Day color scheme',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: Object.keys(theme.colorSchemes),
+      showName: true
+    }
+  },
+  nightScheme: {
+    name: 'Night color scheme',
+    description: 'Night color scheme',
+    defaultValue: 'dark',
+    toolbar: {
+      icon: 'circle',
+      items: Object.keys(theme.colorSchemes),
+      showName: true
+    }
+  }
+}
+
+// context.globals.X references items in globalTypes
+const withThemeProvider = (Story, context) => {
+  return (
+    <ThemeProvider
+      colorMode={context.globals.colorMode}
+      dayScheme={context.globals.dayScheme}
+      nightScheme={context.globals.nightScheme}
+    >
+      <GlobalStyle />
+      <Story {...context} />
+    </ThemeProvider>
+  )
+}
+export const decorators = [withThemeProvider]
 
 addons.setConfig({
   // Some stories may set up keyboard event handlers, which can can be interfered
