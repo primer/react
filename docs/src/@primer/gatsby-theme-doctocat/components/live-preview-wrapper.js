54aea3fac9df3847ddc853abdb963d551966726f
Pavithra Kodmad <pksjce@gmail.com>
2021-09-30 16:22:33 +1000

    Fix bug with dropdownopen and realign the button

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -1,12 +1,13 @@
 import {BaseStyles, Box, ThemeProvider, useTheme, DropdownMenu, DropdownButton} from '@primer/components'
 import React from 'react'
 
-function ThemeSwitcher({setShowSwitcher}) {
+function ThemeSwitcher({setShowSwitcher, setIsDropdownOpen}) {
   const {theme, dayScheme, setDayScheme} = useTheme()
   const items = Object.keys(theme.colorSchemes).map(scheme => ({text: scheme.replace(/_/g, ' '), key: scheme}))
   const selectedItem = React.useMemo(() => items.find(item => item.key === dayScheme), [items, dayScheme])
   return (
     <DropdownMenu
+      onOpenChange={setIsDropdownOpen}
       renderAnchor={({children, ...anchorProps}) => (
         <DropdownButton variant="small" {...anchorProps}>
           {children}
@@ -26,6 +27,7 @@ function ThemeSwitcher({setShowSwitcher}) {
 // Users can shadow this file to wrap live previews.
 // This is useful for applying global styles.
 function LivePreviewWrapper({children}) {
+  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false)
   const [showSwitcher, setShowSwitcher] = React.useState(false)
   return (
     <ThemeProvider>
@@ -38,15 +40,15 @@ function LivePreviewWrapper({children}) {
           setShowSwitcher(true)
         }}
         onMouseLeave={() => {
-          setShowSwitcher(false)
+          !isDropdownOpen && setShowSwitcher(false)
         }}
         width="100%"
         bg="canvas.default"
         position="relative"
         sx={{borderTopLeftRadius: 2, borderTopRightRadius: 2}}
       >
-        <Box p={2} display={showSwitcher ? '' : 'none'} zIndex="1" position="absolute" right="2">
-          <ThemeSwitcher setShowSwitcher={setShowSwitcher} />
+        <Box p={2} display={showSwitcher ? '' : 'none'} zIndex="1" position="absolute" top="0" right="0">
+          <ThemeSwitcher setShowSwitcher={setShowSwitcher} setIsDropdownOpen={setIsDropdownOpen} />
         </Box>
         <Box p={3}>
           <BaseStyles>{children}</BaseStyles>
