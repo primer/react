b773f54f6b66bcf4bb9e4de88a99ade2cb207465
Pavithra K <pk@Pavithras-MacBook-Air.local>
2021-10-05 17:28:35 +1100

    Remove hover behaviour

--- docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/live-preview-wrapper.js
@@ -1,13 +1,12 @@
 import {BaseStyles, Box, ThemeProvider, useTheme, DropdownMenu, DropdownButton} from '@primer/components'
 import React from 'react'
 
-function ThemeSwitcher({setIsDropdownOpen}) {
+function ThemeSwitcher() {
   const {theme, dayScheme, setDayScheme} = useTheme()
   const items = Object.keys(theme.colorSchemes).map(scheme => ({text: scheme.replace(/_/g, ' '), key: scheme}))
   const selectedItem = React.useMemo(() => items.find(item => item.key === dayScheme), [items, dayScheme])
   return (
     <DropdownMenu
-      onOpenChange={setIsDropdownOpen}
       renderAnchor={({children, ...anchorProps}) => (
         <DropdownButton variant="small" {...anchorProps}>
           {children}
@@ -25,21 +24,9 @@ function ThemeSwitcher({setIsDropdownOpen}) {
 // Users can shadow this file to wrap live previews.
 // This is useful for applying global styles.
 function LivePreviewWrapper({children}) {
-  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false)
-  const [showSwitcher, setShowSwitcher] = React.useState(false)
   return (
     <ThemeProvider>
       <Box
-        tabIndex="0"
-        onFocusCapture={() => {
-          setShowSwitcher(true)
-        }}
-        onMouseEnter={() => {
-          setShowSwitcher(true)
-        }}
-        onMouseLeave={() => {
-          !isDropdownOpen && setShowSwitcher(false)
-        }}
         width="100%"
         bg="canvas.default"
         position="relative"
@@ -48,8 +35,8 @@ function LivePreviewWrapper({children}) {
         <Box p={3} sx={{flexGrow: 1}}>
           <BaseStyles>{children}</BaseStyles>
         </Box>
-        <Box p={2} display={showSwitcher ? '' : 'none'}>
-          <ThemeSwitcher setIsDropdownOpen={setIsDropdownOpen} />
+        <Box p={2}>
+          <ThemeSwitcher />
         </Box>
       </Box>
     </ThemeProvider>
