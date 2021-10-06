9637cc42d7522a65b8c6b0fb117028f581d76f6e
Mike Perrotti <mperrotti@github.com>
2021-10-05 21:16:26 -0400

    improves Storybook stories

--- src/stories/Token.stories.tsx
+++ src/stories/Token.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Meta} from '@storybook/react'
 import {VerifiedIcon} from '@primer/octicons-react'
+import {withKnobs, color} from '@storybook/addon-knobs'
 
 import {get} from '../constants'
 import {BaseStyles, ThemeProvider} from '..'
@@ -14,6 +15,7 @@ export default {
   title: 'Prototyping/Token',
 
   decorators: [
+    withKnobs,
     Story => {
       return (
         <ThemeProvider>
@@ -62,6 +64,12 @@ export const defaultToken = () => (
     <SingleExampleContainer label="Resting">
       <Token text="Default Token" />
     </SingleExampleContainer>
+    <SingleExampleContainer label="w/ leadingVisual passed">
+      <Token text="Default Token" leadingVisual={() => <VerifiedIcon size={12} />} />
+    </SingleExampleContainer>
+    <SingleExampleContainer label="isSelected">
+      <Token text="Default Token" isSelected={true} />
+    </SingleExampleContainer>
     <SingleExampleContainer label="Interactive">
       <Box
         display="flex"
@@ -83,7 +91,7 @@ export const defaultToken = () => (
         }}
       />
     </SingleExampleContainer>
-    <SingleExampleContainer label="w/ onRemove passed and the token is clickable">
+    <SingleExampleContainer label="w/ onRemove passed and the token is interactive">
       <Box
         display="flex"
         sx={{
@@ -118,11 +126,43 @@ export const defaultToken = () => (
         />
       </Box>
     </SingleExampleContainer>
-    <SingleExampleContainer label="w/ leadingVisual passed">
-      <Token text="Default Token" leadingVisual={() => <VerifiedIcon size={12} />} />
-    </SingleExampleContainer>
-    <SingleExampleContainer label="isSelected">
-      <Token text="Default Token" isSelected={true} />
+    <SingleExampleContainer label="size options">
+      <Box
+        display="flex"
+        sx={{
+          alignItems: 'start',
+          gap: get('space.2')
+        }}
+      >
+        <Token
+          size="small"
+          text="'small' Token"
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
+        <Token
+          size="medium"
+          text="'medium' Token"
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
+        <Token
+          size="large"
+          text="'large' Token (default)"
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
+        <Token
+          size="xlarge"
+          text="'xlarge' Token"
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
+      </Box>
     </SingleExampleContainer>
   </ExampleCollectionContainer>
 )
@@ -130,31 +170,15 @@ export const defaultToken = () => (
 export const profileToken = () => (
   <ExampleCollectionContainer>
     <SingleExampleContainer label="Resting">
-      <ProfileToken size="large" avatarSrc="https://avatars.githubusercontent.com/mperrotti" text="Mike Perrotti" />
-    </SingleExampleContainer>
-    <SingleExampleContainer label="w/ onRemove passed">
-      <ProfileToken
-        size="large"
-        avatarSrc="https://avatars.githubusercontent.com/mperrotti"
-        text="Mike Perrotti"
-        onRemove={() => {
-          console.log('remove me')
-        }}
-      />
+      <ProfileToken avatarSrc="https://avatars.githubusercontent.com/mperrotti" text="Mike Perrotti" />
     </SingleExampleContainer>
     <SingleExampleContainer label="isSelected">
       <ProfileToken
-        size="large"
         avatarSrc="https://avatars.githubusercontent.com/mperrotti"
         text="Mike Perrotti"
         isSelected={true}
       />
     </SingleExampleContainer>
-  </ExampleCollectionContainer>
-)
-
-export const labelToken = () => (
-  <ExampleCollectionContainer>
     <SingleExampleContainer label="Interactive">
       <Box
         display="flex"
@@ -163,43 +187,37 @@ export const labelToken = () => (
           gap: get('space.2')
         }}
       >
-        <IssueLabelToken as="a" href="http://google.com/" text="Link" />
-        <IssueLabelToken as="button" onClick={() => console.log('clicked')} text="Button" />
-        <IssueLabelToken as="span" tabIndex={0} onFocus={() => console.log('focused')} text="Focusable Span" />
-      </Box>
-    </SingleExampleContainer>
-    <SingleExampleContainer label="Interactive (#656BFE fill color passed)">
-      <Box
-        display="flex"
-        sx={{
-          alignItems: 'start',
-          gap: get('space.2')
-        }}
-      >
-        <IssueLabelToken fillColor="#656BFE" as="a" href="http://google.com/" text="Link" />
-        <IssueLabelToken fillColor="#656BFE" as="button" onClick={() => console.log('clicked')} text="Button" />
-        <IssueLabelToken
-          fillColor="#656BFE"
+        <ProfileToken
+          as="a"
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          href="http://google.com/"
+          text="Link"
+        />
+        <ProfileToken
+          as="button"
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          onClick={() => console.log('clicked')}
+          text="Button"
+        />
+        <ProfileToken
           as="span"
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
           tabIndex={0}
           onFocus={() => console.log('focused')}
           text="Focusable Span"
         />
       </Box>
     </SingleExampleContainer>
-    <SingleExampleContainer label="Default (#656BFE fill color passed)">
-      <IssueLabelToken text="good first issue" fillColor="#656BFE" />
-    </SingleExampleContainer>
     <SingleExampleContainer label="w/ onRemove passed">
-      <IssueLabelToken
-        text="good first issue"
-        fillColor="#656BFE"
+      <ProfileToken
+        avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+        text="Mike Perrotti"
         onRemove={() => {
           console.log('remove me')
         }}
       />
     </SingleExampleContainer>
-    <SingleExampleContainer label="w/ onRemove passed and the token is clickable">
+    <SingleExampleContainer label="w/ onRemove passed and the token is interactive">
       <Box
         display="flex"
         sx={{
@@ -207,63 +225,206 @@ export const labelToken = () => (
           gap: get('space.2')
         }}
       >
-        <IssueLabelToken
+        <ProfileToken
           as="a"
           href="http://google.com/"
-          text="good first issue"
-          fillColor="#656BFE"
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          text="Link"
           onRemove={() => {
             console.log('remove me')
           }}
         />
-        <IssueLabelToken
+        <ProfileToken
           as="button"
           onClick={() => console.log('clicked')}
-          text="good first issue"
-          fillColor="#656BFE"
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          text="Button"
           onRemove={() => {
             console.log('remove me')
           }}
         />
-        <IssueLabelToken
+        <ProfileToken
           as="span"
           tabIndex={0}
           onFocus={() => console.log('focused')}
-          text="good first issue"
-          fillColor="#656BFE"
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          text="Focusable Span"
           onRemove={() => {
             console.log('remove me')
           }}
         />
       </Box>
     </SingleExampleContainer>
-    <SingleExampleContainer label="isSelected">
-      <IssueLabelToken text="good first issue" fillColor="#656BFE" isSelected={true} />
-    </SingleExampleContainer>
-    <SingleExampleContainer label="Automatically picks a readable text color based on fill color">
-      <Box display="flex" sx={{gap: get('space.2')}}>
-        <IssueLabelToken text="good first issue" fillColor="#656BFE" />
-        <IssueLabelToken text="bug" fillColor="#FFF06C" />
+    <SingleExampleContainer label="size options">
+      <Box
+        display="flex"
+        sx={{
+          alignItems: 'start',
+          gap: get('space.2')
+        }}
+      >
+        <ProfileToken
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          size="small"
+          text="'small' Token"
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
+        <ProfileToken
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          size="medium"
+          text="'medium' Token"
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
+        <ProfileToken
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          size="large"
+          text="'large' Token (default)"
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
+        <ProfileToken
+          avatarSrc="https://avatars.githubusercontent.com/mperrotti"
+          size="xlarge"
+          text="'xlarge' Token"
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
       </Box>
     </SingleExampleContainer>
   </ExampleCollectionContainer>
 )
 
-export const sizes = () => (
-  <ExampleCollectionContainer>
-    <SingleExampleContainer label="default Token component">
-      <Box
-        display="flex"
-        sx={{
-          alignItems: 'start',
-          gap: get('space.2')
-        }}
-      >
-        <Token size="small" text="'small' Token" />
-        <Token size="medium" text="'medium' Token" />
-        <Token size="large" text="'large' Token (default)" />
-        <Token size="xlarge" text="'xlarge' Token" />
-      </Box>
-    </SingleExampleContainer>
-  </ExampleCollectionContainer>
-)
+export const labelToken = () => {
+  const label = 'Color'
+  const defaultValue = '#0366d6'
+  const groupId = 'labelToken'
+  const value = color(label, defaultValue, groupId)
+
+  return (
+    <ExampleCollectionContainer>
+      <Text fontSize={1} color="fg.subtle">
+        Hint: use the "Knobs" tab in the Addons panel to change the token colors
+      </Text>
+      <SingleExampleContainer label="Default">
+        <IssueLabelToken text="good first issue" fillColor={value} />
+      </SingleExampleContainer>
+      <SingleExampleContainer label="isSelected">
+        <IssueLabelToken text="good first issue" fillColor={value} isSelected={true} />
+      </SingleExampleContainer>
+      <SingleExampleContainer label="Interactive">
+        <Box
+          display="flex"
+          sx={{
+            alignItems: 'start',
+            gap: get('space.2')
+          }}
+        >
+          <IssueLabelToken fillColor={value} as="a" href="http://google.com/" text="Link" />
+          <IssueLabelToken fillColor={value} as="button" onClick={() => console.log('clicked')} text="Button" />
+          <IssueLabelToken
+            fillColor={value}
+            as="span"
+            tabIndex={0}
+            onFocus={() => console.log('focused')}
+            text="Focusable Span"
+          />
+        </Box>
+      </SingleExampleContainer>
+      <SingleExampleContainer label="w/ onRemove passed">
+        <IssueLabelToken
+          text="good first issue"
+          fillColor={value}
+          onRemove={() => {
+            console.log('remove me')
+          }}
+        />
+      </SingleExampleContainer>
+      <SingleExampleContainer label="w/ onRemove passed and the token is clickable">
+        <Box
+          display="flex"
+          sx={{
+            alignItems: 'start',
+            gap: get('space.2')
+          }}
+        >
+          <IssueLabelToken
+            as="a"
+            href="http://google.com/"
+            text="good first issue"
+            fillColor={value}
+            onRemove={() => {
+              console.log('remove me')
+            }}
+          />
+          <IssueLabelToken
+            as="button"
+            onClick={() => console.log('clicked')}
+            text="good first issue"
+            fillColor={value}
+            onRemove={() => {
+              console.log('remove me')
+            }}
+          />
+          <IssueLabelToken
+            as="span"
+            tabIndex={0}
+            onFocus={() => console.log('focused')}
+            text="good first issue"
+            fillColor={value}
+            onRemove={() => {
+              console.log('remove me')
+            }}
+          />
+        </Box>
+      </SingleExampleContainer>
+      <SingleExampleContainer label="size options">
+        <Box
+          display="flex"
+          sx={{
+            alignItems: 'start',
+            gap: get('space.2')
+          }}
+        >
+          <IssueLabelToken
+            fillColor={value}
+            size="small"
+            text="'small' Token"
+            onRemove={() => {
+              console.log('remove me')
+            }}
+          />
+          <IssueLabelToken
+            fillColor={value}
+            size="medium"
+            text="'medium' Token"
+            onRemove={() => {
+              console.log('remove me')
+            }}
+          />
+          <IssueLabelToken
+            fillColor={value}
+            size="large"
+            text="'large' Token (default)"
+            onRemove={() => {
+              console.log('remove me')
+            }}
+          />
+          <IssueLabelToken
+            fillColor={value}
+            size="xlarge"
+            text="'xlarge' Token"
+            onRemove={() => {
+              console.log('remove me')
+            }}
+          />
+        </Box>
+      </SingleExampleContainer>
+    </ExampleCollectionContainer>
+  )
+}
