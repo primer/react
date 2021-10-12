fbd148ed5971f8fa06bd2fdcd29647c467afe883
Dustin Savery <dusave@github.com>
2021-10-11 22:18:14 -0700

    linking anchor to overlay

--- src/DatePicker/DatePicker.tsx
+++ src/DatePicker/DatePicker.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import {OverlayProps} from '../Overlay'
 import {AnchoredOverlay} from '../AnchoredOverlay'
 import {FocusTrapHookSettings} from '../hooks/useFocusTrap'
 import {FocusZoneHookSettings} from '../hooks/useFocusZone'
+import {DatePickerAnchor} from './DatePickerAnchor'
+import {addDays} from 'date-fns'
 
 export interface DatePickerProps {
-  anchorVariant: 'full' | 'iconOnly'
+  /**
+   * An override to the internal ref that will be spread on to the renderAnchor
+   */
+  anchorRef?: React.RefObject<HTMLElement>
+  anchorVariant?: 'full' | 'iconOnly'
   /**
    * Settings to apply to the Focus Zone on the internal `Overlay` component.
    */
@@ -20,7 +26,7 @@ export interface DatePickerProps {
   /**
    * Determines whether the overlay portion of the component should be shown or not
    */
-  open: boolean
+  open?: boolean
 
   /**
    * A callback which is called whenever the overlay is currently closed and an "open gesture" is detected.
@@ -36,32 +42,69 @@ export interface DatePickerProps {
    * Props to be spread on the internal `Overlay` component.
    */
   overlayProps?: Partial<OverlayProps>
-
+  /**
+   * A custom function component used to render the anchor element.
+   * Will receive the selected text as `children` prop when an item is activated.
+   */
+  renderAnchor: <T extends React.HTMLAttributes<HTMLElement>>(props: T) => JSX.Element
   value?: Date | string | null
 }
 
 export const DatePicker: React.FC<DatePickerProps> = ({
+  anchorRef: externalAnchorRef,
   focusTrapSettings,
   focusZoneSettings,
-  initialValue,
-  onOpen,
-  onClose,
+  onOpen: onOpenExternal,
+  onClose: onCloseExternal,
   open,
   overlayProps,
+  renderAnchor,
   value
 }) => {
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onOpen = (gesture: 'anchor-click' | 'anchor-key-press') => {
+    setIsOpen(true)
+    onOpenExternal?.(gesture)
+  }
+
+  const onClose = (gesture: 'anchor-click' | 'click-outside' | 'escape') => {
+    setIsOpen(false)
+    onCloseExternal?.(gesture)
+  }
+
+  const toggleIsOpen = () => {
+    if (isOpen) {
+      setIsOpen(false)
+      onCloseExternal?.('anchor-click')
+    } else {
+      setIsOpen(true)
+      onCloseExternal?.('anchor-click')
+    }
+  }
+
   return (
-    <AnchoredOverlay
-      renderAnchor={renderMenuAnchor}
-      anchorRef={anchorRef}
-      open={open}
-      onOpen={onOpen}
-      onClose={onClose}
-      overlayProps={overlayProps}
-      focusTrapSettings={focusTrapSettings}
-      focusZoneSettings={focusZoneSettings}
-    >
-      {value}
-    </AnchoredOverlay>
+    <>
+      <DatePickerAnchor
+        ref={buttonRef}
+        fromDate={new Date()}
+        toDate={addDays(new Date(), 7)}
+        dateFormat="short"
+        onAction={toggleIsOpen}
+      />
+      <AnchoredOverlay
+        anchorRef={externalAnchorRef ?? buttonRef}
+        renderAnchor={renderAnchor}
+        open={open ?? isOpen}
+        onOpen={onOpen}
+        onClose={onClose}
+        overlayProps={overlayProps}
+        focusTrapSettings={focusTrapSettings}
+        focusZoneSettings={focusZoneSettings}
+      >
+        {value}
+      </AnchoredOverlay>
+    </>
   )
 }
--- src/DatePicker/DatePickerAnchor.tsx
+++ src/DatePicker/DatePickerAnchor.tsx
@@ -1,17 +1,19 @@
 import {CalendarIcon} from '@primer/octicons-react'
 import styled from 'styled-components'
 import {format} from 'date-fns'
-import React, {useMemo} from 'react'
+import React, {useCallback, useMemo} from 'react'
 import Button from '../Button'
 import Text from '../Text'
 import {get} from '../constants'
 import {StyledOcticon} from '..'
 
 export interface DatePickerAnchorProps {
-  iconOnly?: boolean
-  fromDate: Date
-  toDate?: Date
   dateFormat?: 'short' | 'long' | string
+  disabled?: boolean
+  fromDate: Date
+  iconOnly?: boolean
+  onAction?: (event?: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLDivElement>) => void
+  toDate?: Date
 }
 
 const DatePickerAnchorButton = styled(Button)`
@@ -25,36 +27,57 @@ const DatePickerAnchorButton = styled(Button)`
   }
 `
 
-export const DatePickerAnchor: React.FC<DatePickerAnchorProps> = ({
-  iconOnly = false,
-  dateFormat = 'short',
-  fromDate,
-  toDate
-}) => {
-  const formattedDate = useMemo(() => {
-    if (iconOnly) return
+export const DatePickerAnchor = React.forwardRef<HTMLButtonElement, DatePickerAnchorProps>(
+  ({dateFormat = 'short', disabled, fromDate, iconOnly = false, onAction, toDate}, ref) => {
+    const formattedDate = useMemo(() => {
+      if (iconOnly) return
 
-    if (dateFormat === 'short') {
-      return <Text>{`${format(fromDate, 'MMM d')}${toDate ? ' - ' : ''}${toDate ? format(toDate, 'MMM d') : ''}`}</Text>
-    } else if (dateFormat === 'long') {
-      return (
-        <Text>
-          {`${format(fromDate, 'MMM d, yyyy')}${toDate ? ' - ' : ''}${toDate ? format(toDate, 'MMM d, yyyy') : ''}`}
-        </Text>
-      )
-    } else {
-      return (
-        <Text>
-          {`${format(fromDate, dateFormat)}${toDate ? ' - ' : ''}${toDate ? format(toDate, dateFormat) : ''}`}
-        </Text>
-      )
-    }
-  }, [dateFormat, fromDate, iconOnly, toDate])
+      if (dateFormat === 'short') {
+        return (
+          <Text>{`${format(fromDate, 'MMM d')}${toDate ? ' - ' : ''}${toDate ? format(toDate, 'MMM d') : ''}`}</Text>
+        )
+      } else if (dateFormat === 'long') {
+        return (
+          <Text>
+            {`${format(fromDate, 'MMM d, yyyy')}${toDate ? ' - ' : ''}${toDate ? format(toDate, 'MMM d, yyyy') : ''}`}
+          </Text>
+        )
+      } else {
+        return (
+          <Text>
+            {`${format(fromDate, dateFormat)}${toDate ? ' - ' : ''}${toDate ? format(toDate, dateFormat) : ''}`}
+          </Text>
+        )
+      }
+    }, [dateFormat, fromDate, iconOnly, toDate])
 
-  return (
-    <DatePickerAnchorButton>
-      <StyledOcticon icon={CalendarIcon} color="fg.muted" sx={{my: '2px'}} />
-      {formattedDate}
-    </DatePickerAnchorButton>
-  )
-}
+    const keyPressHandler = useCallback(
+      event => {
+        if (disabled) {
+          return
+        }
+        if ([' ', 'Enter'].includes(event.key)) {
+          onAction?.(event)
+        }
+      },
+      [disabled, onAction]
+    )
+
+    const clickHandler = useCallback(
+      event => {
+        if (disabled) {
+          return
+        }
+        onAction?.(event)
+      },
+      [disabled, onAction]
+    )
+
+    return (
+      <DatePickerAnchorButton ref={ref} onClick={clickHandler} onKeyPress={keyPressHandler}>
+        <StyledOcticon icon={CalendarIcon} color="fg.muted" sx={{my: '2px'}} />
+        {formattedDate}
+      </DatePickerAnchorButton>
+    )
+  }
+)
