2454919713c404bed41b24250372dfa0344d9bd8
Cole Bemis <colebemis@github.com>
2021-10-14 14:31:21 -0700

    Bump Doctocat to v2.0.0 (#1515)
    * Bump doctocat to v2.0.0
    
    * Remove unnecessary files
    
    * Update hero to work with Doctocat v2

--- docs/src/@primer/gatsby-theme-doctocat/components/hero.js
+++ docs/src/@primer/gatsby-theme-doctocat/components/hero.js
@@ -1,4 +1,4 @@
-import {Box, Heading, Text} from '@primer/components'
+import {Box, Heading, Text, ThemeProvider} from '@primer/components'
 import React from 'react'
 import {Container} from '@primer/gatsby-theme-doctocat'
 import heroIllustration from '../primer-components-hero.svg'
@@ -6,16 +6,18 @@ import {version} from '../../../../../package.json'
 
 export default function Hero() {
   return (
-    <Box bg="black" py={6}>
-      <Container>
-        <Heading color="blue.4" fontSize={7} lineHeight="condensed" pb={3} m={0}>
-          Primer React
-        </Heading>
-        <Text as="p" fontFamily="mono" mt={0} mb={2} color="blue.3" fontSize={2}>
-          v{version}
-        </Text>
-        <img src={heroIllustration} alt="" width="100%" />
-      </Container>
-    </Box>
+    <ThemeProvider colorMode="night" nightScheme="dark_dimmed">
+      <Box bg="canvas.default" py={6}>
+        <Container>
+          <Heading color="accent.fg" fontSize={7} lineHeight="condensed" pb={3} m={0}>
+            Primer React
+          </Heading>
+          <Text as="p" fontFamily="mono" mt={0} mb={2} color="fg.default" fontSize={2}>
+            v{version}
+          </Text>
+          <img src={heroIllustration} alt="" width="100%" />
+        </Container>
+      </Box>
+    </ThemeProvider>
   )
 }
