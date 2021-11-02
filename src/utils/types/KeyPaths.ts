063cb182a3b4e119dabaf27f4d6d2bc7f22fda7e
Jonathan Fuchs <jfuchs@github.com>
2021-11-02 11:13:32 -0700

    "fixes" a tsc error. Adds ts-toolbelt

--- src/utils/types/KeyPaths.ts
+++ src/utils/types/KeyPaths.ts
@@ -1,4 +1,10 @@
 // Produces a union of dot-delimited keypaths to the string values in a nested object:
 export type KeyPaths<O extends Record<string, unknown>> = {
-  [K in keyof O]: K extends string ? (O[K] extends string ? `${K}` : `${K}.${KeyPaths<O[K]>}`) : never
+  [K in keyof O]: K extends string
+    ? O[K] extends string
+      ? `${K}`
+      : // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore TypeScript has bested me, but the KeyPaths type is tested.
+        `${K}.${KeyPaths<O[K]>}`
+    : never
 }[keyof O]
