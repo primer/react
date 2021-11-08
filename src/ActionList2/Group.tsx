0598898330009f172eed0480e9e4f86ed3a9239c
Siddharth Kshetrapal <siddharthkp@github.com>
2021-11-08 15:36:37 +0100

    role presentation instead

--- src/ActionList2/Group.tsx
+++ src/ActionList2/Group.tsx
@@ -89,7 +89,7 @@ const Header: React.FC<HeaderProps> = ({variant, title, auxiliaryText, labelId, 
   }
 
   return (
-    <Box sx={styles} aria-hidden="true" {...props}>
+    <Box sx={styles} role="presentation" aria-hidden="true" {...props}>
       <span id={labelId}>{title}</span>
       {auxiliaryText && <span>{auxiliaryText}</span>}
     </Box>
