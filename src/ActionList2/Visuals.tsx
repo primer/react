bacd12c1f2c56a8ea4595efcfaee5f1757a003b9
Siddharth Kshetrapal <siddharthkp@github.com>
2021-11-10 11:51:33 +0100

    accept html attributes on visual

--- src/ActionList2/Visuals.tsx
+++ src/ActionList2/Visuals.tsx
@@ -4,7 +4,7 @@ import {SxProp, merge} from '../sx'
 import {get} from '../constants'
 import {getVariantStyles, Slot, ItemContext, TEXT_ROW_HEIGHT} from './Item'
 
-type VisualProps = SxProp & {children: React.ReactNode}
+type VisualProps = SxProp & React.HTMLAttributes<HTMLSpanElement>
 
 export const LeadingVisualContainer: React.FC<VisualProps> = ({sx = {}, ...props}) => {
   return (
