6dba0da5c390f4fdda2a4d3994273d9bb0b7c24e
Jonathan Fuchs <jfuchs@github.com>
2021-11-16 12:07:08 -0800

    Popover no longer accepts styled system props (#1570)

---
+++ src/__tests__/Popover.types.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Popover from '../Popover'
+
+export function shouldAcceptCallWithNoProps() {
+  return <Popover />
+}
+
+export function shouldNotAcceptSystemProps() {
+  return (
+    <>
+      {/* @ts-expect-error system props should not be accepted */}
+      <Popover backgroundColor="palegreen" />
+      {/* @ts-expect-error system props should not be accepted */}
+      <Popover.Content backgroundColor="paleturquoise" />
+    </>
+  )
+}
