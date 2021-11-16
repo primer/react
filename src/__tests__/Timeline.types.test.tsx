31100b5f4378012f0115b74f862f7fd19f93247a
Jonathan Fuchs <jfuchs@github.com>
2021-11-16 12:10:02 -0800

    Timeline no longer accepts styled system props (#1576)

---
+++ src/__tests__/Timeline.types.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Timeline from '../Timeline'
+
+export function shouldAcceptCallWithNoProps() {
+  return (
+    <>
+      <Timeline />
+      <Timeline.Item />
+      <Timeline.Badge />
+      <Timeline.Body />
+      <Timeline.Break />
+    </>
+  )
+}
+
+export function shouldNotAcceptSystemProps() {
+  return (
+    <>
+      {/* @ts-expect-error system props should not be accepted */}
+      <Timeline backgroundColor="red" />
+      {/* @ts-expect-error system props should not be accepted */}
+      <Timeline.Item backgroundColor="orange" />
+      {/* @ts-expect-error system props should not be accepted */}
+      <Timeline.Badge backgroundColor="yellow" />
+      {/* @ts-expect-error system props should not be accepted */}
+      <Timeline.Body backgroundColor="green" />
+      {/* @ts-expect-error system props should not be accepted */}
+      <Timeline.Break backgroundColor="blue" />
+    </>
+  )
+}
