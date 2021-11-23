cc1c0ac2bfe401bd09f23356257c3a80e0926c5a
Jonathan Fuchs <jfuchs@github.com>
2021-11-23 13:30:44 -0800

    Fixes Overlay types to allow DOM props

--- src/__tests__/Overlay.types.test.tsx
+++ src/__tests__/Overlay.types.test.tsx
@@ -5,6 +5,21 @@ export function shouldAcceptCallWithNoProps(ref: React.RefObject<HTMLElement>) {
   return <Overlay returnFocusRef={ref} onClickOutside={() => null} onEscape={() => null} />
 }
 
+export function shouldAcceptCallWithDOMProps(ref: React.RefObject<HTMLElement>) {
+  return <Overlay returnFocusRef={ref} onClickOutside={() => null} onEscape={() => null} onMouseDown={() => null} />
+}
+
+export function shouldNotAcceptCallWithDOMPropsThatDontMatchElement(ref: React.RefObject<HTMLElement>) {
+  // @ts-expect-error href should not be allowed on a <div>
+  return <Overlay returnFocusRef={ref} onClickOutside={() => null} onEscape={() => null} href="//primer.style/" />
+}
+
+export function shouldAcceptCallWithAsAndDOMProps(ref: React.RefObject<HTMLElement>) {
+  return (
+    <Overlay as="a" returnFocusRef={ref} onClickOutside={() => null} onEscape={() => null} href="//primer.style/" />
+  )
+}
+
 export function shouldNotAcceptSystemProps(ref: React.RefObject<HTMLElement>) {
   return (
     <Overlay
---
+++ src/__tests__/SelectPanel.types.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {SelectPanel} from '../SelectPanel'
+
+export function shouldAcceptCallWithNoProps() {
+  return (
+    <SelectPanel
+      placeholderText=""
+      open={false}
+      onOpenChange={() => null}
+      items={[]}
+      selected={[]}
+      onSelectedChange={() => null}
+      onFilterChange={() => null}
+    />
+  )
+}
+
+export function shouldAcceptDOMPropsOnOverlay() {
+  return (
+    <SelectPanel
+      placeholderText=""
+      open={false}
+      onOpenChange={() => null}
+      items={[]}
+      selected={[]}
+      onSelectedChange={() => null}
+      onFilterChange={() => null}
+      overlayProps={{onMouseDown: () => null}}
+    />
+  )
+}
