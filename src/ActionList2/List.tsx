f62bcd9b18768fe1b863286a0e7ec9703f00ca76
Siddharth Kshetrapal <siddharthkp@github.com>
2021-11-30 12:44:48 +0100

    missed role in bad merge!

--- src/ActionList2/List.tsx
+++ src/ActionList2/List.tsx
@@ -44,7 +44,7 @@ export const List = React.forwardRef<HTMLUListElement, ListProps>(
     const {listRole} = React.useContext(MenuContext)
 
     return (
-      <ListBox sx={merge(styles, sxProp as SxProp)} {...props} ref={forwardedRef}>
+      <ListBox sx={merge(styles, sxProp as SxProp)} role={role || listRole} {...props} ref={forwardedRef}>
         <ListContext.Provider value={{variant, selectionVariant, showDividers}}>{props.children}</ListContext.Provider>
       </ListBox>
     )
