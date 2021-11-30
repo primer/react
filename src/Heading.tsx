3938550fb3ea3fbec542a0cadaf6aeb734fba938
Jonathan Fuchs <jfuchs@github.com>
2021-11-30 10:06:45 -0800

    Avatar, BranchName, Details, Heading, and ProgressBar no longer accept styled-system props. (#1643)

--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,21 +1,14 @@
 import styled from 'styled-components'
-import {COMMON, get, SystemCommonProps, SystemTypographyProps, TYPOGRAPHY} from './constants'
+import {get} from './constants'
 import sx, {SxProp} from './sx'
-import theme from './theme'
 import {ComponentProps} from './utils/types'
 
-const Heading = styled.h2<SystemTypographyProps & SystemCommonProps & SxProp>`
+const Heading = styled.h2<SxProp>`
   font-weight: ${get('fontWeights.bold')};
   font-size: ${get('fontSizes.5')};
   margin: 0;
-  ${TYPOGRAPHY};
-  ${COMMON};
   ${sx};
 `
-
-Heading.defaultProps = {
-  theme
-}
 
 export type HeadingProps = ComponentProps<typeof Heading>
 export default Heading
