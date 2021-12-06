2380b66848a885ee5dd2e6b07a1173201976545f
Siddharth Kshetrapal <siddharthkp@github.com>
2021-12-06 14:09:25 +0100

    ActionMenu.Overlay for ActionMenu v2 (#1675)
    * steal props from overlay :D
    
    * Alternate implementation with context
    
    * add changelog

---
+++ .changeset/actionmenu-overlay.md
@@ -0,0 +1,5 @@
+---
+'@primer/components': patch
+---
+
+ActionMenu v2: Added `ActionMenu.Overlay` which accepts props to customize the Menu overlay.
--- src/ActionList2/MenuContext.tsx
+++
@@ -1,6 +0,0 @@
-/** This context can be used by components that compose ActionList inside a Menu */
-
-import React from 'react'
-
-type ContextProps = {parent?: string; listRole?: string; itemRole?: string; afterSelect?: () => void}
-export const MenuContext = React.createContext<ContextProps>({})
