8653fd2e6396a37bcdb9202e0abbb3e415697481
Pavithra Kodmad <pksjce@github.com>
2021-12-13 14:20:32 +1100

    Add config to support profiling in our deployed storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,5 +5,22 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-performance/register'
-  ]
+  ],
+  webpackFinal: async (config, {configType}) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.resolve.push({
+      alias: {
+        ...config.module.resolve.alias,
+        'react-dom': 'react-dom/profiling',
+        'scheduler/tracing': 'scheduler/tracing-profiling'
+      }
+    })
+
+    // Return the altered config
+    return config
+  }
 }
