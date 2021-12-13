4005d6a4ea5c6d24170ddc9f7ca970fa1bbe5ea7
Pavithra Kodmad <pksjce@github.com>
2021-12-14 09:33:06 +1100

    Button: enhancements with ButtonLink and IconButton (#1659)
    * Create icon only button
    
    * Add ButtonLink and IconButton components
    
    * Lint and test
    
    * Remove react router for now. Maybe add an example later
    
    * Fix the typescript error on as prop
    
    * Fix lint issue
    
    * Fix
    1. Bug with disabled
    2. Tests in testing lib
    
    * lint issues

--- src/NewButton/index.ts
+++ src/NewButton/index.ts
@@ -1,10 +1,11 @@
 import {Button} from './button'
 import {Counter} from './button-counter'
-import {ButtonProps} from './types'
+import IconButton from './icon-button'
+import ButtonLink from './button-link'
+import {ButtonProps, IconButtonProps} from './types'
 // change this when moving to released state
-
-export type {ButtonProps as NewButtonProps}
-
+export type {ButtonProps as NewButtonProps, IconButtonProps}
 export const NewButton = Object.assign(Button, {
   Counter
 })
+export {IconButton, ButtonLink}
