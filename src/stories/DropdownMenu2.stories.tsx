55a758e18eb67043d8e692a243edaf76d9d08c38
Siddharth Kshetrapal <siddharthkp@github.com>
2021-12-15 16:33:18 +0100

    Add story for controlled menu and external anchor

--- src/stories/DropdownMenu2.stories.tsx
+++ src/stories/DropdownMenu2.stories.tsx
@@ -105,7 +105,7 @@ export function MemexIteration(): JSX.Element {
 
   return (
     <>
-      <h1>Simple DropdownMenu</h1>
+      <h1>Memex Iteration Menu</h1>
 
       <DropdownMenu>
         <DropdownMenu.Button
@@ -132,75 +132,13 @@ export function MemexIteration(): JSX.Element {
     </>
   )
 }
-MemexIteration.storyName = 'Memex Iteration'
+MemexIteration.storyName = 'Memex Iteration Menu'
 
 const milestones = [
   {name: 'v29.2.0', due: 'September 30, 2021', progress: 95},
   {name: 'v30.0.0', due: 'December 1, 2021', progress: 40},
   {name: 'FY22 - Q3', due: 'December 31, 2021', progress: 10}
 ]
-
-export function SingleSelectListStory(): JSX.Element {
-  const [selectedIndex, setSelectedIndex] = React.useState(-1)
-
-  const selectedMilestone = milestones[selectedIndex] as typeof milestones[0] | undefined
-
-  return (
-    <>
-      <h1>Single Select List</h1>
-      <Box sx={{width: 200}}>
-        <DropdownMenu>
-          <DropdownMenu.Anchor aria-label="Select a project">
-            <DropdownMenu.Anchor>
-              <Button
-                aria-label="Select iteration duration"
-                variant="invisible"
-                trailingIcon={GearIcon}
-                sx={{
-                  color: 'fg.muted',
-                  ':hover, :focus': {background: 'none !important', color: 'accent.fg'},
-                  width: '100%',
-                  textAlign: 'left',
-                  paddingX: 0
-                }}
-              >
-                Milestone
-              </Button>
-            </DropdownMenu.Anchor>
-          </DropdownMenu.Anchor>
-          <DropdownMenu.Overlay width="medium">
-            <ActionList selectionVariant="single" showDividers>
-              {milestones.map((milestone, index) => (
-                <ActionList.Item
-                  key={index}
-                  selected={index === selectedIndex}
-                  onSelect={() => setSelectedIndex(index)}
-                >
-                  <ActionList.LeadingVisual>
-                    <MilestoneIcon />
-                  </ActionList.LeadingVisual>
-                  {milestone.name}
-                  <ActionList.Description variant="block">Due by {milestone.due}</ActionList.Description>
-                </ActionList.Item>
-              ))}
-            </ActionList>
-          </DropdownMenu.Overlay>
-        </DropdownMenu>
-        {selectedMilestone ? (
-          <Text as="div" color="fg.muted" sx={{fontSize: 1, mt: 2}}>
-            <ProgressBar progress={selectedMilestone.progress} sx={{mb: 2}} />
-            {selectedMilestone.name}
-          </Text>
-        ) : (
-          <Text as="div" color="fg.muted" sx={{fontSize: 1, mt: 2}}>
-            No milestone
-          </Text>
-        )}
-      </Box>
-    </>
-  )
-}
-SingleSelectListStory.storyName = 'Single Select'
 
 export function MemexAddColumn(): JSX.Element {
   const [selectedIndex, setSelectedIndex] = React.useState(0)
@@ -260,3 +198,137 @@ export function MemexAddColumn(): JSX.Element {
   )
 }
 MemexAddColumn.storyName = 'Memex Add Column'
+
+export function MilestoneStory(): JSX.Element {
+  const [selectedIndex, setSelectedIndex] = React.useState(-1)
+
+  const selectedMilestone = milestones[selectedIndex] as typeof milestones[0] | undefined
+
+  return (
+    <>
+      <h1>Milestone selector</h1>
+      <Box sx={{width: 200}}>
+        <DropdownMenu>
+          <DropdownMenu.Anchor aria-label="Select a project">
+            <DropdownMenu.Anchor>
+              <Button
+                aria-label="Select iteration duration"
+                variant="invisible"
+                trailingIcon={GearIcon}
+                sx={{
+                  color: 'fg.muted',
+                  ':hover, :focus': {background: 'none !important', color: 'accent.fg'},
+                  width: '100%',
+                  textAlign: 'left',
+                  paddingX: 0
+                }}
+              >
+                Milestone
+              </Button>
+            </DropdownMenu.Anchor>
+          </DropdownMenu.Anchor>
+          <DropdownMenu.Overlay width="medium">
+            <ActionList selectionVariant="single" showDividers>
+              {milestones.map((milestone, index) => (
+                <ActionList.Item
+                  key={index}
+                  selected={index === selectedIndex}
+                  onSelect={() => setSelectedIndex(index)}
+                >
+                  <ActionList.LeadingVisual>
+                    <MilestoneIcon />
+                  </ActionList.LeadingVisual>
+                  {milestone.name}
+                  <ActionList.Description variant="block">Due by {milestone.due}</ActionList.Description>
+                </ActionList.Item>
+              ))}
+            </ActionList>
+          </DropdownMenu.Overlay>
+        </DropdownMenu>
+        {selectedMilestone ? (
+          <Text as="div" color="fg.muted" sx={{fontSize: 1, mt: 2}}>
+            <ProgressBar progress={selectedMilestone.progress} sx={{mb: 2}} />
+            {selectedMilestone.name}
+          </Text>
+        ) : (
+          <Text as="div" color="fg.muted" sx={{fontSize: 1, mt: 2}}>
+            No milestone
+          </Text>
+        )}
+      </Box>
+    </>
+  )
+}
+MilestoneStory.storyName = 'Milestone selector'
+
+export function ControlledMenu(): JSX.Element {
+  const [open, setOpen] = React.useState(false)
+  const [selectedIndex, setSelectedIndex] = React.useState(0)
+
+  return (
+    <>
+      <h1>Controlled Menu</h1>
+
+      <h2>External Open State: {open ? 'Open' : 'Closed'}</h2>
+      <h2>selected Value: {fieldTypes[selectedIndex].name}</h2>
+
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenu.Button onClick={() => setOpen(!open)}>{open ? 'Close Menu' : 'Open Menu'}</DropdownMenu.Button>
+        <DropdownMenu.Overlay>
+          <ActionList selectionVariant="single">
+            {fieldTypes.map((type, index) => (
+              <ActionList.Item key={index} selected={index === selectedIndex} onSelect={() => setSelectedIndex(index)}>
+                {type.name}
+              </ActionList.Item>
+            ))}
+          </ActionList>
+        </DropdownMenu.Overlay>
+      </DropdownMenu>
+    </>
+  )
+}
+ControlledMenu.storyName = 'Controlled Menu'
+
+export function ExternalAnchor(): JSX.Element {
+  const [open, setOpen] = React.useState(false)
+  const triggerRef = React.createRef<HTMLButtonElement>()
+  const anchorRef = React.createRef<HTMLDivElement>()
+
+  const [selectedIndex, setSelectedIndex] = React.useState(0)
+
+  return (
+    <>
+      <h1>External Anchor</h1>
+
+      <h2>External Open State: {open ? 'Open' : 'Closed'}</h2>
+      <h2>selected Value: {fieldTypes[selectedIndex].name}</h2>
+
+      <Button ref={triggerRef} onClick={() => setOpen(!open)}>
+        {open ? 'Close Menu' : 'Open Menu'}
+      </Button>
+
+      <Box ref={anchorRef} sx={{backgroundColor: 'papayawhip', mt: 10, p: 4}}>
+        Anchored on me!
+      </Box>
+
+      <DropdownMenu open={open} onOpenChange={setOpen} anchorRef={anchorRef}>
+        <DropdownMenu.Overlay
+          ignoreClickRefs={[
+            // Because the component is controlled from a button but anchored elsewhere,
+            // clicking the external button should not be counted as "clicking outside"
+            triggerRef
+          ]}
+        >
+          <ActionList selectionVariant="single">
+            {fieldTypes.map((type, index) => (
+              <ActionList.Item key={index} selected={index === selectedIndex} onSelect={() => setSelectedIndex(index)}>
+                {type.name}
+              </ActionList.Item>
+            ))}
+          </ActionList>
+        </DropdownMenu.Overlay>
+      </DropdownMenu>
+    </>
+  )
+}
+ExternalAnchor.storyName = 'External Anchor'
