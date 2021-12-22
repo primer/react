dc0ff1a534c87ba85d8630bd20c885c37b80a719
Siddharth Kshetrapal <siddharthkp@github.com>
2021-12-22 16:03:29 +0100

    Add aria-labelledby to ActionList inside ActionMenu

---
+++ .changeset/actionmenu2-aria-labelledby.md
@@ -0,0 +1,6 @@
+---
+'@primer/react': patch
+---
+
+AnchoredOverlay: Add support for passing an id to the anchor
+ActionMenu v2: Add aria-labelledby for ActionList
--- src/ActionList2/ActionListContainerContext.tsx
+++ src/ActionList2/ActionListContainerContext.tsx
@@ -6,6 +6,7 @@ type ContextProps = {
   container?: string
   listRole?: string
   itemRole?: string
+  listLabelledBy?: string
   // This can be any function, we don't know anything about the arguments
   // to be more specific here, this is as good as (...args: any[]) => unknown
   // eslint-disable-next-line @typescript-eslint/ban-types
