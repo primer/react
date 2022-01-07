7e6e464a766ab0951e950632927fce138d7d5d90
Siddharth Kshetrapal <siddharthkp@github.com>
2022-01-07 15:31:28 +0530

    Use Button v2 in ActionMenu v2

--- src/ActionMenu2.tsx
+++ src/ActionMenu2.tsx
@@ -1,10 +1,11 @@
-import Button, {ButtonProps} from './Button'
 import React from 'react'
+import {TriangleDownIcon} from '@primer/octicons-react'
 import {AnchoredOverlay, AnchoredOverlayProps} from './AnchoredOverlay'
 import {OverlayProps} from './Overlay'
 import {useProvidedRefOrCreate, useProvidedStateOrCreate} from './hooks'
 import {Divider} from './ActionList2/Divider'
 import {ActionListContainerContext} from './ActionList2/ActionListContainerContext'
+import {Button, ButtonProps} from './Button2'
 import {MandateProps} from './utils/types'
 
 type MenuContextProps = Pick<AnchoredOverlayProps, 'anchorRef' | 'renderAnchor' | 'open' | 'onOpen' | 'onClose'>
@@ -70,7 +71,7 @@ export type MenuButtonProps = ButtonProps
 const MenuButton = React.forwardRef<AnchoredOverlayProps['anchorRef'], ButtonProps>((props, anchorRef) => {
   return (
     <Anchor ref={anchorRef}>
-      <Button {...props} />
+      <Button trailingIcon={TriangleDownIcon} type="button" {...props} />
     </Anchor>
   )
 })
