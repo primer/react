90a1f14c6097cbb880712c876d2fffaf1ff22079
Cole Bemis <colebemis@github.com>
2022-01-24 12:43:58 -0800

    Scaffold pagelayout component

---
+++ src/PageLayout/PageLayout.stories.tsx
@@ -0,0 +1,12 @@
+import {Meta} from '@storybook/react'
+import React from 'react'
+import {PageLayout} from './PageLayout'
+
+const meta: Meta = {
+  title: 'Layout/PageLayout',
+  component: PageLayout
+}
+
+export const Default = () => <PageLayout />
+
+export default meta
---
+++ src/PageLayout/PageLayout.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function PageLayout() {
+  return <div>PageLayout</div>
+}
