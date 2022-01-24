89cf8327f1c19930d8c4192efb819bd68ce64344
Cole Bemis <colebemis@github.com>
2022-01-24 13:36:55 -0800

    Create subcomponents

--- src/PageLayout/PageLayout.stories.tsx
+++ src/PageLayout/PageLayout.stories.tsx
@@ -1,12 +1,54 @@
 import {Meta} from '@storybook/react'
 import React from 'react'
+import {Box} from '..'
 import {PageLayout} from './PageLayout'
 
 const meta: Meta = {
   title: 'Layout/PageLayout',
-  component: PageLayout
+  component: PageLayout,
+  parameters: {
+    layout: 'fullscreen'
+  }
 }
 
-export const Default = () => <PageLayout />
+// This might be useful in other stories/docs
+const Placeholder: React.FC<{
+  width?: number | string
+  height: number | string
+  label?: string
+}> = ({width, height, label}) => {
+  return (
+    <Box
+      sx={{
+        width: width ?? '100%',
+        height,
+        display: 'grid',
+        placeItems: 'center',
+        bg: 'canvas.inset',
+        border: '1px solid',
+        borderColor: 'border.default',
+        borderRadius: 2
+      }}
+    >
+      {label}
+    </Box>
+  )
+}
+export const Default = () => (
+  <PageLayout>
+    <PageLayout.Header>
+      <Placeholder height={64} label="Header" />
+    </PageLayout.Header>
+    <PageLayout.Content>
+      <Placeholder height={400} label="Content" />
+    </PageLayout.Content>
+    <PageLayout.Pane>
+      <Placeholder width={400} height={200} label="Pane" />
+    </PageLayout.Pane>
+    <PageLayout.Footer>
+      <Placeholder height={64} label="Footer" />
+    </PageLayout.Footer>
+  </PageLayout>
+)
 
 export default meta
--- src/PageLayout/PageLayout.tsx
+++ src/PageLayout/PageLayout.tsx
@@ -1,5 +1,61 @@
 import React from 'react'
+import {Box} from '..'
 
-export function PageLayout() {
-  return <div>PageLayout</div>
+// TODO: refs
+const Root: React.FC = ({children}) => {
+  return (
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateAreas: `"header header" "content pane" "footer footer"`,
+        gridTemplateColumns: '1fr auto',
+        padding: 4,
+        rowGap: 4,
+        columnGap: 4
+      }}
+    >
+      {children}
+    </Box>
+  )
 }
+
+Root.displayName = 'PageLayout'
+
+// PageLayout.Header
+
+const Header: React.FC = ({children}) => {
+  return <Box sx={{gridArea: 'header'}}>{children}</Box>
+}
+
+Header.displayName = 'PageLayout.Header'
+
+// PageLayout.Content
+
+const Content: React.FC = ({children}) => {
+  return <Box sx={{gridArea: 'content'}}>{children}</Box>
+}
+
+Content.displayName = 'PageLayout.Content'
+
+// PageLayout.Pane
+
+const Pane: React.FC = ({children}) => {
+  return <Box sx={{gridArea: 'pane'}}>{children}</Box>
+}
+
+Pane.displayName = 'PageLayout.Pane'
+
+// PageLayout.Footer
+
+const Footer: React.FC = ({children}) => {
+  return <Box sx={{gridArea: 'footer'}}>{children}</Box>
+}
+
+Footer.displayName = 'PageLayout.Footer'
+
+export const PageLayout = Object.assign(Root, {
+  Header,
+  Content,
+  Pane,
+  Footer
+})
