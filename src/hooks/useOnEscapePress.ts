720b77a66d4e114d8eaee3adb0c7dd4047b8da74
Siddharth Kshetrapal <siddharthkp@github.com>
2022-01-26 14:45:08 +0100

    handler can be removed by now

--- src/hooks/useOnEscapePress.ts
+++ src/hooks/useOnEscapePress.ts
@@ -9,7 +9,7 @@ const handlers: ((e: KeyboardEvent) => void)[] = []
 function handleEscape(event: KeyboardEvent) {
   if (event.key === 'Escape' && !event.defaultPrevented) {
     for (let i = handlers.length - 1; i >= 0; --i) {
-      handlers[i](event)
+      if (typeof handlers[i] === 'function') handlers[i](event)
       // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
       if (event.defaultPrevented) {
         break
