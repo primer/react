216178c00045e2fef1c1937ef67133be338d44cd
Mike Perrotti <mperrotti@github.com>
2022-02-02 18:07:08 -0500

    adds FormControl component and stories

---
+++ src/FormControl/_FormControlLeadingVisual.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Slot} from './slots'
+
+const FormControlLeadingVisual: React.FC = ({children}) => <Slot name="LeadingVisual">{children}</Slot>
+
+export default FormControlLeadingVisual
