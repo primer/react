60ca8f1d2b245f37e9df98db85754e3a7e1162ac
Mike Perrotti <mperrotti@github.com>
2022-02-17 11:13:45 -0500

    addresses PR feedback

--- src/ChoiceGroup/ChoiceGroup.tsx
+++ src/ChoiceGroup/ChoiceGroup.tsx
@@ -135,9 +135,9 @@ const ChoiceGroup: React.FC<ChoiceGroupProps> = ({
                   <Box as="legend" mb={isLegendVisible ? 2 : undefined} padding={0}>
                     {slots.Label}
                     {slots.Caption}
-                    <VisuallyHidden>
-                      {React.isValidElement(slots.Validation) && slots.Validation.props.children}
-                    </VisuallyHidden>
+                    {React.isValidElement(slots.Validation) && slots.Validation.props.children && (
+                      <VisuallyHidden>{slots.Validation.props.children}</VisuallyHidden>
+                    )}
                   </Box>
                 ) : (
                   /*
