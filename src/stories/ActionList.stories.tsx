9ae5917322e9bd6cb0f21952a67e4f1103d021a5
Siddharth Kshetrapal <siddharthkp@github.com>
2022-02-25 18:32:17 +0100

    Revert "Deprecate ActionMenu v1 & Promote drafts/ActionMenu v2  (#1895)"
    This reverts commit f366604526d13e21f5bcbdf508842bb52e0fbf76.

--- src/stories/ActionList.stories.tsx
+++ src/stories/ActionList.stories.tsx
@@ -14,18 +14,18 @@ import {
 import {Meta} from '@storybook/react'
 import React, {forwardRef} from 'react'
 import styled from 'styled-components'
-import {Label, ThemeProvider} from '../..'
-import {ActionList as _ActionList} from '../../deprecated/ActionList'
-import {Header} from '../../deprecated/ActionList/Header'
-import BaseStyles from '../../BaseStyles'
-import sx from '../../sx'
+import {Label, ThemeProvider} from '..'
+import {ActionList as _ActionList} from '../ActionList'
+import {Header} from '../ActionList/Header'
+import BaseStyles from '../BaseStyles'
+import sx from '../sx'
 
 const ActionList = Object.assign(_ActionList, {
   Header
 })
 
 const meta: Meta = {
-  title: 'Deprecated components/ActionList',
+  title: 'Composite components/ActionList',
   component: ActionList,
   decorators: [
     (Story: React.ComponentType): JSX.Element => (
