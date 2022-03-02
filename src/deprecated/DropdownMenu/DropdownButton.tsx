39d9f00f360ee32a325508efda42edbf78a55438
Reza Rahman <rezrah@github.com>
2022-03-02 12:11:39 +0000

    chore: ad-hoc fixes for next-major branch

--- src/deprecated/DropdownMenu/DropdownButton.tsx
+++ src/deprecated/DropdownMenu/DropdownButton.tsx
@@ -5,6 +5,9 @@ import StyledOcticon from '../../StyledOcticon'
 
 export type DropdownButtonProps = ButtonProps
 
+/**
+ * @deprecated Use Button with Octicons instead. See https://primer.style/react/drafts/Button2#appending-an-icon for more details.
+ */
 export const DropdownButton = React.forwardRef<HTMLElement, React.PropsWithChildren<DropdownButtonProps>>(
   ({children, ...props}: React.PropsWithChildren<DropdownButtonProps>, ref): JSX.Element => (
     <Button ref={ref} type="button" {...props}>
