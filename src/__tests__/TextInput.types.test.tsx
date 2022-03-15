42d96ce0441651da98a05b91233c16fc0151128b
Cole Bemis <colebemis@github.com>
2022-03-15 14:36:45 -0700

    Add another TextInput type test

--- src/__tests__/TextInput.types.test.tsx
+++ src/__tests__/TextInput.types.test.tsx
@@ -5,3 +5,8 @@ export function shouldNotAcceptInvalidDomProps() {
   // @ts-expect-error invalid DOM props should not be accepted
   return <TextInput onKeyDown={true} />
 }
+
+export function shouldNotAcceptInvalidSize() {
+  // @ts-expect-error invalid size value should not be accepted
+  return <TextInput size="big" />
+}
