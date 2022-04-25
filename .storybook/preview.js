967e961e8eaf8b45ce761bd5271fb7d82c95fb1a
Siddharth Kshetrapal <siddharthkp@github.com>
2022-04-25 15:51:51 +0200

    use special preview.js for primer/react

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,19 @@
-export * from '@primer/react-scripts/storybook/preview'
+/**
+ * primer/react uses a special preview.js which is a subset of the recommended preview.js.
+ * We do this so that we can use local withThemeProvider instead of node_modules
+ */
 
+// export default config
+export * from '@primer/react-scripts/storybook/preview-primer-react'
+
+import {withThemeProvider, toolbarTypes} from '../src/utils/story-helpers'
+import {decorators} from '@primer/react-scripts/storybook/preview-primer-react'
+
+export const globalTypes = toolbarTypes
+decorators.push(withThemeProvider)
+export {decorators}
+
+// additional parameters for addons
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
 
