853271ecf8792641d7d750c478a333cccce5eb9e
Siddharth Kshetrapal <siddharthkp@github.com>
2022-04-25 14:31:13 +0200

    update to latest primer-scripts

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,9 @@ const defaultConfig = require('@primer/react-scripts/storybook/main')
 
 module.exports = {
   ...defaultConfig,
-
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     ...defaultConfig.addons,
-
     ...(process.env.NODE_ENV === 'production' && process.env.GITHUB_JOB !== 'chromatic'
       ? ['@whitespace/storybook-addon-html']
       : [])
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,9 @@
-import {addons} from '@storybook/addons'
-
 export * from '@primer/react-scripts/storybook/preview'
-
-addons.setConfig({
-  // Some stories may set up keyboard event handlers, which can be interfered
-  // with by these keyboard shortcuts.
-  enableShortcuts: false
-})
 
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
-  // @whitespace/storybook-addon-html
+
+  // for @whitespace/storybook-addon-html,
   html: {
     root: '#html-addon-root',
     removeEmptyComments: true
