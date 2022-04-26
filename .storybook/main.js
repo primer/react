dd603d17a152a7573b33bd3f5bf750f2799beae4
Siddharth Kshetrapal <siddharthkp@github.com>
2022-04-26 14:59:19 +0200

    use webpack aliases to use local version of primer/react

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const defaultConfig = require('@primer/react-scripts/storybook/main')
 
 module.exports = {
@@ -9,6 +10,11 @@ module.exports = {
       ? ['@whitespace/storybook-addon-html']
       : [])
   ],
+  webpackFinal: config => {
+    // we alias node_modules/@primer/react for storybook to use local version instead
+    config.resolve.alias['@primer/react/lib-esm'] = path.resolve(__dirname, '../src')
+    return config
+  },
   babel: options => {
     options.plugins.push(['open-source', {editor: process.env.NODE_ENV === 'production' ? 'github' : 'vscode'}])
     return options
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,4 @@
-/**
- * primer/react uses a special preview.js which is a subset of the recommended preview.js.
- * We do this so that we can use local withThemeProvider instead of node_modules
- */
-
-// export default config
-export * from '@primer/react-scripts/storybook/preview-primer-react'
-
-import {withThemeProvider, toolbarTypes} from '../src/utils/story-helpers'
-import {decorators} from '@primer/react-scripts/storybook/preview-primer-react'
-
-export const globalTypes = toolbarTypes
-decorators.push(withThemeProvider)
-export {decorators}
+export * from '@primer/react-scripts/storybook/preview'
 
 // additional parameters for addons
 export const parameters = {
