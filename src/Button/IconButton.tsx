98fdc4a2a2646a491cf25c24f10a8cb52b2ae59f
Siddharth Kshetrapal <siddharthkp@github.com>
2022-04-28 18:01:21 +0200

    Use Tooltip in IconButton

--- src/Button/IconButton.tsx
+++ src/Button/IconButton.tsx
@@ -4,23 +4,46 @@ import {useTheme} from '../ThemeProvider'
 import Box from '../Box'
 import {IconButtonProps, StyledButton} from './types'
 import {getBaseStyles, getSizeStyles, getVariantStyles} from './styles'
+import {Tooltip, TooltipContext, TooltipProps} from '../Tooltip'
 
 const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, forwardedRef): JSX.Element => {
-  const {variant = 'default', size = 'medium', sx: sxProp = {}, icon: Icon, ...rest} = props
+  const {variant = 'default', size = 'medium', sx: sxProp = {}, icon: Icon, disableTooltip = false, ...rest} = props
   const {theme} = useTheme()
+
   const sxStyles = merge.all([
     getBaseStyles(theme),
     getSizeStyles(size, variant, true),
     getVariantStyles(variant, theme),
     sxProp as SxProp
   ])
+
   return (
-    <StyledButton sx={sxStyles} {...rest} ref={forwardedRef}>
-      <Box as="span" sx={{display: 'inline-block'}}>
-        <Icon />
-      </Box>
-    </StyledButton>
+    <ConditionalTooltip text={props['aria-label']} disableTooltip={disableTooltip}>
+      <StyledButton sx={sxStyles} {...rest} ref={forwardedRef}>
+        <Box as="span" sx={{display: 'inline-block'}}>
+          <Icon />
+        </Box>
+      </StyledButton>
+    </ConditionalTooltip>
   )
 })
 
 export {IconButton}
+
+const ConditionalTooltip: React.FC<TooltipProps & Pick<IconButtonProps, 'disableTooltip'>> = ({
+  text,
+  disableTooltip,
+  children
+}) => {
+  // If button is already wrapped in a Tooltip,
+  // do not add another.
+  const {tooltipId} = React.useContext(TooltipContext)
+
+  if (tooltipId || disableTooltip) return children
+
+  return (
+    <Tooltip text={text} type="label">
+      {children}
+    </Tooltip>
+  )
+}
--- src/Button/types.ts
+++ src/Button/types.ts
@@ -44,6 +44,8 @@ export type ButtonProps = {
 
 export type IconButtonProps = ButtonA11yProps & {
   icon: React.FunctionComponent<IconProps>
+  'aria-label': string
+  disableTooltip?: boolean
 } & ButtonBaseProps
 
 // adopted from React.AnchorHTMLAttributes
