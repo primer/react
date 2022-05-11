73c3d4a1c4eee7c67455b8d515b0176930eb35f9
Cole Bemis <colebemis@github.com>
2022-05-10 18:23:31 -0700

    wip subnav

---
+++ src/NavList/NavList.stories.tsx
@@ -0,0 +1,35 @@
+import {Meta, Story} from '@storybook/react'
+import React from 'react'
+import {PageLayout} from '../PageLayout'
+import {NavList} from './NavList'
+
+const meta: Meta = {
+  title: 'Composite components/NavList',
+  component: NavList,
+  parameters: {
+    layout: 'fullscreen'
+  }
+}
+
+export const SubItems: Story = () => (
+  <PageLayout>
+    <PageLayout.Pane position="start">
+      <NavList>
+        <NavList.Item href="#">Item 1</NavList.Item>
+        <NavList.Item>
+          Item 2
+          <NavList.SubNav>
+            <NavList.Item href="#" aria-current="page">
+              Sub item 1
+            </NavList.Item>
+            <NavList.Item href="#">Sub item 2</NavList.Item>
+          </NavList.SubNav>
+        </NavList.Item>
+        <NavList.Item href="#">Item 3</NavList.Item>
+      </NavList>
+    </PageLayout.Pane>
+    <PageLayout.Content></PageLayout.Content>
+  </PageLayout>
+)
+
+export default meta
--- src/NavList/NavList.tsx
+++ src/NavList/NavList.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import {ChevronDownIcon} from '@primer/octicons-react'
+import {useSSRSafeId} from '@react-aria/ssr'
+import React, {isValidElement} from 'react'
 import {ActionList} from '../ActionList'
+import Box from '../Box'
+import StyledOcticon from '../StyledOcticon'
 
 // ----------------------------------------------------------------------------
 // NavList
@@ -29,8 +33,32 @@ export type NavListItemProps = {
   // sx
 }
 
+const ItemContext = React.createContext<{depth: number}>({depth: 0})
+
 const Item = React.forwardRef<HTMLAnchorElement, NavListItemProps>(
   ({href, 'aria-current': ariaCurrent, children}, ref) => {
+    const {depth} = React.useContext(ItemContext)
+
+    // TODO: Test two-level nav list
+    if (depth > 1) {
+      // eslint-disable-next-line no-console
+      console.error('NavList only supports one level of nesting')
+      return null
+    }
+
+    // Get SubNav from children
+    const subNav = React.Children.toArray(children).find(child => isValidElement(child) && child.type === SubNav)
+
+    // Get children without SubNav
+    const childrenWithoutSubNav = React.Children.toArray(children).filter(child =>
+      isValidElement(child) ? child.type !== SubNav : true
+    )
+
+    // Render ItemWithSubNav if SubNav is present
+    if (subNav) {
+      return <ItemWithSubNav subNav={subNav}>{childrenWithoutSubNav}</ItemWithSubNav>
+    }
+
     return (
       <ActionList.LinkItem
         ref={ref}
@@ -38,8 +66,10 @@ const Item = React.forwardRef<HTMLAnchorElement, NavListItemProps>(
         aria-current={ariaCurrent}
         sx={{
           position: 'relative',
+          paddingLeft: depth === 1 ? 5 : null, // Indent sub-items
+          fontSize: depth === 1 ? 0 : null, // Reduce font size of sub-items
           '&[aria-current]': {
-            fontWeight: 'bold',
+            fontWeight: depth === 0 ? 'bold' : null, // Sub-items don't get bolded
             bg: 'actionListItem.default.selectedBg',
             '&::after': {
               position: 'absolute',
@@ -59,6 +89,86 @@ const Item = React.forwardRef<HTMLAnchorElement, NavListItemProps>(
     )
   }
 )
+
+Item.displayName = 'NavList.Item'
+
+// ----------------------------------------------------------------------------
+// ItemWithSubNav (internal)
+
+type ItemWithSubNavProps = {
+  children: React.ReactNode
+  subNav: React.ReactNode
+  // sx
+}
+
+const ItemWithSubNavContext = React.createContext<{buttonId: string; subNavId: string}>({buttonId: '', subNavId: ''})
+
+// TODO: Forward ref
+function ItemWithSubNav({children, subNav}: ItemWithSubNavProps) {
+  const buttonId = useSSRSafeId()
+  const subNavId = useSSRSafeId()
+  const subNavRef = React.useRef<HTMLDivElement>(null)
+  const [isOpen, setIsOpen] = React.useState(false)
+  // TODO: Check if subNav contains aria-current
+  // TODO: Animation
+  return (
+    <ItemWithSubNavContext.Provider value={{buttonId, subNavId}}>
+      <Box as="li" aria-labelledby={buttonId} sx={{listStyle: 'none'}}>
+        {/* TODO: parent of aria-current should be bold, and have active styles if closed */}
+        <ActionList.Item
+          role="button"
+          id={buttonId}
+          aria-expanded={isOpen}
+          aria-controls={subNavId}
+          onSelect={() => setIsOpen(open => !open)}
+        >
+          {children}
+          {/* What happens if the user provides a TrailingVisual? */}
+          <ActionList.TrailingVisual>
+            <StyledOcticon
+              icon={ChevronDownIcon}
+              sx={{
+                transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)'
+              }}
+            />
+          </ActionList.TrailingVisual>
+        </ActionList.Item>
+
+        {isOpen ? <div ref={subNavRef}>{subNav}</div> : null}
+      </Box>
+    </ItemWithSubNavContext.Provider>
+  )
+}
+
+// ----------------------------------------------------------------------------
+// NavList.SubNav
+
+type NavListSubNavProps = {
+  children: React.ReactNode
+  // sx
+}
+
+// SubNav must be a direct child of an Item
+// TODO: Forward ref
+const SubNav = ({children}: NavListSubNavProps) => {
+  const {buttonId, subNavId} = React.useContext(ItemWithSubNavContext)
+  const {depth} = React.useContext(ItemContext)
+
+  if (!buttonId || !subNavId) {
+    // eslint-disable-next-line no-console
+    console.error('NavList.SubNav must be a child of a NavList.Item')
+  }
+
+  return (
+    <ItemContext.Provider value={{depth: depth + 1}}>
+      <Box as="ul" id={subNavId} aria-labelledby={buttonId} sx={{padding: 0, margin: 0}}>
+        {children}
+      </Box>
+    </ItemContext.Provider>
+  )
+}
+
+SubNav.displayName = 'NavList.SubNav'
 
 // ----------------------------------------------------------------------------
 // NavList.LeadingVisual
@@ -108,6 +218,7 @@ Group.displayName = 'NavList.Group'
 
 export const NavList = Object.assign(Root, {
   Item,
+  SubNav,
   LeadingVisual,
   TrailingVisual,
   Divider,
