4fa6631e77adabfc0c4eed9a131db8b2a784caca
Siddharth Kshetrapal <siddharthkp@github.com>
2022-05-24 17:55:21 +0200

    test: optimizationLevel: 2

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,9 +5,8 @@ module.exports = {
     '@storybook/addon-links',
     {name: '@storybook/addon-essentials', options: {backgrounds: false}},
     'storybook-addon-performance/register',
-    ...(process.env.NODE_ENV === 'production' && process.env.GITHUB_JOB !== 'chromatic'
-      ? ['@whitespace/storybook-addon-html', 'storybook-addon-turbo-build']
-      : [])
+    {name: 'storybook-addon-turbo-build', options: {optimizationLevel: 2}},
+    '@whitespace/storybook-addon-html'
   ],
   babel: options => {
     options.plugins.push(['open-source', {editor: process.env.NODE_ENV === 'production' ? 'github' : 'vscode'}])
