fb68ddc35b7e870cf78fa45a38d1698ebb35acf1
Siddharth Kshetrapal <siddharthkp@github.com>
2022-05-25 17:37:34 +0200

    Revert "Merge branch 'siddharthkp-patch-2' into siddharth/actionmenu-inline-label"
    This reverts commit c8058239ee0be55500c7145ae639723e2f58dc77, reversing
    changes made to 74bafd6ed8694f667f7e17b140c3bca67bd94f0e.

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
     '@storybook/addon-links',
     {name: '@storybook/addon-essentials', options: {backgrounds: false}},
     'storybook-addon-performance/register',
-    {name: 'storybook-addon-turbo-build', options: {optimizationLevel: 2}},
     ...(process.env.NODE_ENV === 'production' && process.env.GITHUB_JOB !== 'chromatic'
       ? ['@whitespace/storybook-addon-html']
       : [])
