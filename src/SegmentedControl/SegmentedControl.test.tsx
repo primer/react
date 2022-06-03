7b64cf9169c8b872f14425e1ac35663687b5219c
Siddharth Kshetrapal <siddharthkp@github.com>
2022-06-03 15:21:56 +0200

    import SegmentedControl in test?

--- src/SegmentedControl/SegmentedControl.test.tsx
+++ src/SegmentedControl/SegmentedControl.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 
+import SegmentedControl from './'
+
 describe('SegmentedControl', () => {
   it('passes if the tests are fake', () => {
     expect(true).toBe(true)
