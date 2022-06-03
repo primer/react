fa012e21d7fc2f9c4d799e13f6f9634107674f28
Siddharth Kshetrapal <siddharthkp@github.com>
2022-06-03 15:26:41 +0200

    behaves as component?

--- src/SegmentedControl/SegmentedControl.test.tsx
+++ src/SegmentedControl/SegmentedControl.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-
+import {behavesAsComponent} from '../utils/testing'
 import SegmentedControl from './'
 
+// TODO: improve test coverage
 describe('SegmentedControl', () => {
-  it('passes if the tests are fake', () => {
-    expect(true).toBe(true)
+  behavesAsComponent({
+    Component: SegmentedControl,
+    toRender: () => (
+      <SegmentedControl aria-label="File view">
+        <SegmentedControl.Button selected>Preview</SegmentedControl.Button>
+        <SegmentedControl.Button>Raw</SegmentedControl.Button>
+        <SegmentedControl.Button>Blame</SegmentedControl.Button>
+      </SegmentedControl>
+    )
   })
 })
