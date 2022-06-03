65accfd9073aeaa83c7f2677bcb2e3ee8ba226cb
Siddharth Kshetrapal <siddharthkp@github.com>
2022-06-03 15:31:40 +0200

    test instead of behavesAsComponent?

--- src/SegmentedControl/SegmentedControl.test.tsx
+++ src/SegmentedControl/SegmentedControl.test.tsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import {behavesAsComponent} from '../utils/testing'
+import {render} from '@testing-library/react'
+import {EyeIcon, FileCodeIcon, PeopleIcon} from '@primer/octicons-react'
+// import userEvent from '@testing-library/user-event'
+// import {behavesAsComponent, checkExports, checkStoriesForAxeViolations} from '../utils/testing'
 import SegmentedControl from './'
+
+const segmentData = [
+  {label: 'Preview', iconLabel: 'EyeIcon', icon: () => <EyeIcon aria-label="EyeIcon" />},
+  {label: 'Raw', iconLabel: 'FileCodeIcon', icon: () => <FileCodeIcon aria-label="FileCodeIcon" />},
+  {label: 'Blame', iconLabel: 'PeopleIcon', icon: () => <PeopleIcon aria-label="PeopleIcon" />}
+]
 
 // TODO: improve test coverage
 describe('SegmentedControl', () => {
-  behavesAsComponent({
-    Component: SegmentedControl,
-    toRender: () => (
+  // behavesAsComponent({
+  //   Component: SegmentedControl,
+  //   toRender: () => (
+  //     <SegmentedControl aria-label="File view">
+  //       <SegmentedControl.Button selected>Preview</SegmentedControl.Button>
+  //       <SegmentedControl.Button>Raw</SegmentedControl.Button>
+  //       <SegmentedControl.Button>Blame</SegmentedControl.Button>
+  //     </SegmentedControl>
+  //   )
+  // })
+
+  it('renders with a selected segment', () => {
+    const {getByText} = render(
       <SegmentedControl aria-label="File view">
-        <SegmentedControl.Button selected>Preview</SegmentedControl.Button>
-        <SegmentedControl.Button>Raw</SegmentedControl.Button>
-        <SegmentedControl.Button>Blame</SegmentedControl.Button>
+        {segmentData.map(({label}, index) => (
+          <SegmentedControl.Button selected={index === 1} key={label}>
+            {label}
+          </SegmentedControl.Button>
+        ))}
       </SegmentedControl>
     )
+    const selectedButton = getByText('Raw').closest('button')
+    expect(selectedButton?.getAttribute('aria-pressed')).toBe('true')
   })
 })
