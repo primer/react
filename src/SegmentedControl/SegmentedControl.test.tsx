6f5b3052ed1e501048a5d35918269eebbc4cebab
Siddharth Kshetrapal <siddharthkp@github.com>
2022-06-03 15:13:32 +0200

    bring back behavesAsComponent + checkExports

--- src/SegmentedControl/SegmentedControl.test.tsx
+++ src/SegmentedControl/SegmentedControl.test.tsx
@@ -3,12 +3,8 @@ import '@testing-library/jest-dom/extend-expect'
 import {render} from '@testing-library/react'
 // import {EyeIcon, FileCodeIcon, PeopleIcon} from '@primer/octicons-react'
 import userEvent from '@testing-library/user-event'
-import {
-  // behavesAsComponent,
-  //  checkExports,
-  checkStoriesForAxeViolations
-} from '../utils/testing'
-// import SegmentedControl from './'
+import {behavesAsComponent, checkExports, checkStoriesForAxeViolations} from '../utils/testing'
+import SegmentedControl from './'
 
 // const segmentData = [
 //   {label: 'Preview', iconLabel: 'EyeIcon', icon: () => <EyeIcon aria-label="EyeIcon" />},
@@ -18,19 +14,19 @@ import {
 
 // TODO: improve test coverage
 describe('SegmentedControl', () => {
-  // behavesAsComponent({
-  //   Component: SegmentedControl,
-  //   toRender: () => (
-  //     <SegmentedControl aria-label="File view">
-  //       <SegmentedControl.Button selected>Preview</SegmentedControl.Button>
-  //       <SegmentedControl.Button>Raw</SegmentedControl.Button>
-  //       <SegmentedControl.Button>Blame</SegmentedControl.Button>
-  //     </SegmentedControl>
-  //   )
-  // })
-  // checkExports('SegmentedControl', {
-  //   default: SegmentedControl
-  // })
+  behavesAsComponent({
+    Component: SegmentedControl,
+    toRender: () => (
+      <SegmentedControl aria-label="File view">
+        <SegmentedControl.Button selected>Preview</SegmentedControl.Button>
+        <SegmentedControl.Button>Raw</SegmentedControl.Button>
+        <SegmentedControl.Button>Blame</SegmentedControl.Button>
+      </SegmentedControl>
+    )
+  })
+  checkExports('SegmentedControl', {
+    default: SegmentedControl
+  })
   // it('renders with a selected segment', () => {
   //   const {getByText} = render(
   //     <SegmentedControl aria-label="File view">
