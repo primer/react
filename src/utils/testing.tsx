306c3b214118abeb7b8ed882b105cc2f9b0b2bd4
Siddharth Kshetrapal <siddharthkp@github.com>
2022-06-03 15:02:09 +0200

    bring utils/testing changes

--- src/utils/testing.tsx
+++ src/utils/testing.tsx
@@ -245,9 +245,9 @@ export function checkExports(path: string, exports: Record<any, any>): void {
 }
 
 expect.extend(toHaveNoViolations)
-export function checkStoriesForAxeViolations(name: string) {
+export function checkStoriesForAxeViolations(name: string, storyDir?: string) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const stories = require(`../stories/${name}.stories`)
+  const stories = require(`${storyDir || '../stories/'}${name}.stories`)
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars -- _meta
   const {default: _meta, ...Stories} = stories
