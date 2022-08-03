5098829b0fb2c9ac58be7e82998a78a1bce39763
Ian Sanders <iansan5653@github.com>
2022-08-03 20:37:53 +0000

    Fix `useIgnoreKeyboardActionsWhileComposing` tests

--- src/drafts/hooks/useIgnoreKeyboardActionsWhileComposing.test.tsx
+++ src/drafts/hooks/useIgnoreKeyboardActionsWhileComposing.test.tsx
@@ -14,18 +14,20 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
   })
 
   it('should allow typing when no composition occurs', async () => {
+    const user = userEvent.setup()
     const onKeyDown = jest.fn()
     const {result} = renderHook(() => useIgnoreKeyboardActionsWhileComposing(onKeyDown))
 
     render(<textarea {...result.current} />)
 
     const textarea = screen.getByRole('textbox')
-    userEvent.type(textarea, 'ime composition')
+    await user.type(textarea, 'ime composition')
 
     expect(screen.getByRole('textbox')).toHaveValue('ime composition')
   })
 
   it('should allow typing when composition occurs', async () => {
+    const user = userEvent.setup()
     const onKeyDown = jest.fn()
     const {result} = renderHook(() => useIgnoreKeyboardActionsWhileComposing(onKeyDown))
 
@@ -33,15 +35,16 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
 
     const textarea = screen.getByRole('textbox')
     fireEvent.compositionStart(textarea)
-    userEvent.type(textarea, 'ime composition')
+    await user.type(textarea, 'ime composition')
     fireEvent.compositionEnd(textarea)
 
     expect(screen.getByRole('textbox')).toHaveValue('ime composition')
   })
 
   it('should ignore unprintable `229` keydown event typing after composition end on macOS', async () => {
+    const user = userEvent.setup()
     const isMacOSMock = isMacOS as jest.Mock
-    isMacOSMock.mockReturnValue(false)
+    isMacOSMock.mockReturnValue(true)
 
     const onKeyDown = jest.fn()
     const {result} = renderHook(() => useIgnoreKeyboardActionsWhileComposing(onKeyDown))
@@ -50,7 +53,7 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
 
     const textarea = screen.getByRole('textbox')
     fireEvent.compositionStart(textarea)
-    userEvent.type(textarea, 'ime composition')
+    await user.type(textarea, 'ime composition')
     fireEvent.compositionEnd(textarea)
     onKeyDown.mockReset()
     fireEvent.keyDown(textarea, {keyCode: 229})
@@ -60,6 +63,7 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
   })
 
   it('should not ignore unprintable `229` keydown event typing after composition ends not on macOS', async () => {
+    const user = userEvent.setup()
     const onKeyDown = jest.fn()
     const {result} = renderHook(() => useIgnoreKeyboardActionsWhileComposing(onKeyDown))
 
@@ -67,7 +71,7 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
 
     const textarea = screen.getByRole('textbox')
     fireEvent.compositionStart(textarea)
-    userEvent.type(textarea, 'ime composition')
+    await user.type(textarea, 'ime composition')
     fireEvent.compositionEnd(textarea)
     onKeyDown.mockReset()
     fireEvent.keyDown(textarea, {keyCode: 229})
@@ -77,6 +81,7 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
   })
 
   it('should not ignore unprintable `229` keydown event typing before composition ends not on macOS', async () => {
+    const user = userEvent.setup()
     const onKeyDown = jest.fn()
     const {result} = renderHook(() => useIgnoreKeyboardActionsWhileComposing(onKeyDown))
 
@@ -84,7 +89,7 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
 
     const textarea = screen.getByRole('textbox')
     fireEvent.compositionStart(textarea)
-    userEvent.type(textarea, 'ime composition')
+    await user.type(textarea, 'ime composition')
     onKeyDown.mockReset()
     fireEvent.keyDown(textarea, {keyCode: 229})
     fireEvent.compositionEnd(textarea)
@@ -94,6 +99,7 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
   })
 
   it('should ignore Enter keydown event before composition ends', async () => {
+    const user = userEvent.setup()
     const onKeyDown = jest.fn()
     const {result} = renderHook(() => useIgnoreKeyboardActionsWhileComposing(onKeyDown))
 
@@ -101,14 +107,15 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
 
     const textarea = screen.getByRole('textbox')
     fireEvent.compositionStart(textarea)
-    userEvent.type(textarea, 'ime composition')
+    await user.type(textarea, 'ime composition')
     onKeyDown.mockReset()
-    userEvent.type(textarea, '{enter}')
+    await user.type(textarea, '{enter}')
 
     expect(onKeyDown).not.toHaveBeenCalled()
   })
 
   it('should not ignore Enter keydown event after composition ends', async () => {
+    const user = userEvent.setup()
     const onKeyDown = jest.fn()
     const {result} = renderHook(() => useIgnoreKeyboardActionsWhileComposing(onKeyDown))
 
@@ -116,10 +123,10 @@ describe('useIgnoreKeyboardActionsWhileComposing', () => {
 
     const textarea = screen.getByRole('textbox')
     fireEvent.compositionStart(textarea)
-    userEvent.type(textarea, 'ime composition')
+    await user.type(textarea, 'ime composition')
     fireEvent.compositionEnd(textarea)
     onKeyDown.mockReset()
-    userEvent.type(textarea, '{enter}')
+    await user.type(textarea, '{enter}')
 
     expect(onKeyDown).toHaveBeenCalled()
   })
