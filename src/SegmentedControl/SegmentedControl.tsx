7edf134757837e3a8b71bf31f67242226b1a2fd2
Mike Perrotti <mperrotti@github.com>
2022-08-05 11:17:32 -0400

    SegmentedControl - make fullWidth prop responsive (#2191)
    * updates fullWidth prop to be responsive, replaces useMatchMedia with new useResponsive hook
    
    * adds changeset
    
    * Update src/SegmentedControl/SegmentedControl.tsx
    
    Co-authored-by: Cole Bemis <colebemis@github.com>
    
    * Update docs/content/SegmentedControl.mdx
    
    Co-authored-by: Cole Bemis <colebemis@github.com>
    
    * Update docs/content/SegmentedControl.mdx
    
    Co-authored-by: Cole Bemis <colebemis@github.com>
    
    * imports ResponsiveValues type
    
    Co-authored-by: Cole Bemis <colebemis@github.com>

--- src/SegmentedControl/SegmentedControl.tsx
+++ src/SegmentedControl/SegmentedControl.tsx
@@ -3,7 +3,7 @@ import Button, {SegmentedControlButtonProps} from './SegmentedControlButton'
 import SegmentedControlIconButton, {SegmentedControlIconButtonProps} from './SegmentedControlIconButton'
 import {ActionList, ActionMenu, Box, useTheme} from '..'
 import {merge, SxProp} from '../sx'
-import useMatchMedia from '../hooks/useMatchMedia'
+import {ResponsiveValue, useResponsiveValue} from '../hooks/useResponsiveValue'
 import {ViewportRangeKeys} from '../utils/types/ViewportRangeKeys'
 import {FocusKeys, FocusZoneHookSettings, useFocusZone} from '../hooks/useFocusZone'
 
@@ -14,20 +14,20 @@ type SegmentedControlProps = {
   'aria-labelledby'?: string
   'aria-describedby'?: string
   /** Whether the control fills the width of its parent */
-  fullWidth?: boolean
+  fullWidth?: boolean | ResponsiveValue<boolean>
   /** The handler that gets called when a segment is selected */
   onChange?: (selectedIndex: number) => void
   /** Configure alternative ways to render the control when it gets rendered in tight spaces */
   variant?: 'default' | Partial<Record<WidthOnlyViewportRangeKeys, 'hideLabels' | 'dropdown' | 'default'>>
 } & SxProp
 
-const getSegmentedControlStyles = (props?: SegmentedControlProps) => ({
+const getSegmentedControlStyles = (isFullWidth?: boolean) => ({
   backgroundColor: 'segmentedControl.bg',
   borderColor: 'border.default',
   borderRadius: 2,
   borderStyle: 'solid',
   borderWidth: 1,
-  display: props?.fullWidth ? 'flex' : 'inline-flex',
+  display: isFullWidth ? 'flex' : 'inline-flex',
   height: '32px' // TODO: use primitive `control.medium.size` when it is available
 })
 
@@ -43,13 +43,8 @@ const Root: React.FC<React.PropsWithChildren<SegmentedControlProps>> = ({
 }) => {
   const segmentedControlContainerRef = useRef<HTMLSpanElement>(null)
   const {theme} = useTheme()
-  const mediaQueryMatches = useMatchMedia(Object.keys(variant || {}) as WidthOnlyViewportRangeKeys[])
-  const mediaQueryMatchesKeys = mediaQueryMatches
-    ? (Object.keys(mediaQueryMatches) as WidthOnlyViewportRangeKeys[]).filter(
-        viewportRangeKey => typeof mediaQueryMatches === 'object' && mediaQueryMatches[viewportRangeKey]
-      )
-    : []
-
+  const responsiveVariant = useResponsiveValue(variant, 'default')
+  const isFullWidth = useResponsiveValue(fullWidth, false)
   const selectedSegments = React.Children.toArray(children).map(
     child =>
       React.isValidElement<SegmentedControlButtonProps | SegmentedControlIconButtonProps>(child) && child.props.selected
@@ -79,13 +74,7 @@ const Root: React.FC<React.PropsWithChildren<SegmentedControlProps>> = ({
 
     return React.isValidElement<SegmentedControlIconButtonProps>(childArg) ? childArg.props['aria-label'] : null
   }
-
-  const sx = merge(
-    getSegmentedControlStyles({
-      fullWidth
-    }),
-    sxProp as SxProp
-  )
+  const sx = merge(getSegmentedControlStyles(isFullWidth), sxProp as SxProp)
 
   const focusInStrategy: FocusZoneHookSettings['focusInStrategy'] = () => {
     if (segmentedControlContainerRef.current) {
@@ -113,37 +102,7 @@ const Root: React.FC<React.PropsWithChildren<SegmentedControlProps>> = ({
     )
   }
 
-  // Since we can have multiple media query matches for `variant` (e.g.: 'regular' and 'wide'),
-  // we need to pick which variant we actually show.
-  const getVariantToRender = () => {
-    // If no variant was passed, return 'default'
-    if (!variant || variant === 'default') {
-      return 'default'
-    }
-
-    // Prioritize viewport range keys that override the 'regular' range in order of
-    // priorty from lowest to highest
-    // Orientation keys beat 'wide' because they are more specific.
-    const viewportRangeKeysByPriority: ViewportRangeKeys[] = ['wide', 'portrait', 'landscape']
-
-    // Filter the viewport range keys to only include those that:
-    // - are in the priority list
-    // - have a variant set
-    const variantPriorityKeys = mediaQueryMatchesKeys.filter(key => {
-      return viewportRangeKeysByPriority.includes(key) && variant[key]
-    })
-
-    // If we have to pick from multiple variants and one or more of them overrides 'regular',
-    // use the last key from the filtered list.
-    if (mediaQueryMatchesKeys.length > 1 && variantPriorityKeys.length) {
-      return variant[variantPriorityKeys[variantPriorityKeys.length - 1]]
-    }
-
-    // Otherwise, use the variant for the first matching media query
-    return typeof mediaQueryMatches === 'object' && variant[mediaQueryMatchesKeys[0]]
-  }
-
-  return getVariantToRender() === 'dropdown' ? (
+  return responsiveVariant === 'dropdown' ? (
     // Render the 'dropdown' variant of the SegmentedControlButton or SegmentedControlIconButton
     <ActionMenu>
       <ActionMenu.Button leadingIcon={getChildIcon(selectedChild)}>{getChildText(selectedChild)}</ActionMenu.Button>
@@ -211,7 +170,7 @@ const Root: React.FC<React.PropsWithChildren<SegmentedControlProps>> = ({
 
         // Render the 'hideLabels' variant of the SegmentedControlButton
         if (
-          getVariantToRender() === 'hideLabels' &&
+          responsiveVariant === 'hideLabels' &&
           React.isValidElement<SegmentedControlButtonProps>(child) &&
           child.type === Button
         ) {
