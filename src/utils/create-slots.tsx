728009d51ae0f981c9344a3f20794ceb5639ee75
Ian Sanders <iansan5653@github.com>
2022-08-19 16:12:52 +0000

    Use `useReducer` instead of `useState` for cleaner code + less memoizing

--- src/utils/create-slots.tsx
+++ src/utils/create-slots.tsx
@@ -1,3 +1,4 @@
+import {omit} from 'lodash'
 import React from 'react'
 import useLayoutEffect from './useIsomorphicLayoutEffect'
 
@@ -7,56 +8,42 @@ import useLayoutEffect from './useIsomorphicLayoutEffect'
  *  + Slot component that is used by LeadingVisual, Description
  */
 const createSlots = <SlotNames extends string>(_slotNames: SlotNames[]) => {
-  type SlotsStore = Partial<Record<SlotNames, React.ReactNode>>
+  type Store = Partial<Record<SlotNames, React.ReactNode>>
 
-  type SlotsContext = {
-    registerSlot: (name: SlotNames, contents: React.ReactNode) => void
-    unregisterSlot: (name: SlotNames) => void
+  type StoreAction =
+    | {type: 'register'; name: SlotNames; children: React.ReactNode}
+    | {type: 'unregister'; name: SlotNames}
+
+  const storeReducer = (slots: Store, action: StoreAction): Store => {
+    switch (action.type) {
+      case 'register':
+        return {...slots, [action.name]: action.children}
+      case 'unregister':
+        return omit(slots, action.name) as Store
+    }
   }
 
-  const SlotsContext = React.createContext<SlotsContext>({
-    registerSlot: () => null,
-    unregisterSlot: () => null
-  })
+  const SlotsContext = React.createContext<React.Dispatch<StoreAction> | null>(null)
 
   const Slots: React.FC<{
-    children: (slots: SlotsStore) => React.ReactNode
+    children: (slots: Store) => React.ReactNode
   }> = ({children}) => {
-    const [slots, setSlots] = React.useState<SlotsStore>({})
+    const [slots, dispatchSlots] = React.useReducer(storeReducer, {})
 
-    const registerSlot = React.useCallback(
-      (name: SlotNames, contents: React.ReactNode) => setSlots(prevSlots => ({...prevSlots, [name]: contents})),
-      []
-    )
-
-    // Slot can be removed from the tree as well,
-    // we need to unregister them from the slot
-    const unregisterSlot = React.useCallback(
-      (name: SlotNames) =>
-        setSlots(prevSlots => {
-          const newSlots = {...prevSlots}
-          delete newSlots[name]
-          return newSlots
-        }),
-      []
-    )
-
-    const context = React.useMemo(() => ({registerSlot, unregisterSlot}), [registerSlot, unregisterSlot])
-
-    return <SlotsContext.Provider value={context}>{children(slots)}</SlotsContext.Provider>
+    return <SlotsContext.Provider value={dispatchSlots}>{children(slots)}</SlotsContext.Provider>
   }
 
   const Slot: React.FC<{
     name: SlotNames
     children: React.ReactNode
   }> = React.memo(({name, children}) => {
-    const {registerSlot, unregisterSlot} = React.useContext(SlotsContext)
+    const dispatchSlots = React.useContext(SlotsContext)
 
-    useLayoutEffect(() => registerSlot(name, children), [name, children, registerSlot])
+    useLayoutEffect(() => dispatchSlots?.({type: 'register', name, children}), [name, children, dispatchSlots])
 
     // We don't need to cleanup on every render, since registering a slot that is already
     // registered will just overwrite the existing slot value. But we do need to cleanup on unmount
-    useLayoutEffect(() => () => unregisterSlot(name), [name, unregisterSlot])
+    useLayoutEffect(() => () => dispatchSlots?.({type: 'unregister', name}), [name, dispatchSlots])
 
     return null
   })
