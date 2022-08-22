41f3fdaa0398ed13f3fc012476c886ca0c58b634
Josh Black <josh@josh.black>
2022-08-22 13:47:26 -0500

    fix(Button): memoize call to merge.all

--- src/Button/ButtonBase.tsx
+++ src/Button/ButtonBase.tsx
@@ -1,4 +1,4 @@
-import React, {ComponentPropsWithRef, forwardRef} from 'react'
+import React, {ComponentPropsWithRef, forwardRef, useMemo} from 'react'
 import {ForwardRefComponent as PolymorphicForwardRefComponent} from '@radix-ui/react-polymorphic'
 import Box from '../Box'
 import {merge, SxProp} from '../sx'
@@ -7,18 +7,20 @@ import {ButtonProps, StyledButton} from './types'
 import {getVariantStyles, getSizeStyles, getButtonStyles} from './styles'
 
 const ButtonBase = forwardRef<HTMLElement, ButtonProps>(
-  ({children, as: Component = 'button', sx: sxProp = {}, ...props}, forwardedRef): JSX.Element => {
+  ({children, as: Component = 'button', sx: sxProp, ...props}, forwardedRef): JSX.Element => {
     const {leadingIcon: LeadingIcon, trailingIcon: TrailingIcon, variant = 'default', size = 'medium'} = props
     const {theme} = useTheme()
     const iconWrapStyles = {
       display: 'inline-block'
     }
-    const sxStyles = merge.all([
-      getButtonStyles(theme),
-      getSizeStyles(size, variant, false),
-      getVariantStyles(variant, theme),
-      sxProp as SxProp
-    ])
+    const sxStyles = useMemo(() => {
+      return merge.all([
+        getButtonStyles(theme),
+        getSizeStyles(size, variant, false),
+        getVariantStyles(variant, theme),
+        (sxProp ?? {}) as SxProp
+      ])
+    }, [theme, size, variant, sxProp])
     return (
       <StyledButton as={Component} sx={sxStyles} {...props} ref={forwardedRef}>
         {LeadingIcon && (
