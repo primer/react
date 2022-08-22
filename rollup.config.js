fec502eca963046d6fab0681a1122a665f831510
Josh Black <josh@josh.black>
2022-08-22 16:55:03 -0500

    refactor(build): update rollup build step

--- rollup.config.js
+++ rollup.config.js
@@ -3,24 +3,60 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 import visualizer from 'rollup-plugin-visualizer'
+import packageJson from './package.json'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
-
 const formats = ['esm', 'umd']
-
-const plugins = [
-  babel({extensions, exclude: 'node_modules/**', runtimeHelpers: true}),
-  resolve({extensions}),
-  commonjs(),
-  terser(),
-  visualizer({sourcemap: true})
+const external = [
+  ...Object.keys(packageJson.peerDependencies),
+  ...Object.keys(packageJson.dependencies),
+  ...Object.keys(packageJson.devDependencies)
 ]
+
+const baseConfig = {
+  input: 'src/index.ts',
+  external: id => {
+    return external.some(pkg => {
+      return id.startsWith(pkg)
+    })
+  },
+  plugins: [
+    resolve({
+      extensions
+    }),
+    commonjs(),
+    babel({
+      extensions,
+      exclude: /node_modules/,
+      runtimeHelpers: true
+    })
+  ]
+}
 
 export default [
   {
-    input: 'src/index.ts',
+    ...baseConfig,
+    output: {
+      dir: 'lib-esm',
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src'
+    }
+  },
+  {
+    ...baseConfig,
+    output: {
+      dir: 'lib',
+      format: 'commonjs',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      exports: 'named'
+    }
+  },
+  {
+    ...baseConfig,
     external: ['styled-components', 'react', 'react-dom'],
-    plugins,
+    plugins: [...baseConfig.plugins, terser(), visualizer({sourcemap: true})],
     output: formats.map(format => ({
       file: `dist/browser.${format}.js`,
       format,
