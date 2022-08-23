1056b8d36efaa2f3d0ce3e85fdc306819668f41d
Josh Black <josh@josh.black>
2022-08-23 10:57:24 -0500

    refactor(button): remove sxProp from useMemo call

--- src/Button/ButtonBase.tsx
+++ src/Button/ButtonBase.tsx
@@ -7,22 +7,17 @@ import {ButtonProps, StyledButton} from './types'
 import {getVariantStyles, getSizeStyles, getButtonStyles} from './styles'
 
 const ButtonBase = forwardRef<HTMLElement, ButtonProps>(
-  ({children, as: Component = 'button', sx: sxProp, ...props}, forwardedRef): JSX.Element => {
+  ({children, as: Component = 'button', sx: sxProp = {}, ...props}, forwardedRef): JSX.Element => {
     const {leadingIcon: LeadingIcon, trailingIcon: TrailingIcon, variant = 'default', size = 'medium'} = props
     const {theme} = useTheme()
     const iconWrapStyles = {
       display: 'inline-block'
     }
     const sxStyles = useMemo(() => {
-      return merge.all([
-        getButtonStyles(theme),
-        getSizeStyles(size, variant, false),
-        getVariantStyles(variant, theme),
-        (sxProp ?? {}) as SxProp
-      ])
-    }, [theme, size, variant, sxProp])
+      return merge.all([getButtonStyles(theme), getSizeStyles(size, variant, false), getVariantStyles(variant, theme)])
+    }, [theme, size, variant])
     return (
-      <StyledButton as={Component} sx={sxStyles} {...props} ref={forwardedRef}>
+      <StyledButton as={Component} sx={merge(sxStyles, sxProp as SxProp)} {...props} ref={forwardedRef}>
         {LeadingIcon && (
           <Box as="span" data-component="leadingIcon" sx={iconWrapStyles}>
             <LeadingIcon />
