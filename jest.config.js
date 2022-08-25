5b1ed48b00b7ba289b79225877923d968312a70e
Josh Black <josh@josh.black>
2022-08-25 13:18:36 -0500

    test: update jest transformIgnorePattern config

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,10 @@ module.exports = {
     '<rootDir>/src/utils/test-helpers.tsx'
   ],
   testMatch: ['<rootDir>/(src|codemods)/**/*.test.[jt]s?(x)', '!**/*.types.test.[jt]s?(x)'],
-  transformIgnorePatterns: [
-    'node_modules/(?!@github/combobox-nav|@koddsson/textarea-caret|@github/markdown-toolbar-element)'
-  ]
+
+  // Note: this configuration option should not exclude any packages within
+  // `node_modules`. When excluding packages from `node_modules`, we have no way
+  // to test if this package is shipping with support for both ESM and CommonJS
+  // @see https://github.com/primer/react/issues/2275
+  transformIgnorePatterns: ['/node_modules/']
 }
