2ad9f735e952c1181ec76170fe39cb275f927b80
Cole Bemis <colebemis@github.com>
2022-09-14 17:00:19 -0700

    Add test for right arrow key expand

--- src/TreeView/TreeView.test.tsx
+++ src/TreeView/TreeView.test.tsx
@@ -114,3 +114,42 @@ it('moves aria-activedescendant with up and down arrow keys', () => {
   // aria-activedescendant should now be set to the second visible treeitem
   expect(root).toHaveAttribute('aria-activedescendant', item2.id)
 })
+
+it('expands a collapsed item with right arrow key', () => {
+  const {getByRole} = render(
+    <TreeView aria-label="Test tree">
+      <TreeView.Item>
+        Parent
+        <TreeView.SubTree>
+          <TreeView.Item>Child</TreeView.Item>
+        </TreeView.SubTree>
+      </TreeView.Item>
+    </TreeView>
+  )
+
+  const root = getByRole('tree')
+  const parentItem = getByRole('treeitem', {name: 'Parent'})
+
+  // aria-activedescendant should be set to the first visible treeitem by default
+  expect(root).toHaveAttribute('aria-activedescendant', parentItem.id)
+
+  // aria-expanded should be false by default
+  expect(parentItem).toHaveAttribute('aria-expanded', 'false')
+
+  // Focus tree
+  root.focus()
+
+  // Press →
+  fireEvent.keyDown(document.activeElement || document.body, {key: 'ArrowRight'})
+
+  // aria-expanded should now be true
+  expect(parentItem).toHaveAttribute('aria-expanded', 'true')
+
+  // aria-activedescendant should still be set to the parent treeitem
+  expect(root).toHaveAttribute('aria-activedescendant', parentItem.id)
+
+  const subtree = getByRole('group')
+
+  // Subtree should now be visible
+  expect(subtree).toBeVisible()
+})
