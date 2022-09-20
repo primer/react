788102290c9509441b050444f48e8ebe67f73381
Cole Bemis <colebemis@github.com>
2022-09-20 11:22:11 -0700

    Create LevelIndicatorLines component

--- src/TreeView/TreeView.tsx
+++ src/TreeView/TreeView.tsx
@@ -323,35 +323,7 @@ const Item: React.FC<TreeViewItemProps> = ({defaultExpanded = false, onSelect, o
           }}
         >
           <Box sx={{gridArea: 'spacer', display: 'flex'}}>
-            {
-              // Indicator lines for each nested level
-              Array.from({length: level - 1}).map((_, index) => (
-                <Box
-                  key={index}
-                  sx={{
-                    width: '100%',
-                    height: '100%',
-                    transition: 'border-color 33.333ms linear',
-                    borderRight: '1px solid',
-
-                    // On devices without hover, the nesting indicator lines
-                    // appear at all times.
-                    borderColor: 'border.subtle',
-
-                    // On devices with :hover support, the nesting indicator lines
-                    // fade in when the user mouses over the entire component,
-                    // or when there's focus inside the component. This makes
-                    // sure the component remains simple when not in use.
-                    '@media (hover: hover)': {
-                      borderColor: 'transparent',
-                      '[role=tree]:hover &, [role=tree]:focus &': {
-                        borderColor: 'border.subtle'
-                      }
-                    }
-                  }}
-                />
-              ))
-            }
+            <LevelIndicatorLines level={level} />
           </Box>
           {hasSubTree ? (
             <Box
@@ -394,6 +366,40 @@ const Item: React.FC<TreeViewItemProps> = ({defaultExpanded = false, onSelect, o
         {subTree}
       </li>
     </ItemContext.Provider>
+  )
+}
+
+/** Lines to indicate the depth of an item in a TreeView */
+const LevelIndicatorLines: React.FC<{level: number}> = ({level}) => {
+  return (
+    <Box sx={{width: '100%', display: 'flex'}}>
+      {Array.from({length: level - 1}).map((_, index) => (
+        <Box
+          key={index}
+          sx={{
+            width: '100%',
+            height: '100%',
+            transition: 'border-color 33.333ms linear',
+            borderRight: '1px solid',
+
+            // On devices without hover, the nesting indicator lines
+            // appear at all times.
+            borderColor: 'border.subtle',
+
+            // On devices with :hover support, the nesting indicator lines
+            // fade in when the user mouses over the entire component,
+            // or when there's focus inside the component. This makes
+            // sure the component remains simple when not in use.
+            '@media (hover: hover)': {
+              borderColor: 'transparent',
+              '[role=tree]:hover &, [role=tree]:focus &': {
+                borderColor: 'border.subtle'
+              }
+            }
+          }}
+        />
+      ))}
+    </Box>
   )
 }
 
