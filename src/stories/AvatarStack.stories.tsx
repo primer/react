1cbe3fc607d6cc3a5f78d78dc125afd8deb7f6cd
Pablo Núñez <pablonete@github.com>
2022-09-23 08:18:24 +0000

    Add dialog to AvatarStack storybook so we can test it grays out and won't overlap.

--- src/stories/AvatarStack.stories.tsx
+++ src/stories/AvatarStack.stories.tsx
@@ -1,8 +1,9 @@
 import AvatarStack from '../AvatarStack'
 import Avatar from '../Avatar'
+import {Dialog} from '../Dialog/Dialog'
 import {Meta} from '@storybook/react'
-import React from 'react'
-import {ThemeProvider} from '..'
+import React, {useState} from 'react'
+import {Button, ThemeProvider} from '..'
 import BaseStyles from '../BaseStyles'
 
 const meta: Meta = {
@@ -26,13 +27,26 @@ const meta: Meta = {
 export default meta
 
 export function AvatarStackStory(): JSX.Element {
+  const [dialogOpen, setDialogOpen] = useState(false)
+
   return (
-    <AvatarStack>
-      <Avatar alt="Primer logo" src="https://avatars.githubusercontent.com/primer" />
-      <Avatar alt="GitHub logo" src="https://avatars.githubusercontent.com/github" />
-      <Avatar alt="Atom logo" src="https://avatars.githubusercontent.com/atom" />
-      <Avatar alt="GitHub Desktop logo" src="https://avatars.githubusercontent.com/desktop" />
-    </AvatarStack>
+    <div>
+      <p>Avatar stacks should gray out when the dialog is displayed, and they should show behind the dialog.</p>
+      <AvatarStack>
+        <Avatar alt="Primer logo" src="https://avatars.githubusercontent.com/primer" />
+        <Avatar alt="GitHub logo" src="https://avatars.githubusercontent.com/github" />
+        <Avatar alt="Atom logo" src="https://avatars.githubusercontent.com/atom" />
+        <Avatar alt="GitHub Desktop logo" src="https://avatars.githubusercontent.com/desktop" />
+      </AvatarStack>
+      <Button onClick={() => setDialogOpen(!dialogOpen)} sx={{my: 4}}>
+        Show dialog
+      </Button>
+      {dialogOpen && (
+        <Dialog title="Test dialog" width="xlarge" onClose={() => setDialogOpen(false)}>
+          Content
+        </Dialog>
+      )}
+    </div>
   )
 }
 AvatarStackStory.storyName = 'AvatarStack'
