d27e956f70a0fa0b5a4c0f5e33a93e43c585fb15
Josh Black <josh@josh.black>
2022-09-29 15:49:42 -0500

    chore(storybook): run sb upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,20 @@ module.exports = {
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-links',
-    {name: '@storybook/addon-essentials', options: {backgrounds: false}},
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false
+      }
+    },
     '@storybook/addon-interactions',
     'storybook-addon-performance/register',
-    {name: 'storybook-addon-turbo-build', options: {optimizationLevel: 2}},
+    {
+      name: 'storybook-addon-turbo-build',
+      options: {
+        optimizationLevel: 2
+      }
+    },
     ...(process.env.NODE_ENV === 'production' && process.env.GITHUB_JOB !== 'chromatic'
       ? ['@whitespace/storybook-addon-html']
       : [])
@@ -23,5 +33,6 @@ module.exports = {
   features: {
     interactionsDebugger: true,
     storyStoreV7: true
-  }
+  },
+  framework: '@storybook/react'
 }
