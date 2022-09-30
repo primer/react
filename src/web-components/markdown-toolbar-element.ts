fe2674e75b7e64bc53305fed1990ffad4bc529d3
Josh Black <josh@josh.black>
2022-09-30 13:11:49 -0500

    chore: add comment for eslint violation

--- src/web-components/markdown-toolbar-element.ts
+++ src/web-components/markdown-toolbar-element.ts
@@ -2,6 +2,10 @@ import {canUseDOM} from '../utils/environment'
 
 if (canUseDOM) {
   if (typeof require === 'function') {
+    // Since require() is synchronous, we use Promise.resolve() to create a
+    // promise-chain to then run `require()` in order to emulate `import()`
+    // behavior
+    // eslint-disable-next-line github/no-then
     Promise.resolve().then(() => {
       require('@github/markdown-toolbar-element')
     })
