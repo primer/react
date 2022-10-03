5968a3999ceacc7b1cdbb7717b9d1712dfa552cb
Josh Black <josh@josh.black>
2022-10-03 11:20:42 -0500

    chore: add support for CSS.supports in JSDOM

--- src/utils/test-helpers.tsx
+++ src/utils/test-helpers.tsx
@@ -5,3 +5,12 @@ global.ResizeObserver = jest.fn().mockImplementation(() => {
     disconnect: jest.fn()
   }
 })
+
+global.CSS = {
+  escape: jest.fn(),
+  supports: jest.fn().mockImplementation(() => {
+    return () => {
+      return false
+    }
+  })
+}
