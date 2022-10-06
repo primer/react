bd9ed65b309fb75a3a554ad280f9e6c6dad0a2f3
Armagan Ersoz <broccolinisoup@github.com>
2022-10-06 18:56:33 +1000

    restyle arrow btns and fix focus issue

--- src/UnderlineNav2/UnderlineNavArrowButton.tsx
+++ src/UnderlineNav2/UnderlineNavArrowButton.tsx
@@ -1,9 +1,10 @@
 import React, {useContext} from 'react'
 import {IconButton} from '../Button/IconButton'
 import {ChevronLeftIcon, ChevronRightIcon} from '@primer/octicons-react'
-import {getLeftArrowHiddenBtn, getRightArrowHiddenBtn, getLeftArrowVisibleBtn, getRightArrowVisibleBtn} from './styles'
+import {btnWrapperStyles, getArrowBtnStyles, getArrowHiddenBtn} from './styles'
 import {OnScrollWithButtonEventType} from './types'
 import {UnderlineNavContext} from './UnderlineNavContext'
+import Box from '../Box'
 
 const ArrowButton = ({
   type,
@@ -16,26 +17,32 @@ const ArrowButton = ({
   onScrollWithButton: OnScrollWithButtonEventType
   ariaLabel: string
 }) => {
-  const btnRef = React.useRef<HTMLButtonElement>(null)
+  const leftBtnRef = React.useRef<HTMLButtonElement>(null)
+  const rightBtnRef = React.useRef<HTMLButtonElement>(null)
   const {theme} = useContext(UnderlineNavContext)
   const direction = type === 'left' ? -1 : 1
-  const leftBtnStyle = show ? getLeftArrowVisibleBtn(theme) : getLeftArrowHiddenBtn(theme)
-  const rightBtnStyle = show ? getRightArrowVisibleBtn(theme) : getRightArrowHiddenBtn(theme)
   // re-trigger focus on the button with aria-disabled=true when it becomes hidden to communicate to screen readers that the button is no longer available
   React.useEffect(() => {
-    if (btnRef.current?.getAttribute('aria-disabled') === 'true') {
-      btnRef.current.focus()
+    const currentBtn = type === 'left' ? leftBtnRef.current : rightBtnRef.current
+    if (currentBtn?.getAttribute('aria-disabled') === 'true') {
+      currentBtn.focus()
+    } else {
+      // eslint-disable-next-line github/no-blur
+      currentBtn?.blur()
     }
-  }, [show])
+  }, [show, type])
   return (
-    <IconButton
-      ref={btnRef}
-      aria-label={`Scroll ${ariaLabel} navigation ${type}`}
-      onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => onScrollWithButton(e, direction)}
-      icon={type === 'left' ? ChevronLeftIcon : ChevronRightIcon}
-      sx={type === 'left' ? leftBtnStyle : rightBtnStyle}
-      aria-disabled={!show}
-    />
+    <Box sx={btnWrapperStyles(theme, type, show)}>
+      <IconButton
+        tabIndex={show ? 0 : -1}
+        ref={type === 'left' ? leftBtnRef : rightBtnRef}
+        aria-label={`Scroll ${ariaLabel} navigation ${type}`}
+        onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => onScrollWithButton(e, direction)}
+        icon={type === 'left' ? ChevronLeftIcon : ChevronRightIcon}
+        sx={show ? getArrowBtnStyles(theme, type) : getArrowHiddenBtn(theme, type)}
+        aria-disabled={!show}
+      />
+    </Box>
   )
 }
 
