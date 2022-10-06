0cf2156827a07e87963074a7299e3aa622e717fc
Armagan Ersoz <broccolinisoup@github.com>
2022-10-06 14:15:47 +1000

    fix linting

--- src/UnderlineNav2/UnderlineNav.test.tsx
+++ src/UnderlineNav2/UnderlineNav.test.tsx
@@ -1,4 +1,4 @@
-import React, {useLayoutEffect} from 'react'
+import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import {fireEvent, render} from '@testing-library/react'
 import {
