9b8081926148356f2eaea71a3e969cc15818e4d0
Cole Bemis <colebemis@github.com>
2022-10-14 14:54:09 -0700

    Snapshot exports

---
+++ src/__tests__/exports.test.ts
@@ -0,0 +1,21 @@
+import * as Index from '..'
+import * as Drafts from '../drafts'
+import * as Deprecated from '../deprecated'
+
+describe('@primer/react', () => {
+  it('should not update exports without a semver change', () => {
+    expect(Object.keys(Index).sort()).toMatchSnapshot()
+  })
+})
+
+describe('@primer/react/drafts', () => {
+  it('should not update exports without a semver change', () => {
+    expect(Object.keys(Drafts).sort()).toMatchSnapshot()
+  })
+})
+
+describe('@primer/react/decprecated', () => {
+  it('should not update exports without a semver change', () => {
+    expect(Object.keys(Deprecated).sort()).toMatchSnapshot()
+  })
+})
