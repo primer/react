45d5ec3244a5ac29f53fc8c2cc2a7778857f683d
Matthew Costabile <mattcosta7@github.com>
2022-10-20 13:53:23 +0000

    add a more exhaustive type test

--- src/__tests__/Button.types.test.tsx
+++ src/__tests__/Button.types.test.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {Button} from '../Button'
 
 export function shouldAcceptOnlyAChildProp() {
   return <Button>child</Button>
+}
+
+export function ShouldAcceptKnownButtonPropsAndDomProps() {
+  const buttonEl = useRef<HTMLButtonElement>()
+  return (
+    <Button
+      ref={buttonEl}
+      leadingIcon={() => <></>}
+      trailingIcon={() => <></>}
+      size="medium"
+      variant="primary"
+      disabled
+      aria-label="some label"
+      onClick={e => {
+        // current target is assignable to HTMLButtonElement
+        buttonEl.current = e.currentTarget
+      }}
+      sx={{
+        m: 1
+      }}
+    >
+      Child
+    </Button>
+  )
 }
 
 export function shouldNotAcceptOutlandishProps() {
