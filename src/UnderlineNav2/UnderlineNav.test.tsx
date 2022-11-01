9521aabb0b91f80478170a85bfe8eed28ac03b49
Armagan Ersoz <broccolinisoup@github.com>
2022-11-01 20:27:02 +1000

    UnderlineNav unit tests and snapshot

--- src/UnderlineNav2/UnderlineNav.test.tsx
+++ src/UnderlineNav2/UnderlineNav.test.tsx
@@ -13,6 +13,7 @@ import {
 } from '@primer/octicons-react'
 
 import {UnderlineNav} from '.'
+import {behavesAsComponent, checkExports, checkStoriesForAxeViolations} from '../utils/testing'
 
 // window.matchMedia() is not implemented by JSDOM so we have to create a mock:
 // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
@@ -30,12 +31,22 @@ Object.defineProperty(window, 'matchMedia', {
   }))
 })
 
+const arrowLeft = {key: 'ArrowLeft', code: 37, charCode: 37}
+const arrowRight = {key: 'ArrowRight', code: 39, charCode: 39}
+const arrowUp = {key: 'ArrowUp', code: 38, charCode: 38}
+const arrowDown = {key: 'ArrowDown', code: 40, charCode: 40}
+const tab = {key: 'Tab', code: 9, charCode: 9}
+const enter = {key: 'Enter', code: 13, charCode: 13}
+const space = {key: ' ', code: 32, charCode: 32}
+
 const ResponsiveUnderlineNav = ({
   selectedItemText = 'Code',
-  loadingCounters = false
+  loadingCounters = false,
+  displayExtraEl = false
 }: {
   selectedItemText?: string
   loadingCounters?: boolean
+  displayExtraEl?: boolean
 }) => {
   const items: {navigation: string; icon?: React.FC<IconProps>; counter?: number}[] = [
     {navigation: 'Code', icon: CodeIcon},
@@ -48,23 +59,37 @@ const ResponsiveUnderlineNav = ({
     {navigation: 'Settings', counter: 10},
     {navigation: 'Security', icon: ShieldLockIcon}
   ]
+
   return (
-    <UnderlineNav aria-label="Repository" loadingCounters={loadingCounters}>
-      {items.map(item => (
-        <UnderlineNav.Item
-          key={item.navigation}
-          icon={item.icon}
-          selected={item.navigation === selectedItemText}
-          counter={item.counter}
-        >
-          {item.navigation}
-        </UnderlineNav.Item>
-      ))}
-    </UnderlineNav>
+    <div>
+      <UnderlineNav aria-label="Repository" loadingCounters={loadingCounters}>
+        {items.map(item => (
+          <UnderlineNav.Item
+            key={item.navigation}
+            icon={item.icon}
+            selected={item.navigation === selectedItemText}
+            counter={item.counter}
+          >
+            {item.navigation}
+          </UnderlineNav.Item>
+        ))}
+      </UnderlineNav>
+      {displayExtraEl && <button>Custom button</button>}
+    </div>
   )
 }
 
 describe('UnderlineNav', () => {
+  behavesAsComponent({
+    Component: UnderlineNav,
+    options: {skipAs: true, skipSx: true},
+    toRender: () => <ResponsiveUnderlineNav />
+  })
+
+  checkExports('UnderlineNav2', {
+    default: undefined,
+    UnderlineNav
+  })
   it('renders aria-current attribute to be pages when an item is selected', () => {
     const {getByText} = render(<ResponsiveUnderlineNav />)
     const selectedNavLink = getByText('Code').closest('a')
@@ -95,8 +120,10 @@ describe('UnderlineNav', () => {
     const item = getByText('Item 1')
     fireEvent.click(item)
     expect(onSelect).toHaveBeenCalledTimes(1)
-    fireEvent.keyPress(item, {key: 'Enter', code: 13, charCode: 13})
+    fireEvent.keyPress(item, enter)
     expect(onSelect).toHaveBeenCalledTimes(2)
+    fireEvent.keyPress(item, space)
+    expect(onSelect).toHaveBeenCalledTimes(3)
   })
   it('respects counter prop', () => {
     const {getByText} = render(<ResponsiveUnderlineNav />)
@@ -119,3 +146,55 @@ describe('UnderlineNav', () => {
     expect(heading.textContent).toBe('Repository navigation')
   })
 })
+
+describe('Keyboard Navigation', () => {
+  it('should move focus to the next/previous item on the list with horizontal arrow keys', () => {
+    const {getByText} = render(<ResponsiveUnderlineNav />)
+    const item = getByText('Code').closest('a') as HTMLAnchorElement
+    const nextItem = getByText('Issues').closest('a') as HTMLAnchorElement
+    // Focus first item
+    item.focus()
+    // Press right arrow
+    fireEvent.keyDown(item, arrowRight)
+    // focus should be on the next item
+    expect(nextItem).toHaveFocus()
+    expect(nextItem.getAttribute('tabindex')).toBe('0')
+    fireEvent.keyDown(nextItem, arrowLeft)
+    // focus should be on the previous item
+    expect(item).toHaveFocus()
+    expect(nextItem.getAttribute('tabindex')).toBe('-1')
+    expect(item.getAttribute('tabindex')).toBe('0')
+  })
+  it('should move focus to the next/previous item on the list with vertical arrow keys', () => {
+    const {getByText} = render(<ResponsiveUnderlineNav />)
+    const item = getByText('Code').closest('a') as HTMLAnchorElement
+    const nextItem = getByText('Issues').closest('a') as HTMLAnchorElement
+    // Focus first item
+    item.focus()
+    // Press down arrow
+    fireEvent.keyDown(item, arrowDown)
+    // focus should be on the next item
+    expect(nextItem).toHaveFocus()
+    expect(nextItem.getAttribute('tabindex')).toBe('0')
+    // Press up arrow
+    fireEvent.keyDown(nextItem, arrowUp)
+    // focus should be on the previous item
+    expect(item).toHaveFocus()
+    expect(nextItem.getAttribute('tabindex')).toBe('-1')
+    expect(item.getAttribute('tabindex')).toBe('0')
+  })
+  it('should move focus to the next/previous item on the list with the tab key', () => {
+    const {getByText} = render(<ResponsiveUnderlineNav />)
+    const item = getByText('Code').closest('a') as HTMLAnchorElement
+    const nextItem = getByText('Issues').closest('a') as HTMLAnchorElement
+    // Focus first item
+    item.focus()
+    // Press down arrow
+    fireEvent.keyDown(item, tab)
+    // focus should be on the next item
+    expect(nextItem).toHaveFocus()
+    expect(nextItem.getAttribute('tabindex')).toBe('0')
+  })
+})
+
+checkStoriesForAxeViolations('examples', '../UnderlineNav2/')
