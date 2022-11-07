e62e16ceb5123591c7fd46f2a0ed0fc3c08d7cba
Josep Martins <josepmartins@github.com>
2022-11-07 15:26:55 +0100

    Add playground

--- src/stories/ButtonGroup.stories.tsx
+++ src/stories/ButtonGroup.stories.tsx
@@ -1,31 +1,49 @@
 import React from 'react'
-import {Meta} from '@storybook/react'
-
+import {Story, Meta} from '@storybook/react'
 import ButtonGroup from '../ButtonGroup'
-import {Button, ButtonProps, IconButton} from '../Button'
+import {Button, ButtonProps} from '../Button'
 
 export default {
   title: 'Components/ButtonGroup',
+  component: ButtonGroup,
   argTypes: {
-    size: {
-      control: {
-        type: 'radio'
-      },
-      options: ['small', 'medium', 'large']
-    },
-    disabled: {
-      control: {
-        type: 'boolean',
-        default: false
-      }
-    }
+    as: {table: {disable: true}},
+    ref: {table: {disable: true}},
+    theme: {table: {disable: true}},
+    forwardedAs: {table: {disable: true}},
+    sx: {table: {disable: true}}
   }
-} as Meta
+} as Meta<typeof ButtonGroup>
 
-export const Default = (args: ButtonProps) => (
+export const Default = () => (
+  <ButtonGroup>
+    <Button>Button 1</Button>
+    <Button>Button 2</Button>
+    <Button>Button 3</Button>
+  </ButtonGroup>
+)
+
+export const Playground: Story<ButtonProps> = args => (
   <ButtonGroup>
     <Button {...args}>Button 1</Button>
     <Button {...args}>Button 2</Button>
     <Button {...args}>Button 3</Button>
   </ButtonGroup>
 )
+Playground.args = {
+  size: 'medium',
+  disabled: false
+}
+Playground.argTypes = {
+  size: {
+    control: {
+      type: 'radio'
+    },
+    options: ['small', 'medium', 'large']
+  },
+  disabled: {
+    control: {
+      type: 'boolean'
+    }
+  }
+}
