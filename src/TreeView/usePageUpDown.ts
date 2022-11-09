92e76a97b37285497b496fae3bdc8dcf97d0ded1
Josh Black <josh@josh.black>
2022-11-09 13:06:09 -0600

    refactor(TreeView): update implementation to use keyup plus common scroll helpers

--- src/TreeView/usePageUpDown.ts
+++ src/TreeView/usePageUpDown.ts
@@ -1,11 +1,19 @@
 import React from 'react'
+import {getScrollContainer} from '../utils/scroll'
 
 /**
  * Manage the active element when the user presses `PageUp` or `PageDown`
- * while using the TreeView
+ * while using the TreeView. This hook tackles the following scenarios when
+ * dealing with this scenarios:
+ *
+ * - When the TreeView is within a nested scroll container or scrolls through
+ *   the document
+ * - When the user hits the edge of a scroll container
+ * - When the user presses and holds either PageUp or PageDown
+ * - When the user presses either key and the item is still visible
  */
 export function usePageUpDown(ref: React.RefObject<HTMLElement>) {
-  const {onScrollEnd} = useScrollEnd()
+  const {onScrollEnd} = useScrollEnd(ref)
 
   React.useEffect(() => {
     if (ref.current === null) {
@@ -15,67 +23,26 @@ export function usePageUpDown(ref: React.RefObject<HTMLElement>) {
     const {current: tree} = ref
 
     function listener(event: KeyboardEvent) {
-      // When pressing PageUp, look at treeitem's above the current active element
-      // and find the first one that is not in the viewport. If one exists, focus it
-      if (event.key === 'PageUp') {
+      if (event.key === 'PageUp' || event.key === 'PageDown') {
         onScrollEnd(() => {
-          const items: HTMLElement[] = Array.from(tree.querySelectorAll('[role="treeitem"]'))
-            .filter((item): item is HTMLElement => {
-              if (item.nodeType !== Node.ELEMENT_NODE) {
-                return false
-              }
+          const scrollContainer = getScrollContainer(tree)
 
-              const position = document.activeElement?.compareDocumentPosition(item)
-              if (position === undefined) {
-                return false
-              }
-
-              return !!(position & Node.DOCUMENT_POSITION_PRECEDING)
-            })
-            .reverse()
-
-          for (const item of items) {
-            if (item.firstElementChild === null) {
-              continue
-            }
-
-            // Check the first element element instead of the item in case the
-            // tree contains child items
-            if (isInViewport(item.firstElementChild)) {
-              item.focus()
-              break
-            }
+          if (document.activeElement && isVisible(document.activeElement as HTMLElement, scrollContainer)) {
+            return
           }
-        })
-      }
 
-      // When pressing PageDown, look at treeitem's below the current active element
-      // and find the first one that is not in the viewport. If one exists, focus it
-      if (event.key === 'PageDown') {
-        onScrollEnd(() => {
-          const items: HTMLElement[] = Array.from(tree.querySelectorAll('[role="treeitem"]')).filter(
-            (item): item is HTMLElement => {
-              if (item.nodeType !== Node.ELEMENT_NODE) {
-                return false
-              }
+          const items = Array.from(tree.querySelectorAll('[role="treeitem"]')) as HTMLElement[]
 
-              const position = document.activeElement?.compareDocumentPosition(item)
-              if (position === undefined) {
-                return false
-              }
-
-              return !!(position & Node.DOCUMENT_POSITION_FOLLOWING)
-            }
-          )
+          if (event.key === 'PageUp') {
+            items.reverse()
+          }
 
           for (const item of items) {
             if (item.firstElementChild === null) {
               continue
             }
 
-            // Check the first element element instead of the item in case the
-            // tree contains child items
-            if (isInViewport(item.firstElementChild)) {
+            if (isVisible(item, scrollContainer)) {
               item.focus()
               break
             }
@@ -84,40 +51,87 @@ export function usePageUpDown(ref: React.RefObject<HTMLElement>) {
       }
     }
 
-    tree.addEventListener('keydown', listener)
+    tree.addEventListener('keyup', listener)
 
     return () => {
-      tree.removeEventListener('keydown', listener)
+      tree.removeEventListener('keyup', listener)
     }
   }, [ref, onScrollEnd])
 }
 
 /**
+ * The amount of time (in ms) to wait before registering that scrolling has
+ * ended.
+ */
+const SCROLL_DELAY = 60
+
+/**
  * Provides a function, `onScrollEnd`, that will call the given callback when it
  * detects that the application is no longer scrolling.
  */
-function useScrollEnd() {
+function useScrollEnd(ref: React.RefObject<HTMLElement>) {
   // Keep track of the current timeout and scroll listener in case the owner
   // component unmounts before the timer has a chance to complete
   const timeoutId = React.useRef<number | null>(null)
   const activeListener = React.useRef<(() => void) | null>(null)
-  const onScrollEnd = React.useCallback(callback => {
-    function listener() {
+
+  // Detect when the user has stopped scrolling by listening to scroll events
+  // and calling the given callback after a `scroll` event has not been fired
+  // after a certain amount of time.
+  //
+  // This callback will also be called if no scroll event is detected after a
+  // certain amount of time
+  const onScrollEnd = React.useCallback(
+    callback => {
+      const container = getScrollContainer(ref.current) ?? window
+
+      // If there and currently any timers, like from previous calls to
+      // `onScrollEnd`, cancel them and remove any active event listeners
       if (timeoutId.current !== null) {
         clearTimeout(timeoutId.current)
+        timeoutId.current = null
       }
-      timeoutId.current = window.setTimeout(() => {
-        callback()
-        window.removeEventListener('scroll', listener)
+
+      if (activeListener.current !== null) {
+        container.removeEventListener('scroll', activeListener.current)
         activeListener.current = null
-      }, 60)
-    }
+      }
 
-    window.addEventListener('scroll', listener)
-    activeListener.current = listener
-  }, [])
+      function onScroll() {
+        if (timeoutId.current !== null) {
+          clearTimeout(timeoutId.current)
+        }
+        timeoutId.current = window.setTimeout(scrollEnd, SCROLL_DELAY)
+      }
 
+      // When we have detected that scrolling has ended, clean up any active
+      // event listeners and reset our refs that track our active listener and
+      // timeout
+      function scrollEnd() {
+        container.removeEventListener('scroll', onScroll)
+        activeListener.current = null
+        timeoutId.current = null
+
+        callback()
+      }
+
+      container.addEventListener('scroll', onScroll)
+      activeListener.current = onScroll
+
+      // Add an initial timer in case no event is triggered after the scroll
+      // event is registered. This is useful when at the end of a scroll
+      // container where you want the code to run but technically a scroll event
+      // has not run
+      timeoutId.current = window.setTimeout(scrollEnd, SCROLL_DELAY)
+    },
+    [ref]
+  )
+
+  // If the owner component unmounts, clear up any pending timers and remove any
+  // event listeners that are currently registered
   React.useEffect(() => {
+    const container = getScrollContainer(ref.current) ?? window
+
     return () => {
       if (timeoutId.current !== null) {
         clearTimeout(timeoutId.current)
@@ -125,15 +139,34 @@ function useScrollEnd() {
       }
 
       if (activeListener.current !== null) {
-        window.removeEventListener('scroll', activeListener.current)
+        container.removeEventListener('scroll', activeListener.current)
         activeListener.current = null
       }
     }
-  }, [])
+  }, [ref])
 
   return {
     onScrollEnd
   }
+}
+
+/**
+ * Determine the visibility of an element
+ */
+function isVisible(element: HTMLElement, scrollContainer?: Element | null): boolean {
+  // If a scroll container is present, check to see if the element is visible
+  // within it
+  if (scrollContainer) {
+    const elementTop = element.offsetTop
+    const elementBottom = elementTop + element.clientHeight
+    const parentTop = scrollContainer.scrollTop
+    const parentBottom = parentTop + scrollContainer.clientHeight
+
+    return elementTop >= parentTop && elementBottom <= parentBottom
+  }
+
+  // Otherwise, check to see if the element is visible in the viewport
+  return isInViewport(element)
 }
 
 function isInViewport(element: Element): boolean {
