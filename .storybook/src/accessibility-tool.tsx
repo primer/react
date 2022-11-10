ecee24208a38563ecde3d97fae41b9ee792a9be1
kendallgassner <kendallgassner@github.com>
2022-11-10 15:59:07 -0800

    WIP: Add accessible surrounding elements

---
+++ .storybook/src/accessibility-tool.tsx
@@ -0,0 +1,39 @@
+import React, {useCallback} from 'react'
+import {useGlobals} from '@storybook/api'
+import {IconButton, ThemeProvider, Tooltip} from '../../src'
+import {AccessibilityIcon} from '@primer/octicons-react'
+
+export const ADDON_ID = 'myaddon'
+export const TOOL_ID = `${ADDON_ID}/tool`
+
+export const Tool = () => {
+  const [{showSurroundingElements, colorScheme}, updateGlobals] = useGlobals()
+
+  const toggleMyTool = useCallback(
+    () =>
+      updateGlobals({
+        showSurroundingElements: !showSurroundingElements
+      }),
+    [showSurroundingElements]
+  )
+
+  return (
+    <ThemeProvider colorMode="day" dayScheme={colorScheme}>
+      <div style={{display: 'flex', alignItems: 'center', position: 'relative'}}>
+        <Tooltip
+          aria-label={`${showSurroundingElements ? 'Hide' : 'Show'} surrounding elements`}
+          direction="s"
+          sx={{position: 'absolute'}}
+        >
+          <IconButton
+            key={TOOL_ID}
+            icon={AccessibilityIcon}
+            aria-pressed={!!showSurroundingElements}
+            aria-label={`${showSurroundingElements ? 'Hide' : 'Show'} surrounding elements`}
+            onClick={toggleMyTool}
+          />
+        </Tooltip>
+      </div>
+    </ThemeProvider>
+  )
+}
--- src/utils/story-helpers.tsx
+++ src/utils/story-helpers.tsx
@@ -8,7 +8,10 @@ import {Icon} from '@primer/octicons-react'
 
 // we don't import StoryContext from storybook because of exports that conflict
 // with primer/react more: https://github.com/primer/react/runs/6129115026?check_suite_focus=true
-type StoryContext = Record<string, unknown> & {globals: {colorScheme: string}; parameters: Record<string, unknown>}
+type StoryContext = Record<string, unknown> & {
+  globals: {colorScheme: string; showSurroundingElements: boolean}
+  parameters: Record<string, unknown>
+}
 
 type CheckboxOrRadioGroupWrapperArgs = ComponentProps<typeof CheckboxGroup>
 type CheckboxOrRadioGroupLabelArgs = ComponentProps<typeof CheckboxGroup.Label> & {
@@ -107,7 +110,8 @@ export const toolbarTypes = {
       icon: 'photo',
       items: [...Object.keys(theme.colorSchemes), 'all'],
       title: 'Color scheme'
-    }
+    },
+    showSurroundingElements: {}
   }
 }
 
@@ -346,4 +350,25 @@ export const OcticonArgType = (iconList: Icon[]) => {
     },
     mapping: icons
   }
+}
+
+export const withSurroundingElements = (
+  Story: React.FC<React.PropsWithChildren<StoryContext>>,
+  context: StoryContext
+) => {
+  return (
+    <>
+      {context.globals.showSurroundingElements ? (
+        <a href="https://github.com/primer/react">Primer documentation</a>
+      ) : (
+        ''
+      )}
+      {Story(context)}
+      {context.globals.showSurroundingElements ? (
+        <a href="https://github.com/primer/react">Primer documentation</a>
+      ) : (
+        ''
+      )}
+    </>
+  )
 }
