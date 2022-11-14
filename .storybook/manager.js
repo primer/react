c38cd6b974e74c1d97d72bf41fdf88a86ca55682
kendallgassner <kendallgassner@github.com>
2022-11-14 14:01:28 -0800

    update name

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.register(ADDON_ID, () => {
   // Register the tool
   addons.add(TOOL_ID, {
     type: types.TOOL,
-    title: 'My addon',
+    title: 'Show surrounding links',
     match: ({viewMode}) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
     render: Tool
   })
