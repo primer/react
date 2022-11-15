1c42fec3fd11d42a79f06ca619d40447c2d72d3f
kendallgassner <kendallgassner@github.com>
2022-11-15 14:03:06 -0800

    replace with IconButton

--- .storybook/src/accessibility-tool.tsx
+++ .storybook/src/accessibility-tool.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback} from 'react'
 import {useGlobals} from '@storybook/api'
-import {ThemeProvider, ToggleSwitch, Box} from '../../src'
+import {IconButton, Icons} from '@storybook/components'
 
 export const ADDON_ID = 'show-surrounding-links'
 export const TOOL_ID = `${ADDON_ID}/tool`
@@ -19,20 +19,15 @@ export const Tool = () => {
   }, [showSurroundingElements])
 
   return (
-    <ThemeProvider colorMode="day" dayScheme="light">
-      <Box style={{display: 'flex', alignItems: 'center'}}>
-        <Box fontSize={0} fontWeight="bold" id={`${ADDON_ID}-label`} color="black">
-          Links before/after
-        </Box>
-        <ToggleSwitch
-          key={TOOL_ID}
-          aria-labelledby={`${ADDON_ID}-label`}
-          onClick={toggleMyTool}
-          defaultChecked={showSurroundingElements ?? window?.localStorage.getItem('showSurroundingElements') === 'true'}
-          checked={showSurroundingElements}
-          size="small"
-        />
-      </Box>
-    </ThemeProvider>
+    <IconButton
+      key={TOOL_ID}
+      aria-label="Links before/after"
+      title="Links before/after"
+      onClick={toggleMyTool}
+      defaultChecked={showSurroundingElements ?? window?.localStorage.getItem('showSurroundingElements') === 'true'}
+      aria-selected={showSurroundingElements}
+    >
+      <Icons icon="accessibilityalt" />
+    </IconButton>
   )
 }
