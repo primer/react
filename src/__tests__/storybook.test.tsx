5efd9e5bdbaff704a3b96edf667086e7fe65f868
Cole Bemis <colebemis@github.com>
2022-11-16 15:54:22 -0800

    Assert that component story titles start with Component/

--- src/__tests__/storybook.test.tsx
+++ src/__tests__/storybook.test.tsx
@@ -4,14 +4,20 @@ import glob from 'fast-glob'
 // Components opted into the new story format
 const components = ['TreeView']
 
-const storyFiles = glob
+const componentStoryFiles = glob
   .sync('src/**/*.stories.tsx')
   .filter(file => components.some(component => file.includes(component)))
   .filter(file => !file.endsWith('.features.stories.tsx'))
 
-for (const storyFile of storyFiles) {
+for (const storyFile of componentStoryFiles) {
   describe(`${storyFile}`, () => {
     const storyFileContents = fs.readFileSync(storyFile, 'utf8')
+
+    // TODO: Replace regex with a proper parser
+
+    test('title starts with "Components/"', () => {
+      expect(storyFileContents).toMatch(/title: 'Components\//)
+    })
 
     test('exports a Default story', () => {
       expect(storyFileContents).toMatch(/export const Default/)
