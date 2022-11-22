f3caadeca1cbbcbc279ba34adb179db05b735c96
Keith Cirkel <keithamus@users.noreply.github.com>
2022-11-22 18:19:04 +0000

    add event stub

--- src/polyfills/custom-elements.ts
+++ src/polyfills/custom-elements.ts
@@ -8,3 +8,4 @@ root.customElements ??= {
   }
 } as unknown as CustomElementRegistry
 root.HTMLElement ??= class HTMLElement {} as unknown as typeof HTMLElement
+root.Event ??= class Event {} as unknown as typeof Event
