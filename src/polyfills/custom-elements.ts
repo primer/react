8a8d75fa773bc1c40c809f6dc2dc85a9dde1146d
Keith Cirkel <keithamus@users.noreply.github.com>
2022-11-23 09:53:49 +0000

    drop event stub

--- src/polyfills/custom-elements.ts
+++ src/polyfills/custom-elements.ts
@@ -8,4 +8,3 @@ root.customElements ??= {
   }
 } as unknown as CustomElementRegistry
 root.HTMLElement ??= class HTMLElement {} as unknown as typeof HTMLElement
-root.Event ??= class Event {} as unknown as typeof Event
