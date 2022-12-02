7a2bac21ab57da242e54a31b71a5ffc209b14806
Josh Black <josh@josh.black>
2022-12-02 11:55:31 -0600

    chore: update moduleNameMapper for React 17

--- jest.config.js
+++ jest.config.js
@@ -6,21 +6,17 @@ const useReact18 = process.env.REACT_18 === 'true'
 module.exports = {
   testEnvironment: 'jsdom',
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/stories/**', '!**/*.stories.{js,jsx,ts,tsx}'],
-  moduleNameMapper: {
-    '^react$': useReact18 ? require.resolve('react-18') : require.resolve('react'),
-    '^react-dom$': useReact18 ? require.resolve('react-dom-18') : require.resolve('react-dom'),
-    '^react-dom/client$': useReact18 ? require.resolve('react-dom-18/client') : require.resolve('react-dom/client'),
-    '^react-dom/server$': useReact18 ? require.resolve('react-dom-18/server') : require.resolve('react-dom/server'),
-    '^react-dom/test-utils$': useReact18
-      ? require.resolve('react-dom-18/test-utils')
-      : require.resolve('react-dom/test-utils'),
-    '^react-test-renderer$': useReact18
-      ? require.resolve('react-test-renderer-18')
-      : require.resolve('react-test-renderer'),
-    '^@testing-library/react$': useReact18
-      ? require.resolve('@testing-library/react-18')
-      : require.resolve('@testing-library/react')
-  },
+  moduleNameMapper: useReact18
+    ? {
+        '^react$': require.resolve('react-18'),
+        '^react-dom$': require.resolve('react-dom-18'),
+        '^react-dom/client$': require.resolve('react-dom-18/client'),
+        '^react-dom/server$': require.resolve('react-dom-18/server'),
+        '^react-dom/test-utils$': require.resolve('react-dom-18/test-utils'),
+        '^react-test-renderer$': require.resolve('react-test-renderer-18'),
+        '^@testing-library/react$': require.resolve('@testing-library/react-18')
+      }
+    : {},
   setupFiles: ['<rootDir>/src/utils/test-helpers.tsx'],
   setupFilesAfterEnv: ['<rootDir>/src/utils/test-matchers.tsx', '<rootDir>/src/utils/test-deprecations.tsx'],
   testMatch: ['<rootDir>/(src|codemods)/**/*.test.[jt]s?(x)', '!**/*.types.test.[jt]s?(x)'],
