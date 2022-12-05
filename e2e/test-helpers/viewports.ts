bb4509f213b97c6c6e2d3ceccd3292c2e6fce3a4
Armağan <broccolinisoup@github.com>
2022-12-06 09:28:28 +1000

    test(e2e): add UnderlineNav interaction tests (#2632)
    * test: add interaction tests for underlineNav
    
    * Add @vrt tag to the test
    
    * test(vrt): update snapshots
    
    * run prettier for diff files to set trailing comma to es5
    
    Co-authored-by: broccolinisoup <broccolinisoup@users.noreply.github.com>

---
+++ e2e/test-helpers/viewports.ts
@@ -0,0 +1,7 @@
+export const viewports: {[key: string]: number} = {
+  'primer.breakpoint.xs': 544,
+  'primer.breakpoint.sm': 768,
+  'primer.breakpoint.md': 1012,
+  'primer.breakpoint.lg': 1280,
+  'primer.breakpoint.xl': 1400,
+}
