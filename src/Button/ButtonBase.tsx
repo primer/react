d28127f9ce642428b17475625124762bf0fc3fae
kendallgassner <kendallgassner@github.com>
2022-12-07 09:50:10 -0800

    use local mergeRef

--- src/Button/ButtonBase.tsx
+++ src/Button/ButtonBase.tsx
@@ -1,11 +1,11 @@
-import React, {ComponentPropsWithRef, forwardRef, useMemo} from 'react'
-import {mergeRefs} from 'react-merge-refs'
+import React, {ComponentPropsWithRef, ForwardedRef, forwardRef, useMemo} from 'react'
 import {ForwardRefComponent as PolymorphicForwardRefComponent} from '../utils/polymorphic'
 import Box from '../Box'
 import {merge, SxProp} from '../sx'
 import {useTheme} from '../ThemeProvider'
 import {ButtonProps, StyledButton} from './types'
 import {getVariantStyles, getSizeStyles, getButtonStyles} from './styles'
+import {useRefObjectAsForwardedRef} from '../hooks/useRefObjectAsForwardedRef'
 
 const defaultSxProp = {}
 const iconWrapStyles = {
@@ -19,7 +19,9 @@ const trailingIconStyles = {
 const ButtonBase = forwardRef<HTMLElement, ButtonProps>(
   ({children, as: Component = 'button', sx: sxProp = defaultSxProp, ...props}, forwardedRef): JSX.Element => {
     const {leadingIcon: LeadingIcon, trailingIcon: TrailingIcon, variant = 'default', size = 'medium', ...rest} = props
-    const innerRef = React.useRef<HTMLElement>()
+    const innerRef = React.useRef<HTMLElement>(null)
+    useRefObjectAsForwardedRef(forwardedRef, innerRef)
+
     const {theme} = useTheme()
     const baseStyles = useMemo(() => {
       return merge.all([getButtonStyles(theme), getSizeStyles(size, variant, false), getVariantStyles(variant, theme)])
@@ -36,7 +38,7 @@ const ButtonBase = forwardRef<HTMLElement, ButtonProps>(
     }, [innerRef])
 
     return (
-      <StyledButton as={Component} sx={sxStyles} {...rest} ref={mergeRefs([innerRef, forwardedRef])}>
+      <StyledButton as={Component} sx={sxStyles} {...rest} ref={innerRef}>
         {LeadingIcon && (
           <Box as="span" data-component="leadingIcon" sx={iconWrapStyles}>
             <LeadingIcon />
