23b51380466a11577cbe02a0d10a92ef49aba02d
Jeremy Neal <hello@radglob.com>
2022-12-07 14:54:39 -0500

    Added story to recreate issue.

--- src/stories/Tooltip.stories.tsx
+++ src/stories/Tooltip.stories.tsx
@@ -4,6 +4,7 @@ import {BaseStyles, ThemeProvider, IconButton} from '..'
 import Box from '../Box'
 import Tooltip from '../Tooltip'
 import {SearchIcon} from '@primer/octicons-react'
+import {Placeholder} from '../Placeholder'
 
 export default {
   title: 'Components/Tooltip/Default',
@@ -34,4 +35,55 @@ export const IconButtonTooltip = () => (
       <IconButton icon={SearchIcon} aria-label="Search" />
     </Tooltip>
   </Box>
+)
+
+export const TooltipWithAncestor = () => (
+  <div className="App">
+    <Box>
+      <Box
+        sx={{
+          background: '#a8a8a8',
+          zIndex: 1,
+          position: 'sticky',
+          top: 0,
+          p: 4,
+        }}
+      >
+        This is a sticky header
+      </Box>
+      <Box
+        sx={{
+          p: 3,
+          // this creates a new "stacking context" so that this whole content section falls behind the main sticky header
+          position: 'relative',
+          zIndex: 0,
+        }}
+      >
+        <Box
+          sx={{
+            p: 1,
+            position: 'sticky',
+            top: 82,
+            // because content is position: relative, we need to set a higher z-index on this header
+            zIndex: 1,
+            background: 'cornflowerblue',
+          }}
+        >
+          <Tooltip aria-label="Tooltip currently falls behind the top sticky header">
+            Second sticky header, Text with a tooltip, within a stacking context
+          </Tooltip>
+        </Box>
+        <Box
+          sx={{
+            height: 500,
+            // having content that is position: relative can change stacking order with sticky headers
+            position: 'relative',
+          }}
+        >
+          Content goes here, falls behind the sticky headers
+        </Box>
+      </Box>
+    </Box>
+    <Box sx={{height: 1000}}>Extra padding that pushes the sticky header up</Box>
+  </div>
 )
