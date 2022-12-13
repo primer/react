95ba079070cfdd01931f64418fbfa288a0474a97
Josh Black <joshblack@github.com>
2022-12-13 12:19:48 -0600

    fix(Radio): remove redundant aria-disabled (#2635)
    * fix(Radio): remove redundant aria-disabled
    
    * test(e2e): refactor toHaveNoViolations to use axe-core directly for custom rules
    
    * test: update Radio test snapshots
    
    * chore: add changeset
    
    * test(vrt): update snapshots
    
    * test: update snapshots
    
    * chore: run prettier
    
    Co-authored-by: joshblack <joshblack@users.noreply.github.com>

---
+++ .changeset/seven-chefs-confess.md
@@ -0,0 +1,5 @@
+---
+'@primer/react': patch
+---
+
+Update Radio to only use disabled when provided and no longer set aria-disabled
--- e2e/test-helpers/storybook.ts
+++ e2e/test-helpers/storybook.ts
@@ -3,18 +3,26 @@ import {waitForImages} from './waitForImages'
 
 interface Options {
   id: string
+  args?: Record<string, string | boolean>
   globals?: Record<string, string>
 }
 
 const {STORYBOOK_URL = 'http://localhost:6006'} = process.env
 
 export async function visit(page: Page, options: Options) {
-  const {id, globals} = options
+  const {id, args, globals} = options
   // In CI, the static server strips `.html` extensions
   const url = process.env.CI ? new URL(`${STORYBOOK_URL}/iframe`) : new URL(`${STORYBOOK_URL}/iframe.html`)
 
   url.searchParams.set('id', id)
   url.searchParams.set('viewMode', 'story')
+
+  if (args) {
+    const serialized = Object.entries(args)
+      .map(([key, value]) => `${key}:${value}`)
+      .join(',')
+    url.searchParams.set('args', serialized)
+  }
 
   if (globals) {
     let params = ''
