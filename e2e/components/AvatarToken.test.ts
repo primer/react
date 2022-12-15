27381eb9e5b6cb4577fefdfa026116b9edbd354c
Josh Black <joshblack@users.noreply.github.com>
2022-12-15 17:51:35 -0600

    test(e2e): add AvatarToken test

---
+++ e2e/components/AvatarToken.test.ts
@@ -0,0 +1,91 @@
+import {test, expect} from '@playwright/test'
+import {visit} from '../test-helpers/storybook'
+import {themes} from '../test-helpers/themes'
+import {matrix, serialize} from '../test-helpers/props'
+
+const scenarios = matrix({
+  size: ['small', 'medium', 'large', 'xlarge', 'extralarge'],
+  isSelected: [true, false],
+})
+
+test.describe('AvatarToken', () => {
+  test.describe('Default', () => {
+    for (const theme of themes) {
+      test.describe(theme, () => {
+        for (const args of scenarios) {
+          const suffix = serialize(args)
+
+          test.describe(suffix, () => {
+            test(`default @vrt`, async ({page}) => {
+              await visit(page, {
+                id: 'components-avatartoken--default-token',
+                globals: {
+                  colorScheme: theme,
+                },
+                args,
+              })
+
+              // Default state
+              expect(await page.screenshot()).toMatchSnapshot(`AvatarToken.Default.${theme}.${suffix}.png`)
+            })
+
+            test('axe @aat', async ({page}) => {
+              await visit(page, {
+                id: 'components-avatartoken--default-token',
+                globals: {
+                  colorScheme: theme,
+                },
+                args,
+              })
+              await expect(page).toHaveNoViolations({
+                rules: {
+                  'color-contrast': {
+                    enabled: theme !== 'dark_dimmed',
+                  },
+                },
+              })
+            })
+          })
+        }
+      })
+    }
+  })
+
+  test.describe('Interactive', () => {
+    for (const theme of themes) {
+      test.describe(theme, () => {
+        test('default @vrt', async ({page}) => {
+          await visit(page, {
+            id: 'components-avatartoken--interactive',
+            globals: {
+              colorScheme: theme,
+            },
+          })
+
+          // Default state
+          expect(await page.screenshot()).toMatchSnapshot(`AvatarToken.Interactive.${theme}.png`)
+
+          // Hover state
+          await page.hover('button')
+          expect(await page.screenshot()).toMatchSnapshot(`AvatarToken.Interactive.${theme}.hover.png`)
+        })
+
+        test('axe @aat', async ({page}) => {
+          await visit(page, {
+            id: 'components-avatartoken--default-token',
+            globals: {
+              colorScheme: theme,
+            },
+          })
+          await expect(page).toHaveNoViolations({
+            rules: {
+              'color-contrast': {
+                enabled: theme !== 'dark_dimmed',
+              },
+            },
+          })
+        })
+      })
+    }
+  })
+})
