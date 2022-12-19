9ae31a742bffc545bfd3abf858bbc9be9715c390
Jeremy Neal <radglob@github.com>
2022-12-19 09:58:57 -0500

    Update src/PageLayout/PageLayout.tsx
    Co-authored-by: Cole Bemis <colebemis@github.com>

--- src/PageLayout/PageLayout.tsx
+++ src/PageLayout/PageLayout.tsx
@@ -676,7 +676,7 @@ const Pane = React.forwardRef<HTMLDivElement, React.PropsWithChildren<PageLayout
               ? ['100%', null, 'clamp(var(--pane-min-width), var(--pane-width), var(--pane-max-width))']
               : paneWidths[width],
             padding: SPACING_MAP[padding],
-            overflow: [null, 'auto'],
+            overflow: [null, null, 'auto'],
 
             [`@media screen and (min-width: ${theme.breakpoints[3]})`]: {
               '--pane-max-width': 'calc(100vw - 959px)',
