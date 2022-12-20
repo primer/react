44b4f35db7678149dccf3378979543c636e4b0cb
langermank <18661030+langermank@users.noreply.github.com>
2022-12-19 16:39:20 -0800

    add back aria label

--- src/stories/ActionMenu/examples.stories.tsx
+++ src/stories/ActionMenu/examples.stories.tsx
@@ -288,7 +288,7 @@ export function MixedSelection(): JSX.Element {
       </p>
 
       <ActionMenu>
-        <ActionMenu.Button leadingIcon={selectedOption && selectedOption.icon}>
+        <ActionMenu.Button aria-label="Group by" leadingIcon={selectedOption && selectedOption.icon}>
           {selectedOption ? `Group by ${selectedOption.text}` : 'Group items by'}
         </ActionMenu.Button>
         <ActionMenu.Overlay width="medium">
