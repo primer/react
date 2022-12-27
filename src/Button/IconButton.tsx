d356be836c1d16666d4e952c21e71e4318749f46
Matthew Costabile <mattcosta7@github.com>
2022-12-27 15:21:59 -0500

    Update types for IconButton, ActionMenu.Button, TabNav.Link and internal consumers (#2677)
    * update types for many buttony things
    
    * update anchor type
    
    * changeset
    
    * avoid a type assignment for button component
    
    * use default sx propr broader
    
    * slight type tweak
    
    * fixup invalid types
    
    * scope readonly to dev
    
    * update tabnavlink to work with button props
    
    * explitly allow href, even if as doesnt support it
    
    * lint
    
    * fix unused
    
    * fix types
    
    * remove a prop re-added in a merge

--- src/Button/IconButton.tsx
+++ src/Button/IconButton.tsx
@@ -1,18 +1,27 @@
 import React, {forwardRef} from 'react'
-import {merge, SxProp} from '../sx'
+import {merge, BetterSystemStyleObject} from '../sx'
 import {useTheme} from '../ThemeProvider'
 import {IconButtonProps, StyledButton} from './types'
 import {getBaseStyles, getVariantStyles} from './styles'
+import {ForwardRefComponent as PolymorphicForwardRefComponent} from '../utils/polymorphic'
+import {defaultSxProp} from '../utils/defaultSxProp'
 
-const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, forwardedRef): JSX.Element => {
-  const {variant = 'default', size = 'medium', sx: sxProp = {}, icon: Icon, ...rest} = props
+const IconButton = forwardRef((props, forwardedRef): JSX.Element => {
+  const {variant = 'default', size = 'medium', sx: sxProp = defaultSxProp, icon: Icon, ...rest} = props
   const {theme} = useTheme()
-  const sxStyles = merge.all([getBaseStyles(theme), getVariantStyles(variant, theme), sxProp as SxProp])
+  const sxStyles = merge.all<BetterSystemStyleObject>([getBaseStyles(theme), getVariantStyles(variant, theme), sxProp])
   return (
-    <StyledButton data-component="IconButton" data-size={size} sx={sxStyles} {...rest} ref={forwardedRef}>
+    <StyledButton
+      data-component="IconButton"
+      data-size={size}
+      sx={sxStyles}
+      {...rest}
+      // @ts-expect-error StyledButton wants both Anchor and Button refs
+      ref={forwardedRef}
+    >
       <Icon />
     </StyledButton>
   )
-})
+}) as PolymorphicForwardRefComponent<'button' | 'a', IconButtonProps>
 
 export {IconButton}
