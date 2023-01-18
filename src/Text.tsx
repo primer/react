6bcbd11c19579866af544920350b9bbdcc6203ef
Tyler Jones <tylerjdev@Tylers-MBP.lan>
2023-01-18 18:10:38 -0500

    Add lint fixes

--- src/Text.tsx
+++ src/Text.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import styled from 'styled-components'
 import {COMMON, SystemCommonProps, SystemTypographyProps, TYPOGRAPHY} from './constants'
 import sx, {SxProp} from './sx'
 import {ComponentProps} from './utils/types'
 
 const RestrictedAs = {
-  inlineText: ['span', 'p', 'b', 'i', 'em', 'small', 'strong', 'u', 'code', 'q', 's'],
+  inlineText: ['span', 'p', 'b', 'i', 'em', 'small', 'strong', 'u', 'code', 'q', 's', 'label', 'div'],
 } as const
 
 const StyledText = styled.span<SystemTypographyProps & SystemCommonProps & SxProp>`
