35b7397a31d1190215a875ff6155ba23a4253f4a
Tyler Jones <tylerjdev@Tylers-MacBook-Pro.local>
2023-01-20 13:49:53 -0500

    Use forwardRef, add console warning, adjust TS usage

--- src/Text.tsx
+++ src/Text.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import {COMMON, SystemCommonProps, SystemTypographyProps, TYPOGRAPHY} from './constants'
+import {ForwardRefComponent as PolymorphicForwardRefComponent} from './utils/polymorphic'
 import sx, {SxProp} from './sx'
 
-const RestrictedAs = {
+const restrictedAs = {
   inlineText: ['span', 'p', 'b', 'i', 'em', 'small', 'strong', 'u', 'code', 'q', 's', 'label', 'div'],
 } as const
 
@@ -14,15 +15,23 @@ const StyledText = styled.span<SystemTypographyProps & SystemCommonProps & SxPro
 `
 
 export type TextProps = {
-  as?: typeof RestrictedAs.inlineText[number]
+  as?: typeof restrictedAs.inlineText[number]
 } & React.ComponentProps<'span' & typeof StyledText>
 
-const Text = ({children, as = 'span', ...rest}: TextProps) => {
+const Text = React.forwardRef(({children, as = 'span', ...rest}, forwardedRef) => {
+  if (__DEV__ && !restrictedAs.inlineText.includes(as)) {
+    console.warn(
+      `The usage of \`as\` on the \`<Text>\` component can only be one of following element types: <${restrictedAs.inlineText.join(
+        '>, <',
+      )}>. Defaults to \`<span>\`.`,
+    )
+  }
+
   return (
-    <StyledText as={RestrictedAs.inlineText.includes(as) ? as : undefined} {...rest}>
+    <StyledText ref={forwardedRef} as={restrictedAs.inlineText.includes(as) ? as : undefined} {...rest}>
       {children}
     </StyledText>
   )
-}
+}) as PolymorphicForwardRefComponent<'span', TextProps>
 
 export default Text
