4258011dd1532df3b3eda38d3f1de6eca7835335
langermank <18661030+langermank@users.noreply.github.com>
2023-01-19 16:29:53 -0800

    add more stories

--- src/Button/IconButton.features.stories.tsx
+++ src/Button/IconButton.features.stories.tsx
@@ -1,6 +1,7 @@
 import {HeartIcon, ChevronDownIcon, ChevronUpIcon} from '@primer/octicons-react'
 import React from 'react'
 import {IconButton} from '.'
+import {Button} from '../Button'
 
 export default {
   title: 'Components/IconButton/Features',
@@ -24,7 +25,24 @@ export const Large = () => <IconButton size="large" icon={HeartIcon} aria-label=
 
 export const ChevronTest = () => (
   <>
-    <IconButton size="medium" icon={ChevronDownIcon} aria-label="Default" />
-    <IconButton size="medium" icon={ChevronUpIcon} aria-label="Default" />
+    <div style={{marginBottom: '2rem'}}>
+      <IconButton size="medium" variant="invisible" icon={ChevronDownIcon} aria-label="Default" />
+      <IconButton size="medium" variant="invisible" icon={ChevronUpIcon} aria-label="Default" />
+    </div>
+    <div style={{marginBottom: '2rem'}}>
+      <IconButton size="medium" variant="default" icon={ChevronDownIcon} aria-label="Default" />
+      <IconButton size="medium" variant="default" icon={ChevronUpIcon} aria-label="Default" />
+    </div>
+    <div style={{marginBottom: '2rem'}}>
+      <IconButton size="medium" variant="default" icon={ChevronDownIcon} aria-label="Default" />
+    </div>
+    <div style={{marginBottom: '2rem'}}>
+      <IconButton size="medium" variant="default" icon={ChevronUpIcon} aria-label="Default" />
+    </div>
+    <div style={{marginBottom: '2rem'}}>
+      <Button size="medium" variant="default" trailingIcon={ChevronDownIcon}>
+        Button
+      </Button>
+    </div>
   </>
 )
