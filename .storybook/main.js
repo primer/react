81c6a27c8b945aebe904b75c44df48394ab49a2e
Josh Black <joshblack@users.noreply.github.com>
2023-01-23 17:11:09 -0600

    chore: import type from framework package

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,30 @@
 /**
- * @type {import('@storybook/core-common').StorybookConfig}
+ * @type {import('@storybook/react-webpack5').StorybookConfig}
  */
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [{
-    name: '@storybook/addon-essentials',
-    options: {
-      backgrounds: false
-    }
-  }, '@storybook/addon-storysource', '@storybook/addon-interactions', '@storybook/addon-a11y', '@storybook/addon-links', {
-    name: 'storybook-addon-turbo-build',
-    options: {
-      optimizationLevel: 2
-    }
-  }],
+  addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+      },
+    },
+    '@storybook/addon-storysource',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    {
+      name: 'storybook-addon-turbo-build',
+      options: {
+        optimizationLevel: 2,
+      },
+    },
+  ],
   features: {
     interactionsDebugger: true,
     storyStoreV7: true,
-    buildStoriesJson: true
+    buildStoriesJson: true,
   },
   framework: {
     name: '@storybook/react-webpack5',
@@ -25,12 +32,12 @@ module.exports = {
       fastRefresh: true,
       strictMode: true,
       builder: {
-        fsCache: true
-      }
-    }
+        fsCache: true,
+      },
+    },
   },
   reactOptions: {
     fastRefresh: true,
-    strictMode: true
-  }
-};
+    strictMode: true,
+  },
+}
