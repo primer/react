99156441b873c9c50f3caba2b3438f0cef74e5e0
Cole Bemis <colebemis@github.com>
2023-01-24 13:10:07 -0700

    Fix Label as prop regression

--- src/Label.tsx
+++ src/Label.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import {variant} from 'styled-system'
-import sx, {SxProp, BetterSystemStyleObject} from './sx'
 import {get} from './constants'
+import sx, {BetterSystemStyleObject, SxProp} from './sx'
+import {ForwardRefComponent as PolymorphicForwardRefComponent} from './utils/polymorphic'
 
 type StyledLabelProps = {
   /** The color of the label */
@@ -77,7 +78,7 @@ const sizes: Record<LabelSizeKeys, BetterSystemStyleObject> = {
   },
 }
 
-const StyledLabel = styled.span<LabelProps>`
+const StyledLabel = styled.span<StyledLabelProps>`
   align-items: center;
   background-color: transparent;
   border-width: 1px;
@@ -93,10 +94,9 @@ const StyledLabel = styled.span<LabelProps>`
   ${sx};
 `
 
-export type LabelProps = StyledLabelProps & React.ComponentPropsWithoutRef<'span'>
-const Label = React.forwardRef<HTMLSpanElement, LabelProps>(({size = 'small', variant = 'default', ...rest}, ref) => (
+const Label = React.forwardRef(({size = 'small', variant = 'default', ...rest}, ref) => (
   <StyledLabel ref={ref} size={size} variant={variant} {...rest} />
-))
+)) as PolymorphicForwardRefComponent<'span', StyledLabelProps>
 
 Label.displayName = 'Label'
 
