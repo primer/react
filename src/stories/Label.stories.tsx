8365da0a11b110a332ef7be968dcecb4a5f6ba30
langermank <18661030+langermank@users.noreply.github.com>
2023-01-26 13:32:34 -0800

    Co-authored-by: Josh Black <joshblack@users.noreply.github.com>

--- src/stories/Label.stories.tsx
+++ src/stories/Label.stories.tsx
@@ -1,54 +1,42 @@
 import React from 'react'
-import {Meta} from '@storybook/react'
-import {BaseStyles, ThemeProvider} from '..'
-import {ComponentProps} from '../utils/types'
+import {Meta, Story} from '@storybook/react'
+// import {ComponentProps} from '../utils/types'
 import Label from '../Label'
 
-type Args = ComponentProps<typeof Label>
+// type Args = ComponentProps<typeof Label>
 
 export default {
   title: 'Components/Label',
   component: Label,
   args: {
-    variant: 'default',
-    size: 'large',
+    //   variant: 'default',
+    //   size: 'large',
   },
   argTypes: {
-    variant: {
-      control: {
-        type: 'select',
-      },
-      options: [
-        'default',
-        'primary',
-        'secondary',
-        'accent',
-        'success',
-        'attention',
-        'severe',
-        'danger',
-        'done',
-        'sponsors',
-      ],
-    },
-    size: {
-      control: {
-        type: 'radio',
-      },
-      options: ['small', 'large'],
-    },
+    //   variant: {
+    //     control: {
+    //       type: 'select',
+    //     },
+    //     options: [
+    //       'default',
+    //       'primary',
+    //       'secondary',
+    //       'accent',
+    //       'success',
+    //       'attention',
+    //       'severe',
+    //       'danger',
+    //       'done',
+    //       'sponsors',
+    //     ],
+    //   },
+    //   size: {
+    //     control: {
+    //       type: 'radio',
+    //     },
+    //     options: ['small', 'large'],
+    //   },
   },
-  decorators: [
-    Story => {
-      return (
-        <ThemeProvider>
-          <BaseStyles>
-            <Story />
-          </BaseStyles>
-        </ThemeProvider>
-      )
-    },
-  ],
-} as Meta
+} as Meta<typeof Label>
 
-export const label = (args: Args) => <Label {...args}>Label</Label>
+export const Playground: Story<typeof Label> = args => <Label {...args}>Label</Label>
