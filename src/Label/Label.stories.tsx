d20febbfc3b4231b46430e016d4a10a9e4756a55
langermank <18661030+langermank@users.noreply.github.com>
2023-01-27 07:13:48 -0800

    move args

--- src/Label/Label.stories.tsx
+++ src/Label/Label.stories.tsx
@@ -6,44 +6,46 @@ import Label from './Label'
 export default {
   title: 'Components/Label',
   component: Label,
-  args: {
-    variant: 'default',
-    size: 'small',
-  },
-  argTypes: {
-    ref: {
-      control: false,
-      table: {
-        disable: true,
-      },
-    },
-    as: {
-      control: false,
-      table: {
-        disable: true,
-      },
-    },
-    theme: {
-      control: false,
-      table: {
-        disable: true,
-      },
-    },
-    forwardedAs: {
-      control: false,
-      table: {
-        disable: true,
-      },
-    },
-    sx: {
-      control: false,
-      table: {
-        disable: true,
-      },
-    },
-  },
+  argTypes: {},
 } as Meta<ComponentProps<typeof Label>>
 
 export const Playground: Story<ComponentProps<typeof Label>> = args => <Label {...args}>Label</Label>
+
+Playground.args = {
+  variant: 'default',
+  size: 'small',
+}
+Playground.argTypes = {
+  ref: {
+    control: false,
+    table: {
+      disable: true,
+    },
+  },
+  as: {
+    control: false,
+    table: {
+      disable: true,
+    },
+  },
+  theme: {
+    control: false,
+    table: {
+      disable: true,
+    },
+  },
+  forwardedAs: {
+    control: false,
+    table: {
+      disable: true,
+    },
+  },
+  sx: {
+    control: false,
+    table: {
+      disable: true,
+    },
+  },
+}
 
 export const Default = () => <Label>Default</Label>
