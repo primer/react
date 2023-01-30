e24716539e1dcae92e6d07ab3e64abc77482e8ad
Tyler Jones <tylerjdev@Tylers-MBP.lan>
2023-01-30 16:20:02 -0500

    Add more elements to restrictedAs array

--- src/Text.tsx
+++ src/Text.tsx
@@ -4,9 +4,25 @@ import {COMMON, SystemCommonProps, SystemTypographyProps, TYPOGRAPHY} from './co
 import {ForwardRefComponent as PolymorphicForwardRefComponent} from './utils/polymorphic'
 import sx, {SxProp} from './sx'
 
-const restrictedAs = {
-  inlineText: ['span', 'p', 'b', 'i', 'em', 'small', 'strong', 'u', 'code', 'q', 's', 'label', 'div'],
-} as const
+const restrictedAs = [
+  'span',
+  'p',
+  'b',
+  'i',
+  'em',
+  'small',
+  'strong',
+  'u',
+  'code',
+  'q',
+  's',
+  'label',
+  'ins',
+  'del',
+  'mark',
+  'kbd',
+  'abbr',
+] as const
 
 const StyledText = styled.span<SystemTypographyProps & SystemCommonProps & SxProp>`
   ${TYPOGRAPHY};
@@ -15,20 +31,20 @@ const StyledText = styled.span<SystemTypographyProps & SystemCommonProps & SxPro
 `
 
 export type TextProps = {
-  as?: typeof restrictedAs.inlineText[number]
+  as?: typeof restrictedAs[number]
 } & React.ComponentProps<'span' & typeof StyledText>
 
 const Text = React.forwardRef(({children, as = 'span', ...rest}, forwardedRef) => {
-  if (__DEV__ && !restrictedAs.inlineText.includes(as)) {
+  if (__DEV__ && !restrictedAs.includes(as)) {
     console.warn(
-      `The usage of \`as\` on the \`<Text>\` component can only be one of following element types: <${restrictedAs.inlineText.join(
+      `The usage of \`as\` on the \`<Text>\` component can only be one of following element types: <${restrictedAs.join(
         '>, <',
       )}>. Defaults to \`<span>\`.`,
     )
   }
 
   return (
-    <StyledText ref={forwardedRef} as={restrictedAs.inlineText.includes(as) ? as : undefined} {...rest}>
+    <StyledText ref={forwardedRef} as={restrictedAs.includes(as) ? as : undefined} {...rest}>
       {children}
     </StyledText>
   )
