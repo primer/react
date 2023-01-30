1fda9b72af84ad2f81d9cf0e6a6828f582ca789a
Josh Black <joshblack@users.noreply.github.com>
2023-01-30 17:24:21 -0600

    chore: update stories

--- src/stories/deprecated/Button.stories.tsx
+++ src/stories/deprecated/Button.stories.tsx
@@ -41,29 +41,42 @@ export default {
   },
 } as Meta
 
-export const DefaultButton = (args: StrictButtonStyleProps) => <Button {...args}>Default Button</Button>
-export const DangerButton = (args: StrictButtonStyleProps) => <ButtonDanger {...args}>Danger Button</ButtonDanger>
-export const OutlineButton = (args: StrictButtonStyleProps) => <ButtonOutline {...args}>Outline Button</ButtonOutline>
-export const PrimaryButton = (args: StrictButtonStyleProps) => <ButtonPrimary {...args}>Primary Button</ButtonPrimary>
-export const InvisibleButton = (args: StrictButtonStyleProps) => (
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const defaultButton = (args: StrictButtonStyleProps) => <Button {...args}>Default Button</Button>
+defaultButton.args = {variant: 'medium'}
+
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const dangerButton = (args: StrictButtonStyleProps) => <ButtonDanger {...args}>Danger Button</ButtonDanger>
+dangerButton.args = {variant: 'medium'}
+
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const outlineButton = (args: StrictButtonStyleProps) => <ButtonOutline {...args}>Outline Button</ButtonOutline>
+outlineButton.args = {variant: 'medium'}
+
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const primaryButton = (args: StrictButtonStyleProps) => <ButtonPrimary {...args}>Primary Button</ButtonPrimary>
+primaryButton.args = {variant: 'medium'}
+
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const invisibleButton = (args: StrictButtonStyleProps) => (
   <ButtonInvisible {...args}>Invisible Button</ButtonInvisible>
 )
+invisibleButton.args = {variant: 'medium'}
 
-export const CloseButton = (args: ButtonStyleProps) => (
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const closeButton = (args: ButtonStyleProps) => (
   <ButtonClose {...args} onClick={() => alert('button clicked.')} />
 )
-export const ButtonTableList = (args: ButtonStyleProps) => (
+closeButton.args = {variant: 'medium'}
+
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const buttonTableList = (args: ButtonStyleProps) => (
   <ButtonTableList {...args}>Button Table List</ButtonTableList>
 )
-export const DisabledButton = (args: StrictButtonStyleProps) => {
+buttonTableList.args = {variant: 'medium'}
+
+// eslint-disable-next-line storybook/prefer-pascal-case
+export const disabledButton = (args: StrictButtonStyleProps) => {
   const props = {disabled: true, ...args}
   return <Button {...props}>Disabled</Button>
 }
-
-DefaultButton.args = {variant: 'medium'}
-DangerButton.args = {variant: 'medium'}
-OutlineButton.args = {variant: 'medium'}
-PrimaryButton.args = {variant: 'medium'}
-InvisibleButton.args = {variant: 'medium'}
-CloseButton.args = {variant: 'medium'}
-ButtonTableList.args = {variant: 'medium'}
