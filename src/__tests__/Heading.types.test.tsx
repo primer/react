5055b91b9cf40e163b9b19fa4a5bcc707adae329
Amanda Brown <amanda.brown@testdouble.com>
2023-01-30 13:22:36 -0500

    1572 accessibility request primer react heading enforce what as can be set to with a runtime validation rule (#2768)
    * fix(Heading): as prop fix, testing type and logging
    
    * chore: add another changeset
    
    * Delete small-bikes-carry.md
    
    Not needed
    
    * chore(Heading): resolved type check error from test

--- src/__tests__/Heading.types.test.tsx
+++ src/__tests__/Heading.types.test.tsx
@@ -9,3 +9,8 @@ export function shouldNotAcceptSystemProps() {
   // @ts-expect-error system props should not be accepted
   return <Heading backgroundColor="thistle" />
 }
+
+export function shouldNotAcceptInvalidAsProp() {
+  // @ts-expect-error as prop should not be accepted
+  return <Heading as="p" />
+}
