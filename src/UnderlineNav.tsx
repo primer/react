eb2ac5b474c2ab61cd40bf90fe75958d9d325f08
Armagan Ersoz <broccolinisoup@github.com>
2023-01-30 14:15:04 +1000

    Issue @deprecate notice for the UnderlineNav v1

--- src/UnderlineNav.tsx
+++ src/UnderlineNav.tsx
@@ -111,4 +111,7 @@ const UnderlineNavLink = styled.a.attrs<StyledUnderlineNavLinkProps>(props => ({
 UnderlineNavLink.displayName = 'UnderlineNav.Link'
 
 export type UnderlineNavLinkProps = ComponentProps<typeof UnderlineNavLink>
+/**
+ * @deprecated UnderlineNav to be deprecated in the next major Primer React release. Please use the new responsive and accessible UnderlineNav. See https://primer.style/react/drafts/UnderlineNav2 for more details.
+ */
 export default Object.assign(UnderlineNav, {Link: UnderlineNavLink})
