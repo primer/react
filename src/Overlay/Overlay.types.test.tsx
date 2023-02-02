82181e872e575f167ee04f18cbc2ee85caa7401d
Amanda G. Brown <green6erry@gmail.com>
2023-02-02 15:02:32 -0500

    refactor: moves tests into individual component folders

---
+++ src/Overlay/Overlay.types.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Overlay from './Overlay'
+
+export function shouldAcceptCallWithNoProps(ref: React.RefObject<HTMLElement>) {
+  return <Overlay returnFocusRef={ref} onClickOutside={() => null} onEscape={() => null} />
+}
+
+export function shouldAcceptCallWithDOMProps(ref: React.RefObject<HTMLElement>) {
+  return <Overlay returnFocusRef={ref} onClickOutside={() => null} onEscape={() => null} onMouseDown={() => null} />
+}
+
+export function shouldNotAcceptCallWithDOMPropsThatDontMatchElement(ref: React.RefObject<HTMLElement>) {
+  // @ts-expect-error href should not be allowed on a <div>
+  return <Overlay returnFocusRef={ref} onClickOutside={() => null} onEscape={() => null} href="//primer.style/" />
+}
+
+export function shouldAcceptCallWithAsAndDOMProps(ref: React.RefObject<HTMLElement>) {
+  return (
+    <Overlay as="a" returnFocusRef={ref} onClickOutside={() => null} onEscape={() => null} href="//primer.style/" />
+  )
+}
+
+export function shouldNotAcceptSystemProps(ref: React.RefObject<HTMLElement>) {
+  return (
+    <Overlay
+      returnFocusRef={ref}
+      onClickOutside={() => null}
+      onEscape={() => null}
+      // @ts-expect-error system props should not be accepted
+      backgroundColor="olivedrab"
+    />
+  )
+}
