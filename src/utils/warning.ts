c5289e9455e8e12a39c624c331b7a2d62d54dbe0
Josh Black <joshblack@users.noreply.github.com>
2023-02-22 11:34:18 -0600

    chore: address eslint violations

--- src/utils/warning.ts
+++ src/utils/warning.ts
@@ -1,15 +1,15 @@
-const emptyFunction = function () {}
+function emptyFunction() {}
 
 const warning = __DEV__
-  ? function warning(condition: any, format: string, ...args: Array<any>) {
+  ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    function warning(condition: any, format: string, ...args: Array<any>) {
       if (!condition) {
         let index = 0
         const message = format.replace(/%s/g, () => {
           return args[index++]
         })
-
         // eslint-disable-next-line no-console
-        console.warn('Warning: ' + message)
+        console.warn(`Warning: ${message}`)
       }
     }
   : emptyFunction
