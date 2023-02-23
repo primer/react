8f201193b72c4345a68f9372672910fe2b347ce9
Josh Black <joshblack@users.noreply.github.com>
2023-02-23 15:22:50 -0600

    chore(project): update storybook to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,36 +2,34 @@
  * @type {import('@storybook/core-common').StorybookConfig}
  */
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    {name: '@storybook/addon-essentials', options: {backgrounds: false}},
-    '@storybook/addon-storysource',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-    '@storybook/addon-links',
-    {
-      name: 'storybook-addon-turbo-build',
-      options: {
-        optimizationLevel: 2,
-      },
-    },
-  ],
-  core: {
-    builder: {
-      name: 'webpack5',
-      options: {
-        fsCache: true,
-      },
-    },
-  },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [{
+    name: '@storybook/addon-essentials',
+    options: {
+      backgrounds: false
+    }
+  }, '@storybook/addon-storysource', '@storybook/addon-interactions', '@storybook/addon-a11y', '@storybook/addon-links', {
+    name: 'storybook-addon-turbo-build',
+    options: {
+      optimizationLevel: 2
+    }
+  }],
   features: {
     interactionsDebugger: true,
     storyStoreV7: true,
-    buildStoriesJson: true,
+    buildStoriesJson: true
   },
-  framework: '@storybook/react',
-  reactOptions: {
-    fastRefresh: true,
-    strictMode: true,
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      fastRefresh: true,
+      strictMode: true,
+      builder: {
+        fsCache: true
+      }
+    }
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
