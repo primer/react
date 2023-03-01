2bb64caeab9d1b22c7bbc74637af45f6c3e53421
langermank <18661030+langermank@users.noreply.github.com>
2023-03-01 15:52:40 -0800

    select

--- src/Select/Select.stories.tsx
+++ src/Select/Select.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Meta} from '@storybook/react'
-
 import {Select, FormControl, Box} from '..'
 import {SelectProps} from '../Select'
 import {
@@ -9,25 +8,49 @@ import {
   formControlArgTypes,
   getFormControlArgsByChildComponent,
   inputWrapperArgTypes,
-} from '../utils/story-helpers'
+} from '../utils/form-story-helpers'
 
 export default {
-  title: 'Components/Forms/Select',
+  title: 'Components/Select',
   component: Select,
-  args: {
-    ...formControlArgs,
-    required: false,
-  },
-  argTypes: {
-    ...inputWrapperArgTypes,
-    ...formControlArgTypes,
-  },
   parameters: {
     controls: {
       exclude: ['contrast', 'hasTrailingAction', 'monospace', 'isInputFocused', 'sx', 'size'],
     },
   },
 } as Meta
+
+export const Playground = (args: FormControlArgs<SelectProps>) => {
+  const {parentArgs, labelArgs, captionArgs, validationArgs} = getFormControlArgsByChildComponent(args)
+
+  return (
+    <Box as="form">
+      <FormControl {...parentArgs}>
+        <FormControl.Label {...labelArgs} />
+        <Select {...args}>
+          <Select.Option value="one">Choice one</Select.Option>
+          <Select.Option value="two">Choice two</Select.Option>
+          <Select.Option value="three">Choice three</Select.Option>
+          <Select.Option value="four">Choice four</Select.Option>
+          <Select.Option value="five">Choice five</Select.Option>
+          <Select.Option value="six">Choice six</Select.Option>
+        </Select>
+        {captionArgs.children && <FormControl.Caption {...captionArgs} />}
+        {validationArgs.children && validationArgs.variant && (
+          <FormControl.Validation {...validationArgs} variant={validationArgs.variant} />
+        )}
+      </FormControl>
+    </Box>
+  )
+}
+Playground.args = {
+  required: false,
+  ...formControlArgs,
+}
+Playground.argTypes = {
+  ...inputWrapperArgTypes,
+  ...formControlArgTypes,
+},
 
 export const Default = (args: FormControlArgs<SelectProps>) => {
   const {parentArgs, labelArgs, captionArgs, validationArgs} = getFormControlArgsByChildComponent(args)
--- src/__tests__/storybook.test.tsx
+++ src/__tests__/storybook.test.tsx
@@ -21,6 +21,7 @@ const allowlist = [
   'Link',
   'Radio',
   'RadioGroup',
+  'Select',
   'StateLabel',
   'TabNav',
   'TreeView',
