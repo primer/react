0ca1cfee1520afeb7e8c4418b074598c6b699c18
Armagan Ersoz <broccolinisoup@github.com>
2023-03-09 17:00:27 +1000

    Token: Reformat stories and add e2e tests

--- src/Token/__tests__/Token.test.tsx
+++ src/Token/__tests__/Token.test.tsx
@@ -1,12 +1,12 @@
 import {render as HTMLRender, fireEvent} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import React from 'react'
-import Token from '../Token/Token'
-import {render, behavesAsComponent} from '../utils/testing'
+import Token from '../Token'
+import {render, behavesAsComponent} from '../../utils/testing'
 import {axe} from 'jest-axe'
-import {TokenSizeKeys, tokenSizes} from '../Token/TokenBase'
-import {IssueLabelToken, AvatarToken} from '../Token'
-import {AvatarTokenProps} from '../Token/AvatarToken'
+import {TokenSizeKeys, tokenSizes} from '../TokenBase'
+import {IssueLabelToken, AvatarToken} from '..'
+import {AvatarTokenProps} from '../AvatarToken'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const testTokenComponent = (Component: React.ComponentType<React.PropsWithChildren<any>>) => {
