2237fc729c5a0c2b3804f72b318754c2f9132d5e
Josh Black <joshblack@users.noreply.github.com>
2023-03-10 13:58:57 -0600

    Revert "test(Hidden): update export"
    This reverts commit f29edd9299b17da9064a25f4a08ae1c46324748d.

--- src/Hidden/Hidden.test.tsx
+++ src/Hidden/Hidden.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
 import {render} from '@testing-library/react'
-import {Hidden} from './Hidden'
+import {Hidden} from '.'
 import MatchMediaMock from 'jest-matchmedia-mock'
 import {behavesAsComponent, checkExports, renderStyles, checkStoriesForAxeViolations} from '../utils/testing'
 import {mediaQueries} from '../utils/layout'
