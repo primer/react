7e47e68f1cee509464ec5db24f73b13a15ca4229
simurai <simurai@github.com>
2023-03-14 16:02:22 +0900

    Move `Box` to its own directory

--- src/Box/Box.tsx
+++ src/Box/Box.tsx
@@ -19,10 +19,10 @@ import {
   space,
   SpaceProps,
   typography,
-  TypographyProps,
+  TypographyProps
 } from 'styled-system'
-import sx, {SxProp} from './sx'
-import {ComponentProps} from './utils/types'
+import sx, {SxProp} from '../sx'
+import {ComponentProps} from '../utils/types'
 
 type StyledBoxProps = SpaceProps &
   ColorProps &
@@ -47,7 +47,7 @@ const Box = styled.div<StyledBoxProps>(
   border,
   position,
   shadow,
-  sx,
+  sx
 )
 
 export type BoxProps = ComponentProps<typeof Box>
