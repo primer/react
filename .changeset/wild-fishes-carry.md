63b1dc7f886e0ae5ab8569b04242e71893b97051
Ederson Lucas <edersonlucas@outlook.com.br>
2023-03-14 22:02:04 -0300

    docs(TextInputInnerAction): update docs

--- .changeset/wild-fishes-carry.md
+++ .changeset/wild-fishes-carry.md
@@ -1,5 +1,4 @@
 ---
-'@primer/react': patch
+'@primer/react': minor
 ---
-
 Add the `tooltip-direction` property to the `TextInput.Action` component to make the tooltip position flexible.
--- src/TextInput/TextInput.features.stories.tsx
+++ src/TextInput/TextInput.features.stories.tsx
@@ -137,6 +137,35 @@ export const WithTrailingAction = () => {
               }}
               icon={XCircleFillIcon}
               aria-label="Clear input"
+              sx={{color: 'fg.subtle'}}
+            />
+          }
+          value={value}
+          onChange={handleChange}
+        />
+      </FormControl>
+    </Box>
+  )
+}
+
+export const WithTooltipDirection = () => {
+  const [value, setValue] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+  return (
+    <Box as="form">
+      <FormControl>
+        <FormControl.Label>Default label</FormControl.Label>
+        <TextInput
+          trailingAction={
+            <TextInput.Action
+              onClick={() => {
+                setValue('')
+              }}
+              icon={XCircleFillIcon}
+              aria-label="Clear input"
               tooltip-direction="nw"
               sx={{color: 'fg.subtle'}}
             />
