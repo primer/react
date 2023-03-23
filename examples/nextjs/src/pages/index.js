c70ca7182ca8bcb08ff60f3fda6e27b66975540d
Armagan Ersoz <broccolinisoup@github.com>
2023-03-23 14:20:02 +1000

    update layout

--- examples/nextjs/src/pages/index.js
+++ examples/nextjs/src/pages/index.js
@@ -1,19 +1,6 @@
 import {Box} from '@primer/react'
-import {
-  CodeIcon,
-  IssueOpenedIcon,
-  GitPullRequestIcon,
-  CommentDiscussionIcon,
-  PlayIcon,
-  ProjectIcon,
-  GraphIcon,
-  GearIcon,
-  ShieldLockIcon,
-} from '@primer/octicons-react'
 import React from 'react'
-import {UnderlineNav} from '@primer/react/drafts'
-import Navigation from '../components/Navigation'
 
 export default function IndexPage() {
-  return <Box>Welcome to Primer's Next.js Kitchen Sink</Box>
+  return <Box sx={{paddingY: 4, textAlign: 'center'}}>Welcome to Primer React's Next.js Kitchen Sink</Box>
 }
