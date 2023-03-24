fa375f9ce3f6e80ac710b86c70a0ff9096af070f
Armağan <broccolinisoup@github.com>
2023-03-24 10:45:17 +1000

    Examples/Next.js: Update dependencies and add `UnderlineNav` to the kitchen sink (#3066)
    * Add UnderlineNav to the nextjs add and update dependencies
    
    * update layout
    
    * loosen the esm externals

--- examples/nextjs/src/pages/index.js
+++ examples/nextjs/src/pages/index.js
@@ -1,6 +1,6 @@
-import {Button} from '@primer/react'
+import {Box} from '@primer/react'
 import React from 'react'
 
 export default function IndexPage() {
-  return <Button>Hello world</Button>
+  return <Box sx={{paddingY: 4, textAlign: 'center'}}>Welcome to Primer React's Next.js Kitchen Sink</Box>
 }
