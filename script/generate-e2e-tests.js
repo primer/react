7eaad160075827a79ac35a64f9a76dc460f4dd3a
Jeremy Neal <radglob@github.com>
2023-04-04 16:00:14 -0400

    refactor(CircleBadge): move files into folder, add e2e tests. (#3118)
    * Reorganize CircleBadge, add e2e tests.
    
    * Update generated/components.json
    
    * test(vrt): update snapshots
    
    * Update docs reference.
    
    ---------
    
    Co-authored-by: radglob <radglob@users.noreply.github.com>

--- script/generate-e2e-tests.js
+++ script/generate-e2e-tests.js
@@ -311,6 +311,21 @@ const components = new Map([
     },
   ],
   [
+    'CircleBadge',
+    {
+      stories: [
+        {
+          id: 'components-circlebadge--default',
+          name: 'Default',
+        },
+        {
+          id: 'components-circlebadge--playground',
+          name: 'Playground',
+        },
+      ],
+    },
+  ],
+  [
     'CounterLabel',
     {
       stories: [
