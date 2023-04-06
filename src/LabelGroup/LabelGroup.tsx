03555215ea2a5f2ed0661463d65ec45a31a38d66
Jeremy Neal <radglob@github.com>
2023-04-06 12:10:01 -0400

    Refactor(LabelGroup): add files to folder, add storybook and e2e tests. (#3131)
    * Move files to folder, add storybook and e2e tests.
    
    * Update generated/components.json
    
    * test(vrt): update snapshots
    
    ---------
    
    Co-authored-by: radglob <radglob@users.noreply.github.com>

--- src/LabelGroup/LabelGroup.tsx
+++ src/LabelGroup/LabelGroup.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
-import {get} from './constants'
-import sx, {SxProp} from './sx'
-import {ComponentProps} from './utils/types'
+import {get} from '../constants'
+import sx, {SxProp} from '../sx'
+import {ComponentProps} from '../utils/types'
 
 const LabelGroup = styled.span<SxProp>`
   & * {
