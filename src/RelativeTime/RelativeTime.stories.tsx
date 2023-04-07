d54101b3c903fdd774bc0705d98560142772f43e
Cole Bemis <colebemis@github.com>
2023-04-06 23:24:04 -0600

    Update RelativeTime stories (#3136)
    * Update RelativeTime stories
    
    * Update generated/components.json
    
    ---------
    
    Co-authored-by: colebemis <colebemis@users.noreply.github.com>

--- src/RelativeTime/RelativeTime.stories.tsx
+++ src/RelativeTime/RelativeTime.stories.tsx
@@ -15,146 +15,130 @@ const meta: Meta = {
     // it's not possible to snapshot test relative time because the output keeps changing
     chromatic: {disableSnapshot: true},
   },
-  args: {
-    date: new Date('2020-01-01T00:00:00Z'),
-    second: '',
-    minute: '',
-    hour: '',
-    weekday: '',
-    day: 'numeric',
-    month: 'short',
-    year: '',
-    timeZoneName: '',
-  },
-  argTypes: {
-    hour: {
-      type: {
-        name: 'enum',
-        value: ['', 'numeric', '2-digit'],
-      },
-      control: {
-        type: 'select',
-        labels: {
-          '': '(None)',
-        },
-      },
-    },
-    minute: {
-      type: {
-        name: 'enum',
-        value: ['', 'numeric', '2-digit'],
-      },
-      control: {
-        type: 'select',
-        labels: {
-          '': '(None)',
-        },
-      },
-    },
-    second: {
-      type: {
-        name: 'enum',
-        value: ['', 'numeric', '2-digit'],
-      },
-      control: {
-        type: 'select',
-        labels: {
-          '': '(None)',
-        },
-      },
-    },
-    weekday: {
-      type: {
-        name: 'enum',
-        value: ['', 'short', 'long', 'narrow'],
-      },
-      control: {
-        type: 'select',
-        labels: {
-          '': '(None)',
-        },
-      },
-    },
-    day: {
-      type: {
-        name: 'enum',
-        value: ['', 'numeric', '2-digit'],
-      },
-      control: {
-        type: 'select',
-        labels: {
-          '': '(None)',
-        },
-      },
-    },
-    month: {
-      type: {
-        name: 'enum',
-        value: ['', 'numeric', '2-digit', 'long', 'short', 'narrow'],
-      },
-      control: {
-        type: 'select',
-        labels: {
-          '': '(None)',
-        },
-      },
-    },
-    year: {
-      type: {
-        name: 'enum',
-        value: ['', 'numeric', '2-digit'],
-      },
-      control: {
-        type: 'select',
-        labels: {
-          '': '(None)',
-        },
-      },
-    },
-    timeZoneName: {
-      type: {
-        name: 'enum',
-        value: ['', 'long', 'short', 'longOffset', 'shortOffset', 'longGeneric', 'shortGeneric'],
-      },
-      control: {
-        type: 'select',
-        labels: {
-          '': '(None)',
-        },
-      },
-    },
-    date: {
-      control: 'date',
-    },
-    format: {
-      control: 'string',
-    },
-  },
 }
 
-export const Default: Story = args => {
+export const Default: Story = () => <RelativeTime date={new Date('2020-01-01T00:00:00Z')} />
+
+export const Playground: Story = args => {
   const {date, ...rest} = args
   return <RelativeTime {...rest} date={new Date(date)} />
 }
 
-export const MicroFormat: Story = args => {
-  const {date, ...rest} = args
-  return <RelativeTime {...rest} date={new Date(date)} format="micro" />
+Playground.args = {
+  date: new Date('2020-01-01T00:00:00Z'),
+  second: '',
+  minute: '',
+  hour: '',
+  weekday: '',
+  day: 'numeric',
+  month: 'short',
+  year: '',
+  timeZoneName: '',
 }
-MicroFormat.args = {tense: 'past'}
-MicroFormat.argTypes = {format: {control: false}}
 
-export const RecentTime: Story = args => {
-  const {...rest} = args
-  return <RelativeTime {...rest} date={new Date()} />
+Playground.argTypes = {
+  hour: {
+    type: {
+      name: 'enum',
+      value: ['', 'numeric', '2-digit'],
+    },
+    control: {
+      type: 'select',
+      labels: {
+        '': '(None)',
+      },
+    },
+  },
+  minute: {
+    type: {
+      name: 'enum',
+      value: ['', 'numeric', '2-digit'],
+    },
+    control: {
+      type: 'select',
+      labels: {
+        '': '(None)',
+      },
+    },
+  },
+  second: {
+    type: {
+      name: 'enum',
+      value: ['', 'numeric', '2-digit'],
+    },
+    control: {
+      type: 'select',
+      labels: {
+        '': '(None)',
+      },
+    },
+  },
+  weekday: {
+    type: {
+      name: 'enum',
+      value: ['', 'short', 'long', 'narrow'],
+    },
+    control: {
+      type: 'select',
+      labels: {
+        '': '(None)',
+      },
+    },
+  },
+  day: {
+    type: {
+      name: 'enum',
+      value: ['', 'numeric', '2-digit'],
+    },
+    control: {
+      type: 'select',
+      labels: {
+        '': '(None)',
+      },
+    },
+  },
+  month: {
+    type: {
+      name: 'enum',
+      value: ['', 'numeric', '2-digit', 'long', 'short', 'narrow'],
+    },
+    control: {
+      type: 'select',
+      labels: {
+        '': '(None)',
+      },
+    },
+  },
+  year: {
+    type: {
+      name: 'enum',
+      value: ['', 'numeric', '2-digit'],
+    },
+    control: {
+      type: 'select',
+      labels: {
+        '': '(None)',
+      },
+    },
+  },
+  timeZoneName: {
+    type: {
+      name: 'enum',
+      value: ['', 'long', 'short', 'longOffset', 'shortOffset', 'longGeneric', 'shortGeneric'],
+    },
+    control: {
+      type: 'select',
+      labels: {
+        '': '(None)',
+      },
+    },
+  },
+  date: {
+    control: 'date',
+  },
+  format: {
+    control: 'string',
+  },
 }
-RecentTime.argTypes = {date: {control: false}}
-
-export const CountDownTimer: Story = args => {
-  const {...rest} = args
-  return <RelativeTime {...rest} />
-}
-CountDownTimer.args = {date: new Date('2038-01-19T03:14:08Z'), format: 'elapsed', day: '', month: ''}
-CountDownTimer.argTypes = {date: {control: false}, format: {control: false}}
 
 export default meta
