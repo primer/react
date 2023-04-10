28254cc169d1283f580f26bf2d22aa486e1607f9
Jeremy Neal <hello@radglob.com>
2023-04-10 10:56:33 -0400

    Change padding again.

--- src/ActionMenu/ActionMenu.examples.stories.tsx
+++ src/ActionMenu/ActionMenu.examples.stories.tsx
@@ -32,7 +32,7 @@ export const GroupsAndDescriptions = () => {
         Milestone
       </ActionMenu.Button>
       <ActionMenu.Overlay width="medium">
-        <ActionList selectionVariant="single" showDividers sx={{paddingTop: 1}}>
+        <ActionList selectionVariant="single" showDividers sx={{paddingTop: 2}}>
           <ActionList.Heading title="Open" />
           {milestones
             .filter(milestone => !milestone.name.includes('21'))
