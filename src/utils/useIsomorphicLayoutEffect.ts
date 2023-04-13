7272924143accf7c79dd290203e34c33e4043207
Cole Bemis <colebemis@github.com>
2023-04-13 10:21:28 -0600

    Configure ssr-friendly eslint plugin (#3164)
    * Configure ssr-friendly eslint plugin
    
    * Fix ssr lint error

--- src/utils/useIsomorphicLayoutEffect.ts
+++ src/utils/useIsomorphicLayoutEffect.ts
@@ -2,7 +2,9 @@ import {useEffect, useLayoutEffect} from 'react'
 
 const useIsomorphicLayoutEffect =
   typeof window !== 'undefined' &&
+  // eslint-disable-next-line ssr-friendly/no-dom-globals-in-module-scope
   typeof window.document !== 'undefined' &&
+  // eslint-disable-next-line ssr-friendly/no-dom-globals-in-module-scope
   typeof window.document.createElement !== 'undefined'
     ? useLayoutEffect
     : useEffect
