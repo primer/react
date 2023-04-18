3149fb8c8bbcb58b6df7587f295a8706e91ec93d
Jeremy Neal <hello@radglob.com>
2023-04-18 11:03:16 -0400

    Use selector to set disabled invisible button text color correctly.

--- src/Button/ButtonBase.tsx
+++ src/Button/ButtonBase.tsx
@@ -65,7 +65,7 @@ const ButtonBase = forwardRef(
         ref={innerRef}
         data-block={block ? 'block' : null}
         data-size={size === 'small' || size === 'large' ? size : undefined}
-        data-no-visuals={!LeadingIcon && !TrailingIcon && !TrailingAction && variant !== 'invisible' ? true : undefined}
+        data-no-visuals={!LeadingIcon && !TrailingIcon && !TrailingAction ? true : undefined}
       >
         {Icon ? (
           <Icon />
