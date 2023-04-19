444fe30e7da57ffca541125fbd68805ecfa3826f
Siddharth Kshetrapal <siddharthkp@github.com>
2023-04-19 21:23:32 +0200

    Setup snapshots for visual regression for  experimental/Button2 (#3181)
    * duplicate button into drafts
    
    * update story title
    
    * add visual tests for Button2
    
    * test(vrt): update snapshots
    
    ---------
    
    Co-authored-by: siddharthkp <siddharthkp@users.noreply.github.com>

---
+++ src/drafts/Button2/Button.features.stories.tsx
@@ -0,0 +1,41 @@
+import {EyeIcon, TriangleDownIcon, HeartIcon} from '@primer/octicons-react'
+import React, {useState} from 'react'
+import {Button} from '.'
+
+export default {
+  title: 'Drafts/Components/Button/Features',
+}
+
+export const Primary = () => <Button variant="primary">Primary</Button>
+
+export const Danger = () => <Button variant="danger">Danger</Button>
+
+export const Invisible = () => <Button variant="invisible">Invisible</Button>
+
+export const Outline = () => <Button variant="outline">Outline</Button>
+
+export const LeadingVisual = () => <Button leadingIcon={HeartIcon}>Leading visual</Button>
+
+export const TrailingVisual = () => <Button trailingIcon={EyeIcon}>Trailing visual</Button>
+
+export const TrailingCounter = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <Button onClick={() => setCount(count + 1)}>
+      Watch
+      <Button.Counter>{count}</Button.Counter>
+    </Button>
+  )
+}
+
+export const TrailingAction = () => <Button trailingAction={TriangleDownIcon}>Trailing action</Button>
+
+export const Block = () => <Button block>Default</Button>
+
+export const Disabled = () => <Button disabled>Default</Button>
+
+export const Small = () => <Button size="small">Default</Button>
+
+export const Medium = () => <Button size="medium">Default</Button>
+
+export const Large = () => <Button size="large">Default</Button>
