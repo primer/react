ee0834673616484b57ffdd0712a4d23f45de839e
Josh Black <joshblack@users.noreply.github.com>
2023-04-21 11:17:24 -0500

    chore(project): move primer/react into packages/react

--- codemods/lib/replaceImportSource.js
+++
@@ -1,7 +0,0 @@
-module.exports = function replaceImportSource(ast, j, from, to) {
-  ast
-    .find(j.ImportDeclaration, decl => decl.source.value === from)
-    .forEach(decl => {
-      j(decl).find(j.Literal, {value: from}).replaceWith(j.literal(to))
-    })
-}
