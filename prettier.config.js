e9190fddeaf10dda9ea38f8a37203c4c41d12bba
Josh Black <joshblack@users.noreply.github.com>
2023-05-26 11:51:40 -0500

    chore: address eslint violations

--- prettier.config.js
+++ prettier.config.js
@@ -4,6 +4,7 @@ const config = require('@github/prettier-config')
 
 module.exports = {
   ...config,
+  // eslint-disable-next-line github/unescaped-html-literal
   importOrder: ['^node:(.*)$', '<THIRD_PARTY_MODULES>', '^[./]'],
   importOrderSeparation: false,
   importOrderSortSpecifiers: true,
