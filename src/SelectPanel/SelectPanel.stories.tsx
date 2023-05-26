d3bc7217ec39de80ae24265c6df426141e24f3a7
Jeremy Neal <radglob@github.com>
2023-05-26 10:13:04 -0400

    Refactor FilteredActionList to address a11y violations and use new ActionList. (#3247)
    * Update FilteredActionList to use non-deprecated ActionList.
    
    * Use non-deprecated ActionList in FilteredActionList.
    
    * Fix a11y issues in FilteredActionList story.
    
    * Add prop to hide selection component if needed.
    
    * Remove unused hook import.
    
    * Get SavedReplies to look as it did with deprecated ActionList.
    
    * Fix failing test.
    
    * Create weak-jokes-chew.md
    
    * Update generated/components.json
    
    * Fix themePreval snapshot.
    
    * Linting fixes.
    
    * Fix type-check errors.
    
    * Update themePreval snapshot again.
    
    * Fix themePreval snapshot to match origin. Unsure why it's not generating the same way.
    
    * Hide selections in MarkdownEditor saved replies.
    
    * Remove hideSelection prop and add defaultRenderFn to FilteredActionList so these don't have to be defined manually everywhere.
    
    * Fix selection rendering (needed explicit selected boolean) and fix SelectPanel docs.
    
    * Pass selectionVariant illegally to SelectPanel in src/MarkdownEditor/_SavedReplies.tsx so Selection components don't render.
    
    * Remove remaining references of hideSelection prop.
    
    * Update changeset to reflect that changes impact SelectPanel.
    
    * Remove renderFn prop from SelectPanel and use default for FilteredActionList, which seems to cover most cases.
    
    * Fix truncation in SavedReplies descriptions.
    
    * Update generated/components.json
    
    * Fix linting error.
    
    * Don't make renderFn a prop (if we need to make this configurable, we can expose it later. Use maxWidth 100% for SavedReplies truncation.
    
    * Use showDividers prop in SelectPanel story.
    
    * Formatting.
    
    * Add temporary support for showItemDividers prop to SelectPanel to keep backwards compatibility.
    
    * Support passing deprecated showItemDividers prop in ActionList.
    
    ---------
    
    Co-authored-by: radglob <radglob@users.noreply.github.com>

--- src/SelectPanel/SelectPanel.stories.tsx
+++ src/SelectPanel/SelectPanel.stories.tsx
@@ -2,10 +2,10 @@ import {TriangleDownIcon} from '@primer/octicons-react'
 import {ComponentMeta} from '@storybook/react'
 import React, {useState} from 'react'
 
-import Box from '../Box'
 import {Button} from '../Button'
 import {SelectPanel} from '../SelectPanel'
-import {ItemInput} from '../deprecated/ActionList/List'
+import Box from '../Box'
+import {ItemInput} from '../FilteredActionList'
 
 export default {
   title: 'Components/SelectPanel',
@@ -13,32 +13,30 @@ export default {
 } as ComponentMeta<typeof SelectPanel>
 
 function getColorCircle(color: string) {
-  return function () {
-    return (
-      <Box
-        sx={{
-          backgroundColor: color,
-          borderColor: color,
-          width: 14,
-          height: 14,
-          borderRadius: 10,
-          margin: 'auto',
-          borderWidth: '1px',
-          borderStyle: 'solid',
-        }}
-      />
-    )
-  }
+  return (
+    <Box
+      sx={{
+        backgroundColor: color,
+        borderColor: color,
+        width: 14,
+        height: 14,
+        borderRadius: 10,
+        margin: 'auto',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+      }}
+    />
+  )
 }
 
 const items = [
-  {leadingVisual: getColorCircle('#a2eeef'), text: 'enhancement', id: 1},
-  {leadingVisual: getColorCircle('#d73a4a'), text: 'bug', id: 2},
-  {leadingVisual: getColorCircle('#0cf478'), text: 'good first issue', id: 3},
-  {leadingVisual: getColorCircle('#ffd78e'), text: 'design', id: 4},
-  {leadingVisual: getColorCircle('#ff0000'), text: 'blocker', id: 5},
-  {leadingVisual: getColorCircle('#a4f287'), text: 'backend', id: 6},
-  {leadingVisual: getColorCircle('#8dc6fc'), text: 'frontend', id: 7},
+  {leadingVisual: getColorCircle('#a2eeef'), text: 'enhancement', id: '1'},
+  {leadingVisual: getColorCircle('#d73a4a'), text: 'bug', id: '2'},
+  {leadingVisual: getColorCircle('#0cf478'), text: 'good first issue', id: '3'},
+  {leadingVisual: getColorCircle('#ffd78e'), text: 'design', id: '4'},
+  {leadingVisual: getColorCircle('#ff0000'), text: 'blocker', id: '5'},
+  {leadingVisual: getColorCircle('#a4f287'), text: 'backend', id: '6'},
+  {leadingVisual: getColorCircle('#8dc6fc'), text: 'frontend', id: '7'},
 ]
 
 export const Default = () => {
@@ -65,7 +63,6 @@ export const Default = () => {
         selected={selected}
         onSelectedChange={setSelected}
         onFilterChange={setFilter}
-        showItemDividers={true}
         overlayProps={{width: 'small', height: 'xsmall'}}
       />
     </>
