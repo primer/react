8d3481fe38c810392c466b214357b8463d7bdf24
Armagan Ersoz <broccolinisoup@github.com>
2023-06-06 08:28:37 +1000

    Remove act() warning check

--- src/UnderlineNav/UnderlineNav.test.tsx
+++ src/UnderlineNav/UnderlineNav.test.tsx
@@ -95,7 +95,6 @@ describe('UnderlineNav', () => {
     expect(nav.getElementsByTagName('svg').length).toEqual(7)
   })
   it('fires onSelect on click', async () => {
-    const spy = jest.spyOn(console, 'error').mockImplementation()
     const onSelect = jest.fn()
     const {getByRole} = render(
       <UnderlineNav aria-label="Test Navigation">
@@ -108,14 +107,6 @@ describe('UnderlineNav', () => {
     const user = userEvent.setup()
     await user.click(item)
     expect(onSelect).toHaveBeenCalledTimes(1)
-
-    if (REACT_VERSION_LATEST) {
-      expect(spy).not.toHaveBeenCalled()
-    } else {
-      // Warning: It looks like you're using the wrong act() around your test interactions
-      expect(spy).toHaveBeenCalledTimes(2)
-    }
-    spy.mockRestore()
   })
   it('fires onSelect on keypress', async () => {
     const onSelect = jest.fn()
