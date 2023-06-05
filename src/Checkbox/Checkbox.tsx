62b1daffd0336c59ee3b7c581e8bd254ec8aaf90
Amanda G. Brown <green6erry@gmail.com>
2023-06-05 18:03:31 -0400

    refactor(getGlobalFocusStyles): moved to internal

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -5,7 +5,7 @@ import sx, {SxProp} from '../sx'
 import useLayoutEffect from '../utils/useIsomorphicLayoutEffect'
 import {FormValidationStatus} from '../utils/types/FormValidationStatus'
 import {CheckboxGroupContext} from '../CheckboxGroup/CheckboxGroupContext'
-import getGlobalFocusStyles from '../_getGlobalFocusStyles'
+import getGlobalFocusStyles from '../internal/hooks/getGlobalFocusStyles'
 import {get} from '../constants'
 import {sharedCheckboxAndRadioStyles} from '../_sharedCheckboxAndRadioStyles'
 
