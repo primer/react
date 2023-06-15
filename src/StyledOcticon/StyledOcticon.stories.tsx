f20a2b8bbd6c105ed2d5aaf1d2be0de10c9d5e6f
Jeremy Neal <hello@radglob.com>
2023-06-15 16:35:09 -0400

    Fix reference in StyledOcticon story.

--- src/StyledOcticon/StyledOcticon.stories.tsx
+++ src/StyledOcticon/StyledOcticon.stories.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Meta, ComponentStory} from '@storybook/react'
-import Octicon from './Octicon'
+import StyledOcticon from './StyledOcticon'
 import {HeartFillIcon} from '@primer/octicons-react'
 
 export default {
   title: 'Components/StyledOcticon',
-  component: Octicon,
-} as Meta<typeof Octicon>
+  component: StyledOcticon,
+} as Meta<typeof StyledOcticon>
 
-export const Default = () => <Octicon icon={HeartFillIcon} size={32} />
+export const Default = () => <StyledOcticon icon={HeartFillIcon} size={32} />
 
-export const Playground: ComponentStory<typeof Octicon> = args => <Octicon icon={HeartFillIcon} {...args} />
+export const Playground: ComponentStory<typeof Octicon> = args => <StyledOcticon icon={HeartFillIcon} {...args} />
 
 Playground.args = {
   ariaLabel: 'Heart',
