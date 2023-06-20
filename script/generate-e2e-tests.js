c9c8b328b29d34a6943d5e13fc417eab1b7d36cf
Jeremy Neal <radglob@github.com>
2023-06-20 09:27:50 -0400

    Refactor(InlineAutocomplete): reorganize stories, add e2e tests. (#3420)
    * Separate stories into features, add playground.
    
    * Add e2e tests.
    
    * test(vrt): update snapshots
    
    * Fix typecheck errors.
    
    ---------
    
    Co-authored-by: radglob <radglob@users.noreply.github.com>

--- script/generate-e2e-tests.js
+++ script/generate-e2e-tests.js
@@ -574,6 +574,29 @@ const components = new Map([
     },
   ],
   [
+    'InlineAutocomplete',
+    {
+      stories: [
+        {
+          id: 'components-forms-inlineautocomplete--default',
+          name: 'Default',
+        },
+        {
+          id: 'components-forms-inlineautocomplete--playground',
+          name: 'Playground',
+        },
+        {
+          id: 'components-forms-inlineautocomplete-features--custom-rendering',
+          name: 'Custom Rendering',
+        },
+        {
+          id: 'components-forms-inlineautocomplete-features--single-line',
+          name: 'Single Line',
+        },
+      ],
+    },
+  ],
+  [
     'Label',
     {
       stories: [
