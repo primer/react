da0c8d5a10b2ca132fefdb9396b942e24cf31aca
Mike Perrotti <mperrotti@github.com>
2023-06-27 16:28:52 -0400

    fixes import path in deprecated Dialog types test

--- src/__tests__/deprecated/Dialog.types.test.tsx
+++ src/__tests__/deprecated/Dialog.types.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Dialog from '../Dialog'
+import Dialog from '../../deprecated/Dialog/Dialog'
 
 export function shouldAcceptCallWithNoProps() {
   return <Dialog />
