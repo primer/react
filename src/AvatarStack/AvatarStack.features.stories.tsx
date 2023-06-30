b56fb7531c2fcd85653c90245f1d9d2332c924c5
Mike Perrotti <mperrotti@github.com>
2023-06-30 16:04:23 -0400

    updates tests and stories

--- src/AvatarStack/AvatarStack.features.stories.tsx
+++ src/AvatarStack/AvatarStack.features.stories.tsx
@@ -25,3 +25,30 @@ export const AlignRight = () => (
     <Avatar alt="GitHub Desktop logo" src="https://avatars.githubusercontent.com/desktop" />
   </AvatarStack>
 )
+
+export const DisableExpandOnHover = () => (
+  <AvatarStack disableExpand>
+    <Avatar alt="Primer logo" src="https://avatars.githubusercontent.com/primer" />
+    <Avatar alt="GitHub logo" src="https://avatars.githubusercontent.com/github" />
+    <Avatar alt="Atom logo" src="https://avatars.githubusercontent.com/atom" />
+    <Avatar alt="GitHub Desktop logo" src="https://avatars.githubusercontent.com/desktop" />
+  </AvatarStack>
+)
+
+export const CustomSizeOnParent = () => (
+  <AvatarStack size={32}>
+    <Avatar alt="Primer logo" src="https://avatars.githubusercontent.com/primer" />
+    <Avatar alt="GitHub logo" src="https://avatars.githubusercontent.com/github" />
+    <Avatar alt="Atom logo" src="https://avatars.githubusercontent.com/atom" />
+    <Avatar alt="GitHub Desktop logo" src="https://avatars.githubusercontent.com/desktop" />
+  </AvatarStack>
+)
+
+export const CustomSizeOnChildren = () => (
+  <AvatarStack>
+    <Avatar size={32} alt="Primer logo" src="https://avatars.githubusercontent.com/primer" />
+    <Avatar size={32} alt="GitHub logo" src="https://avatars.githubusercontent.com/github" />
+    <Avatar size={32} alt="Atom logo" src="https://avatars.githubusercontent.com/atom" />
+    <Avatar size={32} alt="GitHub Desktop logo" src="https://avatars.githubusercontent.com/desktop" />
+  </AvatarStack>
+)
