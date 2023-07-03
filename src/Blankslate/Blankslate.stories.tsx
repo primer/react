8af751fbeb0ce61b2026e376c2754bb69f2271e2
Josh Black <joshblack@users.noreply.github.com>
2023-07-03 09:45:06 -0500

    docs: update titles and order for storybook hierarchy

--- src/Blankslate/Blankslate.stories.tsx
+++ src/Blankslate/Blankslate.stories.tsx
@@ -5,7 +5,7 @@ import {Blankslate} from '../Blankslate'
 import {ComponentProps} from '../utils/types'
 
 export default {
-  title: 'Drafts/Components/Blankslate',
+  title: 'Private/Components/Blankslate',
   component: Blankslate,
   subcomponents: {
     'Blankslate.Visual': Blankslate.Visual,
