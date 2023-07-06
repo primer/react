05e70b272f7e711ac05a5edde08ddc73f4e2d322
Jeremy Neal <hello@radglob.com>
2023-07-06 16:20:54 -0400

    Remove deprecated docs props.

--- docs/src/@primer/gatsby-theme-doctocat/mdx-components.js
+++ docs/src/@primer/gatsby-theme-doctocat/mdx-components.js
@@ -1,8 +1,6 @@
 import {Box, Link} from '@primer/react'
 import InlineCode from '@primer/gatsby-theme-doctocat/src/components/inline-code'
 import {ComponentChecklist} from '../../component-checklist'
-// eslint-disable-next-line import/no-deprecated
-import {Props} from '../../props'
 import {PropsTable} from '../../props-table'
 import {ComponentProps} from '../../component-props'
 
@@ -11,8 +9,6 @@ export default {
   Link,
   InlineCode,
   ComponentChecklist,
-  // eslint-disable-next-line import/no-deprecated
-  Props,
   PropsTable,
   ComponentProps,
   // HACK: MDX doesn't support rendering subcomponents with dot notation
