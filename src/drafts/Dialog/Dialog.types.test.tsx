7ce384e64cc7bd6e69b215bda2e322c90eb0d0ea
Mike Perrotti <mperrotti@github.com>
2023-07-14 14:57:02 -0400

    mv draft Dialog types test

--- src/drafts/Dialog/Dialog.types.test.tsx
+++ src/drafts/Dialog/Dialog.types.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Dialog} from '../drafts/Dialog/Dialog'
+import {Dialog} from './Dialog'
 
 export function shouldAcceptCallWithNoProps() {
   return <Dialog onClose={() => null} />
