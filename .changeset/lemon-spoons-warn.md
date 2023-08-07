2b37b5e986ae6fd7a02c19a2a070cf68cabfa161
Siddharth Kshetrapal <siddharthkp@github.com>
2023-08-07 15:25:54 +0200

    Revert "Revert "Show underline on Link when hovered or focused. (#3586)" (#3590)"
    This reverts commit eaf6dc9b101369c88142c37a4e4069fa00f39f95.

---
+++ .changeset/lemon-spoons-warn.md
@@ -0,0 +1,7 @@
+---
+'@primer/react': patch
+---
+
+Show underline on Link when hovered or focused.
+
+<!-- Changed components: Link -->
--- src/Link/Link.tsx
+++ src/Link/Link.tsx
@@ -24,7 +24,11 @@ const StyledLink = styled.a<StyledLinkProps>`
   color: ${props => (props.muted ? get('colors.fg.muted')(props) : get('colors.accent.fg')(props))};
   text-decoration: ${props => (props.underline ? 'underline' : 'none')};
   &:hover {
-    text-decoration: ${props => (props.muted ? 'none' : 'underline')};
+    text-decoration: underline;
+    ${props => (props.hoverColor ? hoverColor : props.muted ? `color: ${get('colors.accent.fg')(props)}` : '')};
+  }
+  &:focus {
+    text-decoration: underline;
     ${props => (props.hoverColor ? hoverColor : props.muted ? `color: ${get('colors.accent.fg')(props)}` : '')};
   }
   &:is(button) {
