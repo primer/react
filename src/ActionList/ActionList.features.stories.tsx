576161f4a884817fe535e1d9a5439cbe5f9261b7
Armagan Ersoz <broccolinisoup@github.com>
2023-08-09 11:22:53 +1000

    heading styles

--- src/ActionList/ActionList.features.stories.tsx
+++ src/ActionList/ActionList.features.stories.tsx
@@ -78,6 +78,42 @@ export const WithHeading = () => (
   </ActionList>
 )
 
+export const WithHeadingInFullVariant = () => (
+  <ActionList variant="full">
+    <ActionList.Heading as="h1">Details</ActionList.Heading>
+    <ActionList.LinkItem href="https://github.com/primer/react#readme">
+      <ActionList.LeadingVisual>
+        <BookIcon />
+      </ActionList.LeadingVisual>
+      Readme
+    </ActionList.LinkItem>
+    <ActionList.LinkItem href="https://github.com/primer/react/blob/main/LICENSE">
+      <ActionList.LeadingVisual>
+        <LawIcon />
+      </ActionList.LeadingVisual>
+      MIT License
+    </ActionList.LinkItem>
+    <ActionList.LinkItem href="https://github.com/primer/react/stargazers">
+      <ActionList.LeadingVisual>
+        <StarIcon />
+      </ActionList.LeadingVisual>
+      <strong>1.5k</strong> stars
+    </ActionList.LinkItem>
+    <ActionList.LinkItem href="https://github.com/primer/react/watchers">
+      <ActionList.LeadingVisual>
+        <EyeIcon />
+      </ActionList.LeadingVisual>
+      <strong>21</strong> watching
+    </ActionList.LinkItem>
+    <ActionList.LinkItem href="https://github.com/primer/react/network/members">
+      <ActionList.LeadingVisual>
+        <RepoForkedIcon />
+      </ActionList.LeadingVisual>
+      <strong>225</strong> forks
+    </ActionList.LinkItem>
+  </ActionList>
+)
+
 export const WithHeadingInMenu = () => (
   <ActionMenu>
     <ActionMenu.Button>Open menu</ActionMenu.Button>
--- src/ActionList/Heading.tsx
+++ src/ActionList/Heading.tsx
@@ -8,35 +8,53 @@ import {ListContext} from './List'
 
 export type ActionListHeadingProps = {
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  /**
+   * Style variations. Usage is discretionary.
+   *
+   * - `"filled"` - Superimposed on a background, offset from nearby content
+   * - `"subtle"` - Relatively less offset from nearby content
+   */
+  variant?: 'subtle' | 'filled'
 } & SxProp
 
-export const Heading = forwardRef(({as: Component = 'h3', children, sx = defaultSxProp, ...props}, forwardedRef) => {
-  const innerRef = React.useRef<HTMLHeadingElement>(null)
-  useRefObjectAsForwardedRef(forwardedRef, innerRef)
+export const Heading = forwardRef(
+  ({as: Component = 'h3', children, sx = defaultSxProp, variant = 'subtle', ...props}, forwardedRef) => {
+    const innerRef = React.useRef<HTMLHeadingElement>(null)
+    useRefObjectAsForwardedRef(forwardedRef, innerRef)
 
-  const {headingId: headingId} = React.useContext(ListContext)
+    const {headingId: headingId, variant: listVariant} = React.useContext(ListContext)
 
-  const styles = {
-    paddingY: '6px',
-    paddingX: 2,
-    fontSize: 0,
-    fontWeight: 'bold',
-    color: 'fg.muted',
-    listStyle: 'none',
-  }
+    const styles = {
+      paddingY: 2,
+      paddingX: listVariant === 'full' ? 2 : 3,
+      fontSize: 0,
+      fontWeight: 'bold',
+      color: 'fg.muted',
+      listStyle: 'none',
+      lineHeight: 'calc(20 / 12)',
+      ...(variant === 'filled' && {
+        backgroundColor: 'canvas.subtle',
+        marginX: 0,
+        marginBottom: 2,
+        borderTop: '1px solid',
+        borderBottom: '1px solid',
+        borderColor: 'neutral.muted',
+      }),
+    }
 
-  return (
-    <HeadingComponent
-      as={Component}
-      ref={innerRef}
-      // use custom id if it is provided. Otherwise, use the id from the context
-      id={props.id ?? headingId}
-      sx={merge<BetterSystemStyleObject>(styles, sx)}
-      {...props}
-    >
-      {children}
-    </HeadingComponent>
-  )
-}) as PolymorphicForwardRefComponent<'h3', ActionListHeadingProps>
+    return (
+      <HeadingComponent
+        as={Component}
+        ref={innerRef}
+        // use custom id if it is provided. Otherwise, use the id from the context
+        id={props.id ?? headingId}
+        sx={merge<BetterSystemStyleObject>(styles, sx)}
+        {...props}
+      >
+        {children}
+      </HeadingComponent>
+    )
+  },
+) as PolymorphicForwardRefComponent<'h3', ActionListHeadingProps>
 
 Heading.displayName = 'ActionList.Heading'
