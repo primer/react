2457e2eab6d1294eb2d2ab158a4144cd5d623445
Tyler Jones <tylerjdev@github.com>
2023-08-10 13:19:34 -0400

    Adjust MarkdownViewer story

--- src/drafts/MarkdownViewer/MarkdownViewer.stories.tsx
+++ src/drafts/MarkdownViewer/MarkdownViewer.stories.tsx
@@ -73,11 +73,11 @@ const sampleHtml = `
 <li><a href="https://primer.style" rel="nofollow">Primer</a></li>
 <li><a href="https://www.githubuniverse.com/" rel="nofollow">Universe</a></li>
 </ul>
-<h4 id="tasks">Tasks</h4>
+<h4>Tasks</h4>
 <ul class="contains-task-list">
-<li class="task-list-item"><input type="checkbox" aria-labelledby="tasks" id="" disabled="" class="task-list-item-checkbox"> Task 1</li>
-<li class="task-list-item"><input type="checkbox" aria-labelledby="tasks" id="" disabled="" class="task-list-item-checkbox"> Task 2</li>
-<li class="task-list-item"><input type="checkbox" aria-labelledby="tasks" id="" disabled="" class="task-list-item-checkbox"> Task 3</li>
+<li class="task-list-item"><label><input type="checkbox" id="" disabled="" class="task-list-item-checkbox"> Task 1</label></li>
+<li class="task-list-item"><label><input type="checkbox" id="" disabled="" class="task-list-item-checkbox"> Task 2</label></li>
+<li class="task-list-item"><label><input type="checkbox" id="" disabled="" class="task-list-item-checkbox"> Task 3</label></li>
 </ul>`
 
 const htmlObject = {__html: sampleHtml}
