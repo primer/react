d0989ed2a47aeea07460d6236d25126712f44c2a
Armagan Ersoz <broccolinisoup@github.com>
2023-08-29 17:45:26 +1000

    update comments

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -66,11 +66,12 @@ export function generateCustomSxProp(
   props: Partial<Pick<ButtonProps, 'size' | 'block' | 'leadingIcon' | 'trailingIcon' | 'trailingAction'>>,
   providedSx: BetterSystemStyleObject,
 ) {
-  // Possible data attributes for button component: data-size, data-block, data-no-visuals
+  // Possible data attributes: data-size, data-block, data-no-visuals
   const size = props.size && props.size !== 'medium' ? `[data-size="${props.size}"]` : '' // medium is a default size therefore it doesn't have a data attribute that used for styling
   const block = props.block ? `[data-block="block"]` : ''
   const noVisuals = props.leadingIcon || props.trailingIcon || props.trailingAction ? '' : '[data-no-visuals="true"]'
-  // We need to make sure we add the data attributes to the base css class (& -> &[data-attributename="value"]]) otherwise sx styles are applied due to data attribute has more specificity than class selector
+
+  // this is a custom selector. We need to make sure we add the data attributes to the base css class (& -> &[data-attributename="value"]])
   const cssSelector = `&${size}${block}${noVisuals}` // &[data-size="small"][data-block="block"][data-no-visuals="true"]
 
   const customSxProp: {
@@ -87,11 +88,12 @@ export function generateCustomSxProp(
   if (props.trailingIcon) cssSelectorList.push(`& [data-component="trailingVisual"]`)
   if (props.trailingAction) cssSelectorList.push(`& [data-component="trailingAction"]`)
 
-  // @ts-ignore - We are sure that color exists in providedSx
+  // @ts-ignore for now
   const {color} = providedSx
   if (!color) return customSxProp
   for (const selector of cssSelectorList) {
-    customSxProp[selector] = {color} // We only want to apply the color to the children of button component, because it is the one that we are overriding with data attributes that has more specificity than the parent button component.
+    // We only want to apply the color to the children of button component, because it is the one that we are overriding with data attributes that has more specificity than the parent button component.https://github.com/primer/react/blob/main/src/Button/styles.ts#L29
+    customSxProp[selector] = {color}
   }
   return customSxProp
 }
