b7030bc3caa5aa578606c0345900479fe5a22b66
Amanda Brown <amanda.brown@testdouble.com>
2023-09-12 19:07:15 -0400

    Update src/drafts/MarkdownEditor/MarkdownEditor.features.stories.tsx
    Co-authored-by: Armağan <broccolinisoup@github.com>

--- src/drafts/MarkdownEditor/MarkdownEditor.features.stories.tsx
+++ src/drafts/MarkdownEditor/MarkdownEditor.features.stories.tsx
@@ -1,10 +1,8 @@
 import {DiffIcon, PlusIcon} from '@primer/octicons-react'
 import React, {Meta} from '@storybook/react'
 import {useRef, useState} from 'react'
-import BaseStyles from '../../BaseStyles'
 import Box from '../../Box'
 import MarkdownEditor, {Emoji, Mentionable, Reference, SavedReply} from '.'
-import ThemeProvider from '../../ThemeProvider'
 
 const meta: Meta = {
   title: 'Drafts/Components/MarkdownEditor/Features',
