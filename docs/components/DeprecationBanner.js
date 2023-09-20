034b480a7a4e450020e1497f8e30bc8128b77e84
Cameron Dutro <camertron@gmail.com>
2023-09-20 13:42:05 -0700

    Update docs/components/DeprecationBanner.js
    Co-authored-by: Josep Martins <josepmartins@github.com>

--- docs/components/DeprecationBanner.js
+++ docs/components/DeprecationBanner.js
@@ -4,10 +4,9 @@ import {Note} from '@primer/gatsby-theme-doctocat'
 
 const DeprecationBanner = ({replacementUrl}) => {
   return (
-    <Note variant="warning" sx={{marginBottom: '40px'}}>
-      <strong>NOTE:</strong> The information on this page is moving to a new home. Please update your links and
-      bookmarks! The new content can be found{' '}
-      <GatsbyLink href={replacementUrl} target="_blank">
+    <Note variant="warning" sx={{marginBottom: '40px'}}> 
+      This documentation is moving to a new home. Please update any link or bookmark that points to this page. The new content can be found{' '}
+      <GatsbyLink href={replacementUrl} >
         here
       </GatsbyLink>
       .
