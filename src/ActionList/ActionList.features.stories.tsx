c68c0cc77312935c0f26cf900e565d773a40e07b
Armagan Ersoz <broccolinisoup@github.com>
2023-09-28 08:53:59 +1000

    remove the full variant story because there is no full variant list heading

--- src/ActionList/ActionList.features.stories.tsx
+++ src/ActionList/ActionList.features.stories.tsx
@@ -96,42 +96,6 @@ export const WithVisualListHeading = () => (
   </ActionList>
 )
 
-export const WithHeadingInFullVariant = () => (
-  <ActionList variant="full">
-    <ActionList.Heading as="h1">Details</ActionList.Heading>
-    <ActionList.LinkItem href="https://github.com/primer/react#readme">
-      <ActionList.LeadingVisual>
-        <BookIcon />
-      </ActionList.LeadingVisual>
-      Readme
-    </ActionList.LinkItem>
-    <ActionList.LinkItem href="https://github.com/primer/react/blob/main/LICENSE">
-      <ActionList.LeadingVisual>
-        <LawIcon />
-      </ActionList.LeadingVisual>
-      MIT License
-    </ActionList.LinkItem>
-    <ActionList.LinkItem href="https://github.com/primer/react/stargazers">
-      <ActionList.LeadingVisual>
-        <StarIcon />
-      </ActionList.LeadingVisual>
-      <strong>1.5k</strong> stars
-    </ActionList.LinkItem>
-    <ActionList.LinkItem href="https://github.com/primer/react/watchers">
-      <ActionList.LeadingVisual>
-        <EyeIcon />
-      </ActionList.LeadingVisual>
-      <strong>21</strong> watching
-    </ActionList.LinkItem>
-    <ActionList.LinkItem href="https://github.com/primer/react/network/members">
-      <ActionList.LeadingVisual>
-        <RepoForkedIcon />
-      </ActionList.LeadingVisual>
-      <strong>225</strong> forks
-    </ActionList.LinkItem>
-  </ActionList>
-)
-
 export const WithCustomHeading = () => (
   <>
     <Heading as="h1" id="list-heading" sx={{fontSize: 3, marginX: 3}}>
