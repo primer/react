65e27040a01318a8d2ec89a572dbd003ec0fa19b
langermank <18661030+langermank@users.noreply.github.com>
2023-10-05 09:43:34 -0700

    cleanup

--- src/legacy-theme/js/index.js
+++ src/legacy-theme/js/index.js
@@ -1,17 +1,3 @@
-/* eslint-disable camelcase */
-/* eslint-disable no-invalid-this */
-/* eslint-disable no-var */
-// 'use strict'
-// var __importDefault =
-//   (this && this.__importDefault) ||
-//   function (mod) {
-//     return mod && mod.__esModule ? mod : {default: mod}
-//   }
-// Object.defineProperty(exports, '__esModule', {value: true})
-// const colors_1 = __importDefault(require('./colors'))
-// const typography_1 = __importDefault(require('./typography'))
-// exports.default = {colors: colors_1.default, typography: typography_1.default}
-
 import colors from './colors'
 import typography from './typography'
 
--- src/legacy-theme/js/typography/index.js
+++ src/legacy-theme/js/typography/index.js
@@ -1,22 +1,3 @@
-/* eslint-disable camelcase */
-/* eslint-disable no-invalid-this */
-/* eslint-disable no-var */
-// 'use strict'
-// var __importDefault =
-//   (this && this.__importDefault) ||
-//   function (mod) {
-//     return mod && mod.__esModule ? mod : {default: mod}
-//   }
-// Object.defineProperty(exports, '__esModule', {value: true})
-// const normal_1 = __importDefault(require('./normal'))
-// exports.default = {normal: normal_1.default}
-
-// import normal from './normal'
-
-// export default {
-//   normal,
-// }
-
 const lineHeight = {
   condensedUltra: 1,
   condensed: 1.25,
