f3d6e4f3026756a186123b4a20c22074c832cadf
David Strack <davidstrack@gmail.com>
2023-11-07 11:58:37 -0600

    use fixed border on hover

--- src/AvatarStack/AvatarStack.tsx
+++ src/AvatarStack/AvatarStack.tsx
@@ -137,8 +137,7 @@ const AvatarStackWrapper = styled.span<StyledAvatarStackWrapperProps>`
       margin-left: ${get('space.1')};
       opacity: 100%;
       visibility: visible;
-      box-shadow: 0 0 0 var(--avatar-border-width)
-        ${props => (props.count === 1 ? get('colors.avatar.border') : get('colors.canvas.default'))};
+      box-shadow: 0 0 0 4px ${props => (props.count === 1 ? get('colors.avatar.border') : get('colors.canvas.default'))};
       transition:
         margin 0.2s ease-in-out,
         opacity 0.2s ease-in-out,
