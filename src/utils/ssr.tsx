d10aa5f881d20fcb13195a38805b5bde23b6574f
Josh Black <joshblack@users.noreply.github.com>
2023-11-28 12:30:47 -0600

    chore: update type for SSRProvider

--- src/utils/ssr.tsx
+++ src/utils/ssr.tsx
@@ -1,7 +1,7 @@
 import type {PropsWithChildren} from 'react'
 import {useId} from '../hooks/useId'
 
-export function SSRProvider({children}: PropsWithChildren<{}>) {
+export function SSRProvider({children}: PropsWithChildren) {
   return children
 }
 
