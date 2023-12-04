ecc8b7fe00dec5a53b785688d3e3f9b0c147805f
Keith Cirkel <keithamus@users.noreply.github.com>
2023-12-04 16:31:35 +0000

    ensure hostelement is connected to DOM for confirmation dialogs

--- src/ConfirmationDialog/ConfirmationDialog.tsx
+++ src/ConfirmationDialog/ConfirmationDialog.tsx
@@ -145,11 +145,14 @@ export const ConfirmationDialog: React.FC<React.PropsWithChildren<ConfirmationDi
   )
 }
 
+let hostElement: Element | null = null
 export type ConfirmOptions = Omit<ConfirmationDialogProps, 'onClose'> & {content: React.ReactNode}
 async function confirm(themeProps: ThemeProviderProps, options: ConfirmOptions): Promise<boolean> {
   const {content, ...confirmationDialogProps} = options
   return new Promise(resolve => {
-    const root = createRoot(document.createElement('div'))
+    hostElement = document.createElement('div')
+    if (!hostElement.isConnected) document.body.append(hostElement)
+    const root = createRoot(hostElement)
     const onClose: ConfirmationDialogProps['onClose'] = gesture => {
       root.unmount()
       if (gesture === 'confirm') {
