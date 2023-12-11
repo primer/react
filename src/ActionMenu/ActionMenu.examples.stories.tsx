68269c75f53db6f5feb628e5d488e7a5dc709cb4
Mike Perrotti <mperrotti@github.com>
2023-12-11 12:22:53 -0500

    improves docs for inactive actionlist items

--- src/ActionMenu/ActionMenu.examples.stories.tsx
+++ src/ActionMenu/ActionMenu.examples.stories.tsx
@@ -13,6 +13,11 @@ import {
   XIcon,
   CheckIcon,
   CopyIcon,
+  ArchiveIcon,
+  BookIcon,
+  CommentIcon,
+  RocketIcon,
+  WorkflowIcon,
 } from '@primer/octicons-react'
 
 export default {
@@ -330,6 +335,61 @@ export const OnRightSide = () => (
           Delete file
           <ActionList.TrailingVisual>⌘D</ActionList.TrailingVisual>
         </ActionList.Item>
+      </ActionList>
+    </ActionMenu.Overlay>
+  </ActionMenu>
+)
+
+export const OnlyInactiveItems = () => (
+  <ActionMenu>
+    <ActionMenu.Button inactive>Open menu</ActionMenu.Button>
+    <ActionMenu.Overlay width="auto">
+      <ActionList>
+        <ActionList.Item onSelect={() => alert('Workflows clicked')} inactiveText="Unavailable due to an outage">
+          Workflows
+          <ActionList.LeadingVisual>
+            <WorkflowIcon />
+          </ActionList.LeadingVisual>
+        </ActionList.Item>
+        <ActionList.Item onSelect={() => alert('Archived items clicked')} inactiveText="Unavailable due to an outage">
+          Archived items
+          <ActionList.LeadingVisual>
+            <ArchiveIcon />
+          </ActionList.LeadingVisual>
+        </ActionList.Item>
+        <ActionList.LinkItem href="/" inactiveText="Unavailable due to an outage">
+          Settings
+          <ActionList.LeadingVisual>
+            <GearIcon />
+          </ActionList.LeadingVisual>
+        </ActionList.LinkItem>
+        <ActionList.Item onSelect={() => alert('Make a copy clicked')} inactiveText="Unavailable due to an outage">
+          Make a copy
+          <ActionList.LeadingVisual>
+            <CopyIcon />
+          </ActionList.LeadingVisual>
+        </ActionList.Item>
+        <ActionList.Divider />
+        <ActionList.Group title="Github projects">
+          <ActionList.LinkItem href="/" inactiveText="Unavailable due to an outage">
+            What&apos;s new
+            <ActionList.LeadingVisual>
+              <RocketIcon />
+            </ActionList.LeadingVisual>
+          </ActionList.LinkItem>
+          <ActionList.LinkItem href="/" inactiveText="Unavailable due to an outage">
+            Give feedback
+            <ActionList.LeadingVisual>
+              <CommentIcon />
+            </ActionList.LeadingVisual>
+          </ActionList.LinkItem>
+          <ActionList.LinkItem href="/" inactiveText="Unavailable due to an outage">
+            GitHub Docs
+            <ActionList.LeadingVisual>
+              <BookIcon />
+            </ActionList.LeadingVisual>
+          </ActionList.LinkItem>
+        </ActionList.Group>
       </ActionList>
     </ActionMenu.Overlay>
   </ActionMenu>
