5de27294e9981a90e5dc6dbe0aaa1347758190a4
Siddharth Kshetrapal <siddharthkp@github.com>
2023-12-18 16:33:22 +0100

    Revert "add temporary example for question"
    This reverts commit 19bc49249ce474343fe99744c9b68de168874736.

--- src/drafts/SelectPanel2/stories/SelectPanel.examples.stories.tsx
+++ src/drafts/SelectPanel2/stories/SelectPanel.examples.stories.tsx
@@ -66,61 +66,6 @@ export const Minimal = () => {
   )
 }
 
-export const Temporary = () => {
-  const initialSelectedLabels = data.issue.labelIds // mock initial state: has selected labels
-  const [selectedLabelIds, setSelectedLabelIds] = React.useState<string[]>(initialSelectedLabels)
-
-  /* Selection */
-  const onLabelSelect = (labelId: string) => {
-    if (!selectedLabelIds.includes(labelId)) setSelectedLabelIds([...selectedLabelIds, labelId])
-    else setSelectedLabelIds(selectedLabelIds.filter(id => id !== labelId))
-  }
-
-  const onSubmit = () => {
-    data.issue.labelIds = selectedLabelIds // pretending to persist changes
-
-    // eslint-disable-next-line no-console
-    console.log('form submitted')
-  }
-
-  const sortingFn = (itemA: {id: string}, itemB: {id: string}) => {
-    const initialSelectedIds = data.issue.labelIds
-    if (initialSelectedIds.includes(itemA.id) && initialSelectedIds.includes(itemB.id)) return 1
-    else if (initialSelectedIds.includes(itemA.id)) return -1
-    else if (initialSelectedIds.includes(itemB.id)) return 1
-    else return 1
-  }
-
-  const onClearSelection = () => setSelectedLabelIds([])
-
-  const itemsToShow = data.labels.sort(sortingFn)
-
-  return (
-    <>
-      <h1>Temporary SelectPanel</h1>
-
-      <SelectPanel title="Select labels" onSubmit={onSubmit} onClearSelection={onClearSelection}>
-        <SelectPanel.Button>Assign label</SelectPanel.Button>
-
-        <ActionList>
-          {itemsToShow.map(label => (
-            <ActionList.Item
-              key={label.id}
-              onSelect={() => onLabelSelect(label.id)}
-              selected={selectedLabelIds.includes(label.id)}
-            >
-              <ActionList.LeadingVisual>{getCircle(label.color)}</ActionList.LeadingVisual>
-              {label.name}
-              <ActionList.Description variant="block">{label.description}</ActionList.Description>
-            </ActionList.Item>
-          ))}
-        </ActionList>
-        <SelectPanel.Footer />
-      </SelectPanel>
-    </>
-  )
-}
-
 export const WithGroups = () => {
   /* Selection */
   const initialAssigneeIds = data.issue.assigneeIds // mock initial state
