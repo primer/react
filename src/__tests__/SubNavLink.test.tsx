beeb1c68ba98be88e87de8d2faa35ab7608165c3
Cam McHenry <camchenry@users.noreply.github.com>
2024-01-04 13:58:49 +0000

    Update tests

--- src/__tests__/SubNavLink.test.tsx
+++ src/__tests__/SubNavLink.test.tsx
@@ -7,14 +7,24 @@ import {axe, toHaveNoViolations} from 'jest-axe'
 expect.extend(toHaveNoViolations)
 
 describe('SubNav.Link', () => {
-  behavesAsComponent({Component: SubNav.Link})
+  // This component does respect the `as` prop, but it affects the link inside, rather than the wrapping element,
+  // which causes the test to fail.
+  behavesAsComponent({Component: SubNav.Link, options: {skipAs: true}})
 
-  it('renders an <a> by default', () => {
-    expect(render(<SubNav.Link />).type).toEqual('a')
+  it('renders an <li> + <a> by default', () => {
+    const result = render(<SubNav.Link />)
+    expect(result.type).toEqual('li')
+    const firstChild = result.children![0]!
+    if (typeof firstChild !== 'object') throw new Error(`unexpected child type: ${typeof firstChild}`)
+    expect(firstChild.type).toEqual('a')
   })
 
   it('should have no axe violations', async () => {
-    const {container} = HTMLRender(<SubNav.Link />)
+    const {container} = HTMLRender(
+      <SubNav.Links>
+        <SubNav.Link />
+      </SubNav.Links>,
+    )
     const results = await axe(container)
     expect(results).toHaveNoViolations()
   })
