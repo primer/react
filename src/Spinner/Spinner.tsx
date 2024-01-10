fe7f92daef975e9f273af481f03e5cf66cffe3c4
Mike Perrotti <mperrotti@github.com>
2024-01-10 15:18:41 -0500

    updates Spinner to accept screen reader text

--- src/Spinner/Spinner.tsx
+++ src/Spinner/Spinner.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import sx, {SxProp} from '../sx'
 import {ComponentProps} from '../utils/types'
+import {VisuallyHidden} from '../internal/components/VisuallyHidden'
 
 const sizeMap = {
   small: '16px',
@@ -12,30 +13,34 @@ const sizeMap = {
 export interface SpinnerInternalProps {
   /** Sets the width and height of the spinner. */
   size?: keyof typeof sizeMap
+  srText?: string
 }
 
-function Spinner({size: sizeKey = 'medium', ...props}: SpinnerInternalProps) {
+function Spinner({size: sizeKey = 'medium', srText = 'Loading', ...props}: SpinnerInternalProps) {
   const size = sizeMap[sizeKey]
 
   return (
-    <svg height={size} width={size} viewBox="0 0 16 16" fill="none" {...props}>
-      <circle
-        cx="8"
-        cy="8"
-        r="7"
-        stroke="currentColor"
-        strokeOpacity="0.25"
-        strokeWidth="2"
-        vectorEffect="non-scaling-stroke"
-      />
-      <path
-        d="M15 8a7.002 7.002 0 00-7-7"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        vectorEffect="non-scaling-stroke"
-      />
-    </svg>
+    <span role="status">
+      <svg height={size} width={size} viewBox="0 0 16 16" fill="none" aria-hidden {...props}>
+        <circle
+          cx="8"
+          cy="8"
+          r="7"
+          stroke="currentColor"
+          strokeOpacity="0.25"
+          strokeWidth="2"
+          vectorEffect="non-scaling-stroke"
+        />
+        <path
+          d="M15 8a7.002 7.002 0 00-7-7"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          vectorEffect="non-scaling-stroke"
+        />
+      </svg>
+      <VisuallyHidden>{srText}</VisuallyHidden>
+    </span>
   )
 }
 
