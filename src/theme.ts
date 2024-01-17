6dcc321ca4c7df191271191cd3faa5a94c0ba787
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-01-17 13:13:02 -0800

    Update src/theme.ts
    Co-authored-by: Josh Black <joshblack@github.com>

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,6 @@ import {KeyPaths} from './utils/types/KeyPaths'
 import {colors} from './legacy-theme/ts/colors'
 import {partitionColors, fontStack, omitScale} from './utils/theme'
 
-// const {lineHeight: lineHeights} = primitives.typography
 
 const animation = {
   easeOutCubic: 'cubic-bezier(0.33, 1, 0.68, 1)',
