6b4f43ceaf85cc7c9f0a868c530a6770be8d021d
langermank <18661030+langermank@users.noreply.github.com>
2024-01-26 11:52:05 -0800

    add dismiss option

--- src/Flash/Flash.features.stories.tsx
+++ src/Flash/Flash.features.stories.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import {ComponentMeta} from '@storybook/react'
 import Flash from './Flash'
 import Octicon from '../Octicon'
-import {InfoIcon} from '@primer/octicons-react'
-import {Button} from '../Button'
+import {InfoIcon, XIcon} from '@primer/octicons-react'
+import {Button, IconButton} from '../Button'
 import Link from '../Link'
 import Box from '../Box'
 
@@ -27,6 +27,52 @@ export const WithIconAndAction = () => (
       gridTemplateColumns: 'min-content 1fr minmax(0, auto)',
       gridTemplateRows: 'min-content',
       gridTemplateAreas: `'visual message actions'`,
+      '@media screen and (max-width: 543.98px)': {
+        gridTemplateColumns: 'min-content 1fr',
+        gridTemplateRows: 'min-content min-content',
+        gridTemplateAreas: `
+        'visual message'
+        '.      actions'
+      `,
+      },
+    }}
+  >
+    <Box sx={{display: 'grid', paddingBlock: 'var(--base-size-8)', alignSelf: 'start', gridArea: 'visual'}}>
+      <Octicon icon={InfoIcon} />
+    </Box>
+    <Box
+      sx={{
+        fontSize: 1,
+        lineHeight: '1.5',
+        padding: '0.375rem var(--base-size-8)',
+        alignSelf: 'center',
+        gridArea: 'message',
+      }}
+    >
+      This is a flash message with an icon and an action.
+      <Link href="/"> Learn more.</Link>
+    </Box>
+    <Box
+      sx={{
+        gridArea: 'actions',
+        '@media screen and (max-width: 543.98px)': {
+          alignSelf: 'start',
+          margin: 'var(--base-size-8) 0 0 var(--base-size-8)',
+        },
+      }}
+    >
+      <Button>Join waitlist</Button>
+    </Box>
+  </Flash>
+)
+
+export const WithIconActionDismiss = () => (
+  <Flash
+    sx={{
+      display: 'grid',
+      gridTemplateColumns: 'min-content 1fr minmax(0, auto)',
+      gridTemplateRows: 'min-content',
+      gridTemplateAreas: `'visual message actions close'`,
       '@media screen and (max-width: 543.98px)': {
         gridTemplateColumns: 'min-content 1fr',
         gridTemplateRows: 'min-content min-content',
@@ -62,6 +108,14 @@ export const WithIconAndAction = () => (
       }}
     >
       <Button>Join waitlist</Button>
+    </Box>
+    <Box
+      sx={{
+        gridArea: 'close',
+        marginLeft: 'var(--controlStack-medium-gap-condensed)',
+      }}
+    >
+      <IconButton variant="invisible" icon={XIcon} aria-label="Dismiss" sx={{svg: {margin: '0', color: 'fg.muted'}}} />
     </Box>
   </Flash>
 )
