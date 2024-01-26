e02619c796df7b81bac958210a71733a9921d4fc
langermank <18661030+langermank@users.noreply.github.com>
2024-01-26 11:36:48 -0800

    add example

--- src/Flash/Flash.features.stories.tsx
+++ src/Flash/Flash.features.stories.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import {ComponentMeta} from '@storybook/react'
 import Flash from './Flash'
+import Octicon from '../Octicon'
+import {InfoIcon} from '@primer/octicons-react'
+import {Button} from '../Button'
+import Link from '../Link'
+import Box from '../Box'
 
 export default {
   title: 'Components/Flash/Features',
@@ -14,3 +19,49 @@ export const Danger = () => <Flash variant="danger">Danger</Flash>
 export const Warning = () => <Flash variant="warning">Warning</Flash>
 
 export const Full = () => <Flash full>Full</Flash>
+
+export const WithIconAndAction = () => (
+  <Flash
+    sx={{
+      display: 'grid',
+      gridTemplateColumns: 'min-content 1fr minmax(0, auto)',
+      gridTemplateRows: 'min-content',
+      gridTemplateAreas: `'visual message actions'`,
+      '@media screen and (max-width: 543.98px)': {
+        gridTemplateColumns: 'min-content 1fr',
+        gridTemplateRows: 'min-content min-content',
+        gridTemplateAreas: `
+        'visual message close'
+        '.      actions actions'
+      `,
+      },
+    }}
+  >
+    <Box sx={{display: 'grid', paddingBlock: 'var(--base-size-8)', alignSelf: 'start', gridArea: 'visual'}}>
+      <Octicon icon={InfoIcon} />
+    </Box>
+    <Box
+      sx={{
+        fontSize: 1,
+        lineHeight: '1.5',
+        padding: '0.375rem var(--base-size-8)',
+        alignSelf: 'center',
+        gridArea: 'message',
+      }}
+    >
+      This is a flash message with an icon and an action.
+      <Link href="/"> Learn more.</Link>
+    </Box>
+    <Box
+      sx={{
+        gridArea: 'actions',
+        '@media screen and (max-width: 543.98px)': {
+          alignSelf: 'start',
+          margin: 'var(--base-size-8) 0 0 var(--base-size-8)',
+        },
+      }}
+    >
+      <Button>Join waitlist</Button>
+    </Box>
+  </Flash>
+)
