b2b720b247651c0b84de775990c611860aaf06bc
Josh Black <joshblack@users.noreply.github.com>
2024-01-26 13:12:51 -0600

    feat: add global helpers to live-region-element

--- src/internal/components/LiveRegion/live-region-element/index.ts
+++ src/internal/components/LiveRegion/live-region-element/index.ts
@@ -1,2 +1,47 @@
-export {LiveRegionElement, templateContent} from './element'
-export type {AnnounceOptions} from './element'
+import {LiveRegionElement, templateContent} from './element'
+import type {AnnounceOptions} from './element'
+
+type GlobalAnnounceOptions = AnnounceOptions & {
+  appendTo?: HTMLElement
+  from?: HTMLElement
+}
+
+export function announce(message: string, options: GlobalAnnounceOptions = {}) {
+  const liveRegion = findOrCreateLiveRegion(options.from)
+  liveRegion.announce(message, options)
+}
+
+export function announceFromElement(element: HTMLElement, options: GlobalAnnounceOptions = {}) {
+  const liveRegion = findOrCreateLiveRegion(options.from ?? element)
+  liveRegion.announceFromElement(element, options)
+}
+
+let liveRegion: LiveRegionElement | null = null
+
+export function findOrCreateLiveRegion(from?: HTMLElement, appendTo?: HTMLElement): LiveRegionElement {
+  if (liveRegion !== null) {
+    return liveRegion
+  }
+
+  liveRegion = from?.closest('live-region') ?? null
+  if (liveRegion !== null) {
+    return liveRegion as LiveRegionElement
+  }
+
+  liveRegion = document.querySelector('live-region')
+  if (liveRegion !== null) {
+    return liveRegion as LiveRegionElement
+  }
+
+  liveRegion = document.createElement('live-region') as LiveRegionElement
+  if (appendTo) {
+    appendTo.appendChild(liveRegion)
+  } else {
+    document.documentElement.appendChild(liveRegion)
+  }
+
+  return liveRegion
+}
+
+export {LiveRegionElement, templateContent}
+export type {AnnounceOptions}
