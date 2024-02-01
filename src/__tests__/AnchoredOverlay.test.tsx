0448c28926fa807716d0ceca57b43f355818d995
Owen Niblock <owenniblock@github.com>
2024-02-01 11:48:38 +0000

    Fix console.error call in test

--- src/__tests__/AnchoredOverlay.test.tsx
+++ src/__tests__/AnchoredOverlay.test.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback, useState} from 'react'
 import {AnchoredOverlay} from '../AnchoredOverlay'
 import {behavesAsComponent, checkExports} from '../utils/testing'
-import {render as HTMLRender, fireEvent} from '@testing-library/react'
+import {render as HTMLRender, act, fireEvent} from '@testing-library/react'
 import {axe} from 'jest-axe'
 import {SSRProvider} from '../index'
 import {Button} from '../deprecated'
@@ -66,9 +66,12 @@ describe('AnchoredOverlay', () => {
   })
 
   it('should have no axe violations when open', async () => {
-    const {container} = HTMLRender(<AnchoredOverlayTestComponent initiallyOpen={true}></AnchoredOverlayTestComponent>)
-    const results = await axe(container)
-    expect(results).toHaveNoViolations()
+    act(async () => {
+      const {container} = HTMLRender(<AnchoredOverlayTestComponent initiallyOpen={true}></AnchoredOverlayTestComponent>)
+
+      const results = await axe(container)
+      expect(results).toHaveNoViolations()
+    })
   })
 
   it('should have no axe violations when closed', async () => {
