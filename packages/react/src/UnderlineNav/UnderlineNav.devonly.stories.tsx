d35c3385e88249657c714fb110ae2ee615662638
Paul Jacks <Rebstorm@users.noreply.github.com>
2024-02-05 10:13:58 +0100

    removing test story

--- packages/react/src/UnderlineNav/UnderlineNav.devonly.stories.tsx
+++ packages/react/src/UnderlineNav/UnderlineNav.devonly.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import {ComponentStory, Meta, StoryFn} from '@storybook/react'
+import {Meta} from '@storybook/react'
 import {UnderlineNav} from './index'
-import {UnderlineNavItem} from './UnderlineNavItem'
 
 export default {
   title: 'Components/UnderlineNav/DevOnly',
@@ -13,19 +12,6 @@ export const IllegalState = () => {
       <UnderlineNav.Item aria-current="page">Code</UnderlineNav.Item>
       <UnderlineNav.Item aria-current="page">Issues</UnderlineNav.Item>
       <UnderlineNav.Item aria-current="page">Pull Requests</UnderlineNav.Item>
-    </UnderlineNav>
-  )
-}
-
-export const PopOverPosition: StoryFn<typeof UnderlineNav> = () => {
-  const children = ['Code', 'Pull requests', 'Actions', 'Projects', 'Wiki']
-  return (
-    <UnderlineNav aria-label="Repository">
-      {children.map((child: string, index: number) => (
-        <UnderlineNavItem key={index} href="#" aria-current={index === 0 ? 'page' : undefined}>
-          {child}
-        </UnderlineNavItem>
-      ))}
     </UnderlineNav>
   )
 }
