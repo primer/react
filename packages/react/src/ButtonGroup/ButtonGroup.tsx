3acbd80719141f277ce3a91295187c3a9ba5c2c5
Armağan <broccolinisoup@github.com>
2024-02-08 12:41:41 +1000

    ButtonGroup: Fix button and icon button styling when tooltips are used on them (#4210)
    * fix button group styling for tooltip and tooltip v2
    
    * Add changeset
    
    * take the a tag into account when styling
    
    * add e2e tests for the buttons groups that use tooltip
    
    * test(vrt): update snapshots
    
    ---------
    
    Co-authored-by: broccolinisoup <broccolinisoup@users.noreply.github.com>

--- packages/react/src/ButtonGroup/ButtonGroup.tsx
+++ packages/react/src/ButtonGroup/ButtonGroup.tsx
@@ -8,26 +8,30 @@ const ButtonGroup = styled.div`
   vertical-align: middle;
   isolation: isolate;
 
-  && > * {
+  && > [type='button'],
+  /* this specific selection is required to cover for tooltip. Because the buttons are wrapped with the tooltip span */
+  && > span[role='tooltip'] > [type='button'] {
     margin-inline-end: -1px;
     position: relative;
     border-radius: 0;
-
-    :first-child {
-      border-top-left-radius: ${get('radii.2')};
-      border-bottom-left-radius: ${get('radii.2')};
-    }
-
-    :last-child {
-      border-top-right-radius: ${get('radii.2')};
-      border-bottom-right-radius: ${get('radii.2')};
-    }
 
     :focus,
     :active,
     :hover {
       z-index: 1;
     }
+  }
+
+  && > [type='button']:first-child,
+  && > span[role='tooltip']:first-of-type > [type='button'] {
+    border-top-left-radius: ${get('radii.2')};
+    border-bottom-left-radius: ${get('radii.2')};
+  }
+
+  && > [type='button']:last-of-type,
+  && > span[role='tooltip']:last-of-type > [type='button'] {
+    border-top-right-radius: ${get('radii.2')};
+    border-bottom-right-radius: ${get('radii.2')};
   }
 
   ${sx};
