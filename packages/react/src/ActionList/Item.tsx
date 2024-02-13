e7cd953febd22e25f356863c6f9a99ccf78cd4ee
Siddharth Kshetrapal <siddharthkp@github.com>
2024-02-13 15:34:35 +0100

    immediately reset defaultPrevented

--- packages/react/src/ActionList/Item.tsx
+++ packages/react/src/ActionList/Item.tsx
@@ -229,7 +229,11 @@ export const Item = React.forwardRef<HTMLLIElement, ActionListItemProps>(
       (event: React.KeyboardEvent<HTMLLIElement>) => {
         if (disabled || inactive) return
         if ([' ', 'Enter'].includes(event.key)) {
-          if (event.key === ' ') event.preventDefault() // prevent scroll on space
+          if (event.key === ' ') {
+            event.preventDefault() // prevent scroll on space
+            // immediately reset defaultPrevented to not disturb control flow!
+            event.defaultPrevented = false
+          }
           onSelect(event, afterSelect)
         }
       },
