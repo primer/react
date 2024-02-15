ecdafed2fb17001caf53494364c83be2e2ccc813
Josh Black <joshblack@users.noreply.github.com>
2024-02-15 09:56:37 -0600

    feat: add feature flags exploration

---
+++ packages/react/src/FeatureFlags/FeatureFlags.tsx
@@ -0,0 +1,16 @@
+import React, {useContext, useMemo} from 'react'
+import {FeatureFlagContext} from './FeatureFlagContext'
+import {FeatureFlagScope, type FeatureFlags} from './FeatureFlagScope'
+
+export type FeatureFlagsProps = React.PropsWithChildren<{
+  flags: FeatureFlags
+}>
+
+export function FeatureFlags({children, flags}: FeatureFlagsProps) {
+  const parentScope = useContext(FeatureFlagContext)
+  const value = useMemo(() => {
+    const scope = FeatureFlagScope.create(flags)
+    return parentScope ? FeatureFlagScope.merge(parentScope, scope) : scope
+  }, [parentScope, flags])
+  return <FeatureFlagContext.Provider value={value}>{children}</FeatureFlagContext.Provider>
+}
