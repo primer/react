999a81554bbe8d7e4046f20552612fc92032bc72
Owen Niblock <owenniblock@github.com>
2024-02-15 16:04:48 +0000

    Fix TabPanels tests

--- e2e/components/TabPanels.test.ts
+++ e2e/components/TabPanels.test.ts
@@ -2,25 +2,25 @@ import {test, expect} from '@playwright/test'
 import {visit} from '../test-helpers/storybook'
 import {themes} from '../test-helpers/themes'
 
-test.describe('TabPages', () => {
+test.describe('TabPanels', () => {
   test.describe('Default', () => {
     for (const theme of themes) {
       test.describe(theme, () => {
         test('default @vrt', async ({page}) => {
           await visit(page, {
-            id: 'components-tabpages--default',
+            id: 'components-tabpanels--default',
             globals: {
               colorScheme: theme,
             },
           })
 
           // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`TabPages.Default.${theme}.png`)
+          expect(await page.screenshot()).toMatchSnapshot(`TabPanels.Default.${theme}.png`)
         })
 
         test('axe @aat', async ({page}) => {
           await visit(page, {
-            id: 'components-tabpages--default',
+            id: 'components-tabpanels--default',
             globals: {
               colorScheme: theme,
             },
