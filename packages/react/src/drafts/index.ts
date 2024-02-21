853709369141d180cfb2f0439dc4fe0c2abcb142
Owen Niblock <owenniblock@github.com>
2024-02-21 20:37:04 +0000

    Move to drafts (and consequently experimental)

--- packages/react/src/drafts/index.ts
+++ packages/react/src/drafts/index.ts
@@ -61,4 +61,6 @@ export type {
   NavListDividerProps,
 } from '../NavList'
 export * from './SelectPanel2'
+export {default as TabPanels} from '../TabPanels'
+export type {TabPanelsProps, TabPanelsTabProps, TabPanelsPanelProps} from '../TabPanels'
 export * from '../TooltipV2'
