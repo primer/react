ed4c52132453ee1f257b4bcad080d5bd2e418b10
Owen Niblock <owenniblock@github.com>
2024-02-22 13:59:41 +0000

    Minimise content flash when JS is slow

--- packages/react/src/TabPanels/TabPanels.features.stories.tsx
+++ packages/react/src/TabPanels/TabPanels.features.stories.tsx
@@ -24,13 +24,41 @@ export const Selected = () => (
 
 export const AdditionalContent = () => (
   <TabPanels aria-label="Main">
+    <Button>Non-tab content (before tabs)</Button>
     <TabPanels.Tab id="tab-1">One</TabPanels.Tab>
     <TabPanels.Tab id="tab-2">Two</TabPanels.Tab>
     <TabPanels.Tab id="tab-3">Three</TabPanels.Tab>
-    <Button>Non-tab content</Button>
+    <Button>Non-tab content (after tabs)</Button>
     <TabPanels.Panel aria-labelledby="tab-1">One</TabPanels.Panel>
     <TabPanels.Panel aria-labelledby="tab-2">Two</TabPanels.Panel>
     <TabPanels.Panel aria-labelledby="tab-3">Three</TabPanels.Panel>
     <div>Additional content after the panels</div>
+  </TabPanels>
+)
+
+export const ManyTabs = () => (
+  <TabPanels aria-label="Main">
+    <TabPanels.Tab id="tab-1">One</TabPanels.Tab>
+    <TabPanels.Tab id="tab-2">Two</TabPanels.Tab>
+    <TabPanels.Tab id="tab-3">Three</TabPanels.Tab>
+    <TabPanels.Tab id="tab-4" selected>Four</TabPanels.Tab>
+    <TabPanels.Tab id="tab-5">Five</TabPanels.Tab>
+    <TabPanels.Tab id="tab-6">Six</TabPanels.Tab>
+    <TabPanels.Tab id="tab-7">Seven</TabPanels.Tab>
+    <TabPanels.Tab id="tab-8">Eight</TabPanels.Tab>
+    <TabPanels.Tab id="tab-9">Nine</TabPanels.Tab>
+    <TabPanels.Tab id="tab-10">Ten</TabPanels.Tab>
+    <TabPanels.Tab id="tab-11">Eleven</TabPanels.Tab>
+    <TabPanels.Panel aria-labelledby="tab-1">One</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-2">Two</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-3">Three</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-4">Four</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-5">Five</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-6">Six</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-7">Seven</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-8">Eight</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-9">Nine</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-10">Ten</TabPanels.Panel>
+    <TabPanels.Panel aria-labelledby="tab-11">Eleven</TabPanels.Panel>
   </TabPanels>
 )
--- packages/react/src/TabPanels/TabPanels.tsx
+++ packages/react/src/TabPanels/TabPanels.tsx
@@ -12,41 +12,92 @@ import getGlobalFocusStyles from '../internal/utils/getGlobalFocusStyles'
 const TAB_CLASS = 'TabPanel-tab'
 
 const TabContainer = styled(createComponent(React, 'tab-container', TabContainerElement))`
+  & > :not([role=tabpanel]) {
+    display: inline-block;
+  }
+
   &::part(tablist-wrapper) {
     margin-top: 0;
     margin-bottom: 16px;
     border-bottom: 1px solid ${get('colors.border.default')};
   }
 
+  &:not(:defined) [role=tabpanel] {
+    margin-top: 17px;
+    display: none;
+  }
+
+  &:not(:defined) [role=tab]:nth-of-type(1)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(1),
+  &:not(:defined) [role=tab]:nth-of-type(2)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(2), 
+  &:not(:defined) [role=tab]:nth-of-type(3)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(3),
+  &:not(:defined) [role=tab]:nth-of-type(4)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(4),
+  &:not(:defined) [role=tab]:nth-of-type(5)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(5),
+  &:not(:defined) [role=tab]:nth-of-type(6)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(6),
+  &:not(:defined) [role=tab]:nth-of-type(7)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(7),
+  &:not(:defined) [role=tab]:nth-of-type(8)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(8),
+  &:not(:defined) [role=tab]:nth-of-type(9)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(9),
+  &:not(:defined) [role=tab]:nth-of-type(10)[aria-selected=true] ~ [role=tabpanel]:nth-of-type(10) {
+    display: block
+  }
+
+  &:not(:defined):not(:has([aria-selected=true])) [role=tabpanel]:first-of-type {
+    display: block
+  }
+
+  &:not(:has([aria-selected=true])) [role=tab] ~ [role=tab] {
+    color: ${get('colors.fg.muted')};
+    background-color: transparent;
+    border: 1px solid transparent;
+  }
+
+  &:not(:has([aria-selected=true])) [role=tab],
+  & [role=tab][aria-selected=true] {
+    color: ${get('colors.fg.default')};
+    border-color: ${get('colors.border.default')};
+    border-top-right-radius: ${get('radii.2')};
+    border-top-left-radius: ${get('radii.2')};
+    background-color: ${get('colors.canvas.default')};
+  }
+
+  &:not(:defined):not(:has([aria-selected=true])) [role=tab] ~ [role=tab],
+  &:not(:defined):has([aria-selected=true]) [role=tab]:not([aria-selected=true]) {
+    padding: 8px 16px;
+    border-bottom: 1px solid ${get('colors.border.default')};
+  }
+
+  &:not(:defined) :not([role=tabpanel]) {
+    vertical-align: top;
+  }
+
   ${sx};
 `
 
+//  
+
 export type TabPanelsProps = ComponentProps<typeof TabContainer>
 
-function TabPanels({children, 'aria-label': ariaLabel, onTabContainerChange, onTabContainerChanged}: TabPanelsProps) {
-  // Find all of the tabs, create a map of their IDs to their selected state
-  const tabMap = new Map<string, boolean>()
-  React.Children.forEach(children, child => {
-    if (React.isValidElement(child) && child.type === Tab) {
-      tabMap.set(child.props.id, !!child.props.selected)
-    }
-  })
-  
-  // Update children to set hidden on the panels which do not correspond to the selected tab
-  const updatedChildren = React.Children.map(children, child => {
-    if (React.isValidElement(child) && child.type === Panel) {
-      return React.cloneElement(child, {hidden: !child.props['aria-labelledby'] || !tabMap.get(child.props['aria-labelledby'])})
-    }
-    return child
-  })
-  
+function TabPanels({children, ...props}: TabPanelsProps) {
+  // // Find all of the tabs, create a map of their IDs to their selected state
+  // const tabMap = new Map<string, boolean>()
+  // for (const child of React.Children.toArray(children)) {
+  //   if (React.isValidElement(child) && child.type === Tab) {
+  //     tabMap.set(child.props.id, !!child.props.selected)
+  //   }
+  // }
+
+  // // Update children to set hidden on the panels which do not correspond to the selected tab
+  // const updatedChildren = React.Children.map(children, child => {
+  //   if (React.isValidElement(child) && child.type === Panel) {
+  //     return React.cloneElement(child, {
+  //       hidden: !child.props['aria-labelledby'] || !tabMap.get(child.props['aria-labelledby']),
+  //     })
+  //   }
+  //   return child
+  // })
+
   return (
-    <TabContainer
-      aria-label={ariaLabel}
-      onTabContainerChange={onTabContainerChange}
-      onTabContainerChanged={onTabContainerChanged}
-    >
-      {updatedChildren}
+    <TabContainer {...props}>
+      {children}
     </TabContainer>
   )
 }
@@ -84,14 +135,6 @@ const Tab = styled.button.attrs<TabPanelsTabProps>(props => ({
   &:hover {
     transition-duration: 0.1s;
     transition-property: color;
-  }
-
-  &[aria-selected='true'] {
-    color: ${get('colors.fg.default')};
-    border-color: ${get('colors.border.default')};
-    border-top-right-radius: ${get('radii.2')};
-    border-top-left-radius: ${get('radii.2')};
-    background-color: ${get('colors.canvas.default')};
   }
 
   ${sx};
