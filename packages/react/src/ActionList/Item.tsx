a77b45aabd39de7253a5d1f063172a9b1b654e7b
Tyler Jones <tylerjdev@github.com>
2024-03-01 11:05:11 -0500

    Switch selector to class, add to ActionMenu example

--- packages/react/src/ActionList/Item.tsx
+++ packages/react/src/ActionList/Item.tsx
@@ -174,7 +174,7 @@ export const Item = React.forwardRef<HTMLLIElement, ActionListItemProps>(
           color: getVariantStyles(variant, disabled, inactive).hoverColor,
           boxShadow: `inset 0 0 0 max(1px, 0.0625rem) ${theme?.colors.actionListItem.default.activeBorder}`,
         },
-        '&:focus-visible, > a:focus-visible, &:focus.focus-visible': {
+        '&:focus-visible, > a.focus-visible, &:focus.focus-visible': {
           outline: 'none',
           border: `2 solid`,
           boxShadow: `0 0 0 2px ${theme?.colors.accent.emphasis}`,
