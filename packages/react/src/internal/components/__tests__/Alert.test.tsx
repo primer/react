e0ff928540352ea7b28e12d4bb63e19aedd5f55c
Josh Black <joshblack@users.noreply.github.com>
2024-03-05 14:15:22 -0600

    chore(eslint): fix lint warnings

--- packages/react/src/internal/components/__tests__/Alert.test.tsx
+++ packages/react/src/internal/components/__tests__/Alert.test.tsx
@@ -1,6 +1,6 @@
 import {render, screen} from '@testing-library/react'
 import React from 'react'
-import {LiveRegionElement} from '@primer/live-region-element'
+import type {LiveRegionElement} from '@primer/live-region-element'
 import {Alert} from '../Alert'
 
 function getLiveRegion(): LiveRegionElement {
