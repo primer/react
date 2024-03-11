f31250d43e4050406cb41e6fbad9ae771dc697f8
Owen Niblock <owenniblock@github.com>
2024-03-11 11:52:24 +0000

    Where we are, here be dragons

--- packages/react/src/utils/custom-element.ts
+++ packages/react/src/utils/custom-element.ts
@@ -4,6 +4,18 @@ import type {EventName} from '@lit-labs/react'
 import {createComponent as create} from '@lit-labs/react'
 import sx, {type SxProp} from '../sx'
 
+export interface ElementRender {
+  renderShadow(): string
+  shadowRootOptions?: {
+    shadowrootmode?: 'open' | 'closed'
+    delegatesFocus?: boolean
+  }
+}
+
+function isElementRender(elementClass: unknown): elementClass is ElementRender {
+  return 'renderShadow' in elementClass && typeof elementClass.renderShadow === 'function'
+}
+
 type EventNames = Record<string, EventName | string>
 const rename = (str: string): string => str[0].toUpperCase() + str.slice(1).replace(/(-\w)/g, s => s[1].toUpperCase())
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -12,7 +24,7 @@ export const createComponent = <I extends HTMLElement, E extends EventNames = {}
   tagName: string,
   events: E | undefined = undefined,
 ) => {
-  const output = Object.assign(
+  const Output = Object.assign(
     styled(
       create<I, E>({
         tagName,
@@ -26,7 +38,17 @@ export const createComponent = <I extends HTMLElement, E extends EventNames = {}
     },
   )
 
-  return output as React.ComponentType<React.ComponentProps<typeof output> & SxProp>
+  if (isElementRender(elementClass)) {
+    return ({children, ...props}: React.ComponentProps<typeof Output> & SxProp) => {
+      const {shadowrootmode = 'open', ...templateProps} = elementClass.shadowRootOptions || {}
+      return React.createElement(Output, props, [
+        React.createElement('template', {shadowrootmode, ...templateProps}),
+        ...children,
+      ])
+    }
+  }
+
+  return Output as React.ComponentType<React.ComponentProps<typeof Output> & SxProp>
 }
 
 export default createComponent
