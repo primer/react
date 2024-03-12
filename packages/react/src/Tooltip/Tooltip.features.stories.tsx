11c9fcdc95f0795bb5ddca6c8cd423d74055a590
Armagan Ersoz <broccolinisoup@github.com>
2024-03-12 10:15:49 +1000

    Revert "Tooltip v1: Remove the caret from the tooltip to make it consistent with the new tooltip, css and rails - Do not merge (#4156)"
    This reverts commit 9bd3a0c2596604946df87a3fbb56f6c6cfb337da.

--- packages/react/src/Tooltip/Tooltip.features.stories.tsx
+++ packages/react/src/Tooltip/Tooltip.features.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type {Meta} from '@storybook/react'
-import {BaseStyles, ThemeProvider, IconButton, Button} from '..'
+import {BaseStyles, ThemeProvider, IconButton} from '..'
 import Box from '../Box'
 import Tooltip from './Tooltip'
 import {SearchIcon} from '@primer/octicons-react'
@@ -23,35 +23,6 @@ export default {
     },
   ],
 } as Meta
-
-export const AllDirections = () => (
-  <Box sx={{padding: 5, display: 'flex', gap: '8px'}}>
-    <Tooltip direction="n" aria-label="Supplementary text">
-      <Button>North</Button>
-    </Tooltip>
-    <Tooltip direction="s" aria-label="Supplementary text">
-      <Button>South</Button>
-    </Tooltip>
-    <Tooltip direction="e" aria-label="Supplementary text">
-      <Button>East</Button>
-    </Tooltip>
-    <Tooltip direction="w" aria-label="Supplementary text">
-      <Button>West</Button>
-    </Tooltip>
-    <Tooltip direction="ne" aria-label="Supplementary text">
-      <Button>North East</Button>
-    </Tooltip>
-    <Tooltip direction="nw" aria-label="Supplementary text">
-      <Button>North West</Button>
-    </Tooltip>
-    <Tooltip direction="se" aria-label="Supplementary text">
-      <Button>Southeast</Button>
-    </Tooltip>
-    <Tooltip direction="sw" aria-label="Supplementary text">
-      <Button>Southwest</Button>
-    </Tooltip>
-  </Box>
-)
 
 export const IconButtonTooltip = () => (
   <Box sx={{p: 5}}>
