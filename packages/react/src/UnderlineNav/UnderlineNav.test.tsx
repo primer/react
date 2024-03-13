d71e0f65c1e3b6d15c901add7b61de07d4481918
Siddharth Kshetrapal <siddharthkp@github.com>
2024-03-13 17:09:54 +0100

    add matchMedia mock to test-helpers

--- packages/react/src/UnderlineNav/UnderlineNav.test.tsx
+++ packages/react/src/UnderlineNav/UnderlineNav.test.tsx
@@ -15,22 +15,6 @@ import {
 import {UnderlineNav} from '.'
 import {checkExports, checkStoriesForAxeViolations} from '../utils/testing'
 
-// window.matchMedia() is not implemented by JSDOM so we have to create a mock:
-// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-})
-
 const ResponsiveUnderlineNav = ({
   selectedItemText = 'Code',
   loadingCounters = false,
