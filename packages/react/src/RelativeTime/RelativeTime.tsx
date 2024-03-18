7e75152f6413a62743530177b18e57680d95d319
Keith Cirkel <keithamus@users.noreply.github.com>
2024-03-18 16:30:05 +0000

    fix types

--- packages/react/src/RelativeTime/RelativeTime.tsx
+++ packages/react/src/RelativeTime/RelativeTime.tsx
@@ -5,19 +5,8 @@ import {createComponent} from '../utils/custom-element'
 
 const RelativeTimeComponent = createComponent(RelativeTimeElement, 'relative-time')
 
-interface RelativeTimePropsWithDate extends ComponentProps<typeof RelativeTimeComponent> {
-  datetime: never
-}
-
-interface RelativeTimePropsWithDateTime extends ComponentProps<typeof RelativeTimeComponent> {
-  date: never
-}
-
-export type RelativeTimeProps = RelativeTimePropsWithDateTime | RelativeTimePropsWithDate
-
 const localeOptions: Intl.DateTimeFormatOptions = {month: 'short', day: 'numeric', year: 'numeric'}
 function RelativeTime({date, datetime, ...props}: RelativeTimeProps) {
-  if (date && datetime) throw new Error(`Ambiguous use of both date and datetime props`)
   if (datetime) date = new Date(datetime)
   return (
     <RelativeTimeComponent {...props} date={date}>
@@ -26,4 +15,5 @@ function RelativeTime({date, datetime, ...props}: RelativeTimeProps) {
   )
 }
 
+export type RelativeTimeProps = ComponentProps<typeof RelativeTimeComponent>
 export default RelativeTime
