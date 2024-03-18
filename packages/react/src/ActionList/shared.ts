65c23c81df81bba40f9ddc8e2aadd5b5e13b8dc5
Tyler Jones <tylerjdev@github.com>
2024-03-18 18:47:15 +0000

    Changes from feedback

--- packages/react/src/ActionList/shared.ts
+++ packages/react/src/ActionList/shared.ts
@@ -11,7 +11,9 @@ export type ActionListItemProps = {
    * Callback that will trigger both on click selection and keyboard selection.
    * This is not called for disabled or inactive items.
    */
-  onSelect?: (event: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => void
+  onSelect?: (
+    event: React.MouseEvent<HTMLLIElement | HTMLButtonElement> | React.KeyboardEvent<HTMLLIElement | HTMLButtonElement>,
+  ) => void
   /**
    * Is the `Item` is currently selected?
    */
