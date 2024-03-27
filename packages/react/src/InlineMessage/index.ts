a187a558b9209f252c10d11b1bc9ecd13e3607d2
Josh Black <joshblack@users.noreply.github.com>
2024-03-27 10:41:06 -0500

    chore: run format

--- packages/react/src/InlineMessage/index.ts
+++ packages/react/src/InlineMessage/index.ts
@@ -1,2 +1,2 @@
-export { InlineMessage } from './InlineMessage';
-export type { InlineMessageProps } from './InlineMessage';
+export {InlineMessage} from './InlineMessage'
+export type {InlineMessageProps} from './InlineMessage'
