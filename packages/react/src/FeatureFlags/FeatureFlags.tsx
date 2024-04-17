f21a1096d8994529565e8b887ee477a74fdb0b14
Josh Black <joshblack@users.noreply.github.com>
2024-04-17 11:59:27 -0500

    chore: only merge global scope instead of parent scope

--- packages/react/src/FeatureFlags/FeatureFlags.tsx
+++ packages/react/src/FeatureFlags/FeatureFlags.tsx
@@ -1,16 +1,16 @@
-import React, {useContext, useMemo} from 'react'
+import React, {useMemo} from 'react'
 import {FeatureFlagContext} from './FeatureFlagContext'
 import {FeatureFlagScope, type FeatureFlags} from './FeatureFlagScope'
+import {GlobalFeatureFlags} from './GlobalFeatureFlags'
 
 export type FeatureFlagsProps = React.PropsWithChildren<{
   flags: FeatureFlags
 }>
 
 export function FeatureFlags({children, flags}: FeatureFlagsProps) {
-  const parentScope = useContext(FeatureFlagContext)
   const value = useMemo(() => {
-    const scope = FeatureFlagScope.create(flags)
-    return FeatureFlagScope.merge(parentScope, scope)
-  }, [parentScope, flags])
+    const scope = FeatureFlagScope.merge(GlobalFeatureFlags, FeatureFlagScope.create(flags))
+    return scope
+  }, [flags])
   return <FeatureFlagContext.Provider value={value}>{children}</FeatureFlagContext.Provider>
 }
