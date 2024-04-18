682211f2627406c39e7b89eef033dc82258818cd
Josh Black <joshblack@users.noreply.github.com>
2024-04-18 16:23:19 -0500

    test(e2e): remove default stories

--- e2e/components/Stack.test.ts
+++ e2e/components/Stack.test.ts
@@ -1,14 +1,6 @@
 import {test, expect} from '@playwright/test'
 import {visit} from '../test-helpers/storybook'
-import {themes} from '../test-helpers/themes'
 import {matrix, serialize} from '../test-helpers/matrix'
-
-const stories = [
-  {
-    title: 'Default',
-    id: 'drafts-components-stack--default',
-  },
-]
 
 const scenarios = matrix({
   align: ['stretch', 'start', 'center', 'end', 'baseline'],
@@ -20,36 +12,6 @@ const scenarios = matrix({
 })
 
 test.describe('Stack', () => {
-  for (const story of stories) {
-    test.describe(story.title, () => {
-      for (const theme of themes) {
-        test.describe(theme, () => {
-          test('default @vrt', async ({page}) => {
-            await visit(page, {
-              id: story.id,
-              globals: {
-                colorScheme: theme,
-              },
-            })
-
-            // Default state
-            expect(await page.screenshot()).toMatchSnapshot(`Stack.${story.title}.${theme}.png`)
-          })
-
-          test('axe @aat', async ({page}) => {
-            await visit(page, {
-              id: story.id,
-              globals: {
-                colorScheme: theme,
-              },
-            })
-            await expect(page).toHaveNoViolations()
-          })
-        })
-      }
-    })
-  }
-
   for (const scenario of scenarios) {
     const id = serialize(scenario)
 
