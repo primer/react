c05ac214d1d322f1c13f4752b120ac7b77ce7ed5
Mike Perrotti <mperrotti@github.com>
2024-05-02 14:35:46 -0400

    adds component docs

--- packages/react/src/drafts/UnderlinePanels/UnderlinePanels.tsx
+++ packages/react/src/drafts/UnderlinePanels/UnderlinePanels.tsx
@@ -28,17 +28,37 @@ type UnderlineTabButtonProps = {
    * Whether this is the selected tab
    */
   'aria-selected'?: boolean
+  /**
+   * Content of CounterLabel rendered after tab text label
+   */
   counter?: number | string
+  /**
+   *  Icon rendered before the tab text label
+   */
   icon?: FC<IconProps>
 } & SxProp
 
 type UnderlineTabbedInterfaceProps = {
+  /**
+   * Tabs (UnderlinePanels.Tab) and panels (UnderlinePanels.Panel) to render
+   */
   children: React.ReactNode
+  /**
+   * Accessible name for the tab list
+   */
   'aria-label'?: React.AriaAttributes['aria-label']
+  /**
+   * ID of the element containing the name for the tab list
+   */
   'aria-labelledby'?: React.AriaAttributes['aria-labelledby']
-  as?: React.ElementType
-  loadingCounters?: boolean
+  /**
+   * Custom string to use when generating the IDs of tabs and `aria-labelledby` for the panels
+   */
   id?: string
+  /**
+   * Loading state for all counters. It displays loading animation for individual counters until all are resolved. It is needed to prevent multiple layout shift.
+   */
+  loadingCounters?: boolean
 } & SxProp
 
 const TabContainerComponent = createComponent(TabContainerElement, 'tab-container')
