fee6ba5055692ffe2de762daa0f84aa475865420
Josh Black <joshblack@users.noreply.github.com>
2024-05-21 10:42:46 -0500

    chore: add group example

--- packages/react/src/Combobox/Combobox.stories.tsx
+++ packages/react/src/Combobox/Combobox.stories.tsx
@@ -315,6 +315,386 @@ export const Default = () => {
   )
 }
 
+type Group = {
+  label: string
+  options: Array<Option>
+}
+
+type Option = {
+  id: string
+  value: string
+}
+
+const data: Array<Group> = [
+  {
+    label: 'Recent',
+    options: [
+      {
+        id: 'recent:1',
+        value: 'Recent project 1',
+      },
+      {
+        id: 'recent:2',
+        value: 'Recent project 2',
+      },
+      {
+        id: 'recent:3',
+        value: 'Recent project 3',
+      },
+    ],
+  },
+  {
+    label: 'Repository',
+    options: [
+      {
+        id: 'repo:1',
+        value: 'Repo project 1',
+      },
+      {
+        id: 'repo:2',
+        value: 'Repo project 2',
+      },
+      {
+        id: 'repo:3',
+        value: 'Repo project 3',
+      },
+    ],
+  },
+  {
+    label: 'Organization',
+    options: [
+      {
+        id: 'org:1',
+        value: 'Org project 1',
+      },
+      {
+        id: 'org:2',
+        value: 'Org project 2',
+      },
+      {
+        id: 'org:3',
+        value: 'Org project 3',
+      },
+    ],
+  },
+] as const
+
+export const Groups = () => {
+  const comboboxRef = React.useRef<React.ElementRef<'input'>>(null)
+  const listboxRef = React.useRef<React.ElementRef<'div'>>(null)
+  const labelId = React.useId()
+  const comboboxId = React.useId()
+  const listboxId = React.useId()
+  const selectedId = React.useId()
+  const [groups, setGroups] = React.useState(data)
+  const [activeOptionId, setActiveOptionId] = React.useState<string>(() => {
+    if (data.length > 0) {
+      const group = data[0]
+      if (group.options.length > 0) {
+        return group.options[0].id
+      }
+    }
+    return ''
+  })
+  const [popupVisible, setPopupVisible] = React.useState(false)
+  const [value, setValue] = React.useState('')
+  const [selected, setSelected] = React.useState<Array<string>>([])
+
+  function setNextOptionActive() {
+    const options = groups.flatMap(group => {
+      return group.options.map(option => {
+        return {
+          ...option,
+          group: group.label,
+        }
+      })
+    })
+    const activeOptionIndex = options.findIndex(option => {
+      return option.id === activeOptionId
+    })
+    const activeOption = options[activeOptionIndex]
+    const nextActiveOptionIndex = Math.min(options.length - 1, activeOptionIndex + 1)
+    const nextActiveOption = options[nextActiveOptionIndex]
+    setActiveOptionId(nextActiveOption.id)
+
+    const item = selected.includes(nextActiveOption.id) ? `${nextActiveOption.value} selected` : nextActiveOption.value
+
+    // Changing groups
+    if (activeOption.group !== nextActiveOption.group) {
+      const group = groups.find(group => {
+        return group.label === nextActiveOption.group
+      })
+      if (group) {
+        announce(`${group.label} group entered, with ${group.options.length} options. ${item}`, {
+          politeness: 'assertive',
+        })
+      }
+    } else {
+      announce(item, {
+        politeness: 'assertive',
+      })
+    }
+  }
+
+  function setPreviousOptionActive() {
+    const options = groups.flatMap(group => {
+      return group.options.map(option => {
+        return {
+          ...option,
+          group: group.label,
+        }
+      })
+    })
+    const activeOptionIndex = options.findIndex(option => {
+      return option.id === activeOptionId
+    })
+    const activeOption = options[activeOptionIndex]
+    const nextActiveOptionIndex = Math.max(0, activeOptionIndex - 1)
+    const nextActiveOption = options[nextActiveOptionIndex]
+    setActiveOptionId(nextActiveOption.id)
+
+    const item = selected.includes(nextActiveOption.id) ? `${nextActiveOption.value} selected` : nextActiveOption.value
+
+    // Changing groups
+    if (activeOption.group !== nextActiveOption.group) {
+      const group = groups.find(group => {
+        return group.label === nextActiveOption.group
+      })
+      if (group) {
+        announce(`${group.label} group entered, with ${group.options.length} options. ${item}`, {
+          politeness: 'assertive',
+        })
+      }
+    } else {
+      announce(item, {
+        politeness: 'assertive',
+      })
+    }
+  }
+
+  function toggleOptionSelection(optionId: string) {
+    if (selected.includes(optionId)) {
+      setSelected(
+        selected.filter(id => {
+          return id !== optionId
+        }),
+      )
+    } else {
+      setSelected([...selected, optionId])
+    }
+  }
+
+  const closePopup = useEventCallback(() => {
+    setPopupVisible(false)
+  })
+
+  React.useEffect(() => {
+    function handler(event: MouseEvent) {
+      if (comboboxRef.current?.contains(event.target as HTMLElement)) {
+        return
+      } else if (listboxRef.current?.contains(event.target as HTMLElement)) {
+        return
+      }
+      closePopup()
+    }
+
+    document.addEventListener('click', handler)
+    return () => {
+      document.removeEventListener('click', handler)
+    }
+  }, [closePopup])
+
+  return (
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <ul id={selectedId}>
+        {selected.map(id => {
+          const [option] = data.flatMap(group => {
+            const option = group.options.find(option => {
+              return option.id === id
+            })
+            if (option) {
+              return option
+            }
+            return []
+          })
+
+          return <li key={id}>{option.value}</li>
+        })}
+      </ul>
+      <label htmlFor="group-combobox" id={labelId}>
+        Projects
+      </label>
+      <input
+        ref={comboboxRef}
+        aria-activedescendant={activeOptionId}
+        aria-controls={listboxId}
+        aria-expanded={popupVisible}
+        aria-labelledby={`${labelId} ${selectedId}`}
+        aria-multiselectable={true}
+        id={comboboxId}
+        value={value}
+        onChange={event => {
+          setValue(event.target.value)
+          if (event.target.value === '') {
+            setGroups(data)
+          } else {
+            setPopupVisible(true)
+
+            const value = event.target.value.toLowerCase()
+            const nextGroups = data.flatMap(group => {
+              const options = group.options.filter(option => {
+                return option.value.toLowerCase().includes(value)
+              })
+              if (options.length === 0) {
+                return []
+              }
+              return {
+                ...group,
+                options,
+              }
+            })
+
+            const group = nextGroups.find(group => {
+              return group.options.find(option => {
+                return option.id === activeOptionId
+              })
+            })
+            setGroups(nextGroups)
+
+            if (nextGroups.length === 0) {
+              announce(`No options available`, {
+                politeness: 'assertive',
+              })
+            } else if (group) {
+              const count = nextGroups.reduce((sum, group) => {
+                return sum + group.options.length
+              }, 0)
+              announce(`${count} options available`, {
+                politeness: 'assertive',
+              })
+            } else {
+              const count = nextGroups.reduce((sum, group) => {
+                return sum + group.options.length
+              }, 0)
+              const option = nextGroups[0].options[0]
+              if (option) {
+                setActiveOptionId(option.id)
+                announce(`${count} options available. ${option.value}`, {
+                  politeness: 'assertive',
+                })
+              } else {
+                announce(`${count} options available`, {
+                  politeness: 'assertive',
+                })
+              }
+            }
+          }
+        }}
+        onClick={() => {
+          if (!popupVisible) {
+            setPopupVisible(true)
+            const option = groups
+              .flatMap(group => {
+                return group.options
+              })
+              .find(option => {
+                return option.id === activeOptionId
+              })
+            if (option) {
+              const announcement = selected.includes(option.id) ? `${option.value} selected` : option.value
+              announce(announcement, {
+                politeness: 'assertive',
+              })
+            }
+          }
+        }}
+        onKeyDown={event => {
+          if (!popupVisible) {
+            if (event.key === 'ArrowDown') {
+              event.preventDefault()
+              setPopupVisible(true)
+
+              const option = groups
+                .flatMap(group => {
+                  return group.options
+                })
+                .find(option => {
+                  return option.id === activeOptionId
+                })
+              if (option) {
+                const announcement = selected.includes(option.id) ? `${option.value} selected` : option.value
+                announce(announcement)
+              }
+            }
+            return
+          }
+          if (event.key === 'ArrowDown') {
+            setNextOptionActive()
+          } else if (event.key === 'ArrowUp') {
+            setPreviousOptionActive()
+          } else if (event.key === 'Enter') {
+            toggleOptionSelection(activeOptionId)
+          } else if (event.key === 'Escape') {
+            setPopupVisible(false)
+          }
+        }}
+        role="combobox"
+        type="text"
+      />
+      <div
+        ref={listboxRef}
+        role="listbox"
+        tabIndex={-1}
+        hidden={!popupVisible}
+        style={{
+          border: '1px solid black',
+        }}
+      >
+        {groups.map((group, index) => {
+          const groupLabelId = `${listboxId}-group-${index}-label`
+          return (
+            <div key={group.label} role="group" aria-labelledby={groupLabelId}>
+              <div
+                id={groupLabelId}
+                style={{
+                  padding: '0.25rem 1rem',
+                  background: 'rgba(0, 0, 0, 0.05)',
+                }}
+              >
+                {group.label}
+              </div>
+              {group.options.map(option => {
+                const active = activeOptionId === option.id
+                return (
+                  <div
+                    key={option.id}
+                    aria-disabled={false}
+                    aria-selected={selected.includes(option.id)}
+                    data-active={active}
+                    id={option.id}
+                    onClick={() => {
+                      toggleOptionSelection(option.id)
+                    }}
+                    role="option"
+                    style={{
+                      background: active ? 'rgba(0, 0, 0, 0.1)' : 'transparent',
+                      cursor: 'pointer',
+                      padding: '0.25rem 1rem',
+                    }}
+                  >
+                    {selected.includes(option.id) ? '✅ ' : ''}
+                    {option.value}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
 function useEventCallback<T extends (...args: Array<any>) => void>(callback: T) {
   const savedCallback = React.useRef(callback)
 
@@ -322,7 +702,7 @@ function useEventCallback<T extends (...args: Array<any>) => void>(callback: T) 
     savedCallback.current = callback
   }, [callback])
 
-  return React.useCallback<T>((...args) => {
+  return React.useCallback((...args: Parameters<T>) => {
     savedCallback.current?.(...args)
   }, [])
 }
