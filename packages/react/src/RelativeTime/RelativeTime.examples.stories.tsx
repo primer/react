a7cbda4461ec0d25f1a52b52069fc187d4009235
Lindsey Wild <35239154+lindseywild@users.noreply.github.com>
2024-05-30 18:00:34 +0000

    Removes iconbutton story

--- packages/react/src/RelativeTime/RelativeTime.examples.stories.tsx
+++ packages/react/src/RelativeTime/RelativeTime.examples.stories.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import RelativeTime from './RelativeTime'
-import {ClockIcon} from '@primer/octicons-react'
 import Link from '../Link'
 import {Tooltip} from '../TooltipV2'
-import {IconButton} from '../Button'
 
 export default {
   title: 'Components/RelativeTime/Examples',
@@ -18,14 +16,4 @@ export const LinkWithTooltip = () => (
       <RelativeTime date={new Date('2020-01-01T00:00:00Z')} noTitle={true} />
     </Link>
   </Tooltip>
-)
-
-// Needs to do something? What can it do...
-export const StaticTextWithAdjacentIconButton = () => (
-  <div>
-    <RelativeTime date={new Date('2020-01-01T00:00:00Z')} sx={{mr: 2}} noTitle={true} />
-    <Tooltip text={new Date('2020-01-01T00:00:00Z').toString()}>
-      <IconButton icon={ClockIcon} aria-label="Relative time" />
-    </Tooltip>
-  </div>
 )
