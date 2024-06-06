b20fc5e4e9ef8ca905646251aa0f37ccb521653d
Kate Higa <16447748+khiga8@users.noreply.github.com>
2024-06-06 10:49:42 -0400

    Limit TrailingAction props

--- packages/react/src/ActionList/TrailingAction.tsx
+++ packages/react/src/ActionList/TrailingAction.tsx
@@ -1,27 +1,36 @@
 import React, {forwardRef} from 'react'
-// import type {SxProp} from '../sx'
-import type {IconButtonProps} from '../Button'
 import Box from '../Box'
 import {IconButton} from '../Button'
 import type {ForwardRefComponent as PolymorphicForwardRefComponent} from '../utils/polymorphic'
 
-// import {TEXT_ROW_HEIGHT} from './shared'
+export type ActionListTrailingActionProps = {
+  as?: 'button' | 'a'
+  icon: React.ElementType
+  'aria-label': string
+  showOnHover?: boolean
+}
 
-export type ActionListTrailingActionProps = IconButtonProps
-
-// const trailingActionRef = useRef()
-// <ActionItem.TrailingAction ref={trailingActionRef}
-export const TrailingAction = forwardRef(({...props}, forwardedRef) => {
-  return (
-    <Box
-      as="span"
-      sx={{
-        flexShrink: 0,
-      }}
-    >
-      <IconButton variant="invisible" unsafeDisableTooltip={false} tooltipDirection="w" {...props} ref={forwardedRef} />
-    </Box>
-  )
-}) as PolymorphicForwardRefComponent<'button', ActionListTrailingActionProps>
+export const TrailingAction = forwardRef(
+  ({as = 'button', icon, 'aria-label': ariaLabel, showOnHover = false}, forwardedRef) => {
+    return (
+      <Box
+        as="span"
+        sx={{
+          flexShrink: 0,
+        }}
+      >
+        <IconButton
+          as={as}
+          icon={icon}
+          variant="invisible"
+          unsafeDisableTooltip={false}
+          tooltipDirection="w"
+          aria-label={ariaLabel}
+          ref={forwardedRef}
+        />
+      </Box>
+    )
+  },
+) as PolymorphicForwardRefComponent<'button', ActionListTrailingActionProps>
 
 TrailingAction.displayName = 'ActionList.TrailingAction'
