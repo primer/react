f61a8f1e7e4d38b447cda2f5f5dad9983a77e941
Josh Black <joshblack@users.noreply.github.com>
2024-06-17 11:28:02 -0500

    chore: enable moduleResolution: Bundler

--- packages/react/src/drafts/MarkdownEditor/MarkdownEditor.test.tsx
+++ packages/react/src/drafts/MarkdownEditor/MarkdownEditor.test.tsx
@@ -1,7 +1,7 @@
 import {DiffAddedIcon} from '@primer/octicons-react'
 import {fireEvent, render as _render, waitFor, within, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import type {UserEvent} from '@testing-library/user-event/dist/types/setup/setup'
+import type {UserEvent} from '@testing-library/user-event'
 import React, {forwardRef, useRef, useState} from 'react'
 import type {MarkdownEditorHandle, MarkdownEditorProps, Mentionable, Reference, SavedReply} from '.'
 import MarkdownEditor from '.'
