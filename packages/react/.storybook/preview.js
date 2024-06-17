d0a6c5419d4dfb6e144f1599f66171c4ad9adcd3
Tyler Jones <tylerjdev@github.com>
2024-06-17 12:34:02 +0000

    Add feature flag to all stories

--- packages/react/.storybook/preview.js
+++ packages/react/.storybook/preview.js
@@ -1,6 +1,7 @@
 import {PrimerBreakpoints} from '../src/utils/layout'
 import {useEffect} from 'react'
 import {ThemeProvider, BaseStyles, theme} from '../src'
+import {FeatureFlags} from '../src/FeatureFlags'
 import clsx from 'clsx'
 
 import './storybook.css'
@@ -223,7 +224,9 @@ export const decorators = [
         <div className={clsx('story-wrap')}>
           <BaseStyles>
             {showSurroundingElements ? <a href="https://github.com/primer/react">Primer documentation</a> : ''}
-            <Story {...context} />
+            <FeatureFlags flags={{primer_react_action_list_item_as_button: true}}>
+              <Story {...context} />
+            </FeatureFlags>
             {showSurroundingElements ? <a href="https://github.com/primer/react">Primer documentation</a> : ''}
           </BaseStyles>
         </div>
