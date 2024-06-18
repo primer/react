3e493ba0723c9f0eadfacae6389959023561a700
Mike Perrotti <mperrotti@github.com>
2024-06-18 18:57:34 -0400

    Adds UnderlinePanels to `drafts/` (#4550)
    * adds UnderlinePanels components and stories
    
    stylistic tweaks
    
    more stylistic tweaks
    
    * adds tests, minor stylistic updates
    
    * adds component docs
    
    * adds changeset
    
    * corrects changeset
    
    * fixes circ dep, tests, misc bugs
    
    * more test fixes
    
    * appease the linter
    
    * updates snaps, fixes tiny visual regression
    
    * test(vrt): update snapshots
    
    * cleanup
    
    * upgrades @github/tab-container-element
    
    * fixes createComponent import
    
    * updates jest snapshots
    
    * Apply suggestion from @broccolinisoup
    
    Co-authored-by: Armağan <broccolinisoup@github.com>
    
    * addresses PR feedback
    
    * fix broken import
    
    * addresses more PR feedback
    
    * test(vrt): update snapshots
    
    * test(vrt): update snapshots
    
    * adds fallbacks to CSS vars
    
    * fixes CSS typo
    
    * fixes more typos
    
    ---------
    
    Co-authored-by: mperrotti <mperrotti@users.noreply.github.com>
    Co-authored-by: Armağan <broccolinisoup@github.com>

--- packages/react/src/drafts/index.ts
+++ packages/react/src/drafts/index.ts
@@ -75,4 +75,6 @@ export * from '../ActionBar'
 export {Stack} from '../Stack'
 export type {StackProps, StackItemProps} from '../Stack'
 
+export * from './UnderlinePanels'
+
 export {SkeletonBox, SkeletonText, SkeletonAvatar} from './Skeleton'
