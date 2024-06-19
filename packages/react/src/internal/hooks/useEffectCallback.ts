e50c56279737d86cbe4629dbfc224aa6953186b4
Josh Black <joshblack@users.noreply.github.com>
2024-06-19 15:41:10 -0500

    docs: update jsdoc for hook

--- packages/react/src/internal/hooks/useEffectCallback.ts
+++ packages/react/src/internal/hooks/useEffectCallback.ts
@@ -1,10 +1,10 @@
 import {useCallback, useEffect, useRef} from 'react'
 
 /**
- * Create a callback that can be used within an effect will not triggering it
- * when the values used within the callback change. The callback passed to this
- * hook will always see the latest snapshot of values that it uses and does not
- * need to use a dependency array.
+ * Create a callback that can be used within an effect without re-running the
+ * effect when the values used change. The callback passed to this hook will
+ * always see the latest snapshot of values that it uses and does not need to
+ * use a dependency array.
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useEffectCallback<T extends (...args: any) => any>(callback: T) {
