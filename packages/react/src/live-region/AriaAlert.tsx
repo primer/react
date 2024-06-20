813845c999772da257f6654d7bb877375e14ac60
Josh Black <joshblack@users.noreply.github.com>
2024-06-20 11:39:16 -0500

    chore: add support for sx to components

--- packages/react/src/live-region/AriaAlert.tsx
+++ packages/react/src/live-region/AriaAlert.tsx
@@ -1,25 +1,28 @@
 import React, {type ElementType} from 'react'
 import {Announce} from './Announce'
+import type {SxProp} from '../sx'
 
-export type AriaAlertProps<As extends ElementType> = React.PropsWithChildren<{
-  /**
-   * Customize the element type of the rendered container
-   */
-  as?: As
+export type AriaAlertProps<As extends ElementType> = React.PropsWithChildren<
+  {
+    /**
+     * Customize the element type of the rendered container
+     */
+    as?: As
 
-  /**
-   * Specify if the content of the element should be announced when this
-   * component is rendered and is not hidden
-   * @default true
-   */
-  announceOnShow?: boolean
+    /**
+     * Specify if the content of the element should be announced when this
+     * component is rendered and is not hidden
+     * @default true
+     */
+    announceOnShow?: boolean
 
-  /**
-   * Specify if the element is hidden
-   * @default false
-   */
-  hidden?: boolean
-}>
+    /**
+     * Specify if the element is hidden
+     * @default false
+     */
+    hidden?: boolean
+  } & SxProp
+>
 
 export function AriaAlert<As extends ElementType>({
   announceOnShow = true,
