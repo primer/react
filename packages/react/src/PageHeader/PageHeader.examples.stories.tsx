1403ef7e015165119b03b73832c5aa8b2f093021
Armağan <broccolinisoup@github.com>
2024-06-26 15:07:16 +1000

    PageHeader: Fixes layout issues on title and sub components (#4669)
    * layout shift issue resolve
    
    * do not inherit font and line styles from parent for description and navigation slot
    
    * font initial size and line hight only weight
    
    * set font styles to sub components
    
    * style update
    
    * test fix
    
    * Create two-hotels-attend.md
    
    * test(vrt): update snapshots
    
    * update line height to medium
    
    * test(vrt): update snapshots
    
    * breadcrumb line height update
    
    * test(vrt): update snapshots
    
    ---------
    
    Co-authored-by: broccolinisoup <broccolinisoup@users.noreply.github.com>

--- packages/react/src/PageHeader/PageHeader.examples.stories.tsx
+++ packages/react/src/PageHeader/PageHeader.examples.stories.tsx
@@ -110,7 +110,7 @@ export const PullRequestPage = () => (
       </PageHeader.Actions>
       <PageHeader.Description>
         <StateLabel status="pullOpened">Open</StateLabel>
-        <Text sx={{fontSize: 1, color: 'fg.muted'}}>
+        <Text sx={{color: 'fg.muted'}}>
           <Link href="https://github.com/broccolinisoup" sx={{fontWeight: 'bold'}}>
             broccolinisoup
           </Link>{' '}
@@ -148,7 +148,7 @@ export const FilesPage = () => (
   <Box sx={{padding: 3}}>
     <PageHeader>
       <PageHeader.TitleArea sx={{alignItems: 'center'}}>
-        <Text sx={{color: 'rgb(101, 109, 118)', fontSize: '14px'}}>/</Text>
+        <Text sx={{color: 'rgb(101, 109, 118)', fontSize: '14px', fontWeight: 'normal'}}>/</Text>
         <PageHeader.Title as="h1" sx={{fontSize: '14px', height: '21px'}}>
           PageHeader.tsx
         </PageHeader.Title>
