46f808344c8c19e63aad56c886ffb661dbb77370
Kate Higa <16447748+khiga8@users.noreply.github.com>
2024-06-27 09:50:03 -0400

    Add bad storybook example

--- packages/react/src/NavList/NavList.stories.tsx
+++ packages/react/src/NavList/NavList.stories.tsx
@@ -278,4 +278,38 @@ export const WithTrailingAction = () => {
   )
 }
 
+export const WithSubNavAndTrailingAction = () => {
+  return (
+    <PageLayout>
+      <PageLayout.Pane position="start">
+        <NavList>
+          <NavList.Item>
+            <NavList.LeadingVisual>
+              <FileDirectoryIcon />
+            </NavList.LeadingVisual>
+            Item 1
+            <NavList.TrailingAction label="Expand sidebar" icon={ArrowLeftIcon} />
+          </NavList.Item>
+          <NavList.Item>
+            Item 2
+            <NavList.TrailingAction as="a" href="#" label="Some action" icon={ArrowRightIcon} />
+          </NavList.Item>
+          <NavList.Item>
+            Item 3
+            <NavList.TrailingAction label="This is not supported and should not render!!!!!" icon={BookIcon} />
+            <NavList.SubNav>
+              <NavList.Item href="#">
+                Sub item 1
+                <NavList.TrailingAction label="Another action" icon={BookIcon} />
+              </NavList.Item>
+            </NavList.SubNav>
+          </NavList.Item>
+        </NavList>
+      </PageLayout.Pane>
+    </PageLayout>
+  )
+}
+
+WithSubNavAndTrailingAction.storyName = 'With SubNav and Trailing Action (Bad example, do not copy)'
+
 export default meta
