da3f7420830da18bb1a12ea984e21da07b87e80e
Jon Rohan <yes@jonrohan.codes>
2024-07-03 00:17:30 +0000

    Glob the fallbacks files

--- packages/react/rollup.config.mjs
+++ packages/react/rollup.config.mjs
@@ -133,25 +133,21 @@ const baseConfig = {
         customPropertiesFallback({
           importFrom: [
             () => {
-              const primitiveFallbacks = [
-                'color-fallbacks.json',
-                'base/size/size.json',
-                'base/typography/typography.json',
-                'functional/size/border.json',
-                'functional/size/breakpoints.json',
-                'functional/size/size-coarse.json',
-                'functional/size/size-fine.json',
-                'functional/size/size.json',
-                'functional/size/viewport.json',
-                'functional/typography/typography.json',
-              ]
               let customProperties = {}
-              for (const filePath of primitiveFallbacks) {
+              const filePaths = glob.sync(['**/*.json'], {
+                cwd: path.join(__dirname, '../../node_modules/@primer/primitives/dist/fallbacks/'),
+                ignore: ['color-fallbacks.json'],
+              })
+
+              for (const filePath of filePaths) {
                 const fileData = fs.readFileSync(
                   path.join(__dirname, '../../node_modules/@primer/primitives/dist/fallbacks/', filePath),
                   'utf8',
                 )
-                customProperties = {...customProperties, ...JSON.parse(fileData)}
+                customProperties = {
+                  ...customProperties,
+                  ...JSON.parse(fileData),
+                }
               }
 
               return {customProperties}
