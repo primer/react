89c3a087fa7d725f8842028b5a26d4e868a05b25
Lukas Oppermann <lukasoppermann@github.com>
2024-07-03 17:22:36 +0200

    Figma connect files (#4681)
    * working
    
    * add code connect
    
    * disable failing eslint
    
    * chore: fix TypeScript errors
    
    * Revert "chore: fix TypeScript errors"
    
    This reverts commit 0cdec4b6da3a401654eef44936c61f2fa403e79c.
    
    * trying to fix ts issues
    
    * fixes
    
    * readonly -> readOnly
    
    * disable type checking until the compinent is fixed
    
    ---------
    
    Co-authored-by: Josh Black <joshblack@users.noreply.github.com>

--- packages/react/src/BranchName/BranchName.figma.tsx
+++ packages/react/src/BranchName/BranchName.figma.tsx
@@ -17,11 +17,12 @@ figma.connect(
   'https://www.figma.com/file/GCvY3Qv8czRgZgvl1dG6lp/Primer-Web?type=design&node-id=3655-7208&mode=design&t=HqwKHI6akvFT5reK-4',
   {
     props: {
+      label: figma.textContent('branch_name'),
       as: figma.enum('type', {
         text: 'span',
         link: undefined,
       }),
     },
-    example: ({as}) => <BranchName as={as} />,
+    example: ({as, label}) => <BranchName as={as}>{label}</BranchName>,
   },
 )
