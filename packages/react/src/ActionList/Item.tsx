71859edc30664e259c855ffdc3732cda8dc6d169
Tyler Jones <tylerjdev@github.com>
2024-07-03 13:18:32 -0400

    ActionList: Ensure `variant` is respected when `full` (#4706)
    * Fix `variant` issue when `full` and FF is enabled
    
    * Adjust width styles for FF
    
    * Add changeset
    
    * Add full variant vrt tests

--- packages/react/src/ActionList/Item.tsx
+++ packages/react/src/ActionList/Item.tsx
@@ -183,8 +183,8 @@ export const Item = React.forwardRef<HTMLLIElement, ActionListItemProps>(
       display: 'flex',
       // show between 2 items
       ':not(:first-of-type)': {'--divider-color': theme?.colors.actionListItem.inlineDivider},
-      width: 'calc(100% - 16px)',
-      marginX: buttonSemantics ? '2' : '0',
+      width: buttonSemantics && listVariant !== 'full' ? 'calc(100% - 16px)' : '100%',
+      marginX: buttonSemantics && listVariant !== 'full' ? '2' : '0',
       ...(buttonSemantics ? hoverStyles : {}),
     }
 
