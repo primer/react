ace154230ec7fd70fc04b4125c55bd99c6d1c6d9
Armagan Ersoz <broccolinisoup@github.com>
2024-07-11 13:35:29 +1000

    add prx example for tmp

--- packages/react/src/ButtonGroup/ButtonGroup.features.stories.tsx
+++ packages/react/src/ButtonGroup/ButtonGroup.features.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import type {Meta} from '@storybook/react'
 import ButtonGroup from './ButtonGroup'
 import {Button, IconButton} from '../Button'
+import {ActionMenu} from '../ActionMenu'
+import {ActionList} from '../ActionList'
 import {PlusIcon, DashIcon} from '@primer/octicons-react'
 import {Tooltip} from '../next'
 
@@ -48,3 +50,41 @@ export const LinksWithTooltip = () => (
     </Tooltip>
   </ButtonGroup>
 )
+
+export const InactiveButtonsGroup = () => {
+  const primaryButton = (
+    <Button inactive={true} aria-disabled={true} onClick={() => {}}>
+      Primary Button
+    </Button>
+  )
+
+  const secondaryButton = (
+    <IconButton
+      unsafeDisableTooltip={true}
+      aria-label="Secondary Button Aria Label"
+      aria-disabled={true}
+      inactive={true}
+      icon={DashIcon}
+    />
+  )
+
+  return (
+    <ButtonGroup>
+      <Tooltip text="This button is inactive" direction="ne">
+        {primaryButton}
+      </Tooltip>
+
+      <ActionMenu open={false} onOpenChange={() => {}}>
+        <ActionMenu.Anchor>
+          <Tooltip text="this button is inactive" direction="ne" type="label">
+            {secondaryButton}
+          </Tooltip>
+        </ActionMenu.Anchor>
+        <ActionMenu.Overlay align="end">
+          <ActionList.Item>Item 1</ActionList.Item>
+          <ActionList.Item>Item 2</ActionList.Item>
+        </ActionMenu.Overlay>
+      </ActionMenu>
+    </ButtonGroup>
+  )
+}
