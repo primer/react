daea8e665bc55b84b9ff4ebc570aa42bfa97b1ee
Ian Sanders <iansan5653@github.com>
2024-07-18 19:31:30 +0000

    Try `canUseDOM` instead of `window !== undefined`

--- packages/react/src/hooks/useIsMacOS.ts
+++ packages/react/src/hooks/useIsMacOS.ts
@@ -1,14 +1,13 @@
 import {isMacOS as ssrUnsafeIsMacOS} from '@primer/behaviors/utils'
 import {useEffect, useState} from 'react'
-
+import {canUseDOM} from '../utils/environment'
 /**
  * SSR-safe hook for determining if the current platform is MacOS. When rendering
  * server-side, will default to non-MacOS and then re-render in an effect if the
  * client turns out to be a MacOS device.
  */
 export function useIsMacOS() {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  const [isMacOS, setIsMacOS] = useState(() => (window !== undefined ? ssrUnsafeIsMacOS() : false))
+  const [isMacOS, setIsMacOS] = useState(() => (canUseDOM ? ssrUnsafeIsMacOS() : false))
 
   useEffect(() => setIsMacOS(ssrUnsafeIsMacOS()), [])
 
