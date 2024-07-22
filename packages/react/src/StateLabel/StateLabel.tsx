af3dc9c0057b4d3a34e6389abc12cdc8275f914a
Siddharth Kshetrapal <siddharthkp@github.com>
2024-07-22 18:08:57 +0200

    run lint locally (sigh)

--- packages/react/src/StateLabel/StateLabel.tsx
+++ packages/react/src/StateLabel/StateLabel.tsx
@@ -133,7 +133,14 @@ function StateLabel({children, status, variant: variantProp = 'normal', ...rest}
   return (
     <StateLabelBase {...rest} variant={variantProp} status={status}>
       {/* eslint-disable-next-line @typescript-eslint/no-unnecessary-condition */}
-      {status && <Octicon {...octiconProps} icon={octiconMap[status] || QuestionIcon} aria-label={labelMap[status]} sx={{mr: 1}} />}
+      {status && (
+        <Octicon
+          {...octiconProps}
+          icon={octiconMap[status] || QuestionIcon}
+          aria-label={labelMap[status]}
+          sx={{mr: 1}}
+        />
+      )}
       {children}
     </StateLabelBase>
   )
