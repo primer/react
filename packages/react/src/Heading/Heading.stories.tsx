5c410335c6ab47cc47cdca780d6509cc0c77e56a
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-08-01 14:38:40 -0700

    fix

--- packages/react/src/Heading/Heading.stories.tsx
+++ packages/react/src/Heading/Heading.stories.tsx
@@ -29,20 +29,3 @@ Playground.argTypes = {
     options: ['large', 'medium', 'small'],
   },
 }
-
-export const TestSx: StoryFn<typeof Heading> = () => (
-  <Heading
-    sx={{
-      fontSize: 2,
-      fontWeight: 'normal',
-    }}
-  >
-    Heading with sx override
-  </Heading>
-)
-
-export const Small: StoryFn<typeof Heading> = () => <Heading variant="small">Small heading</Heading>
-
-export const Medium: StoryFn<typeof Heading> = () => <Heading variant="medium">Medium heading</Heading>
-
-export const Large: StoryFn<typeof Heading> = () => <Heading variant="large">Large heading</Heading>
