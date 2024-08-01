c242f85f46ea22144c16e18d7173f8f85239f7e6
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-08-01 10:21:20 -0700

    ui mob pair

--- e2e/components/Heading.test.ts
+++ e2e/components/Heading.test.ts
@@ -30,4 +30,88 @@ test.describe('Heading', () => {
       })
     }
   })
+
+  test.describe('Small', () => {
+    for (const theme of themes) {
+      test.describe(theme, () => {
+        test('default @vrt', async ({page}) => {
+          await visit(page, {
+            id: 'components-heading--small',
+            globals: {
+              colorScheme: theme,
+            },
+          })
+
+          // Default state
+          expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
+        })
+
+        test('axe @aat', async ({page}) => {
+          await visit(page, {
+            id: 'components-heading--small',
+            globals: {
+              colorScheme: theme,
+            },
+          })
+          await expect(page).toHaveNoViolations()
+        })
+      })
+    }
+  })
+
+  test.describe('Medium', () => {
+    for (const theme of themes) {
+      test.describe(theme, () => {
+        test('default @vrt', async ({page}) => {
+          await visit(page, {
+            id: 'components-heading--medium',
+            globals: {
+              colorScheme: theme,
+            },
+          })
+
+          // Default state
+          expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
+        })
+
+        test('axe @aat', async ({page}) => {
+          await visit(page, {
+            id: 'components-heading--medium',
+            globals: {
+              colorScheme: theme,
+            },
+          })
+          await expect(page).toHaveNoViolations()
+        })
+      })
+    }
+  })
+
+  test.describe('Large', () => {
+    for (const theme of themes) {
+      test.describe(theme, () => {
+        test('default @vrt', async ({page}) => {
+          await visit(page, {
+            id: 'components-heading--large',
+            globals: {
+              colorScheme: theme,
+            },
+          })
+
+          // Default state
+          expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
+        })
+
+        test('axe @aat', async ({page}) => {
+          await visit(page, {
+            id: 'components-heading--large',
+            globals: {
+              colorScheme: theme,
+            },
+          })
+          await expect(page).toHaveNoViolations()
+        })
+      })
+    }
+  })
 })
--- packages/react/src/Heading/Heading.stories.tsx
+++ packages/react/src/Heading/Heading.stories.tsx
@@ -6,6 +6,7 @@ export default {
   title: 'Components/Heading',
   component: Heading,
 } as Meta<typeof Heading>
+
 export const Default: StoryFn<typeof Heading> = () => <Heading>Default H2 Heading</Heading>
 
 export const Playground: StoryFn<typeof Heading> = args => <Heading {...args}>Heading</Heading>
@@ -17,8 +18,31 @@ Playground.args = {
 Playground.argTypes = {
   as: {
     control: {
-      type: 'select',
-      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+      type: 'radio',
     },
+    options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+  },
+  variant: {
+    control: {
+      type: 'radio',
+    },
+    options: ['large', 'medium', 'small'],
   },
 }
+
+export const TestSx: StoryFn<typeof Heading> = () => (
+  <Heading
+    sx={{
+      fontSize: 2,
+      fontWeight: 'normal',
+    }}
+  >
+    Heading with sx override
+  </Heading>
+)
+
+export const Small: StoryFn<typeof Heading> = () => <Heading variant="small">Small heading</Heading>
+
+export const Medium: StoryFn<typeof Heading> = () => <Heading variant="medium">Medium heading</Heading>
+
+export const Large: StoryFn<typeof Heading> = () => <Heading variant="large">Large heading</Heading>
