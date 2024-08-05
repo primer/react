37d3334cf77536c48d2086949035cea1912accd6
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-08-05 09:52:19 -0700

    try to only test light

--- e2e/components/Heading.test.ts
+++ e2e/components/Heading.test.ts
@@ -1,117 +1,110 @@
 import {test, expect} from '@playwright/test'
 import {visit} from '../test-helpers/storybook'
-import {themes} from '../test-helpers/themes'
+
+const theme = 'light'
 
 test.describe('Heading', () => {
   test.describe('Default', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-heading--default',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
+    test.describe(theme, () => {
+      test('default @vrt', async ({page}) => {
+        await visit(page, {
+          id: 'components-heading--default',
+          globals: {
+            colorScheme: theme,
+          },
         })
 
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-heading--default',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations()
-        })
+        // Default state
+        expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
       })
-    }
+
+      test('axe @aat', async ({page}) => {
+        await visit(page, {
+          id: 'components-heading--default',
+          globals: {
+            colorScheme: theme,
+          },
+        })
+        await expect(page).toHaveNoViolations()
+      })
+    })
   })
 
   test.describe('Small', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-heading--small',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
+    test.describe(theme, () => {
+      test('default @vrt', async ({page}) => {
+        await visit(page, {
+          id: 'components-heading--small',
+          globals: {
+            colorScheme: theme,
+          },
         })
 
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-heading--small',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations()
-        })
+        // Default state
+        expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
       })
-    }
+
+      test('axe @aat', async ({page}) => {
+        await visit(page, {
+          id: 'components-heading--small',
+          globals: {
+            colorScheme: theme,
+          },
+        })
+        await expect(page).toHaveNoViolations()
+      })
+    })
   })
 
   test.describe('Medium', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-heading--medium',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
+    test.describe(theme, () => {
+      test('default @vrt', async ({page}) => {
+        await visit(page, {
+          id: 'components-heading--medium',
+          globals: {
+            colorScheme: theme,
+          },
         })
 
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-heading--medium',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations()
-        })
+        // Default state
+        expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
       })
-    }
+
+      test('axe @aat', async ({page}) => {
+        await visit(page, {
+          id: 'components-heading--medium',
+          globals: {
+            colorScheme: theme,
+          },
+        })
+        await expect(page).toHaveNoViolations()
+      })
+    })
   })
 
   test.describe('Large', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-heading--large',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
+    test.describe(theme, () => {
+      test('default @vrt', async ({page}) => {
+        await visit(page, {
+          id: 'components-heading--large',
+          globals: {
+            colorScheme: theme,
+          },
         })
 
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-heading--large',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations()
-        })
+        // Default state
+        expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.${theme}.png`)
       })
-    }
+
+      test('axe @aat', async ({page}) => {
+        await visit(page, {
+          id: 'components-heading--large',
+          globals: {
+            colorScheme: theme,
+          },
+        })
+        await expect(page).toHaveNoViolations()
+      })
+    })
   })
 })
