1a715d03ede1198dc096daf1be2183a2cbe3d792
Josh Black <joshblack@users.noreply.github.com>
2024-08-07 15:07:57 -0500

    test(e2e): update snapshots for Heading

--- e2e/components/Heading.test.ts
+++ e2e/components/Heading.test.ts
@@ -1,74 +1,54 @@
 import {test, expect} from '@playwright/test'
 import {visit} from '../test-helpers/storybook'
+import {themes} from '../test-helpers/themes'
+
+const stories = [
+  {
+    title: 'Default',
+    id: 'components-heading--default',
+  },
+  {
+    title: 'Small',
+    id: 'components-heading-features--small',
+  },
+  {
+    title: 'Medium',
+    id: 'components-heading-features--medium',
+  },
+  {
+    title: 'Large',
+    id: 'components-heading-features--large',
+  },
+] as const
 
 test.describe('Heading', () => {
-  test.describe('Default', () => {
-    test('default @vrt', async ({page}) => {
-      await visit(page, {
-        id: 'components-heading--default',
-      })
+  for (const story of stories) {
+    test.describe(story.title, () => {
+      for (const theme of themes) {
+        test.describe(theme, () => {
+          test('default @vrt', async ({page}) => {
+            await visit(page, {
+              id: story.id,
+              globals: {
+                colorScheme: theme,
+              },
+            })
 
-      // Default state
-      expect(await page.screenshot()).toMatchSnapshot(`Heading.Default.png`)
+            // Default state
+            expect(await page.screenshot()).toMatchSnapshot(`Heading.${story.title}.${theme}.png`)
+          })
+
+          test('axe @aat', async ({page}) => {
+            await visit(page, {
+              id: story.id,
+              globals: {
+                colorScheme: theme,
+              },
+            })
+            await expect(page).toHaveNoViolations()
+          })
+        })
+      }
     })
-
-    test('axe @aat', async ({page}) => {
-      await visit(page, {
-        id: 'components-heading--default',
-      })
-      await expect(page).toHaveNoViolations()
-    })
-  })
-
-  test.describe('Small', () => {
-    test('default @vrt', async ({page}) => {
-      await visit(page, {
-        id: 'components-heading-features--small',
-      })
-
-      expect(await page.screenshot()).toMatchSnapshot(`Heading.Small.png`)
-    })
-
-    test('axe @aat', async ({page}) => {
-      await visit(page, {
-        id: 'components-heading-features--small',
-      })
-      await expect(page).toHaveNoViolations()
-    })
-  })
-
-  test.describe('Medium', () => {
-    test('default @vrt', async ({page}) => {
-      await visit(page, {
-        id: 'components-heading-features--medium',
-      })
-
-      expect(await page.screenshot()).toMatchSnapshot(`Heading.Medium.png`)
-    })
-
-    test('axe @aat', async ({page}) => {
-      await visit(page, {
-        id: 'components-heading-features--medium',
-      })
-      await expect(page).toHaveNoViolations()
-    })
-  })
-
-  test.describe('Large', () => {
-    test('default @vrt', async ({page}) => {
-      await visit(page, {
-        id: 'components-heading-features--large',
-      })
-
-      // Default state
-      expect(await page.screenshot()).toMatchSnapshot(`Heading.Large.png`)
-    })
-
-    test('axe @aat', async ({page}) => {
-      await visit(page, {
-        id: 'components-heading-features--large',
-      })
-      await expect(page).toHaveNoViolations()
-    })
-  })
+  }
 })
