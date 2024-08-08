78745103938ca36628083973a1dd03735cf883b4
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-08-08 14:36:09 -0700

    add fallbacks

--- packages/react/src/Text/Text.tsx
+++ packages/react/src/Text/Text.tsx
@@ -16,34 +16,34 @@ const StyledText = styled.span<StyledTextProps>`
   ${COMMON};
 
   &:where([data-size='small']) {
-    font-size: var(--text-body-size-small);
-    line-height: var(--text-body-lineHeight-small);
+    font-size: var(--text-body-size-small, 0.75rem);
+    line-height: var(--text-body-lineHeight-small, 1.6666);
   }
 
   &:where([data-size='medium']) {
-    font-size: var(--text-body-size-medium);
-    line-height: var(--text-body-lineHeight-medium);
+    font-size: var(--text-body-size-medium, 0.875rem);
+    line-height: var(--text-body-lineHeight-medium, 1.4285);
   }
 
   &:where([data-size='large']) {
-    font-size: var(--text-body-size-large);
-    line-height: var(--text-body-lineHeight-large);
+    font-size: var(--text-body-size-large, 1rem);
+    line-height: var(--text-body-lineHeight-large, 1.5);
   }
 
   &:where([data-weight='light']) {
-    font-weight: var(--base-text-weight-light);
+    font-weight: var(--base-text-weight-light, 300);
   }
 
   &:where([data-weight='normal']) {
-    font-weight: var(--base-text-weight-normal);
+    font-weight: var(--base-text-weight-normal, 400);
   }
 
   &:where([data-weight='medium']) {
-    font-weight: var(--base-text-weight-medium);
+    font-weight: var(--base-text-weight-medium, 500);
   }
 
   &:where([data-weight='semibold']) {
-    font-weight: var(--base-text-weight-semibold);
+    font-weight: var(--base-text-weight-semibold, 600);
   }
 
   ${sx};
