7d21f9bac64f5aa1e04e2bfd4b2c62bdd0dea84d
Jon Rohan <yes@jonrohan.codes>
2024-08-12 18:41:33 +0000

    Run tests inside fhte feature flag also

--- e2e/components/Link.test.ts
+++ e2e/components/Link.test.ts
@@ -3,163 +3,189 @@ import {visit} from '../test-helpers/storybook'
 import {themes} from '../test-helpers/themes'
 
 test.describe('Link', () => {
-  test.describe('Default', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-link--default',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Default.${theme}.png`)
-
-          // Hover state
-          await page.getByRole('link').hover()
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Default.${theme}.hover.png`)
-
-          // Focus state
-          await page.keyboard.press('Tab')
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Default.${theme}.focus.png`)
-        })
-
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-link--default',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations({
-            rules: {
-              'color-contrast': {
-                enabled: theme !== 'dark_dimmed',
+  for (const featureFlagEnabled of [true, false]) {
+    test.describe('Default', () => {
+      for (const theme of themes) {
+        test.describe(theme, () => {
+          test('default @vrt', async ({page}) => {
+            await visit(page, {
+              id: 'components-link--default',
+              globals: {
+                colorScheme: theme,
+                featureFlags: {
+                  primer_react_css_modules: featureFlagEnabled,
+                },
               },
-            },
-          })
-        })
-      })
-    }
-  })
+            })
 
-  test.describe('Muted', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-link-features--muted',
-            globals: {
-              colorScheme: theme,
-            },
+            // Default state
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Default.${theme}.png`)
+
+            // Hover state
+            await page.getByRole('link').hover()
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Default.${theme}.hover.png`)
+
+            // Focus state
+            await page.keyboard.press('Tab')
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Default.${theme}.focus.png`)
           })
 
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Muted.${theme}.png`)
-
-          // Hover state
-          await page.getByRole('link').hover()
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Muted.${theme}.hover.png`)
-
-          // Focus state
-          await page.keyboard.press('Tab')
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Muted.${theme}.focus.png`)
-        })
-
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-link-features--muted',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations({
-            rules: {
-              'color-contrast': {
-                enabled: theme !== 'dark_dimmed',
+          test('axe @aat', async ({page}) => {
+            await visit(page, {
+              id: 'components-link--default',
+              globals: {
+                colorScheme: theme,
+                featureFlags: {
+                  primer_react_css_modules: featureFlagEnabled,
+                },
               },
-            },
-          })
-        })
-      })
-    }
-  })
-
-  test.describe('Underline', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-link-features--underline',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Underline.${theme}.png`)
-
-          // Hover state
-          await page.getByRole('link').hover()
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Underline.${theme}.hover.png`)
-
-          // Focus state
-          await page.keyboard.press('Tab')
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Underline.${theme}.focus.png`)
-        })
-
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-link-features--underline',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations({
-            rules: {
-              'color-contrast': {
-                enabled: theme !== 'dark_dimmed',
+            })
+            await expect(page).toHaveNoViolations({
+              rules: {
+                'color-contrast': {
+                  enabled: theme !== 'dark_dimmed',
+                },
               },
-            },
+            })
           })
         })
-      })
-    }
-  })
+      }
+    })
 
-  test.describe('Dev: Inline', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-link-devonly--inline',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Link.Inline.${theme}.png`)
-        })
-
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-link-devonly--inline',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations({
-            rules: {
-              'color-contrast': {
-                enabled: theme !== 'dark_dimmed',
+    test.describe('Muted', () => {
+      for (const theme of themes) {
+        test.describe(theme, () => {
+          test('default @vrt', async ({page}) => {
+            await visit(page, {
+              id: 'components-link-features--muted',
+              globals: {
+                colorScheme: theme,
+                featureFlags: {
+                  primer_react_css_modules: featureFlagEnabled,
+                },
               },
-            },
+            })
+
+            // Default state
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Muted.${theme}.png`)
+
+            // Hover state
+            await page.getByRole('link').hover()
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Muted.${theme}.hover.png`)
+
+            // Focus state
+            await page.keyboard.press('Tab')
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Muted.${theme}.focus.png`)
+          })
+
+          test('axe @aat', async ({page}) => {
+            await visit(page, {
+              id: 'components-link-features--muted',
+              globals: {
+                colorScheme: theme,
+                featureFlags: {
+                  primer_react_css_modules: featureFlagEnabled,
+                },
+              },
+            })
+            await expect(page).toHaveNoViolations({
+              rules: {
+                'color-contrast': {
+                  enabled: theme !== 'dark_dimmed',
+                },
+              },
+            })
           })
         })
-      })
-    }
-  })
+      }
+    })
+
+    test.describe('Underline', () => {
+      for (const theme of themes) {
+        test.describe(theme, () => {
+          test('default @vrt', async ({page}) => {
+            await visit(page, {
+              id: 'components-link-features--underline',
+              globals: {
+                colorScheme: theme,
+                featureFlags: {
+                  primer_react_css_modules: featureFlagEnabled,
+                },
+              },
+            })
+
+            // Default state
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Underline.${theme}.png`)
+
+            // Hover state
+            await page.getByRole('link').hover()
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Underline.${theme}.hover.png`)
+
+            // Focus state
+            await page.keyboard.press('Tab')
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Underline.${theme}.focus.png`)
+          })
+
+          test('axe @aat', async ({page}) => {
+            await visit(page, {
+              id: 'components-link-features--underline',
+              globals: {
+                colorScheme: theme,
+                featureFlags: {
+                  primer_react_css_modules: featureFlagEnabled,
+                },
+              },
+            })
+            await expect(page).toHaveNoViolations({
+              rules: {
+                'color-contrast': {
+                  enabled: theme !== 'dark_dimmed',
+                },
+              },
+            })
+          })
+        })
+      }
+    })
+
+    test.describe('Dev: Inline', () => {
+      for (const theme of themes) {
+        test.describe(theme, () => {
+          test('default @vrt', async ({page}) => {
+            await visit(page, {
+              id: 'components-link-devonly--inline',
+              globals: {
+                colorScheme: theme,
+                featureFlags: {
+                  primer_react_css_modules: featureFlagEnabled,
+                },
+              },
+            })
+
+            // Default state
+            expect(await page.screenshot()).toMatchSnapshot(`Link.Inline.${theme}.png`)
+          })
+
+          test('axe @aat', async ({page}) => {
+            await visit(page, {
+              id: 'components-link-devonly--inline',
+              globals: {
+                colorScheme: theme,
+                featureFlags: {
+                  primer_react_css_modules: featureFlagEnabled,
+                },
+              },
+            })
+            await expect(page).toHaveNoViolations({
+              rules: {
+                'color-contrast': {
+                  enabled: theme !== 'dark_dimmed',
+                },
+              },
+            })
+          })
+        })
+      }
+    })
+  }
 })
