1afe16a7650c90960cb24d6c099e1f80cc27f0ce
Jon Rohan <yes@jonrohan.codes>
2024-08-21 16:49:12 -0700

    Remove duplicate flag

--- packages/react/src/FeatureFlags/DefaultFeatureFlags.ts
+++ packages/react/src/FeatureFlags/DefaultFeatureFlags.ts
@@ -2,7 +2,6 @@ import {FeatureFlagScope} from './FeatureFlagScope'
 
 export const DefaultFeatureFlags = FeatureFlagScope.create({
   primer_react_css_modules_team: false,
-  primer_react_css_modules_team: false,
   primer_react_css_modules_staff: false,
   primer_react_css_modules_ga: false,
   primer_react_action_list_item_as_button: false,
