0005b8a0a55d405d26df9237a358c9f41fc87183
Jon Rohan <yes@jonrohan.codes>
2024-08-21 21:53:04 +0000

    use _team instead of component name

--- packages/react/src/FeatureFlags/DefaultFeatureFlags.ts
+++ packages/react/src/FeatureFlags/DefaultFeatureFlags.ts
@@ -1,8 +1,8 @@
 import {FeatureFlagScope} from './FeatureFlagScope'
 
 export const DefaultFeatureFlags = FeatureFlagScope.create({
-  primer_react_css_modules_link: false,
-  primer_react_css_modules_heading: false,
+  primer_react_css_modules_team: false,
+  primer_react_css_modules_team: false,
   primer_react_css_modules_staff: false,
   primer_react_css_modules_ga: false,
   primer_react_action_list_item_as_button: false,
