5dc6adbf704b87867154513ea63e1ee81c54cdac
Siddharth Kshetrapal <siddharthkp@github.com>
2024-08-27 15:47:54 +0200

    clean up dual filter list setup

--- packages/react/src/FeatureFlags/DefaultFeatureFlags.ts
+++ packages/react/src/FeatureFlags/DefaultFeatureFlags.ts
@@ -3,4 +3,5 @@ import {FeatureFlagScope} from './FeatureFlagScope'
 export const DefaultFeatureFlags = FeatureFlagScope.create({
   primer_react_css_modules: false,
   primer_react_action_list_item_as_button: false,
+  primer_react_select_panel_with_modern_action_list: false,
 })
--- packages/react/src/FilteredActionList/FilteredActionListEntry.tsx
+++ packages/react/src/FilteredActionList/FilteredActionListEntry.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import type {FilteredActionListProps} from './FilteredActionList'
-import {FilteredActionList as WithDeprecatedActionList} from './FilteredActionList'
+import type {FilteredActionListProps} from './FilteredActionListWithDeprecatedActionList'
+import {FilteredActionList as WithDeprecatedActionList} from './FilteredActionListWithDeprecatedActionList'
 import {FilteredActionList as WithStableActionList} from './FilteredActionListWithModernActionList'
 import {useFeatureFlag} from '../FeatureFlags'
 
 export function FilteredActionList(props: FilteredActionListProps): JSX.Element {
-  const enabled = useFeatureFlag('primer_react_select_panel_modern_action_list')
+  const enabled = useFeatureFlag('primer_react_select_panel_with_modern_action_list')
 
   if (enabled) return <WithStableActionList {...props} />
   else return <WithDeprecatedActionList {...props} />
