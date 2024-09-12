b7a0220d05d285b9da136c1fe9649d612b13be2a
Josh Black <joshblack@users.noreply.github.com>
2024-09-12 12:55:10 -0500

    refactor(storybook): update from @storybook/react-webpack5 to @storybook/react-vite

---
+++ packages/react/.storybook/main.ts
@@ -0,0 +1,45 @@
+import {createRequire} from 'node:module'
+import path from 'node:path'
+import type {StorybookConfig} from '@storybook/react-vite'
+
+const require = createRequire(import.meta.url)
+
+const {DEPLOY_ENV = 'development'} = process.env
+
+const config: StorybookConfig = {
+  stories:
+    DEPLOY_ENV === 'development'
+      ? ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
+      : // Don't include dev stories in production
+        ['../src/**/*.mdx', '../src/**/!(*.dev).stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+      },
+    },
+    getAbsolutePath('@storybook/addon-storysource'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('@storybook/addon-links'),
+  ],
+
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  async viteFinal(config) {
+    config.define = {
+      ...config.define,
+      __DEV__: JSON.stringify(true),
+    }
+    return config
+  },
+}
+
+export default config
+
+function getAbsolutePath(value) {
+  return path.dirname(require.resolve(path.join(value, 'package.json')))
+}
