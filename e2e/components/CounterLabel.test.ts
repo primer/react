40c6b9675cf9f39fb9efa8535e899e925eef07c5
Josh Black <joshblack@users.noreply.github.com>
2024-09-13 15:44:24 -0500

    refactor(CounterLabel): update to use CSS Modules behind flag

--- e2e/components/CounterLabel.test.ts
+++ e2e/components/CounterLabel.test.ts
@@ -2,72 +2,85 @@ import {test, expect} from '@playwright/test'
 import {visit} from '../test-helpers/storybook'
 import {themes} from '../test-helpers/themes'
 
+const stories = [
+  {
+    title: 'Default',
+    id: 'components-counterlabel--default',
+  },
+  {
+    title: 'Primary Theme',
+    id: 'components-counterlabel--primary-theme',
+  },
+  {
+    title: 'Primary Theme',
+    id: 'components-counterlabel--primary-theme',
+  },
+  {
+    title: 'Secondary Theme',
+    id: 'components-counterlabel--secondary-theme',
+  },
+] as const
+
 test.describe('CounterLabel', () => {
-  test.describe('Default', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
+  for (const story of stories) {
+    test.describe(story.title, () => {
+      for (const theme of themes) {
         test('default @vrt', async ({page}) => {
           await visit(page, {
-            id: 'components-counterlabel--default',
+            id: story.id,
             globals: {
               colorScheme: theme,
+              featureFlags: {
+                primer_react_css_modules_team: true,
+              },
             },
           })
 
           // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`CounterLabel.Default.${theme}.png`)
+          expect(await page.screenshot()).toMatchSnapshot(`CounterLabel.${story.title}.${theme}.png`)
         })
 
-        test('axe @aat', async ({page}) => {
+        test('default (styled-components) @vrt', async ({page}) => {
           await visit(page, {
-            id: 'components-counterlabel--default',
+            id: story.id,
             globals: {
               colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations({
-            rules: {
-              'color-contrast': {
-                enabled: theme !== 'dark_dimmed',
+              featureFlags: {
+                primer_react_css_modules_team: false,
               },
-            },
-          })
-        })
-      })
-    }
-  })
-
-  test.describe('Primary Theme', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-counterlabel-features--primary-theme',
-            globals: {
-              colorScheme: theme,
             },
           })
 
           // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`CounterLabel.Primary Theme.${theme}.png`)
+          expect(await page.screenshot()).toMatchSnapshot(`CounterLabel.${story.title}.${theme}.png`)
         })
 
         test('axe @aat', async ({page}) => {
           await visit(page, {
-            id: 'components-counterlabel-features--primary-theme',
+            id: story.id,
             globals: {
               colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations({
-            rules: {
-              'color-contrast': {
-                enabled: theme !== 'dark_dimmed',
+              featureFlags: {
+                primer_react_css_modules_team: true,
               },
             },
           })
+          await expect(page).toHaveNoViolations()
         })
-      })
-    }
-  })
+
+        test('axe (styled-components) @aat', async ({page}) => {
+          await visit(page, {
+            id: story.id,
+            globals: {
+              colorScheme: theme,
+              featureFlags: {
+                primer_react_css_modules_team: false,
+              },
+            },
+          })
+          await expect(page).toHaveNoViolations()
+        })
+      }
+    })
+  }
 })
