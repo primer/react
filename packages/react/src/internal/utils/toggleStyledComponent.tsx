6c9121e217e4815c7c6ab3c03474b2ca632f05e9
Josh Black <joshblack@github.com>
2024-09-17 17:11:22 -0500

    feat(Banner): update Banner to use CSS Modules behind feature flag (#4913)
    * feat(Banner): update Banner to use CSS Modules behind feature flag
    
    * chore: add changeset
    
    * chore: remove autogenerated pnpm packageManager change
    
    * chore: fix stylelint errors
    
    * chore: add back in underline for fallback case
    
    * refactor: update autofix and eslint warning
    
    * chore: update class order for tests
    
    * chore: fix stylelint errors
    
    * Update packages/react/src/Banner/Banner.module.css
    
    Co-authored-by: Katie Langerman <18661030+langermank@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Josh Black <joshblack@users.noreply.github.com>
    Co-authored-by: Katie Langerman <18661030+langermank@users.noreply.github.com>

---
+++ packages/react/src/internal/utils/toggleStyledComponent.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {useFeatureFlag} from '../../FeatureFlags'
+
+type CSSModulesProps = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  as?: string | React.ComponentType<any>
+}
+
+/**
+ * Utility to toggle rendering a styled component or a "plain" component based
+ * on the provided `as` prop. When the provided feature flag is enabled, the
+ * props will be passed through to an element or component created with the `as`
+ * prop. When it is disabled, the provided styled component is used instead.
+ *
+ * @param flag - the feature flag that will control whether or not the provided
+ * styled component is used
+ * @param Component - the styled component that will be used if the feature flag
+ * is disabled
+ */
+export function toggleStyledComponent<T, P extends CSSModulesProps>(flag: string, Component: React.ComponentType<P>) {
+  const Wrapper = React.forwardRef<T, P>(function Wrapper({as: BaseComponent = 'div', ...rest}, ref) {
+    const enabled = useFeatureFlag(flag)
+    if (enabled) {
+      return <BaseComponent {...rest} ref={ref} />
+    }
+    return <Component as={BaseComponent} {...(rest as P)} ref={ref} />
+  })
+
+  return Wrapper
+}
