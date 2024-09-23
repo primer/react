7a5205d88e6c1600e83232a102378d97639b4223
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-09-23 13:58:59 -0700

    Bug fix: `Button` aria-expanded state (CSS Modules) (#5011)
    * reorder + test
    
    * add snap
    
    * Create weak-ravens-greet.md
    
    * danger active icon
    
    * test(vrt): update snapshots
    
    * test disable
    
    ---------
    
    Co-authored-by: langermank <langermank@users.noreply.github.com>

--- e2e/components/Button.test.ts
+++ e2e/components/Button.test.ts
@@ -700,6 +700,51 @@ test.describe('Button', () => {
           })
         }
       })
+
+      test.describe('Aria expanded buttons', () => {
+        for (const theme of themes) {
+          test.describe(theme, () => {
+            test('default @vrt', async ({page}) => {
+              await visit(page, {
+                id: 'components-button-features--expanded-button',
+                globals: {
+                  colorScheme: theme,
+                  featureFlags: {
+                    primer_react_css_modules_team: featureFlagOn,
+                  },
+                },
+              })
+
+              // Default state
+              expect(await page.screenshot({animations: 'disabled'})).toMatchSnapshot(
+                `Button.Aria expanded buttons.${theme}.png`,
+              )
+            })
+
+            // temporarily disabled due to a bug in primer/primitves
+
+            // eslint-disable-next-line jest/no-commented-out-tests
+            // test('axe @aat', async ({page}) => {
+            //   await visit(page, {
+            //     id: 'components-button-features--expanded-button',
+            //     globals: {
+            //       colorScheme: theme,
+            //       featureFlags: {
+            //         primer_react_css_modules_team: featureFlagOn,
+            //       },
+            //     },
+            //   })
+            //   await expect(page).toHaveNoViolations({
+            //     rules: {
+            //       'color-contrast': {
+            //         enabled: theme !== 'dark_dimmed',
+            //       },
+            //     },
+            //   })
+            // })
+          })
+        }
+      })
     })
   }
 })
