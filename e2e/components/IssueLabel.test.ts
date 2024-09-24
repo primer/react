3ddf7aa8da216d17e5e52aee12a56a50ba1debb2
Katie Langerman <18661030+langermank@users.noreply.github.com>
2024-09-24 16:49:37 -0700

    fix vrt

--- e2e/components/IssueLabel.test.ts
+++ e2e/components/IssueLabel.test.ts
@@ -9,79 +9,79 @@ const variants = [
   },
   {
     title: 'Auburn',
-    id: 'experimental-components-issuelabel-features--auburn',
+    id: 'experimental-components-issuelabel-features--variant-auburn',
   },
   {
     title: 'Blue',
-    id: 'experimental-components-issuelabel-features--blue',
+    id: 'experimental-components-issuelabel-features--variant-blue',
   },
   {
     title: 'Brown',
-    id: 'experimental-components-issuelabel-features--brown',
+    id: 'experimental-components-issuelabel-features--variant-brown',
   },
   {
     title: 'Coral',
-    id: 'experimental-components-issuelabel-features--coral',
+    id: 'experimental-components-issuelabel-features--variant-coral',
   },
   {
     title: 'Cyan',
-    id: 'experimental-components-issuelabel-features--cyan',
+    id: 'experimental-components-issuelabel-features--variant-cyan',
   },
   {
     title: 'Gray',
-    id: 'experimental-components-issuelabel-features--gray',
+    id: 'experimental-components-issuelabel-features--variant-gray',
   },
   {
     title: 'Green',
-    id: 'experimental-components-issuelabel-features--green',
+    id: 'experimental-components-issuelabel-features--variant-green',
   },
   {
     title: 'Indigo',
-    id: 'experimental-components-issuelabel-features--indigo',
+    id: 'experimental-components-issuelabel-features--variant-indigo',
   },
   {
     title: 'Lemon',
-    id: 'experimental-components-issuelabel-features--lemon',
+    id: 'experimental-components-issuelabel-features--variant-lemon',
   },
   {
     title: 'Lime',
-    id: 'experimental-components-issuelabel-features--lime',
+    id: 'experimental-components-issuelabel-features--variant-lime',
   },
   {
     title: 'Olive',
-    id: 'experimental-components-issuelabel-features--olive',
+    id: 'experimental-components-issuelabel-features--variant-olive',
   },
   {
     title: 'Orange',
-    id: 'experimental-components-issuelabel-features--orange',
+    id: 'experimental-components-issuelabel-features--variant-orange',
   },
   {
     title: 'Pine',
-    id: 'experimental-components-issuelabel-features--pine',
+    id: 'experimental-components-issuelabel-features--variant-pine',
   },
   {
     title: 'Pink',
-    id: 'experimental-components-issuelabel-features--pink',
+    id: 'experimental-components-issuelabel-features--variant-pink',
   },
   {
     title: 'Plum',
-    id: 'experimental-components-issuelabel-features--plum',
+    id: 'experimental-components-issuelabel-features--variant-plum',
   },
   {
     title: 'Purple',
-    id: 'experimental-components-issuelabel-features--purple',
+    id: 'experimental-components-issuelabel-features--variant-purple',
   },
   {
     title: 'Red',
-    id: 'experimental-components-issuelabel-features--red',
+    id: 'experimental-components-issuelabel-features--variant-red',
   },
   {
     title: 'Teal',
-    id: 'experimental-components-issuelabel-features--teal',
+    id: 'experimental-components-issuelabel-features--variant-teal',
   },
   {
     title: 'Yellow',
-    id: 'experimental-components-issuelabel-features--yellow',
+    id: 'experimental-components-issuelabel-features--variant-yellow',
   },
   {
     title: 'Button',
