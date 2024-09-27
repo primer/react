2c1f1fa0d54e91ea881f2760e19e0ec569978542
Marie Lucca <francinelucca@github.com>
2024-09-27 14:54:20 -0400

    test(ActionMenu): adjust for aria-hidden tooltip

--- packages/react/src/__tests__/ActionMenu.test.tsx
+++ packages/react/src/__tests__/ActionMenu.test.tsx
@@ -401,7 +401,7 @@ describe('ActionMenu', () => {
       button.focus()
     })
 
-    expect(component.getByRole('tooltip')).toBeInTheDocument()
+    expect(component.getByRole('tooltip', {hidden: true})).toBeInTheDocument()
   })
 
   it('should open menu on menu anchor click and it is wrapped with tooltip v2', async () => {
@@ -437,7 +437,7 @@ describe('ActionMenu', () => {
       button.focus()
     })
 
-    expect(component.getByRole('tooltip')).toBeInTheDocument()
+    expect(component.getByRole('tooltip', {hidden: true})).toBeInTheDocument()
   })
 
   it('should pass the "id" prop from ActionMenu.Button to the HTML button', async () => {
