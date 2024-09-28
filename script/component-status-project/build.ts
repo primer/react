707bc4e5e95264337e06b80f8ab485d097407e21
Elham_Barzegar <elhambarzegar.hs@gmail.com>
2024-09-28 22:20:59 +0330

    Update build.ts

--- script/component-status-project/build.ts
+++ script/component-status-project/build.ts
@@ -1,100 +1,79 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs/promises';  
+import path from 'path';  
+import fm from 'front-matter'; // به جای require از import استفاده شده است  
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const fm = require('front-matter') // FIXME after this bugfix is merged https://github.com/jxson/front-matter/pull/77
+const sourceDirectory = path.resolve(__dirname, '../../docs/content/');  
+const outputDir = path.resolve(__dirname, '../../dist/');  
 
-const sourceDirectory = path.resolve(__dirname, '../../docs/content/')
-const outputDir = path.resolve(__dirname, '../../dist/')
+type ComponentStatus = {  
+  [component: string]: string;  
+};  
 
-type ComponentStatus = {
-  [component: string]: string
-}
+/**  
+ * Extracts the component status for each file in the given directory.  
+ *  
+ * @param filenames Array of filenames to read front-matter from  
+ * @param dir Absolute path to directory containing files  
+ * @returns A promise that resolves to an array containing outcome of file front-matter extraction  
+ */  
+async function getComponentStatuses(filenames: string[], dir: string): Promise<ComponentStatus[]> {  
+  const promises = filenames.map(async (filename) => {  
+    try {  
+      const content = await fs.readFile(path.resolve(dir, filename), 'utf-8');  
 
-/**
- * Extracts the component status for each file in the given directory.
- *
- * @param filenames Array of filenames to read front-matter from
- * @param dir Absolute path to directory containing files
- * @returns A promise that resolves to an array containing outcome of file front-matter extraction
- */
-function getComponentStatuses(filenames: string[], dir: string) {
-  const promises: Promise<ComponentStatus | null>[] = []
+      if (fm.test(content)) {  
+        const {  
+          attributes: { title, status },  
+        } = fm(content);  
 
-  const handleCallback = (
-    filename: string,
-    resolve: (value: ComponentStatus | null) => void,
-    reject: (value: unknown) => void,
-  ) => {
-    fs.readFile(path.resolve(dir, filename), 'utf-8', (err, content) => {
-      if (err) return reject(err)
+        if (status) {  
+          return { [title]: status };  
+        }  
+      }  
+    } catch (error) {  
+      console.error(`Error reading file ${filename}: ${error}`);  
+    }  
+    return null; // Return null if not valid  
+  });  
 
-      if (fm.test(content)) {
-        const {
-          attributes: {title, status},
-        } = fm(content)
+  return (await Promise.all(promises)).filter(Boolean) as ComponentStatus[];  
+}  
 
-        if (status) {
-          return resolve({[title]: status})
-        }
-      }
+/**  
+ * Orchestrates the process of reading component status for each file in the given directory.  
+ *  
+ * @param dir Directory to source files where status will be extracted from  
+ * @returns A promise that resolves to an object containing component statuses  
+ */  
+async function readFiles(dir: string): Promise<ComponentStatus> {  
+  try {  
+    const dirContents = await fs.readdir(dir, { withFileTypes: true });  
+    const filenames = dirContents.filter(dirent => dirent.isFile()).map(dirent => dirent.name);  
 
-      resolve(null)
-    })
-  }
+    const componentStatusesArray = await getComponentStatuses(filenames, dir);  
 
-  for (const filename of filenames) {
-    const promise: Promise<ComponentStatus | null> = new Promise((resolve, reject) => {
-      return handleCallback(filename, resolve, reject)
-    })
-    promises.push(promise)
-  }
+    return componentStatusesArray.reduce((acc, file) => ({  
+      ...acc,  
+      ...file,  
+    }), {});  
+  } catch (error) {  
+    throw new Error(`Error reading files: ${error}`);  
+  }  
+}  
 
-  return Promise.all(promises)
-}
+/**  
+ * Writes the component status to the given file.  
+ */  
+async function build() {  
+  try {  
+    const componentStatuses = await readFiles(sourceDirectory);  
 
-/**
- * Orchestrates the process of reading component status for each file in the given directory.
- *
- * @param dir Directory to source files where status will be extracted from
- * @returns A promise that resolves to an object containing component statuses
- */
-async function readFiles(dir: string) {
-  try {
-    const dirContents = fs.readdirSync(dir, {withFileTypes: true})
-    const filenames = dirContents.filter(dirent => dirent.isFile()).map(dirent => dirent.name)
-    const componentStatuses = await getComponentStatuses(filenames, dir)
+    await fs.mkdir(outputDir, { recursive: true });  
 
-    return componentStatuses
-      .filter(Boolean)
-      .reverse()
-      .reduce(
-        (acc, file) => ({
-          ...acc,
-          ...file,
-        }),
-        {},
-      )
-  } catch (err) {
-    throw new Error(`error reading files: ${err}`)
-  }
-}
+    await fs.writeFile(path.join(outputDir, 'component-status.json'), JSON.stringify(componentStatuses));  
+  } catch (error) {  
+    throw new Error(`Error building component status object: ${error}`);  
+  }  
+}  
 
-/**
- * Writes the component status to the given file.
- */
-async function build() {
-  try {
-    const componentStatuses = await readFiles(sourceDirectory)
-
-    if (!fs.existsSync(outputDir)) {
-      fs.mkdirSync(outputDir)
-    }
-
-    fs.writeFileSync(`${outputDir}/component-status.json`, JSON.stringify(componentStatuses))
-  } catch (error) {
-    throw new Error(`error building component status object: ${error}`)
-  }
-}
-
-build()
+build();
