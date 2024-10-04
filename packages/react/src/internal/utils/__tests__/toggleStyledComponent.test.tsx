b8d0b9b2d3901dccff9ad49a704b1a775c9706c9
Jon Rohan <yes@jonrohan.codes>
2024-10-04 19:54:45 +0000

    Adding tests

---
+++ packages/react/src/internal/utils/__tests__/toggleStyledComponent.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import {FeatureFlags} from '../../../FeatureFlags'
+import {toggleStyledComponent} from '../toggleStyledComponent'
+import styled from 'styled-components'
+
+describe('toggleStyledComponent', () => {
+  test('renders the `as` prop when flag is enabled', () => {
+    const TestComponent = toggleStyledComponent('testFeatureFlag', () => <div />)
+    const {container} = render(
+      <FeatureFlags flags={{testFeatureFlag: true}}>
+        <TestComponent as="button" />
+      </FeatureFlags>,
+    )
+    expect(container.firstChild).toBeInstanceOf(HTMLButtonElement)
+  })
+
+  test('renders a div as fallback when flag is enabled and no `as` prop is provided', () => {
+    const TestComponent = toggleStyledComponent('testFeatureFlag', () => <div />)
+    const {container} = render(
+      <FeatureFlags flags={{testFeatureFlag: true}}>
+        <TestComponent />
+      </FeatureFlags>,
+    )
+    expect(container.firstChild).toBeInstanceOf(HTMLDivElement)
+  })
+
+  test('renders Box with `as` if `sx` is provided and flag is enabled', () => {
+    const TestComponent = toggleStyledComponent('testFeatureFlag', () => styled.div``)
+    const {container} = render(
+      <FeatureFlags flags={{testFeatureFlag: true}}>
+        <TestComponent sx={{color: 'red'}} />
+      </FeatureFlags>,
+    )
+
+    expect(container.firstChild).toHaveStyle('color: red')
+  })
+
+  test('renders styled component when flag is disabled', () => {
+    const StyledComponent = toggleStyledComponent('testFeatureFlag', styled.div.attrs({['data-styled']: true})``)
+    const {container} = render(
+      <FeatureFlags flags={{testFeatureFlag: false}}>
+        <StyledComponent />
+      </FeatureFlags>,
+    )
+    expect(container.firstChild).toHaveAttribute('data-styled')
+  })
+})
