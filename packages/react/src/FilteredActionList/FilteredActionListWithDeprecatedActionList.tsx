f93eb9be2baaa14212c8a3306ee1b2d699b9b8f0
Josh Black <joshblack@github.com>
2024-10-16 13:53:12 -0500

    Revert "Select panel loading states (#4929)" (#5136)
    This reverts commit 843ea4744c9d41209fcbc557f54f7a6f8e6bb8ae.

--- packages/react/src/FilteredActionList/FilteredActionListWithDeprecatedActionList.tsx
+++ packages/react/src/FilteredActionList/FilteredActionListWithDeprecatedActionList.tsx
@@ -4,6 +4,7 @@ import type {KeyboardEventHandler} from 'react'
 import React, {useCallback, useEffect, useRef} from 'react'
 import styled from 'styled-components'
 import Box from '../Box'
+import Spinner from '../Spinner'
 import type {TextInputProps} from '../TextInput'
 import TextInput from '../TextInput'
 import {get} from '../constants'
@@ -16,11 +17,6 @@ import {useProvidedStateOrCreate} from '../hooks/useProvidedStateOrCreate'
 import useScrollFlash from '../hooks/useScrollFlash'
 import {VisuallyHidden} from '../VisuallyHidden'
 import type {SxProp} from '../sx'
-import {
-  type FilteredActionListLoadingType,
-  FilteredActionListBodyLoader,
-  FilteredActionListLoadingTypes,
-} from './FilteredActionListLoaders'
 
 const menuScrollMargins: ScrollIntoViewOptions = {startMargin: 0, endMargin: 8}
 
@@ -29,10 +25,9 @@ export interface FilteredActionListProps
     ListPropsBase,
     SxProp {
   loading?: boolean
-  loadingType?: FilteredActionListLoadingType
   placeholderText?: string
   filterValue?: string
-  onFilterChange: (value: string, e: React.ChangeEvent<HTMLInputElement> | null) => void
+  onFilterChange: (value: string, e: React.ChangeEvent<HTMLInputElement>) => void
   textInputProps?: Partial<Omit<TextInputProps, 'onChange'>>
   inputRef?: React.RefObject<HTMLInputElement>
 }
@@ -44,7 +39,6 @@ const StyledHeader = styled.div`
 
 export function FilteredActionList({
   loading = false,
-  loadingType = FilteredActionListLoadingTypes.bodySpinner,
   placeholderText,
   filterValue: externalFilterValue,
   onFilterChange,
@@ -116,7 +110,7 @@ export function FilteredActionList({
   useScrollFlash(scrollContainerRef)
 
   return (
-    <Box display="flex" flexDirection="column" overflow="hidden" flexGrow={1} sx={sx}>
+    <Box display="flex" flexDirection="column" overflow="hidden" sx={sx}>
       <StyledHeader>
         <TextInput
           ref={inputRef}
@@ -130,15 +124,15 @@ export function FilteredActionList({
           aria-label={placeholderText}
           aria-controls={listId}
           aria-describedby={inputDescriptionTextId}
-          loaderPosition={'leading'}
-          loading={loading && !loadingType.appearsInBody}
           {...textInputProps}
         />
       </StyledHeader>
       <VisuallyHidden id={inputDescriptionTextId}>Items will be filtered as you type</VisuallyHidden>
-      <Box ref={scrollContainerRef} overflow="auto" flexGrow={1}>
-        {loading && scrollContainerRef.current && loadingType.appearsInBody ? (
-          <FilteredActionListBodyLoader loadingType={loadingType} height={scrollContainerRef.current.clientHeight} />
+      <Box ref={scrollContainerRef} overflow="auto">
+        {loading ? (
+          <Box width="100%" display="flex" flexDirection="row" justifyContent="center" pt={6} pb={7}>
+            <Spinner />
+          </Box>
         ) : (
           <ActionList ref={listContainerRef} items={items} {...listProps} role="listbox" id={listId} />
         )}
