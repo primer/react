7f6b50bd55b7bf628d7d7637664b5e01b48befc1
Marie Lucca <40550942+francinelucca@users.noreply.github.com>
2024-10-16 23:05:52 -0400

    Revert "revert: set global focus styles (#5114)"
    This reverts commit 9f377074f1176e9bcf5cdc39bca1442e63863dc6.

--- packages/react/src/SegmentedControl/SegmentedControlIconButton.tsx
+++ packages/react/src/SegmentedControl/SegmentedControlIconButton.tsx
@@ -8,6 +8,7 @@ import {getSegmentedControlButtonStyles, getSegmentedControlListItemStyles} from
 import Box from '../Box'
 import {defaultSxProp} from '../utils/defaultSxProp'
 import {isElement} from 'react-is'
+import getGlobalFocusStyles from '../internal/utils/getGlobalFocusStyles'
 
 export type SegmentedControlIconButtonProps = {
   'aria-label': string
@@ -21,6 +22,7 @@ export type SegmentedControlIconButtonProps = {
   ButtonHTMLAttributes<HTMLButtonElement | HTMLLIElement>
 
 const SegmentedControlIconButtonStyled = styled.button`
+  ${getGlobalFocusStyles()};
   ${sx};
 `
 
