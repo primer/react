46ed885df97c55460363b2e11444bd084cb6c77b
Tyler Jones <tylerjdev@github.com>
2024-10-30 09:35:58 -0400

    Fix test

--- packages/react/src/AnchoredOverlay/AnchoredOverlay.tsx
+++ packages/react/src/AnchoredOverlay/AnchoredOverlay.tsx
@@ -198,7 +198,7 @@ export const AnchoredOverlay: React.FC<React.PropsWithChildren<AnchoredOverlayPr
           left={position?.left || 0}
           anchorSide={position?.anchorSide}
           className={className}
-          preventOverflow={false}
+          preventOverflow={true}
           {...overlayProps}
         >
           {children}
