e985472402df75afbbccb2a06ccbf34de26417d3
Josh Black <joshblack@users.noreply.github.com>
2024-11-05 12:26:22 -0600

    refactor(FormControl): update InputCaption to use CSS Modules behind flag

--- packages/react/src/internal/components/InputCaption.tsx
+++ packages/react/src/internal/components/InputCaption.tsx
@@ -1,30 +1,68 @@
+import {clsx} from 'clsx'
 import React from 'react'
 import Text from '../../Text'
 import type {SxProp} from '../../sx'
+import classes from './InputCaption.module.css'
+import {useFeatureFlag} from '../../FeatureFlags'
 
 type Props = {
   /**
    * The unique identifier used to associate the caption with an input
    */
   id: string
+
   /**
    * Whether the input associated with this caption is disabled
    */
   disabled?: boolean
+
+  /**
+   * Provide an optional class name applied to the outermost element
+   */
+  className?: string
 } & SxProp
 
-const InputCaption: React.FC<React.PropsWithChildren<Props>> = ({children, disabled, id, sx}) => (
-  <Text
-    id={id}
-    sx={{
-      color: disabled ? 'var(--control-fgColor-disabled)' : 'var(--fgColor-muted)',
-      display: 'block',
-      fontSize: 0,
-      ...sx,
-    }}
-  >
-    {children}
-  </Text>
-)
+const InputCaption: React.FC<React.PropsWithChildren<Props>> = ({
+  children,
+  className: customClassName,
+  disabled,
+  id,
+  sx,
+}) => {
+  const enabled = useFeatureFlag('primer_react_css_modules_team')
+  const className = clsx(customClassName, {
+    [classes.InputCaption]: enabled,
+  })
+
+  if (enabled) {
+    if (sx) {
+      return (
+        <Text id={id} data-disabled={disabled ? '' : undefined} className={className} sx={sx}>
+          {children}
+        </Text>
+      )
+    }
+    return (
+      <Text id={id} data-disabled={disabled ? '' : undefined} className={className}>
+        {children}
+      </Text>
+    )
+  }
+
+  return (
+    <Text
+      id={id}
+      className={className}
+      sx={{
+        color: disabled ? 'var(--control-fgColor-disabled)' : 'var(--fgColor-muted)',
+        display: 'block',
+        fontSize: 0,
+        ...sx,
+      }}
+    >
+      {children}
+    </Text>
+  )
+}
 
 export default InputCaption
