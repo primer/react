a95a8607114c3f67d82a7d630da6e9cdebaa4eb2
Marie Lucca <francinelucca@github.com>
2024-11-05 12:02:05 -0500

    test(Timeline): update tests, add tests for group

--- packages/react/src/Timeline/__tests__/Timeline.test.tsx
+++ packages/react/src/Timeline/__tests__/Timeline.test.tsx
@@ -44,6 +44,22 @@ describe('Timeline.Item', () => {
   it('adds the Timeline-Item class', () => {
     expect(rendersClass(<Timeline.Item />, 'Timeline-Item')).toEqual(true)
   })
+
+  it("should render as 'li' if FF is turned on", () => {
+    const {getByRole} = HTMLRender(
+      <FeatureFlags flags={{primer_react_timeline_as_list: true}}>
+        <Timeline.Item />
+      </FeatureFlags>,
+    )
+    expect(getByRole('listitem')).toBeInTheDocument()
+    expect(getByRole('listitem').tagName).toBe('LI')
+  })
+
+  it("should render as 'div' by default", () => {
+    const {queryByRole, getByText} = HTMLRender(<Timeline.Item>test</Timeline.Item>)
+    expect(queryByRole('listitem')).toBeNull()
+    expect(getByText('test').tagName).toBe('DIV')
+  })
 })
 
 describe('Timeline.Badge', () => {
@@ -69,9 +85,6 @@ describe('Timeline.Break', () => {
 describe('Timeline.Group', () => {
   behavesAsComponent({Component: Timeline.Group, options: {skipAs: true, skipSx: true}})
 
-  // TODO
-  it.todo('correctly renders with Timeline.Group')
-
   it('should have no axe violations', async () => {
     const {container} = HTMLRender(
       <FeatureFlags flags={{primer_react_timeline_as_list: true}}>
@@ -84,5 +97,22 @@ describe('Timeline.Group', () => {
     )
     const results = await axe.run(container)
     expect(results).toHaveNoViolations()
+  })
+
+  it('should throw warning if FF not enabled', async () => {
+    const spy = jest.spyOn(console, 'warn').mockImplementation(() => {})
+    HTMLRender(
+      <Timeline>
+        <Timeline.Group>
+          <Timeline.Item>test</Timeline.Item>
+        </Timeline.Group>
+      </Timeline>,
+    )
+    expect(spy).toHaveBeenCalledWith(
+      'Warning:',
+      "Timeline.Group is only meant to be used with the timeline as list feature, you may want to turn on the 'primer_react_timeline_as_list' feature flag. Using Timeline.Group without this feature may have unintended consequences",
+    )
+
+    spy.mockRestore()
   })
 })
