ba0b8ced7c78eb9bc4a8015da130ed52fa15409a
Randall Krauskopf <randall-krauskopf@github.com>
2024-11-05 06:32:02 -0600

    update e2e

--- e2e/components/Token.test.ts
+++ e2e/components/Token.test.ts
@@ -2,31 +2,46 @@ import {test, expect} from '@playwright/test'
 import {visit} from '../test-helpers/storybook'
 import {themes} from '../test-helpers/themes'
 
+const stories = [
+  {
+    title: 'Default',
+    id: 'components-token--default',
+  },
+  {
+    title: 'Dev Default',
+    id: 'components-token-dev--dev-default',
+  },
+]
+
 test.describe('Token', () => {
   test.describe('Default', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-token--default',
-            globals: {
-              colorScheme: theme,
-            },
-          })
+    for (const story of stories) {
+      test.describe(story.title, () => {
+        for (const theme of themes) {
+          test.describe(theme, () => {
+            test('default @vrt', async ({page}) => {
+              await visit(page, {
+                id: 'components-token--default',
+                globals: {
+                  colorScheme: theme,
+                },
+              })
 
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`Token.Default.${theme}.png`)
-        })
+              // Default state
+              expect(await page.screenshot()).toMatchSnapshot(`Token.Default.${theme}.png`)
+            })
 
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-token--default',
-            globals: {
-              colorScheme: theme,
-            },
+            test('axe @aat', async ({page}) => {
+              await visit(page, {
+                id: 'components-token--default',
+                globals: {
+                  colorScheme: theme,
+                },
+              })
+              await expect(page).toHaveNoViolations()
+            })
           })
-          await expect(page).toHaveNoViolations()
-        })
+        }
       })
     }
   })
