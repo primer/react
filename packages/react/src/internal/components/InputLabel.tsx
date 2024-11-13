72ebe696afe8324f09e0365522cf489a54671745
Josh Black <joshblack@github.com>
2024-11-13 16:26:00 -0600

    chore: check-in work for InputLabel refactor

--- packages/react/src/internal/components/InputLabel.tsx
+++ packages/react/src/internal/components/InputLabel.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Box from '../../Box'
 import type {SxProp} from '../../sx'
 import VisuallyHidden from '../../_VisuallyHidden'
+// import {VisuallyHidden} from '../../VisuallyHidden'
+import {featureFlag} from '../../FormControl/FormControlFeatureFlags'
+import {useFeatureFlag} from '../../FeatureFlags'
+import {Stack} from '../../Stack'
+import {clsx} from 'clsx'
+import classes from './InputLabel.module.css'
 
 type BaseProps = SxProp & {
   disabled?: boolean
@@ -39,6 +45,54 @@ const InputLabel: React.FC<React.PropsWithChildren<Props>> = ({
   className,
   ...props
 }) => {
+  const enabled = useFeatureFlag(featureFlag)
+
+  if (enabled) {
+    if (sx) {
+      return (
+        <VisuallyHidden
+          isVisible={!visuallyHidden}
+          as={
+            as as 'label' /* This assertion is clearly wrong, but it's the only way TS will allow the htmlFor prop to be possibly defined */
+          }
+          htmlFor={htmlFor}
+          id={id}
+          className={clsx(className, classes.InputLabel)}
+          sx={sx}
+          {...props}
+        >
+          {required || requiredText ? (
+            <Stack as="span" direction="horizontal" gap="none">
+              <div className={classes.RequiredText}>{children}</div>
+              <span aria-hidden={requiredIndicator ? undefined : true}>{requiredText ?? '*'}</span>
+            </Stack>
+          ) : (
+            children
+          )}
+        </VisuallyHidden>
+      )
+    }
+
+    const label = (
+      <label htmlFor={htmlFor} id={id} className={clsx(className, classes.InputLabel)} {...props}>
+        {required || requiredText ? (
+          <Stack as="span" direction="horizontal" gap="none">
+            <div className={classes.RequiredText}>{children}</div>
+            <span aria-hidden={requiredIndicator ? undefined : true}>{requiredText ?? '*'}</span>
+          </Stack>
+        ) : (
+          children
+        )}
+      </label>
+    )
+
+    if (visuallyHidden) {
+      return <VisuallyHidden>{label}</VisuallyHidden>
+    }
+
+    return label
+  }
+
   return (
     <VisuallyHidden
       isVisible={!visuallyHidden}
@@ -60,10 +114,10 @@ const InputLabel: React.FC<React.PropsWithChildren<Props>> = ({
       {...props}
     >
       {required || requiredText ? (
-        <Box display="flex" as="span">
+        <Stack as="span" direction="horizontal" gap="none">
           <Box mr={1}>{children}</Box>
           <span aria-hidden={requiredIndicator ? undefined : true}>{requiredText ?? '*'}</span>
-        </Box>
+        </Stack>
       ) : (
         children
       )}
