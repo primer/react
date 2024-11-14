10f6f2deb3f5949d12f0e9ee432d4ac91c7e3901
Jon Rohan <yes@jonrohan.codes>
2024-11-14 00:22:51 +0000

    Fix test

--- packages/react/src/Banner/Banner.test.tsx
+++ packages/react/src/Banner/Banner.test.tsx
@@ -115,9 +115,9 @@ describe('Banner', () => {
       />,
     )
 
-    expect(screen.getByRole('button', {name: 'test primary action'})).toBeInTheDocument()
+    expect(screen.queryAllByText('test primary action')[0]).toBeInTheDocument()
 
-    await user.click(screen.getByRole('button', {name: 'test primary action'}))
+    await user.click(screen.queryAllByText('test primary action')[0])
     expect(onClick).toHaveBeenCalledTimes(1)
   })
 
@@ -132,9 +132,9 @@ describe('Banner', () => {
       />,
     )
 
-    expect(screen.getByRole('button', {name: 'test secondary action'})).toBeInTheDocument()
+    expect(screen.queryAllByText('test secondary action')[0]).toBeInTheDocument()
 
-    await user.click(screen.getByRole('button', {name: 'test secondary action'}))
+    await user.click(screen.queryAllByText('test secondary action')[0])
     expect(onClick).toHaveBeenCalledTimes(1)
   })
 
@@ -148,8 +148,8 @@ describe('Banner', () => {
       />,
     )
 
-    expect(screen.getByRole('button', {name: 'test primary action'})).toBeInTheDocument()
-    expect(screen.getByRole('button', {name: 'test secondary action'})).toBeInTheDocument()
+    expect(screen.queryAllByText('test primary action')[0]).toBeInTheDocument()
+    expect(screen.queryAllByText('test secondary action')[0]).toBeInTheDocument()
   })
 
   it('should call `onDismiss` when the dismiss button is activated', async () => {
