8232bdfb00f04bd70e0ffc0a4374e62603198ddb
Marie Lucca <francinelucca@github.com>
2024-11-19 17:36:39 -0500

    fix(Overlay): add aria-label to dialog stories

--- packages/react/src/Overlay/Overlay.stories.tsx
+++ packages/react/src/Overlay/Overlay.stories.tsx
@@ -246,6 +246,7 @@ export const Default = () => {
           anchorSide="inside-right"
           role="dialog"
           aria-modal="true"
+          aria-label="Sample overlay"
           ref={containerRef}
         >
           <Box
@@ -306,6 +307,7 @@ export const Playground = (args: Args) => {
           width={args.width}
           height={args.height}
           aria-modal={args.role === 'dialog'}
+          aria-label={args.role === 'list' ? 'Sample overlay' : undefined}
           ref={containerRef}
           {...args}
         >
