a833e24bd160a0d359f4bcb648f18fd09b3712df
Tyler Jones <tylerjdev@github.com>
2024-11-22 13:13:05 -0500

    Add feature flag to PointerBox

--- packages/react/src/PointerBox/PointerBox.tsx
+++ packages/react/src/PointerBox/PointerBox.tsx
@@ -6,6 +6,11 @@ import type {CaretProps} from '../Caret'
 import Caret from '../Caret'
 import {get} from '../constants'
 import type {SxProp} from '../sx'
+import {useFeatureFlag} from '../FeatureFlags'
+import classes from './PointerBox.module.css'
+import {clsx} from 'clsx'
+
+const CSS_MODULES_FEATURE_FLAG = 'primer_react_css_modules_team'
 
 // FIXME: Make this work with BetterStyledSystem types
 type MutatedSxProps = {
@@ -21,13 +26,14 @@ export type PointerBoxProps = {
   bg?: CaretProps['bg']
   borderColor?: CaretProps['borderColor']
   border?: CaretProps['borderWidth']
+  className?: string
 } & BoxProps &
   MutatedSxProps
 
 function PointerBox(props: PointerBoxProps) {
   // don't destructure these, just grab them
   const themeContext = React.useContext(ThemeContext)
-  const {bg, border, borderColor, theme: themeProp, sx} = props
+  const {bg, border, borderColor, theme: themeProp, sx, className} = props
   const {caret, children, ...boxProps} = props
   const {bg: sxBg, backgroundColor, ...sxRest} = sx || {}
   const theme = themeProp || themeContext
@@ -42,6 +48,43 @@ function PointerBox(props: PointerBoxProps) {
   }
 
   const defaultBoxProps = {borderWidth: '1px', borderStyle: 'solid', borderColor: 'border.default', borderRadius: 2}
+  const enabled = useFeatureFlag(CSS_MODULES_FEATURE_FLAG)
+
+  if (enabled) {
+    const styles: {[key: string]: string} = {}
+    if (customBackground) {
+      const pointerBoxBg = `var(--bgColor-${customBackground})`
+      styles['background-color'] = pointerBoxBg
+      styles['background-image'] =
+        `linear-gradient(${pointerBoxBg}, ${pointerBoxBg}), linear-gradient(var(--bgColor-default), var(--bgColor-default))`
+    }
+
+    if (sx) {
+      return (
+        <Box
+          {...boxProps}
+          className={clsx(className, {[classes.PointerBox]: enabled})}
+          sx={{
+            ...sxRest,
+            '--custom-bg': get(`colors.accent.emphasis`)({theme}),
+            backgroundImage: customBackground
+              ? `linear-gradient(var(--custom-bg), var(--custom-bg)), linear-gradient(var(--bgColor-default), var(--bgColor-default))`
+              : undefined,
+            position: 'relative',
+          }}
+        >
+          {children}
+          <Caret {...caretProps} />
+        </Box>
+      )
+    }
+    return (
+      <div {...boxProps} className={clsx(className, {[classes.PointerBox]: enabled})} style={styles}>
+        {children}
+        <Caret {...caretProps} />
+      </div>
+    )
+  }
 
   return (
     <Box
