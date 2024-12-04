0d6f81448ad7dc8a0ad129f9dd9e4d3c018557b8
Stephanie Hong <41085564+JelloBagel@users.noreply.github.com>
2024-12-04 18:04:31 +0000

    Convert BaseStyles to CSS modules behind team feature flag

--- packages/react/src/BaseStyles.tsx
+++ packages/react/src/BaseStyles.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import {clsx} from 'clsx'
 import styled, {createGlobalStyle} from 'styled-components'
 import type {SystemCommonProps, SystemTypographyProps} from './constants'
 import {COMMON, TYPOGRAPHY} from './constants'
 import {useTheme} from './ThemeProvider'
 import type {ComponentProps} from './utils/types'
+import {useFeatureFlag} from './FeatureFlags'
+import {toggleStyledComponent} from './internal/utils/toggleStyledComponent'
+import classes from './BaseStyles.module.css'
 
 // load polyfill for :focus-visible
 import 'focus-visible'
+
+const CSS_MODULES_FEATURE_FLAG = 'primer_react_css_modules_team'
 
 const GlobalStyle = createGlobalStyle<{colorScheme?: 'light' | 'dark'}>`
   * { box-sizing: border-box; }
@@ -34,12 +40,19 @@ const Base = styled.div<SystemTypographyProps & SystemCommonProps>`
   ${COMMON};
 `
 
-export type BaseStylesProps = ComponentProps<typeof Base>
+export type BaseStylesProps = ComponentProps<typeof Base> & {
+  style?: React.CSSProperties
+}
 
 function BaseStyles(props: BaseStylesProps) {
-  const {children, color = 'fg.default', fontFamily = 'normal', lineHeight = 'default', ...rest} = props
+  const {children, color = 'fg.default', fontFamily = 'normal', lineHeight = 'default', className, ...rest} = props
 
   const {colorScheme, dayScheme, nightScheme} = useTheme()
+  const enabled = useFeatureFlag(CSS_MODULES_FEATURE_FLAG)
+
+  const stylingProps = enabled ? {className: clsx(classes.BaseStyles, className)} : {className}
+
+  const StyledDiv = toggleStyledComponent(CSS_MODULES_FEATURE_FLAG, 'div', Base)
 
   /**
    * We need to map valid primer/react color modes onto valid color modes for primer/primitives
@@ -47,8 +60,9 @@ function BaseStyles(props: BaseStylesProps) {
    * valid color modes for primer/primer: auto | day | night | light | dark
    */
   return (
-    <Base
+    <StyledDiv
       {...rest}
+      {...stylingProps}
       color={color}
       fontFamily={fontFamily}
       lineHeight={lineHeight}
@@ -57,9 +71,9 @@ function BaseStyles(props: BaseStylesProps) {
       data-light-theme={dayScheme}
       data-dark-theme={nightScheme}
     >
-      <GlobalStyle colorScheme={colorScheme?.includes('dark') ? 'dark' : 'light'} />
+      {!enabled && <GlobalStyle colorScheme={colorScheme?.includes('dark') ? 'dark' : 'light'} />}
       {children}
-    </Base>
+    </StyledDiv>
   )
 }
 
