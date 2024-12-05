967127df4d3646ba2dff50657c3244d3cf788e45
Josh Black <joshblack@github.com>
2024-12-05 16:42:11 -0600

    refactor: move form control test files into folder

--- packages/react/src/FormControl/__tests__/FormControl.test.tsx
+++ packages/react/src/FormControl/__tests__/FormControl.test.tsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import {render} from '@testing-library/react'
-import {renderHook} from '@testing-library/react-hooks'
-import axe from 'axe-core'
-import {
-  Autocomplete,
-  Checkbox,
-  CheckboxGroup,
-  FormControl,
-  Radio,
-  Select,
-  Textarea,
-  TextInput,
-  TextInputWithTokens,
-  useFormControlForwardedProps,
-} from '..'
+import {Autocomplete, Checkbox, CheckboxGroup, Radio, Select, Textarea, TextInput, TextInputWithTokens} from '../..'
+import FormControl from '../../FormControl'
 import {MarkGithubIcon} from '@primer/octicons-react'
 
 const LABEL_TEXT = 'Form control'
@@ -331,18 +319,6 @@ describe('FormControl', () => {
         spy.mockRestore()
       })
     })
-
-    it('should have no axe violations', async () => {
-      const {container} = render(
-        <FormControl>
-          <FormControl.Label>{LABEL_TEXT}</FormControl.Label>
-          <TextInput />
-          <FormControl.Caption>{CAPTION_TEXT}</FormControl.Caption>
-        </FormControl>,
-      )
-      const results = await axe.run(container)
-      expect(results).toHaveNoViolations()
-    })
   })
 
   describe('checkbox and radio layout', () => {
@@ -440,67 +416,5 @@ describe('FormControl', () => {
         expect(getByTestId('checkbox-2')).not.toBeRequired()
       })
     })
-
-    it('should have no axe violations', async () => {
-      const {container} = render(
-        <FormControl>
-          <FormControl.Label>{LABEL_TEXT}</FormControl.Label>
-          <Checkbox />
-          <FormControl.Caption>{CAPTION_TEXT}</FormControl.Caption>
-        </FormControl>,
-      )
-      const results = await axe.run(container)
-      expect(results).toHaveNoViolations()
-    })
-  })
-})
-
-describe('useFormControlForwardedProps', () => {
-  describe('when used outside FormControl', () => {
-    test('returns empty object when no props object passed', () => {
-      const result = renderHook(() => useFormControlForwardedProps())
-      expect(result.result.current).toEqual({})
-    })
-
-    test('returns passed props object instance when passed', () => {
-      const props = {id: 'test-id'}
-      const result = renderHook(() => useFormControlForwardedProps(props))
-      expect(result.result.current).toBe(props)
-    })
-  })
-
-  test('provides context value when no props object is passed', () => {
-    const id = 'test-id'
-
-    const {result} = renderHook(() => useFormControlForwardedProps(), {
-      wrapper: ({children}: {children: React.ReactNode}) => (
-        <FormControl id={id} disabled required>
-          <FormControl.Label>Label</FormControl.Label>
-          {children}
-        </FormControl>
-      ),
-    })
-
-    expect(result.current.disabled).toBe(true)
-    expect(result.current.id).toBe(id)
-    expect(result.current.required).toBe(true)
-  })
-
-  test('merges with props object, overriding to prioritize props when conflicting', () => {
-    const props = {id: 'override-id', xyz: 'someValue'}
-
-    const {result} = renderHook(() => useFormControlForwardedProps(props), {
-      wrapper: ({children}: {children: React.ReactNode}) => (
-        <FormControl id="form-control-id" disabled>
-          <FormControl.Label>Label</FormControl.Label>
-          {children}
-        </FormControl>
-      ),
-    })
-
-    expect(result.current.disabled).toBe(true)
-    expect(result.current.id).toBe(props.id)
-    expect(result.current.required).toBeFalsy()
-    expect(result.current.xyz).toBe(props.xyz)
   })
 })
