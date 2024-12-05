f96d723f53c9175fb241023d89bd47c530a93078
Marie Lucca <francinelucca@github.com>
2024-12-05 17:51:26 -0500

    Revert "chore(ButtonGroup): Remove the CSS modules feature flag from ButtonGroup (#5339)"
    This reverts commit dc2f083eb3c07a4bcb9adcf9d2e8395d4daef1a9.

--- packages/react/src/ButtonGroup/ButtonGroup.tsx
+++ packages/react/src/ButtonGroup/ButtonGroup.tsx
@@ -1,24 +1,88 @@
+import styled from 'styled-components'
 import React from 'react'
+import {get} from '../constants'
+import sx from '../sx'
+import type {ComponentProps} from '../utils/types'
 import classes from './ButtonGroup.module.css'
+import {toggleStyledComponent} from '../internal/utils/toggleStyledComponent'
 import {clsx} from 'clsx'
+import {useFeatureFlag} from '../FeatureFlags'
 import {FocusKeys, useFocusZone} from '../hooks/useFocusZone'
 import {useProvidedRefOrCreate} from '../hooks'
 import type {ForwardRefComponent as PolymorphicForwardRefComponent} from '../utils/polymorphic'
-import {defaultSxProp} from '../utils/defaultSxProp'
-import Box from '../Box'
-import type {SxProp} from '../sx'
 
-export type ButtonGroupProps = {
-  /** The role of the group */
-  role?: string
-  /** className passed in for styling */
-  className?: string
-} & SxProp
+const StyledButtonGroup = toggleStyledComponent(
+  'primer_react_css_modules_ga',
+  'div',
+  styled.div`
+    display: inline-flex;
+    vertical-align: middle;
+    isolation: isolate;
+
+    && > *:not([data-loading-wrapper]):is(button, a) {
+      margin-inline-end: -1px;
+      position: relative;
+      border-radius: 0;
+
+      :first-of-type {
+        border-top-left-radius: ${get('radii.2')};
+        border-bottom-left-radius: ${get('radii.2')};
+      }
+
+      :last-of-type {
+        border-top-right-radius: ${get('radii.2')};
+        border-bottom-right-radius: ${get('radii.2')};
+      }
+
+      :focus,
+      :active,
+      :hover {
+        z-index: 1;
+      }
+    }
+
+    // if child is loading button
+    [data-loading-wrapper] {
+      :first-child {
+        button,
+        a {
+          border-top-left-radius: ${get('radii.2')};
+          border-bottom-left-radius: ${get('radii.2')};
+        }
+      }
+
+      :last-child {
+        button,
+        a {
+          border-top-right-radius: ${get('radii.2')};
+          border-bottom-right-radius: ${get('radii.2')};
+        }
+      }
+    }
+
+    [data-loading-wrapper] > * {
+      margin-inline-end: -1px;
+      position: relative;
+      border-radius: 0;
+
+      :focus,
+      :active,
+      :hover {
+        z-index: 1;
+      }
+    }
+
+    ${sx};
+  `,
+)
+
+export type ButtonGroupProps = ComponentProps<typeof StyledButtonGroup>
 
 const ButtonGroup = React.forwardRef<HTMLElement, ButtonGroupProps>(function ButtonGroup(
-  {className, role, sx, ...rest},
+  {children, className, role, ...rest},
   forwardRef,
 ) {
+  const enabled = useFeatureFlag('primer_react_css_modules_ga')
   const buttonRef = useProvidedRefOrCreate(forwardRef as React.RefObject<HTMLDivElement>)
 
   useFocusZone({
@@ -28,13 +92,18 @@ const ButtonGroup = React.forwardRef<HTMLElement, ButtonGroupProps>(function But
     focusOutBehavior: 'wrap',
   })
 
-  if (sx !== defaultSxProp) {
-    return (
-      <Box as="div" className={clsx(className, classes.ButtonGroup)} role={role} {...rest} sx={sx} ref={buttonRef} />
-    )
-  }
-
-  return <div ref={buttonRef} className={clsx(className, classes.ButtonGroup)} role={role} {...rest} />
+  return (
+    <StyledButtonGroup
+      ref={buttonRef}
+      className={clsx(className, {
+        [classes.ButtonGroup]: enabled,
+      })}
+      role={role}
+      {...rest}
+    >
+      {children}
+    </StyledButtonGroup>
+  )
 }) as PolymorphicForwardRefComponent<'div', ButtonGroupProps>
 
 ButtonGroup.displayName = 'ButtonGroup'
