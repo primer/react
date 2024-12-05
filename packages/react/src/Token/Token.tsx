f1f8177bacf46dd072b6422b7aaa88ce25eac9fe
Marie Lucca <francinelucca@github.com>
2024-12-05 17:56:50 -0500

    Revert "feat(Token): Migrate to CSS modules behind feature flag Pt 2 (#5271)"
    This reverts commit 05db651698af5a713cf0a5624016a2f86ab13a90.

--- packages/react/src/Token/Token.tsx
+++ packages/react/src/Token/Token.tsx
@@ -1,7 +1,8 @@
 import type {MouseEventHandler} from 'react'
 import React, {forwardRef} from 'react'
 import Box from '../Box'
-import {merge, type BetterSystemStyleObject, type SxProp} from '../sx'
+import type {BetterSystemStyleObject, SxProp} from '../sx'
+import {merge} from '../sx'
 import {defaultSxProp} from '../utils/defaultSxProp'
 import type {TokenBaseProps} from './TokenBase'
 import TokenBase, {defaultTokenSize, isTokenInteractive} from './TokenBase'
@@ -9,10 +10,6 @@ import RemoveTokenButton from './_RemoveTokenButton'
 import TokenTextContainer from './_TokenTextContainer'
 import type {ForwardRefComponent as PolymorphicForwardRefComponent} from '../utils/polymorphic'
 import VisuallyHidden from '../_VisuallyHidden'
-import {useFeatureFlag} from '../FeatureFlags'
-
-import classes from './Token.module.css'
-import {clsx} from 'clsx'
 
 // Omitting onResize and onResizeCapture because seems like React 18 types includes these menthod in the expansion but React 17 doesn't.
 // TODO: This is a temporary solution until we figure out why these methods are causing type errors.
@@ -22,8 +19,6 @@ export interface TokenProps extends TokenBaseProps, SxProp {
    */
   leadingVisual?: React.ElementType
 }
-
-const CSS_MODULES_FEATURE_FLAG = 'primer_react_css_modules_team'
 
 const tokenBorderWidthPx = 1
 
@@ -40,8 +35,6 @@ const LeadingVisualContainer: React.FC<React.PropsWithChildren<Pick<TokenBasePro
 )
 
 const Token = forwardRef((props, forwardedRef) => {
-  const enabled = useFeatureFlag(CSS_MODULES_FEATURE_FLAG)
-
   const {
     as,
     onRemove,
@@ -53,8 +46,6 @@ const Token = forwardRef((props, forwardedRef) => {
     href,
     onClick,
     sx: sxProp = defaultSxProp,
-    className,
-    style,
     ...rest
   } = props
   const hasMultipleActionTargets = isTokenInteractive(props) && Boolean(onRemove) && !hideRemoveButton
@@ -67,8 +58,7 @@ const Token = forwardRef((props, forwardedRef) => {
     href,
     onClick,
   }
-
-  const mergedSx = merge<BetterSystemStyleObject>(
+  const sx = merge<BetterSystemStyleObject>(
     {
       backgroundColor: 'neutral.subtle',
       borderColor: props.isSelected ? 'fg.default' : 'border.subtle',
@@ -90,56 +80,16 @@ const Token = forwardRef((props, forwardedRef) => {
     sxProp,
   )
 
-  if (enabled) {
-    return (
-      <TokenBase
-        onRemove={onRemove}
-        id={id?.toString()}
-        className={clsx(className, classes.Token)}
-        text={text}
-        size={size}
-        sx={sxProp}
-        data-is-selected={props.isSelected}
-        data-is-remove-btn={!(hideRemoveButton || !onRemove)}
-        {...(!hasMultipleActionTargets ? interactiveTokenProps : {})}
-        {...rest}
-        ref={forwardedRef}
-        style={{borderWidth: `${tokenBorderWidthPx}px`, ...style}}
-      >
-        {LeadingVisual ? (
-          <LeadingVisualContainer size={size}>
-            <LeadingVisual />
-          </LeadingVisualContainer>
-        ) : null}
-        <TokenTextContainer {...(hasMultipleActionTargets ? interactiveTokenProps : {})}>
-          {text}
-          {onRemove && <VisuallyHidden> (press backspace or delete to remove)</VisuallyHidden>}
-        </TokenTextContainer>
-
-        {!hideRemoveButton && onRemove ? (
-          <RemoveTokenButton
-            borderOffset={tokenBorderWidthPx}
-            onClick={onRemoveClick}
-            size={size}
-            isParentInteractive={isTokenInteractive(props)}
-            aria-hidden={hasMultipleActionTargets ? 'true' : 'false'}
-          />
-        ) : null}
-      </TokenBase>
-    )
-  }
-
   return (
     <TokenBase
       onRemove={onRemove}
       id={id?.toString()}
       text={text}
       size={size}
-      sx={mergedSx}
+      sx={sx}
       {...(!hasMultipleActionTargets ? interactiveTokenProps : {})}
       {...rest}
       ref={forwardedRef}
-      style={style}
     >
       {LeadingVisual ? (
         <LeadingVisualContainer size={size}>
