642d28ce7198ca40c45c4b7a5ac7d76ab1ba638e
Stephanie Hong <41085564+JelloBagel@users.noreply.github.com>
2024-12-11 00:27:46 +0000

    Convert SideNavLink to CSS modules behind feature flag

--- packages/react/src/SideNav.tsx
+++ packages/react/src/SideNav.tsx
@@ -3,8 +3,8 @@ import styled, {css} from 'styled-components'
 
 import Box from './Box'
 import type {ComponentProps} from './utils/types'
-import Link from './Link'
-import React from 'react'
+import Link, {type LinkProps} from './Link'
+import React, {type PropsWithChildren} from 'react'
 import {clsx} from 'clsx'
 import type {SxProp} from './sx'
 import sx from './sx'
@@ -61,10 +61,14 @@ function SideNav({
   const enabled = useFeatureFlag(CSS_MODULES_FEATURE_FLAG)
   const variantClassName = variant === 'lightweight' ? 'lightweight' : 'normal'
   const newClassName = clsx(
-    {[classes.SideNav]: enabled, [classes.SideNavBordered]: enabled && bordered},
-    'sidenav',
+    {
+      [classes.SideNav]: enabled,
+      [classes.SideNavBordered]: enabled && bordered,
+      [classes[`SideNavVariant-${variantClassName}`]]: enabled,
+      sidenav: !enabled,
+      [`variant-${variantClassName}`]: !enabled,
+    },
     className,
-    `variant-${variantClassName}`,
   )
   return (
     <StyledNav
@@ -79,11 +83,12 @@ function SideNav({
   )
 }
 
-type StyledSideNavLinkProps = {
+type StyledSideNavLinkProps = PropsWithChildren<{
   to?: To
   selected?: boolean
   variant?: 'full' | 'normal'
-}
+}> &
+  LinkProps
 
 // used for variant normal hover, focus pseudo selectors
 const CommonAccessibilityVariantNormalStyles = css`
@@ -97,106 +102,124 @@ const CommonAccessibilityVariantLightWeightStyles = css`
   text-decoration: none;
 `
 
-const SideNavLink = styled(Link).attrs<StyledSideNavLinkProps>(props => {
-  const isReactRouter = typeof props.to === 'string'
-  if (isReactRouter || props.selected) {
-    // according to their docs, NavLink supports aria-current:
-    // https://reacttraining.com/react-router/web/api/NavLink/aria-current-string
-    return {'aria-current': 'page'}
-  } else {
-    return {}
-  }
-})<StyledSideNavLinkProps & SxProp>`
-  position: relative;
-  display: block;
-  ${props =>
-    props.variant === 'full' &&
-    css`
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    `}
-  width: 100%;
-  text-align: left;
-  font-size: var(--base-size-14);
+const StyledSideNavLink = toggleStyledComponent(
+  CSS_MODULES_FEATURE_FLAG,
+  Link,
+  styled(Link)<StyledSideNavLinkProps & SxProp>`
+    position: relative;
+    display: block;
+    width: 100%;
+    font-size: 14px;
+    text-align: left;
+    text-decoration: none;
+    ${props =>
+      props.variant === 'full' &&
+      css`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      `}
 
-  & > .sidenav {
-    border-bottom: none;
-  }
-
-  .sidenav.variant-normal > & {
-    color: var(--fgColor-default);
-    padding: var(--base-size-16);
-    border: 0;
-    border-top: var(--borderWidth-thin) solid var(--borderColor-muted);
-
-    &:first-child {
-      border-top: 0;
-      border-top-right-radius: var(--borderRadius-medium);
-      border-top-left-radius: var(--borderRadius-medium);
+    & > .sidenav {
+      border-bottom: none;
     }
 
-    &:last-child {
-      border-bottom-right-radius: var(--borderRadius-medium);
-      border-bottom-left-radius: var(--borderRadius-medium);
-    }
+    .sidenav.variant-normal > & {
+      color: var(--fgColor-default);
+      padding: var(--base-size-16);
+      border: 0;
+      border-top: var(--borderWidth-thin) solid var(--borderColor-muted);
 
-    // Bar on the left
-    &::before {
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      z-index: 1;
-      width: 3px;
-      pointer-events: none;
-      content: '';
-    }
+      &:first-child {
+        border-top: 0;
+        border-top-right-radius: var(--borderRadius-medium);
+        border-top-left-radius: var(--borderRadius-medium);
+      }
 
-    &:hover {
-      ${CommonAccessibilityVariantNormalStyles}
-    }
-
-    &:focus {
-      ${CommonAccessibilityVariantNormalStyles}
-      outline: solid 2px var(--fgColor-accent);
-      z-index: 1;
-    }
-
-    &[aria-current='page'],
-    &[aria-selected='true'] {
-      background-color: var(--bgColor-default);
+      &:last-child {
+        border-bottom-right-radius: var(--borderRadius-medium);
+        border-bottom-left-radius: var(--borderRadius-medium);
+      }
 
       // Bar on the left
       &::before {
-        background-color: #fd8c73;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 1;
+        width: 3px;
+        pointer-events: none;
+        content: '';
+      }
+
+      &:hover {
+        ${CommonAccessibilityVariantNormalStyles}
+      }
+
+      &:focus {
+        ${CommonAccessibilityVariantNormalStyles}
+        outline: solid 2px var(--fgColor-accent);
+        z-index: 1;
+      }
+
+      &[aria-current='page'],
+      &[aria-selected='true'] {
+        background-color: var(--bgColor-default);
+
+        // Bar on the left
+        &::before {
+          background-color: var(--underlineNav-borderColor-active, var(--color-primer-border-active, #fd8c73));
+        }
       }
     }
-  }
 
-  .sidenav.variant-lightweight > & {
-    padding: var(--base-size-4) 0;
-    color: var(--fgColor-accent);
+    .sidenav.variant-lightweight > & {
+      padding: var(--base-size-4) 0;
+      color: var(--fgColor-accent);
 
-    &:hover {
-      ${CommonAccessibilityVariantLightWeightStyles}
+      &:hover {
+        ${CommonAccessibilityVariantLightWeightStyles}
+      }
+
+      &:focus {
+        ${CommonAccessibilityVariantLightWeightStyles}
+        outline: solid 1px var(--fgColor-accent);
+        z-index: 1;
+      }
+
+      &[aria-current='page'],
+      &[aria-selected='true'] {
+        color: var(--fgColor-default);
+        font-weight: var(--base-text-weight-medium);
+      }
     }
 
-    &:focus {
-      ${CommonAccessibilityVariantLightWeightStyles}
-      outline: solid 1px var(--fgColor-accent);
-      z-index: 1;
-    }
+    ${sx};
+  `,
+)
 
-    &[aria-current='page'],
-    &[aria-selected='true'] {
-      color: var(--fgColor-default);
-      font-weight: var(--base-text-weight-medium);
-    }
-  }
+const SideNavLink = ({selected, to, variant, className, children, ...rest}: StyledSideNavLinkProps) => {
+  const isReactRouter = typeof to === 'string'
+  const enabled = useFeatureFlag(CSS_MODULES_FEATURE_FLAG)
+  const newClassName = clsx(
+    {[classes.SideNavLink]: true, [classes.SideNavLinkFull]: enabled && variant === 'full'},
+    className,
+  )
 
-  ${sx};
-`
+  // according to their docs, NavLink supports aria-current:
+  // https://reacttraining.com/react-router/web/api/NavLink/aria-current-string
+  return (
+    <StyledSideNavLink
+      aria-current={isReactRouter || selected ? 'page' : undefined}
+      className={newClassName}
+      variant={variant}
+      {...rest}
+    >
+      {children}
+    </StyledSideNavLink>
+  )
+}
 
 SideNavLink.displayName = 'SideNav.Link'
 
