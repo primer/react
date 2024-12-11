0a05161374dbd12601b8fb8e293cc68a84292e31
Marie Lucca <francinelucca@github.com>
2024-12-11 16:41:54 -0500

    test(AutoComplete): add e2e tests

---
+++ e2e/components/Autocomplete.test.ts
@@ -0,0 +1,154 @@
+import {test, expect, type Page} from '@playwright/test'
+import {visit} from '../test-helpers/storybook'
+import {themes} from '../test-helpers/themes'
+
+const stories: Array<{title: string; id: string; setup: (page: Page) => void}> = [
+  {
+    title: 'Default',
+    id: 'components-autocomplete--default',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'Playground',
+    id: 'components-autocomplete--playground',
+    setup: async page => {
+      await page.keyboard.press('Backspace')
+    },
+  },
+  {
+    title: 'Add New Item',
+    id: 'components-autocomplete-features--add-new-item',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'Async Loading Of Items',
+    id: 'components-autocomplete-features--async-loading-of-items',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'Custom Overlay Menu Anchor',
+    id: 'components-autocomplete-features--custom-overlay-menu-anchor',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'Custom Search Filter Fn',
+    id: 'components-autocomplete-features--custom-search-filter-fn',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'Custom Sort After Menu Close',
+    id: 'components-autocomplete-features--custom-sort-after-menu-close',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'In A Dialog',
+    id: 'components-autocomplete-features--in-a-dialog',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('Enter')
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'In Overlay With Custom Scroll Container Ref',
+    id: 'components-autocomplete-features--in-overlay-with-custom-scroll-container-ref',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('Enter')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'Rendering The Menu Outside An Overlay',
+    id: 'components-autocomplete-features--rendering-the-menu-outside-an-overlay',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'With Callback When Overlay Open State Changes',
+    id: 'components-autocomplete-features--with-callback-when-overlay-open-state-changes',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'With Token Input',
+    id: 'components-autocomplete-features--with-token-input',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+  {
+    title: 'Sx Prop',
+    id: 'components-autocomplete-dev--sx-prop',
+    setup: async page => {
+      await page.keyboard.press('Tab')
+      await page.keyboard.press('D')
+    },
+  },
+]
+
+test.describe('Autocomplete', () => {
+  for (const story of stories) {
+    test.describe(story.title, () => {
+      for (const theme of themes) {
+        test.describe(theme, () => {
+          test('@vrt', async ({page}) => {
+            await visit(page, {
+              id: story.id,
+              globals: {
+                colorScheme: theme,
+              },
+            })
+
+            await story.setup(page)
+
+            expect(await page.screenshot()).toMatchSnapshot(`Autocomplete.${story.title}.${theme}.png`)
+          })
+
+          test('@aat', async ({page}) => {
+            await visit(page, {
+              id: story.id,
+              globals: {
+                colorScheme: theme,
+              },
+            })
+
+            await story.setup(page)
+
+            await expect(page).toHaveNoViolations({
+              rules: {
+                'color-contrast': {
+                  enabled: theme !== 'dark_dimmed',
+                },
+              },
+            })
+          })
+        })
+      }
+    })
+  }
+})
