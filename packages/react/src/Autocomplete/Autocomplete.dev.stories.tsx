06121c0284465fd34fa22b8aee2cb2d304ffd70c
Randall Krauskopf <104226843+randall-krauskopf@users.noreply.github.com>
2024-12-16 20:20:47 +0000

    fix dev storybook accessibility

--- packages/react/src/Autocomplete/Autocomplete.dev.stories.tsx
+++ packages/react/src/Autocomplete/Autocomplete.dev.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import type {Meta} from '@storybook/react'
 
 import Autocomplete from './Autocomplete'
+import FormControl from '../FormControl'
+import Box from '../Box'
 
 const autocompleteStoryMeta: Meta = {
   title: 'Components/Autocomplete/Dev',
@@ -9,31 +11,36 @@ const autocompleteStoryMeta: Meta = {
 
 export const SxProp = () => {
   return (
-    <Autocomplete id="autocompleteId">
-      <Autocomplete.Input
-        sx={{
-          width: '100%',
-          marginRight: '10px',
-        }}
-        size="medium"
-        style={{backgroundColor: 'pink'}}
-      />
-      <Autocomplete.Overlay
-        className="test-class-name"
-        visibility="visible"
-        sx={{
-          maxHeight: 'min(50vh, 280px)',
-          overflowY: 'scroll',
-          ' div': {
-            flexDirection: 'column',
-            whiteSpace: 'pre-wrap',
-          },
-        }}
-        style={{backgroundColor: 'lightblue'}}
-      >
-        <Autocomplete.Menu items={[]} selectedItemIds={[]} aria-labelledby="sample" />
-      </Autocomplete.Overlay>
-    </Autocomplete>
+    <form>
+      <FormControl required={true}>
+        <FormControl.Label id="autocompleteLabel">Label</FormControl.Label>
+        <Autocomplete>
+          <Autocomplete.Input
+            sx={{
+              width: '100%',
+              marginRight: '10px',
+            }}
+            size="medium"
+            style={{backgroundColor: 'pink'}}
+          />
+          <Autocomplete.Overlay
+            className="test-class-name"
+            visibility="visible"
+            sx={{
+              maxHeight: 'min(50vh, 280px)',
+              overflowY: 'scroll',
+              ' div': {
+                flexDirection: 'column',
+                whiteSpace: 'pre-wrap',
+              },
+            }}
+            style={{backgroundColor: 'lightblue'}}
+          >
+            <Autocomplete.Menu items={[]} selectedItemIds={[]} aria-labelledby="autocompleteLabel" />
+          </Autocomplete.Overlay>
+        </Autocomplete>
+      </FormControl>
+    </form>
   )
 }
 
