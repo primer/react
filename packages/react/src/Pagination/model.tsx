5dbc09a0ef65aa7ff6cfb752687abda7f09a77fa
hectahertz <hectahertz@gmail.com>
2025-01-07 19:18:48 +0100

    Simplify Pagination algorithm with an early return

--- packages/react/src/Pagination/model.tsx
+++ packages/react/src/Pagination/model.tsx
@@ -5,81 +5,94 @@ export function buildPaginationModel(
   marginPageCount: number,
   surroundingPageCount: number,
 ) {
+  const prev = {type: 'PREV', num: currentPage - 1, disabled: currentPage === 1}
+  const next = {type: 'NEXT', num: currentPage + 1, disabled: currentPage === pageCount}
+
+  if (!showPages) {
+    return [prev, next]
+  }
+
   const pages = []
-
-  if (showPages) {
-    const pageNums: Array<number> = []
-    const addPage = (n: number) => {
-      if (n >= 1 && n <= pageCount) {
-        pageNums.push(n)
-      }
+  const pageNums: Array<number> = []
+  const addPage = (n: number) => {
+    if (n >= 1 && n <= pageCount) {
+      pageNums.push(n)
     }
+  }
 
-    // Start by defining the window of pages to show around the current page.
-    // If the window goes off either edge, shift it until it fits.
-    let extentLeft = currentPage - surroundingPageCount
-    let extentRight = currentPage + surroundingPageCount
-    if (extentLeft < 1 && extentRight > pageCount) {
-      // Our window is larger than the entire range,
-      // so simply display every page.
-      extentLeft = 1
-      extentRight = pageCount
-    } else if (extentLeft < 1) {
-      while (extentLeft < 1) {
-        extentLeft++
-        extentRight++
-      }
-    } else if (extentRight > pageCount) {
-      while (extentRight > pageCount) {
-        extentLeft--
-        extentRight--
-      }
+  // Start by defining the window of pages to show around the current page.
+  // If the window goes off either edge, shift it until it fits.
+  let extentLeft = currentPage - surroundingPageCount
+  let extentRight = currentPage + surroundingPageCount
+  if (extentLeft < 1 && extentRight > pageCount) {
+    // Our window is larger than the entire range,
+    // so simply display every page.
+    extentLeft = 1
+    extentRight = pageCount
+  } else if (extentLeft < 1) {
+    while (extentLeft < 1) {
+      extentLeft++
+      extentRight++
     }
-
-    // Next, include the pages in the margins.
-    // If a margin page is already covered in the window,
-    // extend the window to the other direction.
-    for (let i = 1; i <= marginPageCount; i++) {
-      const leftPage = i
-      const rightPage = pageCount - (i - 1)
-      if (leftPage >= extentLeft) {
-        extentRight++
-      } else {
-        addPage(leftPage)
-      }
-      if (rightPage <= extentRight) {
-        extentLeft--
-      } else {
-        addPage(rightPage)
-      }
+  } else if (extentRight > pageCount) {
+    while (extentRight > pageCount) {
+      extentLeft--
+      extentRight--
     }
+  }
 
-    for (let i = extentLeft; i <= extentRight; i++) {
-      addPage(i)
+  // Next, include the pages in the margins.
+  // If a margin page is already covered in the window,
+  // extend the window to the other direction.
+  for (let i = 1; i <= marginPageCount; i++) {
+    const leftPage = i
+    const rightPage = pageCount - (i - 1)
+    if (leftPage >= extentLeft) {
+      extentRight++
+    } else {
+      addPage(leftPage)
     }
+    if (rightPage <= extentRight) {
+      extentLeft--
+    } else {
+      addPage(rightPage)
+    }
+  }
 
-    const sorted = pageNums
-      .slice()
-      .sort((a, b) => a - b)
-      .filter((item, idx, ary) => !idx || item !== ary[idx - 1])
-    for (let idx = 0; idx < sorted.length; idx++) {
-      const num = sorted[idx]
-      const selected = num === currentPage
-      const last = sorted[idx - 1]
-      const next = sorted[idx + 1]
-      const lastDelta = num - last
-      const nextDelta = num - next
-      const precedesBreak = nextDelta !== -1
+  for (let i = extentLeft; i <= extentRight; i++) {
+    addPage(i)
+  }
 
-      if (idx === 0) {
-        if (num !== 1) {
-          // If the first page isn't page one,
-          // we need to add a break
-          pages.push({
-            type: 'BREAK',
-            num: 1,
-          })
-        }
+  const sorted = pageNums
+    .slice()
+    .sort((a, b) => a - b)
+    .filter((item, idx, ary) => !idx || item !== ary[idx - 1])
+  for (let idx = 0; idx < sorted.length; idx++) {
+    const num = sorted[idx]
+    const selected = num === currentPage
+    const last = sorted[idx - 1]
+    const next = sorted[idx + 1]
+    const lastDelta = num - last
+    const nextDelta = num - next
+    const precedesBreak = nextDelta !== -1
+
+    if (idx === 0) {
+      if (num !== 1) {
+        // If the first page isn't page one,
+        // we need to add a break
+        pages.push({
+          type: 'BREAK',
+          num: 1,
+        })
+      }
+      pages.push({
+        type: 'NUM',
+        num,
+        selected,
+        precedesBreak,
+      })
+    } else {
+      if (lastDelta === 1) {
         pages.push({
           type: 'NUM',
           num,
@@ -87,42 +100,31 @@ export function buildPaginationModel(
           precedesBreak,
         })
       } else {
-        if (lastDelta === 1) {
-          pages.push({
-            type: 'NUM',
-            num,
-            selected,
-            precedesBreak,
-          })
-        } else {
-          // We skipped some, so add a break
-          pages.push({
-            type: 'BREAK',
-            num: num - 1,
-          })
-          pages.push({
-            type: 'NUM',
-            num,
-            selected,
-            precedesBreak: false,
-          })
-        }
+        // We skipped some, so add a break
+        pages.push({
+          type: 'BREAK',
+          num: num - 1,
+        })
+        pages.push({
+          type: 'NUM',
+          num,
+          selected,
+          precedesBreak: false,
+        })
       }
-    }
-
-    const lastPage = pages[pages.length - 1]
-    if (lastPage.type === 'NUM' && lastPage.num !== pageCount) {
-      // The last page we rendered wasn't the actual last page,
-      // so we need an additional break
-      pages.push({
-        type: 'BREAK',
-        num: pageCount,
-      })
     }
   }
 
-  const prev = {type: 'PREV', num: currentPage - 1, disabled: currentPage === 1}
-  const next = {type: 'NEXT', num: currentPage + 1, disabled: currentPage === pageCount}
+  const lastPage = pages[pages.length - 1]
+  if (lastPage.type === 'NUM' && lastPage.num !== pageCount) {
+    // The last page we rendered wasn't the actual last page,
+    // so we need an additional break
+    pages.push({
+      type: 'BREAK',
+      num: pageCount,
+    })
+  }
+
   return [prev, ...pages, next]
 }
 
