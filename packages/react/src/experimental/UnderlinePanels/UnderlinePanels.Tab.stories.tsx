868145abcad728b809effa69aac3755c4c68e3dd
Daniel Doyle <ddoyle2017@github.com>
2025-01-15 15:43:31 +0000

    fixed UnderlinePanels.Tab story rendering issues

--- packages/react/src/experimental/UnderlinePanels/UnderlinePanels.Tab.stories.tsx
+++ packages/react/src/experimental/UnderlinePanels/UnderlinePanels.Tab.stories.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import type {Meta, StoryFn} from '@storybook/react'
 import UnderlinePanels from './UnderlinePanels'
-import {PeopleIcon, TerminalIcon, CodespacesIcon} from '@primer/octicons-react'
-import {OcticonArgType} from '../../utils/story-helpers'
 
 export default {
   title: 'Experimental/Components/UnderlinePanels.Tab',
   component: UnderlinePanels.Tab,
-  decorators: [
-    Story => {
-      return (
-        <UnderlinePanels aria-label="Select a tab">
-          <Story />
-        </UnderlinePanels>
-      )
-    },
-  ],
   parameters: {
     controls: {
       expanded: true,
@@ -25,7 +14,6 @@ export default {
   args: {
     'aria-selected': true,
     counter: '14K',
-    icon: PeopleIcon,
   },
   argTypes: {
     'aria-selected': {
@@ -36,7 +24,6 @@ export default {
     counter: {
       type: 'string',
     },
-    icon: OcticonArgType([PeopleIcon, TerminalIcon, CodespacesIcon]),
   },
 } as Meta<typeof UnderlinePanels.Tab>
 
