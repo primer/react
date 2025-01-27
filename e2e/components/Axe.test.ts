74302e662e6b908aca169ed4c56dac6c89069066
Kendall Gassner <kendallgassner@github.com>
2025-01-27 12:36:01 -0800

    test

--- e2e/components/Axe.test.ts
+++ e2e/components/Axe.test.ts
@@ -1,33 +1,30 @@
 import {test, expect} from '@playwright/test'
-import componentsConfig from '../../packages/react/generated/components.json'
+import componentsConfig from '../../docs/public/storybook/index.json'
 import {visit} from '../test-helpers/storybook'
 import {themes} from '../test-helpers/themes'
 
 type Component = {
-  stories: Array<{
-    id: string
-  }>
+  name: string
 }
 
-const {components} = componentsConfig
+const {entries} = componentsConfig
 
 test.describe('@aat', () => {
-  for (const [name, component] of Object.entries(components as Record<string, Component>)) {
-    test.describe(name, () => {
-      for (const story of component.stories) {
-        for (const theme of themes) {
-          test.describe(theme, () => {
-            test(`${story.id}`, async ({page}) => {
-              await visit(page, {
-                id: story.id,
-                globals: {
-                  colorScheme: themes[0],
-                },
-              })
-              await expect(page).toHaveNoViolations()
+  for (const [id, entry] of Object.entries(entries as Record<string, Component>)) {
+    const {name} = entry
+    test.describe(id, () => {
+      for (const theme of themes) {
+        test.describe(theme, () => {
+          test(name, async ({page}) => {
+            await visit(page, {
+              id: id,
+              globals: {
+                colorScheme: themes[0],
+              },
             })
+            await expect(page).toHaveNoViolations()
           })
-        }
+        })
       }
     })
   }
