8ea95ad9f57cdc32b82cd346d79ab55b8c55ce72
Tyler Jones <tylerjdev@github.com>
2025-01-28 14:05:39 -0500

    Remove story test

--- e2e/components/NavList.test.ts
+++ e2e/components/NavList.test.ts
@@ -170,32 +170,4 @@ test.describe('NavList', () => {
       })
     }
   })
-
-  test.describe('With group expand', () => {
-    for (const theme of themes) {
-      test.describe(theme, () => {
-        test('default @vrt', async ({page}) => {
-          await visit(page, {
-            id: 'components-navlist--with-group-expand',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-
-          // Default state
-          expect(await page.screenshot()).toMatchSnapshot(`NavList.With group expand.${theme}.png`)
-        })
-
-        test('axe @aat', async ({page}) => {
-          await visit(page, {
-            id: 'components-navlist--with-group-expand',
-            globals: {
-              colorScheme: theme,
-            },
-          })
-          await expect(page).toHaveNoViolations()
-        })
-      })
-    }
-  })
 })
