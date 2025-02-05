d4253a841b8a047f7a8617d1eb1eb61404dc16de
Kate Travers <8152930+ktravers@users.noreply.github.com>
2025-02-05 02:40:39 +0000

    fixup callback

--- packages/react/src/hooks/useOpenAndCloseFocus.ts
+++ packages/react/src/hooks/useOpenAndCloseFocus.ts
@@ -16,25 +16,21 @@ export function useOpenAndCloseFocus({
   preventFocusOnOpen,
 }: UseOpenAndCloseFocusSettings): void {
   useEffect(() => {
-    const isOpen = containerRef.current || initialFocusRef?.current
-
-    // Do nothing if component is open and focus should be prevented on open
-    if (isOpen && preventFocusOnOpen) {
-      return
+    // If focus should be applied on open, apply focus to correct element,
+    // either the initialFocusRef if given, otherwise the first focusable element
+    if (!preventFocusOnOpen) {
+      if (initialFocusRef && initialFocusRef.current) {
+        initialFocusRef.current.focus()
+      } else if (containerRef.current) {
+        const firstItem = iterateFocusableElements(containerRef.current).next().value
+        firstItem?.focus()
+      }
     }
 
-    // If initial focus ref is given and present, apply focus
-    if (initialFocusRef && initialFocusRef.current) {
-      initialFocusRef.current.focus()
-    // If container ref is present, apply focus
-    } else if (containerRef.current) {
-      const firstItem = iterateFocusableElements(containerRef.current).next().value
-      firstItem?.focus()
-    }
-
-    // Else, component is closed, and focus should be applied to given returnFocusRef element
+    // If returnFocusRef element is rendered, apply focus
+    const returnFocusRefCurrent = returnFocusRef.current
     return function () {
-      returnFocusRef.current?.focus()
+      returnFocusRefCurrent?.focus()
     }
   }, [initialFocusRef, returnFocusRef, containerRef, preventFocusOnOpen])
 }
