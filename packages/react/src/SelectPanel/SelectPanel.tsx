ef3ce9e0957b5f7440aa52ad012ac9058e20e1ee
Siddharth Kshetrapal <siddharthkp@github.com>
2025-03-13 22:18:38 +0100

    add onCancel prop

--- packages/react/src/SelectPanel/SelectPanel.tsx
+++ packages/react/src/SelectPanel/SelectPanel.tsx
@@ -1,4 +1,4 @@
-import {SearchIcon, TriangleDownIcon} from '@primer/octicons-react'
+import {SearchIcon, TriangleDownIcon, XIcon} from '@primer/octicons-react'
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react'
 import type {AnchoredOverlayProps} from '../AnchoredOverlay'
 import {AnchoredOverlay} from '../AnchoredOverlay'
@@ -11,7 +11,7 @@ import type {OverlayProps} from '../Overlay'
 import type {TextInputProps} from '../TextInput'
 import type {ItemProps, ItemInput} from './types'
 
-import {Button} from '../Button'
+import {Button, IconButton} from '../Button'
 import {useProvidedRefOrCreate} from '../hooks'
 import type {FocusZoneHookSettings} from '../hooks/useFocusZone'
 import {useId} from '../hooks/useId'
@@ -22,7 +22,6 @@ import type {FilteredActionListLoadingType} from '../FilteredActionList/Filtered
 import {FilteredActionListLoadingTypes} from '../FilteredActionList/FilteredActionListLoaders'
 import {useFeatureFlag} from '../FeatureFlags'
 import {announce} from '@primer/live-region-element'
-
 import classes from './SelectPanel.module.css'
 import {clsx} from 'clsx'
 
@@ -128,6 +127,7 @@ interface SelectPanelBaseProps {
   footer?: string | React.ReactElement
   initialLoadingType?: InitialLoadingType
   className?: string
+  onCancel?: () => void
 }
 
 export type SelectPanelProps = SelectPanelBaseProps &
@@ -189,6 +189,7 @@ export function SelectPanel({
   height,
   width,
   id,
+  onCancel,
   ...listProps
 }: SelectPanelProps): JSX.Element {
   const titleId = useId()
@@ -352,7 +353,7 @@ export function SelectPanel({
     [onOpenChange],
   )
   const onClose = useCallback(
-    (gesture: Parameters<Exclude<AnchoredOverlayProps['onClose'], undefined>>[0] | 'selection') => {
+    (gesture: Parameters<Exclude<AnchoredOverlayProps['onClose'], undefined>>[0] | 'selection' | 'escape') => {
       onOpenChange(false, gesture)
     },
     [onOpenChange],
@@ -473,24 +474,54 @@ export function SelectPanel({
           sx={enabled ? undefined : {display: 'flex', flexDirection: 'column', height: 'inherit', maxHeight: 'inherit'}}
           className={enabled ? classes.Wrapper : undefined}
         >
-          <Box sx={enabled ? undefined : {pt: 2, px: 3}} className={enabled ? classes.Content : undefined}>
-            <Heading
-              as="h1"
-              id={titleId}
-              sx={enabled ? undefined : {fontSize: 1}}
-              className={enabled ? classes.Title : undefined}
-            >
-              {title}
-            </Heading>
-            {subtitle ? (
-              <Box
-                id={subtitleId}
-                sx={enabled ? undefined : {fontSize: 0, color: 'fg.muted'}}
-                className={enabled ? classes.Subtitle : undefined}
+          <Box
+            sx={
+              enabled
+                ? undefined
+                : {
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    paddingTop: 2,
+                    paddingRight: 2,
+                    paddingLeft: 2,
+                  }
+            }
+            className={enabled ? classes.Header : undefined}
+          >
+            <div>
+              <Heading
+                as="h1"
+                id={titleId}
+                sx={enabled ? undefined : {fontSize: 1, marginLeft: 2}}
+                className={enabled ? classes.Title : undefined}
               >
-                {subtitle}
-              </Box>
-            ) : null}
+                {title}
+              </Heading>
+              {subtitle ? (
+                <Box
+                  id={subtitleId}
+                  sx={enabled ? undefined : {marginLeft: 2, fontSize: 0, color: 'fg.muted'}}
+                  className={enabled ? classes.Subtitle : undefined}
+                >
+                  {subtitle}
+                </Box>
+              ) : null}
+            </div>
+            {onCancel && (
+              <IconButton
+                type="button"
+                variant="invisible"
+                icon={XIcon}
+                aria-label="Cancel and close"
+                sx={enabled ? undefined : {display: ['inline-grid', 'inline-grid', 'none', 'none']}}
+                className={enabled ? classes.ResponsiveCloseButton : undefined}
+                onClick={() => {
+                  onCancel()
+                  onClose('escape')
+                }}
+              />
+            )}
           </Box>
           <FilteredActionList
             filterValue={filterValue}
@@ -533,11 +564,40 @@ export function SelectPanel({
             </Box>
           ) : (
             <Box
-              sx={enabled ? undefined : {margin: 2, display: ['block', 'block', 'none', 'none']}}
-              className={enabled ? classes.ResponsiveSaveButton : undefined}
+              sx={
+                enabled
+                  ? undefined
+                  : {
+                      // .responsiveFooter
+                      display: ['flex', 'flex', 'none', 'none'],
+                      gap: 'var(--stack-gap-condensed)',
+                      justifyContent: 'right',
+                      padding: 3,
+                      // .Footer
+                      borderTop: 'var(--borderWidth-thin) solid',
+                      borderTopColor: 'var(--borderColor-default)',
+                    }
+              }
+              className={enabled ? clsx(classes.Footer, classes.ResponsiveFooter) : undefined}
             >
-              {/* we add a save button on narrow screens because SelectPanel is full-screen */}
-              <Button variant="primary" block onClick={() => onClose('click-outside')}>
+              {/* we add a save and cancel button on narrow screens when SelectPanel is full-screen */}
+              {onCancel && (
+                <Button
+                  size="medium"
+                  onClick={() => {
+                    onCancel()
+                    onClose('escape')
+                  }}
+                >
+                  Cancel
+                </Button>
+              )}
+              <Button
+                variant="primary"
+                size="medium"
+                block={onCancel ? false : true}
+                onClick={() => onClose('click-outside')}
+              >
                 Save
               </Button>
             </Box>
