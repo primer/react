21e9aa5f0e80c26d0a7058ef46f3535dac641f83
Lukas Oppermann <lukasoppermann@github.com>
2025-03-17 10:07:46 +0100

    Update packages/react/src/Button/Button.features.stories.tsx
    Co-authored-by: Josh Black <joshblack@github.com>

--- packages/react/src/Button/Button.features.stories.tsx
+++ packages/react/src/Button/Button.features.stories.tsx
@@ -64,7 +64,7 @@ export const TrailingCounter = () => {
 export const TrailingCounterWithNoText = () => <Button aria-label="Comments" leadingVisual={CommentIcon} count={3} />
 
 export const TrailingCounterWithHumanFormat = () => (
-  <Button aria-label="Comments" leadingVisual={CommentIcon} count={'3.2k'} />
+  <Button aria-label="Comments" leadingVisual={CommentIcon} count="3.2k" />
 )
 
 export const TrailingCounterAllVariants = () => {
