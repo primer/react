f91b71e4f4d7bfa4ecfd0510956f0d1b711c2733
Siddharth Kshetrapal <siddharthkp@github.com>
2025-03-19 12:02:21 +0100

    add onCancel to single selection story

--- packages/react/src/SelectPanel/SelectPanel.features.stories.tsx
+++ packages/react/src/SelectPanel/SelectPanel.features.stories.tsx
@@ -171,6 +171,7 @@ export const SingleSelect = () => {
         selected={selected}
         onSelectedChange={setSelected}
         onFilterChange={setFilter}
+        onCancel={() => setOpen(false)}
         width="medium"
       />
     </FormControl>
