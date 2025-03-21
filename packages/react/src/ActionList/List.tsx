0d0b1812d6ad9d27fc5dc39f3f0359cbb3189997
Jon Rohan <yes@jonrohan.codes>
2025-03-21 16:53:33 +0000

    Remove the CSS module feature flag from ActionList.List

--- packages/react/src/ActionList/List.tsx
+++ packages/react/src/ActionList/List.tsx
@@ -12,9 +12,7 @@ import {ListContext, type ActionListProps} from './shared'
 import {useProvidedRefOrCreate} from '../hooks'
 import {FocusKeys, useFocusZone} from '../hooks/useFocusZone'
 import {clsx} from 'clsx'
-import {useFeatureFlag} from '../FeatureFlags'
 import classes from './ActionList.module.css'
-import {actionListCssModulesFlag} from './featureflag'
 
 const ListBox = styled.ul<SxProp>(sx)
 
@@ -23,12 +21,6 @@ export const List = React.forwardRef<HTMLUListElement, ActionListProps>(
     {variant = 'inset', selectionVariant, showDividers = false, role, sx: sxProp = defaultSxProp, className, ...props},
     forwardedRef,
   ): JSX.Element => {
-    const styles = {
-      margin: 0,
-      paddingInlineStart: 0, // reset ul styles
-      paddingY: variant === 'inset' ? 2 : 0,
-    }
-
     const [slots, childrenWithoutSlots] = useSlots(props.children, {
       heading: Heading,
     })
@@ -58,8 +50,6 @@ export const List = React.forwardRef<HTMLUListElement, ActionListProps>(
       focusOutBehavior: listRole === 'menu' ? 'wrap' : undefined,
     })
 
-    const enabled = useFeatureFlag(actionListCssModulesFlag)
-
     return (
       <ListContext.Provider
         value={{
@@ -71,44 +61,31 @@ export const List = React.forwardRef<HTMLUListElement, ActionListProps>(
         }}
       >
         {slots.heading}
-        {enabled ? (
-          sxProp !== defaultSxProp ? (
-            <ListBox
-              sx={merge(styles, sxProp as SxProp)}
-              className={clsx(classes.ActionList, className)}
-              role={listRole}
-              aria-labelledby={ariaLabelledBy}
-              ref={listRef}
-              data-dividers={showDividers}
-              data-variant={variant}
-              {...props}
-            >
-              {childrenWithoutSlots}
-            </ListBox>
-          ) : (
-            <ul
-              className={clsx(classes.ActionList, className)}
-              role={listRole}
-              aria-labelledby={ariaLabelledBy}
-              ref={listRef}
-              data-dividers={showDividers}
-              data-variant={variant}
-              {...props}
-            >
-              {childrenWithoutSlots}
-            </ul>
-          )
-        ) : (
+        {sxProp !== defaultSxProp ? (
           <ListBox
-            sx={merge(styles, sxProp as SxProp)}
+            sx={sxProp as SxProp}
+            className={clsx(classes.ActionList, className)}
             role={listRole}
             aria-labelledby={ariaLabelledBy}
-            {...props}
             ref={listRef}
-            className={className}
+            data-dividers={showDividers}
+            data-variant={variant}
+            {...props}
           >
             {childrenWithoutSlots}
           </ListBox>
+        ) : (
+          <ul
+            className={clsx(classes.ActionList, className)}
+            role={listRole}
+            aria-labelledby={ariaLabelledBy}
+            ref={listRef}
+            data-dividers={showDividers}
+            data-variant={variant}
+            {...props}
+          >
+            {childrenWithoutSlots}
+          </ul>
         )}
       </ListContext.Provider>
     )
