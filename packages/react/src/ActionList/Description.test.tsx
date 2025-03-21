d3eb7e583a64b1f5dc299289e0d5ed2ed5f257b6
Jon Rohan <yes@jonrohan.codes>
2025-03-21 17:34:22 +0000

    Remove import

--- packages/react/src/ActionList/Description.test.tsx
+++ packages/react/src/ActionList/Description.test.tsx
@@ -1,7 +1,6 @@
 import {render as HTMLRender} from '@testing-library/react'
 import React from 'react'
 import {ActionList} from '.'
-import {FeatureFlags} from '../FeatureFlags'
 
 describe('ActionList.Description', () => {
   it('should render the description as inline without truncation by default', () => {
