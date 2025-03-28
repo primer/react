81a619b209fff5fcbfa6f8cc0cbc2b75a8323075
Marie Lucca <francinelucca@github.com>
2025-03-27 23:48:46 -0400

    add deo

--- packages/react/src/FilteredActionList/useAnnouncements.tsx
+++ packages/react/src/FilteredActionList/useAnnouncements.tsx
@@ -115,6 +115,6 @@ export const useAnnouncements = (
         })
       })
     },
-    [announce, isFirstRender, items, listContainerRef, liveRegion],
+    [announce, isFirstRender, items, listContainerRef, liveRegion, loading],
   )
 }
