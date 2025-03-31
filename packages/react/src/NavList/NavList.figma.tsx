afafa7de855d3bee63d10716e3318b3a4625e315
Lukas Oppermann <lukasoppermann@github.com>
2025-03-31 18:51:34 +0200

    Code Connect for NavList (#5755)

--- packages/react/src/NavList/NavList.figma.tsx
+++ packages/react/src/NavList/NavList.figma.tsx
@@ -12,3 +12,70 @@ figma.connect(
     example: ({children}) => <NavList>{children}</NavList>,
   },
 )
+
+figma.connect(
+  NavList.Item,
+  'https://www.figma.com/design/GCvY3Qv8czRgZgvl1dG6lp/Primer-Web?node-id=40009-6314&t=3Scp7X8EXitkqgLF-4',
+  {
+    props: {
+      actionList: figma.nestedProps('label and description', {
+        text: figma.textContent('label'),
+      }),
+    },
+    example: ({actionList}) => <NavList.Item href="#">{actionList.text}</NavList.Item>,
+  },
+)
+/** Subitem **/
+figma.connect(
+  NavList.Item,
+  'https://www.figma.com/design/GCvY3Qv8czRgZgvl1dG6lp/Primer-Web?node-id=15175-71561&m=dev',
+  {
+    props: {
+      actionList: figma.nestedProps('label and description', {
+        text: figma.textContent('label'),
+      }),
+    },
+    example: ({actionList}) => <NavList.Item href="#">{actionList.text}</NavList.Item>,
+  },
+)
+/** Collapsable item **/
+figma.connect(
+  NavList.Item,
+  'https://www.figma.com/design/GCvY3Qv8czRgZgvl1dG6lp/Primer-Web?node-id=16716-62586&m=dev',
+  {
+    props: {
+      actionList: figma.nestedProps('label and description', {
+        text: figma.textContent('label'),
+      }),
+      children: figma.children('NavList.Item/SubItem'),
+    },
+    example: ({actionList, children}) => (
+      <NavList.Item>
+        {actionList.text}
+        <NavList.SubNav>{children}</NavList.SubNav>
+      </NavList.Item>
+    ),
+  },
+)
+
+figma.connect(
+  NavList.GroupHeading,
+  'https://www.figma.com/design/GCvY3Qv8czRgZgvl1dG6lp/Primer-Web?node-id=40065-84253&t=3Scp7X8EXitkqgLF-4',
+  {
+    props: {
+      actionList: figma.nestedProps('ActionList.GroupHeading', {
+        title: figma.string('title'),
+      }),
+    },
+    example: ({actionList}) => <NavList.GroupHeading>{actionList.title}</NavList.GroupHeading>,
+  },
+)
+
+figma.connect(
+  NavList.Divider,
+  'https://www.figma.com/design/GCvY3Qv8czRgZgvl1dG6lp/Primer-Web?node-id=40065-83912&t=3Scp7X8EXitkqgLF-4',
+  {
+    props: {},
+    example: () => <NavList.Divider />,
+  },
+)
