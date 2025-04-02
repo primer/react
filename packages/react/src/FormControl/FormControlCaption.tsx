a812a868c8eb98c3a1ad6540806b7db2bb75aa0d
Jamie Shark <5520141+jamieshark@users.noreply.github.com>
2025-04-02 17:06:12 -0500

    Remove the CSS modules feature flag from the FormControl component (#5851)
    Co-authored-by: francinelucca <40550942+francinelucca@users.noreply.github.com>

--- packages/react/src/FormControl/FormControlCaption.tsx
+++ packages/react/src/FormControl/FormControlCaption.tsx
@@ -1,14 +1,10 @@
 import {clsx} from 'clsx'
 import React from 'react'
-import styled from 'styled-components'
-import {cssModulesFlag} from './feature-flags'
-import {useFeatureFlag} from '../FeatureFlags'
 import Text from '../Text'
-import sx from '../sx'
 import type {SxProp} from '../sx'
 import classes from './FormControlCaption.module.css'
 import {useFormControlContext} from './_FormControlContext'
-import {toggleStyledComponent} from '../internal/utils/toggleStyledComponent'
+import {toggleSxComponent} from '../internal/utils/toggleSxComponent'
 
 type FormControlCaptionProps = React.PropsWithChildren<
   {
@@ -17,37 +13,21 @@ type FormControlCaptionProps = React.PropsWithChildren<
   } & SxProp
 >
 
+const Caption = toggleSxComponent({}, Text) as React.ComponentType<FormControlCaptionProps>
+
 function FormControlCaption({id, children, sx, className}: FormControlCaptionProps) {
-  const enabled = useFeatureFlag(cssModulesFlag)
   const {captionId, disabled} = useFormControlContext()
+
   return (
-    <StyledCaption
+    <Caption
       id={id ?? captionId}
-      className={clsx(className, {
-        [classes.Caption]: enabled,
-      })}
+      className={clsx(className, classes.Caption)}
       data-control-disabled={disabled ? '' : undefined}
       sx={sx}
     >
       {children}
-    </StyledCaption>
+    </Caption>
   )
 }
-
-const StyledCaption = toggleStyledComponent(
-  cssModulesFlag,
-  Text,
-  styled(Text)`
-    color: var(--fgColor-muted);
-    display: block;
-    font-size: var(--text-body-size-small);
-
-    &:where([data-control-disabled]) {
-      color: var(--control-fgColor-disabled);
-    }
-
-    ${sx}
-  `,
-)
 
 export {FormControlCaption}
--- packages/react/src/internal/components/InputValidation.tsx
+++ packages/react/src/internal/components/InputValidation.tsx
@@ -1,14 +1,9 @@
 import type {IconProps} from '@primer/octicons-react'
 import {AlertFillIcon, CheckCircleFillIcon} from '@primer/octicons-react'
-import {clsx} from 'clsx'
 import React from 'react'
-import styled from 'styled-components'
 import Text from '../../Text'
-import sx from '../../sx'
 import type {SxProp} from '../../sx'
-import {cssModulesFlag} from '../../FormControl/feature-flags'
 import type {FormValidationStatus} from '../../utils/types/FormValidationStatus'
-import {useFeatureFlag} from '../../FeatureFlags'
 import classes from './InputValidation.module.css'
 
 type Props = {
@@ -25,7 +20,6 @@ const validationIconMap: Record<
 }
 
 const InputValidation: React.FC<React.PropsWithChildren<Props>> = ({children, id, validationStatus, sx}) => {
-  const enabled = useFeatureFlag(cssModulesFlag)
   const IconComponent = validationStatus ? validationIconMap[validationStatus] : undefined
 
   // TODO: use `text-caption-lineHeight` token as a custom property when it's available
@@ -35,19 +29,11 @@ const InputValidation: React.FC<React.PropsWithChildren<Props>> = ({children, id
   const iconBoxMinHeight = iconSize * captionLineHeight
 
   return (
-    <StyledInputValidation
-      className={clsx({
-        [classes.InputValidation]: enabled,
-      })}
-      data-validation-status={validationStatus}
-      sx={sx}
-    >
+    <Text className={classes.InputValidation} data-validation-status={validationStatus} sx={sx}>
       {IconComponent ? (
-        <StyledValidationIcon
+        <span
           aria-hidden="true"
-          className={clsx({
-            [classes.ValidationIcon]: enabled,
-          })}
+          className={classes.ValidationIcon}
           style={
             {
               '--inputValidation-iconSize': iconBoxMinHeight,
@@ -55,52 +41,17 @@ const InputValidation: React.FC<React.PropsWithChildren<Props>> = ({children, id
           }
         >
           <IconComponent size={iconSize} fill="currentColor" />
-        </StyledValidationIcon>
+        </span>
       ) : null}
-      <StyledValidationText
+      <span
         id={id}
-        className={clsx({
-          [classes.ValidationText]: enabled,
-        })}
+        className={classes.ValidationText}
         style={{'--inputValidation-lineHeight': captionLineHeight} as React.CSSProperties}
       >
         {children}
-      </StyledValidationText>
-    </StyledInputValidation>
+      </span>
+    </Text>
   )
 }
-
-const StyledInputValidation = styled(Text)`
-  color: var(--inputValidation-fgColor);
-  display: flex;
-  font-size: var(--text-body-size-small);
-  font-weight: 600;
-
-  & :where(a) {
-    color: currentColor;
-    text-dectoration: underline;
-  }
-
-  &:where([data-validation-status='success']) {
-    --inputValidation-fgColor: var(--fgColor-success);
-  }
-
-  &:where([data-validation-status='error']) {
-    --inputValidation-fgColor: var(--fgColor-danger);
-  }
-
-  ${sx}
-`
-
-const StyledValidationIcon = styled.span`
-  align-items: center;
-  display: flex;
-  margin-inline-end: var(--base-size-4);
-  min-height: var(--inputValidation-iconSize);
-`
-
-const StyledValidationText = styled.span`
-  line-height: var(--inputValidation-lineHeight);
-`
 
 export default InputValidation
