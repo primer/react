6e3c5bfff055f187b62eaf1105c903b7876f8dee
hectahertz <hectahertz@gmail.com>
2025-04-03 15:20:35 +0200

    Clean up showCancelSaveButtons logic

--- packages/react/src/SelectPanel/SelectPanel.tsx
+++ packages/react/src/SelectPanel/SelectPanel.tsx
@@ -413,6 +413,10 @@ export function SelectPanel({
     }
   }
 
+  // We add a save and cancel button on narrow screens when SelectPanel is full-screen
+  // Save and Cancel buttons are only useful for multiple selection, single selection instantly closes the panel
+  const showCancelSaveButtons = isMultiSelectVariant(selected) && usingFullScreenOnNarrow
+
   return (
     <>
       <AnchoredOverlay
@@ -557,10 +561,8 @@ export function SelectPanel({
             >
               {footer}
             </Box>
-          ) : isMultiSelectVariant(selected) && usingFullScreenOnNarrow ? (
-            /* Save and Cancel buttons are only useful for multiple selection, single selection instantly closes the panel */
+          ) : showCancelSaveButtons ? (
             <div className={clsx(classes.Footer, classes.ResponsiveFooter)}>
-              {/* we add a save and cancel button on narrow screens when SelectPanel is full-screen */}
               {onCancel && (
                 <Button
                   size="medium"
