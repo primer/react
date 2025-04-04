869305a7f683cdb39eebbdfc8c60308affb51341
Marie Lucca <40550942+francinelucca@users.noreply.github.com>
2025-04-04 12:55:54 -0400

    Revert "chore(AvatarStack): Remove the CSS modules feature flag from the Avat…"
    This reverts commit 5525ed20592a7c03e8ea1406f73ec86a7d6f11e9.

--- packages/react/src/internal/utils/toggleSxComponent.tsx
+++ packages/react/src/internal/utils/toggleSxComponent.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Box from '../../Box'
 import {defaultSxProp} from '../../utils/defaultSxProp'
-import type {BetterSystemStyleObject} from '../../sx'
 
 type CSSModulesProps = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   as?: string | React.ComponentType<any>
-  sx?: React.CSSProperties | BetterSystemStyleObject
-} & React.HTMLAttributes<HTMLElement>
+  sx?: React.CSSProperties
+}
 
 /**
  * Utility to toggle rendering a Box component that receives sx props
