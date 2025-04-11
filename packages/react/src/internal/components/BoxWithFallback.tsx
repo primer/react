869a737f9aef54492127e797f929a825c747f622
Josh Black <joshblack@github.com>
2025-04-11 12:38:41 -0500

    feat: add support for system props detection

--- packages/react/src/internal/components/BoxWithFallback.tsx
+++ packages/react/src/internal/components/BoxWithFallback.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Box from '../../Box'
 import {defaultSxProp} from '../../utils/defaultSxProp'
 import type {StyledComponent} from 'styled-components'
+import {includesSystemProps} from '../../utils/includeSystemProps'
 
 const BoxWithFallback = React.forwardRef(function BoxWithFallback(
   {as: BaseComponent = 'div', sx = defaultSxProp, ...rest},
   ref,
 ) {
-  if (sx !== defaultSxProp) {
+  if (sx !== defaultSxProp || includesSystemProps(rest)) {
     return <Box {...rest} ref={ref} as={BaseComponent} sx={sx} />
   }
   return <BaseComponent {...rest} ref={ref} />
