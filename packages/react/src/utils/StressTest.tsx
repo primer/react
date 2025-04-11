4b56f2cb2c5b67194628c77e544f1d996c854faf
hectahertz <hectahertz@gmail.com>
2025-04-11 19:46:18 +0200

    Styling

--- packages/react/src/utils/StressTest.tsx
+++ packages/react/src/utils/StressTest.tsx
@@ -3,6 +3,7 @@ import afterFrame from 'afterframe'
 
 import {Button} from '../Button'
 import Text from '../Text'
+import classes from './StressTest.module.css'
 
 export interface StressTestProps {
   renderIteration: (count: number, totalIterations: number) => React.ReactNode
@@ -11,7 +12,7 @@ export interface StressTestProps {
 export const StressTest: React.FC<StressTestProps> = ({renderIteration}) => {
   const [count, setCount] = useState(1)
   const [result, setResult] = useState<undefined | number>(undefined)
-  const totalIterations = 100
+  const totalIterations = 300
 
   // Initialize the observer to log performance metrics, stored in a ref and initialized only once
   const observer = React.useRef<{observer: PerformanceObserver; data: number[]} | null>(null)
@@ -71,25 +72,30 @@ export const StressTest: React.FC<StressTestProps> = ({renderIteration}) => {
   }
 
   return (
-    <>
-      {renderIteration(count, totalIterations)}
+    <div className={classes.Root}>
+      <div className={classes.Container}>{renderIteration(count, totalIterations)}</div>
       <Button
+        className={classes.Button}
         variant="primary"
         disabled={count !== 1 && count !== totalIterations}
         onClick={onClick}
         size="large"
-        block
         data-testid={testState}
       >
         {getButtonLabel(testState)}
       </Button>
-      {result !== undefined && (
-        <Text as="em">
-          Median duration: <span data-testid="result">{result.toFixed(5)}</span>
-          ms
-        </Text>
-      )}
-    </>
+      <Text className={classes.Result} as="em">
+        {result !== undefined ? (
+          <>
+            {'Median duration: '}
+            <span data-testid="result">{result.toFixed(2)}</span>
+            ms
+          </>
+        ) : (
+          <span>...</span>
+        )}
+      </Text>
+    </div>
   )
 }
 
