37204fd8461bb2f69d17215f009f6f0050f977ca
Josh Black <joshblack@github.com>
2025-04-11 12:06:00 -0500

    fix: add ref support to BoxWithFallback

--- packages/react/src/internal/components/BoxWithFallback.tsx
+++ packages/react/src/internal/components/BoxWithFallback.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Box from '../../Box'
 import {defaultSxProp} from '../../utils/defaultSxProp'
+import type {StyledComponent} from 'styled-components'
 
-type BoxWithFallbackProps = React.ComponentPropsWithoutRef<typeof Box>
-
-function BoxWithFallback({as: BaseComponent = 'div', sx = defaultSxProp, ...rest}: BoxWithFallbackProps) {
+const BoxWithFallback = React.forwardRef(function BoxWithFallback(
+  {as: BaseComponent = 'div', sx = defaultSxProp, ...rest},
+  ref,
+) {
   if (sx !== defaultSxProp) {
-    return <Box {...rest} as={BaseComponent} sx={sx} />
+    return <Box {...rest} ref={ref} as={BaseComponent} sx={sx} />
   }
-  return <BaseComponent {...rest} />
-}
+  return <BaseComponent {...rest} ref={ref} />
+}) as StyledComponent<'div', any, React.ComponentPropsWithoutRef<typeof Box>, never>
 
 export {BoxWithFallback}
-export type {BoxWithFallbackProps}
