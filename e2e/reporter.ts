96e24d0046adfc265b009e4a6faa8d747a9b3fe2
hectahertz <hectahertz@gmail.com>
2025-04-12 21:52:52 +0200

    Accumulate all results

--- e2e/reporter.ts
+++ e2e/reporter.ts
@@ -2,6 +2,10 @@ import type {FullConfig, FullResult, Reporter, Suite, TestCase, TestResult} from
 import {writeFileSync} from 'fs'
 
 class MyReporter implements Reporter {
+  // Format we need:
+  // https://github.com/benchmark-action/github-action-benchmark?tab=readme-ov-file#examples
+  results: {name: string; unit: string; value: number}[] = []
+
   onBegin(_config: FullConfig, suite: Suite) {
     console.log(`✨ Starting the run with ${suite.allTests().length} tests`)
   }
@@ -17,31 +21,23 @@ class MyReporter implements Reporter {
       // get the content of the attachment to an object
       if (attachment.body !== undefined && attachment.contentType === 'application/json') {
         const content = JSON.parse(attachment.body.toString())
-        console.log(`✨ Attachment content: ${JSON.stringify(content, null, 2)}`)
-        // ✨ Attachment content: {
-        // "id": "stresstests-components-actionlist--single-select",
-        // "duration": "31.50"
-        // }
-
-        // Save to a file compatible with the format we need
-        // https://github.com/benchmark-action/github-action-benchmark?tab=readme-ov-file#examples
-
-        const fileName = 'results.json'
-        const fileContent = {
+        console.log(`✨ Attachment content: ${content}`)
+        this.results.push({
           name: content.id,
           unit: 'ms',
           value: parseFloat(content.duration),
-        }
-        const fileContentString = JSON.stringify(fileContent, null, 2)
-        console.log(`✨ File content: ${fileContentString}`)
-        writeFileSync(fileName, fileContentString)
-        console.log(`✨ File saved: ${fileName}`)
+        })
       }
     }
   }
 
   onEnd(result: FullResult) {
     console.log(`✨ Finished the run: ${result.status}`)
+    const fileName = 'results.json'
+    const fileContentString = JSON.stringify(this.results, null, 2)
+    console.log(`✨ File content: ${fileContentString}`)
+    writeFileSync(fileName, fileContentString)
+    console.log(`✨ File saved: ${fileName}`)
   }
 }
 
