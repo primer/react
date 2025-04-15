73b81e844caea119c8c8ae993df3262bbcbfd93d
Josh Black <joshblack@github.com>
2025-04-15 11:21:06 -0500

    refactor: add BoxWithFallback component to use with sx (#5919)

--- packages/react/src/BranchName/BranchName.tsx
+++ packages/react/src/BranchName/BranchName.tsx
@@ -2,7 +2,7 @@ import React, {type ForwardedRef} from 'react'
 import {clsx} from 'clsx'
 import type {SxProp} from '../sx'
 import classes from './BranchName.module.css'
-import {toggleSxComponent} from '../internal/utils/toggleSxComponent'
+import {BoxWithFallback} from '../internal/components/BoxWithFallback'
 
 type BranchNameProps<As extends React.ElementType> = {
   as?: As
@@ -10,17 +10,13 @@ type BranchNameProps<As extends React.ElementType> = {
   Omit<SxProp, 'sx'> &
   SxProp
 
-const BaseComponent = toggleSxComponent('div') as React.ComponentType<
-  React.PropsWithChildren<BranchNameProps<React.ElementType> & React.RefAttributes<HTMLElement>>
->
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function BranchName<As extends React.ElementType>(props: BranchNameProps<As>, ref: ForwardedRef<any>) {
   const {as: Component = 'a', className, children, ...rest} = props
   return (
-    <BaseComponent as={Component} {...rest} ref={ref} className={clsx(className, classes.BranchName)}>
+    <BoxWithFallback as={Component} {...rest} ref={ref} className={clsx(className, classes.BranchName)}>
       {children}
-    </BaseComponent>
+    </BoxWithFallback>
   )
 }
 
