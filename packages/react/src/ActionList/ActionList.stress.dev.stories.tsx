294dac335a955b22f73c1427eef183bf4672a01b
Hector Garcia <hectahertz@github.com>
2025-04-22 19:52:58 +0200

    Add the framework required to run component stress tests (#5929)
    Co-authored-by: Jon Rohan <yes@jonrohan.codes>

---
+++ packages/react/src/ActionList/ActionList.stress.dev.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import type {Meta} from '@storybook/react'
+import type {ComponentProps} from '../utils/types'
+import {StressTest} from '../utils/StressTest'
+import {TableIcon} from '@primer/octicons-react'
+import {ActionList} from '.'
+
+export default {
+  title: 'StressTests/Components/ActionList',
+  component: ActionList,
+} as Meta<ComponentProps<typeof ActionList>>
+
+const totalIterations = 100
+
+const projects = Array.from({length: totalIterations}, (_, i) => ({
+  name: `Project ${i + 1}`,
+  scope: `Scope ${i + 1}`,
+}))
+
+export const SingleSelect = () => {
+  return (
+    <StressTest
+      componentName="ActionList"
+      title="Single Select"
+      description="Selecting a single item from a large list."
+      totalIterations={totalIterations}
+      renderIteration={count => {
+        return (
+          <>
+            <ActionList selectionVariant="single" showDividers role="menu" aria-label="Project">
+              {projects.map((project, index) => (
+                <ActionList.Item
+                  key={index}
+                  role="menuitemradio"
+                  selected={index === count}
+                  aria-checked={index === count}
+                >
+                  <ActionList.LeadingVisual>
+                    <TableIcon />
+                  </ActionList.LeadingVisual>
+                  {project.name}
+                  <ActionList.Description variant="block">{project.scope}</ActionList.Description>
+                </ActionList.Item>
+              ))}
+            </ActionList>
+          </>
+        )
+      }}
+    />
+  )
+}
---
+++ packages/react/src/Button/Button.stress.dev.stories.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import type {Meta} from '@storybook/react'
+import type {ComponentProps} from '../utils/types'
+import {StressTest} from '../utils/StressTest'
+import {Button} from '.'
+
+export default {
+  title: 'StressTests/Components/Button',
+  component: Button,
+} as Meta<ComponentProps<typeof Button>>
+
+const totalIterations = 500
+
+export const LabelUpdate = () => {
+  return (
+    <StressTest
+      componentName="Button"
+      title="Label update"
+      description="Update the label a large number of times."
+      totalIterations={totalIterations}
+      renderIteration={count => (
+        <div>
+          <Button variant="primary" size="large" onClick={() => {}}>
+            {`Button ${count + 1}`}
+          </Button>
+          <Button variant="default" size="medium" onClick={() => {}}>
+            {`Button ${count + 1}`}
+          </Button>
+          <Button variant="danger" size="small" onClick={() => {}}>
+            {`Button ${count + 1}`}
+          </Button>
+        </div>
+      )}
+    />
+  )
+}
+
+export const CountUpdate = () => {
+  return (
+    <StressTest
+      componentName="Button"
+      title="Count update"
+      description="Update the count a large number of times."
+      totalIterations={totalIterations}
+      renderIteration={count => (
+        <div>
+          <Button variant="primary" size="large" onClick={() => {}} count={count}>
+            Button
+          </Button>
+          <Button variant="default" size="medium" onClick={() => {}} count={count}>
+            Button
+          </Button>
+          <Button variant="danger" size="small" onClick={() => {}} count={count}>
+            Button
+          </Button>
+        </div>
+      )}
+    />
+  )
+}
