166f07f10cbd001a5ea692ac423f51fccec954d3
Katie Langerman <18661030+langermank@users.noreply.github.com>
2025-04-23 12:08:50 -0700

    add stories

--- packages/react/src/Breadcrumbs/Breadcrumbs.stories.tsx
+++ packages/react/src/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -29,3 +29,58 @@ export const Default = () => (
     </Breadcrumbs.Item>
   </Breadcrumbs>
 )
+
+// Breadcrumb with 6 items
+export const SixItems = () => (
+  <Breadcrumbs>
+    <Breadcrumbs.Item href="#">First</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Second</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Third</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Fourth</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Fifth</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#" selected>
+      Last
+    </Breadcrumbs.Item>
+  </Breadcrumbs>
+)
+
+// Breadcrumb with 8 items
+export const EightItems = () => (
+  <Breadcrumbs>
+    <Breadcrumbs.Item href="#">First</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Second</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Third</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Fourth</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Fifth</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Sixth</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#">Seventh</Breadcrumbs.Item>
+    <Breadcrumbs.Item href="#" selected>
+      Last
+    </Breadcrumbs.Item>
+  </Breadcrumbs>
+)
+
+// Breadcrumb with 3 items in a div that has a max width of 200px
+export const ThreeItems = () => (
+  <div style={{maxWidth: '200px'}}>
+    <Breadcrumbs>
+      <Breadcrumbs.Item href="#">First</Breadcrumbs.Item>
+      <Breadcrumbs.Item href="#">Second</Breadcrumbs.Item>
+      <Breadcrumbs.Item href="#" selected>
+        Last
+      </Breadcrumbs.Item>
+    </Breadcrumbs>
+  </div>
+)
+
+// Breadcrumb with 2 items in a div that has a max width of 200px
+export const TwoItems = () => (
+  <div style={{maxWidth: '200px'}}>
+    <Breadcrumbs>
+      <Breadcrumbs.Item href="#">First</Breadcrumbs.Item>
+      <Breadcrumbs.Item href="#" selected>
+        Last
+      </Breadcrumbs.Item>
+    </Breadcrumbs>
+  </div>
+)
