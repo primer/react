11b1f3adaf2c6118cd67c68297cc72a446d5c0f1
Jon Rohan <yes@jonrohan.codes>
2025-04-24 19:45:14 -0700

    chore(Dialog): Remove the CSS modules feature flag from the Dialog component (#5969)

--- packages/react/src/DialogV1/Dialog.tsx
+++ packages/react/src/DialogV1/Dialog.tsx
@@ -1,21 +1,14 @@
-import React, {forwardRef, useRef} from 'react'
-import styled from 'styled-components'
+import React, {forwardRef, useRef, type HTMLAttributes} from 'react'
 import {IconButton} from '../Button'
-import {get} from '../constants'
 import Box from '../Box'
 import useDialog from '../hooks/useDialog'
 import type {SxProp} from '../sx'
-import sx from '../sx'
-import Text from '../Text'
 import type {ComponentProps} from '../utils/types'
 import {useRefObjectAsForwardedRef} from '../hooks/useRefObjectAsForwardedRef'
 import {XIcon} from '@primer/octicons-react'
-import {toggleStyledComponent} from '../internal/utils/toggleStyledComponent'
-import {useFeatureFlag} from '../FeatureFlags'
 import {clsx} from 'clsx'
 import classes from './Dialog.module.css'
-
-const CSS_MODULES_FEATURE_FLAG = 'primer_react_css_modules_ga'
+import {BoxWithFallback} from '../internal/components/BoxWithFallback'
 
 // Dialog v1
 const noop = () => null
@@ -25,102 +18,27 @@ type StyledDialogBaseProps = {
   wide?: boolean
 } & SxProp
 
-const DialogBase = toggleStyledComponent(
-  CSS_MODULES_FEATURE_FLAG,
-  'div',
-  styled.div<StyledDialogBaseProps>`
-    box-shadow: ${get('shadows.shadow.large')};
-    border-radius: ${get('radii.2')};
-    position: fixed;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    max-height: 80vh;
-    z-index: 999;
-    margin: 10vh auto;
-    background-color: ${get('colors.canvas.default')};
-    width: ${props => (props.narrow ? '320px' : props.wide ? '640px' : '440px')};
-    outline: none;
+export type DialogHeaderProps = React.PropsWithChildren<HTMLAttributes<HTMLDivElement>> & SxProp
 
-    @media screen and (max-width: 750px) {
-      width: 100dvw;
-      margin: 0;
-      border-radius: 0;
-      height: 100dvh;
-    }
-
-    ${sx};
-  `,
-)
-
-const DialogHeaderBase = toggleStyledComponent(
-  CSS_MODULES_FEATURE_FLAG,
-  'div',
-  styled(Box)<SxProp>`
-    border-radius: ${get('radii.2')} ${get('radii.2')} 0px 0px;
-    border-bottom: 1px solid ${get('colors.border.default')};
-    display: flex;
-
-    @media screen and (max-width: 750px) {
-      border-radius: 0px;
-    }
-
-    ${sx};
-  `,
-)
-
-export type DialogHeaderProps = ComponentProps<typeof DialogHeaderBase>
-
-function DialogHeader({theme, children, backgroundColor = 'canvas.subtle', ...rest}: DialogHeaderProps) {
+function DialogHeader({children, className, ...rest}: DialogHeaderProps) {
   if (React.Children.toArray(children).every(ch => typeof ch === 'string')) {
-    children = (
-      <Text fontSize={1} fontWeight="bold">
-        {children}
-      </Text>
-    )
+    children = <span className={classes.HeaderChild}>{children}</span>
   }
 
-  const enabled = useFeatureFlag(CSS_MODULES_FEATURE_FLAG)
-
   return (
-    <DialogHeaderBase
-      theme={theme}
-      p={3}
-      backgroundColor={backgroundColor}
-      {...rest}
-      className={enabled ? classes.Header : undefined}
-    >
+    <BoxWithFallback as="div" {...rest} className={clsx(classes.Header, className)}>
       {children}
-    </DialogHeaderBase>
+    </BoxWithFallback>
   )
 }
-
-const Overlay = toggleStyledComponent(
-  CSS_MODULES_FEATURE_FLAG,
-  'span',
-  styled.span`
-    &:before {
-      position: fixed;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      display: block;
-      cursor: default;
-      content: ' ';
-      background: transparent;
-      z-index: 99;
-      background: ${get('colors.primer.canvas.backdrop')};
-    }
-  `,
-)
 
 type InternalDialogProps = {
   isOpen?: boolean
   onDismiss?: () => void
   initialFocusRef?: React.RefObject<HTMLElement>
   returnFocusRef?: React.RefObject<HTMLElement>
-} & ComponentProps<typeof DialogBase>
+} & StyledDialogBaseProps &
+  HTMLAttributes<HTMLDivElement>
 
 const Dialog = forwardRef<HTMLDivElement, InternalDialogProps>(
   ({children, onDismiss = noop, isOpen, initialFocusRef, returnFocusRef, className, ...props}, forwardedRef) => {
@@ -146,23 +64,18 @@ const Dialog = forwardRef<HTMLDivElement, InternalDialogProps>(
       overlayRef,
     })
 
-    const enabled = useFeatureFlag(CSS_MODULES_FEATURE_FLAG)
-
-    const iconStyles = enabled
-      ? {className: classes.CloseIcon}
-      : {sx: {position: 'absolute', top: '8px', right: '16px'}}
-
     return isOpen ? (
       <>
-        <Overlay className={enabled ? classes.Overlay : undefined} ref={overlayRef} />
-        <DialogBase
+        <BoxWithFallback as="span" className={classes.Overlay} ref={overlayRef} />
+        <BoxWithFallback
+          as="div"
           tabIndex={-1}
           ref={modalRef}
           role="dialog"
           aria-modal="true"
           {...props}
           {...getDialogProps()}
-          className={clsx({[classes.Dialog]: enabled}, className)}
+          className={clsx(classes.Dialog, className)}
           data-width={props.wide ? 'wide' : props.narrow ? 'narrow' : 'default'}
         >
           <IconButton
@@ -171,10 +84,10 @@ const Dialog = forwardRef<HTMLDivElement, InternalDialogProps>(
             onClick={onCloseClick}
             aria-label="Close"
             variant="invisible"
-            {...iconStyles}
+            className={classes.CloseIcon}
           />
           {children}
-        </DialogBase>
+        </BoxWithFallback>
       </>
     ) : null
   },
