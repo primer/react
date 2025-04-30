5a28459c6a1c805419dcb4eae9033285ad3ec943
Marie Lucca <francinelucca@github.com>
2025-04-30 14:24:57 -0400

    fix(SelectPanel): do not depend on id for selection comparison

--- packages/react/src/SelectPanel/SelectPanel.tsx
+++ packages/react/src/SelectPanel/SelectPanel.tsx
@@ -391,11 +391,11 @@ function Panel({
       // For single-select modal, there is an intermediate state when the user has selected
       // an item but has not yet saved the selection. We need to check for this state.
       if (isSingleSelectModal) {
-        return intermediateSelected?.id === item.id
+        return intermediateSelected === item
       }
 
       // For single-select anchored, we just need to check if the item is the selected item
-      return selected?.id === item.id
+      return selected === item
     },
     [selected, intermediateSelected, isSingleSelectModal],
   )
