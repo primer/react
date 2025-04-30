1766e556610d0a574a07d010818347b78a1b7409
Jon Rohan <yes@jonrohan.codes>
2025-04-30 06:50:56 -0700

    chore(BaseStyles): Remove the CSS modules feature flag from the BaseStyles component (#5981)
    Co-authored-by: jonrohan <54012+jonrohan@users.noreply.github.com>
    Co-authored-by: Josh Black <joshblack@github.com>

--- packages/react/src/BaseStyles.dev.stories.tsx
+++ packages/react/src/BaseStyles.dev.stories.tsx
@@ -1,6 +1,7 @@
 import {BaseStyles} from '.'
 import type {Meta} from '@storybook/react'
 import type {ComponentProps} from './utils/types'
+import React from 'react'
 
 export default {
   title: 'Behaviors/BaseStyles/Dev',
@@ -8,3 +9,35 @@ export default {
 } as Meta<ComponentProps<typeof BaseStyles>>
 
 export const Default = () => 'Hello'
+
+export const WithSxProps = () => (
+  <BaseStyles
+    sx={{
+      color: 'red',
+      backgroundColor: 'blue',
+      fontFamily: 'Arial',
+      lineHeight: '1.5',
+    }}
+  >
+    Hello
+  </BaseStyles>
+)
+
+export const WithSystemProps = () => (
+  <BaseStyles color="red" backgroundColor="blue" fontFamily="Arial" fontSize="14px" lineHeight="1.5" display="flex">
+    Hello
+  </BaseStyles>
+)
+
+export const WithStyleProps = () => (
+  <BaseStyles
+    style={{
+      color: 'red',
+      backgroundColor: 'blue',
+      fontFamily: 'Arial',
+      lineHeight: '1.5',
+    }}
+  >
+    Hello
+  </BaseStyles>
+)
