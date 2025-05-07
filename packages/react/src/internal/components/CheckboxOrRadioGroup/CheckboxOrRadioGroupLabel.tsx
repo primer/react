0fce0238e3af5a12b55e7899980786230b540eaf
Jon Rohan <yes@jonrohan.codes>
2025-05-06 19:37:11 -0700

    chore(CheckboxOrRadioGroup): Remove the CSS modules feature flag from the CheckboxOrRadioGroup component (#6013)

--- packages/react/src/internal/components/CheckboxOrRadioGroup/CheckboxOrRadioGroupLabel.tsx
+++ packages/react/src/internal/components/CheckboxOrRadioGroup/CheckboxOrRadioGroupLabel.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import Box from '../../../Box'
 import VisuallyHidden from '../../../_VisuallyHidden'
 import type {SxProp} from '../../../sx'
 import CheckboxOrRadioGroupContext from './CheckboxOrRadioGroupContext'
-import {CSS_MODULES_FLAG} from './FeatureFlag'
-import {useFeatureFlag} from '../../../FeatureFlags'
 import classes from './CheckboxOrRadioGroup.module.css'
 import {Stack} from '../../../Stack'
 import {clsx} from 'clsx'
@@ -25,67 +22,20 @@ const CheckboxOrRadioGroupLabel: React.FC<React.PropsWithChildren<CheckboxOrRadi
   sx,
 }) => {
   const {required, disabled} = React.useContext(CheckboxOrRadioGroupContext)
-  const enabled = useFeatureFlag(CSS_MODULES_FLAG)
-
-  if (enabled) {
-    if (sx) {
-      return (
-        <VisuallyHidden
-          className={clsx(className, classes.RadioGroupLabel)}
-          isVisible={!visuallyHidden}
-          title={required ? 'required field' : undefined}
-          data-label-disabled={disabled ? '' : undefined}
-          sx={sx}
-        >
-          {required ? (
-            <Stack direction="horizontal" gap="none">
-              <div className={classes.GroupLabelChildren}>{children}</div>
-              <span>*</span>
-            </Stack>
-          ) : (
-            children
-          )}
-        </VisuallyHidden>
-      )
-    }
-
-    return (
-      <VisuallyHidden
-        className={clsx(className, classes.RadioGroupLabel)}
-        isVisible={!visuallyHidden}
-        title={required ? 'required field' : undefined}
-        data-label-disabled={disabled ? '' : undefined}
-      >
-        {required ? (
-          <Stack direction="horizontal" gap="none">
-            <div className={classes.GroupLabelChildren}>{children}</div>
-            <span>*</span>
-          </Stack>
-        ) : (
-          children
-        )}
-      </VisuallyHidden>
-    )
-  }
 
   return (
     <VisuallyHidden
-      className={className}
+      className={clsx(className, classes.RadioGroupLabel)}
       isVisible={!visuallyHidden}
       title={required ? 'required field' : undefined}
-      sx={{
-        display: 'block',
-        color: disabled ? 'fg.muted' : undefined,
-        fontSize: 1,
-        fontWeight: 'bold',
-        ...sx,
-      }}
+      data-label-disabled={disabled ? '' : undefined}
+      sx={sx}
     >
       {required ? (
-        <Box display="flex" as="span">
-          <Box mr={1}>{children}</Box>
+        <Stack direction="horizontal" gap="none">
+          <div className={classes.GroupLabelChildren}>{children}</div>
           <span>*</span>
-        </Box>
+        </Stack>
       ) : (
         children
       )}
