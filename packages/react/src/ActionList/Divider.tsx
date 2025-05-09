53a11407b08ecd7962da2832eacf1289719e0fc1
Jon Rohan <yes@jonrohan.codes>
2025-05-09 08:27:19 -0700

    chore(ActionList): Remove the CSS modules feature flag from the ActionList.Divider component (#6022)
    Co-authored-by: primer[bot] <119360173+primer[bot]@users.noreply.github.com>

--- packages/react/src/ActionList/Divider.tsx
+++ packages/react/src/ActionList/Divider.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import Box from '../Box'
-import {get} from '../constants'
-import type {Theme} from '../ThemeProvider'
 import type {SxProp} from '../sx'
-import {merge} from '../sx'
 import {clsx} from 'clsx'
-import {useFeatureFlag} from '../FeatureFlags'
 import classes from './ActionList.module.css'
 import {defaultSxProp} from '../utils/defaultSxProp'
-import {actionListCssModulesFlag} from './featureflag'
+import {BoxWithFallback} from '../internal/components/BoxWithFallback'
 
 export type ActionListDividerProps = SxProp & {
   className?: string
@@ -18,38 +13,13 @@ export type ActionListDividerProps = SxProp & {
  * Visually separates `Item`s or `Group`s in an `ActionList`.
  */
 export const Divider: React.FC<React.PropsWithChildren<ActionListDividerProps>> = ({sx = defaultSxProp, className}) => {
-  const enabled = useFeatureFlag(actionListCssModulesFlag)
-  if (enabled) {
-    if (sx !== defaultSxProp) {
-      return (
-        <Box
-          className={clsx(className, classes.Divider)}
-          as="li"
-          aria-hidden="true"
-          sx={sx}
-          data-component="ActionList.Divider"
-        />
-      )
-    }
-    return <li className={clsx(className, classes.Divider)} aria-hidden="true" data-component="ActionList.Divider" />
-  }
   return (
-    <Box
+    <BoxWithFallback
+      className={clsx(className, classes.Divider)}
       as="li"
       aria-hidden="true"
-      sx={merge(
-        {
-          height: 1,
-          backgroundColor: 'actionListItem.inlineDivider',
-          marginTop: (theme: Theme) => `calc(${get('space.2')(theme)} - 1px)`,
-          marginBottom: 2,
-          listStyle: 'none', // hide the ::marker inserted by browser's stylesheet
-        },
-        sx as SxProp,
-      )}
-      className={className}
+      sx={sx}
       data-component="ActionList.Divider"
     />
   )
 }
-Divider.displayName = 'ActionList.Divider'
