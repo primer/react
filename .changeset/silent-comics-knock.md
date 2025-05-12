107ebc41cd4f7feb8eb021254395f5c624ab9599
Josh Black <joshblack@github.com>
2025-05-12 10:09:42 -0500

    fix(Table): update TableContainer to place `className` on outermost element (#6032)

---
+++ .changeset/silent-comics-knock.md
@@ -0,0 +1,5 @@
+---
+"@primer/react": patch
+---
+
+Update the `Table.Container` component to place `className` on outermost element
--- packages/react/src/DataTable/__tests__/Table.test.tsx
+++ packages/react/src/DataTable/__tests__/Table.test.tsx
@@ -148,6 +148,16 @@ describe('Table', () => {
   })
 
   describe('Table.Container', () => {
+    it('should support additional props on the outermost element', () => {
+      const {container} = render(<Table.Container data-testid="test" />)
+      expect(container.firstElementChild).toHaveAttribute('data-testid', 'test')
+    })
+
+    it('should support a custom `className` through the `className` prop', () => {
+      const {container} = render(<Table.Container className="test" />)
+      expect(container.firstElementChild).toHaveClass('test')
+    })
+
     it('should support custom styles through the `sx` prop', () => {
       const {container} = render(<Table.Container sx={{m: 0}} />)
       expect(container.firstElementChild).toHaveStyle('margin:0')
