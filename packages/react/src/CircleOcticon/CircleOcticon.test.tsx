d9517ff3e8a2951d39fa8538faa2db2a4ba42d3e
Josh Black <joshblack@github.com>
2025-05-12 15:59:52 -0500

    test: fix update in test file

--- packages/react/src/CircleOcticon/CircleOcticon.test.tsx
+++ packages/react/src/CircleOcticon/CircleOcticon.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {CheckIcon} from '@primer/octicons-react'
-import {CircleOcticon} from '..'
+import CircleOcticon from '../CircleOcticon'
 import {render as HTMLRender} from '@testing-library/react'
 import {describe, expect, it} from 'vitest'
 
@@ -11,13 +11,6 @@ describe('CircleOcticon', () => {
     const computedStyle = window.getComputedStyle(element)
     expect(computedStyle.getPropertyValue('width')).toBe('10px')
     expect(computedStyle.getPropertyValue('height')).toBe('10px')
-  })
-
-  it('renders with border-radius: 50%', () => {
-    const {container} = HTMLRender(<CircleOcticon icon={CheckIcon} />)
-    const element = container.firstChild as HTMLElement
-    const computedStyle = window.getComputedStyle(element)
-    expect(computedStyle.getPropertyValue('border-radius')).toBe('50%')
   })
 
   it('has a default size of 32px', () => {
