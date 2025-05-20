815d3d52c8da97c84030088654e1e790f52c49d2
Jon Rohan <yes@jonrohan.codes>
2025-05-20 18:28:29 +0000

    Type fix

--- packages/react/src/_VisuallyHidden.tsx
+++ packages/react/src/_VisuallyHidden.tsx
@@ -1,14 +1,19 @@
 import classes from './_VisuallyHidden.module.css'
 import {clsx} from 'clsx'
 import {BoxWithFallback} from './internal/components/BoxWithFallback'
-import type { SxProp } from './sx'
+import type {SxProp} from './sx'
 
 interface Props {
   isVisible?: boolean
   as?: React.ElementType
-} & SxProp
+}
 
-function VisuallyHidden({isVisible, children, as = 'span', ...rest}: Props & React.HTMLAttributes<HTMLSpanElement>) {
+function VisuallyHidden({
+  isVisible,
+  children,
+  as = 'span',
+  ...rest
+}: Props & React.HTMLAttributes<HTMLSpanElement> & SxProp) {
   return (
     <BoxWithFallback as={as} className={clsx({[classes.InternalVisuallyHidden]: !isVisible})} {...rest}>
       {children}
