670f040154695c006fcc1fca18fc977a7cd39344
Jon Rohan <yes@jonrohan.codes>
2025-05-20 18:26:23 +0000

    Add sx prop

--- packages/react/src/_VisuallyHidden.tsx
+++ packages/react/src/_VisuallyHidden.tsx
@@ -1,11 +1,12 @@
 import classes from './_VisuallyHidden.module.css'
 import {clsx} from 'clsx'
 import {BoxWithFallback} from './internal/components/BoxWithFallback'
+import type { SxProp } from './sx'
 
 interface Props {
   isVisible?: boolean
   as?: React.ElementType
-}
+} & SxProp
 
 function VisuallyHidden({isVisible, children, as = 'span', ...rest}: Props & React.HTMLAttributes<HTMLSpanElement>) {
   return (
