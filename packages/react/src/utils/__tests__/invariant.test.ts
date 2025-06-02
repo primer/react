efc03c6ace40e3f5829990137ee24d1626358162
Josh Black <joshblack@github.com>
2025-06-02 14:03:37 -0500

    refactor: update live-region and utils tests from Jest to Vitest

--- packages/react/src/utils/__tests__/invariant.test.ts
+++ packages/react/src/utils/__tests__/invariant.test.ts
@@ -1,19 +1,22 @@
+import {describe, expect, it} from 'vitest'
 import {invariant} from '../invariant'
 
-test('throws an error when the condition is `false`', () => {
-  expect(() => {
-    invariant(false, 'test')
-  }).toThrow('test')
-})
+describe('invariant', () => {
+  it('throws an error when the condition is `false`', () => {
+    expect(() => {
+      invariant(false, 'test')
+    }).toThrow('test')
+  })
 
-test('does not throw an error when the condition is `true`', () => {
-  expect(() => {
-    invariant(true, 'test')
-  }).not.toThrow()
-})
+  it('does not throw an error when the condition is `true`', () => {
+    expect(() => {
+      invariant(true, 'test')
+    }).not.toThrow()
+  })
 
-test('formats arguments into error string', () => {
-  expect(() => {
-    invariant(false, 'test %s %s %s', 1, 2, 3)
-  }).toThrow('test 1 2 3')
+  it('formats arguments into error string', () => {
+    expect(() => {
+      invariant(false, 'test %s %s %s', 1, 2, 3)
+    }).toThrow('test 1 2 3')
+  })
 })
