f072ff672eb60f39946612fce6d1f469d8d3ac91
Josh Black <joshblack@github.com>
2025-06-06 11:12:04 -0500

    refactor: use rolldown for bundling

--- packages/react/rollup.config.mjs
+++ packages/react/rollup.config.mjs
@@ -1,5 +1,4 @@
 import path from 'node:path'
-import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
 import replace from '@rollup/plugin-replace'
@@ -100,9 +99,6 @@ const baseConfig = {
       ],
     }),
     resolve({
-      extensions,
-    }),
-    commonjs({
       extensions,
     }),
     importCSS({
