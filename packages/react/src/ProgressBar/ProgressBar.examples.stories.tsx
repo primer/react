a92a8848ea5584e33d7e9ab658a4e5c11720bcf2
Tyler Jones <tylerjdev@github.com>
2025-07-02 16:19:22 -0400

    Rely on `progress` prop instead

--- packages/react/src/ProgressBar/ProgressBar.examples.stories.tsx
+++ packages/react/src/ProgressBar/ProgressBar.examples.stories.tsx
@@ -19,7 +19,7 @@ export const WithVisibleTextValue = () => {
 export const MultipleSegments = () => {
   return (
     <>
-      <ProgressBar aria-valuenow={50}>
+      <ProgressBar progress={50}>
         <ProgressBar.Item
           progress={30}
           style={{backgroundColor: 'var(--bgColor-success-emphasis)'}}
