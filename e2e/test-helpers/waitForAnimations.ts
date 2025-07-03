f8a7d963875bbeae6b900fdf5e9cbd32e808ad0a
Adam Dierkens <adam@dierkens.com>
2025-07-03 00:12:04 -0400

    fix selector

--- e2e/test-helpers/waitForAnimations.ts
+++ e2e/test-helpers/waitForAnimations.ts
@@ -2,6 +2,6 @@ import type {Page} from '@playwright/test'
 
 export function waitForAllAnimations(page: Page) {
   return page
-    .locator(`storybook-root`)
+    .locator(`#storybook-root`)
     .evaluate(element => Promise.all(element.getAnimations({subtree: true}).map(animation => animation.finished)))
 }
