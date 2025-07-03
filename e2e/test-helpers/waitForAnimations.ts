22df1b879bb0a830d3b66cf63e0b2e9dcc7b9053
Adam Dierkens <adam@dierkens.com>
2025-07-03 01:06:04 -0400

    update delay

--- e2e/test-helpers/waitForAnimations.ts
+++ e2e/test-helpers/waitForAnimations.ts
@@ -1,9 +1,9 @@
 import type {Page} from '@playwright/test'
 
 export async function waitForAllAnimations(page: Page) {
-  await page
-    .locator(`body`)
-    .evaluate(element => Promise.all(element.getAnimations({subtree: true}).map(animation => animation.finished)))
+  // await page
+  //   .locator(`#storybook-root`)
+  //   .evaluate(element => Promise.all(element.getAnimations({subtree: true}).map(animation => animation.finished)))
 
   // eslint-disable-next-line playwright/no-wait-for-timeout
   await page.waitForTimeout(250)
