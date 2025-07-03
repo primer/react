f1d0489d5b62d62698074321305d2eed5ed979c4
Jon Rohan <yes@jonrohan.codes>
2025-07-03 09:28:33 -0700

    Update ButtonReset.tsx

--- packages/react/src/internal/components/ButtonReset.tsx
+++ packages/react/src/internal/components/ButtonReset.tsx
@@ -4,11 +4,7 @@ import {clsx} from 'clsx'
 /**
  * Provides an unstyled button that can be styled as-needed for components
  */
-export const Button = ({
-  children,
-  className,
-  ...rest
-}: React.ComponentPropsWithoutRef<'button'>) => {
+export const Button = ({ children, className, ...rest }: React.ComponentPropsWithoutRef<'button'>) => {
   return (
     <button className={clsx(className, classes.ButtonReset)} type="button" {...rest}>
       {children}
