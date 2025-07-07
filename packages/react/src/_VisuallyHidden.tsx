671f04799978adcc5988389814bc66142c76fc55
Jon Rohan <yes@jonrohan.codes>
2025-07-07 21:28:38 +0000

    chore(VisuallyHidden): simplify Props interface and remove generic type

--- packages/react/src/_VisuallyHidden.tsx
+++ packages/react/src/_VisuallyHidden.tsx
@@ -3,18 +3,12 @@ import {clsx} from 'clsx'
 import {BoxWithFallback} from './internal/components/BoxWithFallback'
 import type {SxProp} from './sx'
 
-interface Props<As extends React.ElementType> {
+interface Props extends React.ComponentPropsWithoutRef<'span'> {
   isVisible?: boolean
-  as?: As
+  as?: React.ElementType
 }
 
-function VisuallyHidden<As extends React.ElementType>({
-  isVisible,
-  children,
-  as = 'span',
-  className,
-  ...rest
-}: Props & { as?: As } & React.ComponentPropsWithoutRef<ElementType extends As ? As : 'span'> & SxProp) {
+function VisuallyHidden({isVisible, children, as = 'span', className, ...rest}: Props & SxProp) {
   return (
     <BoxWithFallback as={as} className={clsx(className, {[classes.InternalVisuallyHidden]: !isVisible})} {...rest}>
       {children}
