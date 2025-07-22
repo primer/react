72f43a2799fd86f16b041d8cf150928327d975e9
llastflowers <llastflowers@github.com>
2025-07-22 10:32:05 -0700

    change default empty message and update docs accordingly

--- packages/react/src/SelectPanel/SelectPanel.tsx
+++ packages/react/src/SelectPanel/SelectPanel.tsx
@@ -31,8 +31,8 @@ import type {ButtonProps, LinkButtonProps} from '../Button/types'
 const SHORT_DELAY_MS = 500
 const LONG_DELAY_MS = 1000
 const EMPTY_MESSAGE = {
-  title: 'You haven’t created any items yet',
-  description: 'Please add or create new items to populate the list.',
+  title: 'No items available.',
+  description: '',
 }
 
 const DefaultEmptyMessage = (
