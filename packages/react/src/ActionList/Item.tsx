3f68aa7ca2af4e458d0bd4faa3dcc6b2f2c48d08
Katie Langerman <18661030+langermank@users.noreply.github.com>
2025-07-29 10:25:28 -0700

    Add `size` prop to `ActionList.Item` (#6378)

--- packages/react/src/ActionList/Item.tsx
+++ packages/react/src/ActionList/Item.tsx
@@ -48,6 +48,7 @@ export const Item = React.forwardRef<HTMLLIElement, ActionListItemProps>(
   (
     {
       variant = 'default',
+      size = 'medium',
       disabled = false,
       inactiveText,
       selected = undefined,
@@ -233,6 +234,7 @@ export const Item = React.forwardRef<HTMLLIElement, ActionListItemProps>(
       <ItemContext.Provider
         value={{
           variant,
+          size,
           disabled,
           inactive: Boolean(inactiveText),
           inlineDescriptionId,
@@ -252,7 +254,7 @@ export const Item = React.forwardRef<HTMLLIElement, ActionListItemProps>(
           data-has-description={slots.description ? true : false}
           className={clsx(classes.ActionListItem, className)}
         >
-          <ItemWrapper {...wrapperProps} className={classes.ActionListContent}>
+          <ItemWrapper {...wrapperProps} className={classes.ActionListContent} data-size={size}>
             <span className={classes.Spacer} />
             <Selection selected={selected} className={classes.LeadingAction} />
             <VisualOrIndicator
