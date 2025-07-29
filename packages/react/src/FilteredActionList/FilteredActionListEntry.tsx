ea6017c901f519c918398b1facef70c85811db39
Marie Lucca <francinelucca@github.com>
2025-07-28 22:58:10 -0400

    re-add type

--- packages/react/src/FilteredActionList/FilteredActionListEntry.tsx
+++ packages/react/src/FilteredActionList/FilteredActionListEntry.tsx
@@ -3,7 +3,7 @@ import {FilteredActionList as WithDeprecatedActionList} from './FilteredActionLi
 import {FilteredActionList as WithStableActionList} from './FilteredActionListWithModernActionList'
 import {useFeatureFlag} from '../FeatureFlags'
 
-export function FilteredActionList(props: FilteredActionListProps) {
+export function FilteredActionList(props: FilteredActionListProps): JSX.Element {
   const enabled = useFeatureFlag('primer_react_select_panel_with_modern_action_list')
 
   if (enabled) return <WithStableActionList {...props} />
