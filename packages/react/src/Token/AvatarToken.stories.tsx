134cbd34166c87271a0eaf32926a6b34abe96b11
llastflowers <llastflowers@github.com>
2025-08-01 11:16:04 -0700

    Update AvatarToken.stories.tsx to no longer use styled-components

--- packages/react/src/Token/AvatarToken.stories.tsx
+++ packages/react/src/Token/AvatarToken.stories.tsx
@@ -1,13 +1,11 @@
 import type React from 'react'
 import type {Meta} from '@storybook/react-vite'
 import {action} from 'storybook/actions'
-
-import {get} from '../constants'
 import {BaseStyles, ThemeProvider} from '..'
-import Box from '../Box'
-import type {AvatarTokenProps} from './AvatarToken'
 import AvatarToken from './AvatarToken'
+import type {AvatarTokenProps} from './AvatarToken'
 import Text from '../Text'
+import classes from './AvatarToken.stories.module.css'
 
 export default {
   title: 'Components/AvatarToken',
@@ -32,37 +30,23 @@ export default {
 const excludedControlKeys = ['id', 'as', 'tabIndex', 'onRemove']
 
 const SingleExampleContainer: React.FC<React.PropsWithChildren<{label?: string}>> = ({children, label}) => (
-  <Box
-    display="flex"
-    sx={{
-      alignItems: 'start',
-      flexDirection: 'column',
-      gap: get('space.0'),
-    }}
-  >
+  <div className={classes.SingleExampleContainer}>
     {label ? (
       <Text fontSize={0} color="fg.muted">
         {label}
       </Text>
     ) : null}
     {children}
-  </Box>
+  </div>
 )
 
 const ExampleCollectionContainer: React.FC<React.PropsWithChildren<unknown>> = ({children}) => (
-  <Box
-    display="flex"
-    sx={{
-      alignItems: 'start',
-      flexDirection: 'column',
-      gap: get('space.6'),
-    }}
-  >
+  <div className={classes.ExampleCollectionContainer}>
     <Text fontSize={1} color="fg.subtle">
       Hint: use the &quot;Controls&quot; tab in the Addons panel to change the token properties
     </Text>
     {children}
-  </Box>
+  </div>
 )
 
 export const DefaultToken = (args: Omit<AvatarTokenProps, 'ref'>) => {
@@ -78,17 +62,11 @@ DefaultToken.parameters = {controls: {exclude: [...excludedControlKeys, 'hideRem
 export const Interactive = (args: Omit<AvatarTokenProps, 'ref' | 'text'>) => {
   return (
     <ExampleCollectionContainer>
-      <Box
-        display="flex"
-        sx={{
-          alignItems: 'start',
-          gap: get('space.2'),
-        }}
-      >
+      <div className={classes.InteractiveRow}>
         <AvatarToken as="a" href="http://google.com/" {...args} text="Link" />
         <AvatarToken as="button" onClick={action('clicked')} {...args} text="Button" />
         <AvatarToken as="span" tabIndex={0} onFocus={action('focused')} {...args} text="Focusable Span" />
-      </Box>
+      </div>
     </ExampleCollectionContainer>
   )
 }
@@ -101,13 +79,7 @@ export const WithOnRemoveFn = (args: Omit<AvatarTokenProps, 'ref'>) => {
         <AvatarToken onRemove={action('remove me')} {...args} />
       </SingleExampleContainer>
       <SingleExampleContainer label="w/ onRemove passed and the token is interactive">
-        <Box
-          display="flex"
-          sx={{
-            alignItems: 'start',
-            gap: get('space.2'),
-          }}
-        >
+        <div className={classes.InteractiveRow}>
           <AvatarToken as="a" href="http://google.com/" onRemove={action('remove me')} {...args} text="Link" />
           <AvatarToken as="button" onClick={action('clicked')} onRemove={action('remove me')} {...args} text="Button" />
           <AvatarToken
@@ -118,7 +90,7 @@ export const WithOnRemoveFn = (args: Omit<AvatarTokenProps, 'ref'>) => {
             {...args}
             text="Focusable Span"
           />
-        </Box>
+        </div>
       </SingleExampleContainer>
     </ExampleCollectionContainer>
   )
