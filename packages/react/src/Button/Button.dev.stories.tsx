da4b044e02a72da394536c4d054a7513be1c329d
llastflowers <llastflowers@github.com>
2025-08-01 12:03:25 -0700

    Update Button.dev.stories.tsx to no longer use styled-components

--- packages/react/src/Button/Button.dev.stories.tsx
+++ packages/react/src/Button/Button.dev.stories.tsx
@@ -2,6 +2,7 @@ import {SearchIcon, TriangleDownIcon, EyeIcon, IssueClosedIcon, HeartFillIcon} f
 import {Button, IconButton} from '.'
 import {default as Text} from '../Text'
 import {Stack} from '../Stack'
+import classes from './Button.dev.stories.module.css'
 
 export default {
   title: 'Components/Button/Dev',
@@ -10,7 +11,7 @@ export default {
 export const InvisibleVariants = () => {
   const count = 4
   return (
-    <div style={{display: 'flex', flexDirection: 'row', gap: '1rem'}}>
+    <div className={classes.FlexRow}>
       <Button variant="invisible">Button</Button>
       <Button variant="invisible" leadingVisual={SearchIcon}>
         Button
@@ -28,69 +29,6 @@ export const InvisibleVariants = () => {
         Button
       </Button>
       <IconButton icon={TriangleDownIcon} variant="invisible" aria-label="Invisible" />
-    </div>
-  )
-}
-
-export const TestSxProp = () => {
-  const count = 4
-  return (
-    <div style={{display: 'flex', flexDirection: 'row', gap: '1rem'}}>
-      <Button
-        size="medium"
-        sx={{
-          color: 'firebrick',
-          backgroundColor: '#F6F8FA',
-        }}
-      >
-        Medium Red
-      </Button>
-      <Button
-        size="small"
-        sx={{
-          ':hover': {
-            color: 'deepskyblue',
-          },
-          [`@media screen and (max-width: 768px)`]: {
-            color: 'maroon',
-            backgroundColor: '#F6F8FA',
-          },
-          '@media (min-width: 1440)': {
-            color: 'firebrick',
-            backgroundColor: '#F6F8FA',
-          },
-        }}
-      >
-        Red
-      </Button>
-      <Button variant="invisible" sx={{color: 'firebrick', backgroundColor: '#F6F8FA'}}>
-        Invariant color overridden
-      </Button>
-      <Button leadingVisual={IssueClosedIcon} sx={{color: 'done.fg'}}>
-        <Text sx={{color: 'fg.default'}}>Close issue</Text>
-      </Button>
-      <Button
-        size="small"
-        variant="invisible"
-        sx={{
-          width: 32,
-          height: 32,
-          '&:focus': {
-            outline: 0,
-            '& > span': {
-              boxShadow: `inset 0 0 0 2px deeppink`,
-            },
-          },
-        }}
-      >
-        Custom size
-      </Button>
-      <Button size="small" block variant="invisible" sx={{width: 320}}>
-        Overridden Block
-      </Button>
-      <Button sx={{fontSize: 32}} count={count}>
-        Watch
-      </Button>
     </div>
   )
 }
