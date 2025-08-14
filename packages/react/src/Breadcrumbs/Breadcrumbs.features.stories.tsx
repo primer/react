f41527604bf8eb9aaac068eef82a0b40b8166225
Pavithra Kodmad <pksjce@github.com>
2025-08-14 12:28:37 +1000

    Add wrapped breadcrumbs in story

--- packages/react/src/Breadcrumbs/Breadcrumbs.features.stories.tsx
+++ packages/react/src/Breadcrumbs/Breadcrumbs.features.stories.tsx
@@ -1,4 +1,5 @@
 import type {Meta} from '@storybook/react-vite'
+import type React from 'react'
 import type {ComponentProps} from '../utils/types'
 import Breadcrumbs from './Breadcrumbs'
 
@@ -61,4 +62,45 @@ export const OverflowMenuNarrowContainer = () => (
       </Breadcrumbs.Item>
     </Breadcrumbs>
   </div>
+)
+
+// Wrapper components to test that BreadcrumbsItem works when wrapped
+const StyledWrapper = ({children}: {children: React.ReactNode}) => (
+  <span style={{padding: '2px', border: '1px dotted #999'}}>{children}</span>
+)
+
+const ConditionalWrapper = ({children, condition}: {children: React.ReactNode; condition: boolean}) => {
+  return condition ? <strong>{children}</strong> : <>{children}</>
+}
+
+const DataAttributeWrapper = ({children}: {children: React.ReactNode}) => (
+  <span data-testid="wrapper" className="custom-wrapper">
+    {children}
+  </span>
+)
+
+export const WrappedBreadcrumbItemsWithOverflow = () => (
+  <Breadcrumbs overflow="menu">
+    <StyledWrapper>
+      <Breadcrumbs.Item href="#">Wrapped Home</Breadcrumbs.Item>
+    </StyledWrapper>
+    <ConditionalWrapper condition={false}>
+      <Breadcrumbs.Item href="#">Products</Breadcrumbs.Item>
+    </ConditionalWrapper>
+    <DataAttributeWrapper>
+      <Breadcrumbs.Item href="#">Category</Breadcrumbs.Item>
+    </DataAttributeWrapper>
+    <StyledWrapper>
+      <Breadcrumbs.Item href="#">Subcategory</Breadcrumbs.Item>
+    </StyledWrapper>
+    <ConditionalWrapper condition={true}>
+      <Breadcrumbs.Item href="#">Item</Breadcrumbs.Item>
+    </ConditionalWrapper>
+    <DataAttributeWrapper>
+      <Breadcrumbs.Item href="#">Details</Breadcrumbs.Item>
+    </DataAttributeWrapper>
+    <Breadcrumbs.Item href="#" selected>
+      Current Page
+    </Breadcrumbs.Item>
+  </Breadcrumbs>
 )
