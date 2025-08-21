af3c4dad1a29b98c0db1ba1d3bdf081c393e2d73
Siddharth Kshetrapal <siddharthkp@github.com>
2025-08-21 13:08:16 +0200

    add story that loads more items on scroll

--- packages/react/src/SelectPanel/SelectPanel.tsx
+++ packages/react/src/SelectPanel/SelectPanel.tsx
@@ -765,6 +765,7 @@ function Panel({
         onOpen={onOpen}
         onClose={onClose}
         overlayProps={{
+          id: id ? `${id}--dialog` : undefined,
           role: 'dialog',
           'aria-labelledby': titleId,
           'aria-describedby': subtitle ? subtitleId : undefined,
