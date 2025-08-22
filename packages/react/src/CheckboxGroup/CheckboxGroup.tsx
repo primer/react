069ce2ce8d74620f48977b10d26ab342558e48f7
llastflowers <llastflowers@github.com>
2025-08-22 15:11:18 -0700

    remove sx prop from CheckboxGroup.tsx

--- packages/react/src/CheckboxGroup/CheckboxGroup.tsx
+++ packages/react/src/CheckboxGroup/CheckboxGroup.tsx
@@ -6,7 +6,6 @@ import CheckboxOrRadioGroupCaption from '../internal/components/CheckboxOrRadioG
 import CheckboxOrRadioGroupLabel from '../internal/components/CheckboxOrRadioGroup/CheckboxOrRadioGroupLabel'
 import CheckboxOrRadioGroupValidation from '../internal/components/CheckboxOrRadioGroup/CheckboxOrRadioGroupValidation'
 import {useRenderForcingRef} from '../hooks'
-import type {SxProp} from '../sx'
 import FormControl from '../FormControl'
 import Checkbox from '../Checkbox/Checkbox'
 import {CheckboxGroupContext} from './CheckboxGroupContext'
@@ -16,8 +15,7 @@ type CheckboxGroupProps = {
    * An onChange handler that gets called when any of the checkboxes change
    */
   onChange?: (selected: string[], e?: ChangeEvent<HTMLInputElement>) => void
-} & CheckboxOrRadioGroupProps &
-  SxProp
+} & CheckboxOrRadioGroupProps
 
 const CheckboxGroup: FC<React.PropsWithChildren<CheckboxGroupProps>> = ({children, disabled, onChange, ...rest}) => {
   const formControlComponentChildren = React.Children.toArray(children)
