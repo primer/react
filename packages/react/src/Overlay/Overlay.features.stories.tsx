cabae9794e7969fca290efa2127f88d7ad41cc5d
Mike Perrotti <mperrotti@github.com>
2025-08-25 15:04:12 -0400

    appeases Stylelint

--- packages/react/src/Overlay/Overlay.features.stories.tsx
+++ packages/react/src/Overlay/Overlay.features.stories.tsx
@@ -490,7 +490,7 @@ export const MemexIssueOverlay = ({role, open}: Args) => {
                 }}
                 ref={inputRef}
                 block
-                className={classes.memexOverlayIssueTitleInput}
+                className={classes.MemexOverlayIssueTitleInput}
               />
             ) : (
               <Button
