938b37584ca645c83cc36e67ea7d87c9301016cb
LiuLiu <liuliu-dev@github.com>
2025-08-27 07:17:14 -0700

    Fix SelectPanel storybook text truncation  (#6677)

--- packages/react/src/SelectPanel/SelectPanel.examples.stories.tsx
+++ packages/react/src/SelectPanel/SelectPanel.examples.stories.tsx
@@ -1,11 +1,11 @@
 import React, {useState, useMemo, useRef, useEffect} from 'react'
 import type {Meta} from '@storybook/react-vite'
 import {Button} from '../Button'
-import type {ItemInput} from '../deprecated/ActionList/List'
+import type {ItemInput} from '../FilteredActionList'
 import {SelectPanel} from './SelectPanel'
 import type {OverlayProps} from '../Overlay'
 import {TriangleDownIcon} from '@primer/octicons-react'
-import {ActionList} from '../deprecated/ActionList'
+import {ActionList} from '../ActionList'
 import FormControl from '../FormControl'
 import {Stack} from '../Stack'
 import {Dialog} from '../experimental'
@@ -299,9 +299,8 @@ export const CustomItemRenderer = () => {
         onFilterChange={setFilter}
         overlayProps={{width: 'medium'}}
         renderItem={item => (
-          <ActionList.Item {...item} text={undefined} className={styles.CustomActionListItem}>
-            {' '}
-            <div className={styles.TruncatedText}>{item.text}</div>
+          <ActionList.Item id={item.id?.toString()} className={styles.CustomActionListItem}>
+            <ActionList.Description truncate>{item.text}</ActionList.Description>
           </ActionList.Item>
         )}
         message={filteredItems.length === 0 ? NoResultsMessage(filter) : undefined}
