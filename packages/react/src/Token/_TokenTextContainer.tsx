e7c8d70ee585d87f0d501b3316df31319ffd8aa5
Liu Liu <liuliu-dev@github.com>
2025-08-28 16:35:36 -0700

    remove sx props and BoxWithFallback

--- packages/react/src/Token/_TokenTextContainer.tsx
+++ packages/react/src/Token/_TokenTextContainer.tsx
@@ -2,13 +2,12 @@ import type {TokenBaseProps} from './TokenBase'
 import type React from 'react'
 import classes from './_TokenTextContainer.module.css'
 import {clsx} from 'clsx'
-import {BoxWithFallback} from '../internal/components/BoxWithFallback'
 
-const TokenTextContainer = ({children, ...props}: React.PropsWithChildren<Partial<TokenBaseProps>>) => {
+const TokenTextContainer = ({children, id, ...props}: React.PropsWithChildren<Partial<TokenBaseProps>>) => {
   return (
-    <BoxWithFallback as="span" className={clsx(classes.TokenTextContainer)} {...props}>
+    <span className={clsx(classes.TokenTextContainer)} id={id?.toString()} {...props}>
       {children}
-    </BoxWithFallback>
+    </span>
   )
 }
 
