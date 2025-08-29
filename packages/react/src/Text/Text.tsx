7312d6fc7827c38b38bf0bde89ee90cde47eddff
Liu Liu <liuliu-dev@github.com>
2025-08-29 13:52:42 -0700

    remove sx props

--- packages/react/src/Text/Text.tsx
+++ packages/react/src/Text/Text.tsx
@@ -1,11 +1,9 @@
 import {clsx} from 'clsx'
-import {type StyledComponent} from 'styled-components'
 import React, {forwardRef} from 'react'
 import type {SystemCommonProps, SystemTypographyProps} from '../constants'
-import type {SxProp} from '../sx'
 import {useRefObjectAsForwardedRef} from '../hooks'
+import type {ForwardRefComponent as PolymorphicForwardRefComponent} from '../utils/polymorphic'
 import classes from './Text.module.css'
-import {BoxWithFallback} from '../internal/components/BoxWithFallback'
 
 type StyledTextProps = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -14,27 +12,24 @@ type StyledTextProps = {
   weight?: 'light' | 'normal' | 'medium' | 'semibold'
 } & SystemTypographyProps &
   SystemCommonProps &
-  SxProp &
   React.HTMLAttributes<HTMLSpanElement>
 
-const Text = forwardRef(({as: Component = 'span', className, size, weight, ...props}, forwardedRef) => {
-  const innerRef = React.useRef<HTMLElement>(null)
-  useRefObjectAsForwardedRef(forwardedRef, innerRef)
+const Text = forwardRef<HTMLElement, StyledTextProps>(
+  ({as: Component = 'span', className, size, weight, ...props}, forwardedRef) => {
+    const innerRef = React.useRef<HTMLElement>(null)
+    useRefObjectAsForwardedRef(forwardedRef, innerRef)
 
-  return (
-    <BoxWithFallback
-      as={Component}
-      className={clsx(className, classes.Text)}
-      data-size={size}
-      data-weight={weight}
-      {...props}
-      // @ts-ignore shh
-      ref={innerRef}
-    />
-  )
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-}) as StyledComponent<'span', any, StyledTextProps, never>
+    return (
+      <Component
+        className={clsx(className, classes.Text)}
+        data-size={size}
+        data-weight={weight}
+        {...props}
+        ref={innerRef}
+      />
+    )
+  },
+) as PolymorphicForwardRefComponent<'span', StyledTextProps>
 
 Text.displayName = 'Text'
 
