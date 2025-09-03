ed39e35a9d485b636be93ade42f5bb9475b9f52f
Liu Liu <liuliu-dev@github.com>
2025-09-03 15:37:27 -0700

    fix ci

--- packages/react/src/TextInput/TextInput.dev.stories.tsx
+++ packages/react/src/TextInput/TextInput.dev.stories.tsx
@@ -1,6 +1,6 @@
 import type React from 'react'
 import type {Meta} from '@storybook/react-vite'
-import {Box, FormControl} from '..'
+import {FormControl} from '..'
 import TextInput from '.'
 import {textInputExcludedControlKeys} from '../utils/story-helpers'
 
