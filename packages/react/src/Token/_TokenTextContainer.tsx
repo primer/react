91b3c4b44107843a929ffec557782f89f620b306
Liu Liu <liuliu-dev@github.com>
2025-09-05 22:33:19 +0000

    passing as

--- packages/react/src/Token/_TokenTextContainer.tsx
+++ packages/react/src/Token/_TokenTextContainer.tsx
@@ -3,11 +3,24 @@ import type React from 'react'
 import classes from './_TokenTextContainer.module.css'
 import {clsx} from 'clsx'
 
-const TokenTextContainer = ({children, id, ...props}: React.PropsWithChildren<Partial<TokenBaseProps>>) => {
+const TokenTextContainer = ({
+  children,
+  id,
+  as: Component = 'span',
+  ...props
+}: React.PropsWithChildren<Partial<TokenBaseProps>>) => {
   return (
-    <span className={clsx(classes.TokenTextContainer)} id={id?.toString()} {...props}>
+    <Component
+      className={clsx(classes.TokenTextContainer)}
+      id={id?.toString()}
+      {...(Component === 'button'
+        ? (props as React.ButtonHTMLAttributes<HTMLButtonElement>)
+        : Component === 'a'
+          ? (props as React.AnchorHTMLAttributes<HTMLAnchorElement>)
+          : (props as React.HTMLAttributes<HTMLSpanElement>))}
+    >
       {children}
-    </span>
+    </Component>
   )
 }
 
