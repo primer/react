a7f75a38fbb8d2b0466308066f5bd7450a2a5a0e
Josh Black <joshblack@github.com>
2025-09-08 15:35:59 -0500

    feat: remove sx prop from Announce, AriaAlert, AriaStatus

--- packages/react/src/live-region/AriaStatus.tsx
+++ packages/react/src/live-region/AriaStatus.tsx
@@ -1,34 +1,31 @@
 import type React from 'react'
 import {type ElementType} from 'react'
 import {Announce} from './Announce'
-import type {SxProp} from '../sx'
 
-export type AriaStatusProps<As extends ElementType> = React.PropsWithChildren<
-  {
-    /**
-     * Customize the element type of the rendered container
-     */
-    as?: As
+export type AriaStatusProps<As extends ElementType> = React.PropsWithChildren<{
+  /**
+   * Customize the element type of the rendered container
+   */
+  as?: As
 
-    /**
-     * Specify if the content of the element should be announced when this
-     * component is rendered and is not hidden
-     * @default false
-     */
-    announceOnShow?: boolean
+  /**
+   * Specify if the content of the element should be announced when this
+   * component is rendered and is not hidden
+   * @default false
+   */
+  announceOnShow?: boolean
 
-    /**
-     * Specify if the element is hidden
-     * @default false
-     */
-    hidden?: boolean
+  /**
+   * Specify if the element is hidden
+   * @default false
+   */
+  hidden?: boolean
 
-    /**
-     * Provide a delay in milliseconds before the announcement is made
-     */
-    delayMs?: number
-  } & SxProp
->
+  /**
+   * Provide a delay in milliseconds before the announcement is made
+   */
+  delayMs?: number
+}>
 
 export function AriaStatus<As extends ElementType>({
   announceOnShow = false,
