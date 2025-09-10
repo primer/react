9e94a334616ff9ab4ef09698d79518fe0bacaed0
Siddharth Kshetrapal <siddharthkp@github.com>
2025-09-10 13:51:19 +0200

    examples: use ThemeProvider from styled-react

--- examples/codesandbox/src/main.tsx
+++ examples/codesandbox/src/main.tsx
@@ -1,4 +1,5 @@
-import {BaseStyles, ThemeProvider} from '@primer/react'
+import {ThemeProvider} from '@primer/styled-react'
+import {BaseStyles} from '@primer/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
