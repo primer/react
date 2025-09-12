826c522baba1356d19f3e4e4a16cf3ff35723d0f
Josh Black <joshblack@github.com>
2025-09-12 10:33:58 -0500

    test: update vitest config to include browser tests (#6818)
    Co-authored-by: primer[bot] <119360173+primer[bot]@users.noreply.github.com>

--- packages/styled-react/vitest.config.ts
+++ packages/styled-react/vitest.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
   test: {
+    name: '@primer/styled-react (node)',
     environment: 'node',
     exclude: ['src/**/*.browser.test.?(c|m)[jt]s?(x)'],
   },
