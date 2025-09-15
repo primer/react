be2ea128fd89a3b26c3d97b43c73abda2fa8d89d
Josh Black <joshblack@github.com>
2025-09-15 16:39:52 -0500

    refactor: move Box to @primer/styled-react

--- packages/styled-react/src/components/Box/Box.tsx
+++ packages/styled-react/src/components/Box/Box.tsx
@@ -12,9 +12,18 @@ import type {
   TypographyProps,
 } from 'styled-system'
 import {background, border, color, flexbox, grid, layout, position, shadow, space, typography} from 'styled-system'
-import type {SxProp} from '../sx'
-import sx from '../sx'
-import type {ComponentProps} from '../utils/types'
+import type {SxProp} from '../../sx'
+import sx from '../../sx'
+
+/**
+ * Extract a component's props
+ *
+ * Source: https://react-typescript-cheatsheet.netlify.app/docs/advanced/patterns_by_usecase#wrappingmirroring-a-component
+ *
+ * @example ComponentProps<typeof MyComponent>
+ */
+type ComponentProps<T> =
+  T extends React.ComponentType<React.PropsWithChildren<infer Props>> ? (Props extends object ? Props : never) : never
 
 type StyledBoxProps = SxProp &
   SpaceProps &
