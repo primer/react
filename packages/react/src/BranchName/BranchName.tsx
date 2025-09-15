02f350f4393e9e029c2de8eb38d3e779a0034562
Mike Perrotti <mperrotti@github.com>
2025-09-15 11:13:44 -0400

    reverts changes to BranchName

--- packages/react/src/BranchName/BranchName.tsx
+++ packages/react/src/BranchName/BranchName.tsx
@@ -1,29 +1,34 @@
-import type React from 'react'
+import React, {type ForwardedRef} from 'react'
 import {clsx} from 'clsx'
 import classes from './BranchName.module.css'
-import {fixedForwardRef, type PolymorphicProps} from '../utils/modern-polymorphic'
 
-type BranchNameProps<As extends React.ElementType = 'a'> = PolymorphicProps<
-  As,
-  'a',
-  {
+type BranchNameProps<As extends React.ElementType> = {
+  as?: As
+} & DistributiveOmit<React.ComponentPropsWithRef<React.ElementType extends As ? 'a' : As>, 'as'> & {
     className?: string
   }
->
 
-const BranchName = fixedForwardRef<HTMLAnchorElement, BranchNameProps<React.ElementType>>(function BranchName<
-  As extends React.ElementType = 'a',
->(props: BranchNameProps<As>, ref: React.Ref<HTMLAnchorElement>) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function BranchName<As extends React.ElementType>(props: BranchNameProps<As>, ref: ForwardedRef<any>) {
   const {as: Component = 'a', className, children, ...rest} = props
   return (
     <Component {...rest} ref={ref} className={clsx(className, classes.BranchName)}>
       {children}
     </Component>
   )
-})
+}
 
-Object.assign(BranchName, {displayName: 'BranchName'})
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+type FixedForwardRef = <T, P = {}>(
+  render: (props: P, ref: React.Ref<T>) => React.ReactNode,
+) => (props: P & React.RefAttributes<T>) => React.ReactNode
+
+const fixedForwardRef = React.forwardRef as FixedForwardRef
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type DistributiveOmit<T, TOmitted extends PropertyKey> = T extends any ? Omit<T, TOmitted> : never
+
+BranchName.displayName = 'BranchName'
 
 export type {BranchNameProps}
-
-export default BranchName
+export default fixedForwardRef(BranchName)
