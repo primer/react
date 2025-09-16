0319544f53a0d79636adcd47fb356560044f34c3
Liu Liu <liuliu-dev@github.com>
2025-09-16 15:01:40 -0700

    update deprecated and experimental tooltip

--- packages/styled-react/src/deprecated.tsx
+++ packages/styled-react/src/deprecated.tsx
@@ -1,1 +1,13 @@
-export {Dialog, Octicon, TabNav, Tooltip} from '@primer/react/deprecated'
+import {Box, type SxProp} from '@primer/react'
+import {Tooltip as PrimerTooltip, type TooltipProps as PrimerTooltipProps} from '@primer/react/deprecated'
+import React, {forwardRef} from 'react'
+
+type TooltipProps = PrimerTooltipProps & SxProp
+
+const Tooltip = forwardRef<HTMLSpanElement, TooltipProps>(function Tooltip(props, ref) {
+  return <Box as={PrimerTooltip} ref={ref} {...props} />
+})
+
+export {Tooltip, type TooltipProps}
+
+export {Dialog, Octicon, TabNav} from '@primer/react/deprecated'
