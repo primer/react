06ae495e7d4a016939a27000e4f3ddafbd077ee0
Mike Perrotti <mperrotti@github.com>
2025-09-17 01:06:23 -0400

    trying a dumb fix for new ActionList integration test error

--- packages/react/src/ActionList/shared.ts
+++ packages/react/src/ActionList/shared.ts
@@ -14,7 +14,7 @@ export type ActionListItemProps<As extends React.ElementType = 'li'> =
      * Callback that will trigger both on click selection and keyboard selection.
      * This is not called for disabled or inactive items.
      */
-    onSelect?: (event: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => void
+    onSelect?: (event: React.MouseEvent | React.KeyboardEvent) => void
     /**
      * Is the `Item` is currently selected?
      */
