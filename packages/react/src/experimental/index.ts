353c7d008d1afd83e070c94a5f25473c6db6d558
Pavithra Kodmad <pksjce@github.com>
2025-09-17 10:14:09 +1000

    Fix formatting

--- packages/react/src/experimental/index.ts
+++ packages/react/src/experimental/index.ts
@@ -14,7 +14,6 @@ export type {BlankslateProps} from '../Blankslate'
 export {ButtonBase} from '../Button'
 export type {ButtonBaseProps} from '../Button'
 
-
 export {DataTable, Table, createColumnHelper} from '../DataTable'
 export type {
   DataTableProps,
