e08ccecfc662098f91fcb261db429591be6d823d
Pavithra Kodmad <pksjce@github.com>
2025-09-18 10:47:03 +1000

    Fix the underlineitem types Co-authored-by: Marie Lucca <francinelucca@users.noreply.github.com>

--- packages/react/src/experimental/UnderlinePanels/UnderlinePanels.tsx
+++ packages/react/src/experimental/UnderlinePanels/UnderlinePanels.tsx
@@ -11,12 +11,7 @@ import React, {
 import {TabContainerElement} from '@github/tab-container-element'
 import type {IconProps} from '@primer/octicons-react'
 import {createComponent} from '../../utils/create-component'
-import {
-  UnderlineItemList,
-  UnderlineWrapper,
-  UnderlineItem,
-  type UnderlineItemProps,
-} from '../../internal/components/UnderlineTabbedInterface'
+import {UnderlineItemList, UnderlineWrapper, UnderlineItem} from '../../internal/components/UnderlineTabbedInterface'
 import {useId} from '../../hooks'
 import {invariant} from '../../utils/invariant'
 import {useResizeObserver, type ResizeObserverEntry} from '../../hooks/useResizeObserver'
@@ -100,7 +95,7 @@ const UnderlinePanels: FC<UnderlinePanelsProps> = ({
     let panelIndex = 0
 
     const childrenWithProps = Children.map(children, child => {
-      if (isValidElement<UnderlineItemProps>(child) && child.type === Tab) {
+      if (isValidElement(child) && child.type === Tab) {
         return cloneElement(child, {id: `${parentId}-tab-${tabIndex++}`, loadingCounters, iconsVisible})
       }
 
