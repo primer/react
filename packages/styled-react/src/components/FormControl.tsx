0906165d7546c4904bb6aa190ffd9a4e18e756ee
Liu Liu <liuliu-dev@github.com>
2025-09-23 15:39:18 -0700

    sub components

--- packages/styled-react/src/components/FormControl.tsx
+++ packages/styled-react/src/components/FormControl.tsx
@@ -2,73 +2,39 @@ import {
   Box,
   FormControl as PrimerFormControl,
   type FormControlProps as PrimerFormControlProps,
-  type FormControlCaptionProps as PrimerFormControlCaptionProps,
-  type FormControlLabelProps as PrimerFormControlLabelProps,
+  type FormControlCaptionProps,
   type FormControlValidationProps as PrimerFormControlValidationProps,
   type SxProp,
-  sx,
 } from '@primer/react'
-import {forwardRef, type PropsWithChildren, type ComponentType} from 'react'
-import styled from 'styled-components'
-import type {ForwardRefComponent} from '../polymorphic'
+import {forwardRef, type PropsWithChildren} from 'react'
 
-type FormControlProps = PrimerFormControlProps & SxProp
+type FormControlProps = PropsWithChildren<PrimerFormControlProps> & SxProp
 
 const FormControlImpl = forwardRef<HTMLDivElement, FormControlProps>(function FormControl(props, ref) {
   return <Box ref={ref} as={PrimerFormControl} {...props} />
 })
 
-type FormControlCaptionProps = PrimerFormControlCaptionProps & SxProp
+const FormControlCaption = function (props: FormControlCaptionProps) {
+  return <Box as={PrimerFormControl.Caption} {...props} />
+}
+
 type FormControlValidationProps = PropsWithChildren<PrimerFormControlValidationProps> & SxProp
 
-const FormControlCaption: ComponentType<FormControlCaptionProps> = styled(
-  PrimerFormControl.Caption,
-).withConfig<FormControlCaptionProps>({
-  shouldForwardProp: prop => prop !== 'sx',
-})`
-  ${sx}
-`
-
-const FormControlValidation = styled(PrimerFormControl.Validation).withConfig<FormControlValidationProps>({
-  shouldForwardProp: prop => prop !== 'sx',
-})`
-  ${sx}
-`
-
-type BaseProps = SxProp & {
-  disabled?: boolean
-  required?: boolean
-  requiredText?: string
-  requiredIndicator?: boolean
-  visuallyHidden?: boolean
-  id?: string
-  className?: string
+const FormControlValidation = function (props: FormControlValidationProps) {
+  return <Box as={PrimerFormControl.Validation} {...props} />
 }
-
-export type LabelProps = BaseProps & {
-  htmlFor?: string
-  as?: 'label'
-}
-
-type InputLabelProps = PropsWithChildren<LabelProps>
-
-type FormControlLabelProps = PropsWithChildren<{htmlFor?: string} & InputLabelProps & PrimerFormControlLabelProps> &
-  SxProp
-
-const FormControlLabel: ForwardRefComponent<'label', FormControlLabelProps> = styled(
-  PrimerFormControl.Label,
-).withConfig<FormControlLabelProps>({
-  shouldForwardProp: prop => prop !== 'sx',
-})`
-  ${sx}
-`
 
 const FormControl = Object.assign(FormControlImpl, {
   Caption: FormControlCaption,
   LeadingVisual: PrimerFormControl.LeadingVisual,
   Validation: FormControlValidation,
-  Label: FormControlLabel,
-})
+  Label: PrimerFormControl.Label,
+}) as typeof FormControlImpl & {
+  Caption: typeof FormControlCaption
+  LeadingVisual: typeof PrimerFormControl.LeadingVisual
+  Validation: typeof FormControlValidation
+  Label: typeof PrimerFormControl.Label
+}
 
 export {FormControl}
 export type {FormControlProps}
