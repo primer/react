75afc7eecb950fa3b0642110765774000c9ac1c1
Tyler Jones <tylerjdev@github.com>
2025-09-23 11:36:43 -0400

    Fix regex

--- packages/react/src/TreeView/TreeView.test.tsx
+++ packages/react/src/TreeView/TreeView.test.tsx
@@ -1734,7 +1734,7 @@ it('should have keyboard shortcut command as part of accessible name when using 
     </TreeView>,
   )
 
-  expect(screen.getByRole('treeitem', {name: /^Parent[;.][^]*for more actions\.$/})).toBeInTheDocument()
+  expect(screen.getByRole('treeitem', {name: /for more actions\.$/})).toBeInTheDocument()
 })
 
 it('should have keyboard shortcut command as part of accessible name when using `TrailingAction` and `aria-label`', () => {
@@ -1754,7 +1754,7 @@ it('should have keyboard shortcut command as part of accessible name when using 
     </TreeView>,
   )
 
-  expect(screen.getByRole('treeitem', {name: /^Parent[;.][^]*for more actions\.$/})).toBeInTheDocument()
+  expect(screen.getByRole('treeitem', {name: /for more actions\.$/})).toBeInTheDocument()
 })
 
 it('should activate the dialog for trailing action when keyboard shortcut is used', async () => {
@@ -1788,7 +1788,7 @@ it('should activate the dialog for trailing action when keyboard shortcut is use
   )
 
   const treeItem = screen.getByRole('treeitem', {
-    name: /^Parent[;.][^]*for more actions\.$/,
+    name: /for more actions\.$/,
   })
   treeItem.focus()
   expect(treeItem).toHaveFocus()
