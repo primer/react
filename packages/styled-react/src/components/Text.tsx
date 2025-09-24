f08e9a8d888e0ae9d127caccc5d4c33f52431c59
Liu Liu <liuliu-dev@github.com>
2025-09-24 14:53:21 -0700

    remove ForwardRefComponent

--- packages/styled-react/src/components/Text.tsx
+++ packages/styled-react/src/components/Text.tsx
@@ -1,15 +1,13 @@
 import {type SxProp, Text as PrimerText, type TextProps as PrimerTextProps} from '@primer/react'
-import type {ForwardRefComponent} from '../polymorphic'
 import {sx} from '../sx'
 import styled from 'styled-components'
 
 type TextProps = PrimerTextProps & SxProp
 
-const Text: ForwardRefComponent<'span' | React.ComponentType<any> | keyof JSX.IntrinsicElements, TextProps> = styled(
-  PrimerText,
-).withConfig<TextProps>({
+const Text = styled(PrimerText).withConfig<TextProps>({
   shouldForwardProp: prop => prop !== 'sx',
 })`
   ${sx}
 `
+
 export {Text, type TextProps}
