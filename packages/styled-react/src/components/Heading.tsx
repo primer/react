89d1da6e6b03c39ee720dbcc700e143af9498918
Marie Lucca <francinelucca@github.com>
2025-09-23 22:37:33 -0400

    fix type

--- packages/styled-react/src/components/Heading.tsx
+++ packages/styled-react/src/components/Heading.tsx
@@ -4,9 +4,11 @@ import type {ForwardRefComponent} from '../polymorphic'
 import {sx, type SxProp} from '../sx'
 import styled from 'styled-components'
 
+type HeadingLevels = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+
 type HeadingProps = PrimerHeadingProps & SxProp
 
-const Heading: ForwardRefComponent<'h2', HeadingProps> = styled(PrimerHeading).withConfig({
+const Heading: ForwardRefComponent<HeadingLevels, HeadingProps> = styled(PrimerHeading).withConfig({
   shouldForwardProp: prop => (prop as keyof HeadingProps) !== 'sx',
 })<HeadingProps>`
   ${sx}
