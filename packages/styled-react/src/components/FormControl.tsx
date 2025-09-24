385acbea8c11fcda01f5cc12211de56fb059391a
Liu Liu <liuliu-dev@github.com>
2025-09-24 10:40:12 -0700

    label fix

--- packages/styled-react/src/components/FormControl.tsx
+++ packages/styled-react/src/components/FormControl.tsx
@@ -29,29 +29,17 @@ const FormControlLeadingVisual = (props: PropsWithChildren<SxProp>) => {
   return <Box as={PrimerFormControl.LeadingVisual} {...props} />
 }
 
-// Define local type instead of using the complex imported type
-type FormControlLabelProps = PropsWithChildren<
-  {
-    htmlFor?: string
-    visuallyHidden?: boolean
-    requiredText?: string
-    requiredIndicator?: boolean
-    id?: string
-    className?: string
-    as?: 'label' | 'legend' | 'span'
-  } & SxProp
->
-
-const FormControlLabel = (props: FormControlLabelProps) => {
-  return <Box as={PrimerFormControl.Label} {...props} />
-}
-
 const FormControl = Object.assign(FormControlImpl, {
   Caption: FormControlCaption,
   LeadingVisual: FormControlLeadingVisual,
   Validation: FormControlValidation,
-  Label: FormControlLabel,
-})
+  Label: PrimerFormControl.Label,
+}) as typeof FormControlImpl & {
+  Caption: typeof FormControlCaption
+  LeadingVisual: typeof FormControlLeadingVisual
+  Validation: typeof FormControlValidation
+  Label: typeof PrimerFormControl.Label
+}
 
 export {FormControl}
 export type {FormControlProps}
