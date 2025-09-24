b0c7810c1fef7d403f299484b4dd486d50f7aa1d
Marie Lucca <francinelucca@github.com>
2025-09-24 09:59:54 -0400

    alphabetical sort

--- packages/styled-react/src/experimental.tsx
+++ packages/styled-react/src/experimental.tsx
@@ -1,8 +1,8 @@
+export {Dialog, type DialogProps} from './components/Dialog'
 export {
   PageHeader,
   type PageHeaderProps,
   type PageHeaderActionsProps,
   type PageHeaderTitleProps,
 } from './components/PageHeader'
-export {Dialog, type DialogProps} from './components/Dialog'
 export {Table, Tooltip, UnderlinePanels} from '@primer/react/experimental'
