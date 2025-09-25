da48f7c01db212448a12a83398da685e8be03f35
Liu Liu <liuliu-dev@github.com>
2025-09-25 10:23:51 -0700

    revert to debug

--- packages/styled-react/src/components/Token.tsx
+++ packages/styled-react/src/components/Token.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import {type TokenProps as PrimerTokenProps, type SxProp, Token as PrimerToken} from '@primer/react'
 import {sx} from '../sx'
 import type {ForwardRefComponent} from '../polymorphic'
@@ -7,28 +6,10 @@ import type {PropsWithChildren} from 'react'
 
 type TokenProps = PropsWithChildren<PrimerTokenProps> & SxProp
 
-// Create a styled wrapper that only handles the sx prop
-const StyledTokenWrapper = styled.span<{sx?: SxProp['sx']}>`
+const Token: ForwardRefComponent<'a' | 'button' | 'span', TokenProps> = styled(PrimerToken).withConfig({
+  shouldForwardProp: (prop: keyof TokenProps) => prop !== 'sx',
+})<TokenProps>`
   ${sx}
 `
-
-const Token: ForwardRefComponent<'a' | 'button' | 'span', TokenProps> = React.forwardRef<
-  HTMLElement,
-  TokenProps
->(({sx: sxProp, ...rest}, ref) => {
-  // If sx is provided, wrap with styled component, otherwise use PrimerToken directly
-  if (sxProp) {
-    return (
-      <StyledTokenWrapper sx={sxProp}>
-        <PrimerToken {...rest} ref={ref} />
-      </StyledTokenWrapper>
-    )
-  }
-  
-  // Pass all props directly to PrimerToken when no sx styling is needed
-  return <PrimerToken {...rest} ref={ref} />
-}) as ForwardRefComponent<'a' | 'button' | 'span', TokenProps>
-
-Token.displayName = 'Token'
 
 export {Token, type TokenProps}
