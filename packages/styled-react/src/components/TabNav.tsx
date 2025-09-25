be66cd38cf03595eb3968fabcd786c2815a34f1f
Marie Lucca <francinelucca@github.com>
2025-09-25 17:27:16 -0400

    fix as prop

--- packages/styled-react/src/components/TabNav.tsx
+++ packages/styled-react/src/components/TabNav.tsx
@@ -1,29 +1,31 @@
 import {TabNav as PrimerTabNav} from '@primer/react/deprecated'
 import type {TabNavProps as PrimerTabNavProps, TabNavLinkProps as PrimerTabNavLinkProps} from '@primer/react/deprecated'
 import {sx, type SxProp} from '../sx'
-import styled, {type StyledComponent} from 'styled-components'
+import styled from 'styled-components'
 import {type ForwardRefComponent} from '../polymorphic'
 
 type TabNavProps = PrimerTabNavProps & SxProp
 type TabNavLinkProps = PrimerTabNavLinkProps & SxProp
 
-const TabNavImpl = styled(PrimerTabNav).withConfig({
+const StyledTabNav = styled(PrimerTabNav).withConfig({
   shouldForwardProp: prop => (prop as keyof TabNavProps) !== 'sx',
 })<TabNavProps>`
   ${sx}
 `
 
-const TabNavLink: ForwardRefComponent<'a', TabNavLinkProps> = styled(PrimerTabNav.Link).withConfig({
+// @ts-ignore
+const TabNavImpl = ({as, ...props}: TabNavProps) => <StyledTabNav forwardedAs={as} {...props} />
+
+const StyledTabNavLink: ForwardRefComponent<'a', TabNavLinkProps> = styled(PrimerTabNav.Link).withConfig({
   shouldForwardProp: prop => (prop as keyof TabNavLinkProps) !== 'sx',
 })<TabNavLinkProps>`
   ${sx}
 `
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type TabNavComponent = StyledComponent<typeof PrimerTabNav, any, TabNavProps, never> & {
-  Link: typeof TabNavLink
-}
 
-const TabNav: TabNavComponent = Object.assign(TabNavImpl, {
+// @ts-ignore
+const TabNavLink = ({as, ...props}: TabNavLinkProps) => <StyledTabNavLink forwardedAs={as} {...props} />
+
+const TabNav = Object.assign(TabNavImpl, {
   Link: TabNavLink,
 })
 
