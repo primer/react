f36c75c2ebefc88724cf9d5c9fc64aeef47025d6
Liu Liu <liuliu-dev@github.com>
2025-09-25 11:44:34 -0700

    for debugging

--- packages/styled-react/src/components/Token.tsx
+++ packages/styled-react/src/components/Token.tsx
@@ -1,34 +1,15 @@
 import {type TokenProps as PrimerTokenProps, type SxProp, Token as PrimerToken} from '@primer/react'
-import {Box} from './Box'
-import {forwardRef, type PropsWithChildren} from 'react'
+import {sx} from '../sx'
+import type {ForwardRefComponent} from '../polymorphic'
+import styled from 'styled-components'
+import type {PropsWithChildren} from 'react'
 
 type TokenProps = PropsWithChildren<PrimerTokenProps> & SxProp
 
-const Token = forwardRef<HTMLSpanElement | HTMLButtonElement | HTMLAnchorElement, TokenProps>(
-  function Token(props, ref) {
-    const {sx, ...restProps} = props
-    if (sx) {
-      // Extract test props and other HTML props that should go on the wrapper
-      const {
-        'data-testid': dataTestId,
-        className,
-        style,
-        ...tokenProps
-      } = restProps as any
-      
-      return (
-        <Box 
-          sx={sx} 
-          data-testid={dataTestId}
-          className={className}
-          style={style}
-        >
-          <PrimerToken ref={ref} {...tokenProps} />
-        </Box>
-      )
-    }
-    return <PrimerToken ref={ref} {...restProps} />
-  },
-)
+const Token: ForwardRefComponent<'a' | 'button' | 'span', TokenProps> = styled(PrimerToken).withConfig<TokenProps>({
+  shouldForwardProp: prop => prop !== 'sx',
+})`
+  ${sx}
+`
 
 export {Token, type TokenProps}
