1cde2a78091c9ad4a1a972bc23243ad5638820c1
Marie Lucca <francinelucca@github.com>
2025-09-25 11:26:05 -0400

    fix ttpes

--- packages/styled-react/src/components/TabNav.tsx
+++ packages/styled-react/src/components/TabNav.tsx
@@ -1,13 +1,12 @@
 import {TabNav as PrimerTabNav} from '@primer/react/deprecated'
 import type {TabNavProps as PrimerTabNavProps, TabNavLinkProps as PrimerTabNavLinkProps} from '@primer/react/deprecated'
-import type {ForwardRefComponent} from '../polymorphic'
 import {sx, type SxProp} from '../sx'
 import styled from 'styled-components'
 
 type TabNavProps = PrimerTabNavProps & SxProp
 type TabNavLinkProps = PrimerTabNavLinkProps & SxProp
 
-const TabNavImpl: ForwardRefComponent<'div', TabNavProps> = styled(PrimerTabNav).withConfig({
+const TabNavImpl = styled(PrimerTabNav).withConfig({
   shouldForwardProp: prop => (prop as keyof TabNavProps) !== 'sx',
 })<TabNavProps>`
   ${sx}
