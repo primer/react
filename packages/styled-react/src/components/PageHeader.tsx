a3c1fb9a5f79af4df236306d45baa882d1a28e25
Marie Lucca <francinelucca@github.com>
2025-09-26 13:54:24 -0400

    use forwardedAs prop in components

--- packages/styled-react/src/components/PageHeader.tsx
+++ packages/styled-react/src/components/PageHeader.tsx
@@ -10,10 +10,11 @@ import {sx, type SxProp} from '../sx'
 import type {ForwardRefComponent} from '../polymorphic'
 import {Box} from './Box'
 import type {PropsWithChildren} from 'react'
+import React from 'react'
 
 type PageHeaderProps = PrimerPageHeaderProps & SxProp
 
-const PageHeaderImpl: ForwardRefComponent<'div', PageHeaderProps> = styled(
+const StyledPageHeader: ForwardRefComponent<'div', PageHeaderProps> = styled(
   PrimerPageHeader,
 ).withConfig<PageHeaderProps>({
   shouldForwardProp: prop => prop !== 'sx',
@@ -21,9 +22,14 @@ const PageHeaderImpl: ForwardRefComponent<'div', PageHeaderProps> = styled(
   ${sx}
 `
 
+const PageHeaderImpl = React.forwardRef<HTMLDivElement, PageHeaderProps>(({as, ...props}, ref) => (
+  // @ts-ignore forwardedAs is valid here but I don't know how to fix the typescript error
+  <StyledPageHeader forwardedAs={as} ref={ref} {...props} />
+)) as ForwardRefComponent<'div', PageHeaderProps>
+
 type PageHeaderActionsProps = PrimerPageHeaderActionsProps & SxProp
 
-function PageHeaderActions({sx, ...rest}: PageHeaderActionsProps) {
+function StyledPageHeaderActions({sx, ...rest}: PageHeaderActionsProps) {
   const style: CSSCustomProperties = {}
   if (sx) {
     // @ts-ignore sx has height attribute
@@ -37,13 +43,16 @@ function PageHeaderActions({sx, ...rest}: PageHeaderActionsProps) {
   return <Box {...rest} as={PrimerPageHeader.Actions} style={style} sx={sx} />
 }
 
+// @ts-ignore forwardedAs is valid here but I don't know how to fix the typescript error
+const PageHeaderActions = ({as, ...props}: PageHeaderProps) => <StyledPageHeaderActions forwardedAs={as} {...props} />
+
 type PageHeaderTitleProps = PropsWithChildren<PrimerPageHeaderTitleProps> & SxProp
 
 type CSSCustomProperties = {
   [key: `--${string}`]: string | number
 }
 
-function PageHeaderTitle({sx, ...rest}: PageHeaderTitleProps) {
+function StyledPageHeaderTitle({sx, ...rest}: PageHeaderTitleProps) {
   const style: CSSCustomProperties = {}
   if (sx) {
     // @ts-ignore sx can have color attribute
@@ -65,6 +74,9 @@ function PageHeaderTitle({sx, ...rest}: PageHeaderTitleProps) {
   return <Box {...rest} as={PrimerPageHeader.Title} style={style} sx={sx} />
 }
 
+// @ts-ignore forwardedAs is valid here but I don't know how to fix the typescript error
+const PageHeaderTitle = ({as, ...props}: PageHeaderTitleProps) => <StyledPageHeaderTitle forwardedAs={as} {...props} />
+
 type PageHeaderTitleAreaProps = PropsWithChildren<PrimerPageHeaderTitleAreaProps> & SxProp
 
 const PageHeaderTitleArea: ForwardRefComponent<'div', PageHeaderTitleAreaProps> = styled(
@@ -75,7 +87,7 @@ const PageHeaderTitleArea: ForwardRefComponent<'div', PageHeaderTitleAreaProps> 
   ${sx}
 `
 
-type PageHeaderComponent = ForwardRefComponent<'div', PageHeaderProps> & {
+type PageHeaderComponentType = ForwardRefComponent<'div', PageHeaderProps> & {
   Actions: typeof PageHeaderActions
   ContextArea: typeof PrimerPageHeader.ContextArea
   ParentLink: typeof PrimerPageHeader.ParentLink
@@ -91,7 +103,7 @@ type PageHeaderComponent = ForwardRefComponent<'div', PageHeaderProps> & {
   TrailingAction: typeof PrimerPageHeader.TrailingAction
 }
 
-const PageHeader: PageHeaderComponent = Object.assign(PageHeaderImpl, {
+const PageHeader: PageHeaderComponentType = Object.assign(PageHeaderImpl, {
   Actions: PageHeaderActions,
   ContextArea: PrimerPageHeader.ContextArea,
   ParentLink: PrimerPageHeader.ParentLink,
