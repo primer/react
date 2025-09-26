b0abd785713d9f95ca9d5e41394e00153176077d
Jon Rohan <yes@jonrohan.codes>
2025-09-26 12:41:16 -0700

    feat(TabNav): remove support for sx prop (#6864)
    Co-authored-by: Marie Lucca <francinelucca@github.com>
    Co-authored-by: Marie Lucca <40550942+francinelucca@users.noreply.github.com>

---
+++ .changeset/lucky-wasps-nail.md
@@ -0,0 +1,6 @@
+---
+"@primer/react": major
+"@primer/styled-react": patch
+---
+
+Remove support for `sx` from the `TabNav` component
---
+++ packages/styled-react/src/components/TabNav.tsx
@@ -0,0 +1,32 @@
+import {TabNav as PrimerTabNav} from '@primer/react/deprecated'
+import type {TabNavProps as PrimerTabNavProps, TabNavLinkProps as PrimerTabNavLinkProps} from '@primer/react/deprecated'
+import {sx, type SxProp} from '../sx'
+import styled from 'styled-components'
+import {type ForwardRefComponent} from '../polymorphic'
+
+type TabNavProps = PrimerTabNavProps & SxProp
+type TabNavLinkProps = PrimerTabNavLinkProps & SxProp
+
+const StyledTabNav = styled(PrimerTabNav).withConfig({
+  shouldForwardProp: prop => (prop as keyof TabNavProps) !== 'sx',
+})<TabNavProps>`
+  ${sx}
+`
+
+// @ts-ignore forwardedAs is valid here but I don't know how to fix the typescript error
+const TabNavImpl = ({as, ...props}: TabNavProps) => <StyledTabNav forwardedAs={as} {...props} />
+
+const StyledTabNavLink: ForwardRefComponent<'a', TabNavLinkProps> = styled(PrimerTabNav.Link).withConfig({
+  shouldForwardProp: prop => (prop as keyof TabNavLinkProps) !== 'sx',
+})<TabNavLinkProps>`
+  ${sx}
+`
+
+// @ts-ignore forwardedAs is valid here but I don't know how to fix the typescript error
+const TabNavLink = ({as, ...props}: TabNavLinkProps) => <StyledTabNavLink forwardedAs={as} {...props} />
+
+const TabNav = Object.assign(TabNavImpl, {
+  Link: TabNavLink,
+})
+
+export {TabNav, type TabNavProps, type TabNavLinkProps}
