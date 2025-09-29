efeb72497d25e614a6551d5eec876c4ee66d8189
llastflowers <llastflowers@github.com>
2025-09-29 12:15:07 -0700

    remove BWF from ActionList/Divider.tsx

--- packages/react/src/ActionList/Divider.tsx
+++ packages/react/src/ActionList/Divider.tsx
@@ -1,22 +1,14 @@
 import type React from 'react'
 import {clsx} from 'clsx'
 import classes from './ActionList.module.css'
-import {BoxWithFallback} from '../internal/components/BoxWithFallback'
 
 export type ActionListDividerProps = {
   className?: string
 }
 
 /**
- * Visually separates `Item`s or `Group`s in an `ActionList`.
+ * Visually separates `Items or `Groups in an `ActionList`.
  */
 export const Divider: React.FC<React.PropsWithChildren<ActionListDividerProps>> = ({className}) => {
-  return (
-    <BoxWithFallback
-      className={clsx(className, classes.Divider)}
-      as="li"
-      aria-hidden="true"
-      data-component="ActionList.Divider"
-    />
-  )
+  return <li className={clsx(className, classes.Divider)} aria-hidden="true" data-component="ActionList.Divider" />
 }
