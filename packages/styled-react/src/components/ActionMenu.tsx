c1ba2b8a7cc2eacde2a5ccb429fc77836ea488aa
Marie Lucca <francinelucca@github.com>
2025-10-01 01:12:23 -0400

    remove custom ActionMenu

--- packages/styled-react/src/components/ActionMenu.tsx
+++ packages/styled-react/src/components/ActionMenu.tsx
@@ -1,13 +1,7 @@
-import {
-  Box,
-  ActionMenu as PrimerActionMenu,
-  type SxProp,
-  type ActionMenuButtonProps as PrimerActionMenuButtonProps,
-} from '@primer/react'
+import {ActionMenu as PrimerActionMenu, type SxProp} from '@primer/react'
 import {sx} from '../sx'
 import styled from 'styled-components'
-import {forwardRef, type ComponentProps} from 'react'
-import type {ForwardRefComponent} from '../polymorphic'
+import type {ComponentProps} from 'react'
 
 type ActionMenuOverlayProps = ComponentProps<typeof PrimerActionMenu.Overlay> & SxProp
 
@@ -17,23 +11,13 @@ const ActionMenuOverlay: React.ComponentType<ActionMenuOverlayProps> = styled(Pr
   ${sx}
 `
 
-export type ActionMenuButtonProps = PrimerActionMenuButtonProps & SxProp & {as?: React.ElementType}
-
-const StyledActionMenuButton = forwardRef<HTMLButtonElement, ActionMenuButtonProps>((props, ref) => {
-  return <Box as={PrimerActionMenu.Button} ref={ref} {...props} />
-})
-
-const ActionMenuButton = forwardRef(({as, ...props}: ActionMenuButtonProps, ref) => (
-  <StyledActionMenuButton {...props} {...(as ? {forwardedAs: as} : {})} ref={ref} />
-)) as ForwardRefComponent<'button', ActionMenuButtonProps>
-
 export const ActionMenu: typeof PrimerActionMenu & {
-  Button: typeof ActionMenuButton
+  Button: typeof PrimerActionMenu.Button
   Anchor: typeof PrimerActionMenu.Anchor
   Overlay: typeof ActionMenuOverlay
   Divider: typeof PrimerActionMenu.Divider
 } = Object.assign(PrimerActionMenu, {
-  Button: ActionMenuButton,
+  Button: PrimerActionMenu.Button,
   Anchor: PrimerActionMenu.Anchor,
   Overlay: ActionMenuOverlay,
   Divider: PrimerActionMenu.Divider,
