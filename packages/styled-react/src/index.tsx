7f53ab9e2fae5ff2e6bf51ee0f6e21d403f6b24f
Marie Lucca <francinelucca@github.com>
2025-09-30 21:02:14 -0400

    fix exports

--- packages/styled-react/src/index.tsx
+++ packages/styled-react/src/index.tsx
@@ -7,9 +7,7 @@ export {IconButton} from '@primer/react'
 export {ProgressBar} from '@primer/react'
 export {Select} from '@primer/react'
 export {Text} from '@primer/react'
-export {TextInput} from '@primer/react'
 export {Tooltip} from '@primer/react'
-export {type TextInputProps} from '@primer/react'
 
 // theming depends on styled-components
 export {ThemeProvider} from '@primer/react'
@@ -55,6 +53,7 @@ export {Spinner, type SpinnerProps} from './components/Spinner'
 export {StateLabel, type StateLabelProps} from './components/StateLabel'
 export {SubNav, type SubNavProps, type SubNavLinkProps} from './components/SubNav'
 export {Textarea, type TextareaProps} from './components/Textarea'
+export {TextInput, type TextInputProps, type TextInputActionProps} from './components/TextInput'
 export {
   Timeline,
   type TimelineProps,
