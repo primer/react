72ef673bf9d6b50194d326b0c9258df86b873e3c
llastflowers <llastflowers@github.com>
2025-10-01 16:07:59 -0700

    fix NavList to finish migration that ActionList relies on

--- packages/styled-react/src/components/NavList.tsx
+++ packages/styled-react/src/components/NavList.tsx
@@ -5,75 +5,70 @@ import type {
   NavListGroupProps as PrimerNavListGroupProps,
   NavListGroupHeadingProps as PrimerNavListGroupHeadingProps,
   NavListLeadingVisualProps as PrimerNavListLeadingVisualProps,
-  NavListTrailingVisualProps as PrimerNavListTrailingVisualProps,
-  NavListTrailingActionProps as PrimerNavListTrailingActionProps,
-  NavListGroupExpandProps as PrimerNavListGroupExpandProps,
 } from '@primer/react'
 import {forwardRef, type ComponentProps, type PropsWithChildren} from 'react'
 import {type SxProp} from '../sx'
 
+type RefComponent<E extends HTMLElement, P> = React.ForwardRefExoticComponent<P & React.RefAttributes<E>>
+
 type NavListProps = PropsWithChildren<PrimerNavListProps> & SxProp
-type NavListItemProps = PropsWithChildren<PrimerNavListItemProps> & SxProp
-type NavListGroupProps = PropsWithChildren<PrimerNavListGroupProps> & SxProp
-type NavListGroupHeadingProps = PropsWithChildren<PrimerNavListGroupHeadingProps> & SxProp
-type NavListLeadingVisualProps = PropsWithChildren<PrimerNavListLeadingVisualProps> & SxProp
-type NavListTrailingVisualProps = PropsWithChildren<PrimerNavListTrailingVisualProps> & SxProp
-type NavListTrailingActionProps = PropsWithChildren<PrimerNavListTrailingActionProps> & SxProp
-type NavListGroupExpandProps = PropsWithChildren<PrimerNavListGroupExpandProps> & SxProp
 
 const NavListImpl = forwardRef<HTMLElement, NavListProps>(function NavList(props, ref) {
   return <Box as={PrimerNavList} ref={ref} {...props} />
 })
 
+type NavListItemProps = PropsWithChildren<PrimerNavListItemProps> & SxProp
+
 const NavListItem = forwardRef<HTMLAnchorElement, NavListItemProps>(function NavListItem(props, ref) {
+  // @ts-expect-error - PrimerNavList.Item is not recognized as a valid component type
   return <Box as={PrimerNavList.Item} ref={ref} {...props} />
 })
 
+type NavListGroupProps = PropsWithChildren<PrimerNavListGroupProps> & SxProp
+
 const NavListGroup = forwardRef<HTMLLIElement, NavListGroupProps>(function NavListGroup(props, ref) {
+  // @ts-expect-error - PrimerNavList.Group is not recognized as a valid component type
   return <Box as={PrimerNavList.Group} ref={ref} {...props} />
 })
 
-// Add missing subcomponents with sx support
-const NavListGroupHeading = forwardRef<HTMLDivElement, NavListGroupHeadingProps>(
-  function NavListGroupHeading(props, ref) {
-    return <Box as={PrimerNavList.GroupHeading} ref={ref} {...props} />
-  },
-)
+type NavListGroupHeadingProps = PropsWithChildren<PrimerNavListGroupHeadingProps> & SxProp
+
+const NavListGroupHeading = forwardRef<HTMLElement, NavListGroupHeadingProps>(function NavListGroupHeading(props, ref) {
+  // @ts-expect-error - PrimerNavList.GroupHeading is not recognized as a valid component type
+  return <Box as={PrimerNavList.GroupHeading} ref={ref} {...props} />
+}) as RefComponent<HTMLElement, NavListGroupHeadingProps>
+
+type NavListLeadingVisualProps = PropsWithChildren<PrimerNavListLeadingVisualProps> & SxProp
 
 const NavListLeadingVisual = forwardRef<HTMLSpanElement, NavListLeadingVisualProps>(
   function NavListLeadingVisual(props, ref) {
+    // @ts-expect-error - PrimerNavList.LeadingVisual is not recognized as a valid component type
     return <Box as={PrimerNavList.LeadingVisual} ref={ref} {...props} />
   },
-)
+) as RefComponent<HTMLSpanElement, NavListLeadingVisualProps>
 
-const NavListTrailingVisual = forwardRef<HTMLSpanElement, NavListTrailingVisualProps>(
-  function NavListTrailingVisual(props, ref) {
-    return <Box as={PrimerNavList.TrailingVisual} ref={ref} {...props} />
-  },
-)
+type NavListCompound = React.ForwardRefExoticComponent<NavListProps & React.RefAttributes<HTMLElement>> & {
+  Item: typeof NavListItem
+  Group: typeof NavListGroup
+  GroupHeading: typeof NavListGroupHeading
+  LeadingVisual: typeof NavListLeadingVisual
+  SubNav: typeof PrimerNavList.SubNav
+  Divider: typeof PrimerNavList.Divider
+  TrailingVisual: typeof PrimerNavList.TrailingVisual
+  TrailingAction: typeof PrimerNavList.TrailingAction
+  GroupExpand: typeof PrimerNavList.GroupExpand
+}
 
-const NavListTrailingAction = forwardRef<HTMLButtonElement, NavListTrailingActionProps>(
-  function NavListTrailingAction(props, ref) {
-    return <Box as={PrimerNavList.TrailingAction} ref={ref} {...props} />
-  },
-)
-
-const NavListGroupExpand = forwardRef<HTMLButtonElement, NavListGroupExpandProps>(
-  function NavListGroupExpand(props, ref) {
-    return <Box as={PrimerNavList.GroupExpand} ref={ref} {...props} />
-  },
-)
-
-const NavList = Object.assign(NavListImpl, {
+const NavList: NavListCompound = Object.assign(NavListImpl, {
   Item: NavListItem,
   Group: NavListGroup,
   GroupHeading: NavListGroupHeading,
   LeadingVisual: NavListLeadingVisual,
-  TrailingVisual: NavListTrailingVisual,
-  TrailingAction: NavListTrailingAction,
-  GroupExpand: NavListGroupExpand,
-  SubNav: PrimerNavList.SubNav as React.FC<React.PropsWithChildren<ComponentProps<typeof PrimerNavList.SubNav>>>,
-  Divider: PrimerNavList.Divider as React.FC<React.PropsWithChildren<ComponentProps<typeof PrimerNavList.Divider>>>,
+  SubNav: PrimerNavList.SubNav,
+  Divider: PrimerNavList.Divider,
+  TrailingVisual: PrimerNavList.TrailingVisual,
+  TrailingAction: PrimerNavList.TrailingAction,
+  GroupExpand: PrimerNavList.GroupExpand,
 })
 
 export {NavList, type NavListProps}
