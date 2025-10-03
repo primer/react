ace1d27bd74314b45e588aa20fc62ad7ed0d8a27
Marie Lucca <francinelucca@github.com>
2025-10-03 13:29:56 -0400

    tooltip classes

--- packages/react/src/Tooltip/Tooltip.tsx
+++ packages/react/src/Tooltip/Tooltip.tsx
@@ -27,6 +27,11 @@ function Tooltip({direction = 'n', children, className, text, noDelay, align, wr
     [classes[`Tooltip--align${align === 'left' ? 'Left' : 'Right'}`]]: align,
     [classes['Tooltip--noDelay']]: noDelay,
     [classes['Tooltip--multiline']]: wrap,
+    // maintaining feature parity with old classes
+    [`tooltipped-${direction}`]: true,
+    [`tooltipped-align-${align === 'left' ? 'left' : 'right'}-2`]: align,
+    'tooltipped-no-delay': noDelay,
+    'tooltipped-multiline': wrap,
   })
 
   const value = useMemo(() => ({tooltipId}), [tooltipId])
