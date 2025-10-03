69cfa2fbb9e921c094f7e808845399543069d8fd
Liu Liu <liuliu-dev@github.com>
2025-10-03 15:48:57 -0700

    export

--- packages/styled-react/src/deprecated.tsx
+++ packages/styled-react/src/deprecated.tsx
@@ -2,3 +2,9 @@ export {TabNav, type TabNavProps, type TabNavLinkProps} from './components/depre
 export {Dialog, type DialogProps, type DialogHeaderProps} from './components/deprecated/DialogV1'
 export {Octicon} from '@primer/react/deprecated'
 export {DeprecatedTooltip as Tooltip, type DeprecatedTooltipProps as TooltipProps} from './components/Tooltip'
+export {
+  ActionList,
+  type ActionListProps,
+  type ActionListItemProps,
+  type ActionListGroupProps,
+} from './components/deprecated/ActionList'
