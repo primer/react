5cbd7ab14157726f96c88f590643b1b8470edddd
Liu Liu <liuliu-dev@github.com>
2025-10-03 12:51:47 -0700

    export in styled-react and remove sx from stories

--- packages/styled-react/src/deprecated.tsx
+++ packages/styled-react/src/deprecated.tsx
@@ -1,4 +1,4 @@
-export {TabNav, type TabNavProps, type TabNavLinkProps} from './components/TabNav'
-export {Dialog, type DialogProps, type DialogHeaderProps} from './components/DialogV1'
+export {TabNav, type TabNavProps, type TabNavLinkProps} from './components/deprecated/TabNav'
+export {Dialog, type DialogProps, type DialogHeaderProps} from './components/deprecated/DialogV1'
 export {Octicon} from '@primer/react/deprecated'
 export {DeprecatedTooltip as Tooltip, type DeprecatedTooltipProps as TooltipProps} from './components/Tooltip'
