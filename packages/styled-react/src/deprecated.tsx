31fac89722e693e018cf7537747cfc1521faf438
Marie Lucca <francinelucca@github.com>
2025-10-06 20:03:40 -0400

    move octicons to deprecated

--- packages/styled-react/src/deprecated.tsx
+++ packages/styled-react/src/deprecated.tsx
@@ -1,6 +1,6 @@
 export {TabNav, type TabNavProps, type TabNavLinkProps} from './components/deprecated/TabNav'
 export {Dialog, type DialogProps, type DialogHeaderProps} from './components/deprecated/DialogV1'
-export {Octicon, type OcticonProps} from './components/Octicon'
+export {Octicon, type OcticonProps} from './components/deprecated/Octicon'
 export {Tooltip, type TooltipProps} from './components/deprecated/Tooltip'
 export {
   ActionList,
