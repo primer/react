446956d3466ca11b95adf720d7cf07a1bb6bad92
LiuLiu <liuliu-dev@github.com>
2025-10-06 17:22:39 -0700

    Remove support for sx from the ActionList component in @primer/react/deprecated (#6944)
    Co-authored-by: Marie Lucca <francinelucca@github.com>
    Co-authored-by: Marie Lucca <40550942+francinelucca@users.noreply.github.com>
    Co-authored-by: llastflowers <llastflowers@github.com>
    Co-authored-by: Brittany L. Houtz <55068883+llastflowers@users.noreply.github.com>

--- packages/react/src/stories/deprecated/ActionList.stories.tsx
+++ packages/react/src/stories/deprecated/ActionList.stories.tsx
@@ -163,7 +163,7 @@ export function ComplexListInsetVariantStory(): JSX.Element {
             {groupId: '0'},
             {groupId: '1', header: {title: 'Live query', variant: 'filled', 'aria-level': 3}},
             {groupId: '2', header: {title: 'Layout', variant: 'subtle', 'aria-level': 3}, showItemDividers: true},
-            {groupId: '3', renderItem: props => <ActionList.Item style={{fontWeight: 'bold'}} {...props} />},
+            {groupId: '3', renderItem: props => <ActionList.Item {...props} className={classes.ActionListEmphasis} />},
             {
               groupId: '4',
               renderItem: ({leadingVisual: LeadingVisual, ...props}) => (
@@ -176,9 +176,7 @@ export function ComplexListInsetVariantStory(): JSX.Element {
                   )}
                 />
               ),
-              renderGroup: ({sx: sxProps, ...props}) => (
-                <ActionList.Group {...props} sx={{...sxProps, backgroundColor: 'cornflowerblue', color: 'white'}} />
-              ),
+              renderGroup: props => <ActionList.Group {...props} className={classes.CustomGroupStyle} />,
             },
           ]}
           items={[
@@ -194,7 +192,7 @@ export function ComplexListInsetVariantStory(): JSX.Element {
               leadingVisual: SearchIcon,
               text: 'repo:github/memex,github/github',
               groupId: '1',
-              renderItem: props => <ActionList.Item style={{color: 'rebeccapurple'}} {...props} />,
+              renderItem: props => <ActionList.Item {...props} className={classes.ActionListAccent} />,
             },
             {
               leadingVisual: NoteIcon,
@@ -237,7 +235,7 @@ export function ComplexListFullVariantStory(): JSX.Element {
             {groupId: '0'},
             {groupId: '1', header: {title: 'Live query', variant: 'filled', 'aria-level': 3}},
             {groupId: '2', header: {title: 'Layout', variant: 'subtle', 'aria-level': 3}},
-            {groupId: '3', renderItem: props => <ActionList.Item style={{fontWeight: 'bold'}} {...props} />},
+            {groupId: '3', renderItem: props => <ActionList.Item {...props} className={classes.ActionListEmphasis} />},
             {
               groupId: '4',
               renderItem: ({leadingVisual: LeadingVisual, ...props}) => (
@@ -250,9 +248,7 @@ export function ComplexListFullVariantStory(): JSX.Element {
                   )}
                 />
               ),
-              renderGroup: ({sx: sxProps, ...props}) => (
-                <ActionList.Group {...props} sx={{...sxProps, backgroundColor: 'cornflowerblue', color: 'white'}} />
-              ),
+              renderGroup: props => <ActionList.Group {...props} className={classes.CustomGroupStyle} />,
             },
           ]}
           items={[
@@ -262,7 +258,7 @@ export function ComplexListFullVariantStory(): JSX.Element {
               leadingVisual: SearchIcon,
               text: 'repo:github/memex,github/github',
               groupId: '1',
-              renderItem: props => <ActionList.Item style={{color: 'rebeccapurple'}} {...props} />,
+              renderItem: props => <ActionList.Item {...props} className={classes.ActionListAccent} />,
             },
             {
               leadingVisual: NoteIcon,
