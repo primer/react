68ea3050f51db12173a0bfedc6a8673cec61d92a
Marie Lucca <francinelucca@github.com>
2025-10-07 12:45:38 -0400

    type fix

--- packages/react/src/internal/components/CheckboxOrRadioGroup/CheckboxOrRadioGroupCaption.tsx
+++ packages/react/src/internal/components/CheckboxOrRadioGroup/CheckboxOrRadioGroupCaption.tsx
@@ -4,7 +4,7 @@ import CheckboxOrRadioGroupContext from './CheckboxOrRadioGroupContext'
 import classes from './CheckboxOrRadioGroup.module.css'
 import {clsx} from 'clsx'
 
-type CheckboxOrRadioGroupCaptionProps = React.PropsWithChildren<HTMLSpanElement> & {className?: string}
+type CheckboxOrRadioGroupCaptionProps = React.PropsWithChildren<{className?: string}>
 
 const CheckboxOrRadioGroupCaption: React.FC<CheckboxOrRadioGroupCaptionProps> = ({className, children}) => {
   const {captionId} = React.useContext(CheckboxOrRadioGroupContext)
