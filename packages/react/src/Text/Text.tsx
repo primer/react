f9fa7e89d144dd87edad747819a99a1480b96399
Marie Lucca <francinelucca@github.com>
2025-10-08 18:37:19 -0400

    text type fixes

--- packages/react/src/Text/Text.tsx
+++ packages/react/src/Text/Text.tsx
@@ -2,30 +2,40 @@ import {clsx} from 'clsx'
 import React, {forwardRef} from 'react'
 import {useRefObjectAsForwardedRef} from '../hooks'
 import classes from './Text.module.css'
-import type {ForwardRefComponent as PolymorphicForwardRefComponent} from '../utils/polymorphic'
 
-type StyledTextProps = {
+export type TextProps = {
   size?: 'large' | 'medium' | 'small'
   weight?: 'light' | 'normal' | 'medium' | 'semibold'
-  className?: string
-} & React.HTMLAttributes<HTMLElement>
+}
 
-const Text = forwardRef(({as: Component = 'span', className, size, weight, ...props}, forwardedRef) => {
-  const innerRef = React.useRef<HTMLElement>(null)
-  useRefObjectAsForwardedRef(forwardedRef, innerRef)
+interface TextComponent {
+  <T extends React.ElementType = 'span'>(
+    props: {
+      as?: T
+    } & TextProps &
+      Omit<React.ComponentPropsWithoutRef<T>, keyof TextProps>,
+  ): React.ReactElement | null
 
-  return (
-    <Component
-      className={clsx(className, classes.Text)}
-      data-size={size}
-      data-weight={weight}
-      {...props}
-      ref={innerRef}
-    />
-  )
-}) as PolymorphicForwardRefComponent<'span', StyledTextProps>
+  displayName?: string
+}
+
+const Text = forwardRef<HTMLElement, {as?: React.ElementType} & TextProps & React.HTMLAttributes<HTMLElement>>(
+  ({as: Component = 'span', className, size, weight, ...props}, forwardedRef) => {
+    const innerRef = React.useRef<HTMLElement>(null)
+    useRefObjectAsForwardedRef(forwardedRef, innerRef)
+
+    return (
+      <Component
+        className={clsx(className, classes.Text)}
+        data-size={size}
+        data-weight={weight}
+        {...props}
+        ref={innerRef}
+      />
+    )
+  },
+) as TextComponent
 
 Text.displayName = 'Text'
 
-export type TextProps = StyledTextProps
 export default Text
