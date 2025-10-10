9ea6440daa3cd05c82c351f5e3515ee1728b4d39
Marie Lucca <40550942+francinelucca@users.noreply.github.com>
2025-10-10 00:37:04 +0000

    add optional chaining

--- packages/react/src/utils/get-slot-name.ts
+++ packages/react/src/utils/get-slot-name.ts
@@ -3,5 +3,5 @@ import type React from 'react'
 
 export function getSlotName(element: React.ReactElement | ReactNode) {
   // @ts-ignore - TypeScript doesn't know about the __SLOT__ prop
-  return element['__SLOT__'] ?? element.type['__SLOT__']
+  return element['__SLOT__'] ?? element.type?.['__SLOT__']
 }
