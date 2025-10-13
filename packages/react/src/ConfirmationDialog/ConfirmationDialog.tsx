22e023334e40a8bac0f226981a2ea1f5ef9fb8ae
Siddharth Kshetrapal <siddharthkp@github.com>
2025-10-13 23:27:44 +0200

    remove Theme from ConfirmationDialog

--- packages/react/src/ConfirmationDialog/ConfirmationDialog.tsx
+++ packages/react/src/ConfirmationDialog/ConfirmationDialog.tsx
@@ -1,13 +1,10 @@
 import type React from 'react'
 import {useCallback} from 'react'
 import {createRoot} from 'react-dom/client'
-import type {ThemeProviderProps} from '../ThemeProvider'
-import {ThemeProvider, useTheme} from '../ThemeProvider'
 import {FocusKeys} from '@primer/behaviors'
 import type {DialogProps, DialogHeaderProps, DialogButtonProps, DialogWidth, DialogHeight} from '../Dialog/Dialog'
 import {Dialog} from '../Dialog/Dialog'
 import {useFocusZone} from '../hooks/useFocusZone'
-import BaseStyles from '../BaseStyles'
 import classes from './ConfirmationDialog.module.css'
 import Heading from '../Heading'
 
@@ -181,7 +178,7 @@ export const ConfirmationDialog: React.FC<React.PropsWithChildren<ConfirmationDi
 
 let hostElement: Element | null = null
 export type ConfirmOptions = Omit<ConfirmationDialogProps, 'onClose'> & {content: React.ReactNode}
-async function confirm(themeProps: ThemeProviderProps, options: ConfirmOptions): Promise<boolean> {
+async function confirm(options: ConfirmOptions): Promise<boolean> {
   const {content, ...confirmationDialogProps} = options
   return new Promise(resolve => {
     hostElement ||= document.createElement('div')
@@ -196,13 +193,9 @@ async function confirm(themeProps: ThemeProviderProps, options: ConfirmOptions):
       }
     }
     root.render(
-      <ThemeProvider {...themeProps}>
-        <BaseStyles>
-          <ConfirmationDialog {...confirmationDialogProps} onClose={onClose}>
-            {content}
-          </ConfirmationDialog>
-        </BaseStyles>
-      </ThemeProvider>,
+      <ConfirmationDialog {...confirmationDialogProps} onClose={onClose}>
+        {content}
+      </ConfirmationDialog>,
     )
   })
 }
@@ -213,13 +206,8 @@ async function confirm(themeProps: ThemeProviderProps, options: ConfirmOptions):
  * resolved with `true` or `false` depending on whether or not the confirm button was used.
  */
 export function useConfirm() {
-  const {theme, colorMode, dayScheme, nightScheme} = useTheme()
-  const result = useCallback(
-    (options: ConfirmOptions) => {
-      const themeProps: ThemeProviderProps = {theme, colorMode, dayScheme, nightScheme}
-      return confirm(themeProps, options)
-    },
-    [theme, colorMode, dayScheme, nightScheme],
-  )
+  const result = (options: ConfirmOptions) => {
+    return confirm(options)
+  }
   return result
 }
