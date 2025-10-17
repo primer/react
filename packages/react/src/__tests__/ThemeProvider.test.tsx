925ad864aed95c11b059a3b330b90b17bc000959
Siddharth Kshetrapal <siddharthkp@github.com>
2025-10-17 02:03:08 +0200

    fix imports for tests

--- packages/react/src/__tests__/ThemeProvider.test.tsx
+++ packages/react/src/__tests__/ThemeProvider.test.tsx
@@ -2,7 +2,7 @@ import {render, screen, waitFor} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {describe, expect, it, vi} from 'vitest'
 import React from 'react'
-import {ThemeProvider, useColorSchemeVar, useTheme} from '../'
+import {ThemeProvider, useColorSchemeVar, useTheme} from '../ThemeProvider'
 import Box from '../Box'
 
 // window.matchMedia() is not implemented by JSDOM so we have to create a mock:
