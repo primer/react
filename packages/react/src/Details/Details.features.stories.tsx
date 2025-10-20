75a4d5cb622e149f0dc38aaca09cd98af711d006
Hector Garcia <hectahertz@github.com>
2025-10-20 11:46:51 +0000

    Separate the story to a new file

--- packages/react/src/Details/Details.features.stories.tsx
+++ packages/react/src/Details/Details.features.stories.tsx
@@ -1,17 +1,17 @@
 import type {StoryFn, Meta} from '@storybook/react-vite'
 import Details from './Details'
-import {Button} from '../Button'
 import useDetails from '../hooks/useDetails'
 
 export default {
-  title: 'Components/Details',
+  title: 'Components/Details/Features',
   component: Details,
 } as Meta<typeof Details>
-export const Default: StoryFn<typeof Details> = () => {
+
+export const WithCustomSummary: StoryFn<typeof Details> = () => {
   const {getDetailsProps} = useDetails({closeOnOutsideClick: true})
   return (
     <Details {...getDetailsProps()}>
-      <Details.Summary as={Button}>See Details</Details.Summary>
+      <summary>Custom see Details</summary>
       This is some content
     </Details>
   )
