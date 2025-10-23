9b9c23cecc19bf146403f0dfcbe22c04bf6553ee
Marie Lucca <francinelucca@github.com>
2025-10-22 23:15:11 -0400

    chore(Dialog): allow direct children

--- packages/styled-react/src/components/Dialog.tsx
+++ packages/styled-react/src/components/Dialog.tsx
@@ -1,5 +1,5 @@
 import {Dialog as PrimerDialog} from '@primer/react'
-import type {DialogProps as PrimerDialogProps} from '@primer/react'
+import type {DialogProps as PrimerDialogProps, SlotMarker} from '@primer/react'
 import {Box} from './Box'
 import type {SxProp} from '../sx'
 import {forwardRef, type ComponentPropsWithoutRef, type PropsWithChildren} from 'react'
@@ -14,6 +14,7 @@ const DialogImpl = forwardRef<HTMLDivElement, DialogProps>(function Dialog(props
 type DialogHeaderProps = ComponentPropsWithoutRef<'div'> & SxProp
 
 const DialogHeader = forwardRef<HTMLDivElement, DialogHeaderProps>(function DialogHeader(props, ref) {
+  // @ts-expect-error - PrimerDialog.Header is not recognized as a valid component type
   return <Box as={PrimerDialog.Header} ref={ref} {...props} />
 })
 
@@ -27,10 +28,16 @@ const DialogBody = forwardRef<HTMLDivElement, StyledBodyProps>(function DialogBo
 type StyledFooterProps = React.ComponentProps<'div'> & SxProp
 
 const DialogFooter = forwardRef<HTMLDivElement, StyledFooterProps>(function DialogFooter(props, ref) {
+  // @ts-expect-error - PrimerDialog.Footer is not recognized as a valid component type
   return <Box as={PrimerDialog.Footer} ref={ref} {...props} />
 })
 
+;(DialogHeader as typeof DialogHeader & SlotMarker).__SLOT__ = PrimerDialog.Header.__SLOT__
+;(DialogBody as typeof DialogBody & SlotMarker).__SLOT__ = PrimerDialog.Body.__SLOT__
+;(DialogFooter as typeof DialogFooter & SlotMarker).__SLOT__ = PrimerDialog.Footer.__SLOT__
+
 const Dialog = Object.assign(DialogImpl, {
+  __SLOT__: PrimerDialog['__SLOT__'],
   Buttons: PrimerDialog.Buttons,
   Header: DialogHeader,
   Body: DialogBody,
