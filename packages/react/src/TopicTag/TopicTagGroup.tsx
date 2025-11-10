eb58ced6a5f158fda9961c6a274ed4462e93e87c
Josh Black <joshblack@github.com>
2025-11-10 13:41:02 -0600

    chore: clean up types for TopicTagGroup

--- packages/react/src/TopicTag/TopicTagGroup.tsx
+++ packages/react/src/TopicTag/TopicTagGroup.tsx
@@ -2,9 +2,7 @@ import {clsx} from 'clsx'
 import type React from 'react'
 import classes from './TopicTagGroup.module.css'
 
-type TopicTagGroupProps = React.HTMLAttributes<HTMLElement> & {
-  //
-}
+type TopicTagGroupProps = React.HTMLAttributes<HTMLElement>
 
 function TopicTagGroup({children, ...rest}: TopicTagGroupProps) {
   return (
