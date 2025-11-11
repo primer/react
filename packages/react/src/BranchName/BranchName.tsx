e63bf3111699126d54363193d39f3143573ba35b
Lukas Oppermann <lukasoppermann@github.com>
2025-11-11 09:35:21 +0100

    update type for branchname

--- packages/react/src/BranchName/BranchName.tsx
+++ packages/react/src/BranchName/BranchName.tsx
@@ -4,16 +4,17 @@ import {clsx} from 'clsx'
 import classes from './BranchName.module.css'
 import {fixedForwardRef, type PolymorphicProps} from '../utils/modern-polymorphic'
 
-export type BranchNameProps<As extends React.ElementType> = PolymorphicProps<
+export type BranchNameProps<As extends React.ElementType = 'a'> = PolymorphicProps<
   As,
   'a',
   {
     className?: string
   }
->
+> &
+  (As extends 'a' ? {href: string} : {href?: never})
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function BranchName<As extends React.ElementType>(props: BranchNameProps<As>, ref: ForwardedRef<any>) {
+function BranchName<As extends React.ElementType = 'a'>(props: BranchNameProps<As>, ref: ForwardedRef<any>) {
   const {as: Component = 'a', className, children, ...rest} = props
   return (
     <Component {...rest} ref={ref} className={clsx(className, classes.BranchName)}>
