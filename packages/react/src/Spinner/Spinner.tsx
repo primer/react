19975e0133725e7fbd6ce1b6cbae599517646473
Josh Black <joshblack@github.com>
2025-12-08 13:21:47 -0600

    refactor(Spinner): update animation to reuse existing css for startTime

--- packages/react/src/Spinner/Spinner.tsx
+++ packages/react/src/Spinner/Spinner.tsx
@@ -1,5 +1,6 @@
+import {clsx} from 'clsx'
 import type React from 'react'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useSyncExternalStore} from 'react'
 import {VisuallyHidden} from '../VisuallyHidden'
 import type {HTMLDataAttributes} from '../internal/internal-types'
 import {useId} from '../hooks'
@@ -35,7 +36,7 @@ function Spinner({
   delay = false,
   ...props
 }: SpinnerProps) {
-  const animation = useSpinnerAnimation()
+  const animationRef = useSpinnerAnimation()
   const size = sizeMap[sizeKey]
   const hasHiddenLabel = srText !== null && ariaLabel === undefined
   const labelId = useId()
@@ -60,7 +61,7 @@ function Spinner({
     /* inline-flex removes the extra line height */
     <span className={classes.Box}>
       <svg
-        ref={animation}
+        ref={animationRef}
         height={size}
         width={size}
         viewBox="0 0 16 16"
@@ -68,7 +69,7 @@ function Spinner({
         aria-hidden
         aria-label={ariaLabel ?? undefined}
         aria-labelledby={hasHiddenLabel ? labelId : undefined}
-        className={className}
+        className={clsx(className, classes.SpinnerAnimation)}
         style={style}
         {...props}
       >
@@ -96,6 +97,61 @@ function Spinner({
 
 Spinner.displayName = 'Spinner'
 
+type Subscriber = () => void
+
+type AnimationTimingValue = {
+  startTime: CSSNumberish | null
+}
+
+type AnimationTimingStore = {
+  subscribers: Set<Subscriber>
+  value: AnimationTimingValue
+  update(startTime: CSSNumberish): void
+  subscribe(subscriber: Subscriber): () => void
+  getSnapshot(): AnimationTimingValue
+  getServerSnapshot(): AnimationTimingValue
+}
+
+const animationTimingStore: AnimationTimingStore = {
+  subscribers: new Set<() => void>(),
+  value: {
+    startTime: null,
+  },
+  update(startTime) {
+    const value = {
+      startTime,
+    }
+    animationTimingStore.value = value
+    for (const subscriber of animationTimingStore.subscribers) {
+      subscriber()
+    }
+  },
+  subscribe(subscriber) {
+    animationTimingStore.subscribers.add(subscriber)
+    return () => {
+      animationTimingStore.subscribers.delete(subscriber)
+    }
+  },
+  getSnapshot() {
+    return animationTimingStore.value
+  },
+  getServerSnapshot() {
+    return animationTimingStore.value
+  },
+}
+
+/**
+ * A utility hook for reading a common `startTime` value so that all animations
+ * are in sync. This is a global value and is coordinated through `useSyncExternalStore`.
+ */
+function useAnimationTiming() {
+  return useSyncExternalStore(
+    animationTimingStore.subscribe,
+    animationTimingStore.getSnapshot,
+    animationTimingStore.getServerSnapshot,
+  )
+}
+
 /**
  * Uses a technique from Spectrum to coordinate animations:
  * @see https://github.com/adobe/react-spectrum/blob/ab5e6f3dba4235dafab9f81f8b5c506ce5f11230/packages/%40react-spectrum/s2/src/Skeleton.tsx#L21
@@ -103,6 +159,7 @@ Spinner.displayName = 'Spinner'
 function useSpinnerAnimation() {
   const ref = useRef<Animation | null>(null)
   const noMotionPreference = useMedia('(prefers-reduced-motion: no-preference)', false)
+  const animationTiming = useAnimationTiming()
   return useCallback(
     (element: HTMLElement | SVGSVGElement | null) => {
       if (!element) {
@@ -114,6 +171,16 @@ function useSpinnerAnimation() {
       }
 
       if (noMotionPreference) {
+        const cssAnimation = element.getAnimations().find((animation): animation is CSSAnimation => {
+          if (animation instanceof CSSAnimation) {
+            return animation.animationName.startsWith('Spinner') && animation.animationName.endsWith('rotate-keyframes')
+          }
+          return false
+        })
+        // If we can find a CSS Animation, pause it and we will use the Web
+        // Animations API to pick up from where it left off
+        cssAnimation?.pause()
+
         ref.current = element.animate(
           [
             {
@@ -126,19 +193,30 @@ function useSpinnerAnimation() {
           {
             // var(--base-duration-1000)
             duration: 1000,
-            iterations: Infinity,
             // var(--base-easing-linear)
             easing: 'cubic-bezier(0,0,1,1)',
+            iterations: Infinity,
           },
         )
 
-        // Used to sync different animations. When all animations have the same
-        // startTime they will be in sync.
-        // @see https://developer.mozilla.org/en-US/docs/Web/API/Animation/startTime#syncing_different_animations
-        ref.current.startTime = 0
+        // When the `startTime` value from `animationTimingStore` is `null` we
+        // are currently hydrating on the client. In this case, the first
+        // spinner to mount will set the `startTime` for all other spinners.
+        if (animationTiming.startTime === null) {
+          const startTime = cssAnimation?.startTime ?? 0
+
+          animationTimingStore.update(startTime)
+
+          // We use `startTime` to sync different animations. When all animations
+          // have the same startTime they will be in sync.
+          // @see https://developer.mozilla.org/en-US/docs/Web/API/Animation/startTime#syncing_different_animations
+          ref.current.startTime = startTime
+        } else {
+          ref.current.startTime = animationTiming.startTime
+        }
       }
     },
-    [noMotionPreference],
+    [noMotionPreference, animationTiming],
   )
 }
 
