a002443bebb0b9da818712b2fb77bc9ce735f69a
Josh Black <joshblack@github.com>
2025-12-10 15:22:56 -0600

    refactor(react): use rolldown over rollup

--- packages/react/rolldown.config.mjs
+++ packages/react/rolldown.config.mjs
@@ -1,10 +1,8 @@
 import path from 'node:path'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
-import {importCSS} from 'rollup-plugin-import-css'
+import {defineConfig} from 'rolldown'
+import {importCSS} from 'rolldown-plugin-import-css'
 import postcssPresetPrimer from 'postcss-preset-primer'
-import MagicString from 'magic-string'
 import {isSupported} from './script/react-compiler.mjs'
 import packageJson from './package.json' with {type: 'json'}
 
@@ -94,106 +92,11 @@ const baseConfig = {
         ],
       ],
     }),
-    resolve({
-      extensions,
-    }),
-    commonjs({
-      extensions,
-    }),
     importCSS({
       modulesRoot: 'src',
       postcssPlugins: [postcssPresetPrimer()],
       postcssModulesOptions,
     }),
-
-    /**
-     * This custom rollup plugin allows us to preserve directives in source
-     * code, such as "use client", in order to support React Server Components.
-     *
-     * The source for this plugin is inspired by:
-     * https://github.com/Ephem/rollup-plugin-preserve-directives
-     */
-    {
-      name: 'preserve-directives',
-      transform(code) {
-        const ast = this.parse(code)
-        if (ast.type !== 'Program' || !ast.body) {
-          return {
-            code,
-            ast,
-            map: null,
-          }
-        }
-
-        let hasClientDirective = false
-
-        for (const node of ast.body) {
-          if (!node) {
-            continue
-          }
-
-          if (node.type !== 'ExpressionStatement') {
-            continue
-          }
-
-          if (node.directive === 'use client') {
-            hasClientDirective = true
-            break
-          }
-        }
-
-        if (hasClientDirective) {
-          return {
-            code,
-            ast,
-            map: null,
-            meta: {
-              hasClientDirective: true,
-            },
-          }
-        }
-
-        return {
-          code,
-          ast,
-          map: null,
-        }
-      },
-      renderChunk: {
-        order: 'post',
-        handler(code, chunk, options) {
-          // If `preserveModules` is not set to true, we can't be sure if the client
-          // directive corresponds to the whole chunk or just a part of it.
-          if (!options.preserveModules) {
-            return undefined
-          }
-
-          let chunkHasClientDirective = false
-
-          for (const moduleId of Object.keys(chunk.modules)) {
-            const hasClientDirective = this.getModuleInfo(moduleId)?.meta?.hasClientDirective
-            if (hasClientDirective) {
-              chunkHasClientDirective = true
-              break
-            }
-          }
-
-          if (chunkHasClientDirective) {
-            const transformed = new MagicString(code)
-            transformed.prepend(`"use client";\n`)
-            const sourcemap = transformed.generateMap({
-              includeContent: true,
-            })
-            return {
-              code: transformed.toString(),
-              map: sourcemap,
-            }
-          }
-
-          return null
-        },
-      },
-    },
   ],
   onwarn(warning, defaultHandler) {
     // Dependencies or modules may use "use client" as an indicator for React
@@ -210,17 +113,13 @@ const baseConfig = {
   },
 }
 
-export default [
-  // ESM
-  {
-    ...baseConfig,
-    external: dependencies.map(createPackageRegex),
-    output: {
-      interop: 'auto',
-      dir: 'dist',
-      format: 'esm',
-      preserveModules: true,
-      preserveModulesRoot: 'src',
-    },
+export default defineConfig({
+  ...baseConfig,
+  external: dependencies.map(createPackageRegex),
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
   },
-]
+})
