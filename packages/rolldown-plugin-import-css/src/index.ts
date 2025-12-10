1dbef544af73a06e80c294b9e24da5639ed6bc4f
Josh Black <joshblack@github.com>
2025-12-10 15:20:12 -0600

    refactor(rolldown-plugin): update to new syntax for better rust compat

--- packages/rolldown-plugin-import-css/src/index.ts
+++ packages/rolldown-plugin-import-css/src/index.ts
@@ -31,106 +31,115 @@ export function importCSS(options: ImportCSSOptions): Plugin {
 
   return {
     name: 'import-css',
-    resolveId(source, importer) {
-      if (!importer) {
-        return
-      }
-
-      const moduleInfo = this.getModuleInfo(importer)
-      if (moduleInfo?.meta['import-css']?.source === source) {
-        return {
-          id: source,
-          external: true,
-          moduleSideEffects: true,
+    resolveId: {
+      filter: {
+        id: /\.css$/,
+      },
+      handler(source, importer) {
+        if (!importer) {
+          return
         }
-      }
 
-      if (source.endsWith('.css')) {
-        const id = path.resolve(path.dirname(importer), source)
-        return path.format({
-          dir: path.dirname(id),
-          base: `${path.basename(id)}.js`,
-        })
-      }
-    },
-    async load(id) {
-      if (id.endsWith('.css.js')) {
-        return ''
-      }
-    },
-    async transform(_code, id) {
-      if (!id.endsWith('.css.js')) {
-        return
-      }
-
-      const sourceId = path.join(path.dirname(id), path.basename(id, '.js'))
-      const code = await fs.readFile(sourceId, 'utf8')
-      const hash = getSourceHash(code)
-      const relativePath = path.relative(rootDirectory, sourceId)
-      const name = path.basename(relativePath, relativePath.endsWith('.module.css') ? '.module.css' : '.css')
-      const fileName = path.join(
-        path.dirname(relativePath),
-        path.format({
-          name: `${name}-${hash}`,
-          ext: '.css',
-        }),
-      )
-
-      // When transforming CSS modules, we want to emit the generated CSS as an
-      // asset and include the generated file in our generated CSS Modules file
-      // which contains the classes. This makes sure that if the file containing
-      // the classes is used, then the associated styles for those classes is
-      // also included
-
-      let cssModuleClasses: {[name: string]: string} | null = null
-      const result = await postcss([
-        ...postcssPlugins,
-        postcssModules({
-          ...postcssModulesOptions,
-          getJSON(filename, json) {
-            if (postcssModulesOptions.getJSON) {
-              postcssModulesOptions.getJSON(filename, json)
-            }
-            cssModuleClasses = json
-          },
-        }),
-      ]).process(code, {
-        from: id,
-        to: fileName,
-        map: {
-          inline: false,
-        },
-      })
-
-      this.emitFile({
-        type: 'asset',
-        source: result.css,
-        fileName,
-      })
-      this.emitFile({
-        type: 'asset',
-        source: result.map.toString(),
-        fileName: `${fileName}.map`,
-      })
-
-      const moduleInfo = this.getModuleInfo(id)
-      const cssSource = `./${path.basename(fileName)}`
-      if (moduleInfo) {
-        moduleInfo.meta['import-css'] = {
-          source: cssSource,
-        }
-      }
-
-      return {
-        code: `
-          import '${cssSource}';
-          ${
-            // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-            cssModuleClasses !== null ? `export default ${JSON.stringify(cssModuleClasses)}` : ''
+        const moduleInfo = this.getModuleInfo(importer)
+        if (moduleInfo?.meta['import-css']?.source === source) {
+          return {
+            id: source,
+            external: true,
+            moduleSideEffects: true,
           }
-        `,
-        moduleSideEffects: 'no-treeshake',
-      }
+        }
+
+        if (source.endsWith('.css')) {
+          const id = path.resolve(path.dirname(importer), source)
+          return path.format({
+            dir: path.dirname(id),
+            base: `${path.basename(id)}.js`,
+          })
+        }
+      },
+    },
+    load: {
+      filter: {
+        id: /\.css.js$/,
+      },
+      handler() {
+        return ''
+      },
+    },
+    transform: {
+      filter: {
+        id: /\.module.css.js$/,
+      },
+      async handler(_code, id) {
+        const sourceId = path.join(path.dirname(id), path.basename(id, '.js'))
+        const code = await fs.readFile(sourceId, 'utf8')
+        const hash = getSourceHash(code)
+        const relativePath = path.relative(rootDirectory, sourceId)
+        const name = path.basename(relativePath, relativePath.endsWith('.module.css') ? '.module.css' : '.css')
+        const fileName = path.join(
+          path.dirname(relativePath),
+          path.format({
+            name: `${name}-${hash}`,
+            ext: '.css',
+          }),
+        )
+
+        // When transforming CSS modules, we want to emit the generated CSS as an
+        // asset and include the generated file in our generated CSS Modules file
+        // which contains the classes. This makes sure that if the file containing
+        // the classes is used, then the associated styles for those classes is
+        // also included
+
+        let cssModuleClasses: {[name: string]: string} | null = null
+        const result = await postcss([
+          ...postcssPlugins,
+          postcssModules({
+            ...postcssModulesOptions,
+            getJSON(filename, json) {
+              if (postcssModulesOptions.getJSON) {
+                postcssModulesOptions.getJSON(filename, json)
+              }
+              cssModuleClasses = json
+            },
+          }),
+        ]).process(code, {
+          from: id,
+          to: fileName,
+          map: {
+            inline: false,
+          },
+        })
+
+        this.emitFile({
+          type: 'asset',
+          source: result.css,
+          fileName,
+        })
+        this.emitFile({
+          type: 'asset',
+          source: result.map.toString(),
+          fileName: `${fileName}.map`,
+        })
+
+        const moduleInfo = this.getModuleInfo(id)
+        const cssSource = `./${path.basename(fileName)}`
+        if (moduleInfo) {
+          moduleInfo.meta['import-css'] = {
+            source: cssSource,
+          }
+        }
+
+        return {
+          code: `
+          import '${cssSource}';
+            ${
+              // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+              cssModuleClasses !== null ? `export default ${JSON.stringify(cssModuleClasses)}` : ''
+            }
+          `,
+          moduleSideEffects: 'no-treeshake',
+        }
+      },
     },
   }
 }
