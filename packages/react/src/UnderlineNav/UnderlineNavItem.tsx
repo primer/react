ab2806cb93396fc4b774e5fcd582b42b648b0cc2
Ian Sanders <iansan5653@github.com>
2026-02-06 20:40:24 +0000

    Remove unecessary memo

--- packages/react/src/UnderlineNav/UnderlineNavItem.tsx
+++ packages/react/src/UnderlineNav/UnderlineNavItem.tsx
@@ -1,5 +1,5 @@
 import type {RefObject} from 'react'
-import React, {forwardRef, useRef, useContext, memo, useEffect, useId, useState} from 'react'
+import React, {forwardRef, useRef, useContext, useEffect, useId, useState} from 'react'
 import type {IconProps} from '@primer/octicons-react'
 import type {ForwardRefComponent as PolymorphicForwardRefComponent} from '../utils/polymorphic'
 import {UnderlineNavContext} from './UnderlineNavContext'
@@ -59,82 +59,80 @@ export type UnderlineNavItemProps = {
   counter?: number | string
 } & LinkProps
 
-export const UnderlineNavItem = memo(
-  forwardRef((allProps, forwardedRef) => {
-    const {
-      as: Component = 'a',
-      href = '#',
-      children,
-      counter,
-      onSelect,
-      'aria-current': ariaCurrent,
-      icon: Icon,
-      leadingVisual,
-      ...props
-    } = allProps
+export const UnderlineNavItem = forwardRef((allProps, forwardedRef) => {
+  const {
+    as: Component = 'a',
+    href = '#',
+    children,
+    counter,
+    onSelect,
+    'aria-current': ariaCurrent,
+    icon: Icon,
+    leadingVisual,
+    ...props
+  } = allProps
 
-    const backupRef = useRef<HTMLElement>(null)
-    const ref = (forwardedRef ?? backupRef) as RefObject<HTMLAnchorElement>
-    const {loadingCounters, containerWidth, registerItem, unregisterItem} = useContext(UnderlineNavContext)
+  const backupRef = useRef<HTMLElement>(null)
+  const ref = (forwardedRef ?? backupRef) as RefObject<HTMLAnchorElement>
+  const {loadingCounters, containerWidth, registerItem, unregisterItem} = useContext(UnderlineNavContext)
 
-    const id = useId()
-    const [isOverflowing, setIsOverflowing] = useState(false)
+  const id = useId()
+  const [isOverflowing, setIsOverflowing] = useState(false)
 
-    useLayoutEffect(() => {
-      if (ref.current) {
-        // Overflowing items wrap onto subsequent lines, so their `offsetTop` increases
-        const isOverflowing = ref.current.offsetTop > 0
-        setIsOverflowing(isOverflowing)
+  useLayoutEffect(() => {
+    if (ref.current) {
+      // Overflowing items wrap onto subsequent lines, so their `offsetTop` increases
+      const isOverflowing = ref.current.offsetTop > 0
+      setIsOverflowing(isOverflowing)
 
-        // Even if an item is not overflowing, it still needs to register itself to claim it's place in the registry.
-        // This preserves order - otherwise, items that overflow first would appear first in the menu.
-        registerItem(id, isOverflowing ? allProps : null)
+      // Even if an item is not overflowing, it still needs to register itself to claim it's place in the registry.
+      // This preserves order - otherwise, items that overflow first would appear first in the menu.
+      registerItem(id, isOverflowing ? allProps : null)
+    }
+
+    // To preserve the item's spot in the registry, we don't unregister until we actually dismount the component.
+  }, [ref, containerWidth, registerItem, id, allProps])
+
+  // Unregister only on dismount:
+  useEffect(() => () => unregisterItem(id), [id, unregisterItem])
+
+  const keyDownHandler = React.useCallback(
+    (event: React.KeyboardEvent<HTMLAnchorElement>) => {
+      if ((event.key === ' ' || event.key === 'Enter') && !event.defaultPrevented && typeof onSelect === 'function') {
+        onSelect(event)
       }
+    },
+    [onSelect],
+  )
+  const clickHandler = React.useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement>) => {
+      if (!event.defaultPrevented && typeof onSelect === 'function') {
+        onSelect(event)
+      }
+    },
+    [onSelect],
+  )
 
-      // To preserve the item's spot in the registry, we don't unregister until we actually dismount the component.
-    }, [ref, containerWidth, registerItem, id, allProps])
-
-    // Unregister only on dismount:
-    useEffect(() => () => unregisterItem(id), [id, unregisterItem])
-
-    const keyDownHandler = React.useCallback(
-      (event: React.KeyboardEvent<HTMLAnchorElement>) => {
-        if ((event.key === ' ' || event.key === 'Enter') && !event.defaultPrevented && typeof onSelect === 'function') {
-          onSelect(event)
-        }
-      },
-      [onSelect],
-    )
-    const clickHandler = React.useCallback(
-      (event: React.MouseEvent<HTMLAnchorElement>) => {
-        if (!event.defaultPrevented && typeof onSelect === 'function') {
-          onSelect(event)
-        }
-      },
-      [onSelect],
-    )
-
-    return (
-      <li className={classes.UnderlineNavItem}>
-        <UnderlineItem
-          ref={ref}
-          as={Component}
-          href={href}
-          aria-current={ariaCurrent}
-          onKeyDown={keyDownHandler}
-          onClick={clickHandler}
-          counter={counter}
-          icon={leadingVisual ?? Icon}
-          loadingCounters={loadingCounters}
-          {...props}
-          aria-hidden={isOverflowing ? true : allProps['aria-hidden']}
-          tabIndex={isOverflowing ? -1 : allProps.tabIndex}
-        >
-          {children}
-        </UnderlineItem>
-      </li>
-    )
-  }) as PolymorphicForwardRefComponent<'a', UnderlineNavItemProps>,
-)
+  return (
+    <li className={classes.UnderlineNavItem}>
+      <UnderlineItem
+        ref={ref}
+        as={Component}
+        href={href}
+        aria-current={ariaCurrent}
+        onKeyDown={keyDownHandler}
+        onClick={clickHandler}
+        counter={counter}
+        icon={leadingVisual ?? Icon}
+        loadingCounters={loadingCounters}
+        {...props}
+        aria-hidden={isOverflowing ? true : allProps['aria-hidden']}
+        tabIndex={isOverflowing ? -1 : allProps.tabIndex}
+      >
+        {children}
+      </UnderlineItem>
+    </li>
+  )
+}) as PolymorphicForwardRefComponent<'a', UnderlineNavItemProps>
 
 UnderlineNavItem.displayName = 'UnderlineNavItem'
