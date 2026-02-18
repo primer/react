399300c4436d4d6a198fc22f51b0f1b9496114d6
Ian Sanders <iansan5653@github.com>
2026-02-18 20:39:22 +0000

    Migrate styles to CSS

--- packages/react/src/UnderlineNav/styles.ts
+++
@@ -1,8 +0,0 @@
-export const dividerStyles = {
-  display: 'inline-block',
-  borderLeft: '1px solid',
-  width: '1px',
-  borderLeftColor: 'var(--borderColor-muted)',
-  marginRight: 'var(--base-size-4)',
-  height: '24px', // The height of the divider - reference from Figma
-}
