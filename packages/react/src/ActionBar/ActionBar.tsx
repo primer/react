2aec372321afc07533cc9675e4028a55fe10b7f9
Ian Sanders <iansan5653@github.com>
2026-02-23 21:50:22 +0000

    Add "descendant registry" pattern

--- packages/react/src/ActionBar/ActionBar.tsx
+++ packages/react/src/ActionBar/ActionBar.tsx
@@ -13,6 +13,7 @@ import {useFocusZone, FocusKeys} from '../hooks/useFocusZone'
 import styles from './ActionBar.module.css'
 import {clsx} from 'clsx'
 import {useRefObjectAsForwardedRef} from '../hooks'
+import {createDescendantRegistry} from '../utils/descendant-registry'
 
 const ACTIONBAR_ITEM_GAP = 8
 
@@ -44,14 +45,10 @@ type ChildRegistry = ReadonlyMap<string, ChildProps | null>
 
 const ActionBarContext = React.createContext<{
   size: Size
-  registerChild: (id: string, props: ChildProps) => void
-  unregisterChild: (id: string, groupId?: string) => void
   isVisibleChild: (id: string) => boolean
   groupId?: string
 }>({
   size: 'medium',
-  registerChild: () => {},
-  unregisterChild: () => {},
   isVisibleChild: () => true,
   groupId: undefined,
 })
@@ -161,6 +158,8 @@ export type ActionBarMenuProps = {
 } & IconButtonProps
 
 const MORE_BTN_WIDTH = 32
+
+const ActionBarItemsRegistry = createDescendantRegistry<ChildProps>()
 
 const calculatePossibleItems = (
   registryEntries: Array<[string, ChildProps]>,
@@ -301,13 +300,7 @@ export const ActionBar: React.FC<React.PropsWithChildren<ActionBarProps>> = prop
   const listRef = useRef<HTMLDivElement>(null)
   const [computedGap, setComputedGap] = useState<number>(ACTIONBAR_ITEM_GAP)
 
-  const [childRegistry, setChildRegistry] = useState<ChildRegistry>(() => new Map())
-
-  const registerChild = useCallback(
-    (id: string, childProps: ChildProps) => setChildRegistry(prev => new Map(prev).set(id, childProps)),
-    [],
-  )
-  const unregisterChild = useCallback((id: string) => setChildRegistry(prev => new Map(prev).set(id, null)), [])
+  const [childRegistry, setChildRegistry] = ActionBarItemsRegistry.useRegistryState()
 
   const [menuItemIds, setMenuItemIds] = useState<Set<string>>(() => new Set())
 
@@ -349,7 +342,7 @@ export const ActionBar: React.FC<React.PropsWithChildren<ActionBarProps>> = prop
     const groupedItemsMap = new Map<string, Array<[string, ChildProps]>>()
 
     for (const [key, childProps] of childRegistry) {
-      if (childProps?.type === 'action' && childProps.groupId) {
+      if (childProps.type === 'action' && childProps.groupId) {
         const existingGroup = groupedItemsMap.get(childProps.groupId) || []
         existingGroup.push([key, childProps])
         groupedItemsMap.set(childProps.groupId, existingGroup)
@@ -359,7 +352,7 @@ export const ActionBar: React.FC<React.PropsWithChildren<ActionBarProps>> = prop
   }, [childRegistry])
 
   return (
-    <ActionBarContext.Provider value={{size, registerChild, unregisterChild, isVisibleChild}}>
+    <ActionBarContext.Provider value={{size, isVisibleChild}}>
       <div ref={navRef} className={clsx(className, styles.Nav)} data-flush={flush}>
         <div
           ref={listRef}
@@ -369,7 +362,7 @@ export const ActionBar: React.FC<React.PropsWithChildren<ActionBarProps>> = prop
           aria-labelledby={ariaLabelledBy}
           data-gap={gap}
         >
-          {children}
+          <ActionBarItemsRegistry.Provider setRegistry={setChildRegistry}>{children}</ActionBarItemsRegistry.Provider>
           {menuItemIds.size > 0 && (
             <ActionMenu>
               <ActionMenu.Anchor>
@@ -466,24 +459,31 @@ export const ActionBar: React.FC<React.PropsWithChildren<ActionBarProps>> = prop
   )
 }
 
+function useWidth(ref: React.RefObject<HTMLElement | null>) {
+  // Storing the width in a ref ensures we don't forget about it when not visible
+  const widthRef = useRef<number>(-1)
+
+  useIsomorphicLayoutEffect(() => {
+    const width = ref.current?.getBoundingClientRect().width
+    if (width) widthRef.current = width
+  }, [])
+
+  return widthRef
+}
+
 export const ActionBarIconButton = forwardRef(
   ({disabled, onClick, ...props}: ActionBarIconButtonProps, forwardedRef) => {
     const ref = useRef<HTMLButtonElement>(null)
     useRefObjectAsForwardedRef(forwardedRef, ref)
     const id = useId()
 
-    const {size, registerChild, unregisterChild, isVisibleChild} = React.useContext(ActionBarContext)
+    const {size, isVisibleChild} = React.useContext(ActionBarContext)
     const {groupId} = React.useContext(ActionBarGroupContext)
 
-    // Storing the width in a ref ensures we don't forget about it when not visible
-    const widthRef = useRef<number>()
+    const widthRef = useWidth(ref)
 
-    useIsomorphicLayoutEffect(() => {
-      const width = ref.current?.getBoundingClientRect().width
-      if (width) widthRef.current = width
-      if (!widthRef.current) return
-
-      registerChild(id, {
+    const registryProps = useCallback(
+      (): ChildProps => ({
         type: 'action',
         label: props['aria-label'] ?? '',
         icon: props.icon,
@@ -491,12 +491,11 @@ export const ActionBarIconButton = forwardRef(
         onClick: onClick as MouseEventHandler,
         width: widthRef.current,
         groupId: groupId ?? undefined,
-      })
+      }),
+      [props, disabled, onClick, groupId, widthRef],
+    )
 
-      return () => {
-        unregisterChild(id)
-      }
-    }, [registerChild, unregisterChild, props['aria-label'], props.icon, disabled, onClick])
+    ActionBarItemsRegistry.useRegisterDescendantCallback(registryProps)
 
     const clickHandler = useCallback(
       (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -529,23 +528,12 @@ export const ActionBarGroup = forwardRef(({children}: React.PropsWithChildren, f
   const backupRef = useRef<HTMLDivElement>(null)
   const ref = (forwardedRef ?? backupRef) as RefObject<HTMLDivElement>
   const id = useId()
-  const {registerChild, unregisterChild} = React.useContext(ActionBarContext)
 
-  // Like IconButton, we store the width in a ref to ensure that we don't forget about it when not visible
-  // If a child has a groupId, it won't be visible if the group isn't visible, so we don't need to check isVisibleChild here
-  const widthRef = useRef<number>()
+  const widthRef = useWidth(ref)
 
-  useIsomorphicLayoutEffect(() => {
-    const width = ref.current?.getBoundingClientRect().width
-    if (width) widthRef.current = width
-    if (!widthRef.current) return
+  const registryProps = useCallback((): ChildProps => ({type: 'group', width: widthRef.current}), [widthRef])
 
-    registerChild(id, {type: 'group', width: widthRef.current})
-
-    return () => {
-      unregisterChild(id)
-    }
-  }, [registerChild, unregisterChild])
+  ActionBarItemsRegistry.useRegisterDescendantCallback(registryProps)
 
   return (
     <ActionBarGroupContext.Provider value={{groupId: id}}>
@@ -564,32 +552,25 @@ export const ActionBarMenu = forwardRef(
     const backupRef = useRef<HTMLButtonElement>(null)
     const ref = (forwardedRef ?? backupRef) as RefObject<HTMLButtonElement>
     const id = useId()
-    const {registerChild, unregisterChild, isVisibleChild} = React.useContext(ActionBarContext)
+    const {isVisibleChild} = React.useContext(ActionBarContext)
 
     const [menuOpen, setMenuOpen] = useState(false)
 
-    // Like IconButton, we store the width in a ref to ensure that we don't forget about it when not visible
-    const widthRef = useRef<number>()
+    const widthRef = useWidth(ref)
 
-    useIsomorphicLayoutEffect(() => {
-      const width = ref.current?.getBoundingClientRect().width
-      if (width) widthRef.current = width
-
-      if (!widthRef.current) return
-
-      registerChild(id, {
+    const registryProps = useCallback(
+      (): ChildProps => ({
         type: 'menu',
         width: widthRef.current,
         label: ariaLabel,
         icon: overflowIcon ? overflowIcon : icon,
         returnFocusRef,
         items,
-      })
+      }),
+      [ariaLabel, overflowIcon, icon, items, returnFocusRef, widthRef],
+    )
 
-      return () => {
-        unregisterChild(id)
-      }
-    }, [registerChild, unregisterChild, ariaLabel, overflowIcon, icon, items])
+    ActionBarItemsRegistry.useRegisterDescendantCallback(registryProps)
 
     if (!isVisibleChild(id)) return null
 
@@ -609,20 +590,13 @@ export const ActionBarMenu = forwardRef(
 export const VerticalDivider = () => {
   const ref = useRef<HTMLDivElement>(null)
   const id = useId()
-  const {registerChild, unregisterChild, isVisibleChild} = React.useContext(ActionBarContext)
+  const {isVisibleChild} = React.useContext(ActionBarContext)
 
-  // Storing the width in a ref ensures we don't forget about it when not visible
-  const widthRef = useRef<number>()
+  const widthRef = useWidth(ref)
 
-  useIsomorphicLayoutEffect(() => {
-    const width = ref.current?.getBoundingClientRect().width
-    if (width) widthRef.current = width
-    if (!widthRef.current) return
+  const registryProps = useCallback((): ChildProps => ({type: 'divider', width: widthRef.current}), [widthRef])
 
-    registerChild(id, {type: 'divider', width: widthRef.current})
-
-    return () => unregisterChild(id)
-  }, [registerChild, unregisterChild])
+  ActionBarItemsRegistry.useRegisterDescendantCallback(registryProps)
 
   if (!isVisibleChild(id)) return null
   return <div ref={ref} data-component="ActionBar.VerticalDivider" aria-hidden="true" className={styles.Divider} />
---
+++ packages/react/src/utils/descendant-registry.tsx
@@ -0,0 +1,127 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useId,
+  useMemo,
+  useRef,
+  useState,
+  type Dispatch,
+  type ReactNode,
+} from 'react'
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect'
+
+interface ProviderProps<T> {
+  children: ReactNode
+  /** State setter from `useRegistryState`. */
+  setRegistry: Dispatch<React.SetStateAction<Map<string, T>>>
+}
+
+/**
+ * Create a "descendant registry" for a component. This allows a parent to store and track an ordered registry of
+ * child components, even if they are deeply nested in the tree. For example, a menu component can use this to track
+ * every menu item inside of it, including menu items inside of fragments and subcomponents. The order of the resulting
+ * `Map` will match the render order in the React tree.
+ *
+ * Usage:
+ * 1. Create a registry at the file level in a similar manner to creating a new React context. Set `T` to the data type
+ *    of the registry - this can be anything **except** a function.
+ * 2. In the parent component, instantiate the registry state via `useRegistryState`. Then wrap `children` in the
+ *    registry context `Provider` with `setRegistry` set to the state setter function.
+ * 3. Register child components via `useRegisterDescendant` or `useRegisterDescendantCallback`.
+ * 4. Access the registered data using the value from `useRegistryState`. This will be a map of `string` to `T`, where
+ *    the string key is a unique and stable identifier for each component which can be used as a `key` if necessary.
+ */
+export function createDescendantRegistry<T>() {
+  const Context = createContext<{
+    register: (id: string, value: T) => () => void
+    key: number
+  }>({
+    register: () => () => {},
+    key: -1,
+  })
+
+  /** Instantiate descendant registry state. */
+  function useRegistryState() {
+    // We could do this inside of `Provider`, but then it would be difficult for the parent itself to access the state value
+    return useState(() => new Map<string, T>())
+  }
+
+  /**
+   * Register a descendant component with the registry, using a callback to get the data. Use this instead of
+   * `useRegisterDescendant` when you need to access a ref value or do some calculation to get the data that you want
+   * to register. The callback will be called during the effect phase. Ensure the callback is stable (`useCallback`).
+   */
+  function useRegisterDescendantCallback(valueCallback: () => T) {
+    const {register, key} = useContext(Context)
+
+    const id = useId()
+
+    useEffect(() => register(id, valueCallback()), [register, id, valueCallback, key])
+  }
+
+  /** Simplified version of `useRegisterDescendantCallback` that allows directly passing a memoized value. */
+  function useRegisterDescendant(value: T) {
+    const valueCallback = useCallback(() => value, [value])
+
+    useRegisterDescendantCallback(valueCallback)
+  }
+
+  /** Provide context for registering descendant components. This only needs to wrap `children`. */
+  function Provider({children, setRegistry}: ProviderProps<T>) {
+    const workingRegistryRef = useRef<Map<string, T> | 'queued' | 'idle'>('queued')
+
+    /** State value to trigger a re-render and force all descendants to re-register. This ensures everything remains ordered. */
+    const [key, setKey] = useState(0)
+
+    // Instantiate a new map before all descendants' effects run to populate it
+    useIsomorphicLayoutEffect(function instantiateNewRegistry() {
+      if (workingRegistryRef.current === 'queued') {
+        workingRegistryRef.current = new Map<string, T>()
+      }
+    })
+
+    // Each descendant will register itself. Initially this will just update the map. After the initial render and
+    // commit, any descendant updates (mounts, unmounts, or value changes) will trigger a full recalculation of the
+    // registry. This is necessary because a descendant might mount into the middle of the tree, and the only way for
+    // the provider to know that is to re-render the tree and listen to every descendant effect in order.
+    const register = useCallback((id: string, value: T) => {
+      function queueRebuild() {
+        // If we instantiate a new map now, we'd commit it in the ensuing provider effect, which would trigger a
+        // new state update and cause an infinite render loop. So we wait to instantiate it until the next layout effect,
+        // which we know will happen because we update state to queue a new render.
+        workingRegistryRef.current = 'queued'
+        setKey(prev => prev + 1)
+      }
+
+      if (workingRegistryRef.current instanceof Map) {
+        workingRegistryRef.current.set(id, value)
+      } else if (workingRegistryRef.current === 'idle') {
+        queueRebuild()
+      }
+
+      return () => {
+        if (workingRegistryRef.current instanceof Map) {
+          workingRegistryRef.current.delete(id)
+        } else if (workingRegistryRef.current === 'idle') {
+          queueRebuild()
+        }
+      }
+    }, [])
+
+    // After all descendants' effects complete, commit the working registry to state
+    useEffect(function commitWorkingRegistry() {
+      if (workingRegistryRef.current instanceof Map) {
+        setRegistry(workingRegistryRef.current)
+        workingRegistryRef.current = 'idle'
+      }
+    })
+
+    const contextValue = useMemo(() => ({register, key}), [register, key])
+
+    return <Context.Provider value={contextValue}>{children}</Context.Provider>
+  }
+
+  return {Provider, useRegistryState, useRegisterDescendant, useRegisterDescendantCallback}
+}
