3e7feffff52aedc850eb8c470e1571dc0697b696
Marie Lucca <francinelucca@github.com>
2026-04-17 00:42:23 -0400

    fix

--- packages/react/src/experimental/index.ts
+++ packages/react/src/experimental/index.ts
@@ -98,8 +98,8 @@ export type {SkeletonAvatarProps} from '../SkeletonAvatar'
 export {FeatureFlags, DefaultFeatureFlags, useFeatureFlag} from '../FeatureFlags'
 export type {FeatureFlagsProps} from '../FeatureFlags'
 
-export {FilteredActionList} from '../FilteredActionList'
-export type {FilteredActionListProps, FilteredActionListLoadingTypes} from '../FilteredActionList'
+export {FilteredActionList, FilteredActionListLoadingTypes} from '../FilteredActionList'
+export type {FilteredActionListProps} from '../FilteredActionList'
 export {IssueLabel} from './IssueLabel'
 export type {IssueLabelProps} from './IssueLabel'
 
