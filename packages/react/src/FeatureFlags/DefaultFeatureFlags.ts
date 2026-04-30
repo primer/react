ae8f6782a7846e9d388ffb70530d577a75c7b234
Marie Lucca <francinelucca@github.com>
2026-04-29 23:50:53 -0400

    chore: forward @primer/react theming from @primer/styled-react under FF

--- packages/react/src/FeatureFlags/DefaultFeatureFlags.ts
+++ packages/react/src/FeatureFlags/DefaultFeatureFlags.ts
@@ -4,4 +4,5 @@ export const DefaultFeatureFlags = FeatureFlagScope.create({
   primer_react_css_anchor_positioning: false,
   primer_react_select_panel_fullscreen_on_narrow: false,
   primer_react_select_panel_order_selected_at_top: false,
+  primer_react_styled_react_use_primer_theme_providers: false,
 })
