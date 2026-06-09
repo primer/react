c7a6da3d237dd91eb67392571691e2fb4407b9d3
mattobee <3172945+mattobee@users.noreply.github.com>
2026-06-09 12:30:00 +0000

    chore: auto-fix lint and formatting issues

--- packages/react/src/ActionList/GroupHeadingLeadingVisual.tsx
+++ packages/react/src/ActionList/GroupHeadingLeadingVisual.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import type React from 'react'
 import {clsx} from 'clsx'
 import groupClasses from './Group.module.css'
 import type {FCWithSlotMarker} from '../utils/types/Slots'
