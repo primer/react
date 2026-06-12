aea6d6812052903b151ca4fb3b4fb3188305c199
Josh Black <joshblack@github.com>
2026-06-12 11:17:04 -0500

    Use valid className test fixtures

--- packages/react/src/SegmentedControl/SegmentedControl.test.tsx
+++ packages/react/src/SegmentedControl/SegmentedControl.test.tsx
@@ -32,7 +32,7 @@ const segmentData = [
 ]
 
 describe('SegmentedControl', () => {
-  implementsClassName(SegmentedControl, classes.SegmentedControl)
+  implementsClassName(props => <SegmentedControl aria-label="File view" {...props} />, classes.SegmentedControl)
 
   it('renders with a selected segment - controlled', () => {
     const {getByText} = render(
