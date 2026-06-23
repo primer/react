a858176ae008f5505389c68bc48a77e6f54161fb
Josh Black <joshblack@github.com>
2026-06-23 16:49:37 -0500

    Use Rolldown DTS plugin for doc-gen declarations

--- packages/doc-gen/rolldown.config.js
+++ packages/doc-gen/rolldown.config.js
@@ -1,4 +1,5 @@
 import {defineConfig} from 'rolldown'
+import {dts} from 'rolldown-plugin-dts'
 import packageJson from './package.json' with {type: 'json'}
 
 const dependencies = [
@@ -11,12 +12,30 @@ function createPackageRegex(name) {
   return new RegExp(`^${name}(/.*)?`)
 }
 
-export default defineConfig({
-  input: './src/index.ts',
-  platform: 'node',
-  external: dependencies.map(createPackageRegex),
-  output: {
-    dir: 'dist',
-    format: 'esm',
+export default defineConfig([
+  {
+    input: './src/index.ts',
+    platform: 'node',
+    external: dependencies.map(createPackageRegex),
+    output: {
+      dir: 'dist',
+      format: 'esm',
+    },
   },
-})
+  {
+    input: './src/index.ts',
+    platform: 'node',
+    external: dependencies.map(createPackageRegex),
+    plugins: [
+      dts({
+        emitDtsOnly: true,
+        sourcemap: true,
+        tsconfig: './tsconfig.build.json',
+      }),
+    ],
+    output: {
+      dir: 'dist',
+      format: 'esm',
+    },
+  },
+])
