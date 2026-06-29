f0c84223abac450d46999520bf400fe1bb427379
Josh Black <joshblack@github.com>
2026-06-29 12:32:59 -0500

    refactor: update build config for @primer/doc-gen to rolldown (#8041)

---
+++ packages/doc-gen/rolldown.config.js
@@ -0,0 +1,41 @@
+import {defineConfig} from 'rolldown'
+import {dts} from 'rolldown-plugin-dts'
+import packageJson from './package.json' with {type: 'json'}
+
+const dependencies = [
+  ...Object.keys(packageJson.peerDependencies ?? {}),
+  ...Object.keys(packageJson.dependencies ?? {}),
+  ...Object.keys(packageJson.devDependencies ?? {}),
+]
+
+function createPackageRegex(name) {
+  return new RegExp(`^${name}(/.*)?`)
+}
+
+export default defineConfig([
+  {
+    input: './src/index.ts',
+    platform: 'node',
+    external: dependencies.map(createPackageRegex),
+    output: {
+      dir: 'dist',
+      format: 'esm',
+    },
+  },
+  {
+    input: './src/index.ts',
+    platform: 'node',
+    external: dependencies.map(createPackageRegex),
+    plugins: [
+      dts({
+        emitDtsOnly: true,
+        sourcemap: true,
+        tsconfig: './tsconfig.build.json',
+      }),
+    ],
+    output: {
+      dir: 'dist',
+      format: 'esm',
+    },
+  },
+])
