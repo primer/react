8ab63ed24d507f2015b3d5fc8a0514d46586f386
Liu Liu <liuliu-dev@github.com>
2026-08-21 10:46:44 -0700

    type

--- packages/react/src/SegmentedControl/SegmentedControlAction.tsx
+++ packages/react/src/SegmentedControl/SegmentedControlAction.tsx
@@ -2,10 +2,11 @@ import {PlusIcon} from '@primer/octicons-react'
 import type {ElementType} from 'react'
 import {IconButton} from '../Button'
 import type {IconButtonProps} from '../Button'
+import type {DistributiveOmit} from '../utils/modern-polymorphic'
 import type {FCWithSlotMarker} from '../utils/types'
 import classes from './SegmentedControl.module.css'
 
-export type SegmentedControlActionProps = Omit<IconButtonProps, 'icon'> & {
+export type SegmentedControlActionProps = DistributiveOmit<IconButtonProps, 'icon'> & {
   icon?: ElementType
 }
 
